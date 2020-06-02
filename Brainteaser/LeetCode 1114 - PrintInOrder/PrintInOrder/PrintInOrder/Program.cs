0f68958413e8140f42870000cca0384a44b7521b
Xing <x.xing.unsw@gmail.com>
2020-06-02 19:19:37 +1000

    init

---
+++ Brainteaser/LeetCode 1114 - PrintInOrder/PrintInOrder/PrintInOrder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace PrintInOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Foo
+    {
+        private AutoResetEvent _second;
+        private AutoResetEvent _third;
+        public Foo()
+        {
+            _second = new AutoResetEvent(false);
+            _third = new AutoResetEvent(false);
+        }
+
+        public void First(Action printFirst)
+        {
+
+            printFirst();
+            _second.Set();
+        }
+
+        public void Second(Action printSecond)
+        {
+            _second.WaitOne();
+            printSecond();
+            _third.Set();
+        }
+
+        public void Third(Action printThird)
+        {
+            _third.WaitOne();
+            printThird();
+        }
+    }
+}
---
+++ Brainteaser/LeetCode 292 - NimGame/NimGame/NimGame/Program.cs
@@ -0,0 +1,18 @@
+﻿//可以拿到倒数第五块石头的肯定赢。所以只要n比4大就让n一直减4，但这种做法会超时，可以用n % 4代替。
+//在n % 4不为0的时候返回true，否则返回false。
+using System;
+
+namespace NimGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static bool CanWinNim(int n)
+        {
+            return n % 4 != 0;
+        }
+    }
+}
