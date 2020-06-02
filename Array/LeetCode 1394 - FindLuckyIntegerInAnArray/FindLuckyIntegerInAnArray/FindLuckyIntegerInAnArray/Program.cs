e952e2d64efd84b2d0ced5c53efc4d7f3583f55d
Xing <x.xing.unsw@gmail.com>
2020-06-02 18:56:56 +1000

    init

---
+++ Array/LeetCode 1394 - FindLuckyIntegerInAnArray/FindLuckyIntegerInAnArray/FindLuckyIntegerInAnArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FindLuckyIntegerInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int FindLucky(int[] arr)
+        {
+            var record = new int[501];
+            foreach (var num in arr)
+                record[num]++;
+            for (int i = record.Length - 1; i > 0; i--)
+                if (i == record[i])
+                    return i;
+            return -1;
+        }
+    }
+}
