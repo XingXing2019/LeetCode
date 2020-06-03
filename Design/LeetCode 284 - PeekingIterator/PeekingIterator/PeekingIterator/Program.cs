906d3114e6c73b5e9f3300e88874ec1984c2fbe2
Xing <x.xing.unsw@gmail.com>
2020-06-03 21:55:09 +1000

    init

---
+++ Design/LeetCode 284 - PeekingIterator/PeekingIterator/PeekingIterator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeekingIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class PeekingIterator
+    {
+        private IEnumerator<int> _iterator;
+        private bool _hasNext;
+        public PeekingIterator(IEnumerator<int> iterator)
+        {
+            _iterator = iterator;
+            _hasNext = true;
+        }
+
+        public int Peek()
+        {
+            return _iterator.Current;
+        }
+
+        public int Next()
+        {
+            var res = _iterator.Current;
+            _hasNext = _iterator.MoveNext();
+            return res;
+        }
+
+        public bool HasNext()
+        {
+            return _hasNext;
+        }
+    }
+}
---
+++ Design/LeetCode 706 - DesignHashMap/DesignHashMap/DesignHashMap/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DesignHashMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class MyHashMap
+    {
+        private int[] record;
+        /** Initialize your data structure here. */
+        public MyHashMap()
+        {
+            record = new int[1000001];
+            for (int i = 0; i < record.Length; i++)
+                record[i] = -1;
+        }
+
+        /** value will always be non-negative. */
+        public void Put(int key, int value)
+        {
+            record[key] = value;
+        }
+
+        /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+        public int Get(int key)
+        {
+            return record[key];
+        }
+
+        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+        public void Remove(int key)
+        {
+            record[key] = -1;
+        }
+    }
+}
