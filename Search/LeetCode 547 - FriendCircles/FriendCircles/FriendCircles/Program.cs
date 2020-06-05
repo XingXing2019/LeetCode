fa7453b552ca6afe2899a32c0d5a3e126873f172
Xing <x.xing.unsw@gmail.com>
2020-06-05 20:51:37 +1000

    init

---
+++ Search/LeetCode 547 - FriendCircles/FriendCircles/FriendCircles/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FriendCircles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int FindCircleNum(int[][] M)
+        {
+            int number = M.Length;
+            var graph = new HashSet<int>[number];
+            for (int i = 0; i < number; i++)
+                graph[i] = new HashSet<int>();
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (M[i][j] == 1)
+                    {
+                        graph[i].Add(j);
+                        graph[j].Add(i);
+                    }
+                }
+            }
+            var visit = new bool[number];
+            var count = 0;
+            var queue = new Queue<int>();
+            for (int i = 0; i < number; i++)
+            {
+                if (visit[i]) continue;
+                queue.Enqueue(i);
+                visit[i] = true;
+                while (queue.Count != 0)
+                {
+                    var cur = queue.Dequeue();
+                    foreach (var next in graph[cur])
+                    {
+                        if (visit[next]) continue;
+                        queue.Enqueue(next);
+                        visit[next] = true;
+                    }
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+}
