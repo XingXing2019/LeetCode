c604fe6f2f8d5a2f5ef291942d969650f391d668
Xing <x.xing.unsw@gmail.com>
2020-06-07 14:50:38 +1000

    init

---
+++ Array/LeetCode 1470 - ShuffleTheArray/ShuffleTheArray/ShuffleTheArray/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace ShuffleTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] Shuffle(int[] nums, int n)
+        {
+            var res = new int[2 * n];
+            int point = 0;
+            for (int i = 0; i < n; i++)
+            {
+                res[point++] = nums[i];
+                res[point++] = nums[i + n];
+            }
+            return res;
+        }
+    }
+}
