9e927dd184ce99a58fc6f81f3ee7e759c6ad2af1
Xing <x.xing.unsw@gmail.com>
2020-06-08 22:38:05 +1000

    init

---
+++ Tree/LeetCode 572 - SubtreeOfAnotherTree/SubtreeOfAnotherTree/SubtreeOfAnotherTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SubtreeOfAnotherTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsSubtree(TreeNode s, TreeNode t)
+        {
+            if (s == null) return false;
+            if (IsSameTree(s, t)) return true;
+            return IsSubtree(s.left, t) || IsSubtree(s.right, t);
+        }
+
+        private bool IsSameTree(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null) return true;
+            if (s == null || t == null) return false;
+            if (s.val != t.val) return false;
+            return IsSameTree(s.left, t.left) && IsSameTree(s.right, t.right);
+        }
+    }
+}
