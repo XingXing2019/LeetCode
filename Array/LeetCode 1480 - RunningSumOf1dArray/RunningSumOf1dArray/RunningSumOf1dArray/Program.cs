a62d8760fbff85fc5f871e5b4485f1380d584841
Xing <x.xing.unsw@gmail.com>
2020-06-14 14:34:17 +1000

    init

---
+++ Array/LeetCode 1480 - RunningSumOf1dArray/RunningSumOf1dArray/RunningSumOf1dArray/Program.cs
@@ -0,0 +1,19 @@
+﻿//直接在原数组上操作。一路加过去。
+using System;
+
+namespace RunningSumOf1dArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+                nums[i] += nums[i - 1];
+            return nums;
+        }
+    }
+}
