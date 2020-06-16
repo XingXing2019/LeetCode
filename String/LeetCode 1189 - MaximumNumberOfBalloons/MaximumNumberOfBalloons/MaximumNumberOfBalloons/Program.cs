39629df618b60820c7b7b02ff03848b6e6a078b4
Xing <x.xing.unsw@gmail.com>
2020-06-16 15:19:19 +1000

    simplified code

--- String/LeetCode 1189 - MaximumNumberOfBalloons/MaximumNumberOfBalloons/MaximumNumberOfBalloons/Program.cs
+++ String/LeetCode 1189 - MaximumNumberOfBalloons/MaximumNumberOfBalloons/MaximumNumberOfBalloons/Program.cs
@@ -1,4 +1,4 @@
-﻿//创建字典balloons，遍历字符串将每个字母的个数记录入balloons。
+﻿//创建数组balloons，遍历字符串将每个字母的个数记录入balloons。
 //遍历字典中的每个键值对。如果key为'l'或'o'，则用num记录该字母的个数，并对其整除2(因为每个balloon单词需要两个'l'和'o')。
 //遍历过程中记录最小的num，并在遍历结束后返回。
 using System;
@@ -10,46 +10,23 @@ namespace MaximumNumberOfBalloons
     {
         static void Main(string[] args)
         {
-            string text = "";
+            string text = "nlaebolko";
             Console.WriteLine(MaxNumberOfBalloons(text));
         }
         static int MaxNumberOfBalloons(string text)
         {
-            Dictionary<char, int> balloons = new Dictionary<char, int>();
-            balloons.Add('b', 0);
-            balloons.Add('a', 0);
-            balloons.Add('l', 0);
-            balloons.Add('o', 0);
-            balloons.Add('n', 0);
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (text[i] == 'b')
-                    balloons['b']++;
-                else if (text[i] == 'a')
-                    balloons['a']++;
-                else if (text[i] == 'l')
-                    balloons['l']++;
-                else if (text[i] == 'o')
-                    balloons['o']++;
-                else if (text[i] == 'n')
-                    balloons['n']++;
-            }
+            var balloons = new int[26];
+            foreach (var letter in text)
+                balloons[letter - 'a']++;
             int count = int.MaxValue;
-            int num = 0;
-            foreach (var keyValuePair in balloons)
+            for (int i = 0; i < balloons.Length; i++)
             {
-                if (keyValuePair.Key == 'l' || keyValuePair.Key == 'o')
-                {
-                    num = keyValuePair.Value;
-                    num /= 2;
-                }
-                else
-                    num = keyValuePair.Value;
-                if (num < count)
-                    count = num;
+                if (i == 'l' - 'a' || i == 'o' - 'a')
+                    count = Math.Min(count, balloons[i] / 2);
+                else if (i == 'a' - 'a' || i == 'b' - 'a' || i == 'n' - 'a')
+                    count = Math.Min(count, balloons[i]);
             }
             return count;
         }
-
     }
 }
