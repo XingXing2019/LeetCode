c46b9c52b285e24eb64d9ebcd4fb0b00253f0eaf
Xing <x.xing.unsw@gmail.com>
2020-06-20 20:57:17 +1000

    Add recursion

--- LinkedList/LeetCode 21 - ZipTwoLists/ZipTwoLists/ZipTwoLists/Program.cs
+++ LinkedList/LeetCode 21 - ZipTwoLists/ZipTwoLists/ZipTwoLists/Program.cs
@@ -36,5 +36,17 @@ namespace ZipTwoLists
             point.next = l1 ?? l2;
             return res.next;
         }
+        public ListNode MergeTwoLists_Recursion(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            if (l1.val < l2.val)
+            {
+                l1.next = MergeTwoLists_Recursion(l1.next, l2);
+                return l1;
+            }
+            l2.next = MergeTwoLists_Recursion(l1, l2.next);
+            return l2;
+        }
     }
 }
