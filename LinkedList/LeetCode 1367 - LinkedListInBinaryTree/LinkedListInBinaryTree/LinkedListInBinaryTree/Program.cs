c7037d7c8ad17ecae3f9a98cd24fae0a05b3a12a
Xing <x.xing.unsw@gmail.com>
2020-06-20 18:14:47 +1000

    init

--- LinkedList/LeetCode 1367 - LinkedListInBinaryTree/LinkedListInBinaryTree/LinkedListInBinaryTree/Program.cs
+++ LinkedList/LeetCode 1367 - LinkedListInBinaryTree/LinkedListInBinaryTree/LinkedListInBinaryTree/Program.cs
@@ -22,32 +22,7 @@ namespace LinkedListInBinaryTree
     {
         static void Main(string[] args)
         {
-            int[] nums = {4, 2, 8};
-            var head = Generate(nums);
-
-            var a = new TreeNode(1);
-            var b = new TreeNode(4);
-            var c = new TreeNode(4);
-            var d = new TreeNode(2);
-            var e = new TreeNode(2);
-            var f = new TreeNode(1);
-            var g = new TreeNode(6);
-            var h = new TreeNode(8);
-            var i = new TreeNode(1);
-            var j = new TreeNode(3);
-
-            a.left = b;
-            a.right = c;
-            b.right = d;
-            d.left = f;
-            c.left = e;
-            e.left = g;
-            e.right = h;
-            h.left = i;
-            h.right = j;
-
-            Console.WriteLine(IsSubPath(head, a));
-
+           
         }
         public bool IsSubPath(ListNode head, TreeNode root)
         {
@@ -62,5 +37,6 @@ namespace LinkedListInBinaryTree
             if (root == null) return false;
             return head.val == root.val && (DFS(head.next, root.left) || DFS(head.next, root.right));
         }
+
     }
 }
