e0f2e0ff3c5f846d32583f4890771d1c8b0b9c5e
Xing <x.xing.unsw@gmail.com>
2020-06-21 21:50:36 +1000

    init

---
+++ HashTable/LeetCode 1488 - AvoidFloodInTheCity/AvoidFloodInTheCity/AvoidFloodInTheCity/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvoidFloodInTheCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rains = { 1, 2, 0, 2, 3, 0, 1 };
+            Console.WriteLine(AvoidFlood(rains));
+        }
+        static int[] AvoidFlood(int[] rains)
+        {
+            var lakes = new Dictionary<int, int>();
+            var result = new int[rains.Length];
+            var noRainDays = new List<int>();
+            for (int i = rains.Length - 1; i >= 0; i--)
+            {
+                if (rains[i] != 0)
+                {
+                    if (lakes.ContainsKey(rains[i]))
+                    {
+                        bool found = false;
+                        for (int j = 0; j < noRainDays.Count; j++)
+                        {
+                            if (lakes[rains[i]] > noRainDays[j])
+                            {
+                                result[noRainDays[j]] = rains[i];
+                                noRainDays.RemoveAt(j);
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found) return new int[0];
+                    }
+                    result[i] = -1;
+                    lakes[rains[i]] = i;
+                }
+                else
+                    noRainDays.Add(i);
+            }
+            for (int j = 0; j < noRainDays.Count; j++)
+                result[noRainDays[j]] = 1;
+            return result;
+        }
+    }
+}
