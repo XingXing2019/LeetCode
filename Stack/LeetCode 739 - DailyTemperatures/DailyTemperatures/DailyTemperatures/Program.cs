f8697f7c1c335054ac3c0d058014e72248abcd23
Xing <x.xing.unsw@gmail.com>
2020-06-23 17:40:24 +1000

    init

--- Stack/LeetCode 739 - DailyTemperatures/DailyTemperatures/DailyTemperatures/Program.cs
+++ Stack/LeetCode 739 - DailyTemperatures/DailyTemperatures/DailyTemperatures/Program.cs
@@ -1,10 +1,8 @@
-﻿//解题关键为将日期压入栈，而不是将温度压入栈。利用压入栈的日期搜寻Temperature数组中代表的温度从而进行比较。
-//创建一个数组res保存结果，初始状态全部刷成0。创建栈用于存储温度所对应的的日期。
-//当栈不为空，并且当前遍历到的温度比栈顶日期所对应的的温度高时，设置index保存栈顶日期，并将栈顶弹出。
-//更新res中index所对应的日期的值为当前日期day与index的差值。
-//将当前日期day压入栈。
+﻿//使用单调栈，将T中每个数字的index压栈。在当前数字比栈顶大的时候就一直弹栈，并记录结果。
+//暴力求解，不会超时，但速度很慢。
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace DailyTemperatures
 {
@@ -15,19 +13,35 @@ namespace DailyTemperatures
             int[] T = { 73, 74, 75, 71, 69, 72, 76, 73 };
             DailyTemperatures(T);
         }
-        static int[] DailyTemperatures(int[] T)
+        static int[] DailyTemperatures_MonoStack(int[] T)
         {
             int[] res = new int[T.Length];
-            Stack temDayStack = new Stack();
-            for (int day = 0; day < T.Length; day++)
+            var stack = new Stack<int>();
+            for (int i = 0; i < T.Length; i++)
             {
-                while (temDayStack.Count != 0 && T[day] > T[(int)temDayStack.Peek()])
+                while (stack.Count != 0 && T[stack.Peek()] < T[i])
                 {
-                    int index = (int)temDayStack.Peek();
-                    temDayStack.Pop();
-                    res[index] = day - index;
+                    var index = stack.Pop();
+                    res[index] = i - index;
                 }
-                temDayStack.Push(day);
+                stack.Push(i);
+            }
+            return res;
+        }
+
+        static int[] DailyTemperatures_BruteForce(int[] T)
+        {
+            int[] res = new int[T.Length];
+            for (int i = 0; i < T.Length; i++)
+            {
+                for (int j = i + 1; j < T.Length; j++)
+                {
+                    if (T[j] > T[i])
+                    {
+                        res[i] = j - i;
+                        break;
+                    }
+                }
             }
             return res;
         }
