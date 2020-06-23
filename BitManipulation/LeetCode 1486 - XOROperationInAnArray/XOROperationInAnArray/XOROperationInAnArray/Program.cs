44976bc5eaf766506096624914cc78a5debf5ddc
Xing <x.xing.unsw@gmail.com>
2020-06-23 18:02:45 +1000

    init

---
+++ BitManipulation/LeetCode 1486 - XOROperationInAnArray/XOROperationInAnArray/XOROperationInAnArray/Program.cs
@@ -0,0 +1,23 @@
+﻿//按照题目要求，随便做做就好了。
+using System;
+
+namespace XOROperationInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int XorOperation(int n, int start)
+        {
+            int res = start;
+            for (int i = 1; i < n; i++)
+            {
+                int temp = start + 2 * i;
+                res ^= temp;
+            }
+            return res;
+        }
+    }
+}
