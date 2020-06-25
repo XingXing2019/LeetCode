0e7710c58ef21bcc70559c7b2683d487d077baf8
Xing <x.xing.unsw@gmail.com>
2020-06-25 18:14:27 +1000

    one more solution

--- TwoPointers/LeetCode 287 - FindTheDuplicateNumber/FindTheDuplicateNumber/FindTheDuplicateNumber/Program.cs
+++ TwoPointers/LeetCode 287 - FindTheDuplicateNumber/FindTheDuplicateNumber/FindTheDuplicateNumber/Program.cs
@@ -1,6 +1,6 @@
-﻿//遍历数组，将遍历到的数字存入列表，如果当前遍历到的数字已经在列表中，则该数字为重复数字。
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace FindTheDuplicateNumber
@@ -10,11 +10,11 @@ namespace FindTheDuplicateNumber
         static void Main(string[] args)
         {
             int[] nums = { 1, 3, 4, 2, 2 };
-            Console.WriteLine(FindDuplicate2(nums));
+            Console.WriteLine(FindDuplicate_BinarySearch(nums));
         }
         
         //Two points O(n)
-        static int FindDuplicate1(int[] nums)
+        static int FindDuplicate_FloydTortoiseHare(int[] nums)
         {
             int slow = nums[0];
             int fast = nums[nums[0]];
@@ -32,17 +32,24 @@ namespace FindTheDuplicateNumber
             return fast;
         }
 
-        //Binary search O(n * log(n))
-        static int FindDuplicate2(int[] nums)
+        static int FindDuplicate_Sort(int[] nums)
         {
-            int li = 1, hi = nums.Length - 1;
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1])
+                    return nums[i];
+            }
+            return -1;
+        }
+
+        static int FindDuplicate_BinarySearch(int[] nums)
+        {
+            int li = 1, hi = nums.Length;
             while (li < hi)
             {
                 int mid = li + (hi - li) / 2;
-                int count = 0;
-                foreach (var num in nums)
-                    if (num <= mid)
-                        count++;
+                int count = nums.Count(x => x <= mid);
                 if (count > mid)
                     hi = mid;
                 else
@@ -51,8 +58,7 @@ namespace FindTheDuplicateNumber
             return li;
         }
 
-        //Hash Table
-        static int FindDuplicate3(int[] nums)
+        static int FindDuplicate_HashTable(int[] nums)
         {
             var record = new HashSet<int>();
             foreach (var num in nums)
