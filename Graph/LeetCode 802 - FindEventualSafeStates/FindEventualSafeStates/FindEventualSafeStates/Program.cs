2b0373962ea20926eec78076ea66773fbb5714e0
Xing <x.xing.unsw@gmail.com>
2020-06-27 21:33:06 +1000

    init

--- Graph/LeetCode 802 - FindEventualSafeStates/FindEventualSafeStates/FindEventualSafeStates/Program.cs
+++ Graph/LeetCode 802 - FindEventualSafeStates/FindEventualSafeStates/FindEventualSafeStates/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//创建入度表和出度表，根据graph填写入度表和出度表。出度表为有几条路径从当前节点出发。入度表为有那几个节点指向当前节点。
+//将出度为0的节点入队，并加入结果。
+//在队不为空的条件下循环，弹出对头，将它入度表的所有节点的出度减一。如果该节点出度为0，则将其加入结果并入队。
+//最后给结果排序。
+using System;
 using System.Collections.Generic;
 
 namespace FindEventualSafeStates
@@ -9,43 +13,52 @@ namespace FindEventualSafeStates
         {
             int[][] graph = new int[][]
             {
+                new int[]{1, 2}, 
+                new int[]{2, 3}, 
+                new int[]{5}, 
                 new int[]{0}, 
-                new int[]{2, 3, 4}, 
-                new int[]{3, 4}, 
-                new int[]{0, 4}, 
+                new int[]{5}, 
                 new int[]{}, 
+                new int[]{} 
             };
             Console.WriteLine(EventualSafeNodes(graph));
         }
         static IList<int> EventualSafeNodes(int[][] graph)
         {
             var res = new List<int>();
+            var outDegree = new int[graph.Length];
+            var inDegree = new List<int>[graph.Length];
+            for (int i = 0; i < inDegree.Length; i++)
+                inDegree[i] = new List<int>();
             for (int i = 0; i < graph.Length; i++)
             {
-                var queue = new Queue<int>();
-                var visit = new bool[graph.Length];
-                queue.Enqueue(i);
-                visit[i] = true;
-                var safe = true;
-                while (queue.Count != 0)
-                {
-                    var cur = queue.Dequeue();
-                    foreach (var next in graph[cur])
-                    {
-                        if (next == i)
-                        {
-                            safe = false;
-                            break;
-                        }
-                        if(visit[next]) continue;
-                        queue.Enqueue(next);
-                        visit[next] = true;
-                    }
-                    if(!safe) break;
-                }
-                if(safe)
-                    res.Add(i);
+                outDegree[i] += graph[i].Length;
+                foreach (var edge in graph[i])
+                    inDegree[edge].Add(i);
             }
+            var queue = new Queue<int>();
+            for (int i = 0; i < inDegree.Length; i++)
+            {
+                if (outDegree[i] == 0)
+                {
+                    queue.Enqueue(i);
+                    res.Add(i);
+                }
+            }
+            while (queue.Count != 0)
+            {
+                var cur = queue.Dequeue();
+                foreach (var next in inDegree[cur])
+                {
+                    outDegree[next]--;
+                    if (outDegree[next] == 0)
+                    {
+                        queue.Enqueue(next);
+                        res.Add(next);
+                    }
+                }
+            }
+            res.Sort();
             return res;
         }
     }
