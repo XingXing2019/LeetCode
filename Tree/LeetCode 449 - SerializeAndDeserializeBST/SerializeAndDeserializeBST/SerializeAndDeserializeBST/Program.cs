cc1b0278fe5a9f406ffde1cc89743c84067d669b
Xing <x.xing.unsw@gmail.com>
2020-07-02 20:27:13 +1000

    init

--- Tree/LeetCode 449 - SerializeAndDeserializeBST/SerializeAndDeserializeBST/SerializeAndDeserializeBST/Program.cs
+++ Tree/LeetCode 449 - SerializeAndDeserializeBST/SerializeAndDeserializeBST/SerializeAndDeserializeBST/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Net.Mail;
 
 namespace SerializeAndDeserializeBST
 {
@@ -22,7 +20,13 @@ namespace SerializeAndDeserializeBST
             //a.right = c;
             var codec = new Codec();
             var data = codec.serialize(a);
-            var node = codec.deserialize(data);
+
+            int[] nums =
+            {
+                41, 37, 24, 1, 0, 2, 4, 3, 9, 7, 6, 5, 8, 11, 10, 16, 15, 12, 13, 14, 19, 18, 17, 20, 22, 21, 23, 35,
+                30, 29, 26, 25, 27, 28, 32, 31, 34, 33, 36, 39, 38, 40, 44, 42, 43, 48, 46, 45, 47, 49
+            };
+            var node = codec.Build(nums);
         }
     }
 
@@ -38,7 +42,7 @@ namespace SerializeAndDeserializeBST
         public string PreOrder(TreeNode node)
         {
             if (node == null) return "";
-            var preOrder = node.val + "-";
+            var preOrder = node.val + "*";
             preOrder += PreOrder(node.left);
             preOrder += PreOrder(node.right);
             return preOrder;
@@ -47,7 +51,8 @@ namespace SerializeAndDeserializeBST
         // Decodes your encoded data to tree.
         public TreeNode deserialize(string data)
         {
-            var nodes = data.Split("-", StringSplitOptions.RemoveEmptyEntries);
+            if(data.Length == 2) return new TreeNode(int.Parse(data.Substring(0, 1)));
+            var nodes = data.Split("*", StringSplitOptions.RemoveEmptyEntries);
             var preOrder = new int[nodes.Length];
             for (int i = 0; i < nodes.Length; i++)
                 preOrder[i] = int.Parse(nodes[i]);
@@ -57,8 +62,15 @@ namespace SerializeAndDeserializeBST
         public TreeNode Build(int[] preOrder)
         {
             if (preOrder.Length == 0) return null;
-            int firstLarger = preOrder.FirstOrDefault(x => x > preOrder[0]);
-            int index = Array.IndexOf(preOrder, firstLarger);
+            int index = -1;
+            for (int i = 0; i < preOrder.Length; i++)
+            {
+                if (preOrder[i] > preOrder[0])
+                {
+                    index = i;
+                    break;
+                }
+            }
             var left = index == -1 ? preOrder[1..] : preOrder[1..index];
             var right = index == -1 ? new int[0] : preOrder[index..];
             return new TreeNode(preOrder[0]) {left = Build(left), right = Build(right)};
