8caeee77cba2038e53a7f0479082b0644cc54197
Xing <x.xing.unsw@gmail.com>
2020-07-05 15:47:58 +1000

    init

---
+++ Sort/LeetCode 1502 - CanMakeArithmeticProgressionFromSequence/CanMakeArithmeticProgression/CanMakeArithmeticProgression/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CanMakeArithmeticProgression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+            var diff = arr[1] - arr[0];
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i] - arr[i - 1] != diff)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
