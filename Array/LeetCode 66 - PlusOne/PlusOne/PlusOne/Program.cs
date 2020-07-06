382a9c19a2e7a46850237b35bccd2e1925218cbf
Xing <x.xing.unsw@gmail.com>
2020-07-06 19:55:32 +1000

    simplified code

--- Array/LeetCode 66 - PlusOne/PlusOne/PlusOne/Program.cs
+++ Array/LeetCode 66 - PlusOne/PlusOne/PlusOne/Program.cs
@@ -12,26 +12,25 @@ namespace PlusOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] digits = {9, 9, 9};
+            Console.WriteLine(PlusOne(digits));
         }
         static int[] PlusOne(int[] digits)
         {
             int car = 0;
-            int cur = (digits[digits.Length - 1] + 1 + car) % 10;
-            car = (digits[digits.Length - 1] + 1 + car) / 10;
-            digits[digits.Length - 1] = cur;
-            for (int i = digits.Length - 2; i >= 0; i--)
+            digits[digits.Length - 1] += 1;
+            for (int i = digits.Length - 1; i >= 0; i--)
             {
-                cur = (digits[i] + car) % 10;
+                var cur = (digits[i] + car) % 10;
                 car = (digits[i] + car) / 10;
                 digits[i] = cur;
-                if (digits[i] != 0)
+                if(car == 0)
                     break;
             }
             if (car != 0)
             {
-                int[] res = new int[digits.Length + 1];
-                res[0] = car;
+                var res = new int[digits.Length + 1];
+                res[0] = 1;
                 Array.Copy(digits, 0, res, 1, digits.Length);
                 return res;
             }
