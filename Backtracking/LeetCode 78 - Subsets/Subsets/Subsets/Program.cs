8121fc7a3025d54b2839d199b866532955061e0c
Xing <x.xing.unsw@gmail.com>
2020-07-11 17:24:24 +1000

    refresh

--- Backtracking/LeetCode 78 - Subsets/Subsets/Subsets/Program.cs
+++ Backtracking/LeetCode 78 - Subsets/Subsets/Subsets/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Subsets
 {
@@ -12,40 +11,21 @@ namespace Subsets
         }
         static IList<IList<int>> Subsets(int[] nums)
         {
-            List<IList<int>> res = new List<IList<int>>();
-            List<int> item = new List<int>();
-            res.Add(item);
-            generate(0, nums, item, res);
+            var res = new List<IList<int>>();
+            var path = new List<int>();
+            DFS(0, nums, path, res);
             return res;
         }
-        static void generate(int i, int[] nums, List<int> item, List<IList<int>> res)
-        {
-            if (i >= nums.Length)
-                return;
-            item.Add(nums[i]);
-            res.Add(new List<int>(item));
-            generate(i + 1, nums, item, res);
-            item.RemoveAt(item.Count - 1);
-            generate(i + 1, nums, item, res);
-        }
 
-        //static IList<IList<int>> Subsets(int[] nums)
-        //{
-        //    List<IList<int>> result = new List<IList<int>>();
-        //    List<int> item = new List<int>();
-        //    Array.Sort(nums);
-        //    generate(result, new List<int>(), nums, 0);
-        //    return result;
-        //}
-        //static void generate(List<IList<int>> result, List<int> tem, int[] nums, int start)
-        //{
-        //    result.Add(new List<int>(tem));
-        //    for (int i = start; i < nums.Length; i++)
-        //    {
-        //        tem.Add(nums[i]);
-        //        generate(result, tem, nums, i + 1);
-        //        tem.RemoveAt(tem.Count - 1);
-        //    }
-        //}
+        static void DFS(int start, int[] nums, List<int> path, List<IList<int>> res)
+        {
+            res.Add(new List<int>(path));
+            for (int i = start; i < nums.Length; i++)
+            {
+                path.Add(nums[i]);
+                DFS(i + 1, nums, path, res);
+                path.RemoveAt(path.Count - 1);
+            }
+        }
     }
 }
