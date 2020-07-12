bf40989274253f07bd965f18b12d3b264fe99f9b
Xing <x.xing.unsw@gmail.com>
2020-07-12 16:14:10 +1000

    init

---
+++ Array/LeetCode 1508 - RangeSumOfSortedSubarraySums/RangeSum/RangeSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RangeSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 2, 3, 4};
+            int n = 4, left = 1, right = 5;
+            Console.WriteLine(RangeSum(nums, n, left, right));
+        }
+        static int RangeSum(int[] nums, int n, int left, int right)
+        {
+            int mod = 1_000_000_000 + 7;
+            int[] record = new int[n * (n + 1) / 2];
+            int index = 0;
+            for (int i = 1; i < nums.Length; i++)
+                nums[i] += nums[i - 1];
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = i; j < nums.Length; j++)
+                    record[index++] = i == 0 ? nums[j] : nums[j] - nums[i - 1];
+            Array.Sort(record);
+            var res = 0;
+            for (int i = left - 1; i < right; i++)
+                res += record[i] % mod;
+            return res;
+        }
+    }
+}
---
+++ HashTable/LeetCode 1512 - NumberOfGoodPairs/NumberOfGoodPairs/NumberOfGoodPairs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberOfGoodPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int NumIdenticalPairs(int[] nums)
+        {
+            var frequencies = new int[101];
+            foreach (var num in nums)
+                frequencies[num]++;
+            int res = 0;
+            foreach (var frequency in frequencies)
+            {
+                if(frequency < 2) continue;
+                res += (frequency - 1) * frequency / 2;
+            }
+            return res;
+        }
+    }
+}
