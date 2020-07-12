2e9cd87ae5582ea9ae6fcfaab4f420d00bde8e74
Xing <x.xing.unsw@gmail.com>
2020-07-12 18:11:35 +1000

    Add DFS solution

--- Graph/LeetCode 1514 - PathWithMaximumProbability/PathWithMaximumProbability/PathWithMaximumProbability/Program.cs
+++ Graph/LeetCode 1514 - PathWithMaximumProbability/PathWithMaximumProbability/PathWithMaximumProbability/Program.cs
@@ -47,5 +47,39 @@ namespace PathWithMaximumProbability
             }
             return probs[end];
         }
+
+        private static double maxProb;
+        static double MaxProbability_DFS(int n, int[][] edges, double[] succProb, int start, int end)
+        {
+            var graph = new List<Tuple<int, double>>[n];
+            for (int i = 0; i < graph.Length; i++)
+                graph[i] = new List<Tuple<int, double>>();
+            for (int i = 0; i < edges.Length; i++)
+            {
+                graph[edges[i][0]].Add(new Tuple<int, double>(edges[i][1], succProb[i]));
+                graph[edges[i][1]].Add(new Tuple<int, double>(edges[i][0], succProb[i]));
+            }
+            double prob = 1;
+            var visit = new bool[n];
+            DFS(graph, prob, visit, start, end);
+            return maxProb;
+        }
+
+        static void DFS(List<Tuple<int, double>>[] graph, double prob, bool[] visit, int cur, int end)
+        {
+            if (cur == end)
+            {
+                maxProb = Math.Max(maxProb, prob);
+                return;
+            }
+            visit[cur] = true;
+            foreach (var next in graph[cur])
+            {
+                if (prob < maxProb) continue;
+                if (visit[next.Item1]) continue;
+                DFS(graph, prob * next.Item2, visit, next.Item1, end);
+                visit[next.Item1] = false;
+            }
+        }
     }
 }
