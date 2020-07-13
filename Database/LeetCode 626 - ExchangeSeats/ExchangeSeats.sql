0d3019b314cd5013ac076d0aed1bec79368bdcd4
Xing <x.xing.unsw@gmail.com>
2020-07-13 21:53:42 +1000

    init

---
+++ Database/LeetCode 626 - ExchangeSeats/ExchangeSeats.sql
@@ -0,0 +1,14 @@
+SELECT 
+	(CASE 
+		WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+		WHEN MOD(id, 2) != 0 AND counts = id THEN id
+		ELSE id - 1 
+	END) AS id,
+	student
+FROM 
+	seat,
+	(SELECT 
+		COUNT(*) AS counts 
+	FROM 
+		seat) AS seat_counts
+ORDER BY id ASC;
