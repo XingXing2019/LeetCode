0967dbe01098cc1becf290ce65bc19b31dfd8e3f
Xing <x.xing.unsw@gmail.com>
2020-07-13 20:17:31 +1000

    init

---
+++ Database/LeetCode 184 - DepartmentHighestSalary/DepartmentHighestSalary.sql
@@ -0,0 +1,16 @@
+SELECT 
+	Department.Name AS Department, 
+	Employee.Name AS Employee, 
+	Salary	
+FROM 
+	Employee JOIN Department 
+	ON Employee.DepartmentId = Department.Id
+WHERE 
+	(Employee.DepartmentId, Salary) IN
+   (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+	);
+
