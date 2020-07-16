5f85855741687338938213359aee78d7f3ae097b
Xing <x.xing.unsw@gmail.com>
2020-07-16 20:24:46 +1000

    init

---
+++ HashTable/LeetCode 535 - EncodeAndDecodeTinyURL/EncodeAndDecodeTinyURL/EncodeAndDecodeTinyURL/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EncodeAndDecodeTinyURL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Codec
+    {
+        private List<string> record = new List<string>();
+        public string encode(string longUrl)
+        {
+            record.Add(longUrl);
+            var index = record.Count - 1;
+            return index.ToString();
+        }
+
+        public string decode(string shortUrl)
+        {
+            int index = int.Parse(shortUrl);
+            return record[index];
+        }
+    }
+}
