f9465f1de0aff93cb871347d5d7da248c0191c42
Xing <x.xing.unsw@gmail.com>
2020-07-16 17:41:24 +1000

    simpilified code

--- Math/LeetCode 50 - Pow(x,n)/PowXN/PowXN/Program.cs
+++ Math/LeetCode 50 - Pow(x,n)/PowXN/PowXN/Program.cs
@@ -9,13 +9,11 @@ namespace PowXN
         {
             double x = 2;
             int n = 10;
-            Console.WriteLine(MyPow(x, n));
         }
-        static double MyPow(double x, int n)
+        public double MyPow(double x, int n)
         {
-            if (x == 1 || x == 0) return x;
-            if (n == 0) return 1.0d;
-            if (n < 0)
+            if (x == 0) return x;
+            if( n < 0)
             {
                 x = 1 / x;
                 n = -n;
@@ -23,9 +21,9 @@ namespace PowXN
             return Recursion(x, n);
         }
 
-        static double Recursion(double x, int n)
+        private double Recursion(double x, int n)
         {
-            if (n == 0) return 1.0d;
+            if (n == 0) return 1.0;
             if (n == 1) return x;
             double temp = Recursion(x, n / 2);
             if (n % 2 == 0) return temp * temp;
