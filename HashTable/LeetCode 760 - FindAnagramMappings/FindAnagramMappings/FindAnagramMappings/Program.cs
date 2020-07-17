6977ee5141d96f24ca15ffecc9c9c772bdfefae0
Xing <x.xing.unsw@gmail.com>
2020-07-17 23:27:55 +1000

    init

---
+++ HashTable/LeetCode 760 - FindAnagramMappings/FindAnagramMappings/FindAnagramMappings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindAnagramMappings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int[] AnagramMappings(int[] A, int[] B)
+        {
+            var index = new Dictionary<int, List<int>>();
+            for (int i = 0; i < B.Length; i++)
+            {
+                if (!index.ContainsKey(B[i]))
+                    index[B[i]] = new List<int> {i};
+                else
+                    index[B[i]].Add(i);
+            }
+            for (int i = 0; i < A.Length; i++)
+            {
+                var temp = A[i];
+                A[i] = index[temp][^1];
+                index[temp].RemoveAt(index[temp].Count - 1);
+            }
+            return A;
+        }
+    }
+}
