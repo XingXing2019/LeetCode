a4ba1ceea58f1555068043dcaa5aed991121df1c
Xing <x.xing.unsw@gmail.com>
2020-07-17 18:25:20 +1000

    init

---
+++ String/LeetCode 1119 - RemoveVowelsFromAString/RemoveVowelsFromAString/RemoveVowelsFromAString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace RemoveVowelsFromAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public string RemoveVowels_Linq(string S)
+        {
+            var letters = S.Where(x => x != 'a' && x != 'e' && x != 'i' && x != 'o' && x != 'u').ToArray();
+            return string.Join("", letters);
+        }
+    }
+}
