d6dcbe78ae55a47f6e73a2d03cad053788445d62
Xing <x.xing.unsw@gmail.com>
2020-07-18 12:10:08 +1000

    init

---
+++ String/LeetCode 1165 - SingleRowKeyboard/SingleRowKeyboard/SingleRowKeyboard/Program.cs
@@ -0,0 +1,27 @@
+﻿//利用数组记录keyboard中每个字母的位置。
+//遍历word计算times
+using System;
+
+namespace SingleRowKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int CalculateTime(string keyboard, string word)
+        {
+            int times = 0, pos = 0;
+            var index = new int[26];
+            for (int i = 0; i < keyboard.Length; i++)
+                index[keyboard[i] - 'a'] = i;
+            foreach (var letter in word)
+            {
+                times += Math.Abs(index[letter - 'a'] - pos);
+                pos = index[letter - 'a'];
+            }
+            return times;
+        }
+    }
+}
