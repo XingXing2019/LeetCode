e3cdd703763fcc25070896f7d0e46c19d4371959
Xing <x.xing.unsw@gmail.com>
2020-07-18 11:28:54 +1000

    init

---
+++ Recursion/Leetcode 1265 - PrintImmutableLinkedListInReverse/PrintImmutableLinkedListInReverse/PrintImmutableLinkedListInReverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintImmutableLinkedListInReverse
+{
+    class ImmutableListNode
+    {
+        public void PrintValue(); // print the value of this node.
+        public ImmutableListNode GetNext(); // return the next node.
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void PrintLinkedListInReverse(ImmutableListNode head)
+        {
+            if (head == null) return;
+            PrintLinkedListInReverse(head.GetNext());
+            head.PrintValue();
+        }
+    }
+}
