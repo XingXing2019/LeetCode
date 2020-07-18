eee9c88eae0b70d6c7d4a051fcb47d83fa6e1c64
Xing <x.xing.unsw@gmail.com>
2020-07-18 14:35:42 +1000

    init

---
+++ Math/LeetCode 1134 - ArmstrongNumber/ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿//按照题意逐位求和。
+using System;
+
+namespace ArmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsArmstrong(int N)
+        {
+            string strN = N.ToString();
+            int k = strN.Length;
+            double sum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                sum += Math.Pow(strN[i] - '0', k);
+                if (sum > N)
+                    return false;
+            }
+            return (int) sum == N;
+        }
+    }
+}
