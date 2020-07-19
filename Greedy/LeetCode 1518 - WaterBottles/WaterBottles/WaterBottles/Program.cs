f429d79858808f003e3d3fc085092ca447380ffe
Xing <x.xing.unsw@gmail.com>
2020-07-19 17:01:00 +1000

    init

---
+++ Greedy/LeetCode 1518 - WaterBottles/WaterBottles/WaterBottles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WaterBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numBottles = 2, numExchange = 3;
+            Console.WriteLine(NumWaterBottles(numBottles, numExchange));
+        }
+        static int NumWaterBottles(int numBottles, int numExchange)
+        {
+            int max = numBottles;
+            while (numBottles >= numExchange)
+            {
+                max += numBottles / numExchange;
+                numBottles = numBottles / numExchange + numBottles % numExchange;  
+            }
+            return max;
+        }
+    }
+}
