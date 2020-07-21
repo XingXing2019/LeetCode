337464f48b5cf01836a75fbfd9ed86ea35dff9db
Xing <x.xing.unsw@gmail.com>
2020-07-21 17:51:39 +1000

    init

---
+++ Array/LeetCode1085 - SumOfDigitsInTheMinimumNumber/SumOfDigitsInTheMinimumNumber/SumOfDigitsInTheMinimumNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfDigitsInTheMinimumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int SumOfDigits(int[] A)
+        {
+            var min = A.Min();
+            var res = 0;
+            while (min != 0)
+            {
+                res += min % 10;
+                min /= 10;
+            }
+            return (res + 1) % 2;
+        }
+    }
+}
