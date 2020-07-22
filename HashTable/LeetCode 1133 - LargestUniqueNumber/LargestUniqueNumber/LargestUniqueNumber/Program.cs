0fa6e48cf582b7cef9238469c22533bc9a02b73c
Xing <x.xing.unsw@gmail.com>
2020-07-22 19:19:17 +1000

    init

---
+++ HashTable/LeetCode 1133 - LargestUniqueNumber/LargestUniqueNumber/LargestUniqueNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LargestUniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int LargestUniqueNumber(int[] A)
+        {
+            var record = new int[1001];
+            foreach (var num in A)
+                record[num]++;
+            for (int i = record.Length - 1; i >= 0; i--)
+            {
+                if (record[i] == 1)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
