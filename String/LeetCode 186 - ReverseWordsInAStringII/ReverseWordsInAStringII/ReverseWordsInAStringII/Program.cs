dbc12023ed75b514de788a7c944812f7a0223727
Xing <x.xing.unsw@gmail.com>
2020-07-22 21:49:45 +1000

    init

---
+++ String/LeetCode 186 - ReverseWordsInAStringII/ReverseWordsInAStringII/ReverseWordsInAStringII/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseWordsInAStringII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = {'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
+            ReverseWords(s);
+        }
+        static void ReverseWords(char[] s)
+        {
+            var reversed = new char[s.Length];
+            var spaceIndex = new List<int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(s[i] == ' ')
+                    spaceIndex.Add(i);
+            }
+            spaceIndex.Add(s.Length);
+            int count = 0, index = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == spaceIndex[index])
+                {
+                    index++;
+                    count = 0;
+                    continue;
+                }
+                reversed[s.Length - spaceIndex[index] + count] = s[i];
+                count++;
+            }
+            for (int i = 0; i < reversed.Length; i++)
+                s[i] = reversed[i] == 0 ? ' ' : reversed[i];
+        }
+    }
+}
