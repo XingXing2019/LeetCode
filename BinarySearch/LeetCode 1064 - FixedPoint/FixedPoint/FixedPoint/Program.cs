e280be4b2ec55c629a6292db5ae84acebbdcc279
Xing <x.xing.unsw@gmail.com>
2020-07-22 18:24:37 +1000

    init

---
+++ BinarySearch/LeetCode 1064 - FixedPoint/FixedPoint/FixedPoint/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FixedPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = {0, 1, 3, 7, 8, 9};
+            Console.WriteLine(FixedPoint(A));
+        }
+        static int FixedPoint(int[] A)
+        {
+            int li = 0, hi = A.Length - 1;
+            while (li < hi)
+            {
+                int mid = li + (hi - li) / 2;
+                if (A[mid] < mid)
+                    li = mid + 1;
+                else
+                    hi = mid;
+            }
+            return A[li] == li ? li : -1;
+        }
+    }
+}
