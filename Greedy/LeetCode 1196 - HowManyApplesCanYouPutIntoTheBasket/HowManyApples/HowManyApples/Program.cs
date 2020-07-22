c69df7278a678a7aaf003ce1faf399686aa3cd69
Xing <x.xing.unsw@gmail.com>
2020-07-22 18:17:17 +1000

    init

---
+++ Greedy/LeetCode 1196 - HowManyApplesCanYouPutIntoTheBasket/HowManyApples/HowManyApples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HowManyApples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int MaxNumberOfApples(int[] arr)
+        {
+            Array.Sort(arr);
+            int count = 0, weight = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                weight += arr[i];
+                if(weight > 5000)
+                    break;
+                count++;
+            }
+            return count;
+        }
+    }
+}
