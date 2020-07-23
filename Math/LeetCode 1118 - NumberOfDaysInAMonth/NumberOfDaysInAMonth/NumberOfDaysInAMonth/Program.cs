fe5794ea82e19875fb9c726c9b2f3f4d36ffc73a
Xing <x.xing.unsw@gmail.com>
2020-07-23 20:03:14 +1000

    init

---
+++ Math/LeetCode 1118 - NumberOfDaysInAMonth/NumberOfDaysInAMonth/NumberOfDaysInAMonth/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfDaysInAMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int NumberOfDays(int Y, int M)
+        {
+            var isLoopYear = (Y % 4 == 0 && Y % 100 != 0) || Y % 400 == 0;
+            if (M == 2)
+                return isLoopYear ? 29 : 28;
+            var thirtyOneDays = new List<int> {1, 3, 5, 7, 8, 10, 12};
+            return thirtyOneDays.Contains(M) ? 31 : 30;
+        }
+    }
+}
