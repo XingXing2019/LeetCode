e876267be69be562b01638b1dbdadfd0fc7b89a4
Xing <x.xing.unsw@gmail.com>
2020-07-23 19:56:59 +1000

    init

---
+++ Math/LeetCode 1271 - Hexspeak/Hexspeak/Hexspeak/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hexspeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string ToHexspeak(string num)
+        {
+            string dict = "OI23456789ABCDEF";
+            var inValid = new List<long> {2, 3, 4, 5, 6, 7, 8, 9};
+            var hex = "";
+            long number = long.Parse(num);
+            while (number != 0)
+            {
+                var digit = number % 16;
+                if (inValid.Contains(digit))
+                    return "ERROR";
+                hex = dict[(int)digit] + hex;
+                number /= 16;
+            }
+            return hex;
+        }
+    }
+}
