56acab53117dd9ffc94f9934b37f4372072eac35
Xing <x.xing.unsw@gmail.com>
2020-07-24 22:30:49 +1000

    init

---
+++ String/LeetCode 293 - FlipGame/FlipGame/FlipGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlipGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static IList<string> GeneratePossibleNextMoves(string s)
+        {
+            var res = new List<string>();
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1] && s[i] == '+')
+                {
+                    var temp = new StringBuilder(s);
+                    temp[i] = '-';
+                    temp[i - 1] = '-';
+                    res.Add(temp.ToString());
+                }
+            }
+            return res;
+        }
+    }
+}
