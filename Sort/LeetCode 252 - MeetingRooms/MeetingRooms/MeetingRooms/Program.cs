bd5b4c4dea3798fbd4499e36e1c7ac22b496a48c
Xing <x.xing.unsw@gmail.com>
2020-07-25 15:59:26 +1000

    init

---
+++ Sort/LeetCode 252 - MeetingRooms/MeetingRooms/MeetingRooms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace MeetingRooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static bool CanAttendMeetings(int[][] intervals)
+        {
+            if (intervals.Length == 1) return true;
+            intervals = intervals.OrderBy(x => x[0]).ToArray();
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                if (intervals[i][0] < intervals[i - 1][1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
