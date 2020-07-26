daf192a85c169ba9f3661b40bfe8db58d0902efb
Xing <x.xing.unsw@gmail.com>
2020-07-26 13:31:34 +1000

    init

---
+++ Math/LeetCode 1524 - NumberOfSubArraysWithOddSum/NumberOfSubArraysWithOddSum/NumberOfSubArraysWithOddSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumberOfSubArraysWithOddSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int NumOfSubarrays(int[] arr)
+        {
+            int even = 0, odd = 0;
+            int sum = 0, count = 0, mod = 1_000_000_000 + 7;
+            foreach (var num in arr)
+            {
+                sum += num;
+                if (sum % 2 != 0)
+                {
+                    count = (count + even + 1) % mod;
+                    odd++;
+                }
+                else
+                {
+                    count = (count + odd) % mod;
+                    even++;
+                }
+            }
+            return count;
+        }
+    }
+}
