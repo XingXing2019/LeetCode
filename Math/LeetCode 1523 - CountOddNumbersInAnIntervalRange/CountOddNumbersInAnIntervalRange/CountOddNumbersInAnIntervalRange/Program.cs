385cc6fa9b429cb56d3812cc49bc79a03c322118
Xing <x.xing.unsw@gmail.com>
2020-07-26 11:01:10 +1000

    init

---
+++ Math/LeetCode 1523 - CountOddNumbersInAnIntervalRange/CountOddNumbersInAnIntervalRange/CountOddNumbersInAnIntervalRange/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CountOddNumbersInAnIntervalRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountOdds(int low, int high)
+        {
+            if (low == high)
+                return low % 2 == 0 ? 0 : 1;
+            var res = low % 2 != 0 ? (high - low - 1) / 2 : (high - low) / 2;
+            if (low % 2 != 0) 
+                res++;
+            if (high % 2 != 0) 
+                res++;
+            return res;
+        }
+    }
+}
