93510536d8bb4f0f05b2fd9ee9bc0a3b2d24fa06
Xing <x.xing.unsw@gmail.com>
2020-07-27 18:26:47 +1000

    init

---
+++ String/LeetCode 1529 - BulbSwitcherIV/BulbSwitcherIV/BulbSwitcherIV/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BulbSwitcherIV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = "000";
+            Console.WriteLine(MinFlips(target));
+        }
+        static int MinFlips(string target)
+        {
+            var res = 0;
+            var state = '0';
+            foreach (var letter in target)
+            {
+                if (state == '0')
+                {
+                    if (letter == '1')
+                    {
+                        res++;
+                        state = '1';
+                    }
+                }
+                else
+                {
+                    if (letter == '0')
+                    {
+                        res++;
+                        state = '0';
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
