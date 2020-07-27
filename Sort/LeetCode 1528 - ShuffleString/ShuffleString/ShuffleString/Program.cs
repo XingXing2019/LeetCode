af061b79834aaa98c894672603eeff48028f5dfe
Xing <x.xing.unsw@gmail.com>
2020-07-27 18:14:15 +1000

    init

---
+++ Sort/LeetCode 1528 - ShuffleString/ShuffleString/ShuffleString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace ShuffleString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string RestoreString(string s, int[] indices)
+        {
+            var res = new char[s.Length];
+            for (int i = 0; i < indices.Length; i++)
+                res[indices[i]] = s[i];
+            return string.Join("", res);
+        }
+    }
+}
