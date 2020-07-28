c6f494c383ce4a4f1c74e59289f43b100fcf9d15
Xing <x.xing.unsw@gmail.com>
2020-07-28 20:23:16 +1000

    init

---
+++ Math/LeetCode 1228 - MissingNumberInArithmeticProgression/MissingNumberInArithmeticProgression/MissingNumberInArithmeticProgression/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace MissingNumberInArithmeticProgression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int MissingNumber(int[] arr)
+        {
+            return (arr[0] + arr[^1]) * (arr.Length + 1) / 2 - arr.Sum();
+        }
+    }
+}
