4fdf06c9c6d86ec5fedafe82d804c8470dbb5868
Xing <x.xing.unsw@gmail.com>
2020-07-28 20:31:47 +1000

    init

---
+++ HashTable/LeetCode 246 - StrobogrammaticNumber/StrobogrammaticNumber/StrobogrammaticNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrobogrammaticNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsStrobogrammatic(string num)
+        {
+            var dict = new Dictionary<char, char>
+            {
+                {'0', '0'},
+                {'1', '1'},
+                {'6', '9'},
+                {'8', '8'},
+                {'9', '6'}
+            };
+            int li = 0, hi = num.Length - 1;
+            while (li <= hi)
+            {
+                if (!dict.ContainsKey(num[li]) || dict[num[li]] != num[hi])
+                    return false;
+                li++;
+                hi--;
+            }
+            return true;
+        }
+    }
+}
+
