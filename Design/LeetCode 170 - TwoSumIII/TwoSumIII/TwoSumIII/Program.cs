fac74d8ad3913e223a02990c714e94d6e33ceca9
Xing <x.xing.unsw@gmail.com>
2020-07-30 21:19:08 +1000

    init

---
+++ Design/LeetCode 170 - TwoSumIII/TwoSumIII/TwoSumIII/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoSumIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class TwoSum
+    {
+        private List<int> nums;
+        public TwoSum()
+        {
+            nums = new List<int>();
+        }
+        public void Add(int number)
+        {
+            nums.Add(number);
+        }
+
+        public bool Find(int value)
+        {
+            var set = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (set.Contains(value - num))
+                    return true;
+                set.Add(num);
+            }
+            return false;
+        }
+    }
+}
