5a28a737602e3cdc7aa71cc39170eefd5e07ac34
Xing <x.xing.unsw@gmail.com>
2020-07-31 19:30:48 +1000

    init

---
+++ Array/LeetCode 1099 - TwoSumLessThanK/TwoSumLessThanK/TwoSumLessThanK/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TwoSumLessThanK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int TwoSumLessThanK(int[] A, int K)
+        {
+            int res = -1;
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    if (A[i] + A[j] < K)
+                        res = Math.Max(res, A[i] + A[j]);
+                }
+            }
+            return res;
+        }
+    }
+}
