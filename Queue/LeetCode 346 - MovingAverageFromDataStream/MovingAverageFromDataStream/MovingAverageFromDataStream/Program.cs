5204262d406c65f31f0353d514b16325f18001f6
Xing <x.xing.unsw@gmail.com>
2020-07-31 19:41:23 +1000

    init

---
+++ Queue/LeetCode 346 - MovingAverageFromDataStream/MovingAverageFromDataStream/MovingAverageFromDataStream/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovingAverageFromDataStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class MovingAverage
+    {
+        private readonly int _size;
+        private Queue<double> _nums;
+        private double _sum;
+        public MovingAverage(int size)
+        {
+            _size = size;
+            _nums = new Queue<double>();
+        }
+
+        public double Next(int val)
+        {
+            if (_nums.Count >= _size)
+                _sum -= _nums.Dequeue();
+            _nums.Enqueue(val);
+            _sum += val;
+            return _sum / _nums.Count;
+        }
+    }
+}
