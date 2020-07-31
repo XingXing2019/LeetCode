922d10c7ba1ae57699ee570993f0bcfee0aa4665
Xing <x.xing.unsw@gmail.com>
2020-07-31 23:19:54 +1000

    init

---
+++ Array/LeetCode 531 - LonelyPixelI/LonelyPixelI/LonelyPixelI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace LonelyPixelI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static int FindLonelyPixel(char[][] picture)
+        {
+            int res = 0;
+            var rowNumber = new int[picture.Length];
+            for (int r = 0; r < picture.Length; r++)
+            {
+                int count = picture[r].Count(x => x == 'B');
+                res += count;
+                rowNumber[r] = count;
+            }
+            for (int c = 0; c < picture[0].Length; c++)
+            {
+                int count = 0, row = -1;
+                for (int r = 0; r < picture.Length; r++)
+                {
+                    count += picture[r][c] == 'B' ? 1 : 0;
+                    if (count == 1)
+                        row = r;
+                }
+                res -= count > 1 ? count : 0;
+                if (count == 1 && rowNumber[row] > 1)
+                    res--;
+            }
+            return res;
+        }
+    }
+}
