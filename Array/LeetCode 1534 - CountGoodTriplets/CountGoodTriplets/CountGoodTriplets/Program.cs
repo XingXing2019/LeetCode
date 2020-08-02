d44336436f291af8a72218ad63a43f4660e357d5
Xing <x.xing.unsw@gmail.com>
2020-08-02 16:56:34 +1000

    init

---
+++ Array/LeetCode 1534 - CountGoodTriplets/CountGoodTriplets/CountGoodTriplets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CountGoodTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            int res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        if (Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b &&
+                            Math.Abs(arr[i] - arr[k]) <= c)
+                            res++;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
