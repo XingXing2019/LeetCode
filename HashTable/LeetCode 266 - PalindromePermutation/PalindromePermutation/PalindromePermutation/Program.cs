4fa95ffff2e3c56852163d725b030ca37bb6858a
Xing <x.xing.unsw@gmail.com>
2020-08-03 20:14:17 +1000

    init

---
+++ HashTable/LeetCode 266 - PalindromePermutation/PalindromePermutation/PalindromePermutation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromePermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool CanPermutePalindrome(string s)
+        {
+            var record = new int[128];
+            foreach (var character in s)
+                record[character]++;
+            var countOdd = record.Count(x => x % 2 != 0);
+            return countOdd < 2;
+        }
+    }
+}
