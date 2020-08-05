e0ed3aedfd93bc667284b56667714c50bce37e7d
Xing <x.xing.unsw@gmail.com>
2020-08-05 18:22:49 +1000

    init

--- Search/LeetCode 752 - OpenTheLock/OpenTheLock/OpenTheLock/Program.cs
+++ Search/LeetCode 752 - OpenTheLock/OpenTheLock/OpenTheLock/Program.cs
@@ -20,30 +20,24 @@ namespace OpenTheLock
                 if (deadend == "0000") return -1;
                 set.Add(deadend);
             }
-            var queue = new Queue<string>();
+            var queue = new Queue<KeyValuePair<string, int>>();
             var visit = new HashSet<string>();
-            queue.Enqueue("0000");
+            queue.Enqueue(new KeyValuePair<string, int>("0000", 0));
             visit.Add("0000");
-            int level = -1;
             while (queue.Count != 0)
             {
-                int count = queue.Count;
-                level++;
-                while (count != 0)
+                var cur = queue.Dequeue();
+                var level = cur.Value;
+                if (cur.Key == target) return level;
+                for (int i = 0; i < 4; i++)
                 {
-                    var cur = queue.Dequeue();
-                    if (cur == target) return level;
-                    for (int i = 0; i < 4; i++)
-                    {
-                        var digit = cur[i];
-                        var up = cur[i] == '9' ? '0' : (char) (digit + 1);
-                        var down = cur[i] == '0' ? '9' : (char) (digit - 1);
-                        string numUp = cur.Substring(0, i - 0) + up + cur.Substring(i + 1, 3 - i);
-                        string numDown = cur.Substring(0, i - 0) + down + cur.Substring(i + 1, 3 - i);
-                        if(!visit.Contains(numUp) && set.Add(numUp)) queue.Enqueue(numUp);
-                        if(!visit.Contains(numDown) && set.Add(numDown)) queue.Enqueue(numDown);
-                    }
-                    count--;
+                    var digit = cur.Key[i];
+                    var up = cur.Key[i] == '9' ? '0' : (char)(digit + 1);
+                    var down = cur.Key[i] == '0' ? '9' : (char)(digit - 1);
+                    string numUp = cur.Key.Substring(0, i - 0) + up + cur.Key.Substring(i + 1, 3 - i);
+                    string numDown = cur.Key.Substring(0, i - 0) + down + cur.Key.Substring(i + 1, 3 - i);
+                    if (!visit.Contains(numUp) && set.Add(numUp)) queue.Enqueue(new KeyValuePair<string, int>(numUp, level + 1));
+                    if (!visit.Contains(numDown) && set.Add(numDown)) queue.Enqueue(new KeyValuePair<string, int>(numDown, level + 1));
                 }
             }
             return -1;
