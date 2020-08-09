85b41e2d6407353917ff5517b9b5d7fd4b86c846
Xing <x.xing.unsw@gmail.com>
2020-08-09 14:11:55 +1000

    init

---
+++ Array/LeetCode 1539 - KthMissingPositiveNumber/KthMissingPositiveNumber/KthMissingPositiveNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KthMissingPositiveNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {2, 3, 4, 7, 11};
+            int k = 2;
+            Console.WriteLine(FindKthPositive(arr, k));
+        }
+        static int FindKthPositive(int[] arr, int k)
+        {
+            if (arr[0] > k) return k;
+            k -= arr[0] - 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] - arr[i - 1] != 1)
+                {
+                    if (k - (arr[i] - arr[i - 1] - 1) <= 0)
+                        return arr[i - 1] + k;
+                    k -= arr[i] - arr[i - 1] - 1;
+                }
+            }
+            return arr[^1] + k;
+        }
+    }
+}
