9746b7f260087d4321e6f77b30fbb8516e1935c7
Xing <x.xing.unsw@gmail.com>
2020-08-09 15:45:59 +1000

    init

---
+++ Stack/LeetCode 1544 - MakeTheStringGreat/MakeTheStringGreat/MakeTheStringGreat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MakeTheStringGreat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string MakeGood(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var letter in s)
+            {
+                if (stack.Count != 0 && Math.Abs(stack.Peek() - letter) == 32)
+                    stack.Pop();
+                else
+                    stack.Push(letter);
+            }
+            var res = "";
+            while (stack.Count != 0)
+                res = stack.Pop() + res;
+            return res;
+        }
+    }
+}
