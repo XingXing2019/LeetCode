f3c306fe10291de0fa523075647a2453040057c9
Xing <x.xing.unsw@gmail.com>
2020-08-16 15:23:14 +1000

    init

---
+++ Array/LeetCode 1550 - ThreeConsecutiveOdds/ThreeConsecutiveOdds/ThreeConsecutiveOdds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ThreeConsecutiveOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {2, 3, 5};
+            Console.WriteLine(ThreeConsecutiveOdds(arr));
+        }
+        static bool ThreeConsecutiveOdds(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                if(arr[i] % 2 == 0) continue;
+                int count = 1;
+                for (int j = 1; j < 3; j++)
+                {
+                    if(arr[i + j] % 2 == 0) break;
+                    count++;
+                }
+                if (count == 3) return true;
+            }
+            return false;
+        }
+    }
+}
