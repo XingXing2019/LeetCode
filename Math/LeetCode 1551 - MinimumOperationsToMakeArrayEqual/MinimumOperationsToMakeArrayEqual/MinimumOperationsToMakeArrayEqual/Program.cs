cb7ccfae27f13cbf6ea7c87a397ef1d65c64bad3
Xing <x.xing.unsw@gmail.com>
2020-08-16 15:36:27 +1000

    init

---
+++ Math/LeetCode 1551 - MinimumOperationsToMakeArrayEqual/MinimumOperationsToMakeArrayEqual/MinimumOperationsToMakeArrayEqual/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinimumOperationsToMakeArrayEqual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            Console.WriteLine(MinOperations(n));
+        }
+        static int MinOperations(int n)
+        {
+            int target = (1 + 2 * (n - 1) + 1) / 2;
+            int len = n % 2 == 0 ? n / 2 - 1 : n / 2;
+            var res = 0;
+            for (int i = 0; i <= len; i++)
+                res += target - (2 * i + 1);
+            return res;
+        }
+    }
+}
