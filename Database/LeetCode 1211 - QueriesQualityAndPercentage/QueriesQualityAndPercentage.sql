28afe06099726fe1640bd5f7cb10bf9012d08a78
Xing <x.xing.unsw@gmail.com>
2020-08-19 20:24:54 +1000

    init

--- Database/LeetCode 1211 - QueriesQualityAndPercentage/QueriesQualityAndPercentage.sql
+++ Database/LeetCode 1211 - QueriesQualityAndPercentage/QueriesQualityAndPercentage.sql
@@ -1,5 +1,3 @@
-SELECT query_name, ROUND(SUM(rating/ POSITION)/ COUNT(*),2) quality, ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)/ COUNT(*) * 100,2) poor_query_percentage
-FROM Queries
+SELECT query_name, ROUND(AVG(rating / POSITION), 2) quality, ROUND(SUM(case when rating < 3 then 1 ELSE 0 END) / COUNT(*) * 100, 2) poor_query_percentage 
+FROM queries
 GROUP BY query_name;
-
-
