a5e6a993c0f36e370b5a3d4348c817a74c4f92b6
Xing <x.xing.unsw@gmail.com>
2020-08-19 22:08:11 +1000

    init

---
+++ String/LeetCode 820 - ShortEncodingOfWords/ShortEncodingOfWords/ShortEncodingOfWords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShortEncodingOfWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MinimumLengthEncoding(string[] words)
+        {
+            var remains = new HashSet<string>(words);
+            foreach (var word in words)
+            {
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    var tempWord = word.Substring(word.Length - 1 - i);
+                    if (remains.Contains(tempWord))
+                        remains.Remove(tempWord);
+                }
+            }
+            int res = remains.Count;
+            foreach (var word in remains)
+                res += word.Length;
+            return res;
+        }
+    }
+}
