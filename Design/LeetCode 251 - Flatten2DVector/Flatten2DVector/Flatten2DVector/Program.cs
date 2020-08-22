5f13ce91823c65eed52b5741be512a4a6d71164a
Xing <x.xing.unsw@gmail.com>
2020-08-22 18:02:06 +1000

    init

---
+++ Design/LeetCode 251 - Flatten2DVector/Flatten2DVector/Flatten2DVector/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flatten2DVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Vector2D
+    {
+        private List<int> record;
+        private int point;
+        public Vector2D(int[][] v)
+        {
+            record = new List<int>();
+            point = 0;
+            foreach (var numbers in v)
+            {
+                foreach (var number in numbers)
+                {
+                    record.Add(number);
+                }   
+            }
+        }
+
+        public int Next()
+        {
+            return record[point++];
+        }
+
+        public bool HasNext()
+        {
+            return point < record.Count;
+        }
+    }
+}
