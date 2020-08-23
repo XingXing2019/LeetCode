d75f4d83df7555f95b86833ec985385cedc7f4f4
Xing <x.xing.unsw@gmail.com>
2020-08-23 14:02:20 +1000

    init

---
+++ Graph/LeetCode 1557 - MinimumNumberOfVerticesToReachAllNodes/MinimumNumberOfVerticesToReachAllNodes/MinimumNumberOfVerticesToReachAllNodes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinimumNumberOfVerticesToReachAllNodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+        {
+            var income = new int[n];
+            foreach (var edge in edges)
+                income[edge[1]]++;
+            var res = new List<int>();
+            for (int i = 0; i < income.Length; i++)
+            {
+                if (income[i] == 0)
+                    res.Add(i);
+            }
+            return res;
+        }
+    }
+}
