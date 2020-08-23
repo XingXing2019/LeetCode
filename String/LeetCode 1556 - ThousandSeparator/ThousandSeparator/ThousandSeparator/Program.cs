cc2278f3ae1f579f55eeee8d488c110a9308f145
Xing <x.xing.unsw@gmail.com>
2020-08-23 12:43:15 +1000

    init

---
+++ String/LeetCode 1556 - ThousandSeparator/ThousandSeparator/ThousandSeparator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThousandSeparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string ThousandSeparator(int n)
+        {
+            if (n == 0) return "0";
+            var res = "";
+            var count = 0;
+            while (n != 0)
+            {
+                count++;
+                res = n % 10 + res;
+                n /= 10;
+                if (n != 0 && count == 3)
+                {
+                    res = "." + res;
+                    count = 0;
+                }
+            }
+            return res;
+        }
+    }
+}
