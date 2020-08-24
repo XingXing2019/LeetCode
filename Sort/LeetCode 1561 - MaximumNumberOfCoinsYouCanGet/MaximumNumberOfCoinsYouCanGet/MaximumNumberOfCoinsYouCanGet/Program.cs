43d09b677142b5408914ac6d85056681baf31528
Xing <x.xing.unsw@gmail.com>
2020-08-24 17:22:23 +1000

    init

---
+++ Sort/LeetCode 1561 - MaximumNumberOfCoinsYouCanGet/MaximumNumberOfCoinsYouCanGet/MaximumNumberOfCoinsYouCanGet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MaximumNumberOfCoinsYouCanGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MaxCoins(int[] piles)
+        {
+            Array.Sort(piles);
+            var res = 0;
+            for (int i = piles.Length / 3; i < piles.Length; i+= 2)
+                res += piles[i];
+            return res;
+        }
+    }
+}
