17940c99d40d7f2097934f6236a8c474da55b78b
Xing <x.xing.unsw@gmail.com>
2020-08-24 22:30:13 +1000

    init

---
+++ BinarySearch/LeetCode 1150 - CheckIfANumberIsMajorityElementInASortedArray/CheckIfANumberIsMajority/CheckIfANumberIsMajority/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CheckIfANumberIsMajority
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 4, 5, 5, 5, 5, 5, 6, 6 };
+            int target = 6;
+            Console.WriteLine(IsMajorityElement(nums, target));
+        }
+        static bool IsMajorityElement(int[] nums, int target)
+        {
+            if (nums.Length == 1)
+                return nums[0] == target;
+            int li = 0, hi = nums.Length - 1;
+            while (li < hi)
+            {
+                int mid = li + (hi - li) / 2;
+                if (nums[mid] >= target)
+                    hi = mid;
+                else
+                    li = mid + 1;
+            }
+            int low = li;
+            li = 0;
+            hi = nums.Length - 1;
+            while (li <= hi)
+            {
+                int mid = li + (hi - li) / 2;
+                if (nums[mid] > target)
+                    hi = mid - 1;
+                else
+                    li = mid + 1;
+            }
+            int high = li;
+            return high - low > nums.Length / 2;
+        }
+    }
+}
