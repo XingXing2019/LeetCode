689aaea177ba535388e81bb3fd3161a19f08a81d
Xing <x.xing.unsw@gmail.com>
2020-08-27 20:02:49 +1000

    init

---
+++ Design/LeetCode 1244 - DesignALeaderboard/DesignALeaderboard/DesignALeaderboard/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignALeaderboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Leaderboard
+    {
+        private Dictionary<int, int> record;
+        public Leaderboard()
+        {
+            record = new Dictionary<int, int>();
+        }
+
+        public void AddScore(int playerId, int score)
+        {
+            if (!record.ContainsKey(playerId))
+                record[playerId] = 0;
+            record[playerId] += score;
+        }
+
+        public int Top(int K)
+        {
+            int res = 0;
+            var scores = record.OrderByDescending(x => x.Value).Select(x => x.Value).ToList();
+            for (int i = 0; i < K; i++)
+                res += scores[i];
+            return res;
+        }
+
+        public void Reset(int playerId)
+        {
+            record.Remove(playerId);
+        }
+    }
+}
