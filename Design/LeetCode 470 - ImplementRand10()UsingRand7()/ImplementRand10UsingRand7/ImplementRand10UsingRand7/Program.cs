45dca15ba1dc0eb74b80f3ce35a9c82ad02aa7ef
Xing <x.xing.unsw@gmail.com>
2020-08-28 20:43:42 +1000

    init

---
+++ Design/LeetCode 470 - ImplementRand10()UsingRand7()/ImplementRand10UsingRand7/ImplementRand10UsingRand7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ImplementRand10UsingRand7
+{
+    
+    class Program
+    {
+        public int Rand7()
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int Rand10()
+        {
+            return Ran40() % 10 + 1;
+        }
+
+        private int Rand49()
+        {
+            return (Rand7() - 1) * 7 + (Rand7() - 1);
+        }
+
+        private int Ran40()
+        {
+            int r = Rand49();
+            while (r >= 40)
+                r = Rand49();
+            return r;
+        }
+    }
+}
