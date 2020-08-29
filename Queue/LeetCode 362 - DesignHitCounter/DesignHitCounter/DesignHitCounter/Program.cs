dfb1d2ff45a8da86d9a7db06db34b21db0a2a8eb
Xing <x.xing.unsw@gmail.com>
2020-08-29 18:02:15 +1000

    init

---
+++ Queue/LeetCode 362 - DesignHitCounter/DesignHitCounter/DesignHitCounter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignHitCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class HitCounter
+    {
+        private Queue<int> queue;
+        /** Initialize your data structure here. */
+        public HitCounter()
+        {
+            queue = new Queue<int>();
+        }
+
+        /** Record a hit.
+            @param timestamp - The current timestamp (in seconds granularity). */
+        public void Hit(int timestamp)
+        {
+            queue.Enqueue(timestamp);
+        }
+
+        /** Return the number of hits in the past 5 minutes.
+            @param timestamp - The current timestamp (in seconds granularity). */
+        public int GetHits(int timestamp)
+        {
+            while (queue.Count != 0 && queue.Peek() <= timestamp - 300)
+                queue.Dequeue();
+            return queue.Count;
+        }
+    }
+}
