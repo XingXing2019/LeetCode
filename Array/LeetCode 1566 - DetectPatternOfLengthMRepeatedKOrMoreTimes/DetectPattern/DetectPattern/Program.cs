03c529e75f930015c571e89ecf50365e75274ca4
Xing <x.xing.unsw@gmail.com>
2020-08-30 14:20:52 +1000

    init

---
+++ Array/LeetCode 1566 - DetectPatternOfLengthMRepeatedKOrMoreTimes/DetectPattern/DetectPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DetectPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1, 2, 1, 2, 1, 3};
+            int m = 2, k = 3;
+            Console.WriteLine(ContainsPattern(arr, m, k));
+        }
+
+        static bool ContainsPattern(int[] arr, int m, int k)
+        {
+            if (arr.Length < m * k) return false;
+            var str = string.Join("", arr);
+            var patterns = new HashSet<string>();
+            for (int i = 0; i <= str.Length - m; i++)
+            {
+                var pattern = str.Substring(i, m);
+                var temp = "";
+                for (int j = 0; j < k; j++)
+                    temp += pattern;
+                patterns.Add(temp);
+            }
+            foreach (var pattern in patterns)
+            {
+                for (int i = 0; i <= str.Length - m * k; i++)
+                {
+                    if (str.Substring(i, m * k) == pattern)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Greedy/LeetCode 1564 - PutBoxesIntoTheWarehouseI/PutBoxesIntoTheWarehouseI/PutBoxesIntoTheWarehouseI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PutBoxesIntoTheWarehouseI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] boxes = { 4, 3, 4, 1 };
+            int[] warehouses = { 5, 3, 3, 4, 1 };
+            Console.WriteLine(MaxBoxesInWarehouse_BruteForce(boxes, warehouses));
+        }
+        static int MaxBoxesInWarehouse_BruteForce(int[] boxes, int[] warehouse)
+        {
+            Array.Sort(boxes);
+            int lastRoom = warehouse.Length - 1;
+            int res = 0;
+            for (int i = 0; i < boxes.Length; i++)
+            {
+                for (int j = 0; j < lastRoom; j++)
+                {
+                    if (boxes[i] > warehouse[j])
+                    {
+                        lastRoom = j;
+                        break;
+                    }
+                }
+                if (lastRoom != warehouse.Length - 1)
+                    lastRoom--;
+                res += lastRoom >= 0 ? 1 : 0;
+                if (lastRoom < 0) break;
+            }
+            return res;
+        }
+
+        static int MaxBoxesInWarehouse(int[] boxes, int[] warehouse)
+        {
+
+        }
+    }
+}
