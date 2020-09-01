f61068d6d22a2acc2f865fe5567de70265f51c15
Xing <x.xing.unsw@gmail.com>
2020-09-01 20:17:33 +1000

    init

---
+++ String/LeetCode 157 - ReadNCharactersGivenRead4/ReadNCharactersGivenRead4/ReadNCharactersGivenRead4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReadNCharactersGivenRead4
+{
+    class Program
+    {
+        int Read4(char[] buf4)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int Read(char[] buf, int n)
+        {
+            if (n == 0) return 0;
+            char[] temp = new char[4];
+            int count = 0;
+            while (count < n)
+            {
+                int cur = Read4(temp);
+                for (int i = 0; i < cur && count < n; i++)
+                    buf[count++] = temp[i];
+                if (cur < 4) break;
+            }
+            return count;
+        }
+    }
+}
