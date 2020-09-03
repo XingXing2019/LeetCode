cce0885cc9f202e9b6b2bbc9a97e249bd193e6fd
Xing <x.xing.unsw@gmail.com>
2020-09-03 19:17:58 +1000

    init

---
+++ DynamicProgramming/LeetCode 276 - PaintFence/PaintFence/PaintFence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PaintFence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int NumWays(int n, int k)
+        {
+            if (n == 0) return 0;
+            int same = 0, diff = k;
+            for (int i = 1; i < n; i++)
+            {
+                int temp = same;
+                same = diff;
+                diff = (temp + diff) * (k - 1);
+            }
+            return same + diff;
+        }
+    }
+}
