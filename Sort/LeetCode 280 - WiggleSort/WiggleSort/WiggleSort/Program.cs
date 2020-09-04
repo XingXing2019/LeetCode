f74800cde06a408009062aab347dbd6a0ea3ee47
Xing <x.xing.unsw@gmail.com>
2020-09-04 18:03:38 +1000

    init

---
+++ Sort/LeetCode 280 - WiggleSort/WiggleSort/WiggleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WiggleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 4 };
+            WiggleSort(nums);
+        }
+        static void WiggleSort(int[] nums)
+        {
+            var copy = new int[nums.Length];
+            Array.Copy(nums, copy, nums.Length);
+            Array.Sort(copy);
+            int li = 0, hi = copy.Length - 1, index = 0;
+            while (li < hi)
+            {
+                nums[index++] = copy[li++];
+                nums[index++] = copy[hi--];
+            }
+            if (index < nums.Length)
+                nums[index] = copy[li];
+        }
+    }
+}
