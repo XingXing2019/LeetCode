c00a8bc1fe987b8b1c77ad6db073075e9a8dbf1a
Xing <x.xing.unsw@gmail.com>
2020-09-05 11:38:53 +1000

    init

---
+++ Array/LeetCode 370 - RangeAddition/RangeAddition/RangeAddition/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RangeAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int[] GetModifiedArray(int length, int[][] updates)
+        {
+            var res = new int[length];
+            foreach (var update in updates)
+            {
+                res[update[0]] += update[2];
+                if (update[1] < res.Length - 1)
+                    res[update[1] + 1] += -update[2];
+            }
+            for (int i = 1; i < res.Length; i++)
+                res[i] += res[i - 1];
+            return res;
+        }
+    }
+}
