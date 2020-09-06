6a9e7cb3dc300732075661f3e47c6f89ed69719f
Xing <x.xing.unsw@gmail.com>
2020-09-06 17:22:46 +1000

    init

---
+++ Array/LeetCode 1572 - MatrixDiagonalSum/MatrixDiagonalSum/MatrixDiagonalSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace MatrixDiagonalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int DiagonalSum(int[][] mat)
+        {
+            int res = 0, index = 0;
+            foreach (var row in mat)
+            {
+                res += row[index];
+                res += row[^++index];
+            }
+            if (mat.Length % 2 != 0)
+                res -= mat[mat.Length / 2][mat.Length / 2];
+            return res;
+        }
+    }
+}
