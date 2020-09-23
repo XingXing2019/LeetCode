bccd47470a7ddf0a5e1bb37378e5b22e60237f0d
Xing <x.xing.unsw@gmail.com>
2020-09-23 17:12:44 +1000

    init

---
+++ Math/LeetCode 1256 - EncodeNumber/EncodeNumber/EncodeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EncodeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 63;
+            Console.WriteLine(Encode(num));
+        }
+        static string Encode(int num)
+        {
+            if (num == 0) return "";
+            int gap = 2, cur = 1, count = 0;
+            while (cur + gap <= num)
+            {
+                cur += gap;
+                gap <<= 1;
+                count++;
+            }
+            var res = Convert.ToString(num - cur, 2);
+            var len = res.Length;
+            for (int i = 0; i <= count - len; i++)
+                res = "0" + res;
+            return res;
+        }
+    }
+}
