10b00067fc4fd0f214795c8d94fbb7a67db16359
Xing <x.xing.unsw@gmail.com>
2020-09-26 17:53:18 +1000

    init

---
+++ String/LeetCode 800 - SimilarRGBColor/SimilarRGBColor/SimilarRGBColor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SimilarRGBColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public string SimilarRGB(string color)
+        {
+            var r = Convert.ToInt32(color.Substring(1, 2), 16);
+            var g = Convert.ToInt32(color.Substring(3, 2), 16);
+            var b = Convert.ToInt32(color.Substring(5, 2), 16);
+            return $"#{GetMin(r).ToString("x2")}{GetMin(g).ToString("x2")}{GetMin(b).ToString("x2")}";
+        }
+
+        private int GetMin(int num)
+        {
+            int curr = int.MaxValue;
+            int res = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                int candidate = i * 16 + i;
+                int diff = candidate - num;
+                diff *= diff;
+                if (diff < curr)
+                {
+                    curr = diff;
+                    res = candidate;
+                }
+            }
+            return res;
+        }
+    }
+}
