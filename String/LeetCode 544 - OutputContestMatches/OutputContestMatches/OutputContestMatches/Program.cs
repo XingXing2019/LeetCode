cb6cc0b4229d8c238cfbbb0ddcef930fa796e738
Xing <x.xing.unsw@gmail.com>
2020-09-26 16:54:12 +1000

    init

---
+++ String/LeetCode 544 - OutputContestMatches/OutputContestMatches/OutputContestMatches/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OutputContestMatches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string FindContestMatch(int n)
+        {
+            var teams = new string[n];
+            for (int i = 0; i < teams.Length; i++)
+                teams[i] = (i + 1).ToString();
+            while (n > 1)
+            {
+                for (int i = 0; i < n / 2; i++)
+                    teams[i] = $"({teams[i]},{teams[n - 1 - i]})";
+                n /= 2;
+            }
+            return teams[0];
+        }
+    }
+}
