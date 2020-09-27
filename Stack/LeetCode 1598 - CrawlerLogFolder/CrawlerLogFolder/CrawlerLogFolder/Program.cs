666753b534bcd19eb9c86dc3a7655f62d50adc20
Xing <x.xing.unsw@gmail.com>
2020-09-27 14:24:54 +1000

    init

---
+++ Stack/LeetCode 1598 - CrawlerLogFolder/CrawlerLogFolder/CrawlerLogFolder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CrawlerLogFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] logs = {"./", "../", "./"};
+            Console.WriteLine(MinOperations(logs));
+        }
+        static int MinOperations(string[] logs)
+        {
+            var pos = 0;
+            foreach (var log in logs)
+            {
+                if (log == "../")
+                    pos = Math.Max(0, pos - 1);
+                else if (log == "./")
+                    pos += 0;
+                else 
+                    pos += 1;
+            }
+            return pos;
+        }
+    }
+}
