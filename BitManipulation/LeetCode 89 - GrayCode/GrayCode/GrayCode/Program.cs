e835763ca2c37529b9134c0ae5d8d7f36d265ac3
Xing <x.xing.unsw@gmail.com>
2020-09-29 22:32:06 +1000

    init

---
+++ BitManipulation/LeetCode 89 - GrayCode/GrayCode/GrayCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GrayCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static IList<int> GrayCode(int n)
+        {
+            var res = new List<int>();
+            for (int i = 0; i < Math.Pow(2, n); i++)
+                res.Add((i >> 1) ^ i);
+            return res;
+        }
+    }
+}
