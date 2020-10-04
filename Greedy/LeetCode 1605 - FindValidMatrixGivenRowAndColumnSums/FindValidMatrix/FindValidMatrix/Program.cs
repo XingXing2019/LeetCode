767b6434f4b9a1e2fd209dfde8d16932d05d0f6b
Xing <x.xing.unsw@gmail.com>
2020-10-04 18:32:32 +1100

    init

---
+++ Greedy/LeetCode 1605 - FindValidMatrixGivenRowAndColumnSums/FindValidMatrix/FindValidMatrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace FindValidMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int[][] RestoreMatrix(int[] rowSum, int[] colSum)
+        {
+            var res = new int[rowSum.Length][];
+            for (int i = 0; i < res.Length; i++)
+                res[i] = new int[colSum.Length];
+            var count = rowSum.Count(x => x != 0) + colSum.Count(x => x != 0);
+            while (count != 0)
+            {
+                int r = 0, rowMin = int.MaxValue;
+                int c = 0, colMin = int.MaxValue;
+                for (int i = 0; i < rowSum.Length; i++)
+                {
+                    if(rowSum[i] == 0) continue;
+                    if (rowSum[i] < rowMin)
+                    {
+                        rowMin = rowSum[i];
+                        r = i;
+                    }
+                }
+                for (int i = 0; i < colSum.Length; i++)
+                {
+                    if(colSum[i] == 0) continue;
+                    if (colSum[i] < colMin)
+                    {
+                        colMin = colSum[i];
+                        c = i;
+                    }
+                }
+                res[r][c] = Math.Min(rowMin, colMin);
+                rowSum[r] -= Math.Min(rowMin, colMin);
+                colSum[c] -= Math.Min(rowMin, colMin);
+                if (rowSum[r] == 0) count--;
+                if (colSum[c] == 0) count--;
+            }
+            return res;
+        }
+    }
+}
