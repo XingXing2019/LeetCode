9b530b80431dcbf030ddbcee1bbf5ad0b51a387f
Xing <x.xing.unsw@gmail.com>
2020-10-04 16:46:57 +1100

    init

---
+++ Design/LeetCode 1603 - DesignParkingSystem/DesignParkingSystem/DesignParkingSystem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignParkingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class ParkingSystem
+    {
+        private Dictionary<int, int> dict;
+        public ParkingSystem(int big, int medium, int small)
+        {
+            dict = new Dictionary<int, int>{{1, big}, {2, medium}, {3, small}};
+        }
+
+        public bool AddCar(int carType)
+        {
+            dict[carType]--;
+            return dict[carType] >= 0;
+        }
+    }
+
+}
