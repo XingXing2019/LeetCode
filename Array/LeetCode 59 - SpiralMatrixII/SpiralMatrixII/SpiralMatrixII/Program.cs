e2a81f2e8cd858d44260255b79e2660624047fac
Xing <x.xing.unsw@gmail.com>
2020-10-10 21:00:38 +1100

    one more solution

--- Array/LeetCode 59 - SpiralMatrixII/SpiralMatrixII/SpiralMatrixII/Program.cs
+++ Array/LeetCode 59 - SpiralMatrixII/SpiralMatrixII/SpiralMatrixII/Program.cs
@@ -3,6 +3,7 @@
 //创建num用来记录当前需要写入数组的数字。
 //在num小于等于n平方的条件下遍历。依次在res的上右下左各边写入数字。做法和LeetCode-54一样。注意每写入一次数据就让num加一。
 using System;
+using System.Collections.Generic;
 
 namespace SpiralMatrixII
 {
@@ -51,6 +52,36 @@ namespace SpiralMatrixII
                 }
                 width--;
                 startC++;
+            }
+            return res;
+        }
+
+        static int[][] GenerateMatrix_DirectionArray(int n)
+        {
+            var res = new int[n][];
+            for (int i = 0; i < res.Length; i++)
+                res[i] = new int[n];
+            int[] dx = { 0, 1, 0, -1 };
+            int[] dy = { 1, 0, -1, 0 };
+            int direction = 0;
+            int x = 0, y = 0;
+            var visited = new HashSet<string>();
+            for (int i = 1; i <= n * n && visited.Count < n * n; i++)
+            {
+                res[x][y] = i;
+                visited.Add($"{x}:{y}");
+                int newX = x + dx[direction % 4];
+                int newY = y + dy[direction % 4];
+                if (newX < 0 || newX >= n || newY < 0 || newY >= n || visited.Contains($"{newX}:{newY}"))
+                {
+                    direction++;
+                    i--;
+                }
+                else
+                {
+                    x = newX;
+                    y = newY;
+                }
             }
             return res;
         }
