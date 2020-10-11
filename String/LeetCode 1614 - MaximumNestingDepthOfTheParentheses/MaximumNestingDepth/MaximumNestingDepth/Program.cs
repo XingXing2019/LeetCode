3b2091de2aba39999d41c579222c06b9c658cbe8
Xing <x.xing.unsw@gmail.com>
2020-10-11 15:20:26 +1100

    init

---
+++ String/LeetCode 1614 - MaximumNestingDepthOfTheParentheses/MaximumNestingDepth/MaximumNestingDepth/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MaximumNestingDepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MaxDepth(string s)
+        {
+            int left = 0, res = 0;
+            foreach (var letter in s)
+            {
+                if (letter == '(')
+                {
+                    left++;
+                    res = Math.Max(res, left);
+                }
+                else if (letter == ')')
+                    left--;
+            }
+            return res;
+        }
+    }
+}
