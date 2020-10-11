c7c310a1bd136589a7b0dc9c2463927aad8f0cf3
Xing <x.xing.unsw@gmail.com>
2020-10-11 16:04:00 +1100

    init

---
+++ String/LeetCode 1616 - SplitTwoStringsToMakePalindrome/SplitTwoStringsToMakePalindrome/SplitTwoStringsToMakePalindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SplitTwoStringsToMakePalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "pvhmupgqeltozftlmfjjde", b = "yjgpzbezspnnpszebzmhvp";
+            Console.WriteLine(CheckPalindromeFormation(a, b));
+        }
+        static bool CheckPalindromeFormation(string a, string b)
+        {
+            return Check(a, b) || Check(b, a);
+        }
+
+        static bool CheckPalindrome(string str, int li, int hi)
+        {
+            while (li < hi)
+            {
+                if (str[li++] != str[hi--])
+                    return false;
+            }
+            return true;
+        }
+
+        static bool Check(string a, string b)
+        {
+            int li = 0, hi = a.Length - 1;
+            while (li < hi && a[li] == b[hi])
+            {
+                li++;
+                hi--;
+            }
+            return CheckPalindrome(a, li, hi) || CheckPalindrome(b, li, hi);
+        }
+    }
+}
---
+++ Tree/LeetCode 1612 - CheckIfTwoExpressionTreesAreEquivalent/CheckIfTwoExpressionTreesAreEquivalent/CheckIfTwoExpressionTreesAreEquivalent/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CheckIfTwoExpressionTreesAreEquivalent
+{
+    public class Node
+    {
+        public char val;
+        public Node left;
+        public Node right;
+        public Node(char val = ' ', Node left = null, Node right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static bool CheckEquivalence(Node root1, Node root2)
+        {
+            int[] letters1 = new int[26], letters2 = new int[26];
+            DFS(root1, letters1);
+            DFS(root2, letters2);
+            for (int i = 0; i < 26; i++)
+            {
+                if (letters1[i] != letters2[i])
+                    return false;
+            }
+            return true;
+        }
+
+        static void DFS(Node node, int[] letters)
+        {
+            if(node == null) return;
+            if (char.IsLetter(node.val))
+                letters[node.val - 'a']++;
+            DFS(node.left, letters);
+            DFS(node.right, letters);
+        }
+    }
+}
