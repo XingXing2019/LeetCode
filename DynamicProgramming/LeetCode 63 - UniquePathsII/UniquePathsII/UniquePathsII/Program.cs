a6f227a5666fa04347284d416e1438c2f150493f
Xing <x.xing.unsw@gmail.com>
2020-10-12 21:02:07 +1100

    simplified code

--- DynamicProgramming/LeetCode 63 - UniquePathsII/UniquePathsII/UniquePathsII/Program.cs
+++ DynamicProgramming/LeetCode 63 - UniquePathsII/UniquePathsII/UniquePathsII/Program.cs
@@ -18,36 +18,21 @@ namespace UniquePathsII
         }
         static int UniquePathsWithObstacles(int[][] obstacleGrid)
         {
-            if (obstacleGrid[0][0] == 1)
-                return 0;
+            if (obstacleGrid.Length == 0 || obstacleGrid[0].Length == 0 || obstacleGrid[0][0] == 1 
+                || obstacleGrid[obstacleGrid.Length - 1][obstacleGrid[0].Length - 1] == 1) return 0;
             obstacleGrid[0][0] = 1;
-            int m = obstacleGrid.Length;
-            int n = obstacleGrid[0].Length;
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < obstacleGrid.Length; i++)
+                obstacleGrid[i][0] = obstacleGrid[i][0] == 1 ? 0 : obstacleGrid[i - 1][0];
+            for (int i = 1; i < obstacleGrid[0].Length; i++)
+                obstacleGrid[0][i] = obstacleGrid[0][i] == 1 ? 0 : obstacleGrid[0][i - 1];
+            for (int i = 1; i < obstacleGrid.Length; i++)
             {
-                if (obstacleGrid[0][i] == 1)
-                    obstacleGrid[0][i] = 0;
-                else
-                    obstacleGrid[0][i] = obstacleGrid[0][i - 1];
-            }
-            for (int i = 1; i < m; i++)
-            {
-                if (obstacleGrid[i][0] == 1)
-                    obstacleGrid[i][0] = 0;
-                else
-                    obstacleGrid[i][0] = obstacleGrid[i - 1][0];
-            }
-            for (int i = 1; i < m; i++)
-            {
-                for (int j = 1; j < n; j++)
+                for (int j = 1; j < obstacleGrid[0].Length; j++)
                 {
-                    if (obstacleGrid[i][j] == 1)
-                        obstacleGrid[i][j] = 0;
-                    else
-                        obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                    obstacleGrid[i][j] = obstacleGrid[i][j] == 1 ? 0 : obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
                 }
             }
-            return obstacleGrid[m - 1][n - 1];
+            return obstacleGrid[obstacleGrid.Length - 1][obstacleGrid[0].Length - 1];
         }
     }
 }
