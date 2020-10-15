ec83dd3383967e4135d596096ee8c06e17c28ce4
Xing <x.xing.unsw@gmail.com>
2020-10-15 21:09:12 +1100

    one more solution

--- LinkedList/LeetCode 82 - RemoveDuplicatesFromSortedListII/RemoveDuplicatesFromSortedListII/RemoveDuplicatesFromSortedListII/Program.cs
+++ LinkedList/LeetCode 82 - RemoveDuplicatesFromSortedListII/RemoveDuplicatesFromSortedListII/RemoveDuplicatesFromSortedListII/Program.cs
@@ -1,6 +1,7 @@
 ﻿//设置三个指针：dummy，pre和point。遍历链表，只有到point指针的值和其前后指针都不相等时，连接dummy指针和point指针。
 //当point.next为空时停止遍历。人为检查是否连接最后一个节点。
 using System;
+using System.Collections.Generic;
 
 namespace RemoveDuplicatesFromSortedListII
 {
@@ -43,6 +44,28 @@ namespace RemoveDuplicatesFromSortedListII
             }
             else
                 return null;
+        }
+        static ListNode DeleteDuplicates_List(ListNode head)
+        {
+            if (head == null) return null;
+            var record = new List<ListNode>();
+            var remove = false;
+            while (head != null)
+            {
+                if (record.Count == 0 || head.val != record[record.Count - 1].val)
+                {
+                    if (remove) record.RemoveAt(record.Count - 1);
+                    if (record.Count != 0) record[record.Count - 1].next = head;
+                    record.Add(head);
+                    remove = false;
+                }
+                else
+                    remove = true;
+                head = head.next;
+            }
+            if (remove) record.RemoveAt(record.Count - 1);
+            if (record.Count != 0) record[record.Count - 1].next = head;
+            return record.Count == 0 ? null : record[0];
         }
     }
 }
