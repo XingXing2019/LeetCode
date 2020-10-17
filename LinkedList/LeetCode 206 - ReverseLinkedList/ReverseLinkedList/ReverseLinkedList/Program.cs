69a21feb0c39585912f8f40c1ce3ea657d5a9594
Xing <x.xing.unsw@gmail.com>
2020-10-17 22:00:31 +1100

    one more solution

--- LinkedList/LeetCode 206 - ReverseLinkedList/ReverseLinkedList/ReverseLinkedList/Program.cs
+++ LinkedList/LeetCode 206 - ReverseLinkedList/ReverseLinkedList/ReverseLinkedList/Program.cs
@@ -2,31 +2,36 @@
 
 namespace ReverseLinkedList
 {
-    class Node
+    public class ListNode
     {
         public int val;
-        public Node next;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Node list = CreateList(new int[] { 1, 2, 3, 4, 5 });
-            Node res = ReverseLinkedList(list);
+            ListNode list = CreateList(new int[] { 1, 2, 3, 4, 5 });
+            ListNode res = ReverseLinkedList(list);
             PrintList(list);
             Console.WriteLine("");
             PrintList(res);
         }
-        static Node CreateList(int[] nums)
+        static ListNode CreateList(int[] nums)
         {
-            Node head = null;
+            ListNode head = null;
             for (int i = nums.Length - 1; i >= 0; i--)
             {
-                head = new Node() { val = nums[i], next = head }; 
+                head = new ListNode() { val = nums[i], next = head };
             }
             return head;
         }
-        static void PrintList(Node nodeHead)
+        static void PrintList(ListNode nodeHead)
         {
             while (nodeHead != null)
             {
@@ -34,15 +39,28 @@ namespace ReverseLinkedList
                 nodeHead = nodeHead.next;
             }
         }
-        static Node ReverseLinkedList(Node list)
+        static ListNode ReverseLinkedList(ListNode list)
         {
-            Node res = null;
+            ListNode res = null;
             while (list != null)
             {
-                res = new Node() { val = list.val, next = res };
+                res = new ListNode() { val = list.val, next = res };
                 list = list.next;
             }
             return res;
+        }
+        public ListNode ReverseList_ReverseInPlace(ListNode head)
+        {
+            if (head == null) return null;
+            ListNode pre = new ListNode(0, head);
+            while (head.next != null)
+            {
+                var post = head.next;
+                head.next = post.next;
+                post.next = pre.next;
+                pre.next = post;
+            }
+            return pre.next;
         }
     }
 }
