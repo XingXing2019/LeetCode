d9012a0c0377a63e0c3c5b49ba6bbee6e56db22d
Xing <x.xing.unsw@gmail.com>
2020-10-18 20:35:35 +1100

    init

---
+++ Array/LeetCode 1619 - MeanOfArrayAfterRemovingSomeElements/MeanOfArray/MeanOfArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MeanOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public double TrimMean(int[] arr)
+        {
+            Array.Sort(arr);
+            int start = arr.Length / 20;
+            double sum = 0;
+            for (int i = start; i < arr.Length - start; i++)
+                sum += arr[i];
+            return sum / (arr.Length - 2 * start);
+        }
+    }
+}
