b48571f7f43d7e7d42c4ba9c5add90e08f8ab111
Xing <x.xing.unsw@gmail.com>
2020-10-18 20:46:45 +1100

    init

---
+++ String/LeetCode 1624 - LargestSubstringBetweenTwoEqualCharacters/LargestSubstring/LargestSubstring/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LargestSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int MaxLengthBetweenEqualCharacters(string s)
+        {
+            var record = new int[26];
+            for (int i = 0; i < record.Length; i++)
+                record[i] = -1;
+            var max = -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (record[s[i] - 'a'] == -1)
+                    record[s[i] - 'a'] = i;
+                else
+                    max = Math.Max(max, i - record[s[i] - 'a'] - 1);
+            }
+            return max;
+        }
+    }
+}
