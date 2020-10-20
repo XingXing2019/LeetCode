6643cccda14514d36d09552dffdb551d68b7690c
Xing <x.xing.unsw@gmail.com>
2020-10-20 22:57:50 +1100

    simplified code

--- DynamicProgramming/LeetCode 91 - DecodeWays/DecodeWays/DecodeWays/Program.cs
+++ DynamicProgramming/LeetCode 91 - DecodeWays/DecodeWays/DecodeWays/Program.cs
@@ -19,43 +19,21 @@ namespace DecodeWays
         }
         static int NumDecodings(string s)
         {
-            int[] dp = new int[s.Length];
-            if (s[0] == '0')
-                return 0;
-            else
-                dp[0] = 1;
-            if (s.Length < 2)
-                return s.Length;
-            if (s[1] == '0')
-            {
-                if (s[0] == '1' || s[0] == '2')
-                    dp[1] = 1;
-                else
-                    return 0;
-            }
-            else
-                dp[1] = int.Parse(s.Substring(0, 2)) <= 26 ? 2 : 1;
+            if (s[0] == '0') return 0;
+            if (s.Length < 2) return s.Length;
+            var dp = new int[s.Length];
+            dp[0] = 1;
+            int num = int.Parse(s.Substring(0, 2));
+            if (s[1] == '0' && (num == 0 || num > 26)) return 0;
+            dp[1] = s[1] == '0' || num > 26 ? 1 : 2;
             for (int i = 2; i < s.Length; i++)
             {
+                num = int.Parse(s.Substring(i - 1, 2));
+                if (s[i] == '0' && num > 26 || num == 0) return 0;
                 if (s[i] == '0')
-                {
-                    if (s[i - 1] == '1' || s[i - 1] == '2')
-                        dp[i] = dp[i - 2];
-                    else
-                        return 0;
-                }
+                    dp[i] = dp[i - 2];
                 else
-                {
-                    if (s[i - 1] == '0')
-                        dp[i] = dp[i - 1];
-                    else
-                    {
-                        if (int.Parse(s.Substring(i - 1, 2)) <= 26)
-                            dp[i] = dp[i - 1] + dp[i - 2];
-                        else
-                            dp[i] = dp[i - 1];
-                    }
-                }
+                    dp[i] = num < 10 || num > 26 ? dp[i - 1] : dp[i - 2] + dp[i - 1];
             }
             return dp[dp.Length - 1];
         }
