a3ff7d18266230ed58bc7088290cbec1a7a45fbd
Xing <x.xing.unsw@gmail.com>
2020-10-20 20:58:08 +1100

    init

---
+++ BinarySearch/LeetCode 1618 - MaximumFontToFitASentenceInAScreen/MaximumFont/MaximumFont/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumFont
+{
+    interface FontInfo
+    {
+        public int GetWidth(int fontSize, char ch);
+        public int GetHeight(int fontSize);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MaxFont(string text, int w, int h, int[] fonts, FontInfo fontInfo)
+        {
+            int li = 0, hi = fonts.Length - 1;
+            while (li <= hi)
+            {
+                int mid = li + (hi - li) / 2;
+                var height = fontInfo.GetHeight(fonts[mid]);
+                var width = 0;
+                foreach (var letter in text)
+                    width += fontInfo.GetWidth(fonts[mid], letter);
+                if (height <= h && width <= w)
+                    li = mid + 1;
+                else
+                    hi = mid - 1;
+            }
+            return hi < 0 ? -1 : fonts[hi];
+        }
+    }
+}
