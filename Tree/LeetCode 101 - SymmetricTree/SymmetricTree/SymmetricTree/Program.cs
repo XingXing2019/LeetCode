9b7a828e514c5fda03fc963310569dd6537f017e
Xing <x.xing.unsw@gmail.com>
2020-10-21 20:25:56 +1100

    one more solution

--- Tree/LeetCode 101 - SymmetricTree/SymmetricTree/SymmetricTree/Program.cs
+++ Tree/LeetCode 101 - SymmetricTree/SymmetricTree/SymmetricTree/Program.cs
@@ -42,5 +42,25 @@ namespace SymmetricTree
             else
                 return left == null && right == null;
         }
+        static bool IsSymmetric_Queue(TreeNode root)
+        {
+            if (root == null) return true;
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root.left);
+            queue.Enqueue(root.right);
+            while (queue.Count != 0)
+            {
+                var left = queue.Dequeue();
+                var right = queue.Dequeue();
+                if (left == right) continue;
+                if (left == null && right != null || left != null && right == null) return false;
+                if (left.val != right.val) return false;
+                queue.Enqueue(left.left);
+                queue.Enqueue(right.right);
+                queue.Enqueue(left.right);
+                queue.Enqueue(right.left);
+            }
+            return true;
+        }
     }
 }
