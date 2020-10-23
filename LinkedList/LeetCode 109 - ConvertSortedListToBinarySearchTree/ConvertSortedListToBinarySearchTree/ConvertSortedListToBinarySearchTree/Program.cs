640e0174ae1fc6c3b4a1543237aabfd4be3de8a1
Xing <x.xing.unsw@gmail.com>
2020-10-23 16:01:54 +1100

    recursion solution

--- LinkedList/LeetCode 109 - ConvertSortedListToBinarySearchTree/ConvertSortedListToBinarySearchTree/ConvertSortedListToBinarySearchTree/Program.cs
+++ LinkedList/LeetCode 109 - ConvertSortedListToBinarySearchTree/ConvertSortedListToBinarySearchTree/ConvertSortedListToBinarySearchTree/Program.cs
@@ -8,7 +8,11 @@ namespace ConvertSortedListToBinarySearchTree
     {
         public int val;
         public ListNode next;
-        public ListNode(int x) { val = x; }
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
     }
 
     public class TreeNode
@@ -16,7 +20,12 @@ namespace ConvertSortedListToBinarySearchTree
         public int val;
         public TreeNode left;
         public TreeNode right;
-        public TreeNode(int x) { val = x; }
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
     class Program
     {
@@ -64,8 +73,27 @@ namespace ConvertSortedListToBinarySearchTree
             res[2] = dummy;
             return res;
         }
-        
-    
+
+
+        static TreeNode SortedListToBST_Recursion(ListNode head)
+        {
+            if (head == null) return null;
+            ListNode slow = head, fast = head, pre = new ListNode(0, head);
+            while (fast != null)
+            {
+                fast = fast.next;
+                if (fast == null) break;
+                fast = fast.next;
+                slow = slow.next;
+                pre = pre.next;
+            }
+            var root = new TreeNode(slow.val);
+            root.right = SortedListToBST_Recursion(slow.next);
+            var isHead = pre.next == head;
+            pre.next = null;
+            root.left = SortedListToBST_Recursion(isHead ? null : head);
+            return root;
+        }
 
         static ListNode Generate(int[] nums)
         {
