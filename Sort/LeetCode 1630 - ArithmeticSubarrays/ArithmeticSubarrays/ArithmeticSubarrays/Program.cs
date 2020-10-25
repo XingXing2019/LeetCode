fd931373d0ab827a940317be3753a3b34d2f31f6
Xing <x.xing.unsw@gmail.com>
2020-10-25 17:05:58 +1100

    init

---
+++ Sort/LeetCode 1630 - ArithmeticSubarrays/ArithmeticSubarrays/ArithmeticSubarrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArithmeticSubarrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public IList<bool> CheckArithmeticSubarrays(int[] nums, int[] l, int[] r)
+        {
+            var res = new bool[l.Length];
+            for (int i = 0; i < res.Length; i++)
+                res[i] = true;
+            for (int i = 0; i < l.Length; i++)
+            {
+                var set = nums[l[i]..(r[i] + 1)];
+                Array.Sort(set);
+                var diff = set[1] - set[0];
+                for (int j = 1; j < set.Length; j++)
+                {
+                    if (set[j] - set[j - 1] != diff)
+                    {
+                        res[i] = false;
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
