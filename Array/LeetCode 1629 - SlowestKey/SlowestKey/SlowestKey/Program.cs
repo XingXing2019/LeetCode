b9d3c31754b8c97421b2c852d29a4d92c236e81a
Xing <x.xing.unsw@gmail.com>
2020-10-25 16:44:22 +1100

    init

---
+++ Array/LeetCode 1629 - SlowestKey/SlowestKey/SlowestKey/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SlowestKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] releaseTimes = {9, 29, 49, 50};
+            var keyPressed = "cbcd";
+            Console.WriteLine(SlowestKey(releaseTimes, keyPressed));
+        }
+        static char SlowestKey(int[] releaseTimes, string keysPressed)
+        {
+            var res = keysPressed[0];
+            var max = releaseTimes[0];
+            for (int i = 1; i < releaseTimes.Length; i++)
+            {
+                if (releaseTimes[i] - releaseTimes[i - 1] > max)
+                {
+                    max = releaseTimes[i] - releaseTimes[i - 1];
+                    res = keysPressed[i];
+                }
+                else if(releaseTimes[i] - releaseTimes[i - 1] == max && keysPressed[i] > res)
+                    res = keysPressed[i];
+            }
+            return res;
+        }
+    }
+}
