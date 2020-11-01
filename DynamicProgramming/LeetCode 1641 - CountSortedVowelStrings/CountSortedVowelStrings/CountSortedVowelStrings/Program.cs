0ce2e51dd4ecfe1dde36c6edcda86918c6c706a0
Xing <x.xing.unsw@gmail.com>
2020-11-01 16:23:01 +1100

    init

---
+++ DynamicProgramming/LeetCode 1641 - CountSortedVowelStrings/CountSortedVowelStrings/CountSortedVowelStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CountSortedVowelStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountVowelStrings(int n)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Sort/LeetCode 1637 - WidestVerticalAreaBetweenTwoPointsContainingNoPoints/WidestVerticalArea/WidestVerticalArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace WidestVerticalArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int MaxWidthOfVerticalArea(int[][] points)
+        {
+            points = points.OrderBy(x => x[0]).ToArray();
+            var res = 0;
+            for (int i = 1; i < points.Length; i++)
+                res = Math.Max(res, points[i][0] - points[i - 1][0]);
+            return res;
+        }
+    }
+}
