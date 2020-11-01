f3dc9865a1426a82dae58ee35e439f4817ba6814
Xing <x.xing.unsw@gmail.com>
2020-11-01 15:43:10 +1100

    init

---
+++ Sort/LeetCode 1640 - CheckArrayFormationThroughConcatenation/CheckArray/CheckArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {49, 18, 16};
+            var pieces = new int[][]
+            {
+                new[] {16, 18, 49}
+            };
+            Console.WriteLine(CanFormArray(arr, pieces));
+        }
+        static bool CanFormArray(int[] arr, int[][] pieces)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+                dict[arr[i]] = i;
+            foreach (var piece in pieces)
+            {
+                if (!dict.ContainsKey(piece[0])) return false;
+                for (int i = 0; i < piece.Length; i++)
+                {
+                    if (dict[piece[0]] + i >= arr.Length || piece[i] != arr[dict[piece[0]] + i])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
