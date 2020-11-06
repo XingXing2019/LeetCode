4d30dd40542ac6cd2f20df62c020293f10e9235c
Xing <x.xing.unsw@gmail.com>
2020-11-06 17:04:05 +1100

    one more solution

--- Design/LeetCode 170 - TwoSumIII/TwoSumIII/TwoSumIII/Program.cs
+++ Design/LeetCode 170 - TwoSumIII/TwoSumIII/TwoSumIII/Program.cs
@@ -35,4 +35,38 @@ namespace TwoSumIII
             return false;
         }
     }
+    public class TwoSum_Dictionary
+    {
+
+        private Dictionary<int, int> nums;
+        private HashSet<int> values;
+        public TwoSum_Dictionary()
+        {
+            nums = new Dictionary<int, int>();
+            values = new HashSet<int>();
+        }
+
+        public void Add(int number)
+        {
+            if (!nums.ContainsKey(number))
+                nums[number] = 0;
+            nums[number]++;
+        }
+
+        public bool Find(int value)
+        {
+            if (values.Contains(value))
+                return true;
+            foreach (var kv in nums)
+            {
+                if (kv.Key != value - kv.Key && nums.ContainsKey(value - kv.Key) ||
+                    kv.Key == value - kv.Key && kv.Value > 1)
+                {
+                    values.Add(value);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
