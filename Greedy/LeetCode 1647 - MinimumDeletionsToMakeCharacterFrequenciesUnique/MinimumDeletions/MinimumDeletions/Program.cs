0bf3d6594f0c9a8d2568a25a560a4eb246cf38c0
Xing <x.xing.unsw@gmail.com>
2020-11-08 16:09:21 +1100

    init

---
+++ Greedy/LeetCode 1647 - MinimumDeletionsToMakeCharacterFrequenciesUnique/MinimumDeletions/MinimumDeletions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinimumDeletions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "aaabbbcceeeeffffgggg";
+            Console.WriteLine(MinDeletions(s));
+        }
+        static int MinDeletions(string s)
+        {
+            var record = new int[26];
+            var max = 0;
+            foreach (var letter in s)
+            {
+                record[letter - 'a']++;
+                max = Math.Max(max, record[letter - 'a']);
+            }
+            var frequency = new int[max + 1];
+            var candidates = new Stack<int>();
+            for (int i = 0; i < record.Length; i++)
+            {
+                if(record[i] == 0) continue;
+                frequency[record[i]]++;
+            }
+            var res = 0;
+            for (int i = 1; i < frequency.Length; i++)
+            {
+                if(frequency[i] == 0)
+                    candidates.Push(i);
+                else if(frequency[i] != 1)
+                {
+                    for (int j = 0; j < frequency[i] - 1; j++)
+                        res += candidates.Count == 0 ? i : i - candidates.Pop();
+                }
+            }
+            return res;
+        }
+    }
+}
