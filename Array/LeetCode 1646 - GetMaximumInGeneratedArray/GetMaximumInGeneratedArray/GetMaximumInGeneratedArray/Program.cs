561e1c7ae478e52aadec8d7b71dc914f19aa78fa
Xing <x.xing.unsw@gmail.com>
2020-11-08 15:39:48 +1100

    init

---
+++ Array/LeetCode 1646 - GetMaximumInGeneratedArray/GetMaximumInGeneratedArray/GetMaximumInGeneratedArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GetMaximumInGeneratedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int GetMaximumGenerated(int n)
+        {
+            if (n < 2) return n;
+            var nums = new int[n + 1];
+            nums[1] = 1;
+            var max = 0;
+            for (int i = 2; i < nums.Length; i++)
+            {
+                nums[i] = i % 2 == 0 ? nums[i / 2] : nums[i / 2] + nums[i / 2 + 1];
+                max = Math.Max(max, nums[i]);
+            }
+            return max;
+        }
+    }
+}
