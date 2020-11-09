a264d8e1b1ddee8a0afb431483cd2f06dd954dff
Xing <x.xing.unsw@gmail.com>
2020-11-09 18:16:32 +1100

    one more solution

--- BitManipulation/LeetCode 190 - ReverseBits/ReverseBits/ReverseBits/Program.cs
+++ BitManipulation/LeetCode 190 - ReverseBits/ReverseBits/ReverseBits/Program.cs
@@ -7,9 +7,9 @@ namespace ReverseBits
         static void Main(string[] args)
         {
             uint n = 4294967293;
-            Console.WriteLine(reverseBits(n));
+            Console.WriteLine(reverseBits_XOR(n));
         }
-        static uint reverseBits(uint n)
+        static uint reverseBits_XOR(uint n)
         {
             uint res = 0;
             uint temp = n;
@@ -25,6 +25,27 @@ namespace ReverseBits
                 n >>= 1;
                 if(n == 0)
                     break;
+                res <<= 1;
+            }
+            for (int i = 0; i < 32 - step; i++)
+                res <<= 1;
+            return res;
+        }
+        static uint reverseBits_OR(uint n)
+        {
+            var copy = n;
+            int step = 0;
+            while (copy != 0)
+            {
+                copy >>= 1;
+                step++;
+            }
+            uint res = 0;
+            while (n != 0)
+            {
+                res |= 1 & n;
+                n >>= 1;
+                if (n == 0) break;
                 res <<= 1;
             }
             for (int i = 0; i < 32 - step; i++)
