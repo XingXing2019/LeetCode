6583bb98f9db2db87d1061646a78f7c93d26b7f5
Xing <x.xing.unsw@gmail.com>
2020-11-11 20:17:11 +1100

    simplified code

--- Math/LeetCode 204 - CountPrimes/CountPrimes/CountPrimes/Program.cs
+++ Math/LeetCode 204 - CountPrimes/CountPrimes/CountPrimes/Program.cs
@@ -2,6 +2,7 @@
 //从第二个数开始遍历到n。如果当前数字不是质数，则跳过本次遍历，如果当前数字为质数，则当前数字的从2倍开始的所有倍数都不是质数，应设为false。
 //最后创建count记录结果，从2到n开始遍历，找出所有质数，记录如count并返回。
 using System;
+using System.Linq;
 
 namespace CountPrimes
 {
@@ -14,23 +15,16 @@ namespace CountPrimes
         }
         static int CountPrimes(int n)
         {
-            bool[] isPrimes = new bool[n];
-            for (int i = 2; i < n; i++)
-                isPrimes[i] = true;
-            for (int i = 2; i < n; i++)
+            var isPrime = new bool[n];
+            for (int i = 2; i < isPrime.Length; i++)
+                isPrime[i] = true;
+            for (int i = 2; i < isPrime.Length; i++)
             {
-                if (isPrimes[i] == false)
-                    continue;
-                for (int j = 2*i; j < n; j += i)
-                    isPrimes[j] = false;
+                if (!isPrime[i]) continue;
+                for (int j = 2; i * j < n; j++)
+                    isPrime[i * j] = false;
             }
-            int count = 0;
-            for (int i = 2; i < n; i++)
-            {
-                if (isPrimes[i])
-                    count++;
-            }
-            return count;
+            return isPrime.Count(x => x);
         }
     }
 }
