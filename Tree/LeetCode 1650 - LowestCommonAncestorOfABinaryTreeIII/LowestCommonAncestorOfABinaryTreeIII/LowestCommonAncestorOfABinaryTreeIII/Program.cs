e781baf0195538ae5ed078eae0a37780e09af820
Xing <x.xing.unsw@gmail.com>
2020-11-13 15:02:25 +1100

    init

---
+++ Tree/LeetCode 1650 - LowestCommonAncestorOfABinaryTreeIII/LowestCommonAncestorOfABinaryTreeIII/LowestCommonAncestorOfABinaryTreeIII/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LowestCommonAncestorOfABinaryTreeIII
+{
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static Node LowestCommonAncestor(Node p, Node q)
+        {
+            var set = new HashSet<Node>();
+            while (p != null)
+            {
+                set.Add(p);
+                p = p.parent;
+            }
+            while (q != null)
+            {
+                if (set.Contains(q))
+                    return q;
+                q = q.parent;
+            }
+            return null;
+        }
+    }
+}
