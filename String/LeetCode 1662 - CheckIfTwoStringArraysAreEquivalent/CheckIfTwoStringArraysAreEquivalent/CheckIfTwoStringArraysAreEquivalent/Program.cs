7b193d4b72f26040cb06ab818fa0f7d63cff93aa
Xing <x.xing.unsw@gmail.com>
2020-11-22 16:19:29 +1100

    init

---
+++ String/LeetCode 1662 - CheckIfTwoStringArraysAreEquivalent/CheckIfTwoStringArraysAreEquivalent/CheckIfTwoStringArraysAreEquivalent/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CheckIfTwoStringArraysAreEquivalent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            return string.Join("", word1) == string.Join("", word2);
+        }
+    }
+}
