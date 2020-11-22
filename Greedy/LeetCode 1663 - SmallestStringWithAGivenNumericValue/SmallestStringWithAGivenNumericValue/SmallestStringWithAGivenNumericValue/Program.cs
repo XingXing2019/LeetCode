c1c9280f298550c81e2d80cffe2a260f79b3be15
Xing <x.xing.unsw@gmail.com>
2020-11-22 16:55:59 +1100

    init

---
+++ Greedy/LeetCode 1663 - SmallestStringWithAGivenNumericValue/SmallestStringWithAGivenNumericValue/SmallestStringWithAGivenNumericValue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SmallestStringWithAGivenNumericValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5, k = 73;
+            Console.WriteLine(GetSmallestString(n, k));
+        }
+        static string GetSmallestString(int n, int k)
+        {
+            var count = (k - n) / 25;
+            k -= count * 26;
+            n -= count;
+            var left = new string('a', n - 1);
+            var right = new string('z', count);
+            k -= left.Length;
+            return left + (char)(k - 1 + 'a') + right;
+        }
+    }
+}
