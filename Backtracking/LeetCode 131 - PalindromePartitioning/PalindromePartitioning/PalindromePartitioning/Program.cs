34c59b5a7fcb91e1ce8f281836f868a1caf0cca3
Xing <x.xing.unsw@gmail.com>
2020-11-25 15:20:54 +1100

    init

---
+++ Backtracking/LeetCode 131 - PalindromePartitioning/PalindromePartitioning/PalindromePartitioning/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PalindromePartitioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public IList<IList<string>> Partition(string s)
+        {
+            var res = new List<IList<string>>();
+            DFS(s, new List<string>(), res);
+            return res;
+        }
+
+        private void DFS(string remain, List<string> item, List<IList<string>> res)
+        {
+            if(remain.Length == 0)
+                res.Add(new List<string>(item));
+            for (int i = 0; i < remain.Length; i++)
+            {
+                if (CheckPalindrome(remain.Substring(0, i + 1)))
+                {
+                    item.Add(remain.Substring(0, i + 1));
+                    DFS(remain.Substring(i + 1), item, res);
+                    item.RemoveAt(item.Count - 1);
+                }
+            }
+        }
+
+        private bool CheckPalindrome(string s)
+        {
+            int li = 0, hi = s.Length - 1;
+            while (li < hi)
+            {
+                if (s[li++] != s[hi--])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
