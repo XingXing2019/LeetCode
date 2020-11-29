05f60d3d03bdb0e771673aa4f833696380c89918
Xing <x.xing.unsw@gmail.com>
2020-11-29 16:11:06 +1100

    init

---
+++ Array/LeetCode 1672 - RichestCustomerWealth/RichestCustomerWealth/RichestCustomerWealth/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace RichestCustomerWealth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int MaximumWealth(int[][] accounts)
+        {
+            var res = 0;
+            foreach (var account in accounts)
+                res = Math.Max(res, account.Sum());
+            return res;
+        }
+    }
+}
