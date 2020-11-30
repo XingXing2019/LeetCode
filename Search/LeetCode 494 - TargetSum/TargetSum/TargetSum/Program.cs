c29fa978a49ce4af3f4a490533d8d905d7b04fb4
Xing <x.xing.unsw@gmail.com>
2020-11-30 20:19:08 +1100

    init

---
+++ Search/LeetCode 494 - TargetSum/TargetSum/TargetSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TargetSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 1, 1, 1, 1};
+            int S = 3;
+            Console.WriteLine(FindTargetSumWays(nums, S));
+        }
+        static int FindTargetSumWays(int[] nums, int S)
+        {
+            var res = 0;
+            DFS(nums, S, 0, ref res);
+            return res;
+        }
+
+        static void DFS(int[] nums, int S, int index, ref int res)
+        {
+            if (index == nums.Length)
+            {
+                if (S == 0)
+                    res++;
+            }
+            else
+            {
+                DFS(nums, S + nums[index], index + 1, ref res);
+                DFS(nums, S - nums[index], index + 1, ref res);
+            }
+        }
+    }
+}
