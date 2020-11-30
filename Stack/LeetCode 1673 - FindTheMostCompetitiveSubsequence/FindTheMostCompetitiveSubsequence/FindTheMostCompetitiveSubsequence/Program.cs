ea08ab5c9c91c158d7064d82b689954858d8f6de
Xing <x.xing.unsw@gmail.com>
2020-11-30 19:50:41 +1100

    init

---
+++ Stack/LeetCode 1673 - FindTheMostCompetitiveSubsequence/FindTheMostCompetitiveSubsequence/FindTheMostCompetitiveSubsequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindTheMostCompetitiveSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 4, 3, 3, 5, 4, 9, 6 };
+            int k = 4;
+            Console.WriteLine(MostCompetitive(nums, k));
+        }
+        static int[] MostCompetitive(int[] nums, int k)
+        {
+            var stack = new Stack<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = nums.Length - i;
+                while (stack.Count != 0 && nums[i] < stack.Peek() && stack.Count + count > k) 
+                    stack.Pop();
+                if (stack.Count < k)
+                    stack.Push(nums[i]);
+            }
+            var res = new int[k];
+            int index = k - 1;
+            while (stack.Count != 0)
+                res[index--] = stack.Pop();
+            return res;
+        }
+    }
+}
