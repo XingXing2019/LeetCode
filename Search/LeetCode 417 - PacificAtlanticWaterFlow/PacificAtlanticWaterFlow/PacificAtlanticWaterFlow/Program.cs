c4ab8669450cb42134ed994f37c29927aa4a79f4
Xing <x.xing.unsw@gmail.com>
2020-12-05 15:21:42 +1100

    fast code

--- Search/LeetCode 417 - PacificAtlanticWaterFlow/PacificAtlanticWaterFlow/PacificAtlanticWaterFlow/Program.cs
+++ Search/LeetCode 417 - PacificAtlanticWaterFlow/PacificAtlanticWaterFlow/PacificAtlanticWaterFlow/Program.cs
@@ -7,53 +7,113 @@ namespace PacificAtlanticWaterFlow
     {
         static void Main(string[] args)
         {
-            var matrix = new int[3][]
+            var matrix = new int[][]
             {
-                new int[] {1, 2, 3},
-                new int[] {8, 9, 4},
-                new int[] {7, 6, 5},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
             };
-            Console.WriteLine(PacificAtlantic(matrix));
+            Console.WriteLine(PacificAtlantic_BFS(matrix));
 
         }
-        static IList<IList<int>> PacificAtlantic(int[][] matrix)
+        
+        static IList<IList<int>> PacificAtlantic_BFS(int[][] matrix)
         {
             var res = new List<IList<int>>();
             if (matrix.Length == 0 || matrix[0].Length == 0) return res;
-            for (int r = 0; r < matrix.Length; r++)
+            var reachPacific = new bool[matrix.Length, matrix[0].Length];
+            var reachAtlantic = new bool[matrix.Length, matrix[0].Length];
+            int[] direction = { 0, 1, 0, -1, 0 };
+            for (int i = 0; i < matrix[0].Length; i++)
             {
-                for (int c = 0; c < matrix[0].Length; c++)
+                reachPacific[0, i] = true;
+                reachAtlantic[matrix.Length - 1, i] = true;
+                BFS(matrix, 0, i, direction, reachPacific);
+                BFS(matrix, matrix.Length - 1, i, direction, reachAtlantic);
+            }
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                reachPacific[i, 0] = true;
+                reachAtlantic[i, matrix[0].Length - 1] = true;
+                BFS(matrix, i, 0, direction, reachPacific);
+                BFS(matrix, i, matrix[0].Length - 1, direction, reachAtlantic);
+            }
+            for (int x = 0; x < matrix.Length; x++)
+            {
+                for (int y = 0; y < matrix[0].Length; y++)
                 {
-                    var mark = new int[matrix.Length][];
-                    for (int i = 0; i < mark.Length; i++)
-                        mark[i] = new int[matrix[0].Length];
-                    var reach = new HashSet<string>();
-                    DFS(matrix, mark, r, c, reach);
-                    if (reach.Contains("Pacific") && reach.Contains("Atlantic"))
-                        res.Add(new int[] {r, c});
+                    if (reachPacific[x, y] && reachAtlantic[x, y])
+                        res.Add(new List<int> { x, y });
                 }
             }
             return res;
         }
 
-        static void DFS(int[][] matrix, int[][] mark, int x, int y, HashSet<string> reach)
+        static void BFS(int[][] matrix, int x, int y, int[] direction, bool[,] visit)
         {
-            mark[x][y] = 1;
-            if (x == 0 || y == 0)
-                reach.Add("Pacific");
-            if (x == matrix.Length - 1 || y == matrix[0].Length - 1)
-                reach.Add("Atlantic");
-            if(reach.Contains("Pacific") && reach.Contains("Atlantic")) 
-                return;
-            int[] dx = {-1, 1, 0, 0};
-            int[] dy = {0, 0, -1, 1};
+            var queue = new Queue<int[]>();
+            queue.Enqueue(new[] { x, y });
+            while (queue.Count != 0)
+            {
+                var cur = queue.Dequeue();
+                for (int i = 0; i < 4; i++)
+                {
+                    int newX = direction[i] + cur[0], newY = direction[i + 1] + cur[1];
+                    if (newX < 0 || newX >= matrix.Length || newY < 0 || newY >= matrix[0].Length)
+                        continue;
+                    if (matrix[newX][newY] >= matrix[cur[0]][cur[1]] && !visit[newX, newY])
+                    {
+                        visit[newX, newY] = true;
+                        queue.Enqueue(new[] { newX, newY });
+                    }
+                }
+            }
+        }
+
+
+        static IList<IList<int>> PacificAtlantic_DFS(int[][] matrix)
+        {
+            var res = new List<IList<int>>();
+            if (matrix.Length == 0 || matrix[0].Length == 0) return res;
+            var reachPacific = new bool[matrix.Length, matrix[0].Length];
+            var reachAtlantic = new bool[matrix.Length, matrix[0].Length];
+            int[] direction = { 0, 1, 0, -1, 0 };
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                reachPacific[0, i] = true;
+                reachAtlantic[matrix.Length - 1, i] = true;
+                DFS(matrix, 0, i, direction, reachPacific);
+                DFS(matrix, matrix.Length - 1, i, direction, reachAtlantic);
+            }
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                reachPacific[i, 0] = true;
+                reachAtlantic[i, matrix[0].Length - 1] = true;
+                DFS(matrix, i, 0, direction, reachPacific);
+                DFS(matrix, i, matrix[0].Length - 1, direction, reachAtlantic);
+            }
+            for (int x = 0; x < matrix.Length; x++)
+            {
+                for (int y = 0; y < matrix[0].Length; y++)
+                {
+                    if (reachPacific[x, y] && reachAtlantic[x, y])
+                        res.Add(new List<int> { x, y });
+                }
+            }
+            return res;
+        }
+        static void DFS(int[][] matrix, int x, int y, int[] direction, bool[,] visit)
+        {
             for (int i = 0; i < 4; i++)
             {
-                int newX = x + dx[i];
-                int newY = y + dy[i];
-                if(newX < 0 || newX >= matrix.Length || newY < 0 || newY >= matrix[0].Length) continue;
-                if(matrix[newX][newY] <= matrix[x][y] && mark[newX][newY] == 0) 
-                    DFS(matrix, mark, newX, newY, reach);
+                int newX = direction[i] + x, newY = direction[i + 1] + y;
+                if (newX < 0 || newX >= matrix.Length || newY < 0 || newY >= matrix[0].Length)
+                    continue;
+                if (matrix[newX][newY] >= matrix[x][y] && !visit[newX, newY])
+                {
+                    visit[newX, newY] = true;
+                    DFS(matrix, newX, newY, direction, visit);
+                }
             }
         }
     }
