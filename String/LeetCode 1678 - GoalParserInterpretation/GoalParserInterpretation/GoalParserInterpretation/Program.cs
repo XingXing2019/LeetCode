ff1c68308b1e8b8cdb7f59dd83ec227dbd21403e
Xing <x.xing.unsw@gmail.com>
2020-12-06 16:30:19 +1100

    init

---
+++ String/LeetCode 1678 - GoalParserInterpretation/GoalParserInterpretation/GoalParserInterpretation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GoalParserInterpretation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string Interpret(string command)
+        {
+            var res = command[0] == 'G' ? "G" : "";
+            for (int i = 1; i < command.Length; i++)
+            {
+                if (command[i] == 'G')
+                    res += 'G';
+                else if (command[i] == ')')
+                    res += command[i - 1] == '(' ? "o" : "al";
+            }
+            return res;
+        }
+    }
+}
