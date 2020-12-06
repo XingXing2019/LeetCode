34cc69723676333874bea57da9db6005aef7ec5e
Xing <x.xing.unsw@gmail.com>
2020-12-06 16:38:34 +1100

    init

---
+++ HashTable/LeetCode 1679 - MaxNumberOfKSumPairs/MaxNumberOfKSumPairs/MaxNumberOfKSumPairs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaxNumberOfKSumPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int MaxOperations(int[] nums, int k)
+        {
+            var res = 0;
+            var record = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (!record.ContainsKey(k - num) || record[k - num] == 0)
+                {
+                    if (!record.ContainsKey(num))
+                        record[num] = 0;
+                    record[num]++;
+                }
+                else
+                {
+                    record[k - num]--;
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
