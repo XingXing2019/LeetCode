1ec534ec07b2604fa72c194c905841199a4347d5
Xing <x.xing.unsw@gmail.com>
2020-12-07 20:24:41 +1100

    init

---
+++ Tree/LeetCode 951 - FlipEquivalentBinaryTrees/FlipEquivalentBinaryTrees/FlipEquivalentBinaryTrees/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FlipEquivalentBinaryTrees
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool FlipEquiv(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == root2) return true;
+            if (root1 == null || root2 == null || root1.val != root2.val) return false;
+            return FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right) ||
+                   FlipEquiv(root1.left, root2.right) && FlipEquiv(root1.right, root2.left);
+        }
+    }
+}
