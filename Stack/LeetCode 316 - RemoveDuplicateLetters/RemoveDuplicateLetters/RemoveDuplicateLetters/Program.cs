c2c1371bb296dbd7759761108f91663360aeb934
Xing <x.xing.unsw@gmail.com>
2020-12-10 20:31:46 +1100

    init

---
+++ Stack/LeetCode 316 - RemoveDuplicateLetters/RemoveDuplicateLetters/RemoveDuplicateLetters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveDuplicateLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static string RemoveDuplicateLetters(string s)
+        {
+            var record = new int[26];
+            foreach (var letter in s)
+                record[letter - 'a']++;
+            var stack = new Stack<char>();
+            foreach (var letter in s)
+            {
+                record[letter - 'a']--;
+                if(stack.Contains(letter)) continue;
+                while (stack.Count != 0 && letter < stack.Peek() && record[stack.Peek() - 'a'] > 0)
+                    stack.Pop();
+                stack.Push(letter);
+            }
+            var res = "";
+            while (stack.Count != 0)
+                res = stack.Pop() + res;
+            return res;
+        }
+    }
+}
