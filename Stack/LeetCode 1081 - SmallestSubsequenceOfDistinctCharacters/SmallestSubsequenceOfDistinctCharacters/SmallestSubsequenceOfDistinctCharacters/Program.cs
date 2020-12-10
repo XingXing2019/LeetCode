b592b009fb37b81426b573706d4f63f2d3e41ad4
Xing <x.xing.unsw@gmail.com>
2020-12-10 20:21:40 +1100

    init

---
+++ Stack/LeetCode 1081 - SmallestSubsequenceOfDistinctCharacters/SmallestSubsequenceOfDistinctCharacters/SmallestSubsequenceOfDistinctCharacters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmallestSubsequenceOfDistinctCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //          01234
+            var text = "bcbcbcababa";
+            Console.WriteLine(SmallestSubsequence(text));
+        }
+        static string SmallestSubsequence(string s)
+        {
+            var record = new int[26];
+            foreach (var letter in s)
+                record[letter - 'a']++;
+            var stack = new Stack<char>();
+            foreach (var letter in s)
+            {
+                record[letter - 'a']--;
+                if (stack.Contains(letter)) continue;
+                while (stack.Count != 0 && letter < stack.Peek() && record[stack.Peek() - 'a'] > 0)
+                    stack.Pop();
+                stack.Push(letter);
+            }
+            var res = "";
+            while (stack.Count != 0)
+                res = stack.Pop() + res;
+            return res;
+        }
+    }
+}
