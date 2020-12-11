2b11056ee50d4aa26410d2dd3a261dbda15e9d0a
Xing <x.xing.unsw@gmail.com>
2020-12-11 12:04:26 +1100

    init

---
+++ Array/LeetCode 42 - TrappingRainWater/TrappingRainWater/TrappingRainWater/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TrappingRainWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int Trap(int[] height)
+        {
+            var leftMax = new int[height.Length];
+            var rightMax = new int[height.Length];
+            int left = 0, right = 0, res = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                leftMax[i] = left;
+                left = Math.Max(left, height[i]);
+                rightMax[height.Length - i - 1] = right;
+                right = Math.Max(right, height[height.Length - i - 1]);
+            }
+            for (int i = 0; i < height.Length; i++)
+            {
+                var water = Math.Min(leftMax[i], rightMax[i]) - height[i];
+                if (water > 0) res += water;
+            }
+            return res;
+        }
+    }
+}
