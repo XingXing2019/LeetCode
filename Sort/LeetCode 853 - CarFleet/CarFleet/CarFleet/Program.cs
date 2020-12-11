b038d2536a600d94f8a7f21c3384a46b0cbc2501
Xing <x.xing.unsw@gmail.com>
2020-12-11 20:31:16 +1100

    init

---
+++ Sort/LeetCode 853 - CarFleet/CarFleet/CarFleet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace CarFleet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = 10;
+            int[] position = { 0, 4, 2 }, speed = { 2, 1, 3 };
+            Console.WriteLine(CarFleet(target, position, speed));
+        }
+        static int CarFleet(int target, int[] position, int[] speed)
+        {
+            if (position.Length == 0) return 0;
+            var cars = new int[position.Length][];
+            for (int i = 0; i < position.Length; i++)
+                cars[i] = new[] {position[i], speed[i]};
+            cars = cars.OrderBy(x => x[0]).ToArray();
+            var times = new double[position.Length];
+            for (int i = 0; i < cars.Length; i++)
+                times[i] = (double) (target - cars[i][0]) / cars[i][1];
+            int res = 1;
+            double temp = times[^1];
+            for (int i = times.Length - 1; i >= 0; i--)
+            {
+                if (!(times[i] > temp)) continue;
+                res++;
+                temp = times[i];
+            }
+            return res;
+        }
+    }
+}
