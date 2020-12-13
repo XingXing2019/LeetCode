a8edc46cfbed7584487040194ad6c7fa98e23fff
Xing <x.xing.unsw@gmail.com>
2020-12-13 15:22:03 +1100

    simplified code

---
+++ Database/LeetCode 1683 - InvalidTweets/InvalidTweets_SQLServer.sql
@@ -0,0 +1,2 @@
+select tweet_id from Tweets
+where len(content) > 15;
---
+++ Greedy/LeetCode 1685 - SumOfAbsoluteDifferencesInASortedArray/SumOfAbsoluteDifferences/SumOfAbsoluteDifferences/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfAbsoluteDifferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {2, 3, 5};
+            Console.WriteLine(GetSumAbsoluteDifferences(nums));
+        }
+        static int[] GetSumAbsoluteDifferences(int[] nums)
+        {
+            var len = nums.Length;
+            int[] prefix = new int[len], res = new int[len];
+            for (int i = 0; i < len; i++)
+                prefix[i] = i == 0 ? nums[i] : nums[i] + prefix[i - 1];
+            for (int i = 0; i < len; i++)
+            {
+                var before = i == 0 ? 0 : Math.Abs(prefix[i - 1] - nums[i] * i);
+                var after = i == len - 1 ? 0 : Math.Abs(prefix[^1] - prefix[i] - nums[i] * (len - 1 - i));
+                res[i] = before + after;
+            }
+            return res;
+        }
+    }
+}
