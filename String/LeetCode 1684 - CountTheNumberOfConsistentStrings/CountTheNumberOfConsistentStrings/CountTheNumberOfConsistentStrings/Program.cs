414cb78ab13a719407f15535de74eb0a222599d7
Xing <x.xing.unsw@gmail.com>
2020-12-13 14:50:14 +1100

    init

---
+++ String/LeetCode 1684 - CountTheNumberOfConsistentStrings/CountTheNumberOfConsistentStrings/CountTheNumberOfConsistentStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountTheNumberOfConsistentStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int CountConsistentStrings(string allowed, string[] words)
+        {
+            var set = new HashSet<char>(allowed);
+            var res = 0;
+            foreach (var word in words)
+            {
+                var isConsistent = true;
+                foreach (var letter in word)
+                {
+                    if (!set.Contains(letter))
+                    {
+                        isConsistent = false;
+                        break;
+                    }
+                }
+                if (isConsistent) res++;
+            }
+            return res;
+        }
+    }
+}
