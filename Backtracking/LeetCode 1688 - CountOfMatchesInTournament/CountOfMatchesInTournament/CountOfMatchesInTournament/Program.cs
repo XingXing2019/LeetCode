d1eb83cded37d3cb891613f527e437932687ff49
Xing <x.xing.unsw@gmail.com>
2020-12-13 15:28:48 +1100

    init

---
+++ Backtracking/LeetCode 1688 - CountOfMatchesInTournament/CountOfMatchesInTournament/CountOfMatchesInTournament/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CountOfMatchesInTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int NumberOfMatches(int n)
+        {
+            var res = 0;
+            while (n != 1)
+            {
+                res += n / 2;
+                n = n % 2 == 0 ? n / 2 : n / 2 + 1;
+            }
+            return res;
+        }
+    }
+}
