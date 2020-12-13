507a791a28e209e943d7e3a84be33da3107d5f9d
Xing <x.xing.unsw@gmail.com>
2020-12-13 15:36:16 +1100

    init

---
+++ Greedy/LeetCode 1689 - PartitioningIntoMinimumNumberOfDeciBinaryNumbers/PartitioningIntoMinimumNumber/PartitioningIntoMinimumNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PartitioningIntoMinimumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int MinPartitions(string n)
+        {
+            var res = 0;
+            foreach (var digit in n)
+                res = Math.Max(res, digit - '0');
+            return res;
+        }
+    }
+}
