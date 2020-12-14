3fc5d20fbf3f5a48464b4bc34ab262cfd115e945
Xing <x.xing.unsw@gmail.com>
2020-12-15 10:47:39 +1100

    init

---
+++ Design/LeetCode 1279 - TrafficLightControlledIntersection/TrafficLightControlledIntersection/TrafficLightControlledIntersection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TrafficLightControlledIntersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class TrafficLight
+    {
+        private int curRoad = 1;
+        public TrafficLight()
+        {
+
+        }
+
+        public void CarArrived(
+            int carId,         // ID of the car
+            int roadId,        // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+            int direction,     // Direction of the car
+            Action turnGreen,  // Use turnGreen() to turn light to green on current road
+            Action crossCar    // Use crossCar() to make car cross the intersection
+        )
+        {
+            if (roadId != curRoad)
+            {
+                curRoad = roadId;
+                turnGreen();
+            }
+            crossCar();
+        }
+    }
+}
