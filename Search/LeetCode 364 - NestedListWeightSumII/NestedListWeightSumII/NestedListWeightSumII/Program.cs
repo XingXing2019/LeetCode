cdbeb561dfac20ec99ba0b0c524ea67df0cb2e2a
Xing <x.xing.unsw@gmail.com>
2020-12-16 13:34:42 +1100

    init

---
+++ Search/LeetCode 364 - NestedListWeightSumII/NestedListWeightSumII/NestedListWeightSumII/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NestedListWeightSumII
+{
+    interface NestedInteger
+    {
+        bool IsInteger();
+
+        // @return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        int GetInteger();
+
+        // Set this NestedInteger to hold a single integer.
+        public void SetInteger(int value);
+
+        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+        public void Add(NestedInteger ni);
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return null if this NestedInteger holds a single integer
+        IList<NestedInteger> GetList();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int DepthSumInverse(IList<NestedInteger> nestedList)
+        {
+            int maxHeight = 0, res = 0;
+            var record = new List<int[]>();
+            foreach (var nestedInteger in nestedList)
+                DFS(nestedInteger, 1, record, ref maxHeight);
+            foreach (var pair in record)
+                res += pair[0] * (maxHeight - pair[1] + 1);
+            return res;
+        }
+
+        static void DFS(NestedInteger integer, int height, List<int[]> record, ref int maxHeight)
+        {
+            if (integer.IsInteger())
+            {
+                maxHeight = Math.Max(maxHeight, height);
+                record.Add(new []{integer.GetInteger(), height});
+            }
+            foreach (var nestedInteger in integer.GetList())
+                DFS(nestedInteger, height + 1, record, ref maxHeight);
+        }
+    }
+}
