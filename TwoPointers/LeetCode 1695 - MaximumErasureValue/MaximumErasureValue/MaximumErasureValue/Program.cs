58e2573ae8c472d1d899596e1a25b854dbc7f814
Xing <x.xing.unsw@gmail.com>
2020-12-20 19:33:28 +1100

    init

---
+++ TwoPointers/LeetCode 1695 - MaximumErasureValue/MaximumErasureValue/MaximumErasureValue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumErasureValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1 };
+            Console.WriteLine(MaximumUniqueSubarray(nums));
+        }
+        static int MaximumUniqueSubarray(int[] nums)
+        {
+            int li = 0, hi = 0, max = 0, sum = 0;
+            var set = new HashSet<int>();
+            while (hi < nums.Length)
+            {
+                if (set.Contains(nums[hi]))
+                {
+                    while (set.Contains(nums[hi]))
+                    {
+                        set.Remove(nums[li]);
+                        sum -= nums[li++];
+                    }
+                }
+                sum += nums[hi];
+                set.Add(nums[hi++]);
+                max = Math.Max(max, sum);
+            }
+            return max;
+        }
+    }
+}
