da4241b8d79a567536b6cabc574cf19af98c3676
Xing <x.xing.unsw@gmail.com>
2020-12-27 10:44:10 +1100

    init

---
+++ Math/LeetCode 775 - GlobalAndLocalInversions/GlobalAndLocalInversions/GlobalAndLocalInversions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GlobalAndLocalInversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsIdealPermutation(int[] A)
+        {
+
+        }
+    }
+}
