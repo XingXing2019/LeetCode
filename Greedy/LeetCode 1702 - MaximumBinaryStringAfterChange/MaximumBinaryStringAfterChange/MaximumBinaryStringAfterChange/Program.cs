28eb2c1f2350e0b848a5f60cf11347e32507e0f7
Xing <x.xing.unsw@gmail.com>
2020-12-28 09:00:32 +1100

    init

---
+++ Greedy/LeetCode 1702 - MaximumBinaryStringAfterChange/MaximumBinaryStringAfterChange/MaximumBinaryStringAfterChange/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumBinaryStringAfterChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binary = "1011";
+            Console.WriteLine(MaximumBinaryString(binary));
+        }
+        static string MaximumBinaryString(string binary)
+        {
+            int zero = 0, one = 0, index = 0;
+            while (index < binary.Length && binary[index] == '1')
+                index++;
+            var front = new string('1', index);
+            while (index < binary.Length)
+            {
+                if (binary[index] == '0')
+                    zero++;
+                else
+                    one++;
+                index++;
+            }
+            if (zero == 0) return binary;
+            return front + new string('1', zero - 1) + "0" + new string('1', one);
+        }
+    }
+}
