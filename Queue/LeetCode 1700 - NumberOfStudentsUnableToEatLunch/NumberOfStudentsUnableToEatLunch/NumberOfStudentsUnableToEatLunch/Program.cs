68a4816ad77701fa9b72c197fc45a5206d57ec4c
Xing <x.xing.unsw@gmail.com>
2020-12-27 15:26:52 +1100

    init

---
+++ Queue/LeetCode 1700 - NumberOfStudentsUnableToEatLunch/NumberOfStudentsUnableToEatLunch/NumberOfStudentsUnableToEatLunch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberOfStudentsUnableToEatLunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] students = { 1, 1, 1, 0, 0, 1 };
+            int[] sandwiches = { 1, 0, 0, 0, 1, 1 };
+            Console.WriteLine(CountStudents(students, sandwiches));
+        }
+        static int CountStudents(int[] students, int[] sandwiches)
+        {
+            int[] preference = { students.Count(x => x == 0), students.Count(x => x == 1) };
+            var queue = new Queue<int>(students);
+            int index = 0;
+            while (index < sandwiches.Length && preference[sandwiches[index]] != 0)
+            {
+                if (queue.Peek() == sandwiches[index])
+                {
+                    queue.Dequeue();
+                    preference[sandwiches[index]]--;
+                    index++;
+                }
+                else
+                    queue.Enqueue(queue.Dequeue());
+            }
+            return queue.Count;
+        }
+    }
+}
