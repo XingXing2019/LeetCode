e5c53a99c9d0211014236d37cbe4e6d6e945f892
Xing <x.xing.unsw@gmail.com>
2020-12-27 16:23:43 +1100

    init

---
+++ Array/LeetCode 1701 - AverageWaitingTime/AverageWaitingTime/AverageWaitingTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AverageWaitingTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public double AverageWaitingTime(int[][] customers)
+        {
+            var curTime = customers[0][0];
+            double wait = 0;
+            foreach (var customer in customers)
+            {
+                if (curTime - customer[0] < 0)
+                    curTime = customer[0];
+                wait += curTime - customer[0] + customer[1];
+                curTime += customer[1];
+            }
+            return wait / customers.Length;
+        }
+    }
+}
