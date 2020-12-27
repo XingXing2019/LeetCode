fca376ca671815e88a4926ddc9040d1151aea43a
Xing <x.xing.unsw@gmail.com>
2020-12-27 16:11:34 +1100

    init

---
+++ String/LeetCode 1704 - DetermineIfStringHalvesAreAlike/DetermineIfStringHalvesAreAlike/DetermineIfStringHalvesAreAlike/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DetermineIfStringHalvesAreAlike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool HalvesAreAlike(string s)
+        {
+            var vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+            var count = 0;
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (vowels.Contains(s[i]))
+                    count++;
+                if (vowels.Contains(s[^(i + 1)]))
+                    count--;
+            }
+            return count == 0;
+        }
+    }
+}
