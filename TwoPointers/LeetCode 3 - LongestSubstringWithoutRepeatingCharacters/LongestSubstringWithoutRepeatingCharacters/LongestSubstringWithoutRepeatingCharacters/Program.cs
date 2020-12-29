d5be77c5775f8c45903197cecbfa9899afb189d3
Xing <x.xing.unsw@gmail.com>
2020-12-30 09:40:46 +1100

    init

---
+++ TwoPointers/LeetCode 3 - LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int LengthOfLongestSubstringKDistinct(string s, int k)
+        {
+            int li = 0, hi = 0, res = 0;
+            var dict = new Dictionary<char, int>();
+            while (hi < s.Length)
+            {
+                if (!dict.ContainsKey(s[hi]))
+                    dict[s[hi]] = 0;
+                dict[s[hi]]++;
+                if (dict.Count <= k)
+                    res = Math.Max(res, hi - li + 1);
+                else
+                {
+                    while (dict.Count > k)
+                    {
+                        dict[s[li]]--;
+                        if (dict[s[li]] == 0)
+                            dict.Remove(s[li]);
+                        li++;
+                    }   
+                }
+                hi++;
+            }
+            return res;
+        }
+    }
+}
