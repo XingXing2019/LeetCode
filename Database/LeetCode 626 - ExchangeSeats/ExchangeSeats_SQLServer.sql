869fd24e503b8bc79464e9c6156575dfa905029a
Xing <x.xing.unsw@gmail.com>
2020-12-30 10:16:03 +1100

    one more solution

--- Database/LeetCode 626 - ExchangeSeats/ExchangeSeats_SQLServer.sql
+++ Database/LeetCode 626 - ExchangeSeats/ExchangeSeats_SQLServer.sql
@@ -3,4 +3,12 @@ select s1.id, case
 	else s2.student end as student
 from 
 seat s1 left join seat s2 on  s1.id = s2.id + 1 
-left join seat s3 on s1.id = s3.id - 1
+left join seat s3 on s1.id = s3.id - 1;
+
+select case 
+	when id % 2 <> 0 and id <> count then id + 1
+	when id % 2 <> 0 and id = count then id
+	else id - 1 end as id, student
+from (
+select * from seat, (select count(*) as count from seat) as t) as seatCount
+order by id;
