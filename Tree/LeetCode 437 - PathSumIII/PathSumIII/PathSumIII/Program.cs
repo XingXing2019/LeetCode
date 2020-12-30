5bc935ada67177f83fd471df400b02b547ff327f
Xing <x.xing.unsw@gmail.com>
2020-12-30 21:29:29 +1100

    prefix sum solution

--- Tree/LeetCode 437 - PathSumIII/PathSumIII/PathSumIII/Program.cs
+++ Tree/LeetCode 437 - PathSumIII/PathSumIII/PathSumIII/Program.cs
@@ -3,6 +3,7 @@
 //否则递归遍历当前节点一下的左子树和右子树，寻找能满足和为sum减去当前节点值(sum-node.val)的路径总数，将其加入count。递归结束后返回count。
 //在PathSum方法中对根节点调用DFS，对左右子树分别递归调用PathSum。
 using System;
+using System.Collections.Generic;
 
 namespace PathSumIII
 {
@@ -19,22 +20,41 @@ namespace PathSumIII
         {
             Console.WriteLine("Hello World!");
         }
-        static int PathSum(TreeNode root, int sum)
+        static int PathSum_Recursion(TreeNode root, int sum)
         {
-            if (root == null) 
-                return 0;
-            return DFS(root, sum) + PathSum(root.left, sum) + PathSum(root.right, sum);
+            if (root == null) return 0;
+            return Recursion(root, sum) + PathSum_Recursion(root.left, sum) + PathSum_Recursion(root.right, sum);
         }
-        static int DFS(TreeNode node, int sum)
+
+        static int Recursion(TreeNode node, int sum)
         {
             int count = 0;
-            if (node == null) 
-                return 0;
-            if (node.val == sum)
-                count++;
-            count += DFS(node.left, sum - node.val);
-            count += DFS(node.right, sum - node.val);
+            if (node == null) return 0;
+            if (node.val == sum) count++;
+            count += Recursion(node.left, sum - node.val);
+            count += Recursion(node.right, sum - node.val);
             return count;
+        }
+
+        static int PathSum_PrefixSum(TreeNode root, int sum)
+        {
+            var res = 0;
+            PrefixSum(root, new Dictionary<int, int>{{0, 1}}, 0, sum, ref res);
+            return res;
+        }
+
+        static void PrefixSum(TreeNode node, Dictionary<int, int> dict, int prefix, int sum, ref int count)
+        {
+            if (node == null) return;
+            prefix += node.val;
+            if (dict.ContainsKey(prefix - sum))
+                count += dict[prefix - sum];
+            if (!dict.ContainsKey(prefix))
+                dict[prefix] = 0;
+            dict[prefix]++;
+            PrefixSum(node.left, dict, prefix, sum, ref count);
+            PrefixSum(node.right, dict, prefix, sum, ref count);
+            dict[prefix]--;
         }
     }
 }
