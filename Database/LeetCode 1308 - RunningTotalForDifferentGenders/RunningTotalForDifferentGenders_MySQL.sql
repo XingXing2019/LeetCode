a17a29d07d2772d3db1e969f815acda81727ad15
Xing <x.xing.unsw@gmail.com>
2020-12-31 12:07:10 +1100

    one more solution

--- Database/LeetCode 1308 - RunningTotalForDifferentGenders/RunningTotalForDifferentGenders_MySQL.sql
+++ Database/LeetCode 1308 - RunningTotalForDifferentGenders/RunningTotalForDifferentGenders_MySQL.sql
@@ -1,4 +1,6 @@
 select gender, day, sum(score_points) over(order by day) as total from scores where gender = 'F'
 union
-select gender, day, sum(score_points) over(order by day) as total from scores where gender = 'M'
+select gender, day, sum(score_points) over(order by day) as total from scores where gender = 'M';
+
+select gender, day, sum(score_points) over(partition by gender order by day) as total from scores;
 
