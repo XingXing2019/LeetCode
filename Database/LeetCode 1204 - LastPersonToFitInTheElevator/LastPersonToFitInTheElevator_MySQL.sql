9f3e294e684a1a2d13d493cdb54bc4f2c739f2bc
Xing <x.xing.unsw@gmail.com>
2020-12-31 11:56:04 +1100

    MySQL

---
+++ Database/LeetCode 1204 - LastPersonToFitInTheElevator/LastPersonToFitInTheElevator_MySQL.sql
@@ -0,0 +1,1 @@
+select person_name from (select person_name, sum(weight) over(order by turn) as total from queue) as t where total <= 1000 order by total desc limit 1;
---
+++ Database/LeetCode 1204 - LastPersonToFitInTheElevator/LastPersonToFitInTheElevator_SQLServer.sql
@@ -0,0 +1,1 @@
+select top(1) person_name from (select person_name, sum(weight) over (order by turn) as total from queue ) as t where total <= 1000 order by total desc;
