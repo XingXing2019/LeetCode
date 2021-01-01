5799a7cff70cd8c43a62b9c73c20e4dd916de719
Xing <x.xing.unsw@gmail.com>
2021-01-02 09:51:37 +1100

    init

---
+++ Greedy/LeetCode 1708 - LargestSubarrayLengthK/LargestSubarrayLengthK/LargestSubarrayLengthK/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace LargestSubarrayLengthK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] LargestSubarray(int[] nums, int k)
+        {
+            var index = Array.IndexOf(nums, nums[..(nums.Length - k + 1)].Max());
+            return nums[index..(index + k)];
+        }
+    }
+}
