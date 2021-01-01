2be6d7c40711cf2a54dadd5fab0cb642ad1feef3
Xing <x.xing.unsw@gmail.com>
2021-01-01 14:41:43 +1100

    one more solution

--- Database/LeetCode 1084 - SalesAnalysisIII/SalesAnalysisIII_SQLServer.sql
+++ Database/LeetCode 1084 - SalesAnalysisIII/SalesAnalysisIII_SQLServer.sql
@@ -1,4 +1,7 @@
 select p.product_id, p.product_name from product p left join
 (select product_id, min(sale_date) firestDate, max(sale_date) as lastDate from sales group by product_id) as t 
 on p.product_id = t.product_id
-where t.firestDate >= '2019-01-01' and t.lastDate <= '2019-03-31'
+where t.firestDate >= '2019-01-01' and t.lastDate <= '2019-03-31';
+
+select product_id, product_name from product where product_id not in 
+(select distinct product_id from sales where sale_date not between '2019-01-01' and '2019-03-31');
