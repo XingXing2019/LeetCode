7c3629ac43407c2853de9b2aedba97969eedb507
Xing <x.xing.unsw@gmail.com>
2021-01-02 09:53:48 +1100

    one more solution

--- Greedy/LeetCode 1708 - LargestSubarrayLengthK/LargestSubarrayLengthK/LargestSubarrayLengthK/Program.cs
+++ Greedy/LeetCode 1708 - LargestSubarrayLengthK/LargestSubarrayLengthK/LargestSubarrayLengthK/Program.cs
@@ -14,5 +14,19 @@ namespace LargestSubarrayLengthK
             var index = Array.IndexOf(nums, nums[..(nums.Length - k + 1)].Max());
             return nums[index..(index + k)];
         }
+
+        public int[] LargestSubarray_Loop(int[] nums, int k)
+        {
+            int max = 0, index = 0;
+            for (int i = 0; i <= nums.Length - k; i++)
+            {
+                if (nums[i] > max)
+                {
+                    index = i;
+                    max = nums[i];
+                }
+            }
+            return nums[index..(index + k)];
+        }
     }
 }
