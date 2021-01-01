3a6ab48e7877a2a551bd318590117b5d88133c6e
Xing <x.xing.unsw@gmail.com>
2021-01-01 15:25:51 +1100

    one more solution

--- Array/LeetCode 605 - CanPlaceFlowers/CanPlaceFlowers/CanPlaceFlowers/Program.cs
+++ Array/LeetCode 605 - CanPlaceFlowers/CanPlaceFlowers/CanPlaceFlowers/Program.cs
@@ -36,5 +36,17 @@ namespace CanPlaceFlowers
                 n--;
             return n <= 0;
         }
+        public bool CanPlaceFlowers2(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    n--;
+                    flowerbed[i] = 1;
+                }
+            }
+            return n <= 0;
+        }
     }
 }
