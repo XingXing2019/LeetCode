6c2e808b9948991e887d301de6543956a84531a9
Xing <x.xing.unsw@gmail.com>
2021-01-03 09:27:15 +1100

    init

---
+++ Heap/LeetCode 239 - SlidingWindowMaximum/SlidingWindowMaximum/SlidingWindowMaximum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SlidingWindowMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+            int k = 3;
+            Console.WriteLine(MaxSlidingWindow(nums, k));
+        }
+        static int[] MaxSlidingWindow_Heap(int[] nums, int k)
+        {
+            var heap = new SortedList<int, int>();
+            var res = new int[nums.Length - k + 1];
+            for (int i = 0; i <= nums.Length; i++)
+            {
+                if (i >= k)
+                {
+                    res[i - k] = heap.Keys[^1];
+                    heap[nums[i - k]]--;
+                    if (heap[nums[i - k]] == 0)
+                        heap.Remove(nums[i - k]);
+                    if (i == nums.Length) break;
+                }
+                if (!heap.ContainsKey(nums[i]))
+                    heap[nums[i]] = 0;
+                heap[nums[i]]++;
+            }
+            return res;
+        }
+    }
+}
