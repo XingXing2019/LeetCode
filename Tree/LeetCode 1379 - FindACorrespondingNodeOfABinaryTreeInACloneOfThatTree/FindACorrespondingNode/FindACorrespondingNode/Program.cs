fb20577a59be51fe1899b3caad94a446be425919
Xing <x.xing.unsw@gmail.com>
2021-01-02 21:21:17 +1100

    BFS solution

--- Tree/LeetCode 1379 - FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree/FindACorrespondingNode/FindACorrespondingNode/Program.cs
+++ Tree/LeetCode 1379 - FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree/FindACorrespondingNode/FindACorrespondingNode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FindACorrespondingNode
 {
@@ -16,7 +17,7 @@ namespace FindACorrespondingNode
         {
             Console.WriteLine("Hello World!");
         }
-        public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target)
+        public TreeNode GetTargetCopy_DFS(TreeNode original, TreeNode cloned, TreeNode target)
         {
             SearchTarget(cloned, target);
             return res;
@@ -29,6 +30,30 @@ namespace FindACorrespondingNode
                 res = node;
             SearchTarget(node.left, target);
             SearchTarget(node.right, target);
+        }
+        public TreeNode GetTargetCopy_BFS(TreeNode original, TreeNode cloned, TreeNode target)
+        {
+            var originalQueue = new Queue<TreeNode>();
+            var clonedQueue = new Queue<TreeNode>();
+            originalQueue.Enqueue(original);
+            clonedQueue.Enqueue(cloned);
+            while (originalQueue.Count != 0)
+            {
+                var originalCur = originalQueue.Dequeue();
+                var clonedCur = clonedQueue.Dequeue();
+                if (originalCur == target) return clonedCur;
+                if (originalCur.left != null)
+                {
+                    originalQueue.Enqueue(originalCur.left);
+                    clonedQueue.Enqueue(clonedCur.left);
+                }
+                if (originalCur.right != null)
+                {
+                    originalQueue.Enqueue(originalCur.right);
+                    clonedQueue.Enqueue(clonedCur.right);
+                }
+            }
+            return null;
         }
     }
 }
