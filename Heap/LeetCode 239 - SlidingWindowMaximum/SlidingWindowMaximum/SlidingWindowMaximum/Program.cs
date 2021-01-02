c97d5f50be15ae2c2f7e9c52340785e6e2034644
Xing <x.xing.unsw@gmail.com>
2021-01-03 10:13:39 +1100

    one more solution

--- Heap/LeetCode 239 - SlidingWindowMaximum/SlidingWindowMaximum/SlidingWindowMaximum/Program.cs
+++ Heap/LeetCode 239 - SlidingWindowMaximum/SlidingWindowMaximum/SlidingWindowMaximum/Program.cs
@@ -9,7 +9,7 @@ namespace SlidingWindowMaximum
         {
             int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
             int k = 3;
-            Console.WriteLine(MaxSlidingWindow(nums, k));
+            Console.WriteLine(MaxSlidingWindow_Dp(nums, k));
         }
         static int[] MaxSlidingWindow_Heap(int[] nums, int k)
         {
@@ -29,6 +29,26 @@ namespace SlidingWindowMaximum
                     heap[nums[i]] = 0;
                 heap[nums[i]]++;
             }
+            return res;
+        }
+        static int[] MaxSlidingWindow_Dp(int[] nums, int k)
+        {
+            int[] leftMax = new int[nums.Length];
+            int[] rightMax = new int[nums.Length];
+            int left = nums[0], right = nums[^1];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                left = i % k == 0 ? nums[i] : Math.Max(left, nums[i]);
+                leftMax[i] = left;
+            }
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                right = i == nums.Length - 1 || (i + 1) % k == 0 ? nums[i] : Math.Max(right, nums[i]);
+                rightMax[i] = right;
+            }
+            var res = new int[nums.Length - k + 1];
+            for (int i = 0; i < res.Length; i++)
+                res[i] = Math.Max(rightMax[i], leftMax[i + k - 1]);
             return res;
         }
     }
