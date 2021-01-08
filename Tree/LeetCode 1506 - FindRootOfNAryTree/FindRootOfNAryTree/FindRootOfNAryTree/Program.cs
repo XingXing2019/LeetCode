dac891e9701cb71f003ce453256cf4e86f1965d0
Xing <x.xing.unsw@gmail.com>
2021-01-08 20:40:24 +1100

    init

---
+++ Tree/LeetCode 1506 - FindRootOfNAryTree/FindRootOfNAryTree/FindRootOfNAryTree/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindRootOfNAryTree
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node()
+        {
+            val = 0;
+            children = new List<Node>();
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+            children = new List<Node>();
+        }
+
+        public Node(int _val, List<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public Node FindRoot_HashSet(List<Node> tree)
+        {
+            var set = new HashSet<Node>();
+            foreach (var node in tree)
+            {
+                foreach (var child in node.children)
+                    set.Add(child);
+            }
+            foreach (var node in tree)
+            {
+                if (!set.Contains(node))
+                    return node;
+            }
+            return null;
+        }
+    }
+}
