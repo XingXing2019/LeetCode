256e0eb60d62f6d69f2dadfbf9c2b0fff9a9d11d
Xing <x.xing.unsw@gmail.com>
2021-01-09 22:08:23 +1100

    Simplified code

--- Database/LeetCode 1715 - CountApplesAndOranges/CountApplesAndOranges_SQLServer.sql
+++ Database/LeetCode 1715 - CountApplesAndOranges/CountApplesAndOranges_SQLServer.sql
@@ -1,3 +1,2 @@
-select sum(apple) apple_count, sum(orange) orange_count from
-(select b.apple_count + ISNULL(c.apple_count, 0) apple, b.orange_count + ISNULL(c.orange_count, 0) orange
-from boxes b left join chests c on b.chest_id = c.chest_id) as t
+select sum(b.apple_count + ISNULL(c.apple_count, 0)) apple_count, sum(b.orange_count + ISNULL(c.orange_count, 0)) orange_count
+from boxes b left join chests c on b.chest_id = c.chest_id
