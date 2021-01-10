4a7db903e55882fd33278fb235ddcc08dbdd6a9b
Xing <x.xing.unsw@gmail.com>
2021-01-10 20:09:18 +1100

    one more solution

--- Search/LeetCode 494 - TargetSum/TargetSum/TargetSum/Program.cs
+++ Search/LeetCode 494 - TargetSum/TargetSum/TargetSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TargetSum
 {
@@ -8,9 +9,9 @@ namespace TargetSum
         {
             int[] nums = {1, 1, 1, 1, 1};
             int S = 3;
-            Console.WriteLine(FindTargetSumWays(nums, S));
+            Console.WriteLine(FindTargetSumWays_DP(nums, S));
         }
-        static int FindTargetSumWays(int[] nums, int S)
+        static int FindTargetSumWays_DFS(int[] nums, int S)
         {
             var res = 0;
             DFS(nums, S, 0, ref res);
@@ -23,12 +24,31 @@ namespace TargetSum
             {
                 if (S == 0)
                     res++;
+                return;
             }
-            else
+            DFS(nums, S + nums[index], index + 1, ref res);
+            DFS(nums, S - nums[index], index + 1, ref res);
+        }
+
+        static int FindTargetSumWays_DP(int[] nums, int S)
+        {
+            var sum = nums.Sum();
+            if (S > sum || S < -sum) return 0;
+            var dp = new int[nums.Length + 1][];
+            for (int i = 0; i < dp.Length; i++)
+                dp[i] = new int[2 * sum + 1];
+            dp[0][sum] = 1;
+            for (int i = 1; i < dp.Length; i++)
             {
-                DFS(nums, S + nums[index], index + 1, ref res);
-                DFS(nums, S - nums[index], index + 1, ref res);
+                for (int j = 0; j < dp[i].Length; j++)
+                {
+                    if (j - nums[i - 1] >= 0)
+                        dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                    if (j + nums[i - 1] < dp[i].Length)
+                        dp[i][j] += dp[i - 1][j + nums[i - 1]];
+                }
             }
+            return dp[^1][sum + S];
         }
     }
 }
