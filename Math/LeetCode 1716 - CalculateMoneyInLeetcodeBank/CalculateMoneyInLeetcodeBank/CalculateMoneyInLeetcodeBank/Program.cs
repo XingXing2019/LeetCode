1a4ed44ea78e8ccfbb32b767c476cb221ffabc3a
Xing <x.xing.unsw@gmail.com>
2021-01-10 15:42:58 +1100

    init

---
+++ Math/LeetCode 1716 - CalculateMoneyInLeetcodeBank/CalculateMoneyInLeetcodeBank/CalculateMoneyInLeetcodeBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculateMoneyInLeetcodeBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            Console.WriteLine(TotalMoney(n));
+        }
+        static int TotalMoney(int n)
+        {
+            var weeks = n / 7;
+            var days = n % 7;
+            return 28 * weeks + 7 * (weeks - 1) * weeks / 2 + (weeks + 1 + weeks + days) * days / 2;
+        }
+    }
+}
