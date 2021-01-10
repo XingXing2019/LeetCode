1861172c670d2fa0ae47f9fed99e6525eab59225
Xing <x.xing.unsw@gmail.com>
2021-01-10 15:49:13 +1100

    init

---
+++ BitManipulation/LeetCode 1720 - DecodeXORedArray/DecodeXORedArray/DecodeXORedArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DecodeXORedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int[] Decode(int[] encoded, int first)
+        {
+            var res = new int[encoded.Length + 1];
+            res[0] = first;
+            for (int i = 0; i < encoded.Length; i++)
+                res[i + 1] = encoded[i] ^ res[i];
+            return res;
+        }
+    }
+}
