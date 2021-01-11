dde8d98455a63bdc7cf661e22a8a02aa05a0d601
Xing <x.xing.unsw@gmail.com>
2021-01-11 22:01:02 +1100

    one more solution

--- Heap/LeetCode 973 - KClosestPointsToOrigin/KClosestPointsToOrigin/KClosestPointsToOrigin/Program.cs
+++ Heap/LeetCode 973 - KClosestPointsToOrigin/KClosestPointsToOrigin/KClosestPointsToOrigin/Program.cs
@@ -1,6 +1,8 @@
 ﻿//用Linq将points按照每个点到原点距离排序，然后将前K个点存入res，并返回。
 //耍赖的做法，但是速度打败了99.12%算法。
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace KClosestPointsToOrigin
@@ -27,6 +29,30 @@ namespace KClosestPointsToOrigin
         public int[][] KClosest_Linq(int[][] points, int K)
         {
             return points.OrderBy(x => x[0] * x[0] + x[1] * x[1]).Take(K).ToArray();
+        }
+
+        public int[][] KClosest_SortedList(int[][] points, int K)
+        {
+            var list = new SortedList<int, List<int[]>>();
+            int count = 0;
+            foreach (var point in points)
+            {
+                if (!list.ContainsKey(point[0] * point[0] + point[1] * point[1]))
+                    list[point[0] * point[0] + point[1] * point[1]] = new List<int[]>();
+                list[point[0] * point[0] + point[1] * point[1]].Add(point);
+                count++;
+                if (count > K)
+                {
+                    list[list.Keys[^1]].RemoveAt(list[list.Keys[^1]].Count - 1);
+                    count--;
+                    if (list[list.Keys[^1]].Count == 0)
+                        list.Remove(list.Keys[^1]);
+                }
+            }
+            var res = new List<int[]>();
+            foreach (var kv in list)
+                res.AddRange(kv.Value);
+            return res.ToArray();
         }
     }
 }
