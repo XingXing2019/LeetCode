e6d8e81a7a58c6519a1521092873851d39879d73
Xing <x.xing.unsw@gmail.com>
2021-01-14 20:53:27 +1100

    one more solution

--- BinarySearch/LeetCode 1658 - MinimumOperationsToReduceXToZero/MinimumOperationsToReduceXToZero/MinimumOperationsToReduceXToZero/Program.cs
+++ BinarySearch/LeetCode 1658 - MinimumOperationsToReduceXToZero/MinimumOperationsToReduceXToZero/MinimumOperationsToReduceXToZero/Program.cs
@@ -6,11 +6,11 @@ namespace MinimumOperationsToReduceXToZero
     {
         static void Main(string[] args)
         {
-            int[] nums = {1, 1};
-            int x = 2;
-            Console.WriteLine(MinOperations(nums, x));
+            int[] nums = { 8828, 9581, 49, 9818, 9974, 9869, 9991, 10000, 10000, 10000, 9999, 9993, 9904, 8819, 1231, 6309 };
+            int x = 134365;
+            Console.WriteLine(MinOperations_Prefix(nums, x));
         }
-        static int MinOperations(int[] nums, int x)
+        static int MinOperations_Suffix(int[] nums, int x)
         {
             int[] prefix = new int[nums.Length + 1], suffix = new int[nums.Length + 1];
             var res = int.MaxValue;
@@ -24,6 +24,24 @@ namespace MinimumOperationsToReduceXToZero
                 var index = Array.BinarySearch(suffix, 0, suffix.Length - i, x - prefix[i]);
                 if (index >= 0)
                     res = Math.Min(res, i + index);
+            }
+            return res == int.MaxValue ? -1 : res;
+        }
+        static int MinOperations_Prefix(int[] nums, int x)
+        {
+            var prefix = new int[nums.Length + 1];
+            var suffix = new int[nums.Length + 1];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                prefix[i] = prefix[i - 1] + nums[i - 1];
+                suffix[^(i + 1)] = suffix[^i] + nums[^i];
+            }
+            var res = int.MaxValue;
+            for (int i = suffix.Length - 1; i >= 0; i--)
+            {
+                var index = Array.BinarySearch(prefix, 0, i + 1, x - suffix[i]);
+                if (index >= 0)
+                    res = Math.Min(res, index + nums.Length - i);
             }
             return res == int.MaxValue ? -1 : res;
         }
