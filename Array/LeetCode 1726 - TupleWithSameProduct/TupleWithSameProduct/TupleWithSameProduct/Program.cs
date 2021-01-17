1ae7bbc2613b0a0474c94024466e21a2f4d9f60d
Xing <x.xing.unsw@gmail.com>
2021-01-17 15:55:34 +1100

    init

---
+++ Array/LeetCode 1726 - TupleWithSameProduct/TupleWithSameProduct/TupleWithSameProduct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TupleWithSameProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int TupleSameProduct(int[] nums)
+        {
+            var res = 0;
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (!dict.ContainsKey(nums[i] * nums[j]))
+                        dict[nums[i] * nums[j]] = 0;
+                    else
+                        res += 8 * dict[nums[i] * nums[j]];
+                    dict[nums[i] * nums[j]]++;
+                }
+            }
+            return res;
+        }
+    }
+}
--- Greedy/LeetCode 1725 - NumberOfRectanglesThatCanFormTheLargestSquare/NumberOfRectangles/NumberOfRectangles/Program.cs
+++ Greedy/LeetCode 1725 - NumberOfRectanglesThatCanFormTheLargestSquare/NumberOfRectangles/NumberOfRectangles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace NumberOfRectangles
@@ -9,9 +10,22 @@ namespace NumberOfRectangles
         {
             Console.WriteLine("Hello World!");
         }
-        public int CountGoodRectangles(int[][] rectangles)
+        public int CountGoodRectangles_Linq(int[][] rectangles)
         {
             return rectangles.Select(x => Math.Min(x[0], x[1])).GroupBy(x => x).OrderByDescending(x => x.Key).ToList()[0].Count();
+        }
+        public int CountGoodRectangles_HashTable(int[][] rectangles)
+        {
+            var dict = new Dictionary<int, int>();
+            var max = 0;
+            foreach (var rectangle in rectangles)
+            {
+                max = Math.Max(max, Math.Min(rectangle[0], rectangle[1]));
+                if (!dict.ContainsKey(Math.Min(rectangle[0], rectangle[1])))
+                    dict[Math.Min(rectangle[0], rectangle[1])] = 0;
+                dict[Math.Min(rectangle[0], rectangle[1])]++;
+            }
+            return dict[max];
         }
     }
 }
