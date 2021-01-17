a4de740a5fe728b0d7a5fad797588dbc255ee735
Xing <x.xing.unsw@gmail.com>
2021-01-17 15:37:17 +1100

    init

---
+++ Greedy/LeetCode 1725 - NumberOfRectanglesThatCanFormTheLargestSquare/NumberOfRectangles/NumberOfRectangles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace NumberOfRectangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountGoodRectangles(int[][] rectangles)
+        {
+            return rectangles.Select(x => Math.Min(x[0], x[1])).GroupBy(x => x).OrderByDescending(x => x.Key).ToList()[0].Count();
+        }
+    }
+}
