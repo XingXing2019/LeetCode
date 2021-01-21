1a1df42246efc517c5f8df5918a48f9af44c18da
Xing <x.xing.unsw@gmail.com>
2021-01-21 21:49:17 +1100

    one more solution

--- Array/LeetCode 1470 - ShuffleTheArray/ShuffleTheArray_Java/src/com/company/Main.java
+++ Array/LeetCode 1470 - ShuffleTheArray/ShuffleTheArray_Java/src/com/company/Main.java
@@ -22,4 +22,14 @@ public class Main {
         }
         return res;
     }
+
+    public static int[] shuffle2(int[] nums, int n) {
+        int[] res = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            res[index++] = nums[i];
+            res[index++] = nums[i + n];
+        }
+        return res;
+    }
 }
