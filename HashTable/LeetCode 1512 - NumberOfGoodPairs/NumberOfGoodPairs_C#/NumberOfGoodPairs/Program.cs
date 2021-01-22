9607e854e4705f68361d5d0d220f2d53240a9b1e
Xing <x.xing.unsw@gmail.com>
2021-01-22 16:22:57 +1100

    simplified code

--- HashTable/LeetCode 1512 - NumberOfGoodPairs/NumberOfGoodPairs_C#/NumberOfGoodPairs/Program.cs
+++ HashTable/LeetCode 1512 - NumberOfGoodPairs/NumberOfGoodPairs_C#/NumberOfGoodPairs/Program.cs
@@ -8,16 +8,14 @@ namespace NumberOfGoodPairs
         {
             Console.WriteLine("Hello World!");
         }
-        static int NumIdenticalPairs(int[] nums)
+        static int NumIdenticalPairs_Array(int[] nums)
         {
             var frequencies = new int[101];
+            var res = 0;
             foreach (var num in nums)
-                frequencies[num]++;
-            int res = 0;
-            foreach (var frequency in frequencies)
             {
-                if(frequency < 2) continue;
-                res += (frequency - 1) * frequency / 2;
+                res += frequencies[num];
+                frequencies[num]++;
             }
             return res;
         }
