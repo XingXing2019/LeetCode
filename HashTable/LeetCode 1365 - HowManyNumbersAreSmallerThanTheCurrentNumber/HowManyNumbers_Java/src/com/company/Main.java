dfb9d046da1d988a5479a5bf684ae3c7e588814b
Xing <x.xing.unsw@gmail.com>
2021-01-22 16:46:56 +1100

    one more solution

--- HashTable/LeetCode 1365 - HowManyNumbersAreSmallerThanTheCurrentNumber/HowManyNumbers_Java/src/com/company/Main.java
+++ HashTable/LeetCode 1365 - HowManyNumbersAreSmallerThanTheCurrentNumber/HowManyNumbers_Java/src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
-    public int[] smallerNumbersThanCurrent(int[] nums) {
+    public int[] smallerNumbersThanCurrent_N(int[] nums) {
         var max = 0;
         for (int num : nums)
             max = Math.max(max, num);
@@ -17,6 +20,19 @@ public class Main {
             smaller[i] = smaller[i - 1] + record[i - 1];
         for (int i = 0; i < nums.length; i++)
             nums[i] = smaller[nums[i]];
+        return nums;
+    }
+
+    public int[] smallerNumbersThanCurrent_NLogN(int[] nums) {
+        int[] cloned = nums.clone();
+        Arrays.sort(cloned);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < cloned.length; i++) {
+            if(!map.containsKey(cloned[i]))
+                map.put(cloned[i], i);
+        }
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = map.get(nums[i]);
         return nums;
     }
 }
