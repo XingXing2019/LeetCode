689aa66da71e48a0bca150fd47b18946632b11eb
Xing <x.xing.unsw@gmail.com>
2021-01-22 17:29:44 +1100

    JavaSolution

---
+++ Tree/LeetCode 100 - SameTree/SameTree_Java/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == q) return true;
+        if(p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
