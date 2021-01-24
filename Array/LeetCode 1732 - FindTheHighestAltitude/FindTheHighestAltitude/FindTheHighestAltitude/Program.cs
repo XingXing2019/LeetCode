d868927949b5cdc629441895d2b6f924a01d160a
Xing <x.xing.unsw@gmail.com>
2021-01-24 15:25:50 +1100

    init

---
+++ Array/LeetCode 1732 - FindTheHighestAltitude/FindTheHighestAltitude/FindTheHighestAltitude/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FindTheHighestAltitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int LargestAltitude(int[] gain)
+        {
+            int max = Math.Max(0, gain[0]);
+            for (int i = 1; i < gain.Length; i++)
+            {
+                gain[i] = gain[i - 1] + gain[i];
+                max = Math.Max(max, gain[i]);
+            }
+            return max;
+        }
+    }
+}
