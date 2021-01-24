e6be75473efe66a3afae9eccf4b6d197971a234f
Xing <x.xing.unsw@gmail.com>
2021-01-24 20:59:06 +1100

    init

---
+++ BitManipulation/LeetCode 1734 - DecodeXORedPermutation/DecodeXORedPermutation/DecodeXORedPermutation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DecodeXORedPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] Decode(int[] encoded)
+        {
+            var res = new int[encoded.Length + 1];
+            int first = 0;
+            for (int i = 0; i <= encoded.Length + 1; i++)
+            {
+                first ^= i;
+                if (i < encoded.Length + 1 && i % 2 == 1)
+                    first ^= encoded[i];
+            }
+            res[0] = first;
+            for (int i = 1; i < res.Length; i++)
+                res[i] = res[i - 1] ^ encoded[i - 1];
+            return res;
+        }
+    }
+}
