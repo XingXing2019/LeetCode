f7085f4646d9cbecafd143faa3580352b51188d0
Xing <x.xing.unsw@gmail.com>
2021-01-28 20:50:55 +1100

    fix one bug

--- Greedy/LeetCode 1663 - SmallestStringWithAGivenNumericValue/SmallestStringWithAGivenNumericValue_C#/SmallestStringWithAGivenNumericValue/Program.cs
+++ Greedy/LeetCode 1663 - SmallestStringWithAGivenNumericValue/SmallestStringWithAGivenNumericValue_C#/SmallestStringWithAGivenNumericValue/Program.cs
@@ -12,10 +12,11 @@ namespace SmallestStringWithAGivenNumericValue
         static string GetSmallestString(int n, int k)
         {
             var count = (k - n) / 25;
-            var left = new string('a', n - count - 1);
+            var left = n - count - 1 > 0 ? new string('a', n - count - 1) : "";
             var right = new string('z', count);
             k -= left.Length + 26 * right.Length;
-            return left + (char)(k - 1 + 'a') + right;
+            var mid = k - 1 < 0 ? "" : ((char) (k - 1 + 'a')).ToString();
+            return left + mid + right;
         }
     }
 }
