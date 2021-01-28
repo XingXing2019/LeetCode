65f5827b904f39a62fe398ee8a494765465c1f7f
Xing <x.xing.unsw@gmail.com>
2021-01-28 17:58:07 +1100

    JavaSolution

---
+++ Math/LeetCode 914 - XOfAKindInADeckOfCards/XOfAKindInADeckOfCards_Java/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public boolean hasGroupsSizeX(int[] deck) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int k = 0;
+        for (int num : deck){
+            if(!map.containsKey(num))
+                map.put(num, 0);
+            map.put(num, map.get(num) + 1);
+            k = Math.max(k, map.get(num));
+        }
+        for (var kv : map.entrySet())
+            k = gcd(k, kv.getValue());
+        return k > 1;
+    }
+
+    public int gcd(int a, int b){
+        return a % b == 0 ? b : gcd(b, a % b);
+    }
+}
