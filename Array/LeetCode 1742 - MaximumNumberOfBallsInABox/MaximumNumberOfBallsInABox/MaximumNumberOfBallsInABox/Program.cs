cf1c455c21179c0852957d34594d527d2599ac32
Xing <x.xing.unsw@gmail.com>
2021-01-31 15:20:07 +1100

    init

---
+++ Array/LeetCode 1742 - MaximumNumberOfBallsInABox/MaximumNumberOfBallsInABox/MaximumNumberOfBallsInABox/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumNumberOfBallsInABox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountBalls(int lowLimit, int highLimit)
+        {
+            var dict = new Dictionary<int, int>();
+            int res = 0;
+            for (int i = lowLimit; i <= highLimit; i++)
+            {
+                int temp = 0, ball = i;
+                while (ball != 0)
+                {
+                    temp += ball % 10;
+                    ball /= 10;
+                }
+
+                if (!dict.ContainsKey(temp))
+                    dict[temp] = 0;
+                dict[temp]++;
+                res = Math.Max(res, dict[temp]);
+            }
+            return res;
+        }
+    }
+}
