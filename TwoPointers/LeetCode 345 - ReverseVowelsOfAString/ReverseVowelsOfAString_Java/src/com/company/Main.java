8a75bd8c80b0d5af58765122addd0d32c60e482b
Xing <x.xing.unsw@gmail.com>
2021-02-02 15:58:25 +1100

    faster

--- TwoPointers/LeetCode 345 - ReverseVowelsOfAString/ReverseVowelsOfAString_Java/src/com/company/Main.java
+++ TwoPointers/LeetCode 345 - ReverseVowelsOfAString/ReverseVowelsOfAString_Java/src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
         // write your code here
     }
 
-    public String reverseVowels(String s) {
+    public String reverseVowels_set(String s) {
         char[] letters = s.toCharArray();
         HashSet<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
         int li = 0, hi = letters.length - 1;
@@ -21,6 +21,26 @@ public class Main {
             } else if (!vowels.contains(letters[li]))
                 li++;
             else if (!vowels.contains(letters[hi]))
+                hi--;
+        }
+        return new String(letters);
+    }
+
+    public String reverseVowels_Array(String s) {
+        char[] letters = s.toCharArray();
+        boolean[] check = new boolean[128];
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for (Character v : vowels)
+            check[v] = true;
+        int li = 0, hi = letters.length - 1;
+        while (li < hi) {
+            if (check[letters[li]] && check[letters[hi]]) {
+                char temp = letters[li];
+                letters[li++] = letters[hi];
+                letters[hi--] = temp;
+            } else if (!check[letters[li]])
+                li++;
+            else if (!check[letters[hi]])
                 hi--;
         }
         return new String(letters);
