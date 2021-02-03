e4e1faf3c227100ba12504a84141f7c6016778aa
Xing <x.xing.unsw@gmail.com>
2021-02-03 20:42:59 +1100

    JavaSolution

---
+++ Tree/LeetCode 589 - NaryTreePreorderTraversal/NaryTreePreorderTraversal_Java/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+    public void dfs(Node root, List<Integer> nodes){
+        if (root == null) return;
+        nodes.add(root.val);
+        for (int i = 0; i < root.children.size(); i++)
+            dfs(root.children.get(i), nodes);
+    }
+}
