4eff0db37c7ff558ca53ef5c3f044737e748266d
Xing <x.xing.unsw@gmail.com>
2021-02-03 22:58:22 +1100

    betterSolution

--- Math/LeetCode 7 - ReverseInteger/ReverseInteger_Java/src/com/company/Main.java
+++ Math/LeetCode 7 - ReverseInteger/ReverseInteger_Java/src/com/company/Main.java
@@ -6,11 +6,12 @@ public class Main {
         System.out.print(-123 / 10);
     }
     public int reverse(int x) {
-        long res = 0;
+        int res = 0;
         while (x != 0){
+            if(res > Integer.MAX_VALUE / 10 || res < Integer.MIN_VALUE / 10) return 0;
             res = res * 10 + x % 10;
             x /= 10;
         }
-        return res > Integer.MAX_VALUE || res < Integer.MIN_VALUE ? 0 : (int) res;
+        return res;
     }
 }
