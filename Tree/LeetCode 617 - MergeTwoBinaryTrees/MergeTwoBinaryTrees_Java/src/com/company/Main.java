ff5c5f40a13272579259308526623898051e9a0d
Xing <x.xing.unsw@gmail.com>
2021-02-03 20:11:24 +1100

    JavaSolution

---
+++ Tree/LeetCode 617 - MergeTwoBinaryTrees/MergeTwoBinaryTrees_Java/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == t2) return null;
+        if(t1 == null || t2 == null) return t1 == null ? t2 : t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
