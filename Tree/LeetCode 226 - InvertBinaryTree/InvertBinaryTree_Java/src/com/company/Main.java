06cff2a3cf62fb65c9deb006909d85cf890f0399
Xing <x.xing.unsw@gmail.com>
2021-02-04 18:14:31 +1100

    JavaSolution

---
+++ Tree/LeetCode 226 - InvertBinaryTree/InvertBinaryTree_Java/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
