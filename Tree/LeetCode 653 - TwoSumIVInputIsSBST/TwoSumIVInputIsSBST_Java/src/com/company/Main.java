b617a6d0cb707e1effedda70bd6f74f8dcf14916
Xing <x.xing.unsw@gmail.com>
2021-02-05 20:19:29 +1100

    JavaSolution

---
+++ Tree/LeetCode 653 - TwoSumIVInputIsSBST/TwoSumIVInputIsSBST_Java/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.HashSet;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    HashSet<Integer> set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false;
+        if(set.contains(k - root.val)) return true;
+        set.add(root.val);
+        return findTarget(root.left, k) || findTarget(root.right, k);
+    }
+}
