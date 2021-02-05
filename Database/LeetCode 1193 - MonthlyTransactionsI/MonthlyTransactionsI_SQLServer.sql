1edc0f9b87d0eef5bb1bf1f7b70b827bd155e40f
Xing <x.xing.unsw@gmail.com>
2021-02-05 17:40:40 +1100

    SQLServer

---
+++ Database/LeetCode 1193 - MonthlyTransactionsI/MonthlyTransactionsI_SQLServer.sql
@@ -0,0 +1,6 @@
+SELECT LEFT(trans_date, 7) AS month, country, 
+COUNT(*) AS trans_count,
+SUM(CASE state WHEN 'approved' THEN 1 ELSE 0 END) AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(CASE state WHEN 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM transactions GROUP BY country, LEFT(trans_date, 7);
