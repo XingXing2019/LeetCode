b4744da0ec1fb2bfc487353fa77d017f6648652b
Xing <x.xing.unsw@gmail.com>
2021-02-07 15:02:15 +1100

    init

---
+++ TwoPointers/LeettCode 1750 - MinimumLengthOfStringAfterDeletingSimilarEnds/MinimumLengthOfString/MinimumLengthOfString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MinimumLengthOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MinimumLength(string s)
+        {
+            int li = 0, hi = s.Length - 1;
+            while (li < hi && s[li] == s[hi])
+            {
+                while (li < hi && s[li] == s[li + 1])
+                    li++;
+                while (li < hi && s[hi] == s[hi - 1])
+                    hi--;
+                li++;
+                hi--;
+            }
+            return hi >= li ? hi - li + 1 : 0;
+        }
+    }
+}
