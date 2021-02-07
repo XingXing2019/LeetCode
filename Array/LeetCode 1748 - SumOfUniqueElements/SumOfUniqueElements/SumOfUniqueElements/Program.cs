a2d603cc83ee59cf2f9c42fb6c7dda599be4c247
Xing <x.xing.unsw@gmail.com>
2021-02-07 14:33:43 +1100

    init

---
+++ Array/LeetCode 1748 - SumOfUniqueElements/SumOfUniqueElements/SumOfUniqueElements/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfUniqueElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int SumOfUnique(int[] nums)
+        {
+            return nums.GroupBy(x => x).Where(x => x.Count() == 1).Sum(x => x.Key);
+        }
+    }
+}
