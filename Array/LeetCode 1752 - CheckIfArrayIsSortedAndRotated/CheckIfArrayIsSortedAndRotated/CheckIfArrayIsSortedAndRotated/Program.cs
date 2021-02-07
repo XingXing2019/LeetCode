3e745132bd87ac100dca390a7ae0a37c0eacbe94
Xing <x.xing.unsw@gmail.com>
2021-02-07 15:28:22 +1100

    init

---
+++ Array/LeetCode 1752 - CheckIfArrayIsSortedAndRotated/CheckIfArrayIsSortedAndRotated/CheckIfArrayIsSortedAndRotated/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CheckIfArrayIsSortedAndRotated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool Check(int[] nums)
+        {
+            int count = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[i - 1])
+                {
+                    count++;
+                    if (count > 1) return false;
+                }
+            }
+            return count == 0 || nums[0] >= nums[^1];
+        }
+    }
+}
