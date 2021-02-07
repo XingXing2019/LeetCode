8121e905184b1c5bf1d5a5fc0ac1b1630a9e541d
Xing <x.xing.unsw@gmail.com>
2021-02-07 15:36:14 +1100

    init

---
+++ Heap/LeetCode 1753 - MaximumScoreFromRemovingStones/MaximumScoreFromRemovingStones/MaximumScoreFromRemovingStones/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaximumScoreFromRemovingStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MaximumScore(int a, int b, int c)
+        {
+            int[] piles = {a, b, c};
+            Array.Sort(piles);
+            int res = 0;
+            while (piles[^1] != 0 && piles[^2] != 0)
+            {
+                res++;
+                piles[^1]--;
+                piles[^2]--;
+                Array.Sort(piles);
+            }
+            return res;
+        }
+    }
+}
