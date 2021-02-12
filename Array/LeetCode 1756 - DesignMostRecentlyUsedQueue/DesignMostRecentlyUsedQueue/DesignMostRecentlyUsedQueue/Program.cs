8421e8ff985cb9c7fa05cb76f41a142a8198b564
Xing <x.xing.unsw@gmail.com>
2021-02-12 18:32:45 +1100

    init

---
+++ Array/LeetCode 1756 - DesignMostRecentlyUsedQueue/DesignMostRecentlyUsedQueue/DesignMostRecentlyUsedQueue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DesignMostRecentlyUsedQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class MRUQueue
+    {
+        private int[] nums;
+        public MRUQueue(int n)
+        {
+            nums = new int[n];
+            for (int i = 1; i <= n; i++)
+                nums[i - 1] = i;
+        }
+
+        public int Fetch(int k)
+        {
+            for (int i = k - 1; i < nums.Length - 1; i++)
+            {
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }
+            return nums[^1];
+        }
+    }
+}
