60d40eb6b8ee1c069b3630c508b8663e27bd3f52
Xing <x.xing.unsw@gmail.com>
2021-02-14 16:53:45 +1100

    init

---
+++ String/LeetCode 1759 - CountNumberOfHomogenousSubstrings/CountNumberOfHomogenousSubstrings/CountNumberOfHomogenousSubstrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CountNumberOfHomogenousSubstrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int CountHomogenous(string s)
+        {
+            long res = 0;
+            int li = 0, hi = 0, mod = 1_000_000_000 + 7;
+            while (hi < s.Length)
+            {
+                while (hi < s.Length && s[li] == s[hi])
+                    hi++;
+                int len = hi - li;
+                for (int i = 1; i <= len; i++)
+                    res += i;
+                li = hi;
+            }
+            return (int) (res % mod);
+        }
+    }
+}
