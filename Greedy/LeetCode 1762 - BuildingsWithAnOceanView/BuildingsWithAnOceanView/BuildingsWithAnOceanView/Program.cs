c6f4286a517aabdc6cac4e29484cf64adb8a059f
Xing <x.xing.unsw@gmail.com>
2021-02-18 17:39:29 +1100

    init

---
+++ Greedy/LeetCode 1762 - BuildingsWithAnOceanView/BuildingsWithAnOceanView/BuildingsWithAnOceanView/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuildingsWithAnOceanView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] FindBuildings(int[] heights)
+        {
+            var rightMax = new int[heights.Length];
+            var res = new List<int>();
+            for (int i = heights.Length - 2; i >= 0; i--)
+                rightMax[i] = Math.Max(heights[i + 1], rightMax[i + 1]);
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if(heights[i] > rightMax[i])
+                    res.Add(i);
+            }
+            return res.ToArray();
+        }
+    }
+}
