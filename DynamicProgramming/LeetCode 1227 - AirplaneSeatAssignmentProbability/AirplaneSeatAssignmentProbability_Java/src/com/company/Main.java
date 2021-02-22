f24735e44a8c089133c108e3d44d66fa32ac8408
Xing <x.xing.unsw@gmail.com>
2021-02-22 18:31:18 +1100

    JavaSolution

---
+++ DynamicProgramming/LeetCode 1227 - AirplaneSeatAssignmentProbability/AirplaneSeatAssignmentProbability_Java/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public double nthPersonGetsNthSeat(int n) {
+        return n == 1 ? 1 : 0.5;
+    }
+}
