1e00a8d7ae7d50e379dd890b384f5e0cfc961f46
Xing <x.xing.unsw@gmail.com>
2021-02-22 14:28:37 +1100

    init

---
+++ Greedy/LeetCode 1769 - MinimumNumberOfOperationsToMoveAllBallsToEachBox/MinimumNumberOfOperations/MinimumNumberOfOperations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinimumNumberOfOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] MinOperations(string boxes)
+        {
+            var ones = new List<int>();
+            for (int i = 0; i < boxes.Length; i++)
+                if(boxes[i] == '1') ones.Add(i);
+            var res = new int[boxes.Length];
+            for (int i = 0; i < res.Length; i++)
+                res[i] = ones.Sum(x => Math.Abs(x - i));
+            return res;
+        }
+    }
+}
