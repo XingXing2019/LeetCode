0c072dc50b0bc647ff9801e1f55333dda50c8c99
Xing <x.xing.unsw@gmail.com>
2021-02-25 18:18:38 +1100

    JavaSolution

---
+++ Math/LeetCode 1131 - MaximumOfAbsoluteValueExpression/MaximumOfAbsoluteValueExpression_Java/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    public int maxAbsValExpr(int[] arr1, int[] arr2) {
+        int[] max = {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+        int[] min = {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE};
+        int[] sign = {1, 1, -1, -1, 1};
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                max[j] = Math.max(max[j], arr1[i] + arr2[i] * sign[j] + i * sign[j + 1]);
+                min[j] = Math.min(min[j], arr1[i] + arr2[i] * sign[j] + i * sign[j + 1]);
+                res = Math.max(res, max[j] - min[j]);
+            }
+        }
+        return res;
+    }
+}
