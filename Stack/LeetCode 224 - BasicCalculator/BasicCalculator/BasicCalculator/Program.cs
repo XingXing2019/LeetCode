f0c8bb17cca9dc46e4f0e699eef317d1490f8e2e
Xing <x.xing.unsw@gmail.com>
2021-02-26 22:02:52 +1100

    init

---
+++ Stack/LeetCode 224 - BasicCalculator/BasicCalculator/BasicCalculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = " 2-1 + 2 ";
+            Console.WriteLine(Calculate(s));
+        }
+        public static int Calculate(string s)
+        {
+            s += '@';
+            var num = "";
+            var nums = new Stack<int>();
+            var operators = new Stack<char>();
+            foreach (var l in s)
+            {
+                if (l == ' ') continue;
+                if (char.IsDigit(l))
+                    num += l;
+                else
+                {
+                    if (num != "") nums.Push(int.Parse(num));
+                    num = "";
+                    if (l == ')')
+                    {
+                        int temp = 0;
+                        while (operators.Count != 0 && operators.Peek() != '(')
+                            temp += operators.Pop() == '-' ? -nums.Pop() : nums.Pop();
+                        temp += nums.Pop();
+                        operators.Pop();
+                        nums.Push(temp);
+                    }
+                    else if (l != '@')
+                        operators.Push(l);
+                }
+            }
+            int res = 0;
+            while (nums.Count != 0)
+                res += operators.Count == 0 || operators.Pop() == '+' ? nums.Pop() : -nums.Pop();
+            return res;
+        }
+    }
+}
