40afc45ed91abf11ea3c95ec3b810d070b898b28
Xing <x.xing.unsw@gmail.com>
2021-02-28 15:38:49 +1100

    init

---
+++ Array/LeetCode 1773 - CountItemsMatchingARule/CountItemsMatchingARule/CountItemsMatchingARule/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountItemsMatchingARule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            if (ruleKey == "type") return items.Count(x => x[0] == ruleValue);
+            if (ruleKey == "color") return items.Count(x => x[1] == ruleValue);
+            return items.Count(x => x[2] == ruleValue);
+        }
+    }
+}
