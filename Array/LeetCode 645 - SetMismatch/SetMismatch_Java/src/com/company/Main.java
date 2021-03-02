4e5649a74a4a29155e4602d9587f77c8578b4753
Xing <x.xing.unsw@gmail.com>
2021-03-02 20:34:12 +1100

    JavaSolution

--- Array/LeetCode 645 - SetMismatch/SetMismatch_Java/src/com/company/Main.java
+++ Array/LeetCode 645 - SetMismatch/SetMismatch_Java/src/com/company/Main.java
@@ -3,14 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
     public int[] findErrorNums(int[] nums) {
         int sum = (1 + nums.length) * nums.length / 2;
         int[] res = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            if(nums[Math.abs(nums[i]) - 1] < 0) res[0] = Math.abs(nums[i]);
+            if (nums[Math.abs(nums[i]) - 1] < 0) res[0] = Math.abs(nums[i]);
             else sum -= Math.abs(nums[i]);
             nums[Math.abs(nums[i]) - 1] *= -1;
         }
