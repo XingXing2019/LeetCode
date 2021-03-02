271ac9cf3290f61172b2f45ecac2e4fc5df965a9
Xing <x.xing.unsw@gmail.com>
2021-03-02 21:54:17 +1100

    JavaSolution

--- Greedy/LeetCode 1296 - DivideArrayInSetsOfKConsecutiveNumbers/DivideArrayInSets_Java/src/com/company/Main.java
+++ Greedy/LeetCode 1296 - DivideArrayInSetsOfKConsecutiveNumbers/DivideArrayInSets_Java/src/com/company/Main.java
@@ -1,27 +1,34 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2, 2, 3, 3};
+        int[] nums = {3, 3, 2, 2, 1, 1};
         int k = 3;
-        isPossibleDivide(nums, k);
+        System.out.println(isPossibleDivide(nums, k));
     }
 
     public static boolean isPossibleDivide(int[] nums, int k) {
-        if (nums.length % k != 0) return false;
-        int max = Arrays.stream(nums).max().getAsInt();
-        int[] record = new int[max + 1];
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums)
-            record[num]++;
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        int[][] record = new int[map.size()][];
+        int index = 0;
+        for (int key : map.keySet())
+            record[index++] = new int[]{key, map.get(key)};
+        Arrays.sort(record, (a, b) -> a[0] - b[0]);
         for (int i = 0; i < record.length; i++) {
-            if (record[i] == 0) continue;
-            int count = record[i];
+            if(record[i][1] == 0) continue;
+            int first = record[i][0], count = record[i][1];
             for (int j = 0; j < k; j++) {
-                if(i + j >= record.length || record[i + j] - count < 0) return false;
-                record[i + j] -= count;
+                if(i + j >= record.length || first + j != record[i + j][0] || record[i + j][1] - count < 0)
+                    return false;
+                record[i + j][1] -= count;
             }
         }
         return true;
