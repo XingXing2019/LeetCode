25939202bd253326252f18666927903d8151991e
Xing <x.xing.unsw@gmail.com>
2021-03-03 21:43:58 +1100

    init

---
+++ Backtracking/LeetCode 1774 - ClosestDessertCost/ClosestDessertCost/ClosestDessertCost/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClosestDessertCost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int ClosestCost(int[] baseCosts, int[] toppingCosts, int target)
+        {
+            int res = int.MaxValue;
+            foreach (var baseCost in baseCosts)
+                DFS(toppingCosts, baseCost, target, 0, ref res);
+            return res;
+        }
+
+        public void DFS(int[] toppingCosts, int cost, int target, int index, ref int res)
+        {
+            if (Math.Abs(target - res) > Math.Abs(target - cost))
+                res = cost;
+            else if (Math.Abs(target - res) > Math.Abs(target - cost))
+                res = Math.Min(res, cost);
+            if (index == toppingCosts.Length || cost > target)
+                return;
+            for (int i = 0; i <= 2; i++)
+                DFS(toppingCosts, cost + toppingCosts[index] * i, target, index + 1, ref res);
+        }
+    }
+}
