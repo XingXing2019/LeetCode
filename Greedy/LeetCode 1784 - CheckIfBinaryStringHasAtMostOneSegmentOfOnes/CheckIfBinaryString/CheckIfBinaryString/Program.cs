17c36f1986144b5cf8a1ca7e4d6fbac96fa3780d
Xing <x.xing.unsw@gmail.com>
2021-03-07 20:45:19 +1100

    init

---
+++ Greedy/LeetCode 1784 - CheckIfBinaryStringHasAtMostOneSegmentOfOnes/CheckIfBinaryString/CheckIfBinaryString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CheckIfBinaryString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool CheckOnesSegment(string s)
+        {
+            bool seenZero = false;
+            foreach (var l in s)
+            {
+                if (l == '0') seenZero = true;
+                else if (seenZero) return false;
+            }
+            return true;
+        }
+
+        public bool CheckOnesSegment_OneLine(string s)
+        {
+            return !s.Contains("01");
+        }
+    }
+}
