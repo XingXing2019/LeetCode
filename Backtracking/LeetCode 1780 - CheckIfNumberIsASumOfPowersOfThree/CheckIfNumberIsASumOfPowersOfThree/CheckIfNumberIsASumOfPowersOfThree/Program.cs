ab7db9ff87bbcd45a1bf4c215aceb7e1f0f76238
Xing <x.xing.unsw@gmail.com>
2021-03-07 20:14:04 +1100

    init

---
+++ Backtracking/LeetCode 1780 - CheckIfNumberIsASumOfPowersOfThree/CheckIfNumberIsASumOfPowersOfThree/CheckIfNumberIsASumOfPowersOfThree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckIfNumberIsASumOfPowersOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1000000;
+            Console.WriteLine(CheckPowersOfThree(n));
+        }
+
+        public static bool CheckPowersOfThree(int n)
+        {
+            while (n != 0)
+            {
+                if (n % 3 == 2) return false;
+                n /= 3;
+            }
+            return true;
+        }
+    }
+}
