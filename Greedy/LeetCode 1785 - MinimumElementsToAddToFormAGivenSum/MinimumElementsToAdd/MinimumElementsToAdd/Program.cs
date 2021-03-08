48a83f88648dcb81bd34edaef6a6df00a5663143
Xing <x.xing.unsw@gmail.com>
2021-03-08 16:58:39 +1100

    init

---
+++ Greedy/LeetCode 1785 - MinimumElementsToAddToFormAGivenSum/MinimumElementsToAdd/MinimumElementsToAdd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumElementsToAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public int MinElements(int[] nums, int limit, int goal)
+        {
+            decimal sum = 0;
+            foreach (var num in nums) sum += num;
+            return (int) Math.Ceiling((Math.Abs(sum - goal)) / limit);
+        }
+    }
+}
