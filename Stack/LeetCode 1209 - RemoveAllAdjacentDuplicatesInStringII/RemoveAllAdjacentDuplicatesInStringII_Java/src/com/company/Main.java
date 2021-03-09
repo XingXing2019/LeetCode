3c702df55a737feabbe2830c9c22358c9a11daa6
Xing <x.xing.unsw@gmail.com>
2021-03-09 18:53:06 +1100

    faster

--- Stack/LeetCode 1209 - RemoveAllAdjacentDuplicatesInStringII/RemoveAllAdjacentDuplicatesInStringII_Java/src/com/company/Main.java
+++ Stack/LeetCode 1209 - RemoveAllAdjacentDuplicatesInStringII/RemoveAllAdjacentDuplicatesInStringII_Java/src/com/company/Main.java
@@ -14,19 +14,19 @@ public class Main {
 
     public static String removeDuplicates(String s, int k) {
         Stack<int[]> stack = new Stack<>();
-        for (Character l : s.toCharArray()){
-            if(stack.isEmpty() || l != stack.peek()[0])
+        for (Character l : s.toCharArray()) {
+            if (stack.isEmpty() || l != stack.peek()[0])
                 stack.push(new int[]{l, 1});
             else
                 stack.push(new int[]{l, stack.peek()[1] + 1});
-            if(stack.peek()[1] == k){
+            if (stack.peek()[1] == k) {
                 for (int i = 0; i < k; i++)
                     stack.pop();
             }
         }
-        String res = "";
+        StringBuilder res = new StringBuilder();
         while (!stack.isEmpty())
-            res = (char)stack.pop()[0] + res;
-        return res;
+            res.append((char) stack.pop()[0]);
+        return res.reverse().toString();
     }
 }
