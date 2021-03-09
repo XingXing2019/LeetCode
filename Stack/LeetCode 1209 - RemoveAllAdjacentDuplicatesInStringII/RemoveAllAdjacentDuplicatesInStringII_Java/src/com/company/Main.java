e339a669b845eb73b61774aa2f905a6e5358bdba
Xing <x.xing.unsw@gmail.com>
2021-03-09 19:09:13 +1100

    OneMoreSolution

--- Stack/LeetCode 1209 - RemoveAllAdjacentDuplicatesInStringII/RemoveAllAdjacentDuplicatesInStringII_Java/src/com/company/Main.java
+++ Stack/LeetCode 1209 - RemoveAllAdjacentDuplicatesInStringII/RemoveAllAdjacentDuplicatesInStringII_Java/src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println(removeDuplicates(s, k));
     }
 
-    public static String removeDuplicates(String s, int k) {
+    public static String removeDuplicates_stack(String s, int k) {
         Stack<int[]> stack = new Stack<>();
         for (Character l : s.toCharArray()) {
             if (stack.isEmpty() || l != stack.peek()[0])
@@ -28,5 +28,23 @@ public class Main {
         while (!stack.isEmpty())
             res.append((char) stack.pop()[0]);
         return res.reverse().toString();
+    }
+
+    public static String removeDuplicates_linkedList(String s, int k) {
+        List<int[]> stack = new LinkedList<>();
+        for (Character l : s.toCharArray()) {
+            if (stack.isEmpty() || l != stack.get(stack.size() - 1)[0])
+                stack.add(new int[]{l, 1});
+            else
+                stack.add(new int[]{l, stack.get(stack.size() - 1)[1] + 1});
+            if (stack.get(stack.size() - 1)[1] == k) {
+                for (int i = 0; i < k; i++)
+                    stack.remove(stack.size() - 1);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < stack.size(); i++)
+            res.append((char) stack.get(i)[0]);
+        return res.toString();
     }
 }
