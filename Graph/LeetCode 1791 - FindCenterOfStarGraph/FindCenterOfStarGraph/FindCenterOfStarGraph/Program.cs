0c13fe937f00525ea72363abdf28d077fcec07a1
Xing <x.xing.unsw@gmail.com>
2021-03-14 15:56:58 +1100

    init

---
+++ Graph/LeetCode 1791 - FindCenterOfStarGraph/FindCenterOfStarGraph/FindCenterOfStarGraph/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FindCenterOfStarGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int FindCenter(int[][] edges)
+        {
+            int[] nodes = new int[edges.Length + 2];
+            foreach (var edge in edges)
+            {
+                nodes[edge[0]]++;
+                nodes[edge[1]]++;
+            }
+            return Array.IndexOf(nodes, edges.Length);
+        }
+    }
+}
