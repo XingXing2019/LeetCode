925a2f481d92100cfec7e84f8934c66489415fe5
Xing <x.xing.unsw@gmail.com>
2021-03-17 20:56:35 +1100

    init

---
+++ Math/LeetCode 478 - GenerateRandomPointInACircle/GenerateRandomPointInACircle/GenerateRandomPointInACircle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GenerateRandomPointInACircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        private Random _random;
+        private double _radius;
+        private double _x;
+        private double _y;
+        public Solution(double radius, double x_center, double y_center)
+        {
+            _random = new Random();
+            _radius = radius;
+            _x = x_center;
+            _y = y_center;
+        }
+
+        public double[] RandPoint()
+        {
+            double x = 0, y = 0;
+            do
+            {
+                x = (_random.NextDouble() > 0.5 ? 1 : -1) * _random.NextDouble() * _radius;
+                y = (_random.NextDouble() > 0.5 ? 1 : -1) * _random.NextDouble() * _radius;
+            }
+            while (x * x + y * y > _radius * _radius);
+            return new[] { _x + x, _y + y };
+        }
+    }
+}
