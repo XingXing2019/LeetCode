932c4ec13b4b520082294fb0adc7950ed3f88da4
Xing <x.xing.unsw@gmail.com>
2021-03-21 14:49:15 +1100

    init

---
+++ Design/LeetCode 1797 - DesignAuthenticationManager/DesignAuthenticationManager/DesignAuthenticationManager/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DesignAuthenticationManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class AuthenticationManager
+    {
+        private Dictionary<string, int> expireAt;
+        private int liveTime;
+        public AuthenticationManager(int timeToLive)
+        {
+            expireAt = new Dictionary<string, int>();
+            liveTime = timeToLive;
+        }
+
+        public void Generate(string tokenId, int currentTime)
+        {
+            expireAt[tokenId] = currentTime + liveTime;
+        }
+
+        public void Renew(string tokenId, int currentTime)
+        {
+            if(!expireAt.ContainsKey(tokenId) || expireAt[tokenId] <= currentTime)
+                return;
+            expireAt[tokenId] = currentTime + liveTime;
+        }
+
+        public int CountUnexpiredTokens(int currentTime)
+        {
+            return expireAt.Count(x => x.Value > currentTime);
+        }
+    }
+
+}
