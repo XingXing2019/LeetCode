ff86fb1d5a5789ff50c71349db83a8b7f3484b54
Xing <x.xing.unsw@gmail.com>
2021-03-21 13:55:42 +1100

    init

---
+++ String/LeetCode 1796 - SecondLargestDigitInAString/SecondLargestDigitInAString/SecondLargestDigitInAString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SecondLargestDigitInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abc1111";
+            Console.WriteLine(SecondHighest(s));
+        }
+        public static int SecondHighest(string s)
+        {
+            int max1 = int.MinValue, max2 = int.MinValue;
+            foreach (var l in s)
+            {
+                if (!char.IsDigit(l)) continue;
+                if (l - '0' >= max1)
+                {
+                    if (l - '0' == max1) continue;
+                    max2 = max1;
+                    max1 = l - '0';
+                }
+                else if (l - '0' > max2)
+                    max2 = l - '0';
+            }
+            return max2 == int.MinValue ? -1 : max2;
+        }
+    }
+}
