d98d35010dc679ef12e51df8984ce3b4e2f21933
Xing <x.xing.unsw@gmail.com>
2021-03-21 15:49:05 +1100

    init

---
+++ Greedy/LeetCode 1798 - MaximumNumberOfConsecutiveValuesYouCanMake/MaximumNumberOfConsecutiveValues/MaximumNumberOfConsecutiveValues/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumNumberOfConsecutiveValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coins = {1, 1, 1, 4};
+            Console.WriteLine(GetMaximumConsecutive(coins));
+        }
+        public static int GetMaximumConsecutive(int[] coins)
+        {
+            Array.Sort(coins);
+            int res = 1;
+            foreach (var coin in coins)
+            {
+                if(coin > res) break;
+                res += coin;
+            }
+            return res;
+        }
+    }
+}
