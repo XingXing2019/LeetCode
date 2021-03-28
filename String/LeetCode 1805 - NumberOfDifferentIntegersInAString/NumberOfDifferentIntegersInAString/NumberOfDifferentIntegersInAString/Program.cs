6d5e642c04171ed0de5ff62b01cea256c76fef34
Xing <x.xing.unsw@gmail.com>
2021-03-28 17:51:22 +1100

    init

---
+++ String/LeetCode 1805 - NumberOfDifferentIntegersInAString/NumberOfDifferentIntegersInAString/NumberOfDifferentIntegersInAString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberOfDifferentIntegersInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "a123bc34d8ef34";
+            Console.WriteLine(NumDifferentIntegers(word));
+        }
+        public static int NumDifferentIntegers(string word)
+        {
+            var separators = new char[26];
+            for (int i = 0; i < separators.Length; i++)
+                separators[i] = (char) (i + 'a');
+            var nums = word.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            var set = new HashSet<string>();
+            foreach (var num in nums)
+            {
+                var temp = "";
+                var isLeadingZero = true;
+                foreach (var digit in num)
+                {
+                    if (digit != '0')
+                    {
+                        temp += digit;
+                        isLeadingZero = false;
+                    }
+                    else if (!isLeadingZero)
+                        temp += digit;
+                }
+                set.Add(temp);
+            }
+            return set.Count;
+        }
+    }
+}
