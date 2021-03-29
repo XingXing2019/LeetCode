99aa3dd430eef0a75bf8947a5a682db5056c518b
Xing <x.xing.unsw@gmail.com>
2021-03-29 17:17:06 +1100

    JavaSolution

---
+++ BitManipulation/LeetCode 190 - ReverseBits/ReverseBits_Java/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+}
