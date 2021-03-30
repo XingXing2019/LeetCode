2c556f98d3347c7b54b36035a594a36db0750835
Xing <x.xing.unsw@gmail.com>
2021-03-30 21:58:08 +1100

    init

---
+++ DynamicProgramming/LeetCode 354 - RussianDollEnvelopes/RussianDollEnvelopes/RussianDollEnvelopes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace RussianDollEnvelopes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MaxEnvelopes(int[][] envelopes)
+        {
+            envelopes = envelopes.OrderBy(x => x[0]).ThenBy(x => x[1]).ToArray();
+            var dp = new int[envelopes.Length];
+            Array.Fill(dp, 1);
+            int res = 0;
+            for (int i = 0; i < envelopes.Length; i++)
+            {
+                for (int j = 0; j < envelopes.Length; j++)
+                {
+                    if (envelopes[i][0] < envelopes[j][0] && envelopes[i][1] < envelopes[j][1])
+                        dp[j] = Math.Max(dp[j], dp[i] + 1);
+                }
+                res = Math.Max(res, dp[i]);
+            }
+            return res;
+        }
+    }
+}
