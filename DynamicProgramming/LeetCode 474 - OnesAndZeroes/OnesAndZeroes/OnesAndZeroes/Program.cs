a34b6b1683ab769f6ae24bfe6a804dbc31c27442
Xing <x.xing.unsw@gmail.com>
2021-04-02 21:42:36 +1100

    init

---
+++ DynamicProgramming/LeetCode 474 - OnesAndZeroes/OnesAndZeroes/OnesAndZeroes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace OnesAndZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sts = { "0001", "0101", "1000", "1000" };
+            int m = 9, n = 3;
+            Console.WriteLine(FindMaxForm(sts, m, n));
+        }
+        public static int FindMaxForm(string[] strs, int m, int n)
+        {
+            var dp = new int[m + 1][];
+            for (int i = 0; i < dp.Length; i++)
+                dp[i] = new int[n + 1];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                var zero = strs[i].Count(x => x == '0');
+                var one = strs[i].Count(x => x == '1');
+                for (int j = m; j >= zero; j--)
+                {
+                    for (int k = n; k >= one; k--)
+                        dp[j][k] = Math.Max(dp[j][k], dp[j - zero][k - one] + 1);
+                }
+            }
+            return dp[^1][^1];
+        }
+    }
+}
