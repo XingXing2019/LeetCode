32dc18703dba276b051e1df94103ae604e3af86e
Xing <x.xing.unsw@gmail.com>
2021-04-04 16:38:31 +1000

    init

---
+++ HashTable/LeetCode 1814 - CountNicePairsInAnArray/CountNicePairsInAnArray/CountNicePairsInAnArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountNicePairsInAnArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int CountNicePairs(int[] nums)
+		{
+			int[] rev = new int[nums.Length];
+			for (int i = 0; i < nums.Length; i++)
+			{
+				int num = nums[i];
+				while (num != 0)
+				{
+					rev[i] = rev[i] * 10 + num % 10;
+					num /= 10;
+				}
+			}
+			long res = 0, mod = 1_000_000_000 + 7;
+			var dict = new Dictionary<int, int>();
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (dict.ContainsKey(nums[i] - rev[i]))
+					res += dict[nums[i] - rev[i]];
+				else
+					dict[nums[i] - rev[i]] = 0;
+				dict[nums[i] - rev[i]]++;
+			}
+			return (int) (res % mod);
+		}
+	}
+}
