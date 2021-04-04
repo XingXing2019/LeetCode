ac2afc1a3486035f0f926204e3dc4a5b9431c2d1
Xing <x.xing.unsw@gmail.com>
2021-04-04 15:34:37 +1000

    init

---
+++ String/LeetCode 1816 - TruncateSentence/TruncateSentence/TruncateSentence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TruncateSentence
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public string TruncateSentence(string s, int k)
+		{
+			var words = s.Split(' ');
+			var res = "";
+			for (int i = 0; i < k; i++)
+				res += i == k - 1 ? words[i] : $"{words[i]} ";
+			return res;
+		}
+	}
+}
