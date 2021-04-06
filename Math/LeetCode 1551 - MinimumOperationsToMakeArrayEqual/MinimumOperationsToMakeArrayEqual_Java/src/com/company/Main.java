09b19d8d9a0c001d890cd8a91e494020feeb022a
Xing <x.xing.unsw@gmail.com>
2021-04-06 18:06:48 +1000

    JavaSolution

---
+++ Math/LeetCode 1551 - MinimumOperationsToMakeArrayEqual/MinimumOperationsToMakeArrayEqual_Java/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    public int minOperations(int n) {
+        return (n - n / 2 ) * (n / 2);
+    }
+}
