9d62cc35d0e7fceab3f7b0913e779cb465a3aef8
Xing <x.xing.unsw@gmail.com>
2021-04-11 15:40:10 +1000

    init

---
+++ Math/LeetCode 1822 - SignOfTheProductOfAnArray/SignOfTheProductOfAnArray/SignOfTheProductOfAnArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SignOfTheProductOfAnArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int ArraySign(int[] nums)
+		{
+			int negative = 0;
+			foreach (var num in nums)
+			{
+				if (num == 0) return 0;
+				negative += num < 0 ? 1 : 0;
+			}
+			return negative % 2 == 0 ? 1 : -1;
+		}
+	}
+}
