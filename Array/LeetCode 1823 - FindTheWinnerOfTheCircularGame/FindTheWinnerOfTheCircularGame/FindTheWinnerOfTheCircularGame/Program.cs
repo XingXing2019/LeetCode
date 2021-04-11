192d58ba0d169c713fd49c31080de58c07877380
Xing <x.xing.unsw@gmail.com>
2021-04-11 15:37:28 +1000

    init

---
+++ Array/LeetCode 1823 - FindTheWinnerOfTheCircularGame/FindTheWinnerOfTheCircularGame/FindTheWinnerOfTheCircularGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindTheWinnerOfTheCircularGame
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 10, k = 8;
+			Console.WriteLine(FindTheWinner(n, k));
+		}
+		public static int FindTheWinner(int n, int k)
+		{
+			var queue = new Queue<int>();
+			for (int i = 1; i <= n; i++)
+				queue.Enqueue(i);
+			while (queue.Count != 1)
+			{
+				var times = k % queue.Count == 0 ? queue.Count : k % queue.Count;
+				for (int i = 0; i < times; i++)
+				{
+					var temp = queue.Dequeue();
+					if(i != times - 1)
+						queue.Enqueue(temp);
+				}	
+			}
+			return queue.Dequeue();
+		}
+	}
+}
