486ba1e5adb5c2d804900ee2abd686f2c64a3ca8
Xing <x.xing.unsw@gmail.com>
2021-04-13 17:45:46 +1000

    init

---
+++ BinarySearch/LeetCode 315 - CountOfSmallerNumbersAfterSelf/CountOfSmallerNumbersAfterSelf/CountOfSmallerNumbersAfterSelf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountOfSmallerNumbersAfterSelf
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public IList<int> CountSmaller(int[] nums)
+		{
+			var right = new List<int>();
+			var res = new int[nums.Length];
+			for (int i = nums.Length - 1; i >= 0; i--)
+			{
+				int li = 0, hi = right.Count - 1;
+				while (li <= hi)
+				{
+					int mid = li + (hi - li) / 2;
+					if (right[mid] >= nums[i]) hi = mid - 1;
+					else li = mid + 1;
+				}
+				res[i] = li;
+				right.Insert(li, nums[i]);
+			}
+			return res;
+		}
+	}
+}
