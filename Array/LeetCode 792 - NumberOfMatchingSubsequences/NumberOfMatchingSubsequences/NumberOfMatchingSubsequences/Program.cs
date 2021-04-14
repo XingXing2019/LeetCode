d5636fe525fd266e2e7f2c7625c86ddf5c291ee3
Xing <x.xing.unsw@gmail.com>
2021-04-14 20:49:48 +1000

    init

---
+++ Array/LeetCode 792 - NumberOfMatchingSubsequences/NumberOfMatchingSubsequences/NumberOfMatchingSubsequences/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberOfMatchingSubsequences
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string s = "abcde";
+			string[] words = {"a", "bb", "acd", "ace"};
+			Console.WriteLine(NumMatchingSubseq(s, words));
+		}
+		public static int NumMatchingSubseq(string s, string[] words)
+		{
+			var record = new List<StringBuilder>[26];
+			for (int i = 0; i < record.Length; i++)
+				record[i] = new List<StringBuilder>();
+			int res = 0;
+			foreach (var word in words)
+				record[word[0] - 'a'].Add(new StringBuilder(word));
+			foreach (var l in s)
+			{
+				var temp = record[l - 'a'];
+				record[l - 'a'] = new List<StringBuilder>();
+				foreach (var word in temp)
+				{
+					word.Remove(0, 1);
+					if (word.Length == 0) 
+						res++;
+					else
+						record[word[0] - 'a'].Add(word);
+				}
+			}
+			return res;
+		}
+	}
+}
