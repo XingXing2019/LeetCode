6fe61bb3b08627e11daf48827dabe5d8b45727b9
Xing <x.xing.unsw@gmail.com>
2021-04-18 16:44:48 +1000

    init

---
+++ String/LeetCode 1832 - CheckIfTheSentenceIsPangram/CheckIfTheSentenceIsPangram/CheckIfTheSentenceIsPangram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CheckIfTheSentenceIsPangram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public bool CheckIfPangram(string sentence)
+		{
+			var letters = new int[26];
+			foreach (var l in sentence)
+				letters[l - 'a']++;
+			return letters.All(x => x > 0);
+		}
+	}
+}
