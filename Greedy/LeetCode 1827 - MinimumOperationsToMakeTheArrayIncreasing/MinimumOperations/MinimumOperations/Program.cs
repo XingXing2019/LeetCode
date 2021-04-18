e300767a896e2cfc34801cd5a56963b3632cede4
Xing <x.xing.unsw@gmail.com>
2021-04-18 16:15:35 +1000

    init

---
+++ Greedy/LeetCode 1827 - MinimumOperationsToMakeTheArrayIncreasing/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MinimumOperations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MinOperations(int[] nums)
+		{
+			int res = 0;
+			for (int i = 1; i < nums.Length; i++)
+			{
+				if(nums[i] > nums[i - 1]) continue;
+				res += nums[i - 1] - nums[i] + 1;
+				nums[i] = nums[i - 1] + 1;
+			}
+			return res;
+		}
+	}
+}
