489ad00e45490548a104d126cead8c0fc3d8c41c
Xing <x.xing.unsw@gmail.com>
2021-04-18 17:57:43 +1000

    init

---
+++ Sort/LeetCode 1833 - MaximumIceCreamBars/MaximumIceCreamBars/MaximumIceCreamBars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MaximumIceCreamBars
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MaxIceCream(int[] costs, int coins)
+		{
+			Array.Sort(costs);
+			int res = 0;
+			foreach (var cost in costs)
+			{
+				if (coins - cost < 0)
+					return res;
+				coins -= cost;
+				res++;
+			}
+			return res;
+		}
+	}
+}
