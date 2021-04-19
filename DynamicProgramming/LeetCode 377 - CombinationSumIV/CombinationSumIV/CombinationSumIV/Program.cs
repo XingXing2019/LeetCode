71898249c6202ad55a2194cdf51ef5ee8fb3f259
Xing <x.xing.unsw@gmail.com>
2021-04-19 17:52:10 +1000

    init

---
+++ DynamicProgramming/LeetCode 377 - CombinationSumIV/CombinationSumIV/CombinationSumIV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CombinationSumIV
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int CombinationSum4(int[] nums, int target)
+		{
+			var dp = new int[target + 1];
+			dp[0] = 1;
+			for (int i = 1; i < dp.Length; i++)
+			{
+				foreach (var num in nums)
+				{
+					if(i - num < 0) continue;
+					dp[i] += dp[i - num];
+				}
+			}
+			return dp[^1];
+		}
+	}
+}
