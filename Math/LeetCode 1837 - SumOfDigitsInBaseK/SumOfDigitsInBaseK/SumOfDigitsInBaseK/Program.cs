d632f62067237be2bf21fbc5c20d0c71f05790e5
Xing <x.xing.unsw@gmail.com>
2021-04-25 16:34:52 +1000

    init

---
+++ Math/LeetCode 1837 - SumOfDigitsInBaseK/SumOfDigitsInBaseK/SumOfDigitsInBaseK/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumOfDigitsInBaseK
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int SumBase(int n, int k)
+		{
+			int res = 0;
+			while (n != 0)
+			{
+				res += n % k;
+				n /= k;
+			}
+			return res;
+		}
+	}
+}
