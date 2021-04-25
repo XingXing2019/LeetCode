e3233c3740cecb785902052a1bcfbc1aaa2050d1
Xing <x.xing.unsw@gmail.com>
2021-04-25 17:59:17 +1000

    init

---
+++ Greedy/LeetCode 1838 - FrequencyOfTheMostFrequentElement/FrequencyOfTheMostFrequentElement/FrequencyOfTheMostFrequentElement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace FrequencyOfTheMostFrequentElement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = { 1, 2, 4, 4, 2, 1, 2, 3, 4, 5, 2, 1 };
+			int k = 7;
+			Console.WriteLine(MaxFrequency(nums, k));
+		}
+		public static int MaxFrequency(int[] nums, int k)
+		{
+			Array.Sort(nums);
+			int li = 0, hi = 0, res = 0;
+			long sum = 0;
+			while (hi < nums.Length)
+			{
+				sum += nums[hi];
+				while (sum + k < (long)nums[hi] * (hi - li + 1))
+					sum -= nums[li++];
+				res = Math.Max(res, hi - li + 1);
+				hi++;
+			}
+			return res;
+		}
+	}
+}
