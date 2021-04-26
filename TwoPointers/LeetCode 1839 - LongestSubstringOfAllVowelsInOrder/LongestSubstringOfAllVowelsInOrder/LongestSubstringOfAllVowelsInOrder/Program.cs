c210129ce7f5d36662724cd9f153243fb0cd7d5f
Xing <x.xing.unsw@gmail.com>
2021-04-26 15:26:40 +1000

    faster

--- TwoPointers/LeetCode 1839 - LongestSubstringOfAllVowelsInOrder/LongestSubstringOfAllVowelsInOrder/LongestSubstringOfAllVowelsInOrder/Program.cs
+++ TwoPointers/LeetCode 1839 - LongestSubstringOfAllVowelsInOrder/LongestSubstringOfAllVowelsInOrder/LongestSubstringOfAllVowelsInOrder/Program.cs
@@ -14,22 +14,24 @@ namespace LongestSubstringOfAllVowelsInOrder
 		public static int LongestBeautifulSubstring(string word)
 		{
 			var dict = new Dictionary<char, int>();
-			int li = 0, hi = 0, res = 0;
+			int li = 0, hi = 0, res = 0, max = 0;
 			while (hi < word.Length)
 			{
-				if (dict.Count == 0 || word[hi - 1] >= dict.Where(x => x.Value != 0).Max(x => x.Key))
+				if (dict.Count == 0 || word[hi - 1] >= max)
 				{
 					if (dict.Count == 5)
 						res = Math.Max(res, hi - li);
 					if (!dict.ContainsKey(word[hi]))
 						dict[word[hi]] = 0;
-					dict[word[hi++]]++;
+					dict[word[hi]]++;
+					max = Math.Max(max, word[hi++]);
 				}
 				else
 				{
 					dict[word[li]]--;
 					if (dict[word[li]] == 0)
 						dict.Remove(word[li]);
+					max = dict.Max(x => x.Key);
 					li++;
 				}
 			}
