d61df3e6e41bb5645838edeaf82182703b201ae5
Xing <x.xing.unsw@gmail.com>
2021-04-27 17:59:17 +1000

    JavaSolution

--- Tree/LeetCode 938 - RangeSumOfBST/RangeSumOfBST_Java/src/com/company/Main.java
+++ Tree/LeetCode 938 - RangeSumOfBST/RangeSumOfBST_Java/src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 class TreeNode {
       int val;
       TreeNode left;
@@ -32,12 +35,27 @@ public class Main {
 	    System.out.println(rangeSumBST(a, low, high));
     }
 
-    public static int rangeSumBST(TreeNode root, int low, int high) {
+    public static int rangeSumBST_dfs(TreeNode root, int low, int high) {
         if(root == null) return 0;
         int res = 0;
         if(low <= root.val && root.val <= high) res += root.val;
-        if(root.val >= low) res += rangeSumBST(root.left, low, high);
-        if(root.val <= high) res += rangeSumBST(root.right, low, high);
+        if(root.val >= low) res += rangeSumBST_dfs(root.left, low, high);
+        if(root.val <= high) res += rangeSumBST_dfs(root.right, low, high);
+        return res;
+    }
+    public static int rangeSumBST_bfs(TreeNode root, int low, int high) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if (low <= cur.val && cur.val <= high)
+                res += cur.val;
+            if (cur.val < high && cur.right != null)
+                queue.offer(cur.right);
+            if (cur.val > low && cur.left != null)
+                queue.offer(cur.left);
+        }
         return res;
     }
 }
