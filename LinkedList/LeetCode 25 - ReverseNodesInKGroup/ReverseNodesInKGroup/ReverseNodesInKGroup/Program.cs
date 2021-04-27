e399f6c6fbef79962e8cee410eff2c21c6b4166e
Xing <x.xing.unsw@gmail.com>
2021-04-27 20:34:41 +1000

    init

---
+++ LinkedList/LeetCode 25 - ReverseNodesInKGroup/ReverseNodesInKGroup/ReverseNodesInKGroup/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ReverseNodesInKGroup
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public ListNode ReverseKGroup(ListNode head, int k)
+		{
+			int len = 0;
+			ListNode point = head, dummy = new ListNode(0, head);
+			while (point != null)
+			{
+				len++;
+				point = point.next;
+			}
+			point = dummy;
+			for (int i = 0; i < len / k; i++)
+			{
+				for (int j = 1; j < k; j++)
+				{
+					var next = head.next;
+					head.next = next.next;
+					next.next = point.next;
+					point.next = next;
+				}
+				point = head;
+				head = head.next;
+			}
+			return dummy.next;
+		}
+	}
+}
