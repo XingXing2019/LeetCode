4f372aaf1825cc2969922e19b4ce0a8cd322dc35
Xing <x.xing.unsw@gmail.com>
2021-04-27 21:12:35 +1000

    init

---
+++ Design/LeetCode 1600 - ThroneInheritance/ThroneInheritance/ThroneInheritance/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThroneInheritance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var t = new ThroneInheritance("king");
+			t.Birth("king", "andy");
+			t.Birth("king", "bob");
+			t.Birth("king", "catherine");
+			t.Birth("andy", "matthew");
+			t.Birth("bob", "alex");
+			t.Birth("bob", "asha");
+			t.GetInheritanceOrder();
+			t.Death("bob");
+			t.GetInheritanceOrder();
+		}
+	}
+	public class ThroneInheritance
+	{
+		class Person
+		{
+			public string name;
+			public Dictionary<string, Person> children;
+			public bool alive;
+			public Person(string name)
+			{
+				this.name = name;
+				children = new Dictionary<string, Person>();
+				alive = true;
+			}
+		}
+
+		private Person king;
+		private Dictionary<string, Person> dict;
+		public ThroneInheritance(string kingName)
+		{
+			king = new Person(kingName);
+			dict = new Dictionary<string, Person> {{kingName, king}};
+		}
+
+		public void Birth(string parentName, string childName)
+		{
+			var parent = dict[parentName];
+			var child = new Person(childName);
+			parent.children[childName] = child;
+			dict[childName] = child;
+		}
+
+		public void Death(string name)
+		{
+			dict[name].alive = false;
+		}
+
+		public IList<string> GetInheritanceOrder()
+		{
+			var res = new List<string>();
+			DFS(king, res);
+			return res;
+		}
+
+		private void DFS(Person king, List<string> order)
+		{
+			if (king.alive)
+				order.Add(king.name);
+			foreach (var child in king.children)
+				DFS(child.Value, order);
+		}
+	}
+}
