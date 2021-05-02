2b3697c6fc302446ed4692a692f29897e8bac54a
Xing <x.xing.unsw@gmail.com>
2021-05-02 20:16:26 +1000

    init

---
+++ String/LeetCode 1844 - ReplaceAllDigitsWithCharacters/ReplaceAllDigitsWithCharacters/ReplaceAllDigitsWithCharacters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace ReplaceAllDigitsWithCharacters
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public string ReplaceDigits(string s)
+		{
+			var str = new StringBuilder(s);
+			for (int i = 1; i < str.Length; i += 2)
+				str[i] = (char) (str[i - 1] + str[i] - '0');
+			return str.ToString();
+		}
+	}
+}
