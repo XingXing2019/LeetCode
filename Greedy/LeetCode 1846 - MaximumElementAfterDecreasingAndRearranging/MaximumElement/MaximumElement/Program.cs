8e21800fdd85472419e2f3b2a1ac58a01abb125f
Xing <x.xing.unsw@gmail.com>
2021-05-02 20:25:25 +1000

    init

---
+++ Greedy/LeetCode 1846 - MaximumElementAfterDecreasingAndRearranging/MaximumElement/MaximumElement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MaximumElement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+		{
+			Array.Sort(arr);
+			int res = 1;
+			arr[0] = 1;
+			for (int i = 1; i < arr.Length; i++)
+			{
+				arr[i] = Math.Min(arr[i - 1] + 1, arr[i]);
+				res = Math.Max(res, arr[i]);
+			}
+			return res;
+		}
+	}
+}
