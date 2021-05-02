cacd02e6a08a2648c03ee9e432900178a6cfa350
Xing <x.xing.unsw@gmail.com>
2021-05-02 20:28:29 +1000

    init

---
+++ Array/LeetCode 1848 - MinimumDistanceToTheTargetElement/MinimumDistanceToTheTargetElement/MinimumDistanceToTheTargetElement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinimumDistanceToTheTargetElement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int GetMinDistance(int[] nums, int target, int start)
+		{
+			int res = int.MaxValue;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if(nums[i] != target) continue;
+				res = Math.Min(res, Math.Abs(i - start));
+			}
+			return res;
+		}
+	}
+}
