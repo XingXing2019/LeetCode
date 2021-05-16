7e1243766c839b0aa07e07082d1b9f8743698958
Xing <x.xing.unsw@gmail.com>
2021-05-16 17:00:24 +1000

    init

---
+++ Trie/LeetCode 1858 - LongestWordWithAllPrefixes/LongestWordWithAllPrefixes/LongestWordWithAllPrefixes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace LongestWordWithAllPrefixes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] words = { "a", "e" };
+			Console.WriteLine(LongestWord(words));
+		}
+
+		class Node
+		{
+			public Node[] children;
+			public Node()
+			{
+				children = new Node[26];
+			}
+		}
+		public static string LongestWord(string[] words)
+		{
+			var root = new Node();
+			var res = "";
+			words = words.OrderBy(x => x.Length).ThenByDescending(x => x).ToArray();
+			foreach (var word in words)
+			{
+				var pointer = root;
+				var hasPrefix = true;
+				for (int i = 0; i < word.Length; i++)
+				{
+					if (i != word.Length - 1 && pointer.children[word[i] - 'a'] == null)
+					{
+						hasPrefix = false;
+						break;
+					}
+					if (i == word.Length - 1)
+						pointer.children[word[i] - 'a'] = new Node();
+					pointer = pointer.children[word[i] - 'a'];
+				}
+				if (!hasPrefix) continue;
+				if (words.Length >= res.Length) 
+					res = word;
+			}
+			return res;
+		}
+	}
+}
