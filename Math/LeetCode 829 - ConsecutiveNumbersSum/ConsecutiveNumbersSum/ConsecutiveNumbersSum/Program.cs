44a06b5fa027d1f7902bdc7dcf883d7109a0ba19
Xing <x.xing.unsw@gmail.com>
2021-05-20 21:04:04 +1000

    init

---
+++ Math/LeetCode 829 - ConsecutiveNumbersSum/ConsecutiveNumbersSum/ConsecutiveNumbersSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsecutiveNumbersSum
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 15;
+			Console.WriteLine(ConsecutiveNumbersSum(n));
+		}
+		public static int ConsecutiveNumbersSum(int n)
+		{
+			var res = 0;
+			for (int i = 1; n > (i - 1) * i / 2; i++)
+			{
+				if ((n - (i - 1) * i / 2) % i == 0)
+					res++;
+			}
+			return res;
+		}
+	}
+}
