edd8714824299aa5aa9fc7aec4397e5f57324814
Xing <x.xing.unsw@gmail.com>
2021-05-24 17:48:49 +1000

    init

---
+++ TwoPointers/LeetCode 1869 - LongerContiguousSegmentsOfOnesThanZeros/LongerContiguousSegmentsOfOnesThanZeros/LongerContiguousSegmentsOfOnesThanZeros/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LongerContiguousSegmentsOfOnesThanZeros
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public bool CheckZeroOnes(string s)
+		{
+			if (s.Length == 1) return s[0] == '1';
+			int li = 0, hi = 1;
+			var digits = new int[2];
+			digits[s[0] - '0']++;
+			while (hi < s.Length)
+			{
+				if (s[hi] != s[hi - 1])
+				{
+					digits[s[hi - 1] - '0'] = Math.Max(digits[s[hi - 1] - '0'], hi - li);
+					li = hi;
+				}
+				hi++;
+			}
+			digits[s[hi - 1] - '0'] = Math.Max(digits[s[hi - 1] - '0'], hi - li);
+			return digits[1] > digits[0];
+		}
+	}
+}
