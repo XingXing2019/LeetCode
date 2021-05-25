72e5980003f45e15a9e648254fd2b516e63c8631
Xing <x.xing.unsw@gmail.com>
2021-05-25 20:34:01 +1000

    init

---
+++ Search/LeetCode 1871 - JumpGameVII/JumpGameVII/JumpGameVII/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JumpGameVII
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public bool CanReach(string s, int minJump, int maxJump)
+		{
+			var dp = new bool[s.Length];
+			int pre = 0;
+			dp[0] = true;
+			for (int i = 1; i < dp.Length; i++)
+			{
+				if (i >= minJump && dp[i - minJump])
+					pre++;
+				if (i > maxJump && dp[i - maxJump - 1])
+					pre--;
+				if (pre > 0 && s[i] == '0')
+					dp[i] = true;
+			}
+			return dp[^1];
+		}
+	}
+}
