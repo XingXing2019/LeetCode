c0020a11f80d4c8b8946ca10354ae28dd509609b
Xing <x.xing.unsw@gmail.com>
2021-05-26 21:34:10 +1000

    One more solution

--- Stack/LeetCode 1190 - ReverseSubstringsBetweenEachPairOfParentheses/ReverseSubstringsBetweenParentheses_Java/src/com/company/Main.java
+++ Stack/LeetCode 1190 - ReverseSubstringsBetweenEachPairOfParentheses/ReverseSubstringsBetweenParentheses_Java/src/com/company/Main.java
@@ -28,4 +28,29 @@ public class Main {
             res = s.charAt(stack.pop()) + res;
         return res;
     }
+
+    public String reverseParentheses_index(String s) {
+        Stack<Integer> stack = new Stack<>();
+        char[] letters = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                stack.push(i);
+            else if (s.charAt(i) == ')')
+                reverse(letters, stack.pop(), i);
+        }
+        StringBuilder res = new StringBuilder();
+        for (Character l : letters) {
+            if (l != '(' && l != ')')
+                res.append(l);
+        }
+        return res.toString();
+    }
+
+    public void reverse(char[] letters, int li, int hi) {
+        while (li < hi) {
+            Character temp = letters[hi];
+            letters[hi--] = letters[li];
+            letters[li++] = temp;
+        }
+    }
 }
