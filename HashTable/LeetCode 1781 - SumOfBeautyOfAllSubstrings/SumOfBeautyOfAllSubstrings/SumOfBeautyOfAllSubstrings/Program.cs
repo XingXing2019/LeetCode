907ed9ef16b0e21b957228412d67436ca9d46aed
Xing <x.xing.unsw@gmail.com>
2021-05-26 22:05:05 +1000

    init

---
+++ HashTable/LeetCode 1781 - SumOfBeautyOfAllSubstrings/SumOfBeautyOfAllSubstrings/SumOfBeautyOfAllSubstrings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SumOfBeautyOfAllSubstrings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = "aabcbaa";
+			Console.WriteLine(BeautySum(s));
+		}
+		public static int BeautySum(string s)
+		{
+			var record = new int[s.Length][];
+			record[0] = new int[26];
+			record[0][s[0] - 'a']++;
+			for (int i = 1; i < s.Length; i++)
+			{
+				record[i] = new int[26];
+				for (int j = 0; j < 26; j++)
+					record[i][j] = record[i - 1][j];
+				record[i][s[i] - 'a']++;
+			}
+			int res = 0;
+			for (int i = 0; i < s.Length; i++)
+			{
+				for (int j = i + 1; j < s.Length; j++)
+				{
+					int max = 0, min = int.MaxValue;
+					var temp = i == 0 ? new int[26] : record[i - 1];
+					for (int k = 0; k < 26; k++)
+					{
+						if (record[j][k] == temp[k]) continue;
+						max = Math.Max(max, record[j][k] - temp[k]);
+						min = Math.Min(min, record[j][k] - temp[k]);
+					}
+					res += max - min;
+				}
+			}
+			return res;
+		}
+	}
+}
