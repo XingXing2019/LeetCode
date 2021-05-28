fe325bd2047f70f31ace5170553c3adc2d7abe8f
Xing <x.xing.unsw@gmail.com>
2021-05-28 18:07:23 +1000

    init

---
+++ Array/LeetCode 1333 - FilterRestaurantsByVeganFriendlyPriceAndDistance/FilterRestaurants/FilterRestaurants/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilterRestaurants
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public IList<int> FilterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance)
+		{
+			return restaurants.Where(x => x[2] >= veganFriendly && x[3] <= maxPrice && x[4] <= maxDistance)
+				.OrderByDescending(x => x[1]).ThenByDescending(x => x[0]).Select(x => x[0]).ToList();
+		}
+	}
+}
