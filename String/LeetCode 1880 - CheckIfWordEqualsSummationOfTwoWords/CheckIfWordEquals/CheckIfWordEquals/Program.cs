98e54e05bf340e3b750391d577469c34c8ac922f
Xing <x.xing.unsw@gmail.com>
2021-05-30 21:07:31 +1000

    init

---
+++ String/LeetCode 1880 - CheckIfWordEqualsSummationOfTwoWords/CheckIfWordEquals/CheckIfWordEquals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckIfWordEquals
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string firstWord = "aaa", secondWord = "a", targetWord = "aaaa";
+			Console.WriteLine(IsSumEqual(firstWord, secondWord, targetWord));
+		}
+		public static bool IsSumEqual(string firstWord, string secondWord, string targetWord)
+		{
+			int num1 = 0, num2 = 0, num3 = 0;
+			foreach (var l in firstWord)
+				num1 = num1 * 10 + (l - 'a');
+			foreach (var l in secondWord)
+				num2 = num2 * 10 + (l - 'a');
+			foreach (var l in targetWord)
+				num3 = num3 * 10 + (l - 'a');
+			return num1 + num2 == num3;
+		}
+	}
+}
