bd51a36f90616df044dea9847e7162d5d40cb384
Xing <x.xing.unsw@gmail.com>
2021-05-30 20:43:14 +1000

    init

---
+++ Greedy/LeetCode 1877 - MinimizeMaximumPairSumInArray/MinimizeMaximumPairSumInArray/MinimizeMaximumPairSumInArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MinimizeMaximumPairSumInArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MinPairSum(int[] nums)
+		{
+			Array.Sort(nums);
+			int res = 0;
+			for (int i = 0; i < nums.Length / 2; i++)
+				res = Math.Max(res, nums[i] + nums[^(i + 1)]);
+			return res;
+		}
+	}
+}
