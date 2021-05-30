ba572df845252c7c8362fce7b3d6cc2824af1d96
Xing <x.xing.unsw@gmail.com>
2021-05-30 10:24:59 +1000

    init

---
+++ String/LeetCode 1876 - SubstringsOfSizeThreeWithDistinctCharacters/SubstringsOfSizeThree/SubstringsOfSizeThree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SubstringsOfSizeThree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int CountGoodSubstrings(string s)
+		{
+			if (s.Length < 3) return 0;
+			var res = 0;
+			for (int i = 0; i <= s.Length - 3; i++)
+			{
+				if (s[i] != s[i + 1] && s[i] != s[i + 2] && s[i + 1] != s[i + 2])
+					res++;
+			}
+			return res;
+		}
+	}
+}
