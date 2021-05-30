055c6ad2605dd7efd9e68a3216afb0f41eaeda43
Xing <x.xing.unsw@gmail.com>
2021-05-30 20:36:36 +1000

    init

---
+++ Greedy/LeetCode 1874 - MinimizeProductSumOfTwoArrays/MinimizeProductSumOfTwoArrays/MinimizeProductSumOfTwoArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MinimizeProductSumOfTwoArrays
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MinProductSum(int[] nums1, int[] nums2)
+		{
+			Array.Sort(nums1);
+			Array.Sort(nums2, (a, b) => b - a);
+			var res = 0;
+			for (int i = 0; i < nums1.Length; i++)
+				res += nums1[i] * nums2[i];
+			return res;
+		}
+	}
+}
