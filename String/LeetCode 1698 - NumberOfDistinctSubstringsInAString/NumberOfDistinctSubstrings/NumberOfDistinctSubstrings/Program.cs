eb4f535b83c8106a6d9f8c0c608cb6b638e8b09c
Xing <x.xing.unsw@gmail.com>
2021-05-30 11:02:59 +1000

    init

---
+++ String/LeetCode 1698 - NumberOfDistinctSubstringsInAString/NumberOfDistinctSubstrings/NumberOfDistinctSubstrings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberOfDistinctSubstrings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		class TrieNode
+		{
+			public char letter;
+			public TrieNode[] children;
+			public TrieNode()
+			{
+				children = new TrieNode[26];
+			}
+		}
+		public int CountDistinct(string s)
+		{
+			int res = 0;
+			var root = new TrieNode();
+			for (int i = 0; i < s.Length; i++)
+			{
+				var cur = root;
+				for (int j = i; j < s.Length; j++)
+				{
+					if (cur.children[s[j] - 'a'] == null)
+					{
+						res++;
+						cur.children[s[j] - 'a'] = new TrieNode();
+					}
+					cur = cur.children[s[j] - 'a'];
+				}
+			}
+			return res;
+		}
+	}
+}
