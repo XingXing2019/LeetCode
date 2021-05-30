1f4227e70fddcfe879ecdc68e553f9584a2f30ec
Xing <x.xing.unsw@gmail.com>
2021-05-30 21:18:05 +1000

    init

---
+++ Sort/LeetCode 164 - MaximumGap/MaximumGap/MaximumGap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MaximumGap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MaximumGap(int[] nums)
+		{
+			if (nums.Length < 2) return 0;
+			Array.Sort(nums);
+			int res = 0;
+			for (int i = 1; i < nums.Length; i++)
+				res = Math.Max(res, nums[i] - nums[i - 1]);
+			return res;
+		}
+	}
+}
