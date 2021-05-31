bc2577c5bfc80cc2a7a267b258a2a4c5e35ee7d4
Xing <x.xing.unsw@gmail.com>
2021-05-31 19:33:03 +1000

    init

---
+++ Greedy/LeetCode 1881 - MaximumValueAfterInsertion/MaximumValueAfterInsertion/MaximumValueAfterInsertion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MaximumValueAfterInsertion
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string n = "-13";
+			int x = 3;
+			Console.WriteLine(MaxValue(n, x));
+		}
+		public static string MaxValue(string n, int x)
+		{
+			var isNeg = n[0] == '-';
+			int index = isNeg ? 1 : 0;
+			while (index < n.Length && (isNeg ? n[index] - '0' <= x : n[index] - '0' >= x))
+				index++;
+			return n.Substring(0, index) + x + n.Substring(index);
+		}
+	}
+}
