94992bc65eed6eef9c6485b4aa8ca6374e9470aa
Xing <x.xing.unsw@gmail.com>
2021-06-01 21:43:57 +1000

    init

---
+++ Stack/LeetCode 1541 - MinimumInsertionsToBalanceAParenthesesString/MinimumInsertions/MinimumInsertions/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MinimumInsertions
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string s = ")))()((())))()((";
+			Console.WriteLine(MinInsertions(s));
+		}
+		public static int MinInsertions(string s)
+		{
+			var stack = new Stack<char>();
+			int left = 0, right = 0, res = 0;
+			foreach (var l in s)
+			{
+				if (l == ')')
+				{
+					stack.Push(l);
+					right++;
+					if (right != 2) continue;
+					for (int i = 0; i < right; i++)
+						stack.Pop();
+					if (stack.Count != 0 && stack.Peek() == '(')
+						stack.Pop();
+					else
+						res++;
+					right = 0;
+				}
+				else
+				{
+					if (right == 1)
+					{
+						res++;
+						while (stack.Count != 0 && stack.Peek() != '(')
+							stack.Pop();
+						if (stack.Count != 0)
+							stack.Pop();
+						else
+							res++;
+						right = 0;
+					}
+					stack.Push(l);
+				}
+			}
+			left = stack.Count(x => x == '(');
+			right = stack.Count(x => x == ')');
+			if (left != 0) res += left * 2 - right;
+			else if (right != 0) res += 2;
+			return res;
+		}
+	}
+}
