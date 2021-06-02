e2ef4352f118e865cb1f7fb75f74fdfabf153a8f
Xing <x.xing.unsw@gmail.com>
2021-06-02 21:44:23 +1000

    init

---
+++ Math/LeetCode 523 - ContinuousSubarraySum/ContinuousSubarraySum/ContinuousSubarraySum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContinuousSubarraySum
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = {0, 1};
+			int k = 2;
+			Console.WriteLine(CheckSubarraySum(nums, k));
+		}
+		public static bool CheckSubarraySum(int[] nums, int k)
+		{
+			var dict = new Dictionary<int, int> {{0, -1}};
+			int prefix = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				prefix += nums[i];
+				int mod = prefix % k;
+				if (!dict.ContainsKey(mod))
+					dict[mod] = i;
+				else if (i - dict[mod] != 1)
+					return true;
+			}
+			return false;
+		}
+	}
+}
