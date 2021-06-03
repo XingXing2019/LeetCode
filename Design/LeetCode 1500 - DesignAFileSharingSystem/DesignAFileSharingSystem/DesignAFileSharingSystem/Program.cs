0b65b37e72d2c9ee05dad5c0c749fc52f8c482d3
Xing <x.xing.unsw@gmail.com>
2021-06-03 21:42:42 +1000

    init

---
+++ Design/LeetCode 1500 - DesignAFileSharingSystem/DesignAFileSharingSystem/DesignAFileSharingSystem/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignAFileSharingSystem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+	public class FileSharing
+	{
+		private int id;
+		private List<int> avaliableIds;
+		private Dictionary<int, HashSet<int>> system;
+		public FileSharing(int m)
+		{
+			id = 1;
+			avaliableIds = new List<int>();
+			system = new Dictionary<int, HashSet<int>>();
+		}
+
+		public int Join(IList<int> ownedChunks)
+		{
+			var userId = 0;
+			if (avaliableIds.Count != 0)
+			{
+				userId = avaliableIds[^1];
+				avaliableIds.RemoveAt(avaliableIds.Count - 1);
+			}
+			else
+				userId = id++;
+			system[userId] = new HashSet<int>(ownedChunks);
+			return userId;
+		}
+
+		public void Leave(int userID)
+		{
+			system.Remove(userID);
+			avaliableIds.Add(userID);
+			avaliableIds.Sort((a, b) => b - a);
+		}
+
+		public IList<int> Request(int userID, int chunkID)
+		{
+			var res = new List<int>();
+			foreach (var item in system)
+			{
+				if(item.Value.Contains(chunkID))
+					res.Add(item.Key);
+			}
+			if (res.Count == 0)
+				return res;
+			system[userID].Add(chunkID);
+			res.Sort();
+			return res;
+		}
+	}
+}
