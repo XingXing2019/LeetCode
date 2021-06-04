2201780c55097a4a256b8d1133ed6d432eb51068
Xing <x.xing.unsw@gmail.com>
2021-06-04 21:42:48 +1000

    init

---
+++ Stack/LeetCode 439 - TernaryExpressionParser/TernaryExpressionParser/TernaryExpressionParser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TernaryExpressionParser
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var expression = "F?1:T?4:5";
+			Console.WriteLine(ParseTernary(expression));
+		}
+		public static string ParseTernary(string expression)
+		{
+			var count = 0;
+			for (int i = 0; i < expression.Length; i++)
+			{
+				if (expression[i] == '?') count++;
+				else if (expression[i] == ':') count--;
+				if (count == 0 && expression[i] == ':')
+					return expression[0] == 'T' ? ParseTernary(expression.Substring(2, i - 2)) : ParseTernary(expression.Substring(i + 1));
+			}
+			return expression;
+		}
+	}
+}
