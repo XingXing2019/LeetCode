d59cbbad4f2e3ca44f482ea70ec49ed625e42936
Xing <x.xing.unsw@gmail.com>
2021-06-04 20:29:24 +1000

    init

---
+++ Stack/LeetCode 331 - VerifyPreorderSerializationOfABinaryTree/VerifyPreorderSerializationOfABinaryTree/VerifyPreorderSerializationOfABinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VerifyPreorderSerializationOfABinaryTree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public bool IsValidSerialization(string preorder)
+		{
+			var nodes = preorder.Split(",");
+			int slot = 1;
+			for (int i = 0; i < nodes.Length; i++)
+			{
+				slot += nodes[i] == "#" ? -1 : 1;
+				if (slot == 0 && i != nodes.Length - 1)
+					return false;
+			}
+			return slot == 0;
+		}
+	}
+}
