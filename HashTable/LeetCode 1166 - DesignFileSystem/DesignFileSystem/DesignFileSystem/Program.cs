22e24de6bfd3ca289f2e3d72efed2791383ef559
Xing <x.xing.unsw@gmail.com>
2021-06-05 16:10:23 +1000

    init

---
+++ HashTable/LeetCode 1166 - DesignFileSystem/DesignFileSystem/DesignFileSystem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignFileSystem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var fileSystem = new FileSystem();
+			Console.WriteLine(fileSystem.CreatePath("/a", 1));
+			Console.WriteLine(fileSystem.Get("/a"));
+		}
+	}
+	public class FileSystem
+	{
+		private Dictionary<string, int> dict;
+		public FileSystem()
+		{
+			dict = new Dictionary<string, int>();
+		}
+
+		public bool CreatePath(string path, int value)
+		{
+			if (dict.ContainsKey(path)) return false;
+			var parts = path.Split("/", StringSplitOptions.RemoveEmptyEntries);
+			var dir = "";
+			for (int i = 0; i < parts.Length - 1; i++)
+			{
+				dir += $"/{parts[i]}";
+				if (!dict.ContainsKey(dir))
+					return false;
+			}
+			dict[path] = value;
+			return true;
+		}
+
+		public int Get(string path)
+		{
+			return dict.ContainsKey(path) ? dict[path] : -1;
+		}
+	}
+}
