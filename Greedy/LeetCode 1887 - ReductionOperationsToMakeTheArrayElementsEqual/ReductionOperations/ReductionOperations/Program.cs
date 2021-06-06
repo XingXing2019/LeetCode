8ec8a3abbe5500b483ccde0aba78bef693ed7c33
Xing <x.xing.unsw@gmail.com>
2021-06-06 16:12:43 +1000

    init

---
+++ Greedy/LeetCode 1887 - ReductionOperationsToMakeTheArrayElementsEqual/ReductionOperations/ReductionOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ReductionOperations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int ReductionOperations(int[] nums)
+		{
+			var dict = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+			var record = dict.Select(x => x.Key).OrderBy(x => x).ToArray();
+			for (int i = 0; i < record.Length; i++)
+				dict[record[i]] *= i;
+			return dict.Sum(x => x.Value);
+		}
+	}
+}
