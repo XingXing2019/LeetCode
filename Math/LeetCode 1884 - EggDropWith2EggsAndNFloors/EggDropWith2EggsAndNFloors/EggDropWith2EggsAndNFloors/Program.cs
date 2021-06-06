1c5a7cea1462f30c32ee47e6cd9bfaf3487f1113
Xing <x.xing.unsw@gmail.com>
2021-06-06 11:56:40 +1000

    init

---
+++ Math/LeetCode 1884 - EggDropWith2EggsAndNFloors/EggDropWith2EggsAndNFloors/EggDropWith2EggsAndNFloors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EggDropWith2EggsAndNFloors
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int TwoEggDrop(int n)
+		{
+			return (int) Math.Ceiling((Math.Sqrt(1 + 8 * n) - 1) / 2);
+		}
+	}
+}
