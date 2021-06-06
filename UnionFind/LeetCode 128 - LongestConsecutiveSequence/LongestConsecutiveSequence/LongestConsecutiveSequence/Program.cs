752af5cc84e35428ee59f4c05e18bbb7e61db704
Xing <x.xing.unsw@gmail.com>
2021-06-06 20:40:19 +1000

    one more solution

--- UnionFind/LeetCode 128 - LongestConsecutiveSequence/LongestConsecutiveSequence/LongestConsecutiveSequence/Program.cs
+++ UnionFind/LeetCode 128 - LongestConsecutiveSequence/LongestConsecutiveSequence/LongestConsecutiveSequence/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LongestConsecutiveSequence
 {
@@ -12,7 +13,7 @@ namespace LongestConsecutiveSequence
 
 		private Dictionary<int, int> parents = new Dictionary<int, int>();
 		private Dictionary<int, int> rank = new Dictionary<int, int>();
-		public int LongestConsecutive(int[] nums)
+		public int LongestConsecutive_UnionFind(int[] nums)
 		{
 			foreach (var num in nums)
 			{
@@ -61,6 +62,43 @@ namespace LongestConsecutiveSequence
 			else
 				parents[rootX] = rootY;
 			return true;
+		}
+
+		public int LongestConsecutive_Set(int[] nums)
+		{
+			if (nums.Length == 0) return 0;
+			var set = new HashSet<int>(nums);
+			int res = 1;
+			foreach (var num in set)
+			{
+				if (!set.Contains(num - 1))
+				{
+					var cur = num;
+					while (set.Contains(cur))
+						cur++;
+					res = Math.Max(res, cur - num);
+				}
+			}
+			return res;
+		}
+
+		public int LongestConsecutive_Sort(int[] nums)
+		{
+			if (nums.Length == 0) return 0;
+			var set = new HashSet<int>(nums);
+			var sorted = set.OrderBy(x => x).ToArray();
+			int li = 0, hi = 1, res = 0;
+			while (hi < sorted.Length)
+			{
+				if (sorted[hi] != sorted[hi - 1] + 1)
+				{
+					res = Math.Max(res, hi - li);
+					li = hi;
+				}
+				hi++;
+			}
+			res = Math.Max(res, hi - li);
+			return res;
 		}
 	}
 }
