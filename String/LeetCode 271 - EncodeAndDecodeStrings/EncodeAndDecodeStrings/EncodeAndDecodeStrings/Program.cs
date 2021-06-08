39a56461066fc911381b26cf1fbc5334f6f77750
Xing <x.xing.unsw@gmail.com>
2021-06-08 20:02:36 +1000

    init

---
+++ String/LeetCode 271 - EncodeAndDecodeStrings/EncodeAndDecodeStrings/EncodeAndDecodeStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EncodeAndDecodeStrings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var d = '\u0000';
+			Console.WriteLine(d);
+		}
+	}
+
+	public class Codec
+	{
+		private char delimiter = '\u0000';
+		public string Encode(IList<string> strs)
+		{
+			return string.Join(delimiter, strs);
+		}
+
+		// Decodes a single string to a list of strings.
+		public IList<string> Decode(string s)
+		{
+			return s.Split(delimiter);
+		}
+	}
+}
