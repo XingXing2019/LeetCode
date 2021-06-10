88562e9a8c49fbe8707e019bcadce8e6db68d85b
Xing <x.xing.unsw@gmail.com>
2021-06-10 19:15:26 +1000

    init

---
+++ BinarySearch/LeetCode 1891 - CuttingRibbons/CuttingRibbons/CuttingRibbons/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace CuttingRibbons
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MaxLength(int[] ribbons, int k)
+		{
+			int li = 1, hi = ribbons.Max();
+			while (li <= hi)
+			{
+				int mid = li + (hi - li) / 2;
+				int segments = ribbons.Sum(x => x / mid);
+				if (segments < k)
+					hi = mid - 1;
+				else
+					li = mid + 1;
+			}
+			return hi;
+		}
+	}
+}
