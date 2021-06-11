57c9740ec7c7865d92592abd2970aba68d178fd9
Xing <x.xing.unsw@gmail.com>
2021-06-11 18:20:46 +1000

    JavaSolution

--- DynamicProgramming/LeetCode 279 - PerfectSquares/PerfectSquares_Java/src/com/company/Main.java
+++ DynamicProgramming/LeetCode 279 - PerfectSquares/PerfectSquares_Java/src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int n = 12;
-        System.out.println(numSquares(n));
+        int n = 2;
+        System.out.println(numSquares_dp(n));
     }
 
     public static int numSquares(int n) {
@@ -19,6 +22,18 @@ public class Main {
                     min = Math.min(min, dp[j] + dp[i - j]);
                 dp[i] = min;
             }
+        }
+        return dp[dp.length - 1];
+    }
+
+    public static int numSquares_dp(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        for (int i = 1; i * i <= n; i++)
+            dp[i * i] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; i + j * j <= n; j++)
+                dp[i + j * j] = Math.min(dp[i] + 1, dp[i + j * j]);
         }
         return dp[dp.length - 1];
     }
