851283f8091a204a8103d2621d7d2a4a2a5f5eed
Xing <x.xing.unsw@gmail.com>
2021-06-13 15:20:40 +1000

    init

---
+++ Greedy/LeetCode 1893 - CheckIfAllTheIntegersInARangeAreCovered/CheckIfAllTheIntegersInARangeAreCovered/CheckIfAllTheIntegersInARangeAreCovered/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckIfAllTheIntegersInARangeAreCovered
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public bool IsCovered(int[][] ranges, int left, int right)
+		{
+			var set = new HashSet<int>();
+			for (int i = left; i <= right; i++)
+				set.Add(i);
+			foreach (var range in ranges)
+			{
+				for (int i = range[0]; i <= range[1]; i++)
+					set.Remove(i);
+			}
+			return set.Count == 0;
+		}
+	}
+}
