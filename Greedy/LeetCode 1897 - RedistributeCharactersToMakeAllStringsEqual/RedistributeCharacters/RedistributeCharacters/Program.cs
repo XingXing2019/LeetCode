875e4775de00d4efd4ec1f8a0967efff273ae840
Xing <x.xing.unsw@gmail.com>
2021-06-13 15:25:19 +1000

    init

---
+++ Greedy/LeetCode 1897 - RedistributeCharactersToMakeAllStringsEqual/RedistributeCharacters/RedistributeCharacters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace RedistributeCharacters
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public bool MakeEqual(string[] words)
+		{
+			var letters = new int[26];
+			foreach (var word in words)
+			{
+				foreach (var letter in word)
+					letters[letter - 'a']++;
+			}
+			return letters.All(x => x % words.Length == 0);
+		}
+	}
+}
