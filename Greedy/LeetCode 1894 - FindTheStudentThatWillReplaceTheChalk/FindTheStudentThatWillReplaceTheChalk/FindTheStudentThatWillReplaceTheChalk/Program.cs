86c5b5ad2e8e0db1d53822515b9789246c0293de
Xing <x.xing.unsw@gmail.com>
2021-06-13 15:38:36 +1000

    init

---
+++ Greedy/LeetCode 1894 - FindTheStudentThatWillReplaceTheChalk/FindTheStudentThatWillReplaceTheChalk/FindTheStudentThatWillReplaceTheChalk/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace FindTheStudentThatWillReplaceTheChalk
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] chalk = {3, 4, 1, 2, 1, 2, 3, 3};
+			int k = 41;
+			Console.WriteLine(chalkReplacer(chalk, k));
+		}
+		public static int chalkReplacer(int[] chalk, int k)
+		{
+			long sum = 0;
+			foreach (var c in chalk)
+				sum += c;
+			var left = k % sum;
+			int index = 0;
+			while (index < chalk.Length && left - chalk[index] >= 0)
+				left -= chalk[index++];
+			return index;
+		}
+	}
+}
