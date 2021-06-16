d209d38b996c295680692119927b22160104bbb5
Xing <x.xing.unsw@gmail.com>
2021-06-16 17:41:28 +1000

    JavaSolution

---
+++ DynamicProgramming/LeetCode 877 - StoneGame/StoneGame_Java/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
