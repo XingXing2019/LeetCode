e59f6855a6ea398909430a9cfae4d37ba31bbd58
Xing <x.xing.unsw@gmail.com>
2021-06-19 21:45:58 +1000

    Binary Search Solution

--- BinarySearch/LeetCode 1901 - FindAPeakElementII/FindAPeakElementII/FindAPeakElementII/Program.cs
+++ BinarySearch/LeetCode 1901 - FindAPeakElementII/FindAPeakElementII/FindAPeakElementII/Program.cs
@@ -29,5 +29,26 @@ namespace FindAPeakElementII
 			}
 			return null;
 		}
+
+		public int[] FindPeakGrid_BinarySearch(int[][] mat)
+		{
+			int li = 0, hi = mat.Length - 1;
+			while (li <= hi)
+			{
+				int mid = li + (hi - li) / 2;
+				int index = 0;
+				for (int i = 0; i < mat[0].Length; i++)
+				{
+					if (mat[mid][i] > mat[mid][index])
+						index = i;
+				}
+				int up = mid == 0 ? -1 : mat[mid - 1][index];
+				int down = mid == mat.Length - 1 ? -1 : mat[mid + 1][index];
+				if (mat[mid][index] > up && mat[mid][index] > down) return new[] {mid, index};
+				if (mat[mid][index] < up) hi = mid - 1;
+				else li = mid + 1;
+			}
+			return null;
+		}
 	}
 }
