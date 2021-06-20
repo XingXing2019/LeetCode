88e9662f13dadfef2a51333aeee4179ca88e44c9
Xing <x.xing.unsw@gmail.com>
2021-06-20 16:50:28 +1000

    init

---
+++ Greedy/LeetCode 1903 - LargestOddNumberInString/LargestOddNumberInString/LargestOddNumberInString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LargestOddNumberInString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public string LargestOddNumber(string num)
+		{
+			for (int i = num.Length - 1; i >= 0; i--)
+			{
+				if ((num[i] - '0') % 2 != 0)
+					return num.Substring(0, i + 1);
+			}
+			return "";
+		}
+	}
+}
