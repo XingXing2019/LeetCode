dd12e6f23e2c249c32a7822fd93d87f7af62b361
Xing <x.xing.unsw@gmail.com>
2021-06-20 17:08:59 +1000

    init

---
+++ Greedy/LeetCode 1904 - TheNumberOfFullRoundsYouHavePlayed/TheNumberOfFullRoundsYouHavePlayed/TheNumberOfFullRoundsYouHavePlayed/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TheNumberOfFullRoundsYouHavePlayed
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int NumberOfRounds(string startTime, string finishTime)
+		{
+			var startHr = int.Parse(startTime.Substring(0, 2));
+			var finishHr = int.Parse(finishTime.Substring(0, 2));
+			var startMin = int.Parse(startTime.Substring(3));
+			var finishMin = int.Parse(finishTime.Substring(3));
+			if (startHr > finishHr || startHr == finishHr && startMin > finishMin) 
+				finishHr += 24;
+			return (finishHr - startHr - 1) * 4 + (60 - startMin) / 15 + finishMin / 15;
+		}
+	}
+}
