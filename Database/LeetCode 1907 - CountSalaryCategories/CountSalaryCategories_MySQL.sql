f1cadf881d59b29e0b56ad0db4c99e902690822f
Xing <x.xing.unsw@gmail.com>
2021-06-23 18:02:49 +1000

    MySQL

---
+++ Database/LeetCode 1907 - CountSalaryCategories/CountSalaryCategories_MySQL.sql
@@ -0,0 +1,5 @@
+select 'Low Salary' as category, count(*) as accounts_count from Accounts where income < 20000
+union all
+select 'Average Salary' as category, count(*) as accounts_count from Accounts where income >= 20000 and income <= 50000
+union all
+select 'High Salary' as category, count(*) as accounts_count from Accounts where income > 50000
