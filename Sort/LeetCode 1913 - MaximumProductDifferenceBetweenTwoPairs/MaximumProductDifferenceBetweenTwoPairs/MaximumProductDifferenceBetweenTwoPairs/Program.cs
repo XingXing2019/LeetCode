cec6c3ff56e1c6a68afde9486610745163ed7ac7
Xing <x.xing.unsw@gmail.com>
2021-06-27 14:46:23 +1000

    init

---
+++ Sort/LeetCode 1913 - MaximumProductDifferenceBetweenTwoPairs/MaximumProductDifferenceBetweenTwoPairs/MaximumProductDifferenceBetweenTwoPairs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MaximumProductDifferenceBetweenTwoPairs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MaxProductDifference(int[] nums)
+		{
+			Array.Sort(nums);
+			return nums[^1] * nums[^2] - nums[0] * nums[1];
+		}
+	}
+}
