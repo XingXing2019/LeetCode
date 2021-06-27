9526847453c99f1a14df5ff12099adc05f5401e2
Xing <x.xing.unsw@gmail.com>
2021-06-27 11:44:06 +1000

    init

---
+++ String/LeetCode 1910 - RemoveAllOccurrencesOfASubstring/RemoveAllOccurrencesOfASubstring/RemoveAllOccurrencesOfASubstring/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace RemoveAllOccurrencesOfASubstring
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public string RemoveOccurrences(string s, string part)
+		{
+			var str = new StringBuilder();
+			foreach (var l in s)
+			{
+				str.Append(l);
+				if (str.Length < part.Length) continue;
+				if (str.ToString(str.Length - part.Length, part.Length) == part)
+					str.Remove(str.Length - part.Length, part.Length);
+			}
+			return str.ToString();
+		}
+	}
+}
