72f9820d5a1bbe5d3fc9e6bdf62069153e104bff
Xing <x.xing.unsw@gmail.com>
2021-06-27 11:34:25 +1000

    init

---
+++ Array/LeetCode 1909 - RemoveOneElementToMakeTheArrayStrictlyIncreasing/RemoveOneElement/RemoveOneElement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveOneElement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = { 2, 3, 1, 2 };
+			Console.WriteLine(CanBeIncreasing(nums));
+		}
+		public static bool CanBeIncreasing(int[] nums)
+		{
+			var stack = new Stack<int>();
+			int count = 0;
+			foreach (var num in nums)
+			{
+				if (stack.Count > 0 && num <= stack.Peek())
+				{
+					if (count == 1) return false;
+					count++;
+					int peek = stack.Pop();
+					if (stack.Count > 0)
+						stack.Push(num > stack.Peek() ? num : peek);
+					else
+						stack.Push(num);
+				}
+				else
+					stack.Push(num);
+			}
+			return true;
+		}
+	}
+}
