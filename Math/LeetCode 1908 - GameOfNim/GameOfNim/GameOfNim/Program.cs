9e17efc1f505917c3157ac268b7a2e5d62d111fb
Xing <x.xing.unsw@gmail.com>
2021-06-27 14:42:03 +1000

    init

---
+++ Math/LeetCode 1908 - GameOfNim/GameOfNim/GameOfNim/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GameOfNim
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public bool NimGame(int[] piles)
+		{
+			int xOr = 0;
+			foreach (var pile in piles)
+				xOr ^= pile;
+			return xOr != 0;
+		}
+	}
+}
