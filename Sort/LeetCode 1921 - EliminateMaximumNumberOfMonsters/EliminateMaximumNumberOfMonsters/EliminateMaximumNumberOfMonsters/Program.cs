bdeb3188a19b7fa8fc131f8a3463a92e39cc3a76
Xing <x.xing.unsw@gmail.com>
2021-07-04 14:37:20 +1000

    init

---
+++ Sort/LeetCode 1921 - EliminateMaximumNumberOfMonsters/EliminateMaximumNumberOfMonsters/EliminateMaximumNumberOfMonsters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EliminateMaximumNumberOfMonsters
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int EliminateMaximum(int[] dist, int[] speed)
+		{
+			var reachIn = new double[dist.Length];
+			for (int i = 0; i < reachIn.Length; i++)
+				reachIn[i] = (double) dist[i] / speed[i];
+			Array.Sort(reachIn);
+			int timer = 0, index = 0;
+			while (index < reachIn.Length && reachIn[index] > timer)
+			{
+				index++;
+				timer++;
+			}
+			return index;
+		}
+	}
+}
