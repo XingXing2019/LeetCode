0a2685cf6606c25c2ad973afbcdab69ed34dd4d8
Xing <x.xing.unsw@gmail.com>
2021-07-04 14:25:40 +1000

    init

---
+++ Array/LeetCode 1920 - BuildArrayFromPermutation/BuildArrayFromPermutation/BuildArrayFromPermutation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BuildArrayFromPermutation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int[] BuildArray(int[] nums)
+		{
+			var res = new int[nums.Length];
+			for (int i = 0; i < nums.Length; i++)
+				res[i] = nums[nums[i]];
+			return res;
+		}
+	}
+}
