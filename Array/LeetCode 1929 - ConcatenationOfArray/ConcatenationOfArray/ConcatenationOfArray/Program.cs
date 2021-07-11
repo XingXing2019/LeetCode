a23d3c1dfef0c4c465cb1dbfd9e8c1fc9d299762
Xing <x.xing.unsw@gmail.com>
2021-07-11 15:01:25 +1000

    init

---
+++ Array/LeetCode 1929 - ConcatenationOfArray/ConcatenationOfArray/ConcatenationOfArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConcatenationOfArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int[] GetConcatenation(int[] nums)
+		{
+			var res = new int[nums.Length * 2];
+			for (int i = 0; i < res.Length; i++)
+				res[i] = nums[i % nums.Length];
+			return res;
+		}
+	}
+}
---
+++ Greedy/LeetCode 1925 - CountSquareSumTriples/CountSquareSumTriples/CountSquareSumTriples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CountSquareSumTriples
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 5;
+			Console.WriteLine(CountTriples(n));
+		}
+
+		public static int CountTriples(int n)
+		{
+			int res = 0;
+			for (int a = 1; a <= n; a++)
+			{
+				for (int b = 1; b <= Math.Sqrt(n * n - a * a) ; b++)
+				{
+					var c = Math.Sqrt(a * a + b * b);
+					if (c <= n && c == (int) c)
+						res++;
+				}
+			}
+			return res;
+		}
+	}
+}
