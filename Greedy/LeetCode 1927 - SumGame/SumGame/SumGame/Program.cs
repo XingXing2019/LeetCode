b6e86774001d76ca3ff59894908667b9866476b6
Xing <x.xing.unsw@gmail.com>
2021-07-11 15:51:39 +1000

    init

--- Greedy/LeetCode 1927 - SumGame/SumGame/SumGame/Program.cs
+++ Greedy/LeetCode 1927 - SumGame/SumGame/SumGame/Program.cs
@@ -12,24 +12,21 @@ namespace SumGame
 
 		public static bool SumGame(string num)
 		{
-			int frontMark = 0, backMark = 0;
-			int frontSum = 0, backSum = 0;
+			int sumDiff = 0, markDiff = 0;
 			for (int i = 0; i < num.Length; i++)
 			{
 				if (i < num.Length / 2)
 				{
-					if (num[i] == '?') frontMark++;
-					else frontSum += num[i] - '0';
+					if (num[i] == '?') markDiff++;
+					else sumDiff += num[i] - '0';
 				}
 				else
 				{
-					if (num[i] == '?') backMark++;
-					else backSum += num[i] - '0';
+					if (num[i] == '?') markDiff--;
+					else sumDiff -= num[i] - '0';
 				}
 			}
-			int sumDiff = frontSum - backSum, markDiff = frontMark - backMark;
 			if (sumDiff == 0 && markDiff == 0) return false;
-			if (sumDiff * markDiff >= 0) return true;
 			int bob = -markDiff / 2, alice = -markDiff - bob;
 			if (bob == 0) return true;
 			int left = sumDiff - alice * 9;
