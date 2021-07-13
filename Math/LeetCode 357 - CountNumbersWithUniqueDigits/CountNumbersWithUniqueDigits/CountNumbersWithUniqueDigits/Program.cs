2f239a69546f4d00e11f615c338136c1fadcd0b0
Xing <x.xing.unsw@gmail.com>
2021-07-13 20:35:41 +1000

    init

---
+++ Math/LeetCode 357 - CountNumbersWithUniqueDigits/CountNumbersWithUniqueDigits/CountNumbersWithUniqueDigits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountNumbersWithUniqueDigits
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int CountNumbersWithUniqueDigits(int n)
+		{
+			if (n == 0) return 1;
+			int res = 10, start = 9, temp = 9;
+			for (int i = 2; i <= n; i++)
+			{
+				temp *= start;
+				res += temp;
+				start--;
+			}
+			return res;
+		}
+	}
+}
