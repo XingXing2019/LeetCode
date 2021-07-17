242cf10344a4e6b1eecf55bac8c0bb82fef4d618
Xing <x.xing.unsw@gmail.com>
2021-07-17 16:27:04 +1000

    PythonSolution

---
+++ LinkedList/LeetCode 21 - ZipTwoLists_Python/main.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        point = dummy
+        while l1 is not None and l2 is not None:
+            if l1.val < l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l2.next
+            point = point.next
+        if l1 is not None:
+            point.next = l1
+        if l2 is not None:
+            point.next = l2
+        return dummy.next
