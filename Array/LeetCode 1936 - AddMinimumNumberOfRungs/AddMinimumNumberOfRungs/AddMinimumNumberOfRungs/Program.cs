45119307820527d0ad35bddcc22c5ee462af3110
Xing <x.xing.unsw@gmail.com>
2021-07-18 14:37:02 +1000

    init

---
+++ Array/LeetCode 1936 - AddMinimumNumberOfRungs/AddMinimumNumberOfRungs/AddMinimumNumberOfRungs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AddMinimumNumberOfRungs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int AddRungs(int[] rungs, int dist)
+		{
+			int li = 0, hi = 100000;
+			while (li <= hi)
+			{
+				int mid = li + (hi - li) / 2;
+
+			}
+		}
+	}
+}
