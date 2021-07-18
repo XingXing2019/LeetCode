fc1c8530355c430631c529292e270d796cc9f6d2
Xing <x.xing.unsw@gmail.com>
2021-07-18 14:29:34 +1000

    init

---
+++ String/LeetCode 1935 - MaximumNumberOfWordsYouCanType/MaximumNumberOfWordsYouCanType/MaximumNumberOfWordsYouCanType/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumNumberOfWordsYouCanType
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int CanBeTypedWords(string text, string brokenLetters)
+		{
+			return text.Split(' ').Count(x => !x.Any(y => brokenLetters.Contains(y)));
+		}
+	}
+}
