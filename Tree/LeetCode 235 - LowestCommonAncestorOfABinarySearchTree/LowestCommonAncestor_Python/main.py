499c33e04fd7743ada51e74e83fbfff0204e6e4d
Xing <x.xing.unsw@gmail.com>
2021-07-19 18:10:08 +1000

    PythonSolution

---
+++ Tree/LeetCode 235 - LowestCommonAncestorOfABinarySearchTree/LowestCommonAncestor_Python/main.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+        if p.val <= root.val <= q.val or q.val <= root.val <= p.val:
+            return root
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return self.lowestCommonAncestor(root.right, p, q)
