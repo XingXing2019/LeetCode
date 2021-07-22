f74705a2b8f3fb493bb91f1861dac429c9a2dc95
Xing <x.xing.unsw@gmail.com>
2021-07-22 18:20:39 +1000

    init

---
+++ Array/LeetCode 915 - PartitionArrayIntoDisjointIntervals/PartitionArrayIntoDisjointIntervals/PartitionArrayIntoDisjointIntervals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PartitionArrayIntoDisjointIntervals
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = {5, 0, 3, 8, 6};
+			Console.WriteLine(PartitionDisjoint(nums));
+		}
+
+		public static int PartitionDisjoint(int[] nums)
+		{
+			int[] rightMin = new int[nums.Length], leftMax = new int[nums.Length];
+			int min = nums[^1], max = nums[0], index = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				leftMax[i] = max;
+				max = Math.Max(max, nums[i]);
+				rightMin[^(i + 1)] = min;
+				min = Math.Min(min, nums[^(i + 1)]);
+			}
+			while (index < nums.Length && leftMax[index] > rightMin[index])
+				index++;
+			return index + 1;
+		}
+	}
+}
