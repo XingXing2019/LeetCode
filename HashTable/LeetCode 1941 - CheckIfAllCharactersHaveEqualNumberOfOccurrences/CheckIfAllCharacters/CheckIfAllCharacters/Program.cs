72b411794f8bf835cde2a00c61a296004a91f3a0
Xing <x.xing.unsw@gmail.com>
2021-07-25 15:05:00 +1000

    init

---
+++ HashTable/LeetCode 1941 - CheckIfAllCharactersHaveEqualNumberOfOccurrences/CheckIfAllCharacters/CheckIfAllCharacters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace CheckIfAllCharacters
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public bool AreOccurrencesEqual(string s)
+		{
+			var dict = s.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+			int count = dict.Max(x => x.Value);
+			return dict.All(x => x.Value == count);
+		}
+	}
+}
