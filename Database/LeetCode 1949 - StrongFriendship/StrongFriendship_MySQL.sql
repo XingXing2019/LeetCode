f7ebdf82f2a1fb12c82f932c9dfb8455198dfb89
Xing <x.xing.unsw@gmail.com>
2021-07-28 18:50:03 +1000

    MySQL

--- Database/LeetCode 1949 - StrongFriendship/StrongFriendship_MySQL.sql
+++ Database/LeetCode 1949 - StrongFriendship/StrongFriendship_MySQL.sql
@@ -5,9 +5,9 @@ with friends as (
 ),
 common_friends as (
 	select f1.user1_id, f2.user2_id as user1_friend, f1.user2_id, f3.user2_id as user2_friend
-    from friends f1 
-    join friends f2 on f1.user1_id = f2.user1_id
-    join friends f3 on f1.user2_id = f3.user1_id
+	from friends f1 
+	join friends f2 on f1.user1_id = f2.user1_id
+	join friends f3 on f1.user2_id = f3.user1_id
 )
 select user1_id, user2_id, count(*) as common_friend 
 from common_friends
