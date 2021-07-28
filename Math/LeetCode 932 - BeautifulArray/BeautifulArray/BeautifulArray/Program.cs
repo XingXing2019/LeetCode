f67484d39b3315902b3fbd799c574885c2fb7ddd
Xing <x.xing.unsw@gmail.com>
2021-07-28 20:17:23 +1000

    init

---
+++ Math/LeetCode 932 - BeautifulArray/BeautifulArray/BeautifulArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeautifulArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 5;
+			Console.WriteLine(BeautifulArray(n));
+		}
+		public static int[] BeautifulArray(int n)
+		{
+			var res = new List<int> {1};
+			while (res.Count < n)
+			{
+				var temp = new List<int>();
+				foreach (var num in res)
+				{
+					if(num * 2 - 1 <= n)
+						temp.Add(num * 2 - 1);
+				}
+				foreach (var num in res)
+				{
+					if(num * 2 <= n)
+						temp.Add(num * 2);
+				}
+				res = temp;
+			}
+			return res.ToArray();
+		}
+	}
+}
