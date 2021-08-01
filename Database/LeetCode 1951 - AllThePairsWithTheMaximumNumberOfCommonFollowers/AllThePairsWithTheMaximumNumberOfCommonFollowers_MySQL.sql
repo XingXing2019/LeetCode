f4a59a5186a0132418c9bd37822d2af061a1d88d
Xing <x.xing.unsw@gmail.com>
2021-08-01 16:28:57 +1000

    MySQL

--- Database/LeetCode 1951 - AllThePairsWithTheMaximumNumberOfCommonFollowers/AllThePairsWithTheMaximumNumberOfCommonFollowers_MySQL.sql
+++ Database/LeetCode 1951 - AllThePairsWithTheMaximumNumberOfCommonFollowers/AllThePairsWithTheMaximumNumberOfCommonFollowers_MySQL.sql
@@ -1,7 +1,7 @@
 with common as (
 	select r1.user_id as user1_id, r2.user_id as user2_id, count(distinct r1.follower_id) as count
-    from relations r1 cross join relations r2
-    where r1.user_id < r2.user_id and r1.follower_id = r2.follower_id
+	from relations r1 cross join relations r2
+	where r1.user_id < r2.user_id and r1.follower_id = r2.follower_id
 	group by r1.user_id, r2.user_id
 )
 
