2dfaad0f0e9a09346425accb1018548740e85df4
Xing <x.xing.unsw@gmail.com>
2021-08-01 15:33:12 +1000

    init

---
+++ Math/LeetCode 1954 - MinimumGardenPerimeterToCollectEnoughApples/MinimumGardenPerimeter/MinimumGardenPerimeter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MinimumGardenPerimeter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public long MinimumPerimeter(long neededApples)
+		{
+			long x = 1;
+			while (2 * x * (x - 1) * (2 * x - 1) < neededApples)
+				x++;
+			return (x - 1) * 8;
+		}
+	}
+}
