19537eec533fbf86727b064c2dbb5c6b31311ba2
Xing <x.xing.unsw@gmail.com>
2021-08-01 16:03:17 +1000

    init

---
+++ Math/LeetCode 1953 - MaximumNumberOfWeeksForWhichYouCanWork/MaximumNumberOfWeeks/MaximumNumberOfWeeks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumNumberOfWeeks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public long NumberOfWeeks(int[] milestones)
+		{
+			int max = milestones.Max();
+			long rest = 0, count = 0;
+			foreach (var milestone in milestones)
+			{
+				if (milestone != max)
+					rest += milestone;
+				else
+					count++;
+			}
+			return count > 1 ? rest + max * count : rest + Math.Min(max, rest + 1);
+		}
+	}
+}
