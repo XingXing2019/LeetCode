17b574c62086fc4c357aa3a24f8ffd6f88279820
Xing <x.xing.unsw@gmail.com>
2021-08-01 14:49:17 +1000

    init

---
+++ Math/LeetCode 1952 - ThreeDivisors/ThreeDivisors/ThreeDivisors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ThreeDivisors
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 79;
+			Console.WriteLine(IsThree(n));
+		}
+		public static bool IsThree(int n)
+		{
+			if (n == 1 || (int)Math.Sqrt(n) * (int)Math.Sqrt(n) != n)
+				return false;
+			for (int i = 2; i < Math.Sqrt(n); i++)
+			{
+				if (n % i == 0)
+					return false;
+			}
+			return true;
+		}
+	}
+}
