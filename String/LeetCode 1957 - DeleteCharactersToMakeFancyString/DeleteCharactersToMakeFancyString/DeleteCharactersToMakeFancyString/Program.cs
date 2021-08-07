9887c65547c9f28298150619413bf552dd1b30da
Xing <x.xing.unsw@gmail.com>
2021-08-08 09:00:43 +1000

    init

---
+++ String/LeetCode 1957 - DeleteCharactersToMakeFancyString/DeleteCharactersToMakeFancyString/DeleteCharactersToMakeFancyString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace DeleteCharactersToMakeFancyString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public string MakeFancyString(string s)
+		{
+			var res = new StringBuilder();
+			int count = 0;
+			char character = s[0];
+			foreach (var c in s)
+			{
+				if (c == character)
+					count++;
+				else
+				{
+					character = c;
+					count = 1;
+				}
+				if (count < 3)
+					res.Append(c);
+			}
+			return res.ToString();
+		}
+	}
+}
