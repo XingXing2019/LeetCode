02631c44cbd09dcb4df17b31affc00ef99e1f357
Xing <x.xing.unsw@gmail.com>
2021-08-08 16:52:44 +1000

    init

---
+++ Stack/LeetCode 1963 - MinimumNumberOfSwapsToMakeTheStringBalanced/MinimumNumberOfSwaps/MinimumNumberOfSwaps/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimumNumberOfSwaps
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = "[]]]]][[[[";
+			Console.WriteLine(MinSwaps(s));
+		}
+		public static int MinSwaps(string s)
+		{
+			var stack = new Stack<char>();
+			foreach (var c in s)
+			{
+				if (stack.Count != 0 && c == ']' && stack.Peek() == '[')
+					stack.Pop();
+				else
+					stack.Push(c);
+			}
+			return stack.Count / 4 + stack.Count % 4 / 2;
+		}
+	}
+}
