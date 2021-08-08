57a920dcce7acf1762fa19ce6769a6473ea55cf8
Xing <x.xing.unsw@gmail.com>
2021-08-08 16:16:21 +1000

    init

---
+++ String/LeetCode 1961 - CheckIfStringIsAPrefixOfArray/CheckIfStringIsAPrefixOfArray/CheckIfStringIsAPrefixOfArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CheckIfStringIsAPrefixOfArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public bool IsPrefixString(string s, string[] words)
+		{
+			var pattern = "";
+			for (int i = 0; i < words.Length && pattern.Length < s.Length; i++)
+				pattern += words[i];
+			return s == pattern;
+		}
+	}
+}
