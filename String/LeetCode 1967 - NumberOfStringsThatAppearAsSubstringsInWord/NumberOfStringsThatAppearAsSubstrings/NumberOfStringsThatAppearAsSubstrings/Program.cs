77b8e336eae841b88c04179917611c590fc77f38
Xing <x.xing.unsw@gmail.com>
2021-08-15 15:45:50 +1000

    init

---
+++ String/LeetCode 1967 - NumberOfStringsThatAppearAsSubstringsInWord/NumberOfStringsThatAppearAsSubstrings/NumberOfStringsThatAppearAsSubstrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumberOfStringsThatAppearAsSubstrings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int NumOfStrings(string[] patterns, string word)
+		{
+			var res = 0;
+			foreach (var pattern in patterns)
+			{
+				if (word.Contains(pattern))
+					res++;
+			}
+			return res;
+		}
+	}
+}
