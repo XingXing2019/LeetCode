be23fda3a5436590e0379a87b8f6b2e3986dc78e
Xing <x.xing.unsw@gmail.com>
2021-08-15 15:57:35 +1000

    init

---
+++ Greedy/LeetCode 1968 - ArrayWithElementsNotEqualToAverageOfNeighbors/ArrayWithElementsNotEqualToAverage/ArrayWithElementsNotEqualToAverage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayWithElementsNotEqualToAverage
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int[] RearrangeArray(int[] nums)
+		{
+			Array.Sort(nums);
+			var res = new int[nums.Length];
+			int li = 0, hi = nums.Length - 1, index = 0;
+			while (li <= hi)
+			{
+				res[index++] = nums[li++];
+				if (li > hi) break;
+				res[index++] = nums[hi--];
+			}
+			return res;
+		}
+	}
+}
