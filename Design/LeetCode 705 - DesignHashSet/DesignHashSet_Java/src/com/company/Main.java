d01ab10e5dc3dbcf9969bf70eebcc0505b7b8b5e
Xing <x.xing.unsw@gmail.com>
2021-08-21 09:19:56 +1000

    JavaSolution

---
+++ Design/LeetCode 705 - DesignHashSet/DesignHashSet_Java/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyHashSet set = new MyHashSet();
+        set.add(1);
+        set.add(2);
+        System.out.println(set.contains(1));
+        System.out.println(set.contains(3));
+        set.add(2);
+        System.out.println(set.contains(2));
+        set.remove(2);
+        System.out.println(set.contains(2));
+    }
+}
+
+class MyHashSet {
+    int[] set;
+
+    public MyHashSet() {
+        set = new int[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = 1;
+    }
+
+    public void remove(int key) {
+        set[key] = 0;
+    }
+
+    public boolean contains(int key) {
+        return set[key] == 1;
+    }
+}
