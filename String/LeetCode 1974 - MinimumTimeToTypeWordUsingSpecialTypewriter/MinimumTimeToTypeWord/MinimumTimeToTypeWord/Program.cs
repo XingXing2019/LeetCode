14a797d222772cc746167b9e3a25a86a45c1f96f
Xing <x.xing.unsw@gmail.com>
2021-08-22 09:17:24 +1000

    init

---
+++ String/LeetCode 1974 - MinimumTimeToTypeWordUsingSpecialTypewriter/MinimumTimeToTypeWord/MinimumTimeToTypeWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MinimumTimeToTypeWord
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MinTimeToType(string word)
+		{
+			int res = 0;
+			char cur = 'a';
+			foreach (var letter in word)
+			{
+				if(letter > cur)
+					res += Math.Min(letter - cur, cur + 26 - letter);
+				else
+					res += Math.Min(cur - letter, letter + 26 - cur);
+				cur = letter;
+			}
+			return res + word.Length;
+		}
+	}
+}
