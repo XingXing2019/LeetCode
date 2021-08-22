04e5ed2130a872b0559224102c19a9ac24ffae8a
Xing <x.xing.unsw@gmail.com>
2021-08-22 15:29:34 +1000

    init

---
+++ Search/LeetCode 1981 - MinimizeTheDifferenceBetweenTargetAndChosenElements/MinimizeTheDifference/MinimizeTheDifference/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimizeTheDifference
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MinimizeTheDifference(int[][] mat, int target)
+		{
+			int res = int.MaxValue;
+			var visited = new HashSet<int>[mat.Length];
+			for (int i = 0; i < visited.Length; i++)
+				visited[i] = new HashSet<int>();
+			DFS(mat, 0, target, 0, visited, ref res);
+			return res;
+		}
+
+		public void DFS(int[][] mat, int row, int target, int sum, HashSet<int>[] visited, ref int res)
+		{
+			if (row == mat.Length)
+			{
+				res = Math.Min(res, Math.Abs(sum - target));
+				return;
+			}
+			if (!visited[row].Add(sum) || sum - target > res)
+				return;
+			foreach (var num in mat[row])
+			{
+				DFS(mat, row + 1, target, sum + num, visited, ref res);
+			}
+		}
+	}
+}
