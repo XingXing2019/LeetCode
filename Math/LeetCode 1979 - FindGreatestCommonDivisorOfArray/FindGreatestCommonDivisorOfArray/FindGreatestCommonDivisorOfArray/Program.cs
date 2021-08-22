b03bf8a36c6259adab53196739e03c3b5a20534e
Xing <x.xing.unsw@gmail.com>
2021-08-22 14:38:53 +1000

    init

---
+++ Math/LeetCode 1979 - FindGreatestCommonDivisorOfArray/FindGreatestCommonDivisorOfArray/FindGreatestCommonDivisorOfArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace FindGreatestCommonDivisorOfArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int FindGCD(int[] nums)
+		{
+			return GCD(nums.Max(), nums.Min());
+		}
+
+		public int GCD(int num1, int num2)
+		{
+			return num1 % num2 == 0 ? num2 : GCD(num2, num1 % num2);
+		}
+	}
+}
