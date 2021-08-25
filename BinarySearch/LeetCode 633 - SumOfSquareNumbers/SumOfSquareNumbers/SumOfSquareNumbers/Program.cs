106fc27566fb8ab453a403ec93b3198e8ef48142
Xing <x.xing.unsw@gmail.com>
2021-08-25 18:20:59 +1000

    init

---
+++ BinarySearch/LeetCode 633 - SumOfSquareNumbers/SumOfSquareNumbers/SumOfSquareNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumOfSquareNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int c = 1000;
+			Console.WriteLine(JudgeSquareSum(c));
+		}
+		public static bool JudgeSquareSum(int c)
+		{
+			for (int i = 0; i <= Math.Sqrt(c); i++)
+			{
+				int num = (int) Math.Sqrt(c - i * i);
+				if (num * num == c - i * i)
+					return true;
+			}
+			return false;
+		}
+	}
+}
