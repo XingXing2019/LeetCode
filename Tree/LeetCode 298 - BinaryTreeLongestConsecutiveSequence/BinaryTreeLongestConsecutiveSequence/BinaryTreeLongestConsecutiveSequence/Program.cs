688b50070a90bf5fdb9e257e3bcf3f6b3a13368b
Xing <x.xing.unsw@gmail.com>
2021-08-28 17:19:46 +1000

    init

---
+++ Tree/LeetCode 298 - BinaryTreeLongestConsecutiveSequence/BinaryTreeLongestConsecutiveSequence/BinaryTreeLongestConsecutiveSequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BinaryTreeLongestConsecutiveSequence
+{
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		private int max;
+		public int LongestConsecutive(TreeNode root)
+		{
+			DFS(root, null, 0);
+			return max;
+		}
+
+		public void DFS(TreeNode node, TreeNode parent, int len)
+		{
+			if (node == null) return;
+			len = parent != null && node.val - parent.val == 1 ? len + 1 : 1;
+			max = Math.Max(max, len);
+			DFS(node.left, node, len);
+			DFS(node.right, node, len);
+		}
+	}
+}
