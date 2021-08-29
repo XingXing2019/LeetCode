c064868497ce65d284f2ef8a1513de9bdfe0fffc
Xing <x.xing.unsw@gmail.com>
2021-08-29 15:26:13 +1000

    init

---
+++ TwoPointers/LeetCode 1984 - MinimumDifferenceBetweenHighestAndLowestOfKScores/MinimumDifference/MinimumDifference/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MinimumDifference
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MinimumDifference(int[] nums, int k)
+		{
+			Array.Sort(nums);
+			int min = int.MaxValue;
+			int li = 0, hi = k - 1;
+			while (hi < nums.Length)
+			{
+				min = Math.Min(min, nums[hi] - nums[li]);
+				hi++;
+				li++;
+			}
+			return min;
+		}
+	}
+}
