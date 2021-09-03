2892c073d79e4c43647e72877a9d190eba8f19f4
Xing <x.xing.unsw@gmail.com>
2021-09-03 21:28:38 +1000

    init

---
+++ Greedy/LeetCode 1989 - MaximumNumberOfPeopleThatCanBeCaughtInTag/MaximumNumberOfPeople/MaximumNumberOfPeople/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumNumberOfPeople
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] team = { 1, 1, 1, 1, 0, 0, 0, 0 };
+			int dist = 2;
+			Console.WriteLine(CatchMaximumAmountofPeople(team, dist));
+		}
+		public static int CatchMaximumAmountofPeople(int[] team, int dist)
+		{
+			Queue<int> ones = new Queue<int>(), zeros = new Queue<int>();
+			for (int i = 0; i < team.Length; i++)
+			{
+				if(team[i] == 0)
+					zeros.Enqueue(i);
+				else
+					ones.Enqueue(i);
+			}
+			int res = 0;
+			while (zeros.Count != 0 && ones.Count != 0)
+			{
+				if (zeros.Peek() < ones.Peek())
+				{
+					while (zeros.Count != 0 && ones.Peek() - zeros.Peek() > dist)
+						zeros.Dequeue();
+					if (zeros.Count == 0) continue;
+					zeros.Dequeue();
+					ones.Dequeue();
+					res++;
+				}
+				else
+				{
+					while (ones.Count != 0 && zeros.Peek() - ones.Peek() > dist)
+						ones.Dequeue();
+					if (ones.Count == 0) continue;
+					zeros.Dequeue();
+					ones.Dequeue();
+					res++;
+				}
+			}
+			return res;
+		}
+	}
+}
