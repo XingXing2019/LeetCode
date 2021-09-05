e1a7fc66aeba38ac9966fb84e0b135a0f785493d
Xing <x.xing.unsw@gmail.com>
2021-09-05 15:49:46 +1000

    init

---
+++ Array/LeetCode 1991 - FindTheMiddleIndexInArray/FindTheMiddleIndexInArray/FindTheMiddleIndexInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace FindTheMiddleIndexInArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int FindMiddleIndex(int[] nums)
+		{
+			for (int i = 1; i < nums.Length; i++)
+				nums[i] += nums[i - 1];
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (i == 0 && nums[^1] - nums[i] == 0)
+					return 0;
+				if (i != 0 && nums[^1] - nums[i] == nums[i - 1])
+					return i;
+			}
+			return -1;
+		}
+	}
+}
