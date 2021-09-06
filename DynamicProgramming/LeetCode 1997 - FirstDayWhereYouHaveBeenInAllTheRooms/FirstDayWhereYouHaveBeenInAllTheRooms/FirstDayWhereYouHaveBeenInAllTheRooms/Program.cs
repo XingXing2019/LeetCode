bfdd9d1f5a26fe63ee48a17216e4769fe3f502cc
Xing <x.xing.unsw@gmail.com>
2021-09-06 21:23:58 +1000

    init

---
+++ DynamicProgramming/LeetCode 1997 - FirstDayWhereYouHaveBeenInAllTheRooms/FirstDayWhereYouHaveBeenInAllTheRooms/FirstDayWhereYouHaveBeenInAllTheRooms/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstDayWhereYouHaveBeenInAllTheRooms
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int FirstDayBeenInAllRooms(int[] nextVisit)
+		{
+			var dp = new long[nextVisit.Length];
+			int mod = 1_000_000_000 + 7;
+			for (int i = 1; i < nextVisit.Length; i++)
+			{
+				if (nextVisit[i - 1] == i - 1)
+					dp[i] = dp[i - 1] + 2;
+				else
+					dp[i] = (2 * dp[i - 1] - dp[nextVisit[i - 1]] + 2 + mod) % mod;
+			}
+			return (int) dp[^1] % mod;
+		}
+	}
+}
