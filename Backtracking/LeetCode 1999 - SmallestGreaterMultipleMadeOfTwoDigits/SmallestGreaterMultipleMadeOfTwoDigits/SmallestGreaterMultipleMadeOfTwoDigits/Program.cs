d67feadc3d9f4c622390ac593e770595aee3b057
Xing <x.xing.unsw@gmail.com>
2021-09-10 17:53:07 +1000

    init

---
+++ Backtracking/LeetCode 1999 - SmallestGreaterMultipleMadeOfTwoDigits/SmallestGreaterMultipleMadeOfTwoDigits/SmallestGreaterMultipleMadeOfTwoDigits/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmallestGreaterMultipleMadeOfTwoDigits
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int k = 3, digit1 = 4, digit2 = 2;
+			Console.WriteLine(FindInteger(k, digit1, digit2));
+		}
+
+		
+		public static int FindInteger(int k, int digit1, int digit2)
+		{
+			long res = -1;
+			DFS(k, digit1, digit2, 0, new HashSet<long>(), ref res);
+			return (int) res;
+		}
+
+		public static void DFS(int k, int digit1, int digit2, long num, HashSet<long> visited, ref long res)
+		{
+			if(res != -1 && num > res || !visited.Add(num) || num > int.MaxValue)
+				return;
+			if (num % k == 0 && num > k)
+			{
+				res = num;
+				return;
+			}
+			DFS(k, digit1, digit2, num * 10 + digit1, visited, ref res);
+			DFS(k, digit1, digit2, num * 10 + digit2, visited, ref res);
+		}
+	}
+}
