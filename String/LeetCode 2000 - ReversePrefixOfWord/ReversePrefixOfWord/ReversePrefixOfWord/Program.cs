218dcdc355ed154d7b138ebec7302ad9eb809140
Xing <x.xing.unsw@gmail.com>
2021-09-12 14:08:44 +1000

    init

---
+++ String/LeetCode 2000 - ReversePrefixOfWord/ReversePrefixOfWord/ReversePrefixOfWord/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReversePrefixOfWord
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public string ReversePrefix(string word, char ch)
+		{
+			int index = word.IndexOfAny(new []{ch});
+			if (index == -1) return word;
+			var res = "";
+			for (int i = index; i >= 0; i--)
+				res += word[i];
+			return res + word.Substring(index + 1);
+		}
+	}
+}
