6c3c73a82490165897d628addbe7a4859228d58f
Xing <x.xing.unsw@gmail.com>
2021-09-12 14:14:59 +1000

    init

---
+++ HashTable/LeetCode 2001 - NumberOfPairsOfInterchangeableRectangles/NumberOfPairsOfInterchangeableRectangles/NumberOfPairsOfInterchangeableRectangles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfPairsOfInterchangeableRectangles
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public long InterchangeableRectangles(int[][] rectangles)
+		{
+			var dict = new Dictionary<double, int>();
+			long res = 0;
+			foreach (var rectangle in rectangles)
+			{
+				var ratio = (double) rectangle[0] / rectangle[1];
+				if (!dict.ContainsKey(ratio))
+					dict[ratio] = 0;
+				res += dict[ratio];
+				dict[ratio]++;
+			}
+			return res;
+		}
+	}
+}
