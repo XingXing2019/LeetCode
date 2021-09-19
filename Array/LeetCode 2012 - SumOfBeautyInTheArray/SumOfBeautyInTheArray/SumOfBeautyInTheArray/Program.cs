4195b811d35d9cdc43adcdd87e005c4125ec3ab8
Xing <x.xing.unsw@gmail.com>
2021-09-19 14:44:24 +1000

    init

---
+++ Array/LeetCode 2012 - SumOfBeautyInTheArray/SumOfBeautyInTheArray/SumOfBeautyInTheArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumOfBeautyInTheArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int SumOfBeauties(int[] nums)
+		{
+			int[] leftMax = new int[nums.Length], rightMin = new int[nums.Length];
+			int max = int.MinValue, min = int.MaxValue, res = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				leftMax[i] = max;
+				max = Math.Max(max, nums[i]);
+				rightMin[^(i + 1)] = min;
+				min = Math.Min(min, nums[^(i + 1)]);
+			}
+			for (int i = 1; i < nums.Length - 1; i++)
+			{
+				if (nums[i] > leftMax[i] && nums[i] < rightMin[i])
+					res += 2;
+				else if (nums[i - 1] < nums[i] && nums[i] < nums[i + 1])
+					res++;
+			}
+			return res;
+		}
+	}
+}
