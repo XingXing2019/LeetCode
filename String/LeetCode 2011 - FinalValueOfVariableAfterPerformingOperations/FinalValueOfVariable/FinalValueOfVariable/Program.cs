e7290957d924cbbe43b6153037a246792cc16c07
Xing <x.xing.unsw@gmail.com>
2021-09-19 14:36:50 +1000

    init

---
+++ String/LeetCode 2011 - FinalValueOfVariableAfterPerformingOperations/FinalValueOfVariable/FinalValueOfVariable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace FinalValueOfVariable
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int FinalValueAfterOperations(string[] operations)
+		{
+			return operations.Sum(op => op.Contains("++") ? 1 : -1);
+		}
+	}
+}
