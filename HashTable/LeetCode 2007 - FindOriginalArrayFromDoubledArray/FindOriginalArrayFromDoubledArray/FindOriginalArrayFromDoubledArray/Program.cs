c670b4d859ac9486ad85ab2ff1bb69d8640fa6c6
Xing <x.xing.unsw@gmail.com>
2021-09-19 11:58:05 +1000

    init

---
+++ HashTable/LeetCode 2007 - FindOriginalArrayFromDoubledArray/FindOriginalArrayFromDoubledArray/FindOriginalArrayFromDoubledArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindOriginalArrayFromDoubledArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] changed = {6, 3, 0, 1};
+			Console.WriteLine(FindOriginalArray(changed));
+		}
+
+		public static int[] FindOriginalArray(int[] changed)
+		{
+			if (changed.Length % 2 != 0) return new int[0];
+			Array.Sort(changed);
+			var dict = new Dictionary<int, int>();
+			int[] res = new int[changed.Length / 2];
+			int index = 0;
+			for (int i = changed.Length - 1; i >= 0; i--)
+			{
+				int num = changed[i];
+				if (!dict.ContainsKey(num))
+					dict[num] = 0;
+				dict[num]++;
+				if (dict.ContainsKey(num * 2) && dict[num * 2] > (num == 0 ? 1 : 0))
+				{
+					res[index++] = num;
+					dict[num]--;
+					dict[num * 2]--;
+				}
+			}
+			return index == res.Length ? res : new int[0];
+		}
+	}
+}
