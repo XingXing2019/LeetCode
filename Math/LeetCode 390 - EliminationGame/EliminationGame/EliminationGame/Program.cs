d01c484542718cda6c70bc6b2fa23e10a85a2ea0
Xing <x.xing.unsw@gmail.com>
2021-09-20 18:59:30 +1000

    init

---
+++ Math/LeetCode 390 - EliminationGame/EliminationGame/EliminationGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EliminationGame
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int LastRemaining(int n)
+		{
+			int gap = 1, first = 1, round = 1;
+			while (n != 1)
+			{
+				if (round % 2 != 0 || n % 2 != 0)
+					first += gap;
+				gap *= 2;
+				round++;
+				n /= 2;
+			}
+			return first;
+		}
+	}
+}
