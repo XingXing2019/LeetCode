819bc86cfb012e5e93247b3ca2d2bcec9a5a551b
Xing <x.xing.unsw@gmail.com>
2021-09-25 21:07:42 +1000

    init

---
+++ Backtracking/LeetCode 1087 - BraceExpansion/BraceExpansion/BraceExpansion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+
+namespace BraceExpansion
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public static string[] Expand(string s)
+		{
+			var res = new List<string>();
+			DFS(s, "", res);
+			return res.OrderBy(x => x).ToArray();
+		}
+
+		public static void DFS(string s, string path, List<string> res)
+		{
+			if (s.Length == 0)
+			{
+				res.Add(path);
+				return;
+			}
+			if (s.StartsWith('{'))
+			{
+				var index = s.IndexOf('}');
+				var letters = s.Substring(1, index - 1).Split(',');
+				foreach (var letter in letters)
+					DFS(s.Substring(index + 1), path + letter, res);
+			}
+			else
+				DFS(s.Substring(1), path + s[0], res);
+		}
+	}
+}
