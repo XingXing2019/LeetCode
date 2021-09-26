3980e7acc6024e769556d36ec84b46539aabd49d
Xing <x.xing.unsw@gmail.com>
2021-09-26 15:35:36 +1000

    init

---
+++ Array/LeetCode 2016 - MaximumDifferenceBetweenIncreasingElements/MaximumDifference/MaximumDifference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaximumDifference
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MaximumDifference(int[] nums)
+		{
+			int max = int.MinValue, res = -1;
+			for (int i = nums.Length - 1; i >= 0; i--)
+			{
+				if (i != nums.Length - 1 && nums[i] < max)
+					res = Math.Max(res, max - nums[i]);
+				max = Math.Max(max, nums[i]);
+			}
+			return res;
+		}
+	}
+}
