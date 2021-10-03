7447e350a258890dbc4e36d715c097a58bd2bcef
Xing <x.xing.unsw@gmail.com>
2021-10-03 16:14:59 +1100

    init

---
+++ Math/LeetCode 2028 - FindMissingObservations/FindMissingObservations/FindMissingObservations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace FindMissingObservations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int[] MissingRolls(int[] rolls, int mean, int n)
+		{
+			int sum = mean * (n + rolls.Length) - rolls.Sum();
+			if (sum < n || sum > 6 * n)
+				return new int[0];
+			var res = new int[n];
+			var avg = sum / n;
+			var gap = sum - avg * n;
+			for (int i = 0; i < n; i++)
+				res[i] = i < n - gap ? avg : avg + 1;
+			return res;
+		}
+	}
+}
