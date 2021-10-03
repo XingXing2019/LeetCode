df3afb21245ce0f1999bc3d9c78293405c08a8bd
Xing <x.xing.unsw@gmail.com>
2021-10-03 16:02:34 +1100

    init

---
+++ String/LeetCode 2027 - MinimumMovesToConvertString/MinimumMovesToConvertString/MinimumMovesToConvertString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MinimumMovesToConvertString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int MinimumMoves(string s)
+		{
+			int res = 0, index = 0;
+			while (index < s.Length)
+			{
+				if (s[index] == 'O')
+					index++;
+				else
+				{
+					if (s.Substring(index, Math.Min(3, s.Length - index)).Contains('X'))
+						res++;
+					index += 3;
+				}
+			}
+			return res;
+		}
+	}
+}
