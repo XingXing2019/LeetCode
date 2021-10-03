23b75e7d0dc27ef4d0495565c9bedd516986b85d
Xing <x.xing.unsw@gmail.com>
2021-10-03 17:16:40 +1100

    init

---
+++ TwoPointers/LeetCode 2024 - MaximizeTheConfusionOfAnExam/MaximizeTheConfusionOfAnExam/MaximizeTheConfusionOfAnExam/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximizeTheConfusionOfAnExam
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var answerKey = "TTFTTFTT";
+			int k = 1;
+			Console.WriteLine(MaxConsecutiveAnswers(answerKey, k));
+		}
+
+		public static int MaxConsecutiveAnswers(string answerKey, int k)
+		{
+			int res = 0, li = 0, hi = 0;
+			var letters = new Dictionary<char, int> { { 'T', 0 }, { 'F', 0 } };
+			letters[answerKey[0]]++;
+			while (hi < answerKey.Length)
+			{
+				int maxNum = Math.Max(letters['T'], letters['F']);
+				if (maxNum + k >= hi - li + 1)
+				{
+					if (++hi < answerKey.Length)
+						letters[answerKey[hi]]++;
+				}
+				else
+					letters[answerKey[li++]]--;
+				res = Math.Max(res, hi - li);
+			}
+			return res;
+		}
+	}
+}
