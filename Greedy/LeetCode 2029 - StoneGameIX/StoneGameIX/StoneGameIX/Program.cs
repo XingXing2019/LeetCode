12f899dcf3ffb433dcb8d8e127d28c25bf15de56
Xing <x.xing.unsw@gmail.com>
2021-10-04 10:55:39 +1100

    init

---
+++ Greedy/LeetCode 2029 - StoneGameIX/StoneGameIX/StoneGameIX/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace StoneGameIX
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public bool StoneGameIX(int[] stones)
+		{
+			var freq = new int[3];
+			foreach (var stone in stones)
+				freq[stone % 3]++;
+			if (freq[0] % 2 == 0)
+				return freq[1] != 0 && freq[2] != 0;
+			return Math.Abs(freq[1] - freq[2]) > 2;
+		}
+	}
+}
