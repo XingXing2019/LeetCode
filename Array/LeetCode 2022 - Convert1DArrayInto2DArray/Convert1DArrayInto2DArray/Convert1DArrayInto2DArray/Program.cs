300bc514e57dcf417102630d82a189bfeb7f9c80
Xing <x.xing.unsw@gmail.com>
2021-10-03 15:28:03 +1100

    init

---
+++ Array/LeetCode 2022 - Convert1DArrayInto2DArray/Convert1DArrayInto2DArray/Convert1DArrayInto2DArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Convert1DArrayInto2DArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int[][] Construct2DArray(int[] original, int m, int n)
+		{
+			if (original.Length != m * n)
+				return new int[0][];
+			var res = new int[m][];
+			int index = 0;
+			for (int i = 0; i < m; i++)
+			{
+				res[i] = new int[n];
+				for (int j = 0; j < n; j++)
+					res[i][j] = original[index++];
+			}
+			return res;
+		}
+	}
+}
