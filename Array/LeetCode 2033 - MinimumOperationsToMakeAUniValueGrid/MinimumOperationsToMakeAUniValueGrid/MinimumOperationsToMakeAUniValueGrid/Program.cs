0df8552d3ddf92c816b4cefb37db6dd491057928
Xing <x.xing.unsw@gmail.com>
2021-10-10 16:24:36 +1100

    init

---
+++ Array/LeetCode 2033 - MinimumOperationsToMakeAUniValueGrid/MinimumOperationsToMakeAUniValueGrid/MinimumOperationsToMakeAUniValueGrid/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MinimumOperationsToMakeAUniValueGrid
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public int MinOperations(int[][] grid, int x)
+		{
+
+		}
+	}
+}
