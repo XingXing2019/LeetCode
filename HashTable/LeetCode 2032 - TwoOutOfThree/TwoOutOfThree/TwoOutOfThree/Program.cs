2ed81aa74736c276953816698e80a826f7e3a50c
Xing <x.xing.unsw@gmail.com>
2021-10-10 15:37:49 +1100

    init

---
+++ HashTable/LeetCode 2032 - TwoOutOfThree/TwoOutOfThree/TwoOutOfThree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoOutOfThree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public IList<int> TwoOutOfThree(int[] nums1, int[] nums2, int[] nums3)
+		{
+			HashSet<int> n1 = new HashSet<int>(nums1), n2 = new HashSet<int>(nums2), n3 = new HashSet<int>(nums3);
+			var freq = new int[101];
+			foreach (var num in n1)
+				freq[num]++;
+			foreach (var num in n2)
+				freq[num]++;
+			foreach (var num in n3)
+				freq[num]++;
+			var res = new List<int>();
+			for (int i = 0; i < freq.Length; i++)
+			{
+				if(freq[i] > 1)
+					res.Add(i);
+			}
+			return res;
+		}
+	}
+}
