f452bbdeebf53383d47856d947f1b3a60e9b9172
Xing <x.xing.unsw@gmail.com>
2021-10-11 18:05:02 +1100

    JavaSolution

---
+++ Tree/LeetCode 99 - RecoverBinarySearchTree/RecoverBinarySearchTree_Java/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    public void dfs(TreeNode node, List<Integer> res){
+        if (node == null) return;
+        dfs(node.left, res);
+        res.add(node.val);
+        dfs(node.right, res);
+    }
+}
