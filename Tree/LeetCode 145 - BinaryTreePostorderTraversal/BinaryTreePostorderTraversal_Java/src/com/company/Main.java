7f454f7bc9d4dc40ddf0fa69e2b714c4ae4e91f5
Xing <x.xing.unsw@gmail.com>
2021-10-11 18:20:32 +1100

    JavaSolution

---
+++ Tree/LeetCode 145 - BinaryTreePostorderTraversal/BinaryTreePostorderTraversal_Java/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    public void dfs(TreeNode node, List<Integer> res){
+        if (node == null) return;
+        dfs(node.left, res);
+        dfs(node.right, res);
+        res.add(node.val);
+    }
+}
