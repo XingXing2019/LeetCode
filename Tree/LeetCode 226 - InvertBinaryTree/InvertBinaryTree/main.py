77da3539959a87952330215f73822fd440d72b94
Xing <x.xing.unsw@gmail.com>
2021-10-13 18:01:44 +1100

    PythonSolution

---
+++ Tree/LeetCode 226 - InvertBinaryTree/InvertBinaryTree/main.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
