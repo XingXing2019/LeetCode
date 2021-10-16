1722e35c650b7cbfe058f8e1fad6d8aa75c867c3
Xing <x.xing.unsw@gmail.com>
2021-10-17 08:54:04 +1100

    init

---
+++ Greedy/LeetCode 2038 - RemoveColoredPiecesIfBothNeighborsAreTheSameColor/RemoveColoredPieces/RemoveColoredPieces/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RemoveColoredPieces
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var colors = "AAAABBBB";
+			Console.WriteLine(WinnerOfGame(colors));
+		}
+		public static bool WinnerOfGame(string colors)
+		{
+			colors += '#';
+			return CountChance(colors, 'A') > CountChance(colors, 'B');
+		}
+
+		private static int CountChance(string colors, char target)
+		{
+			int res = 0, count = 0;
+			foreach (var c in colors)
+			{
+				if (c == target)
+					count++;
+				else
+				{
+					if (count >= 3)
+						res += count - 2;
+					count = 0;
+				}
+			}
+			return res;
+		}
+	}
+}
