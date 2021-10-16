cb1c081f38fdcf9d669513fd213ab8a00077ecba
Xing <x.xing.unsw@gmail.com>
2021-10-17 08:37:41 +1100

    init

---
+++ Sort/LeetCode 2037 - MinimumNumberOfMovesToSeatEveryone/MinimumNumberOfMovesToSeatEveryone/MinimumNumberOfMovesToSeatEveryone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinimumNumberOfMovesToSeatEveryone
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int MinMovesToSeat(int[] seats, int[] students)
+		{
+			Array.Sort(seats);
+			Array.Sort(students);
+			var res = 0;
+			for (int i = 0; i < seats.Length; i++)
+				res += Math.Abs(seats[i] - students[i]);
+			return res;
+		}
+	}
+}
