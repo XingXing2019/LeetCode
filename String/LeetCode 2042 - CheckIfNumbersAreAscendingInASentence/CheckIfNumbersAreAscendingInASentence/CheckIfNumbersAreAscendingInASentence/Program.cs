d88f745a4b610a54666fa79635741c70f45b5b65
Xing <x.xing.unsw@gmail.com>
2021-10-17 16:01:56 +1100

    init

---
+++ String/LeetCode 2042 - CheckIfNumbersAreAscendingInASentence/CheckIfNumbersAreAscendingInASentence/CheckIfNumbersAreAscendingInASentence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CheckIfNumbersAreAscendingInASentence
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		public bool AreNumbersAscending(string s)
+		{
+			var tokens = s.Split(" ");
+			int start = 0;
+			foreach (var token in tokens)
+			{
+				if(!int.TryParse(token, out var num))
+					continue;
+				if (num <= start)
+					return false;
+				start = num;
+			}
+			return true;
+		}
+	}
+}
