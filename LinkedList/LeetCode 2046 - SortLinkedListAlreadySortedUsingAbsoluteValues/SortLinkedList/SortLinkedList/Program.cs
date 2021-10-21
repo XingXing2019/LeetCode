428a855fdd1e092ed42a40207546cb96cb45e660
Xing <x.xing.unsw@gmail.com>
2021-10-21 18:34:29 +1100

    init

---
+++ LinkedList/LeetCode 2046 - SortLinkedListAlreadySortedUsingAbsoluteValues/SortLinkedList/SortLinkedList/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SortLinkedList
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var head = Generate(new[] {0, 2, -5, 5, 10, -10});
+			Console.WriteLine(SortLinkedList(head));
+		}
+
+		public static ListNode SortLinkedList(ListNode head)
+		{
+			ListNode posHead = new ListNode(), posPointer = posHead;
+			ListNode negHead = new ListNode(), negPointer = negHead;
+			while (head != null)
+			{
+				if (head.val >= 0)
+				{
+					posPointer.next = new ListNode(head.val);
+					posPointer = posPointer.next;
+				}
+				else
+				{
+					var temp = new ListNode(head.val, negHead.next);
+					negHead.next = temp;
+				}
+				head = head.next;
+			}
+			posPointer.next = null;
+			while (negPointer.next != null) 
+				negPointer = negPointer.next;
+			negPointer.next = posHead.next;
+			return negHead.next;
+		}
+
+		public static ListNode Generate(int[] nums)
+		{
+			ListNode res = null;
+			for (int i = nums.Length - 1; i >= 0; i--)
+				res = new ListNode(nums[i], res);
+			return res;
+		}
+	}
+}
