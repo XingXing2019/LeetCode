22e7a17323815e3dc5f0e69a876f907ba65db19a
Xing <x.xing.unsw@gmail.com>
2021-10-23 11:30:21 +1100

    init

---
+++ BinarySearch/LeetCode 154 - FindMinimumInRotatedSortedArrayII/FindMinimumInRotatedSortedArrayII/FindMinimumInRotatedSortedArrayII/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace FindMinimumInRotatedSortedArrayII
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = {1, 3, 5};
+			Console.WriteLine(FindMin_BinarySearch(nums));
+		}
+
+		public static int FindMin(int[] nums)
+		{
+			return nums.Min();
+		}
+
+		public static int FindMin_BinarySearch(int[] nums)
+		{
+			int li = 0, hi = nums.Length - 1;
+			while (li < hi)
+			{
+				var mid = li + (hi - li) / 2;
+				if (nums[mid] > nums[hi])
+					li = mid + 1;
+				else if (nums[mid] < nums[hi])
+					hi = mid;
+				else
+					hi--;
+			}
+			return nums[li];
+		}
+	}
+}
