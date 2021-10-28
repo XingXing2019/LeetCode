00e540fa56ace3b474ff309e2e3955e6b5d2ac0b
Xing <x.xing.unsw@gmail.com>
2021-10-28 18:20:39 +1100

    init

---
+++ Greedy/LeetCode 2052 - MinimumCostToSeparateSentenceIntoRows/MinimumCostToSeparateSentenceIntoRows/MinimumCostToSeparateSentenceIntoRows/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MinimumCostToSeparateSentenceIntoRows
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sentence = "i love leetcode aaaa";
+			int k = 12;
+			Console.WriteLine(MinimumCost(sentence, k));
+		}
+
+		public static int MinimumCost(string sentence, int k, int start = 0)
+		{
+			return DFS(sentence, k, 0);
+		}
+
+		public static int DFS(string sentence, int k, int start)
+		{
+			if (start + k >= sentence.Length)
+				return 0;
+			int res = int.MaxValue;
+			for (int i = start; i - start <= k; i++)
+			{
+				if (sentence[i] == ' ')
+					res = Math.Min(res, (k - i + start) * (k - i + start) + DFS(sentence, k, i + 1));
+			}
+			return res;
+		}
+	}
+}
