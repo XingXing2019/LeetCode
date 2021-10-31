c3aca946a3b36f335095b458cbc9f97b23f81bbe
Xing <x.xing.unsw@gmail.com>
2021-10-31 15:12:20 +1100

    init

---
+++ Array/LeetCode 2057 - SmallestIndexWithEqualValue/SmallestIndexWithEqualValue/SmallestIndexWithEqualValue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmallestIndexWithEqualValue
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int SmallestEqual(int[] nums)
+		{
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (i % 10 == nums[i])
+					return i;
+			}
+			return -1;
+		}
+	}
+}
