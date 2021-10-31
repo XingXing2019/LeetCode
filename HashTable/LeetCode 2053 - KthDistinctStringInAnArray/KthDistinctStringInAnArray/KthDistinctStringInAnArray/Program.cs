37373c7cffcc12bae094798df60718c6853be798
Xing <x.xing.unsw@gmail.com>
2021-10-31 11:16:43 +1100

    init

---
+++ HashTable/LeetCode 2053 - KthDistinctStringInAnArray/KthDistinctStringInAnArray/KthDistinctStringInAnArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace KthDistinctStringInAnArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public string KthDistinct(string[] arr, int k)
+		{
+			var distinct = arr.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).ToList();
+			return distinct.Count >= k ? distinct[k - 1] : "";
+		}
+	}
+}
