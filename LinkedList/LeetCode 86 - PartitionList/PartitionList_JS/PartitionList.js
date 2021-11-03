658cdb9a3f6cf781b10fee4c4a07380bf00e5da5
Xing <x.xing.unsw@gmail.com>
2021-11-03 17:33:20 +1100

    JSSolution

---
+++ LinkedList/LeetCode 86 - PartitionList/PartitionList_JS/PartitionList.js
@@ -0,0 +1,27 @@
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+    let small = new ListNode(), smallPoint = small;
+    let large = new ListNode(), largePointer = large;
+    while (head != null) {
+        if (head.val < x){
+            smallPoint.next = head;
+            smallPoint = smallPoint.next;
+        }
+        else{
+            largePointer.next = head;
+            largePointer = largePointer.next;
+        }
+        head = head.next;
+    }
+    smallPoint.next = large.next;
+    largePointer.next = null;
+    return small.next;
+};
