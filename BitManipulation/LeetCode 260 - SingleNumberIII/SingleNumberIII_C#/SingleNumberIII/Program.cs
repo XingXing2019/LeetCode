fcd560d1299b28b32b8a80e39eb5f39621e00e58
Xing <x.xing.unsw@gmail.com>
2021-11-07 09:25:57 +1100

    Bit Manipulation

--- BitManipulation/LeetCode 260 - SingleNumberIII/SingleNumberIII_C#/SingleNumberIII/Program.cs
+++ BitManipulation/LeetCode 260 - SingleNumberIII/SingleNumberIII_C#/SingleNumberIII/Program.cs
@@ -1,6 +1,4 @@
-﻿//创建字典record记录数字，遍历nums，将不在record中的数字加入record，将已经存在的数字删除。那么遍历结束后字典中就只有两个数。
-//将这两个数村塾结果并返回。
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,6 +11,21 @@ namespace SingleNumberIII
             int[] nums = { 1, 2, 1, 3, 2, 5 };
             Console.WriteLine(SingleNumber(nums));
         }
+
+        public int[] SingleNumber_BitManipulation(int[] nums)
+        {
+	        int xOr = 0, num1 = 0;
+	        foreach (var num in nums)
+		        xOr ^= num;
+	        var flag = xOr & -xOr;
+	        foreach (var num in nums)
+	        {
+		        if ((flag & num) != 0)
+			        num1 ^= num;
+	        }
+	        return new[] {num1, xOr ^ num1};
+        }
+
         static int[] SingleNumber(int[] nums)
         {
             var record = new Dictionary<int, int>();
