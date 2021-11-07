81eb8d631bb298df1c8dea4b8d151d084949cdfe
Xing <x.xing.unsw@gmail.com>
2021-11-07 17:56:43 +1100

    init

---
+++ BinarySearch/LeetCode 2064 - MinimizedMaximumOfProductsDistributedToAnyStore/MinimizedMaximumOfProducts/MinimizedMaximumOfProducts/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimizedMaximumOfProducts
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public int MinimizedMaximum(int n, int[] quantities)
+		{
+			long sum = 0;
+			foreach (var quantity in quantities)
+				sum += quantity;
+			long li = 1, hi = sum;
+			while (li < hi)
+			{
+				long mid = li + (hi - li) / 2, count = 0;
+				foreach (var quantity in quantities)
+					count += (int)Math.Ceiling((double)quantity / mid);
+				if (count > n)
+					li = mid + 1;
+				else
+					hi = mid;
+			}
+			return (int)li;
+		}
+	}
+}
