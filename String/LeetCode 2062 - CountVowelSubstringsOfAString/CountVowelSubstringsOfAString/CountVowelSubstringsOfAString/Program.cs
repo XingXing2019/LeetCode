6403ea97927bb1432fd39ed9f8733588f88efc5c
Xing <x.xing.unsw@gmail.com>
2021-11-07 18:19:06 +1100

    init

---
+++ String/LeetCode 2062 - CountVowelSubstringsOfAString/CountVowelSubstringsOfAString/CountVowelSubstringsOfAString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountVowelSubstringsOfAString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var word = "aeiouu";
+			Console.WriteLine(CountVowelSubstrings(word));
+		}
+
+		public static int CountVowelSubstrings(string word)
+		{
+			var res = 0;
+			var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+			for (int i = 0; i < word.Length; i++)
+			{
+				if (!vowels.Contains(word[i])) continue;
+				var dict = new Dictionary<char, int>();
+				foreach (var vowel in vowels)
+					dict[vowel] = 0;
+				for (int j = i; j < word.Length && vowels.Contains(word[j]); j++)
+				{
+					if (dict.All(x => x.Value > 0)) res++;
+					dict[word[j]]++;
+				}
+				if (dict.All(x => x.Value > 0)) res++;
+			}
+			return res;
+		}
+	}
+}
