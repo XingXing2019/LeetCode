7b2ee035b2672acc621bf914ce7aa3aa61ce86c5
Xing <x.xing.unsw@gmail.com>
2021-11-08 14:20:49 +1100

    MySQL

---
+++ Database/LeetCode 2066 - AccountBalance/AccountBalance_MySQL.sql
@@ -0,0 +1,7 @@
+select account_id, day, sum(
+	case type
+    when 'Deposit' then amount
+	else -amount end
+) over (partition by account_id order by day) as balance
+from transactions
+order by account_id, day
