17a8719feec37bccea7dcc69258c0c8a9be6f305
Xing <x.xing.unsw@gmail.com>
2021-11-09 17:53:44 +1100

    JSSolution

---
+++ Backtracking/LeetCode 39 - CombinationSum/CombinationSum_JS/CombinationSum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+    var dfs = function (candidates, target, start, nums, res){
+        if (target < 0) return;
+        if (target == 0) {
+            var temp = [];
+            nums.forEach(x => { temp.push(x); });
+            res.push(temp);
+            return;
+        }
+        for (let i = start; i < candidates.length; i++) {
+            nums.push(candidates[i]);
+            dfs(candidates, target - candidates[i], i, nums, res);
+            nums.pop();            
+        }
+    }
+    var res = [];
+    dfs(candidates, target, 0, [], res);
+    return res;
+};
