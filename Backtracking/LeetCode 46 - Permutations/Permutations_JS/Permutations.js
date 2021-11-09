c69375151a5dd7f6a4e34195cff98fd978d09173
Xing <x.xing.unsw@gmail.com>
2021-11-09 21:50:21 +1100

    JSSolution

---
+++ Backtracking/LeetCode 46 - Permutations/Permutations_JS/Permutations.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    var dfs = function (nums, set, path, res) {
+        if (path.length == nums.length){
+            res.push(path.slice())
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if (set.has(nums[i])) continue;
+            set.add(nums[i]);
+            path.push(nums[i]);
+            dfs(nums, set, path, res);
+            path.pop();
+            set.remove(nums[i]);
+        }
+    }
+};
