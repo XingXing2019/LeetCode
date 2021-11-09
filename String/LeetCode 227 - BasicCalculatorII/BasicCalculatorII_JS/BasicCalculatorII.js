3e7f345916a907d1bc43022d90951bb661c7ca91
Xing <x.xing.unsw@gmail.com>
2021-11-09 22:46:16 +1100

    JSSolution

---
+++ String/LeetCode 227 - BasicCalculatorII/BasicCalculatorII_JS/BasicCalculatorII.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var calculate = function (s) {
+    s += '#';
+    var operator = '', num = '';
+    var stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == ' ') continue;
+        if (!isNaN(s[i])) {
+            num += s[i];
+        } else {
+            if (operator == '+')
+                stack.push(parseInt(num));
+            else if (operator == '-')
+                stack.push(-parseInt(num));
+            else if (operator == '*')
+                stack.push(parseInt(num) * stack.pop());
+            else if (operator == '/') {                        
+                stack.push(~~(stack.pop() / parseInt(num)));
+            }
+            else 
+                stack.push(parseInt(num));
+            operator = s[i];
+            num = '';
+        }
+    }
+    var res = 0;
+    while (stack.length != 0)
+        res += stack.pop();
+    return res;
+};
