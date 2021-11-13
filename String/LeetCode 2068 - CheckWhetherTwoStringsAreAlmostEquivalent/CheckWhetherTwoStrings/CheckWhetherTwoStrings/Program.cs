655f2da7f308db02116dff4a895286991decce27
Xing <x.xing.unsw@gmail.com>
2021-11-14 09:10:32 +1100

    init

---
+++ String/LeetCode 2068 - CheckWhetherTwoStringsAreAlmostEquivalent/CheckWhetherTwoStrings/CheckWhetherTwoStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CheckWhetherTwoStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "abcdeef", word2 = "abaaacc";
+            Console.WriteLine(CheckAlmostEquivalent(word1, word2));
+        }
+
+        public static bool CheckAlmostEquivalent(string word1, string word2)
+        {
+            var record = new int[26];
+            foreach (var l in word1)
+                record[l - 'a']++;
+            foreach (var l in word2)
+                record[l - 'a']--;
+            return record.Max(Math.Abs) <= 3;
+        }
+    }
+}
