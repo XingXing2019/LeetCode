c3f509f276237861d56a21bb4af892582e70870d
Xing <x.xing.unsw@gmail.com>
2021-11-14 10:32:29 +1100

    init

---
+++ BinarySearch/LeetCode 2070 - MostBeautifulItemForEachQuery/MostBeautifulItemForEachQuery/MostBeautifulItemForEachQuery/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace MostBeautifulItemForEachQuery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] items = new[]
+            {
+                new[] { 1, 2 },
+                new[] { 3, 2 },
+                new[] { 2, 4 },
+                new[] { 5, 6 },
+                new[] { 3, 5 },
+            };
+            int[] queries = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(MaximumBeauty(items, queries));
+        }
+        public static int[] MaximumBeauty(int[][] items, int[] queries)
+        {
+            items = items.OrderBy(x => x[0]).ToArray();
+            var leftMax = new int[items.Length];
+            var res = new int[queries.Length];
+            var left = items[0][1];
+            for (int i = 0; i < leftMax.Length; i++)
+            {
+                left = Math.Max(left, items[i][1]);
+                leftMax[i] = left;
+            }
+            for (int i = 0; i < queries.Length; i++)
+            {
+                var index = BinarySearch(items, queries[i]);
+                res[i] = index >= 0 && index < leftMax.Length ? leftMax[index] : 0;
+            }
+            return res;
+        }
+
+        public static int BinarySearch(int[][] items, int target)
+        {
+            int li = 0, hi = items.Length - 1;
+            while (li <= hi)
+            {
+                int mid = li + (hi - li) / 2;
+                if (items[mid][0] <= target)
+                    li = mid + 1;
+                else
+                    hi = mid - 1;
+            }
+            return hi;
+        }
+    }
+}
---
+++ Design/LeetCode 2069 - WalkingRobotSimulationII/WalkingRobotSimulationII/WalkingRobotSimulationII/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace WalkingRobotSimulationII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var robot = new Robot(6, 3);
+
+            robot.Move(13);
+        }
+    }
+
+    public class Robot
+    {
+        private int x;
+        private int y;
+        private int index;
+        private int[] dimension;
+        public Robot(int width, int height)
+        {
+            this.dimension = new[] { width, height };
+        }
+
+        public void Move(int num)
+        {
+            var dir = new[] { new[] { 1, 0 }, new[] { 0, 1 }, new[] { -1, 0 }, new[] { 0, -1 } };
+            if (index % 2 == 0)
+            {
+                if (index % 4 == 0 && num > dimension[index % 2] - x - 1)
+                {
+                    num -= dimension[index % 2] - x - 1;
+                    x = dimension[index % 2] - 1;
+                    index++;
+                }
+                else if (index % 4 != 0 && num > x)
+                {
+                    num -= x;
+                    x = 0;
+                    index++;
+                }
+            }
+            else 
+            {
+                if (index % 4 == 1 && num > dimension[index % 2] - y - 1)
+                {
+                    num -= dimension[index % 2] - y - 1;
+                    y = dimension[index % 2] - 1;
+                    index++;
+                }
+                else if (index % 4 != 1 && num > y)
+                {
+                    num -= y;
+                    y = 0;
+                    index++;
+                }
+            }
+
+            num %= dimension[0] + dimension[1];
+
+
+            while (num > dimension[index % 2] - 1)
+            {
+                num -= dimension[index % 2] - 1;
+                if (index % 2 == 0)
+                    x = index % 4 == 0 ? dimension[index % 2] - 1 : 0;
+                else
+                    y = index % 4 == 1 ? dimension[index % 2] - 1 : 0;
+                index++;
+            }
+            x += dir[index % 4][0] * num;
+            y += dir[index % 4][1] * num;
+        }
+
+        public int[] GetPos()
+        {
+            return new[] { x, y };
+        }
+
+        public string GetDir()
+        {
+            var direction = new[] { "East", "North", "West", "South" };
+            return direction[index % 4];
+        }
+    }
+}
