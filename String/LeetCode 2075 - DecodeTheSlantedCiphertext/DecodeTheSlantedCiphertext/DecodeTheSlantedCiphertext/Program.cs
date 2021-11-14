f2db350bd6059230e3da3594d2863ee8c803b337
Xing <x.xing.unsw@gmail.com>
2021-11-14 21:32:18 +1100

    init

---
+++ String/LeetCode 2075 - DecodeTheSlantedCiphertext/DecodeTheSlantedCiphertext/DecodeTheSlantedCiphertext/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecodeTheSlantedCiphertext
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var encodedText = " b  ac";
+            var rows = 2;
+            Console.WriteLine(DecodeCiphertext(encodedText, rows));
+        }
+
+        public static string DecodeCiphertext(string encodedText, int rows)
+        {
+            int size = encodedText.Length / rows, index = 0;
+            var words = new string[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                words[i] = encodedText.Substring(index, size);
+                index += size;
+            }
+            var res = new StringBuilder();
+            for (int j = 0; j < size; j++)
+            {
+                for (int i = 0; i < rows && i + j < size; i++)
+                    res.Append(words[i][i + j]);
+            }
+            return res.ToString().TrimEnd();
+        }
+    }
+}
