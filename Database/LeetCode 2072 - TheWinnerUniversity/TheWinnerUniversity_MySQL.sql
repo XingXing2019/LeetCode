d292fe0f770eea0d1493c8baa5fe9d30ed42fc28
Xing <x.xing.unsw@gmail.com>
2021-11-14 11:28:10 +1100

    MySQL

--- Database/LeetCode 2072 - TheWinnerUniversity/TheWinnerUniversity_MySQL.sql
+++ Database/LeetCode 2072 - TheWinnerUniversity/TheWinnerUniversity_MySQL.sql
@@ -1,6 +1,6 @@
 select (
 	case 
-		when (select count(*) from NewYork where score >= 90) > (select count(*) from California where score >= 90) then 'New York University'
+	when (select count(*) from NewYork where score >= 90) > (select count(*) from California where score >= 90) then 'New York University'
         when (select count(*) from NewYork where score >= 90) < (select count(*) from California where score >= 90) then 'California University'
         else 'No Winner' 
 	end
