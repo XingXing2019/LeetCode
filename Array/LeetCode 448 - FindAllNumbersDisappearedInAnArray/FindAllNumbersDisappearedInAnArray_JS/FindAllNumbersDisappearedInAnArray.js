21f6eae640a499ffc4d71bc3ab8e3409c6e20976
Xing <x.xing.unsw@gmail.com>
2021-11-18 20:25:52 +1100

    JSSolution

--- Array/LeetCode 448 - FindAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray_JS/FindAllNumbersDisappearedInAnArray.js
+++ Array/LeetCode 448 - FindAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray_JS/FindAllNumbersDisappearedInAnArray.js
@@ -3,5 +3,14 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function (nums) {
-
+    for (let i = 0; i < nums.length; i++) {
+        var index = Math.abs(nums[i]) - 1;
+        nums[index] = Math.min(nums[index], -nums[index]);        
+    }
+    var res = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0)
+            res.push(i + 1);
+    }
+    return res;
 };
