0ed567ee1822211a0c452a62910e81790c5ab8c4
Xing <x.xing.unsw@gmail.com>
2021-11-21 16:34:25 +1100

    init

---
+++ Array/LeetCode 2078 - TwoFurthestHousesWithDifferentColors/TwoFurthestHouses/TwoFurthestHouses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TwoFurthestHouses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MaxDistance(int[] colors)
+        {
+            int res = 0;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                int li = 0, hi = colors.Length - 1;
+                while (li < i && colors[li] == colors[i] && i - li > res)
+                    li++;
+                while (i < hi && colors[hi] == colors[i] && hi - i > res)
+                    hi--;
+                res = Math.Max(res, Math.Max(i - li, hi - i));
+            }
+            return res;
+        }
+    }
+}
