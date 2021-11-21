3ec08568805658a8548a618dd54ac18085292777
Xing <x.xing.unsw@gmail.com>
2021-11-21 16:43:21 +1100

    init

---
+++ Array/LeetCode 2079 - WateringPlants/WateringPlants/WateringPlants/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace WateringPlants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int WateringPlants(int[] plants, int capacity)
+        {
+            int res = 0, record = capacity;
+            for (int i = 0; i < plants.Length; i++)
+            {
+                if (capacity >= plants[i])
+                    res++;
+                else
+                {
+                    capacity = record;
+                    res += i * 2 + 1;
+                }
+                capacity -= plants[i];
+            }
+            return res;
+        }
+    }
+}
