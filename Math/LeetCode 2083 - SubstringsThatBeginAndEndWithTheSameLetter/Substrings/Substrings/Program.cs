e6304b14fe706575b4a2702fa863de322564df60
Xing <x.xing.unsw@gmail.com>
2021-11-26 17:04:54 +1100

    init

---
+++ Math/LeetCode 2083 - SubstringsThatBeginAndEndWithTheSameLetter/Substrings/Substrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Substrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long NumberOfSubstrings(string s)
+        {
+            var dict = new Dictionary<char, long>();
+            foreach (var c in s)
+            {
+                if (!dict.ContainsKey(c))
+                    dict[c] = 0;
+                dict[c]++;
+            }
+            long res = 0;
+            foreach (var freq in dict.Values)
+                res += (1 + freq) * freq / 2;
+            return res;
+        }
+    }
+}
