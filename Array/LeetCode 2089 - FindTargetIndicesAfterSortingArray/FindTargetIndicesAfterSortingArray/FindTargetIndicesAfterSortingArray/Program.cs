6acbf550a8f002117b3cdade76688b20561a49b0
Xing <x.xing.unsw@gmail.com>
2021-11-28 17:39:06 +1100

    init

---
+++ Array/LeetCode 2089 - FindTargetIndicesAfterSortingArray/FindTargetIndicesAfterSortingArray/FindTargetIndicesAfterSortingArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindTargetIndicesAfterSortingArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IList<int> TargetIndices(int[] nums, int target)
+        {
+            int count = 0, smaller = 0;
+            foreach (var num in nums)
+            {
+                if (num == target) count++;
+                else if (num < target) smaller++;
+            }
+            var res = new List<int>();
+            var start = smaller;
+            for (int i = 0; i < count; i++)
+                res.Add(start++);
+            return res;
+        }
+    }
+}
