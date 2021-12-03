0b75e524fa8d19159ace8a556ab406c01ebbf9f9
Xing <x.xing.unsw@gmail.com>
2021-12-03 15:29:07 +1100

    JSSolution

--- DynamicProgramming/LeetCode 152 - MaximumProductSubarray/MaximumProductSubarray_JS/MaximumProductSubarray.js
+++ DynamicProgramming/LeetCode 152 - MaximumProductSubarray/MaximumProductSubarray_JS/MaximumProductSubarray.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var maxProduct = function (nums) {
-    var min = [], max = [];
-    min[0] = nums[0];
-    max[0] = nums[0];
-    var res = nums[0];
+    var min = nums[0], max = nums[0], res = nums[0];
     for (let i = 1; i < nums.length; i++) {
-        max[i] = Math.max(nums[i], nums[i] * min[i - 1], nums[i] * max[i - 1]);
-        min[i] = Math.min(nums[i], nums[i] * min[i - 1], nums[i] * max[i - 1]);
-        res = Math.max(res, max[i]);        
+        var tempMax = Math.max(nums[i], nums[i] * min, nums[i] * max);
+        var tempMin = Math.min(nums[i], nums[i] * min, nums[i] * max);
+        max = tempMax;
+        min = tempMin;
+        res = Math.max(res, max);        
     }
     return res;
 };
