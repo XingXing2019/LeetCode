7b56806d0a2341918b734246c581269fa19c43b9
Xing <x.xing.unsw@gmail.com>
2021-12-11 21:15:08 +1100

    init

---
+++ BinarySearch/LeetCode 878 - NthMagicalNumber/NthMagicalNumber/NthMagicalNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NthMagicalNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4, a = 2, b = 3;
+            Console.WriteLine(NthMagicalNumber(n, a, b));
+        }
+
+        public static int NthMagicalNumber(int n, int a, int b)
+        {
+            var lcm = a * b / GCD(a, b);
+            long li = 0, hi = n * (a + b), mod = 1_000_000_000 + 7;
+            while (li < hi)
+            {
+                var mid = li + (hi - li) / 2;
+                var count = mid / a + mid / b - mid / lcm;
+                if (count >= n)
+                    hi = mid;
+                else
+                    li = mid + 1;
+            }
+            return (int) (li % mod);
+        }
+
+        public static int GCD(int a, int b)
+        {
+            if (b == 0) return a;
+            return GCD(b, a % b);
+        }
+    }
+}
