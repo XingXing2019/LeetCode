f2c42e9d9de86c9964dce33c47d91b72a65f5dd2
Xing <x.xing.unsw@gmail.com>
2021-12-12 17:46:33 +1100

    init

---
+++ Graph/LeetCode 2101 - DetonateTheMaximumBombs/DetonateTheMaximumBombs/DetonateTheMaximumBombs/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DetonateTheMaximumBombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] bombs = new[]
+            {
+                new[] { 2, 1, 3 },
+                new[] { 6, 1, 4 },
+            };
+            Console.WriteLine(MaximumDetonation(bombs));
+        }
+
+        public static int MaximumDetonation(int[][] bombs)
+        {
+            var graph = new List<int>[bombs.Length];
+            for (int i = 0; i < bombs.Length; i++)
+            {
+                graph[i] = new List<int>();
+                for (int j = 0; j < bombs.Length; j++)
+                {
+                    if (i == j) continue;
+                    double x = Math.Abs(bombs[i][0] - bombs[j][0]);
+                    double y = Math.Abs(bombs[i][1] - bombs[j][1]);
+                    if (Math.Sqrt(x * x + y * y) <= bombs[i][2])
+                        graph[i].Add(j);
+                }
+            }
+            var res = 0;
+            for (int i = 0; i < bombs.Length; i++)
+            {
+                var set = new HashSet<int>();
+                DFS(graph, i, new HashSet<int>(), set);
+                res = Math.Max(res, set.Count);
+            }
+            return res;
+        }
+
+        public static void DFS(List<int>[] graph, int cur, HashSet<int> visited, HashSet<int> bombs)
+        {
+            if (!visited.Add(cur)) 
+                return;
+            bombs.Add(cur);
+            foreach (var next in graph[cur])
+                DFS(graph, next, visited, bombs);
+        }
+    }
+}
---
+++ HashTable/LeetCode2103 - RingsAndRods/RingsAndRods/RingsAndRods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RingsAndRods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountPoints(string rings)
+        {
+            var record = new int[10];
+            var color = new Dictionary<char, int> { { 'R', 1 }, { 'G', 2 }, { 'B', 4 } };
+            for (int i = 0; i < rings.Length; i+=2)
+            {
+                var num = color[rings[i]];
+                record[rings[i + 1] - '0'] |= num;
+            }
+            var res = 0;
+            foreach (var r in record)
+            {
+                if (r == 7)
+                    res++;
+            }
+            return res;
+        }
+    }
+}
