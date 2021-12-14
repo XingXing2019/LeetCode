56a913ed781fed5a8477ef1a380ec7040a20af28
Xing <x.xing.unsw@gmail.com>
2021-12-14 22:07:22 +1100

    init

---
+++ Stack/LeetCode 1944 - NumberOfVisiblePeopleInAQueue/NumberOfVisiblePeopleInAQueue/NumberOfVisiblePeopleInAQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfVisiblePeopleInAQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] CanSeePersonsCount(int[] heights)
+        {
+            var res = new int[heights.Length];
+            var stack = new Stack<int>();
+            for (int i = 0; i < heights.Length; i++)
+            {
+                while (stack.Count != 0 && heights[stack.Peek()] < heights[i])
+                {
+                    var index = stack.Pop();
+                    res[index]++;
+                }
+                if (stack.Count != 0)
+                    res[stack.Peek()]++;
+                stack.Push(i);
+            }
+            return res;
+        }
+    }
+}
