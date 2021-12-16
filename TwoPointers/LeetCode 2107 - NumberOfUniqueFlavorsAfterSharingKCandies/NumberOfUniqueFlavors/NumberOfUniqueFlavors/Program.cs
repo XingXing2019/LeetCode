66cbbe45e87cf58ae021d7e8115eab6a8fd9618e
Xing <x.xing.unsw@gmail.com>
2021-12-16 18:53:34 +1100

    init

---
+++ TwoPointers/LeetCode 2107 - NumberOfUniqueFlavorsAfterSharingKCandies/NumberOfUniqueFlavors/NumberOfUniqueFlavors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace NumberOfUniqueFlavors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] candies = { 1, 2, 3, 2, 2, 2 };
+            int k = 3;
+            Console.WriteLine(ShareCandies(candies, k));
+        }
+        public static int ShareCandies(int[] candies, int k)
+        {
+            var dict = candies.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            int li = 0, hi = -1;
+            for (int i = 0; i < k; i++)
+            {
+                dict[candies[++hi]]--;
+                if (dict[candies[hi]] == 0)
+                    dict.Remove(candies[hi]);
+            }
+            int res = dict.Count;
+            while (hi < candies.Length - 1)
+            {
+                res = Math.Max(res, dict.Count);
+                if (!dict.ContainsKey(candies[li]))
+                    dict[candies[li]] = 0;
+                dict[candies[li++]]++;
+                dict[candies[++hi]]--; 
+                if (dict[candies[hi]] == 0)
+                    dict.Remove(candies[hi]);
+            }
+            return Math.Max(res, dict.Count);
+        }
+    }
+}
