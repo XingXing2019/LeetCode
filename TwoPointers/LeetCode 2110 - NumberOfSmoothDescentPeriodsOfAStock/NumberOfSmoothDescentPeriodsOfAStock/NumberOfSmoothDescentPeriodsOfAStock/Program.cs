8ad8561d1609854c5c0a1e29b930dac98b8d2078
Xing <x.xing.unsw@gmail.com>
2021-12-19 20:39:20 +1100

    init

---
+++ TwoPointers/LeetCode 2110 - NumberOfSmoothDescentPeriodsOfAStock/NumberOfSmoothDescentPeriodsOfAStock/NumberOfSmoothDescentPeriodsOfAStock/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumberOfSmoothDescentPeriodsOfAStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = { 3, 2, 1, 4 };
+            Console.WriteLine(GetDescentPeriods(prices));
+        }
+
+        public static long GetDescentPeriods(int[] prices)
+        {
+            long res = 0, li = 0, hi = 1;
+            while (hi < prices.Length)
+            {
+                if (prices[hi - 1] - prices[hi] != 1)
+                {
+                    res += (hi - li + 1) * (hi - li) / 2;
+                    li = hi;
+                }
+                hi++;
+            }
+            return res + (hi - li + 1) * (hi - li) / 2;
+        }
+    }
+}
