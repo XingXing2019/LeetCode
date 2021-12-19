9ea591b1fb9ac83d9ec085b322bf3f15a19542fb
Xing <x.xing.unsw@gmail.com>
2021-12-19 17:07:59 +1100

    init

---
+++ String/LeetCode 2108 - FindFirstPalindromicStringInTheArray/FindFirstPalindromicStringInTheArray/FindFirstPalindromicStringInTheArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace FindFirstPalindromicStringInTheArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public string FirstPalindrome(string[] words)
+        {
+            foreach (var word in words)
+            {
+                if (word == string.Join("", word.Reverse()))
+                    return word;
+            }
+            return "";
+        }
+    }
+}
