aaadf07111eb7b0cce0d79856402827eccece0f1
Xing <x.xing.unsw@gmail.com>
2021-12-26 22:16:42 +1100

    init

---
+++ String/LeetCode 2116 - CheckIfAParenthesesStringCanBeValid/CheckIfAParenthesesStringCanBeValid/CheckIfAParenthesesStringCanBeValid/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Linq.Expressions;
+
+namespace CheckIfAParenthesesStringCanBeValid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = ")(", locked = "00";
+            Console.WriteLine(CanBeValid(s, locked));
+        }
+
+        public static bool CanBeValid(string s, string locked)
+        {
+            if (s.Length % 2 != 0) return false;
+            int unpaired = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ')')
+                    unpaired--;
+                else
+                {
+                    unpaired += locked[i] == '0' ? -1 : 1;
+                    if (unpaired > 0)
+                        return false;
+                }
+            }
+            unpaired = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != '(')
+                    unpaired--;
+                else
+                {
+                    unpaired += locked[i] == '0' ? -1 : 1;
+                    if (unpaired > 0)
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
