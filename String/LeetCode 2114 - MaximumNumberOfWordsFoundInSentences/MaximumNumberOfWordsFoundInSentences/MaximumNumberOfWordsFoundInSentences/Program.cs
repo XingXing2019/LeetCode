1c5fda436fd0f4d6f59a95f542c90d6f2652504a
Xing <x.xing.unsw@gmail.com>
2021-12-26 20:49:39 +1100

    init

---
+++ String/LeetCode 2114 - MaximumNumberOfWordsFoundInSentences/MaximumNumberOfWordsFoundInSentences/MaximumNumberOfWordsFoundInSentences/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumNumberOfWordsFoundInSentences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MostWordsFound(string[] sentences)
+        {
+            return sentences.Max(x => x.Split(' ').Length);
+        }
+    }
+}
