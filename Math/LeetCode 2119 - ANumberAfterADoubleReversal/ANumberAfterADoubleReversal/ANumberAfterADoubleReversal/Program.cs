0e5df4c4fb44f708cbc604b7d08cadae9421c434
Xing <x.xing.unsw@gmail.com>
2021-12-27 20:48:01 +1100

    init

---
+++ Math/LeetCode 2119 - ANumberAfterADoubleReversal/ANumberAfterADoubleReversal/ANumberAfterADoubleReversal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ANumberAfterADoubleReversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsSameAfterReversals(int num)
+        {
+            return num == 0 || num % 10 != 0;
+        }
+    }
+}
