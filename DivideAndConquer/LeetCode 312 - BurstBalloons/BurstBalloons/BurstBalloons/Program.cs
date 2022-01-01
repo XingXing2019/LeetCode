1e6f4d66bd3f6f16d44241bbae44fa3bafca8f0f
Xing <x.xing.unsw@gmail.com>
2022-01-01 20:25:44 +1100

    move

--- DivideAndConquer/LeetCode 312 - BurstBalloons/BurstBalloons/BurstBalloons/Program.cs
+++ DivideAndConquer/LeetCode 312 - BurstBalloons/BurstBalloons/BurstBalloons/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// 分治算法
+// 遍历nums中的每一个气球，i代表最后要打的那个气球。
+// 那么打掉这个气球能得到的分数就是 nums[i] * nums[li - 1] * nums[hi + 1]
+// 递归求解i左侧和右侧分别能得到的最大值
+using System;
 
 namespace BurstBalloons
 {
@@ -15,10 +19,10 @@ namespace BurstBalloons
             var dp = new int[nums.Length][];
             for (int i = 0; i < nums.Length; i++)
                 dp[i] = new int[nums.Length];
-            return DFS(nums, dp, 0, nums.Length - 1);
+            return DivideAndConquer(nums, dp, 0, nums.Length - 1);
         }
 
-        public static int DFS(int[] nums, int[][] dp, int li, int hi)
+        public static int DivideAndConquer(int[] nums, int[][] dp, int li, int hi)
         {
             if (li > hi) return 0;
             if (dp[li][hi] != 0) return dp[li][hi];
@@ -26,7 +30,7 @@ namespace BurstBalloons
             for (int i = li; i <= hi; i++)
             {
                 var coins = (li == 0 ? 1 : nums[li - 1]) * nums[i] * (hi == nums.Length - 1 ? 1 : nums[hi + 1]);
-                var cur = DFS(nums, dp, li, i - 1) + DFS(nums, dp, i + 1, hi) + coins;
+                var cur = DivideAndConquer(nums, dp, li, i - 1) + DivideAndConquer(nums, dp, i + 1, hi) + coins;
                 max = Math.Max(max, cur);
             }
             dp[li][hi] = max;
