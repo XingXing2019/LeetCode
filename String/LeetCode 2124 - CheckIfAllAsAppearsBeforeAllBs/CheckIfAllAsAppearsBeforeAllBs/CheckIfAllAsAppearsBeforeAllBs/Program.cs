db1de08b2003c724474c7add7bf8234eac081970
Xing <x.xing.unsw@gmail.com>
2022-01-03 09:15:02 +1100

    init

---
+++ String/LeetCode 2124 - CheckIfAllAsAppearsBeforeAllBs/CheckIfAllAsAppearsBeforeAllBs/CheckIfAllAsAppearsBeforeAllBs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CheckIfAllAsAppearsBeforeAllBs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool CheckString(string s)
+        {
+            return s.IndexOf('b') == -1 || s.IndexOf('b') > s.LastIndexOf('a');
+        }
+    }
+}
