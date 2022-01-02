88576b1ab9a07bb584554862e7314bd82a0d81c8
Xing <x.xing.unsw@gmail.com>
2022-01-03 09:21:31 +1100

    init

---
+++ Array/LeetCode 2125 - NumberOfLaserBeamsInABank/NumberOfLaserBeamsInABank/NumberOfLaserBeamsInABank/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Linq;
+
+namespace NumberOfLaserBeamsInABank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int NumberOfBeams(string[] bank)
+        {
+            int res = 0, last = bank[0].Count(x => x == '1');
+            for (int i = 1; i < bank.Length; i++)
+            {
+                var cur = bank[i].Count(x => x == '1');
+                if (cur == 0) continue;
+                res += last * cur;
+                last = cur;
+            }
+            return res;
+        }
+    }
+}
