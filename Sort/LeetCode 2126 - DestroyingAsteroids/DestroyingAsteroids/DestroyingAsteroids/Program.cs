e15506b323743413a9f2eed76b7a0d0278fe753b
Xing <x.xing.unsw@gmail.com>
2022-01-03 09:26:28 +1100

    init

---
+++ Sort/LeetCode 2126 - DestroyingAsteroids/DestroyingAsteroids/DestroyingAsteroids/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace DestroyingAsteroids
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool AsteroidsDestroyed(int mass, int[] asteroids)
+        {
+            long sum = mass;
+            Array.Sort(asteroids);
+            foreach (var asteroid in asteroids)
+            {
+                if (sum < asteroid)
+                    return false;
+                sum += asteroid;
+            }
+            return true;
+        }
+    }
+}
