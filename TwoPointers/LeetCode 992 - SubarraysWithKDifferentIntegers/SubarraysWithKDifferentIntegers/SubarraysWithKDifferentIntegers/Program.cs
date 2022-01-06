41eb2e7ae7d5b220758cfa3b1653b46b36b65539
Xing <x.xing.unsw@gmail.com>
2022-01-06 22:13:19 +1100

    init

---
+++ TwoPointers/LeetCode 992 - SubarraysWithKDifferentIntegers/SubarraysWithKDifferentIntegers/SubarraysWithKDifferentIntegers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubarraysWithKDifferentIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int SubarraysWithKDistinct(int[] nums, int k)
+        {
+            return CountLess(nums, k) - CountLess(nums, k - 1);
+        }
+
+        public int CountLess(int[] nums, int k)
+        {
+            var dict = new Dictionary<int, int>();
+            int li = 0, hi = 0, res = 0;
+            while (hi < nums.Length)
+            {
+                if (!dict.ContainsKey(nums[hi]))
+                    dict[nums[hi]] = 0;
+                dict[nums[hi++]]++;
+                while (li < hi && dict.Count > k)
+                {
+                    dict[nums[li]]--;
+                    if (dict[nums[li]] == 0)
+                        dict.Remove(nums[li]);
+                    li++;
+                }
+                res += hi - li + 1;
+            }
+            return res;
+        }
+    }
+}
