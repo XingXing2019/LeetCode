e7ec826fd0cbc4fc0a42c76290a0d852e2a27df7
Xing <x.xing.unsw@gmail.com>
2022-01-07 21:12:01 +1100

    JSSolution

--- HashTable/LeetCode 187 - RepeatedDNASequences/RepeatedDNASequences_JS/RepeatedDNASequences.js
+++ HashTable/LeetCode 187 - RepeatedDNASequences/RepeatedDNASequences_JS/RepeatedDNASequences.js
@@ -4,21 +4,21 @@
  */
 var findRepeatedDnaSequences = function (s) {
     if (s.length < 10) return []
-    var mask = 1000000000, window = 0
+    var mask = 1000000000,
+        window = 0
     var map = { A: 1, C: 2, G: 3, T: 4 }
-    for (let i = 0; i < 10; i++)
-        window = window * 10 + map[s[i]]
+    for (let i = 0; i < 10; i++) window = window * 10 + map[s[i]]
     var li = 0, hi = 9
-    var set = new Set(), res = [], added = new Set()
-    set.add(window)
+    var sequence = {}, res = []
+    sequence[window] = 1
     while (hi < s.length - 1) {
         window = (window - mask * map[s[li++]]) * 10 + map[s[++hi]]
-        if (set.has(window) && !added.has(window)) {
-            added.add(window)
+        if (sequence[window] == 1) {
             res.push(s.substring(li, hi + 1))
+            sequence[window]++
         }
         else
-            set.add(window)
+            sequence[window] ? sequence[window]++ : (sequence[window] = 1)
     }
     return res
 }
