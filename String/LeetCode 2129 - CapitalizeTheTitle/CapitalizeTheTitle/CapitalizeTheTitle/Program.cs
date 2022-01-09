69408906c16cbfd665634119ed94cf100cc41fb7
Xing <x.xing.unsw@gmail.com>
2022-01-09 17:40:33 +1100

    init

---
+++ String/LeetCode 2129 - CapitalizeTheTitle/CapitalizeTheTitle/CapitalizeTheTitle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CapitalizeTheTitle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string CapitalizeTitle(string title)
+        {
+            var words = title.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                if (word.Length < 3)
+                    words[i] = word.ToLower();
+                else
+                    words[i] = char.ToUpper(word[0]) + word.Substring(1).ToLower();
+            }
+            return string.Join(' ', words);
+        }
+    }
+}
