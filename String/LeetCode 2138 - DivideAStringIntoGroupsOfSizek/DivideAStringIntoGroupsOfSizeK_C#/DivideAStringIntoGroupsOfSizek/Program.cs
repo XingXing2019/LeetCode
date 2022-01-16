0a22f0257c4472e9d8386b8079613de107ee7780
Xing <x.xing.unsw@gmail.com>
2022-01-16 15:36:07 +1100

    init

---
+++ String/LeetCode 2138 - DivideAStringIntoGroupsOfSizek/DivideAStringIntoGroupsOfSizeK_C#/DivideAStringIntoGroupsOfSizek/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace DivideAStringIntoGroupsOfSizek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public string[] DivideString(string s, int k, char fill)
+        {
+            var len = s.Length % k == 0 ? s.Length / k : s.Length / k + 1;
+            var res = new string[len];
+            var index = 0;
+            for (int i = 0; i < res.Length; i++)
+            {
+                var temp = new StringBuilder();
+                for (int j = 0; j < k; j++)
+                {
+                    if (index < s.Length)
+                        temp.Append(s[index++]);
+                    else
+                        temp.Append(fill);
+                }
+                res[i] = temp.ToString();
+            }
+            return res;
+        }
+    }
+}
