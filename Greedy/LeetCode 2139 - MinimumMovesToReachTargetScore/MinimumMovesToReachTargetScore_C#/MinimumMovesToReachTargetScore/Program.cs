ccaba298fc720029bdaa0bd465f76d0e745bb0e3
Xing <x.xing.unsw@gmail.com>
2022-01-16 15:48:54 +1100

    init

---
+++ Greedy/LeetCode 2139 - MinimumMovesToReachTargetScore/MinimumMovesToReachTargetScore_C#/MinimumMovesToReachTargetScore/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MinimumMovesToReachTargetScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinMoves(int target, int maxDoubles)
+        {
+            var res = 0;
+            while (target != 1 && maxDoubles != 0)
+            {
+                if (target % 2 == 0)
+                {
+                    target /= 2;
+                    maxDoubles--;
+                }
+                else
+                    target--;
+                res++;
+            }
+            return res + (target - 1);
+        }
+    }
+}
