15a0b8cfa95c38650664204138bcfaa313e11b50
Xing <x.xing.unsw@gmail.com>
2022-01-21 13:31:09 +1100

    one more solution

--- Design/LeetCode 355 - DesignTwitter/DesignTwitter_C#/DesignTwitter/Program.cs
+++ Design/LeetCode 355 - DesignTwitter/DesignTwitter_C#/DesignTwitter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DesignTwitter
 {
@@ -9,25 +10,19 @@ namespace DesignTwitter
         {
             var twitter = new Twitter();
             twitter.PostTweet(1, 5);
-            twitter.PostTweet(1, 3);
-            twitter.PostTweet(1, 101);
-            twitter.PostTweet(1, 13);
-            twitter.PostTweet(1, 10);
-            twitter.PostTweet(1, 2);
-            twitter.PostTweet(1, 94);
-            twitter.PostTweet(1, 505);
-            twitter.PostTweet(1, 333);
-            twitter.PostTweet(1, 22);
-            twitter.PostTweet(1, 11);
             twitter.GetNewsFeed(1);
-
+            twitter.Follow(1, 2);
+            twitter.PostTweet(2, 6);
+            twitter.GetNewsFeed(1);
+            twitter.Unfollow(1, 2);
+            twitter.GetNewsFeed(1);
         }
     }
-    public class Twitter
+    public class Twitter1
     {
         private Dictionary<int, HashSet<int>> follow;
         private List<int[]> messages;
-        public Twitter()
+        public Twitter1()
         {
             follow = new Dictionary<int, HashSet<int>>();
             messages = new List<int[]>();
@@ -69,5 +64,65 @@ namespace DesignTwitter
             if (follow.ContainsKey(followerId))
                 follow[followerId].Remove(followeeId);
         }
+    }
+
+    public class Twitter
+    {
+        class User
+        {
+            public List<Message> messages;
+            public Dictionary<int, User> followees;
+            public User()
+            {
+                messages = new List<Message>();
+                followees = new Dictionary<int, User>();
+            }
+        }
+
+        class Message
+        {
+            public int id;
+            public int timestamp;
+
+            public Message(int id, int timestamp)
+            {
+                this.id = id;
+                this.timestamp = timestamp;
+            }
+        }
+
+        private Dictionary<int, User> _users;
+        private int _timestamp;
+
+        public Twitter()
+        {
+            _users = new Dictionary<int, User>();
+            _timestamp = 0;
+        }
+
+        public void PostTweet(int userId, int tweetId)
+        {
+            if (!_users.ContainsKey(userId)) _users[userId] = new User();
+            _users[userId].messages.Add(new Message(tweetId, _timestamp++));
+        }
+
+        public IList<int> GetNewsFeed(int userId)
+        {
+            if (!_users.ContainsKey(userId)) return new List<int>();
+            var msgs = new List<Message>();
+            var user = _users[userId];
+            msgs.AddRange(user.messages);
+            msgs.AddRange(user.followees.Values.SelectMany(x => x.messages));
+            return msgs.OrderByDescending(x => x.timestamp).Take(10).Select(x => x.id).ToList();
+        }
+
+        public void Follow(int followerId, int followeeId)
+        {
+            if (!_users.ContainsKey(followerId)) _users[followerId] = new User();
+            if (!_users.ContainsKey(followeeId)) _users[followeeId] = new User();
+            _users[followerId].followees.TryAdd(followeeId, _users[followeeId]);
+        }
+
+        public void Unfollow(int followerId, int followeeId) => _users[followerId].followees.Remove(followeeId);
     }
 }
