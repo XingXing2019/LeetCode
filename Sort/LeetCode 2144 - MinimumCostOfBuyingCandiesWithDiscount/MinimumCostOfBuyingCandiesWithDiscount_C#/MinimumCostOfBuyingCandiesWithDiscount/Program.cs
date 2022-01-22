63f162f4f88d41a652a53281a4293e99fa647ca2
Xing <x.xing.unsw@gmail.com>
2022-01-23 09:39:45 +1100

    init

---
+++ Sort/LeetCode 2144 - MinimumCostOfBuyingCandiesWithDiscount/MinimumCostOfBuyingCandiesWithDiscount_C#/MinimumCostOfBuyingCandiesWithDiscount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MinimumCostOfBuyingCandiesWithDiscount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinimumCost(int[] cost)
+        {
+            Array.Sort(cost, (a, b) => b - a);
+            int count = 0, res = 0;
+            for (int i = 0; i < cost.Length; i++)
+            {
+                count++;
+                if (count % 3 == 0) continue;
+                res += cost[i];
+            }
+            return res;
+        }
+    }
+}
