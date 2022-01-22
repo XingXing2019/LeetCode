c61f352adcea4521871bdfd7be6a7898aea6b7bd
Xing <x.xing.unsw@gmail.com>
2022-01-23 10:16:35 +1100

    init

---
+++ Array/LeetCode 2145 - CountTheHiddenSequences/CountTheHiddenSequences_C#/CountTheHiddenSequences/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CountTheHiddenSequences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int NumberOfArrays(int[] differences, int lower, int upper)
+        {
+            int min = 0, max = 0, cur = 0;
+            foreach (var difference in differences)
+            {
+                cur += difference;
+                min = Math.Min(min, cur);
+                max = Math.Max(max, cur);
+            }
+            return Math.Max(0, Math.Min(upper - min, upper - max) - Math.Max(lower - min, lower - max) + 1);
+        }
+    }
+}
