ac3b40419f3220740ce884dc244b7c3dcd9c0573
Xing <x.xing.unsw@gmail.com>
2022-01-23 16:51:51 +1100

    init

---
+++ HashTable/LeetCode 2148 - CountElementsWithStrictlySmallerAndGreaterElements/CountElements_C#/CountElements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace CountElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountElements(int[] nums)
+        {
+            var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var ordered = freq.Keys.OrderBy(x => x).ToList();
+            var res = 0;
+            for (int i = 1; i < ordered.Count - 1; i++)
+            {
+                res += freq[ordered[i]];
+            }
+            return res;
+        }
+    }
+}
