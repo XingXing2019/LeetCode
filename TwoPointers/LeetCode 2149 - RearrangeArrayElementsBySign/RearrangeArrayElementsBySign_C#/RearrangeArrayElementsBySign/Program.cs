bd23abbfa39854271e106bab3d077fc08f95c4a4
Xing <x.xing.unsw@gmail.com>
2022-01-23 16:57:14 +1100

    init

---
+++ TwoPointers/LeetCode 2149 - RearrangeArrayElementsBySign/RearrangeArrayElementsBySign_C#/RearrangeArrayElementsBySign/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RearrangeArrayElementsBySign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] RearrangeArray(int[] nums)
+        {
+            var negative = new List<int>();
+            var positive = new List<int>();
+            foreach (var num in nums)
+            {
+                if (num < 0)
+                    negative.Add(num);
+                else
+                    positive.Add(num);
+            }
+            int p1 = 0, p2 = 0, p = 0;
+            var res = new int[nums.Length];
+            while (p < res.Length)
+            {
+                res[p++] = positive[p1++];
+                res[p++] = negative[p2++];
+            }
+            return res;
+        }
+    }
+}
