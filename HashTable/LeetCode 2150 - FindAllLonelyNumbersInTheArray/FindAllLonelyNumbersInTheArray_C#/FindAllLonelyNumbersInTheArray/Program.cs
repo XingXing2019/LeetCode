084c736b8c233fe64e17dcf4c9c06e472c0fcc93
Xing <x.xing.unsw@gmail.com>
2022-01-23 17:02:48 +1100

    init

---
+++ HashTable/LeetCode 2150 - FindAllLonelyNumbersInTheArray/FindAllLonelyNumbersInTheArray_C#/FindAllLonelyNumbersInTheArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindAllLonelyNumbersInTheArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IList<int> FindLonely(int[] nums)
+        {
+            var dict = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            return dict.Where(x => x.Value == 1 && !dict.ContainsKey(x.Key + 1) && !dict.ContainsKey(x.Key - 1)).Select(x => x.Key).ToList();
+        }
+    }
+}
