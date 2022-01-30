204322f76a057c2e4a43821bf8ab281982276133
Xing <x.xing.unsw@gmail.com>
2022-01-30 16:49:25 +1100

    init

---
+++ String/LeetCode 2156 - FindSubstringWithGivenHashValue/FindSubstringWithGivenHashValue/FindSubstringWithGivenHashValue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FindSubstringWithGivenHashValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "fbxd";
+            int power = 31, module = 100, k = 3, hashValue = 32;
+            Console.WriteLine(SubStrHash(s, power, module, k, hashValue));
+        }
+        public static string SubStrHash(string s, int power, int modulo, int k, int hashValue)
+        {
+            long cur = 0, pk = 1;
+            int res = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                cur = (cur * power + s[i] - 'a' + 1) % modulo;
+                if (i + k >= s.Length)
+                    pk = pk * power % modulo;
+                else
+                    cur = (cur - (s[i + k] - 'a' + 1) * pk % modulo + modulo) % modulo;
+                if (cur == hashValue)
+                    res = i;
+            }
+            return s.Substring(res, k);
+        }
+    }
+}
