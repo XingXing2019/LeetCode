d888fddfa788b1ebdcf268a5393460f18135e249
Xing <x.xing.unsw@gmail.com>
2022-01-30 15:18:31 +1100

    init

---
+++ HashTable/LeetCode 2154 - KeepMultiplyingFoundValuesByTwo/KeepMultiplyingFoundValuesByTwo_C#/KeepMultiplyingFoundValuesByTwo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeepMultiplyingFoundValuesByTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int FindFinalValue(int[] nums, int original)
+        {
+            var set = new HashSet<int>(nums);
+            while (set.Contains(original))
+                original *= 2;
+            return original;
+        }
+    }
+}
