08edc15cc5317e9460bd89c32da8d9a339d1ad10
Xing <x.xing.unsw@gmail.com>
2022-02-06 10:39:03 +1100

    init

---
+++ Greedy/LeetCode 2160 - MinimumSumOfFourDigitNumberAfterSplittingDigits/MinimumSum_C#/MinimumSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MinimumSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinimumSum(int num)
+        {
+            var digits = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                digits[i] = num % 10;
+                num /= 10;
+            }
+            Array.Sort(digits);
+            return digits[0] * 10 + digits[2] + digits[1] * 10 + digits[3];
+        }
+    }
+}
