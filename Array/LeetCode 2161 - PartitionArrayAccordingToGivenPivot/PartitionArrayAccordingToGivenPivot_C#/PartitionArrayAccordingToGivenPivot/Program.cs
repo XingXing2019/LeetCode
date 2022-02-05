1a1950abecd5c318901d4dceaef1d91b1bb7aa99
Xing <x.xing.unsw@gmail.com>
2022-02-06 10:48:34 +1100

    init

---
+++ Array/LeetCode 2161 - PartitionArrayAccordingToGivenPivot/PartitionArrayAccordingToGivenPivot_C#/PartitionArrayAccordingToGivenPivot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PartitionArrayAccordingToGivenPivot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] PivotArray(int[] nums, int pivot)
+        {
+            var less = new List<int>();
+            var greater = new List<int>();
+            var equal = new List<int>();
+            foreach (var num in nums)
+            {
+                if (num < pivot)
+                    less.Add(num);
+                else if (num > pivot)
+                    greater.Add(num);
+                else 
+                    equal.Add(num);
+            }
+            less.AddRange(equal);
+            less.AddRange(greater);
+            return less.ToArray();
+        }
+    }
+}
