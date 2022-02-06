93fdbda18873b5e97d5d1a95eee22ee5eb8d5ca8
Xing <x.xing.unsw@gmail.com>
2022-02-06 15:12:49 +1100

    init

---
+++ Sort/LeetCode 2164 - SortEvenAndOddIndicesIndependently/SortEvenAndOddIndicesIndependently_C#/SortEvenAndOddIndicesIndependently/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortEvenAndOddIndicesIndependently
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] SortEvenOdd(int[] nums)
+        {
+            var odd = new List<int>();
+            var even = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                    even.Add(nums[i]);
+                else
+                    odd.Add(nums[i]);
+            }
+            even.Sort();
+            odd.Sort((a, b) => b - a);
+            int p1 = 0, p2 = 0;
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] = i % 2 == 0 ? even[p1++] : odd[p2++];
+            return nums;
+        }
+    }
+}
