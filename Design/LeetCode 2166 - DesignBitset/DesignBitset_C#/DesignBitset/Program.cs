59e6a024ee2a496dbbafafa2732727d7ce77300b
Xing <x.xing.unsw@gmail.com>
2022-02-06 19:07:09 +1100

    init

---
+++ Design/LeetCode 2166 - DesignBitset/DesignBitset_C#/DesignBitset/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace DesignBitset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Bitset
+    {
+        private int[] bits;
+        private int count;
+        private bool isFlip;
+        public Bitset(int size)
+        {
+            bits = new int[size];
+        }
+
+        public void Fix(int idx)
+        {
+            if (!isFlip && bits[idx] == 0 || isFlip && bits[idx] == 1)
+                count++;
+            bits[idx] = isFlip ? 0 : 1;
+        }
+
+        public void Unfix(int idx)
+        {
+            if (!isFlip && bits[idx] == 1 || isFlip && bits[idx] == 0)
+                count--;
+            bits[idx] = isFlip ? 1 : 0;
+        }
+
+        public void Flip()
+        {
+            isFlip = !isFlip;
+            count = bits.Length - count;
+        }
+
+        public bool All() => count == bits.Length;
+
+        public bool One() => count != 0;
+
+        public int Count() => count;
+
+        public string ToString() => string.Join("", bits.Select(x => isFlip ? x ^ 1 : x));
+    }
+}
