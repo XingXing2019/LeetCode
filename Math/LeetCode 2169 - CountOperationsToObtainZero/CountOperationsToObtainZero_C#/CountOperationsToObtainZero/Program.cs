8e286ae7ad23a5820396f9b50711c1a376b8eba8
Xing <x.xing.unsw@gmail.com>
2022-02-13 15:10:25 +1100

    init

---
+++ Math/LeetCode 2169 - CountOperationsToObtainZero/CountOperationsToObtainZero_C#/CountOperationsToObtainZero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CountOperationsToObtainZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountOperations(int num1, int num2)
+        {
+            var res = 0;
+            while (num1 != 0 && num2 != 0)
+            {
+                if (num1 >= num2)
+                    num1 -= num2;
+                else
+                    num2 -= num1;
+                res++;
+            }
+            return res;
+        }
+    }
+}
