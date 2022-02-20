b626a7dd019b96eb69faabfbe3cda9ab555400a9
Xing <x.xing.unsw@gmail.com>
2022-02-20 13:55:40 +1100

    init

---
+++ Math/LeetCode 2177 - FindThreeConsecutiveIntegersThatSumToAGivenNumber/FindThreeConsecutiveIntegers_C#/FindThreeConsecutiveIntegers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FindThreeConsecutiveIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long[] SumOfThree(long num)
+        {
+            return num % 3 != 0
+                ? new long[0]
+                : new long[] { num / 3 - 1, num / 3, num / 3 + 1 };
+        }
+    }
+}
