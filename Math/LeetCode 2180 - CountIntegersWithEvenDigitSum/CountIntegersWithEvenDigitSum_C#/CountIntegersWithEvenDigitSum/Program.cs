d8ab99b1b1a69a29d18511bb4aaaa31c95fac626
Xing <x.xing.unsw@gmail.com>
2022-02-20 17:42:34 +1100

    init

---
+++ Math/LeetCode 2180 - CountIntegersWithEvenDigitSum/CountIntegersWithEvenDigitSum_C#/CountIntegersWithEvenDigitSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CountIntegersWithEvenDigitSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountEven(int num)
+        {
+            var res = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (IsValid(i))
+                    res++;
+            }
+            return res;
+        }
+
+        public bool IsValid(int num)
+        {
+            var sum = 0;
+            while (num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum % 2 == 0;
+        }
+    }
+}
