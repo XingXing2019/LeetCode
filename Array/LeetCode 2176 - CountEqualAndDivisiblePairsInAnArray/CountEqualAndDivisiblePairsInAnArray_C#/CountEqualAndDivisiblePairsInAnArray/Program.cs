7ef65c17c78e0143adfa659502fbd62cb65e8c23
Xing <x.xing.unsw@gmail.com>
2022-02-20 13:46:01 +1100

    init

---
+++ Array/LeetCode 2176 - CountEqualAndDivisiblePairsInAnArray/CountEqualAndDivisiblePairsInAnArray_C#/CountEqualAndDivisiblePairsInAnArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CountEqualAndDivisiblePairsInAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountPairs(int[] nums, int k)
+        {
+            var res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] != nums[j] || i * j % k != 0) continue;
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
