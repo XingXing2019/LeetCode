d23c333ab8f9d16e76ba8208b3ae8a8ba8b06751
Xing <x.xing.unsw@gmail.com>
2022-02-20 17:36:06 +1100

    init

---
+++ Greedy/LeetCode 2178 - MaximumSplitOfPositiveEvenIntegers/MaximumSplitOfPositiveEvenIntegers/MaximumSplitOfPositiveEvenIntegers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace MaximumSplitOfPositiveEvenIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long finalSum = 12;
+            Console.WriteLine(MaximumEvenSplit(finalSum));
+        }
+
+        public static IList<long> MaximumEvenSplit(long finalSum)
+        {
+            if (finalSum % 2 != 0) return new List<long>();
+            var num = 2;
+            var res = new List<long>();
+            while (finalSum - num > num)
+            {
+                res.Add(num);
+                finalSum -= num;
+                num += 2;
+            }
+            res.Add(finalSum);
+            return res;
+        }
+    }
+}
