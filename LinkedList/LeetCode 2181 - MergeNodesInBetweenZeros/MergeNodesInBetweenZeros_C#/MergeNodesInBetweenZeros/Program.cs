ff035a042bdada279293101e38dfed1f0fdd00cc
Xing <x.xing.unsw@gmail.com>
2022-02-20 15:41:28 +1100

    init

---
+++ LinkedList/LeetCode 2181 - MergeNodesInBetweenZeros/MergeNodesInBetweenZeros_C#/MergeNodesInBetweenZeros/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MergeNodesInBetweenZeros
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var head = Generate(new[] { 0, 3, 1, 0, 4, 5, 2, 0 });
+            Console.WriteLine(MergeNodes(head));
+        }
+        public static ListNode MergeNodes(ListNode head)
+        {
+            ListNode p1 = head, dummy = new ListNode(), p2 = dummy;
+            head = head.next;
+            while (head != null)
+            {
+                while (head != null && head.val != 0)
+                {
+                    p1.val += head.val;
+                    head = head.next;
+                }
+                p2.next = new ListNode(p1.val);
+                p2 = p2.next;
+                p1 = head;
+                head = head.next;
+            }
+            return dummy.next;
+        }
+
+        public static ListNode Generate(int[] nums)
+        {
+            ListNode res = null;
+            for (int i = nums.Length - 1; i >= 0; i--)
+                res = new ListNode(nums[i], res);
+            return res;
+        }
+    }
+}
