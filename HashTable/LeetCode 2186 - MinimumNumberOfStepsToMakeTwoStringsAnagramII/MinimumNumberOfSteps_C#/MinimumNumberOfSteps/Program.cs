dc2c1bc5f1e04d0d58600bc50d68b04a19570036
Xing <x.xing.unsw@gmail.com>
2022-02-27 20:25:47 +1100

    init

---
+++ HashTable/LeetCode 2186 - MinimumNumberOfStepsToMakeTwoStringsAnagramII/MinimumNumberOfSteps_C#/MinimumNumberOfSteps/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MinimumNumberOfSteps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinSteps(string s, string t)
+        {
+            var letters1 = new int[26];
+            var letters2 = new int[26];
+            foreach (var letter in s)
+                letters1[letter - 'a']++;
+            foreach (var letter in t)
+                letters2[letter - 'a']++;
+            var res = 0;
+            for (int i = 0; i < 26; i++)
+                res += Math.Abs(letters1[i] - letters2[i]);
+            return res;
+        }
+    }
+}
