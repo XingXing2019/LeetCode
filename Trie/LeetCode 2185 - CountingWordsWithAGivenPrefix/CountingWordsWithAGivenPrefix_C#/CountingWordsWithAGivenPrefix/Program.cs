0f7d732876b852e904d2c0113ef2f580ad570ef6
Xing <x.xing.unsw@gmail.com>
2022-02-27 15:57:55 +1100

    init

---
+++ Trie/LeetCode 2185 - CountingWordsWithAGivenPrefix/CountingWordsWithAGivenPrefix_C#/CountingWordsWithAGivenPrefix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CountingWordsWithAGivenPrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class TrieNode
+        {
+            public TrieNode[] children;
+            public int count;
+
+            public TrieNode()
+            {
+                children = new TrieNode[26];
+            }
+        }
+        public int PrefixCount(string[] words, string pref)
+        {
+            var root = new TrieNode();
+            foreach (var word in words)
+            {
+                var point = root;
+                foreach (var letter in word)
+                {
+                    if (point.children[letter - 'a'] == null)
+                        point.children[letter - 'a'] = new TrieNode();
+                    point = point.children[letter - 'a'];
+                    point.count++;
+                }
+            }
+            foreach (var letter in pref)
+            {
+                if (root.children[letter - 'a'] == null)
+                    return 0;
+                root = root.children[letter - 'a'];
+            }
+            return root.count;
+        }
+    }
+}
