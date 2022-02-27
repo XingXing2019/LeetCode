a32c258f076a004e92aae3800ff6ace4f9853951
Xing <x.xing.unsw@gmail.com>
2022-02-27 20:25:14 +1100

    iint

---
+++ BinarySearch/LeetCode 2187 - MinimumTimeToCompleteTrips/MinimumTimeToCompleteTrips_C#/MinimumTimeToCompleteTrips/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumTimeToCompleteTrips
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] time = { 1, 2, 3 };
+            int totalTrips = 5;
+            Console.WriteLine(MinimumTime(time, totalTrips));
+        }
+
+        public static long MinimumTime(int[] time, int totalTrips)
+        {
+            long li = 0, hi = (long) totalTrips * time.Max();
+            while (li < hi)
+            {
+                var mid = li + (hi - li) / 2;
+                var trips = time.Sum(t => mid / t);
+                if (trips >= totalTrips)
+                    hi = mid;
+                else
+                    li = mid + 1;
+            }
+            return li;
+        }
+    }
+}
