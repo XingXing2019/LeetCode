4ee99bde9ca41bdc345522d6031e52b284771f80
Xing <x.xing.unsw@gmail.com>
2022-02-28 20:13:53 +1100

    init

---
+++ Backtracking/LeetCode 1601 - MaximumNumberOfAchievableTransferRequests/MaximumNumber_C#/MaximumNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int[][] requests = new int[][]
+            {
+                new[] { 2, 1 },
+                new[] { 0, 1 },
+                new[] { 0, 1 },
+            };
+            Console.WriteLine(MaximumRequests(n, requests));
+        }
+
+        public static int MaximumRequests(int n, int[][] requests)
+        {
+            var res = 0;
+            DFS(requests, 0, 0, 0, new int[n], ref res);
+            return res;
+        }
+
+        public static void DFS(int[][] requests, int start, int count, int mask, int[] record, ref int res)
+        {
+            if (mask == 0 && record.All(x => x == 0))
+                res = Math.Max(res, count);
+            for (int i = start; i < requests.Length; i++)
+            {
+                record[requests[i][0]]--;
+                record[requests[i][1]]++;
+                DFS(requests, i + 1, count + 1, mask - (1 << requests[i][0]) + (1 << requests[i][1]), record, ref res);
+                record[requests[i][0]]++;
+                record[requests[i][1]]--;
+            }
+        }
+    }
+}
