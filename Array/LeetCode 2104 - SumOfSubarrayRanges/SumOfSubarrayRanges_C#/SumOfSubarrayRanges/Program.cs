eedea3286a91c06cf907615f2709242efa913559
Xing <x.xing.unsw@gmail.com>
2022-03-04 16:55:41 +1100

    init

---
+++ Array/LeetCode 2104 - SumOfSubarrayRanges/SumOfSubarrayRanges_C#/SumOfSubarrayRanges/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfSubarrayRanges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public long SubArrayRanges(int[] nums)
+        {
+            long res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int min = nums[i], max = nums[i];
+                for (int j = i; j < nums.Length; j++)
+                {
+                    min = Math.Min(min, nums[j]);
+                    max = Math.Max(max, nums[j]);
+                    res += max - min;
+                }
+            }
+            return res;
+        }
+    }
+}
