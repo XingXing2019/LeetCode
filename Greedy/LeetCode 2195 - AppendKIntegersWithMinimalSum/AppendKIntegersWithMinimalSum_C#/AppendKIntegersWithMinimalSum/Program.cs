6245469e181fcc56878e74249b26367b4f9d3b42
Xing <x.xing.unsw@gmail.com>
2022-03-06 17:41:35 +1100

    init

---
+++ Greedy/LeetCode 2195 - AppendKIntegersWithMinimalSum/AppendKIntegersWithMinimalSum_C#/AppendKIntegersWithMinimalSum/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+
+namespace AppendKIntegersWithMinimalSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 6 };
+            int k = 6;
+            Console.WriteLine(MinimalKSum(nums, k));
+        }
+        public static long MinimalKSum(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            long total = k;
+            var count = Math.Min(total, nums[0] - 1);
+            var res = (1 + count) * count / 2;
+            total -= count;
+            for (int i = 1; i < nums.Length && total > 0; i++)
+            {
+                if (nums[i] == nums[i - 1]) continue;
+                count = Math.Min(total, nums[i] - nums[i - 1] - 1);
+                res += (nums[i - 1] + 1 + nums[i - 1] + count) * count / 2;
+                total -= count;
+            }
+            res += (nums[^1] + 1 + nums[^1] + total) * total / 2;
+            return res;
+        }
+    }
+}
