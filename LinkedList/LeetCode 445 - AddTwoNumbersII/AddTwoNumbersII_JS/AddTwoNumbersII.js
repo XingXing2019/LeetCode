3ef77c040232bd26594a2ffac1398fe136121dc2
Xing <x.xing.unsw@gmail.com>
2022-03-07 20:46:06 +1100

    JSSolution

---
+++ LinkedList/LeetCode 445 - AddTwoNumbersII/AddTwoNumbersII_JS/AddTwoNumbersII.js
@@ -0,0 +1,37 @@
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val
+  this.next = next === undefined ? null : next
+}
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    var reverse = function (head) {
+        var dummy = new ListNode(0, head)
+        while (head && head.next) {
+            var next = head.next
+            head.next = next.next
+            next.next = dummy.next
+            dummy.next = next
+        }
+        return dummy.next
+    }
+    l1 = reverse(l1)
+    l2 = reverse(l2)
+    var car = 0, cur = 0
+    var dummy = new ListNode(), point = dummy
+    while (l1 && l2) {
+        cur = (l1.val + l2.val + car) % 10
+        car = ~~((l1.val + l2.val + car) / 10)
+        point.next = new ListNode(cur)
+        point = point.next
+        l1 = l1.next
+        l2 = l2.next
+        if (!l1 && l2) l1 = new ListNode()
+        if (l1 && !l2) l2 = new ListNode()
+    }
+    if (car) point.next = new ListNode(car)
+    return reverse(dummy.next)
+}
