33766b9305e11b4440ad6b57aa83e3d93f957041
Xing <x.xing.unsw@gmail.com>
2022-03-13 21:13:53 +1100

    init

---
+++ BitManipulation/LeetCode 393 - UTF8Validation/UTF8Validation_C#/UTF8Validation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UTF8Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 250, 145, 145, 145, 145 };
+            Console.WriteLine(ValidUtf8(data));
+        }
+
+        public static bool ValidUtf8(int[] data)
+        {
+            var index = 0;
+            while (index < data.Length)
+            {
+                int mask = 255, num = data[index], count = 8;
+                while (mask != num)
+                {
+                    count--;
+                    mask >>= 1;
+                    num >>= 1;
+                }
+                if (num == 1 || count > 4) return false;
+                index++;
+                for (int i = 0; i < count - 1; i++)
+                {
+                    if (index == data.Length || data[index] >> 6 != 2)
+                        return false;
+                    index++;
+                }
+            }
+            return true;
+        }
+    }
+}
