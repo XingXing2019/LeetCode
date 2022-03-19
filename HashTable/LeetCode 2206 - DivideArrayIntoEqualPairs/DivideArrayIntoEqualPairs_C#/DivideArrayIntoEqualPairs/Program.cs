6a527f05771341efb20422c6dfe1c697714596d3
Xing <x.xing.unsw@gmail.com>
2022-03-20 09:02:14 +1100

    init

---
+++ HashTable/LeetCode 2206 - DivideArrayIntoEqualPairs/DivideArrayIntoEqualPairs_C#/DivideArrayIntoEqualPairs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace DivideArrayIntoEqualPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool DivideArray(int[] nums)
+        {
+            return nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count()).All(x => x.Value % 2 == 0);
+        }
+    }
+}
