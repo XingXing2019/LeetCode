4b8a93da1cbed63f41b18b50441ea017ad5c6040
Xing <x.xing.unsw@gmail.com>
2022-03-19 14:52:10 +1100

    MySQL

--- Database/LeetCode 2205 - TheNumberOfUsersThatAreEligibleForDiscount/TheNumberOfUsersThatAreEligibleForDiscount_MySQL.sql
+++ Database/LeetCode 2205 - TheNumberOfUsersThatAreEligibleForDiscount/TheNumberOfUsersThatAreEligibleForDiscount_MySQL.sql
@@ -2,8 +2,8 @@ CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS 
 BEGIN
   RETURN (
 	select count(distinct user_id) as user_cnt
-    from Purchases
-    where time_stamp between startDate and endDate
-    and amount >= minAmount
+	from Purchases
+	where time_stamp between startDate and endDate
+	and amount >= minAmount
   );
 END
--- Database/LeetCode 2205 - TheNumberOfUsersThatAreEligibleForDiscount/TheNumberOfUsersThatAreEligibleForDiscount_SQLServer.sql
+++ Database/LeetCode 2205 - TheNumberOfUsersThatAreEligibleForDiscount/TheNumberOfUsersThatAreEligibleForDiscount_SQLServer.sql
@@ -1,7 +1,7 @@
 CREATE FUNCTION getUserIDs(@startDate DATE, @endDate DATE, @minAmount INT) RETURNS INT AS
 BEGIN
     RETURN (
-        SELECT COUNT(DISTINCT user_id) AS user_cnt
+		SELECT COUNT(DISTINCT user_id) AS user_cnt
 		FROM Purchases
 		WHERE time_stamp BETWEEN @startDate AND @endDate
 		AND amount >= @minAmount        
