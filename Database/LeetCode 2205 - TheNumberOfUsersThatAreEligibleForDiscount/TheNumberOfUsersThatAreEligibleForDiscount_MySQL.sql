4339dac9f1acf88cad5678b5f990c161145f3667
Xing <x.xing.unsw@gmail.com>
2022-03-19 14:50:39 +1100

    MySQL

---
+++ Database/LeetCode 2205 - TheNumberOfUsersThatAreEligibleForDiscount/TheNumberOfUsersThatAreEligibleForDiscount_MySQL.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT
+BEGIN
+  RETURN (
+	select count(distinct user_id) as user_cnt
+    from Purchases
+    where time_stamp between startDate and endDate
+    and amount >= minAmount
+  );
+END
