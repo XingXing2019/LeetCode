00790ca2bc9db74a48ed4d29b870115f4b923694
Xing <x.xing.unsw@gmail.com>
2022-03-20 15:24:44 +1100

    init

---
+++ Array/LeetCode 2211 - CountCollisionsOnARoad/CountCollisionsOnARoad_C#/CountCollisionsOnARoad/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace CountCollisionsOnARoad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountCollisions(string directions)
+        {
+            var block = false;
+            var collisions = new bool[directions.Length];
+            var res = 0;
+            for (int i = 0; i < directions.Length; i++)
+            {
+                if (directions[i] == 'R' || directions[i] == 'S')
+                    block = true;
+                else
+                    collisions[i] = block;
+            }
+            block = false;
+            for (int i = directions.Length - 1; i >= 0; i--)
+            {
+                if (directions[i] == 'L' || directions[i] == 'S')
+                    block = true;
+                else
+                    collisions[i] = block;
+            }
+            return collisions.Count(x => x);
+        }
+    }
+}
