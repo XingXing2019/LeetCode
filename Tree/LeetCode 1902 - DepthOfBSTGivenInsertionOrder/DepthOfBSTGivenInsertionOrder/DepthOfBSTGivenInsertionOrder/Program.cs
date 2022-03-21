93f009e818273cbee92e7c113d3e2db4d5fe81e9
Xing <x.xing.unsw@gmail.com>
2022-03-21 21:15:21 +1100

    init

--- Tree/LeetCode 1902 - DepthOfBSTGivenInsertionOrder/DepthOfBSTGivenInsertionOrder/DepthOfBSTGivenInsertionOrder/Program.cs
+++ Tree/LeetCode 1902 - DepthOfBSTGivenInsertionOrder/DepthOfBSTGivenInsertionOrder/DepthOfBSTGivenInsertionOrder/Program.cs
@@ -7,60 +7,42 @@ namespace DepthOfBSTGivenInsertionOrder
     {
         static void Main(string[] args)
         {
-            int[] order = { 1, 2, 3, 4, 7, 6, 5, 8, 9 };
+            int[] order = { 2, 1, 4, 3 };
             Console.WriteLine(MaxDepthBST(order));
         }
-
-        class Node
-        {
-            public bool IsLeft;
-            public int Depth;
-            public Node Parent;
-            public int Val;
-
-            public Node(bool isLeft, int depth, Node parent, int val)
-            {
-                IsLeft = isLeft;
-                Depth = depth;
-                Parent = parent;
-                Val = val;
-            }
-        }
-
+        
         public static int MaxDepthBST(int[] order)
         {
-            if (order.Length == 1) return 1;
-            var dp = new Node[order.Length];
-            dp[0] = new Node(false, 1, null, order[0]);
-            dp[1] = order[1] < order[0] ? new Node(true, 2, dp[0], order[1]) : new Node(false, 2, dp[0], order[1]);
-            for (int i = 2; i < order.Length; i++)
+            var nodes = new List<int>();
+            var depth = new Dictionary<int, int>();
+            var max = 0;
+            foreach (var node in order)
             {
-                if (dp[i - 1].IsLeft)
+                if (nodes.Count == 0)
                 {
-                    if (order[i] < order[i - 1])
-                        dp[i] = new Node(true, dp[i - 1].Depth + 1, dp[i - 1], order[i]);
-                    else
-                    {
-                        var parent = dp[i - 1];
-                        while (parent.Parent != null && order[i] > parent.Parent.Val && parent.Parent.Val > parent.Val)
-                            parent = parent.Parent;
-                        dp[i] = new Node(false, parent.Depth + 1, parent, order[i]);
-                    }
+                    depth[node] = 1;
+                    nodes.Add(node);
+                }
+                else if (node < nodes[0])
+                {
+                    depth[node] = depth[nodes[0]] + 1;
+                    nodes.Insert(0, node);
+                }
+                else if (node > nodes[^1])
+                {
+                    depth[node] = depth[nodes[^1]] + 1;
+                    nodes.Add(node);
                 }
                 else
                 {
-                    if (order[i] > order[i - 1])
-                        dp[i] = new Node(false, dp[i - 1].Depth + 1, dp[i - 1], order[i]);
-                    else
-                    {
-                        var parent = dp[i - 1];
-                        while (parent.Parent != null && order[i] < parent.Parent.Val && parent.Parent.Val < parent.Val)
-                            parent = parent.Parent;
-                        dp[i] = new Node(true, parent.Depth + 1, parent, order[i]);
-                    }
+                    var index = nodes.BinarySearch(node);
+                    index = ~index;
+                    depth[node] = Math.Max(depth[nodes[index - 1]], depth[nodes[index]]) + 1;
+                    nodes.Insert(index, node);
                 }
+                max = Math.Max(max, depth[node]);
             }
-            return dp[^1].Depth;
+            return max;
         }
     }
 }
