79b6367e9bd0d68acc18615b9ff7c65ec76ba4ba
Xing <x.xing.unsw@gmail.com>
2022-03-24 21:06:05 +1100

    init

---
+++ Greedy/LeetCode 2214 - MinimumHealthToBeatGame/MinimumHealthToBeatGame/MinimumHealthToBeatGame/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumHealthToBeatGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long MinimumHealth(int[] damage, int armor)
+        {
+            var max = damage.Max();
+            long res = 0;
+            foreach (var d in damage)
+            {
+                if (d != max || armor == 0)
+                    res += d;
+                else
+                {
+                    res += Math.Max(0, d - armor);
+                    armor = 0;
+                }
+            }
+            return res + 1;
+        }
+    }
+}
