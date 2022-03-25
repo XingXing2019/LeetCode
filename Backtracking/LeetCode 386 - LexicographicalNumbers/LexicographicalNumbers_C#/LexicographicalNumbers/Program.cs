592733985c41fdabd680ad55217147454723aeb8
Xing <x.xing.unsw@gmail.com>
2022-03-25 21:31:34 +1100

    init

---
+++ Backtracking/LeetCode 386 - LexicographicalNumbers/LexicographicalNumbers_C#/LexicographicalNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LexicographicalNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IList<int> LexicalOrder(int n)
+        {
+            var res = new List<int>();
+            for (int i = 1; i <= 9; i++)
+                DFS(n, i, res);
+            return res;
+        }
+
+        public void DFS(int n, int num, List<int> res)
+        {
+            if (num > n) return;
+            res.Add(num);
+            for (int i = 0; i < 10; i++)
+            {
+                DFS(n, num * 10 + i, res);
+            }
+        }
+    }
+}
