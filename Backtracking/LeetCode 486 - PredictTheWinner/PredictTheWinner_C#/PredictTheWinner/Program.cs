fcf527352eb2ec19c6e2741d347192546e440662
Xing <x.xing.unsw@gmail.com>
2022-03-25 21:19:26 +1100

    init

---
+++ Backtracking/LeetCode 486 - PredictTheWinner/PredictTheWinner_C#/PredictTheWinner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PredictTheWinner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 14 };
+            Console.WriteLine(PredictTheWinner(nums));
+        }
+
+        public static bool PredictTheWinner(int[] nums)
+        {
+            return DFS(nums, 0, nums.Length - 1, 0, 0, 0);
+        }
+
+        public static bool DFS(int[] nums, int li, int hi, int turn, int A, int B)
+        {
+            if (li > hi) return A >= B;
+            return turn % 2 == 0
+                ? DFS(nums, li + 1, hi, turn + 1, A + nums[li], B) || DFS(nums, li, hi - 1, turn + 1, A + nums[hi], B)
+                : DFS(nums, li + 1, hi, turn + 1, A, B + nums[li]) && DFS(nums, li, hi - 1, turn + 1, A, B + nums[hi]);
+        }
+    }
+}
