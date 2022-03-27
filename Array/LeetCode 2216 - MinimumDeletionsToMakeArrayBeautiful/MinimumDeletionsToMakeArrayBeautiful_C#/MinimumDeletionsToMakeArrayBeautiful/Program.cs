e1bee85630315f3facc5567fe0ef7e134a9fb684
Xing <x.xing.unsw@gmail.com>
2022-03-27 15:08:25 +1100

    init

---
+++ Array/LeetCode 2216 - MinimumDeletionsToMakeArrayBeautiful/MinimumDeletionsToMakeArrayBeautiful_C#/MinimumDeletionsToMakeArrayBeautiful/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MinimumDeletionsToMakeArrayBeautiful
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public int MinDeletion(int[] nums)
+        {
+            var res = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if ((i - res) % 2 == 0 && nums[i] == nums[i + 1])
+                    res++;
+            }
+            return (nums.Length - res) % 2 == 0 ? res : res + 1;
+        }
+    }
+}
