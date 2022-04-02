06dd7b9c3e991d81f6876546d6d33844469c0c28
Xing <x.xing.unsw@gmail.com>
2022-04-03 09:25:51 +1000

    init

---
+++ BitManipulation/LeetCode 2220 - MinimumBitFlipsToConvertNumber/MinimumBitFlipsToConvertNumber_C#/MinimumBitFlipsToConvertNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MinimumBitFlipsToConvertNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinBitFlips(int start, int goal)
+        {
+            var res = 0;
+            while (start != 0 && goal != 0)
+            {
+                if ((start & 1) != (goal & 1))
+                    res++;
+                start >>= 1;
+                goal >>= 1;
+            }
+            while (start != 0)
+            {
+                if ((start & 1) != 0)
+                    res++;
+                start >>= 1;
+            }
+            while (goal != 0)
+            {
+                if ((goal & 1) != 0)
+                    res++;
+                goal >>= 1;
+            }
+            return res;
+        }
+    }
+}
