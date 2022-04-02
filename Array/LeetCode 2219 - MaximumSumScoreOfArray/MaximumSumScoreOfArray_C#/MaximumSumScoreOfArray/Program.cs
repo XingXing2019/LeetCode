9d8c5f997c4bffff11647f41b47c3d2f508bd526
Xing <x.xing.unsw@gmail.com>
2022-04-02 14:49:16 +1100

    init

---
+++ Array/LeetCode 2219 - MaximumSumScoreOfArray/MaximumSumScoreOfArray_C#/MaximumSumScoreOfArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MaximumSumScoreOfArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long MaximumSumScore(int[] nums)
+        {
+            var prefix = new long[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+                prefix[i] = i == 0 ? nums[i] : nums[i] + prefix[i - 1];
+            var res = long.MinValue;
+            for (int i = 0; i < prefix.Length; i++)
+            {
+                var front = prefix[i];
+                var after = i == 0 ? prefix[^1] : prefix[^1] - prefix[i - 1];
+                res = Math.Max(res, Math.Max(front, after));
+            }
+            return res;
+        }
+    }
+}
