70fb744d5617fb74700a7710778bec4d9465fd60
Xing <x.xing.unsw@gmail.com>
2022-04-03 09:31:23 +1000

    init

---
+++ Recursion/LeetCode 2221 - FindTriangularSumOfAnArray/FindTriangularSumOfAnArray_C#/FindTriangularSumOfAnArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindTriangularSumOfAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int TriangularSum(int[] nums)
+        {
+            if (nums.Length == 1) return nums[0];
+            var newNums = new int[nums.Length - 1];
+            for (int i = 0; i < newNums.Length; i++)
+                newNums[i] = (nums[i] + nums[i + 1]) % 10;
+            return TriangularSum(newNums);
+        }
+    }
+}
