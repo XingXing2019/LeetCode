833b5dad38926053a2a5764f0279b67c16303c88
Xing <x.xing.unsw@gmail.com>
2022-04-03 17:17:09 +1000

    init

---
+++ BinarySearch/LeetCode 2226 - MaximumCandiesAllocatedToKChildren/MaximumCandiesAllocatedToKChildren/MaximumCandiesAllocatedToKChildren/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumCandiesAllocatedToKChildren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaximumCandies(int[] candies, long k)
+        {
+            long li = 0, hi = candies.Max();
+            while (li <= hi)
+            {
+                var mid = li + (hi - li) / 2;
+                var count = Count(candies, mid);
+                if (count >= k)
+                    li = mid + 1;
+                else
+                    hi = mid - 1;
+            }
+            return Math.Max(0, (int) hi);
+        }
+
+        public long Count(int[] candies, long target)
+        {
+            if (target == 0) return long.MaxValue;
+            long res = 0;
+            foreach (var candy in candies)
+                res += candy / target;
+            return res;
+        }
+    }
+}
