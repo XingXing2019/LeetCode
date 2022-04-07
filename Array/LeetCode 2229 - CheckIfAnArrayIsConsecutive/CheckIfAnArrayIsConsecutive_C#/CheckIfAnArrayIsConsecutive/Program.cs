468bf043ae12f3f73cfb85a35028626d23290e9c
Xing <x.xing.unsw@gmail.com>
2022-04-07 19:47:50 +1000

    int

---
+++ Array/LeetCode 2229 - CheckIfAnArrayIsConsecutive/CheckIfAnArrayIsConsecutive_C#/CheckIfAnArrayIsConsecutive/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool IsConsecutive(int[] nums)
+{
+    var record = new bool[nums.Length];
+    var min = nums.Min();
+    foreach (var num in nums)
+    {
+        if (num - min >= record.Length) return false;
+        if (record[num - min]) return false;
+        record[num - min] = true;
+    }
+    return record.All(x => x);
+}
