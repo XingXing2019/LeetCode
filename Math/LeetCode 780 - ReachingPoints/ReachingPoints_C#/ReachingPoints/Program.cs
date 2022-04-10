3f40915129d48b5b7d27377ae9a29c7b96206fd6
Xing <x.xing.unsw@gmail.com>
2022-04-10 10:07:17 +1000

    init

---
+++ Math/LeetCode 780 - ReachingPoints/ReachingPoints_C#/ReachingPoints/Program.cs
@@ -0,0 +1,20 @@
+﻿int sx = 9, sy = 5, tx = 12, ty = 8;
+Console.WriteLine(ReachingPoints(sx, sy, tx, ty));
+
+
+bool ReachingPoints(int sx, int sy, int tx, int ty)
+{
+    if (sx == tx && sy == ty) return true;
+    if (sx > tx || sy > ty) return false;
+    if (tx == ty) return tx == sx && ty == sy;
+    if (tx > ty)
+    {
+        var n = (tx - sx) / ty == 0 ? 1 : (tx - sx) / ty;
+        return ReachingPoints(sx, sy, tx - n * ty, ty);
+    }
+    else
+    {
+        var n = (ty - sy) / tx == 0 ? 1 : (ty - sy) / tx;
+        return ReachingPoints(sx, sy, tx, ty - n * tx);
+    }
+}
