b9da992b3b0e75f34c29e3af7fcf0ec45e06b068
Xing <x.xing.unsw@gmail.com>
2022-04-10 21:57:19 +1000

    MySQL

---
+++ Database/LeetCode 2230 - TheUsersThatAreEligibleForDiscount/TheUsersThatAreEligibleForDiscount_MySQL.sql
@@ -0,0 +1,8 @@
+create procedure getUserIDs(startDate date, endDate date, minAmount int)
+begin
+	select distinct user_id
+	from purchases
+	where time_stamp between startDate and endDate
+	and amount >= minAmount
+    order by user_id;    
+end
