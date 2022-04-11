46d0e12251d8fdc7ee63e27ac0807cbe6f7e5485
Xing <x.xing.unsw@gmail.com>
2022-04-11 21:44:59 +1000

    init

---
+++ Tree/LeetCode 2236 - RootEqualsSumOfChildren/RootEqualsSumOfChildren/RootEqualsSumOfChildren/Program.cs
@@ -0,0 +1,17 @@
+﻿bool CheckTree(TreeNode root)
+{
+    return root.left.val + root.right.val == root.val;
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
