428f521fc7907916781f4ed9e4b3c9d95641b6bb
Xing <x.xing.unsw@gmail.com>
2022-04-14 19:50:09 +1000

    init

---
+++ HashTable/LeetCode 2237 - CountPositionsOnStreetWithRequiredBrightness/CountPositions_C#/CountPositions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CountPositions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MeetRequirement(int n, int[][] lights, int[] requirement)
+        {
+            var record = new int[n + 1];
+            foreach (var light in lights)
+            {
+                var start = Math.Max(0, light[0] - light[1]);
+                var end = Math.Min(n, light[0] + light[1] + 1);
+                record[start]++;
+                record[end]--;
+            }
+            int count = 0, res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                count += record[i];
+                if (count < requirement[i]) continue;
+                res++;
+            }
+            return res;
+        }
+    }
+}
