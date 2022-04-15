164440a722fb2938092a24559c58fb20f7723056
Xing <x.xing.unsw@gmail.com>
2022-04-15 15:48:42 +1000

    init

--- Database/LeetCode 2238 - NumberOfTimesADriverWasAPassenger/NumberOfTimesADriverWasAPassenger_MySQL.sql
+++ Database/LeetCode 2238 - NumberOfTimesADriverWasAPassenger/NumberOfTimesADriverWasAPassenger_MySQL.sql
@@ -2,7 +2,7 @@ select driver_id, ifnull(cnt, 0) as cnt from
 (select distinct driver_id from rides) as d
 left join (
 	select passenger_id, count(distinct ride_id) as cnt
-    from rides
-    group by passenger_id
+	from rides
+	group by passenger_id
 ) as p
 on d.driver_id = p.passenger_id
