918b303486add40a86e54903c6e1a96c1db174d1
Xing <x.xing.unsw@gmail.com>
2022-04-17 09:17:54 +1000

    init

---
+++ Math/LeetCode 2240 - NumberOfWaysToBuyPensAndPencils/NumberOfWaysToBuyPensAndPencils/NumberOfWaysToBuyPensAndPencils/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace NumberOfWaysToBuyPensAndPencils
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public long WaysToBuyPensPencils(int total, int cost1, int cost2)
+        {
+            long res = 0;
+            for (int i = 0; i <= total / cost1; i++)
+                res += (total - i * cost1) / cost2 + 1;
+            return res;
+        }
+    }
+}
