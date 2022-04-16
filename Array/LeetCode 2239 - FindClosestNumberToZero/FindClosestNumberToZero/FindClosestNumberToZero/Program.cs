35aaef7de268565977ff56fde6d4f0cc6ec02775
Xing <x.xing.unsw@gmail.com>
2022-04-17 09:12:23 +1000

    init

---
+++ Array/LeetCode 2239 - FindClosestNumberToZero/FindClosestNumberToZero/FindClosestNumberToZero/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindClosestNumberToZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int FindClosestNumber(int[] nums)
+        {
+            var dict = new Dictionary<int, List<int>>();
+            var min = int.MaxValue;
+            foreach (var num in nums)
+            {
+                var abs = Math.Abs(num);
+                if (!dict.ContainsKey(abs))
+                    dict[abs] = new List<int>();
+                dict[abs].Add(num);
+                min = Math.Min(min, abs);
+            }
+            return dict[min].Max();
+        }
+    }
+}
