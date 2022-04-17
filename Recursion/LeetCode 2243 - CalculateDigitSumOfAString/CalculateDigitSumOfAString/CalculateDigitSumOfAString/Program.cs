cce7d0c35bed1a193ef7410c5ea5975198a64715
Xing <x.xing.unsw@gmail.com>
2022-04-17 19:53:37 +1000

    init

---
+++ Recursion/LeetCode 2243 - CalculateDigitSumOfAString/CalculateDigitSumOfAString/CalculateDigitSumOfAString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace CalculateDigitSumOfAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "1234";
+            int k = 2;
+            Console.WriteLine(DigitSum(s, k));
+        }
+
+        public static string DigitSum(string s, int k)
+        {
+            if (s.Length <= k) return s;
+            var next = new StringBuilder();
+            int sum = 0, count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                sum += s[i] - '0';
+                count++;
+                if (count != k) continue;
+                next.Append(sum.ToString());
+                count = 0;
+                sum = 0;
+            }
+            if (count != 0) next.Append(sum.ToString());
+            return DigitSum(next.ToString(), k);
+        }
+    }
+}
