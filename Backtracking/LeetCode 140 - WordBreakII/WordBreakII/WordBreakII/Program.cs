7329ecdf6fd23f4a4b3165838f3851301298b443
Xing <x.xing.unsw@gmail.com>
2022-04-19 18:07:31 +1000

    init

---
+++ Backtracking/LeetCode 140 - WordBreakII/WordBreakII/WordBreakII/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordBreakII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            var res = new List<string>();
+            DFS(s, "", new HashSet<string>(wordDict), res);
+            return res;
+        }
+
+        public void DFS(string s, string cur, HashSet<string> wordDict, List<string> res)
+        {
+            if (s.Length == 0)
+                res.Add(cur.Trim());
+            for (int i = 1; i <= s.Length; i++)
+            {
+                var word = s.Substring(0, i);
+                if (!wordDict.Contains(word)) continue;
+                DFS(s.Substring(i), $"{cur} {word}", wordDict, res);
+            }
+        }
+    }
+}
