83cfb7afa7e063578978cfc3d6ba6c6c64296260
Xing <x.xing.unsw@gmail.com>
2022-04-23 20:50:27 +1000

    LinkedList solution

--- Design/LeetCode 622 - DesignCircularQueue/DesignCircularQueue_C#/DesignCircularQueue/Program.cs
+++ Design/LeetCode 622 - DesignCircularQueue/DesignCircularQueue_C#/DesignCircularQueue/Program.cs
@@ -10,11 +10,11 @@ namespace DesignCircularQueue
             Console.WriteLine("Hello World!");
         }
     }
-    public class MyCircularQueue
+    public class MyCircularQueue1
     {
         private List<int> nums;
         private int capacity;
-        public MyCircularQueue(int k)
+        public MyCircularQueue1(int k)
         {
             nums = new List<int>();
             capacity = k;
@@ -56,4 +56,78 @@ namespace DesignCircularQueue
             return nums.Count == capacity;
         }
     }
+
+    public class MyCircularQueue
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val)
+            {
+                this.val = val;
+            }
+        }
+
+        public ListNode front;
+        public ListNode rear;
+        public int size;
+        public int k;
+        public MyCircularQueue(int k)
+        {
+            size = k;
+            this.k = k;
+        }
+
+        public bool EnQueue(int value)
+        {
+            if (size == 0) return false;
+            if (front == null)
+            {
+                front = new ListNode(value);
+                rear = front;
+            }
+            else
+            {
+                rear.next = new ListNode(value);
+                rear = rear.next;
+            }
+            size--;
+            return true;
+        }
+
+        public bool DeQueue()
+        {
+            if (front != null)
+            {
+                front = front.next;
+                if (front == null)
+                    rear = null;
+                size++;
+                return true;
+            }
+            return false;
+        }
+
+        public int Front()
+        {
+            return front == null ? -1 : front.val;
+        }
+
+        public int Rear()
+        {
+            return rear == null ? -1 : rear.val;
+        }
+
+        public bool IsEmpty()
+        {
+            return size == k;
+        }
+
+        public bool IsFull()
+        {
+            return size == 0;
+        }
+    }
+
 }
