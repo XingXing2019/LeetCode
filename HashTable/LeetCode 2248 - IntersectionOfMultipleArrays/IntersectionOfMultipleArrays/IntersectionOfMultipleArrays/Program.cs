0c59ca5b5293dc32e6b673bf6c95bcc6770932d6
Xing <x.xing.unsw@gmail.com>
2022-04-24 20:42:39 +1000

    C#

---
+++ HashTable/LeetCode 2248 - IntersectionOfMultipleArrays/IntersectionOfMultipleArrays/IntersectionOfMultipleArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+IList<int> Intersection(int[][] nums)
+{
+    var dict = new Dictionary<int, int>();
+    foreach (var num in nums)
+    {
+        foreach (var item in num)
+        {
+            if (!dict.ContainsKey(item))
+                dict[item] = 0;
+            dict[item]++;
+        }
+    }
+    return dict.Where(x => x.Value == nums.Length).Select(x => x.Key).OrderBy(x => x).ToList();
+}
