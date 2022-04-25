07dd7e4f79bf43f9fc425cbcb72abbdfe2cede59
Xing <x.xing.unsw@gmail.com>
2022-04-25 20:49:34 +1000

    init

---
+++ Database/LeetCode 2252 - DynamicPivotingOfATable/DynamicPivotingOfATable_SQLServer.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE PivotProducts AS
+BEGIN
+    DECLARE @sql_str NVARCHAR(MAX);
+	DECLARE @sql_col NVARCHAR(MAX);
+	SELECT @sql_col = ISNULL(@sql_col + ',', '') + QUOTENAME(store) FROM Products GROUP BY store ORDER BY store
+	SET @sql_str = '
+	SELECT *
+	FROM (SELECT * FROM Products) AS temp 
+	PIVOT (SUM(price) FOR store IN ('+ @sql_col +')) AS pvt 
+	ORDER BY pvt.product_id'
+
+	EXEC (@sql_str)
+END
