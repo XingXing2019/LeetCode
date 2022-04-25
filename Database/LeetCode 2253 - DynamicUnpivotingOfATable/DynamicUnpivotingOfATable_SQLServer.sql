5eb173cfb63bfd9dd3e4d453cc75d05788cf903d
Xing <x.xing.unsw@gmail.com>
2022-04-25 21:46:51 +1000

    init

---
+++ Database/LeetCode 2253 - DynamicUnpivotingOfATable/DynamicUnpivotingOfATable_SQLServer.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE UnpivotProducts AS
+BEGIN
+    DECLARE @sql_col NVARCHAR(MAX);
+    WITH Cols AS (
+        SELECT NAME FROM SYSCOLUMNS 
+        WHERE ID IN (SELECT ID FROM SYSOBJECTS WHERE NAME = 'Products')
+        AND NAME <> 'product_id'
+    )
+
+    SELECT @sql_col = ISNULL(@sql_col + ',', '') + QUOTENAME(NAME) FROM Cols GROUP BY NAME
+
+    DECLARE @sql_str NVARCHAR(MAX);
+    SET @sql_str = '
+    SELECT * FROM Products
+    UNPIVOT (
+        price FOR store IN ('+ @sql_col +')
+    ) AS unpvt'
+
+    EXEC (@sql_str)
+END
