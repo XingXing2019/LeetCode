7697f2dd024a24fa39969a96e6c4d60c078c1858
Xing <x.xing.unsw@gmail.com>
2022-05-01 08:58:43 +1000

    init

---
+++ Array/LeetCode 2256 - MinimumAverageDifference/MinimumAverageDifference/MinimumAverageDifference/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = { 2, 5, 3, 9, 5, 3 };
+Console.WriteLine(MinimumAverageDifference(nums));
+
+int MinimumAverageDifference(int[] nums)
+{
+    var res = 0;
+    long min = int.MaxValue;
+    var prefix = new long[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+        prefix[i] = i == 0 ? nums[i] : nums[i] + prefix[i - 1];
+    for (int i = 0; i < prefix.Length; i++)
+    {
+        var avg = Math.Abs(prefix[i] / (i + 1) - (i == prefix.Length - 1 ? 0 : (prefix[^1] - prefix[i]) / (prefix.Length - i - 1)));
+        if (min <= avg) continue;
+        min = avg;
+        res = i;
+    }
+    return res;
+}
