082849a0a3261c92200657f747f320becd5d5c76
Xing <x.xing.unsw@gmail.com>
2022-05-01 08:45:14 +1000

    init

---
+++ Trie/LeetCode 2255 - CountPrefixesOfAGivenString/CountPrefixesOfAGivenString/CountPrefixesOfAGivenString/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+int CountPrefixes(string[] words, string s)
+{
+    var root = new TrieNode();
+    var point = root;
+    var res = 0;
+    foreach (var l in s)
+    {
+        point.children[l - 'a'] = new TrieNode();
+        point = point.children[l - 'a'];
+    }
+    foreach (var word in words)
+    {
+        point = root;
+        var isPrefix = true;
+        foreach (var l in word)
+        {
+            if (point.children[l - 'a'] == null)
+            {
+                isPrefix = false;
+                break;
+            }
+            point = point.children[l - 'a'];
+        }
+        if (isPrefix) res++;
+    }
+    return res;
+}
+
+class TrieNode
+{
+    public TrieNode[] children;
+
+    public TrieNode()
+    {
+        children = new TrieNode[26];
+    }
+}
