f4d8e100e3aaa77b8eee33381fea63b3a079a78a
Xing <x.xing.unsw@gmail.com>
2022-05-01 15:23:00 +1000

    init

---
+++ Array/LeetCode 2261 - KDivisibleElementsSubarrays/KDivisibleElementsSubarrays/KDivisibleElementsSubarrays/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] nums = { 2, 3, 3, 2, 2 };
+int k = 2, p = 2;
+Console.WriteLine(CountDistinct(nums, k, p));
+
+int CountDistinct(int[] nums, int k, int p)
+{
+    var set = new HashSet<string>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var arr = new List<int>();
+        var count = 0;
+        for (int j = i; j < nums.Length; j++)
+        {
+            arr.Add(nums[j]);
+            if (nums[j] % p == 0) count++;
+            if (count > k) break;
+            set.Add(string.Join(":", arr));
+        }
+    }
+    return set.Count;
+}
