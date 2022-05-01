7c39356e8d2c8bff0c178361864a7c40767af6e7
Xing <x.xing.unsw@gmail.com>
2022-05-01 14:25:08 +1000

    init

---
+++ Math/LeetCode 2259 - RemoveDigitFromNumberToMaximizeResult/RemoveDigitFromNumberToMaximizeResult/RemoveDigitFromNumberToMaximizeResult/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string RemoveDigit(string number, char digit)
+{
+    var res = new string('0', number.Length - 1);
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (number[i] != digit) continue;
+        var num = number.Substring(0, i) + number.Substring(i + 1);
+        if (Compare(num, res))
+            res = num;
+    }
+    return res;
+}
+
+bool Compare(string num1, string num2)
+{
+    for (int i = 0; i < num1.Length; i++)
+    {
+        if (num1[i] > num2[i])
+            return true;
+        if (num1[i] < num2[i])
+            return false;
+    }
+    return true;
+}
