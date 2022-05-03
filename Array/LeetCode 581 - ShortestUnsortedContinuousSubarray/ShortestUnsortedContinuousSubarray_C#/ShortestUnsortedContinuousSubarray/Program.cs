cc748247ffe03493705d2c38fc5e197a8581fb13
Xing <x.xing.unsw@gmail.com>
2022-05-03 15:15:26 +1000

    simplfied code

--- Array/LeetCode 581 - ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray_C#/ShortestUnsortedContinuousSubarray/Program.cs
+++ Array/LeetCode 581 - ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray_C#/ShortestUnsortedContinuousSubarray/Program.cs
@@ -1,6 +1,7 @@
 ﻿//对原始数组排序，找到新数组与原始数组第一个和最后一个数字不同的位置。这段数组的长度即为结果。
 //需要注意验证原始数组已经是排好序的特殊情况。
 using System;
+using System.Linq;
 
 namespace ShortestUnsortedContinuousSubarray
 {
@@ -11,29 +12,15 @@ namespace ShortestUnsortedContinuousSubarray
             int[] nums = { 2, 6, 4, 8, 10, 9, 15 };
             Console.WriteLine(FindUnsortedSubarray_N(nums));
         }
-        static int FindUnsortedSubarray_LogN(int[] nums)
+        static int FindUnsortedSubarray_NLogN(int[] nums)
         {
-            int[] tem = new int[nums.Length];
-            Array.Copy(nums, tem, nums.Length);
-            Array.Sort(nums);
-            int start = 0, end = 0;
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if(nums[i] != tem[i])
-                {
-                    start = i;
-                    break;
-                }
-            }
-            for (int i = nums.Length-1; i >= 0; i--)
-            {
-                if (nums[i] != tem[i])
-                {
-                    end = i;
-                    break;
-                }
-            }
-            return end == start ? 0 : end - start + 1;
+            var sorted = nums.OrderBy(x => x).ToArray();
+            int li = 0, hi = nums.Length - 1;
+            while (li < hi && nums[li] == sorted[li])
+                li++;
+            while (li < hi && nums[hi] == sorted[hi])
+                hi--;
+            return li == hi ? 0 : hi - li + 1;
         }
 
         static int FindUnsortedSubarray_N(int[] nums)
