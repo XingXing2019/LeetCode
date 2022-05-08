4bc0012dbb5390b2c37d0039a08ec77c8742797d
Xing <x.xing.unsw@gmail.com>
2022-05-08 14:13:36 +1000

    init

---
+++ String/LeetCode 2264 - Largest3SameDigitNumberInString/Largest3SameDigitNumberInString/Largest3SameDigitNumberInString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Largest3SameDigitNumberInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string LargestGoodInteger(string num)
+        {
+            var res = -1;
+            var count = 0;
+            var digit = num[0];
+            foreach (var l in num)
+            {
+                if (l == digit)
+                    count++;
+                else
+                {
+                    digit = l;
+                    count = 1;
+                }
+                if (count == 3)
+                    res = Math.Max(res, int.Parse(new string(digit, 3)));
+            }
+            return res == -1 ? "" : res.ToString("000");
+        }
+    }
+}
