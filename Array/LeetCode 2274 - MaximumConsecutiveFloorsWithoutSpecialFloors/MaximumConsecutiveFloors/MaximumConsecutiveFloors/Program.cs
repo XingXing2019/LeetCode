ba99a4575b2a4ebe86b32c74bd29699abaf6ce68
Xing <x.xing.unsw@gmail.com>
2022-05-15 21:01:45 +1000

    init

---
+++ Array/LeetCode 2274 - MaximumConsecutiveFloorsWithoutSpecialFloors/MaximumConsecutiveFloors/MaximumConsecutiveFloors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MaximumConsecutiveFloors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaxConsecutive(int bottom, int top, int[] special)
+        {
+            Array.Sort(special);
+            var res = 0;
+            for (int i = 0; i < special.Length; i++)
+            {
+                var floors = i == 0 ? special[i] - bottom : special[i] - special[i - 1] - 1;
+                res = Math.Max(res, floors);
+            }
+            return Math.Max(res, top - special[^1]);
+        }
+    }
+}
