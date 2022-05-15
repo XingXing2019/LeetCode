517ed1935274826a95e34b152560e4d9085b3c40
Xing <x.xing.unsw@gmail.com>
2022-05-15 21:11:22 +1000

    init

---
+++ BitManipulation/LeetCode 2275 - LargestCombinationWithBitwiseANDGreaterThanZero/LargestCombination/LargestCombination/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LargestCombination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int LargestCombination(int[] candidates)
+        {
+            var res = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                var count = 0;
+                foreach (var num in candidates)
+                {
+                    if (((num >> i) & 1) == 1)
+                        count++;
+                }
+                res = Math.Max(res, count);
+            }
+            return res;
+        }
+    }
+}
