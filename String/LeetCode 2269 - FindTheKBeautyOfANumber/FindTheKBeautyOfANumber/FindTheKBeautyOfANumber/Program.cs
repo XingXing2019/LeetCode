8e87e1f8c6671933573e206da6d2fac5eb52bac4
Xing <x.xing.unsw@gmail.com>
2022-05-15 10:31:54 +1000

    init

---
+++ String/LeetCode 2269 - FindTheKBeautyOfANumber/FindTheKBeautyOfANumber/FindTheKBeautyOfANumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FindTheKBeautyOfANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int DivisorSubstrings(int num, int k)
+        {
+            var numsStr = num.ToString();
+            var res = 0;
+            for (int i = 0; i <= numsStr.Length - k; i++)
+            {
+                var subNum = int.Parse(numsStr.Substring(i, k));
+                if (subNum == 0) continue;
+                if (num % subNum == 0)
+                    res++;
+            }
+            return res;
+        }
+    }
+}
