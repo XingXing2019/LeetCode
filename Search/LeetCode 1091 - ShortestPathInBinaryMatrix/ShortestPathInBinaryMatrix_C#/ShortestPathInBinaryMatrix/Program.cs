e3757b8549dfecbd6741e7e81d60d5c4c7379645
Xing <x.xing.unsw@gmail.com>
2022-05-16 17:54:48 +1000

    dp solution

--- Search/LeetCode 1091 - ShortestPathInBinaryMatrix/ShortestPathInBinaryMatrix_C#/ShortestPathInBinaryMatrix/Program.cs
+++ Search/LeetCode 1091 - ShortestPathInBinaryMatrix/ShortestPathInBinaryMatrix_C#/ShortestPathInBinaryMatrix/Program.cs
@@ -41,5 +41,39 @@ namespace ShortestPathInBinaryMatrix
             }
             return -1;
         }
+
+        public int ShortestPathBinaryMatrix_DP(int[][] grid)
+        {
+            if (grid[0][0] == 1 || grid[^1][^1] == 1) return -1;
+            int n = grid.Length;
+            var dp = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = new int[n];
+                Array.Fill(dp[i], int.MaxValue);
+            }
+            var queue = new Queue<int[]>();
+            queue.Enqueue(new int[] { 0, 0, 1 });
+            dp[0][0] = 1;
+            int[] dx = { -1, 1, 0, 0, -1, 1, -1, 1 };
+            int[] dy = { 0, 0, -1, 1, -1, -1, 1, 1 };
+            while (queue.Count != 0)
+            {
+                var cur = queue.Dequeue();
+                if (cur[0] == n - 1 && cur[1] == n - 1)
+                    return cur[2];
+                for (int i = 0; i < 8; i++)
+                {
+                    int newX = cur[0] + dx[i], newY = cur[1] + dy[i];
+                    if (newX < 0 || newX >= n || newY < 0 || newY >= n || grid[newX][newY] == 1)
+                        continue;
+                    if (dp[newX][newY] <= cur[2] + 1)
+                        continue;
+                    dp[newX][newY] = cur[2] + 1;
+                    queue.Enqueue(new int[] { newX, newY, cur[2] + 1 });
+                }
+            }
+            return -1;
+        }
     }
 }
