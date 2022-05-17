db4bb806af6a16e07af6086bc5257db5e5a85907
Xing <x.xing.unsw@gmail.com>
2022-05-17 20:07:10 +1000

    sort solution

--- HashTable/LeetCode 953 - VerifyingAnAlienDictionary/VerifyingAnAlienDictionary_C#/VerifyingAnAlienDictionary/Program.cs
+++ HashTable/LeetCode 953 - VerifyingAnAlienDictionary/VerifyingAnAlienDictionary_C#/VerifyingAnAlienDictionary/Program.cs
@@ -37,5 +37,36 @@ namespace VerifyingAnAlienDictionary
             }
             return true;
         }
+
+        public bool IsAlienSorted_Sort(string[] words, string order)
+        {
+            var dict = new Dictionary<char, int>();
+            for (int i = 0; i < order.Length; i++)
+                dict[order[i]] = i;
+            var copy = new string[words.Length];
+            Array.Copy(words, copy, words.Length);
+            Array.Sort(copy, (a, b) => Compare(a, b, dict));
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (copy[i] == words[i]) continue;
+                return false;
+            }
+            return true;
+        }
+
+        public int Compare(string w1, string w2, Dictionary<char, int> dict)
+        {
+            int p1 = 0, p2 = 0;
+            while (p1 < w1.Length && p2 < w2.Length)
+            {
+                if (dict[w1[p1]] < dict[w2[p2]])
+                    return -1;
+                else if (dict[w1[p1]] > dict[w2[p2]])
+                    return 1;
+                p1++;
+                p2++;
+            }
+            return w1.Length < w2.Length ? -1 : 1;
+        }
     }
 }
