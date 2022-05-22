e8db5f9771186f2ebbc9a1c3d4c3ae47de82a3ff
Xing <x.xing.unsw@gmail.com>
2022-05-22 14:08:49 +1000

    init

---
+++ String/LeetCode 2278 - PercentageOfLetterInString/PercentageOfLetterInString/PercentageOfLetterInString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace PercentageOfLetterInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int PercentageLetter(string s, char letter)
+        {
+            var count = s.Count(x => x == letter);
+            return (int)((double)count / s.Length * 100);
+        }
+    }
+}
