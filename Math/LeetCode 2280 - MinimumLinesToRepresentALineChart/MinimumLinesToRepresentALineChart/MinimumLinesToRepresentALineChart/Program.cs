06efb0693aeca00ca460c8b61b56bf2f7449d33f
Xing <x.xing.unsw@gmail.com>
2022-05-22 14:40:39 +1000

    init

---
+++ Math/LeetCode 2280 - MinimumLinesToRepresentALineChart/MinimumLinesToRepresentALineChart/MinimumLinesToRepresentALineChart/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumLinesToRepresentALineChart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MinimumLines(int[][] stockPrices)
+        {
+            if (stockPrices.Length == 1) return 0;
+            stockPrices = stockPrices.OrderBy(x => x[0]).ToArray();
+            decimal slop = stockPrices[1][1] == stockPrices[0][1]
+                ? decimal.MaxValue
+                : (decimal)(stockPrices[1][0] - stockPrices[0][0]) / (stockPrices[1][1] - stockPrices[0][1]);
+            var res = 1;
+            for (int i = 2; i < stockPrices.Length; i++)
+            {
+                decimal newSlop = stockPrices[i][1] == stockPrices[i - 1][1]
+                    ? decimal.MaxValue
+                    : (decimal)(stockPrices[i][0] - stockPrices[i - 1][0]) / (stockPrices[i][1] - stockPrices[i - 1][1]);
+                if (newSlop == slop) continue;
+                slop = newSlop;
+                res++;
+            }
+            return res;
+        }
+    }
+}
