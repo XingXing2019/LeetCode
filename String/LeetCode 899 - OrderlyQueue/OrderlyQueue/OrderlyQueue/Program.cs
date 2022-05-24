c5a9ac002c663ec6459bffb7ab81dc04f8817f03
Xing <x.xing.unsw@gmail.com>
2022-05-24 20:51:34 +1000

    init

---
+++ String/LeetCode 899 - OrderlyQueue/OrderlyQueue/OrderlyQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderlyQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string OrderlyQueue(string s, int k)
+        {
+            if (k > 1)
+                return string.Join("", s.OrderBy(x => x));
+            var visited = new HashSet<string>();
+            var res = s;
+            while (visited.Add(s))
+            {
+                s = s.Substring(1) + s[0];
+                if (s.CompareTo(res) < 0)
+                    res = s;
+            }
+            return res;
+        }
+    }
+}
