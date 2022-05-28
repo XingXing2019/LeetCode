ee55e2caa318cdc9e44cef7420f8dca68c30c664
Xing <x.xing.unsw@gmail.com>
2022-05-29 09:37:49 +1000

    init

---
+++ Graph/LeetCode 2285 - MaximumTotalImportanceOfRoads/MaximumTotalImportanceOfRoads/MaximumTotalImportanceOfRoads/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MaximumTotalImportanceOfRoads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long MaximumImportance(int n, int[][] roads)
+        {
+            var degree = new int[n];
+            foreach (var road in roads)
+            {
+                degree[road[0]]++;
+                degree[road[1]]++;
+            }
+            Array.Sort(degree, (a, b) => b - a);
+            long res = 0;
+            for (int i = 0; i < degree.Length; i++)
+            {
+                res += (long) n * degree[i];
+                n--;
+            }
+            return res;
+        }
+    }
+}
