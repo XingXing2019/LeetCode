8a70476e2193e056f6aaac42d5ac53835e5ceb69
Xing <x.xing.unsw@gmail.com>
2022-05-29 14:19:25 +1000

    init

---
+++ String/LeetCode 2288 - ApplyDiscountToPrices/ApplyDiscountToPrices/ApplyDiscountToPrices/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ApplyDiscountToPrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string DiscountPrices(string sentence, int discount)
+        {
+            var words = sentence.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                var index = words[i].IndexOf("$");
+                if (index == -1 || index != 0)
+                    continue;
+                var temp = words[i].Substring(1);
+                if (!double.TryParse(temp, out var num))
+                    continue;
+                num *= (100 - (double)discount) / 100;
+                words[i] = $"${num:0.00}";
+            }
+            return string.Join(' ', words);
+        }
+    }
+}
