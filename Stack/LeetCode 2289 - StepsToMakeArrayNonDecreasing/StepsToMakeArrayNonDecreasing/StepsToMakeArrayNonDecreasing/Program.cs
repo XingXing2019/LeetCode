5be39dd44a5594b8ae35d9e560b2a9f1a6d7a5ae
Xing <x.xing.unsw@gmail.com>
2022-05-29 20:04:32 +1000

    init

---
+++ Stack/LeetCode 2289 - StepsToMakeArrayNonDecreasing/StepsToMakeArrayNonDecreasing/StepsToMakeArrayNonDecreasing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StepsToMakeArrayNonDecreasing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 1, 2, 3, 4, 5, 6, 1, 2, 3 };
+            Console.WriteLine(TotalSteps(nums));
+        }
+
+        public static int TotalSteps(int[] nums)
+        {
+            var stack = new Stack<int>();
+            var res = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var max = 0;
+                while (stack.Count != 0 && nums[i] >= nums[stack.Peek()])
+                    max = Math.Max(max, res[stack.Pop()]);
+                if (stack.Count != 0)
+                    res[i] = max + 1;
+                stack.Push(i);
+            }
+            return res.Max();
+        }
+    }
+}
