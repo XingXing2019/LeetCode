55bc387ec879d06151c08ab84b36f2e9c092300f
Xing <x.xing.unsw@gmail.com>
2022-05-29 14:09:07 +1000

    init

---
+++ HashTable/LeetCode 2287 - RearrangeCharactersToMakeTargetString/RearrangeCharacters/RearrangeCharacters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace RearrangeCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int RearrangeCharacters(string s, string target)
+        {
+            var sDict = s.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var tDict = target.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var res = int.MaxValue;
+            foreach (var letter in tDict.Keys)
+            {
+                if (!sDict.ContainsKey(letter)) return 0;
+                res = Math.Min(res, sDict[letter] / tDict[letter]);
+            }
+            return res;
+        }
+    }
+}
