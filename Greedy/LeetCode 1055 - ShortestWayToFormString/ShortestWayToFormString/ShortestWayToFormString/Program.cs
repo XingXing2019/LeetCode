3849a1d1ead674f4ffb6a975907f4c28d4d51409
Xing <x.xing.unsw@gmail.com>
2022-06-06 22:04:48 +1000

    init

---
+++ Greedy/LeetCode 1055 - ShortestWayToFormString/ShortestWayToFormString/ShortestWayToFormString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ShortestWayToFormString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int ShortestWay(string source, string target)
+        {
+            if (target.Any(x => !source.Contains(x))) return -1;
+            var word = new StringBuilder();
+            var res = 0;
+            foreach (var l in target)
+            {
+                word.Append(l);
+                if (IsSubsequence(source, word.ToString())) continue;
+                res++;
+                word = new StringBuilder(l.ToString());
+            }
+            if (IsSubsequence(source, word.ToString()))
+                res++;
+            return res;
+        }
+
+        public bool IsSubsequence(string source, string word)
+        {
+            int p1 = 0, p2 = 0;
+            while (p1 < source.Length && p2 < word.Length)
+            {
+                if (source[p1] == word[p2])
+                    p2++;
+                p1++;
+            }
+            return p2 == word.Length;
+        }
+    }
+}
