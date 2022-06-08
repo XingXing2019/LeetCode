31a5683817d8df937d520a0bf38abbd9f9d4753b
Xing <x.xing.unsw@gmail.com>
2022-06-08 17:39:56 +1000

    Math

--- Math/LeetCode 1037 - ValidBoomerang/ValidBoomerang/ValidBoomerang/Program.cs
+++ Math/LeetCode 1037 - ValidBoomerang/ValidBoomerang/ValidBoomerang/Program.cs
@@ -1,5 +1,6 @@
 ﻿//如果三个点没有重复，且两两之间斜率不相等，返回true，否则返回false
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ValidBoomerang
@@ -10,9 +11,9 @@ namespace ValidBoomerang
         {
             int[][] points = new int[3][];
             points[0] = new int[] { 0, 1 };
-            points[1] = new int[] { 2, 2 };
-            points[2] = new int[] { 2, 0 };
-            Console.WriteLine(IsBoomerang(points));
+            points[1] = new int[] { 1, 1 };
+            points[2] = new int[] { 2, 1 };
+            Console.WriteLine(IsBoomerang_Math(points));
         }
         static bool IsBoomerang(int[][] points)
         {
@@ -33,6 +34,17 @@ namespace ValidBoomerang
                 }
             }      
             return true;
+        }
+
+        static bool IsBoomerang_Math(int[][] points)
+        {
+            var set = new HashSet<string>(points.Select(x => $"{x[0]}:{x[1]}"));
+            if (set.Count != 3) return false;
+            var slop1 = ((double)points[0][0] - points[1][0]) / (points[0][1] - points[1][1]);
+            var slop2 = ((double)points[1][0] - points[2][0]) / (points[1][1] - points[2][1]);
+            var slop3 = ((double)points[2][0] - points[0][0]) / (points[2][1] - points[0][1]);
+            var infinity = double.PositiveInfinity;
+            return (slop1 != slop2 || slop2 != slop3) && (Math.Abs(slop1) != infinity || Math.Abs(slop2) != infinity || Math.Abs(slop3) != infinity);
         }
     }
 }
