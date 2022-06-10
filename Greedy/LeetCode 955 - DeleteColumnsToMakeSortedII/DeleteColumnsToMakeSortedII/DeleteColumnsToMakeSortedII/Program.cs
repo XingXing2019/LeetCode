8c1f71fb01f86c7dd4b201e717717b3402831579
Xing <x.xing.unsw@gmail.com>
2022-06-10 21:55:49 +1000

    init

---
+++ Greedy/LeetCode 955 - DeleteColumnsToMakeSortedII/DeleteColumnsToMakeSortedII/DeleteColumnsToMakeSortedII/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DeleteColumnsToMakeSortedII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "btw", "etm", "ezl", "niv" };
+            Console.WriteLine(MinDeletionSize(strs));
+        }
+
+        public static int MinDeletionSize(string[] strs)
+        {
+            int m = strs.Length, n = strs[0].Length, res = 0;
+            var state = new int[m];
+            for (int j = 0; j < n; j++)
+            {
+                var copy = new int[m];
+                Array.Copy(state, copy, m);
+                for (int i = 1; i < m; i++)
+                {
+                    if (state[i] == 1) continue;
+                    if (strs[i][j] > strs[i - 1][j])
+                        state[i] = 1;
+                    else if (strs[i][j] == strs[i - 1][j])
+                        state[i] = 0;
+                    else
+                    {
+                        state = copy;
+                        res++;
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
