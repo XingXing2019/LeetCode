d40a0aaa239d294b71e566b0b5ad22bf13be5ca7
Xing <x.xing.unsw@gmail.com>
2022-06-12 13:44:16 +1000

    init

--- Database/LeetCode 2298 - TasksCountInTheWeekend/TasksCountInTheWeekend_MySQL.sql
+++ Database/LeetCode 2298 - TasksCountInTheWeekend/TasksCountInTheWeekend_MySQL.sql
@@ -1,9 +1,9 @@
 select * from (
 	select count(*) as weekend_cnt
-    from tasks where dayofweek(submit_date) in (1, 7)
+	from tasks where dayofweek(submit_date) in (1, 7)
 ) as Weekend
 join (
 	select count(*) as working_cnt
-    from tasks where dayofweek(submit_date) not in (1, 7)
+	from tasks where dayofweek(submit_date) not in (1, 7)
 ) as WorkingDays
 ON 1 = 1
