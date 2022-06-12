03554da6b8bb58726e55b7c0d2849d873dd2243c
Xing <x.xing.unsw@gmail.com>
2022-06-12 13:52:28 +1000

    init

---
+++ String/LeetCode 2299 - StrongPasswordCheckerII/StrongPasswordCheckerII/StrongPasswordCheckerII/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StrongPasswordCheckerII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool StrongPasswordCheckerII(string password)
+        {
+            var specials = new HashSet<char>("!@#$%^&*()-+");
+            bool hasUpper = false, hasLower = false, hasSpecial = false, hasDigigt = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsUpper(password[i]))
+                    hasUpper = true;
+                if (char.IsLower(password[i]))
+                    hasLower = true;
+                if (char.IsDigit(password[i]))
+                    hasDigigt = true;
+                if (specials.Contains(password[i]))
+                    hasSpecial = true;
+                if (i != 0 && password[i] == password[i - 1])
+                    return false;
+            }
+            return password.Length >= 8 && hasUpper && hasLower && hasSpecial && hasDigigt;
+        }
+    }
+}
