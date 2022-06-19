26d882b96e101fcddc1868c527a11f2b790a772d
Xing <x.xing.unsw@gmail.com>
2022-06-19 20:27:55 +1000

    init

---
+++ Math/LeetCode 2310 - SumOfNumbersWithUnitsDigitK/SumOfNumbersWithUnitsDigitK/SumOfNumbersWithUnitsDigitK/Program.cs
@@ -0,0 +1,10 @@
+﻿int MinimumNumbers(int num, int k)
+{
+    if (num == 0) return 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i * k % 10 == num % 10 && i * k <= num)
+            return i;
+    }
+    return -1;
+}
