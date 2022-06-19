4bbcd20a51a26163f06dc50e47d5ecfe886042a1
Xing <x.xing.unsw@gmail.com>
2022-06-19 20:19:35 +1000

    init

---
+++ String/LeetCode 2309 - GreatestEnglishLetterInUpperAndLowerCase/GreatestEnglishLetter/GreatestEnglishLetter/Program.cs
@@ -0,0 +1,18 @@
+﻿string GreatestLetter(string s)
+{
+    var upper = new bool[26];
+    var lower = new bool[26];
+    foreach (var l in s)
+    {
+        if (char.IsUpper(l))
+            upper[l - 'A'] = true;
+        else
+            lower[l - 'a'] = true;
+    }
+    for (int i = upper.Length - 1; i >= 0; i--)
+    {
+        if (!upper[i] || !lower[i]) continue;
+        return ((char)(i + 'A')).ToString();
+    }
+    return "";
+}
