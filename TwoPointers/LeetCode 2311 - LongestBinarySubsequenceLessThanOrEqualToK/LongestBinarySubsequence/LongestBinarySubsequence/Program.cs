2e357a6bb3b2f0da40e90c89456eab326a624c91
Xing <x.xing.unsw@gmail.com>
2022-06-19 21:23:05 +1000

    init

---
+++ TwoPointers/LeetCode 2311 - LongestBinarySubsequenceLessThanOrEqualToK/LongestBinarySubsequence/LongestBinarySubsequence/Program.cs
@@ -0,0 +1,25 @@
+﻿var s = "11110100010101101010010";
+var k = 9395105;
+
+Console.WriteLine(LongestSubsequence(s, k));
+
+int LongestSubsequence(string s, int k)
+{
+    int num = 0, res = s.Length, digits = 0;
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        if (s[i] == '0')
+            digits++;
+        else
+        {
+            if (digits > 32 || num + Math.Pow(2, digits) > k)
+                res--;
+            else
+            {
+                num += (int)Math.Pow(2, digits);
+                digits++;
+            }
+        }
+    }
+    return res;
+}
