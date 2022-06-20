11a542fcda39a5ea1be3798ae4eaef50ec265da3
Xing <x.xing.unsw@gmail.com>
2022-06-20 20:55:01 +1000

    heap

--- DynamicProgramming/LeetCode 1696 - JumpGameVI/JumpGameVI_C#/JumpGameVI/Program.cs
+++ DynamicProgramming/LeetCode 1696 - JumpGameVI/JumpGameVI_C#/JumpGameVI/Program.cs
@@ -1,31 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace JumpGameVI
+﻿int MaxResult(int[] nums, int k)
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-		
-		public int MaxResult(int[] nums, int k)
-		{
-			var dp = new int[nums.Length];
-			dp[0] = nums[0];
-			var queue = new LinkedList<int>();
-			queue.AddLast(0);
-			for (int i = 1; i < dp.Length; i++)
-			{
-				while (i - queue.First.Value > k)
-					queue.RemoveFirst();
-				dp[i] = dp[queue.First.Value] + nums[i];
-				while (queue.Count > 0 && dp[i] >= dp[queue.Last.Value])
-					queue.RemoveLast();
-				queue.AddLast(i);
-			}
-			return dp[^1];
-		}
-	}
+    var dp = new int[nums.Length];
+    dp[0] = nums[0];
+    var queue = new LinkedList<int>();
+    queue.AddLast(0);
+    for (int i = 1; i < dp.Length; i++)
+    {
+        while (i - queue.First.Value > k)
+            queue.RemoveFirst();
+        dp[i] = dp[queue.First.Value] + nums[i];
+        while (queue.Count > 0 && dp[i] >= dp[queue.Last.Value])
+            queue.RemoveLast();
+        queue.AddLast(i);
+    }
+    return dp[^1];
+}
+
+int MaxResult_Heap(int[] nums, int k)
+{
+    var dp = new int[nums.Length];
+    dp[0] = nums[0];
+    var maxHeap = new PriorityQueue<int[], int>();
+    maxHeap.Enqueue(new []{nums[0], 0}, -nums[0]);
+    for (int i = 1; i < dp.Length; i++)
+    {
+        dp[i] = maxHeap.Peek()[0] + nums[i];
+        maxHeap.Enqueue(new []{dp[i], i}, -dp[i]);
+        while (maxHeap.Count != 0 && maxHeap.Peek()[1] < i - k)
+            maxHeap.Dequeue();
+    }
+    return dp[^1];
 }
