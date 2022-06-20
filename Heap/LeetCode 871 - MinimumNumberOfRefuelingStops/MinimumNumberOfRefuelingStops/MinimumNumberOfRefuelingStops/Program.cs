b9a1b1c6a4f4c7f3927ef7ed3b85d81dd48fb3a7
Xing <x.xing.unsw@gmail.com>
2022-06-20 21:16:32 +1000

    init

---
+++ Heap/LeetCode 871 - MinimumNumberOfRefuelingStops/MinimumNumberOfRefuelingStops/MinimumNumberOfRefuelingStops/Program.cs
@@ -0,0 +1,24 @@
+﻿int target = 100, startFuel = 50;
+int[][] stations = new[]
+{
+    new[] { 25, 25 },
+    new[] { 50, 50 },
+};
+
+Console.WriteLine(MinRefuelStops(target, startFuel, stations));
+
+int MinRefuelStops(int target, int startFuel, int[][] stations)
+{
+    var maxHeap = new PriorityQueue<int[], int>();
+    int res = 0, index = 0;
+    while (startFuel < target)
+    {
+        while (index < stations.Length && startFuel >= stations[index][0])
+            maxHeap.Enqueue(stations[index], -stations[index++][1]);
+        if (maxHeap.Count == 0)
+            return -1;
+        startFuel += maxHeap.Dequeue()[1];
+        res++;
+    }
+    return startFuel < target ? -1 : res;
+}
