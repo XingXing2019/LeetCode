b6f203dc4822472432088333ac28df806b83d93c
Xing <x.xing.unsw@gmail.com>
2022-06-21 20:34:06 +1000

    init

---
+++ Heap/LeetCode 1642 - FurthestBuildingYouCanReach/FurthestBuildingYouCanReach_C#/FurthestBuildingYouCanReach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+int FurthestBuilding(int[] heights, int bricks, int ladders)
+{
+    var minHeap = new PriorityQueue<int, int>();
+    for (int i = 1; i < heights.Length; i++)
+    {
+        var diff = heights[i] - heights[i - 1];
+        if (diff < 0) 
+            continue;
+        minHeap.Enqueue(diff, diff);
+        if (minHeap.Count > ladders)
+            bricks -= minHeap.Dequeue();
+        if (bricks < 0)
+            return i - 1;
+    }
+    return heights.Length - 1;
+}
