8177c08ec8ef2506aec40e6972f2164d1cb95049
Xing <x.xing.unsw@gmail.com>
2022-06-23 11:57:58 +1000

    C#

---
+++ Greedy/LeetCode 630 - CourseScheduleIII/CourseScheduleIII_C#/CourseScheduleIII/Program.cs
@@ -0,0 +1,20 @@
+﻿int ScheduleCourse(int[][] courses)
+{
+    var selectedCourses = new PriorityQueue<int, int>();
+    Array.Sort(courses, (a, b) => a[1] - b[1]);
+    var curDay = 0;
+    foreach (var course in courses)
+    {
+        if (curDay + course[0] <= course[1])
+        {
+            selectedCourses.Enqueue(course[0], -course[0]);
+            curDay += course[0];
+        }
+        else if (selectedCourses.Count != 0 && course[0] < selectedCourses.Peek())
+        {
+            curDay -= selectedCourses.Dequeue() - course[0];
+            selectedCourses.Enqueue(course[0], -course[0]);
+        }
+    }
+    return selectedCourses.Count;
+}
