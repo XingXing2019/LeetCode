d336b4c9ab6cddeec616fd0199862506d83e10a6
Xing <x.xing.unsw@gmail.com>
2022-06-26 09:34:43 +1000

    init

---
+++ String/LeetCode 2315 - CountAsterisks/CountAsterisks/CountAsterisks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CountAsterisks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountAsterisks(string s)
+        {
+            var parts = s.Split('|');
+            var res = 0;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (i % 2 != 0) continue;
+                res += parts[i].Count(x => x == '*');
+            }
+            return res;
+        }
+    }
+}
