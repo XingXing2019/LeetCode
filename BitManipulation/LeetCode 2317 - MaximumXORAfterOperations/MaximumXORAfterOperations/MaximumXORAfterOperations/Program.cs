ba7cfacad100bfa06f643006e93d933d503a93fc
Xing <x.xing.unsw@gmail.com>
2022-06-26 20:47:28 +1000

    init

---
+++ BitManipulation/LeetCode 2317 - MaximumXORAfterOperations/MaximumXORAfterOperations/MaximumXORAfterOperations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumXORAfterOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaximumXOR(int[] nums)
+        {
+            var res = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                var count = nums.Count(x => ((x >> i) & 1) == 1);
+                if (count == 0) continue;
+                res += 1 << i;
+            }
+            return res;
+        }
+    }
+}
