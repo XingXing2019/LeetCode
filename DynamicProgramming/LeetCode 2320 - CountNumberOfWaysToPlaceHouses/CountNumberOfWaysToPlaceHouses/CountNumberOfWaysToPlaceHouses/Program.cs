22b15962f00ba7a3def27c787b86c081cec2e4ff
Xing <x.xing.unsw@gmail.com>
2022-06-26 20:35:16 +1000

    init

---
+++ DynamicProgramming/LeetCode 2320 - CountNumberOfWaysToPlaceHouses/CountNumberOfWaysToPlaceHouses/CountNumberOfWaysToPlaceHouses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountNumberOfWaysToPlaceHouses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountHousePlacements(int n)
+        {
+            long f0 = 1, f1 = 2, mod = 1_000_000_000 + 7;
+            for (int i = 0; i < n; i++)
+            {
+                var temp = (f0 + f1) % mod;
+                f0 = f1;
+                f1 = temp;
+            }
+            return (int)(f0 * f0 % mod);
+        }
+    }
+}
