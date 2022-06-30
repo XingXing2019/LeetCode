503c20312742f22e8f21d86ab8a50ce6bd4bb4b5
Xing <x.xing.unsw@gmail.com>
2022-06-30 13:36:51 +1000

    init

---
+++ Greedy/LeetCode 2323 - FindMinimumTimeToFinishAllJobsII/FindMinimumTimeToFinishAllJobsII/FindMinimumTimeToFinishAllJobsII/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FindMinimumTimeToFinishAllJobsII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinimumTime(int[] jobs, int[] workers)
+        {
+            Array.Sort(jobs);
+            Array.Sort(workers);
+            var res = 0;
+            for (int i = 0; i < jobs.Length; i++)
+                res = Math.Max(res, (int)Math.Ceiling((double)jobs[i] / workers[i]));
+            return res;
+        }
+    }
+}
