0b8867b8755909cb67ba2842c0113515ffef0c9b
Xing <x.xing.unsw@gmail.com>
2022-07-04 08:40:20 +1000

    init

--- Database/LeetCode 2329 - ProductSalesAnalysisV/ProductSalesAnalysisV_MySQL.sql
+++ Database/LeetCode 2329 - ProductSalesAnalysisV/ProductSalesAnalysisV_MySQL.sql
@@ -7,9 +7,9 @@ order by spending desc, s.user_id asc;
 
 with Spending as (
 	select s.user_id, s.quantity * p.price as spending
-    from Sales s
-    join Product p
-    on s.product_id = p.product_id
+	from Sales s
+	join Product p
+	on s.product_id = p.product_id
 )
 
 select user_id, sum(spending) as spending
