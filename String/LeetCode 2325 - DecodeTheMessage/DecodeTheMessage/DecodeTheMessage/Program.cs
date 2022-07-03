81697ac4aeb559e89627cb03a906c33ac9670254
Xing <x.xing.unsw@gmail.com>
2022-07-03 13:47:03 +1000

    init

---
+++ String/LeetCode 2325 - DecodeTheMessage/DecodeTheMessage/DecodeTheMessage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecodeTheMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        string DecodeMessage(string key, string message)
+        {
+            var dict = new Dictionary<char, char>();
+            dict[' '] = ' ';
+            var letter = 0;
+            foreach (var l in key)
+            {
+                if (dict.ContainsKey(l)) continue;
+                dict[l] = (char)(letter + 'a');
+                letter++;
+            }
+            var res = "";
+            foreach (var l in message)
+                res += dict[l];
+            return res;
+        }
+    }
+}
