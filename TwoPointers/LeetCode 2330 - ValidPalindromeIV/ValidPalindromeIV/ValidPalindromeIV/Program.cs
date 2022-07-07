a32f809c2cf0ed11dee6ecae77b40e58280dabac
Xing <x.xing.unsw@gmail.com>
2022-07-07 13:37:46 +1000

    init

---
+++ TwoPointers/LeetCode 2330 - ValidPalindromeIV/ValidPalindromeIV/ValidPalindromeIV/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ValidPalindromeIV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool MakePalindrome(string s)
+        {
+            int li = 0, hi = s.Length - 1, count = 0;
+            while (li < hi)
+            {
+                if (s[li] != s[hi])
+                    count++;
+                li++;
+                hi--;
+            }
+            return count <= 2;
+        }
+    }
+}
