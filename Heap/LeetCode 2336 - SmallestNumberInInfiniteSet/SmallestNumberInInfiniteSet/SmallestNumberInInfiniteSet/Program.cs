f24c321b9cd89c92cad85c19237b61dc178245b2
Xing <x.xing.unsw@gmail.com>
2022-07-10 13:39:55 +1000

    init

---
+++ Heap/LeetCode 2336 - SmallestNumberInInfiniteSet/SmallestNumberInInfiniteSet/SmallestNumberInInfiniteSet/Program.cs
@@ -0,0 +1,30 @@
+﻿public class SmallestInfiniteSet
+{
+    private PriorityQueue<int, int> minHeap;
+    private HashSet<int> set;
+    public SmallestInfiniteSet()
+    {
+        minHeap = new PriorityQueue<int, int>();
+        set = new HashSet<int>();
+        for (int i = 1; i <= 1000; i++)
+        {
+            minHeap.Enqueue(i, i);
+            set.Add(i);
+        }
+    }
+
+    public int PopSmallest()
+    {
+        var res = minHeap.Dequeue();
+        set.Remove(res);
+        return res;
+    }
+
+    public void AddBack(int num)
+    {
+        if (set.Contains(num))
+            return;
+        minHeap.Enqueue(num, num);
+        set.Add(num);
+    }
+}
