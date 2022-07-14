dd79304613aaff1a70e3c85ff3fd3c42d2678d73
Xing <x.xing.unsw@gmail.com>
2022-07-14 15:54:35 +1000

    init

---
+++ Array/LeetCode 2340 - MinimumAdjacentSwapsToMakeAValidArray/MinimumAdjacentSwaps/MinimumAdjacentSwaps/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MinimumAdjacentSwaps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MinimumSwaps(int[] nums)
+        {
+            int min = int.MaxValue, max = int.MinValue;
+            foreach (var num in nums)
+            {
+                min = Math.Min(min, num);
+                max = Math.Max(max, num);
+            }
+            int maxIndex = -1, minIndex = nums.Length;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == max)
+                    maxIndex = Math.Max(maxIndex, i);
+                if (nums[i] == min)
+                    minIndex = Math.Min(minIndex, i);
+            }
+            var res = nums.Length - maxIndex - 1 + minIndex;
+            return minIndex > maxIndex ? res - 1 : res;
+        }
+    }
+}
