ce283cdbed73516a6ca61f508b5c49542fc05b1b
Xing <x.xing.unsw@gmail.com>
2022-07-18 08:30:11 +1000

    init

---
+++ HashTable/LeetCode 2341 - MaximumNumberOfPairsInArray/MaximumNumberOfPairsInArray/MaximumNumberOfPairsInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumNumberOfPairsInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] NumberOfPairs(int[] nums)
+        {
+            var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var res = new int[2];
+            foreach (var f in freq.Values)
+            {
+                res[0] += f / 2;
+                res[1] += f % 2;
+            }
+            return res;
+        }
+    }
+}
