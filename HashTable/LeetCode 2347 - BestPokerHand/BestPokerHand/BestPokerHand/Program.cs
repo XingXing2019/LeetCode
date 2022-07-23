090ef6b44e9098977b8c50d03a6cc05d38c77aac
Xing <x.xing.unsw@gmail.com>
2022-07-24 09:57:43 +1000

    init

---
+++ HashTable/LeetCode 2347 - BestPokerHand/BestPokerHand/BestPokerHand/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace BestPokerHand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string BestHand(int[] ranks, char[] suits)
+        {
+            var suitDict = suits.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var rankDict = ranks.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            if (suitDict.Any(x => x.Value == 5))
+                return "Flush";
+            if (rankDict.Any(x => x.Value >= 3))
+                return "Three of a Kind";
+            if (rankDict.Any(x => x.Value >= 2))
+                return "Pair";
+            return "High Card";
+        }
+    }
+}
