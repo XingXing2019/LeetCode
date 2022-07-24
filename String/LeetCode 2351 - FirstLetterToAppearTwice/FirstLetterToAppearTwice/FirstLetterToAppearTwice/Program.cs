8e8eab59bb4a7139123dfa0f3f6705df378cd454
Xing <x.xing.unsw@gmail.com>
2022-07-24 14:06:33 +1000

    init

---
+++ String/LeetCode 2351 - FirstLetterToAppearTwice/FirstLetterToAppearTwice/FirstLetterToAppearTwice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstLetterToAppearTwice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public char RepeatedCharacter(string s)
+        {
+            var set = new HashSet<char>();
+            foreach (var l in s)
+            {
+                if (set.Add(l)) continue;
+                return l;
+            }
+            return 'a';
+        }
+    }
+}
