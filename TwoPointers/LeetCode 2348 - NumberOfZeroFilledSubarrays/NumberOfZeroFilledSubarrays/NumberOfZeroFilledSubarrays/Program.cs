e8b13dfdc24c7b59d99b1abe0b90b1f9dbec6130
Xing <x.xing.unsw@gmail.com>
2022-07-24 10:14:30 +1000

    init

---
+++ TwoPointers/LeetCode 2348 - NumberOfZeroFilledSubarrays/NumberOfZeroFilledSubarrays/NumberOfZeroFilledSubarrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumberOfZeroFilledSubarrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 0, 0, 2, 0, 0, 4 };
+            Console.WriteLine(ZeroFilledSubarray(nums));
+        }
+        public static long ZeroFilledSubarray(int[] nums)
+        {
+            long res = 0;
+            int li = 0, hi = 0;
+            while (hi < nums.Length)
+            {
+                if (nums[hi] == 0 && nums[li] != 0)
+                    li = hi;
+                else if (nums[hi] != 0)
+                {
+                    if (nums[li] == 0)
+                        res += (long)(hi - li + 1) * (hi - li) / 2;
+                    li = hi;
+                }
+                hi++;
+            }
+            if (nums[li] == 0)
+                res += (long)(hi - li + 1) * (hi - li) / 2;
+            return res;
+        }
+    }
+}
