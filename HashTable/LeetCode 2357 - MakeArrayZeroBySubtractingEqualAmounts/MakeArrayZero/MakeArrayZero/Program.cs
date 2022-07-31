803876f87544ca335266bfb4880959d441a86c39
Xing <x.xing.unsw@gmail.com>
2022-07-31 14:19:52 +1000

    init

---
+++ HashTable/LeetCode 2357 - MakeArrayZeroBySubtractingEqualAmounts/MakeArrayZero/MakeArrayZero/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MakeArrayZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinimumOperations(int[] nums)
+        {
+            var set = new HashSet<int>(nums);
+            set.Remove(0);
+            return set.Count;
+        }
+    }
+}
