2187e42c6b976846f4af82a5d5e8d911f1027317
Xing <x.xing.unsw@gmail.com>
2022-07-31 14:44:23 +1000

    init

---
+++ Math/LeetCode 2358 - MaximumNumberOfGroupsEnteringACompetition/MaximumNumberOfGroups/MaximumNumberOfGroups/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MaximumNumberOfGroups
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public int MaximumGroups(int[] grades)
+        {
+            return (-1 + (int) Math.Sqrt(1 + 8 * grades.Length)) / 2;
+        }
+    }
+}
