af2a9212e52e39f00e2d8f1ecc5a2b6f50558b33
Xing <x.xing.unsw@gmail.com>
2022-08-04 20:14:07 +1000

    init

---
+++ Math/LeetCode 858 - MirrorReflection/MirrorReflection/MirrorReflection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MirrorReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MirrorReflection(int p, int q)
+        {
+            var gcd = GCD(p, q);
+            var lcm = p * q / gcd;
+            if (lcm / p % 2 == 0)
+                return 0;
+            return lcm / q % 2 == 0 ? 2 : 1;
+        }
+
+        public int GCD(int p, int q)
+        {
+            return q == 0 ? p : GCD(q, p % q);
+        }
+    }
+}
