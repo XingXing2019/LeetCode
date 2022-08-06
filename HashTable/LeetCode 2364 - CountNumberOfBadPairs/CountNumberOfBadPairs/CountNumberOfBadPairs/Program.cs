5a578b3e5f7d39cd151a748909037ed5bee8fdd0
Xing <x.xing.unsw@gmail.com>
2022-08-07 09:40:48 +1000

    init

---
+++ HashTable/LeetCode 2364 - CountNumberOfBadPairs/CountNumberOfBadPairs/CountNumberOfBadPairs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountNumberOfBadPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long CountBadPairs(int[] nums)
+        {
+            long res = 0, total = 0;
+            var freq = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var diff = nums[i] - i;
+                total++;
+                freq[diff] = freq.GetValueOrDefault(diff, 0) + 1;
+                res += total - freq[diff];
+            }
+            return res;
+        }
+    }
+}
