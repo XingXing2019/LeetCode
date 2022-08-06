23f73a06fa2736750140924af330c4de4459825e
Xing <x.xing.unsw@gmail.com>
2022-08-06 19:17:32 +1000

    init

---
+++ Math/LeetCode 458 - PoorPigs/PoorPigs/PoorPigs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PoorPigs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+        {
+            var tests = minutesToTest / minutesToDie + 1;
+            return (int)Math.Ceiling(Math.Log(buckets, tests));
+        }
+    }
+}
