8ca18eabda0d7ec440b4f857f65e619175d6649c
Xing <x.xing.unsw@gmail.com>
2022-08-07 09:35:22 +1000

    init

---
+++ HashTable/LeetCode 2363 - MergeSimilarItems/MergeSimilarItems/MergeSimilarItems/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeSimilarItems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IList<IList<int>> MergeSimilarItems(int[][] items1, int[][] items2)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (var item in items1)
+                dict[item[0]] = dict.GetValueOrDefault(item[0], 0) + item[1];
+            foreach (var item in items2)
+                dict[item[0]] = dict.GetValueOrDefault(item[0], 0) + item[1];
+            return dict.OrderBy(x => x.Key).Select(x => new int[] { x.Key, x.Value }).ToArray();
+        }
+    }
+}
