ebb53daaac97ca02ffb40d4e43a4431abaccfe1d
Xing <x.xing.unsw@gmail.com>
2022-08-07 20:00:59 +1000

    init

---
+++ DynamicProgramming/LeetCode 2369 - CheckIfThereIsAValidPartitionForTheArray/CheckIfThereIsAValidPartition/CheckIfThereIsAValidPartition/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CheckIfThereIsAValidPartition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool ValidPartition(int[] nums)
+        {
+            var dp = new bool[nums.Length];
+            dp[1] = nums[0] == nums[1];
+            for (int i = 2; i < dp.Length; i++)
+            {
+                if (nums[i] == nums[i - 1] && dp[i - 2])
+                    dp[i] = true;
+                else if (nums[i] == nums[i - 1] && nums[i] == nums[i - 2] && (i == 2 || dp[i - 3]))
+                    dp[i] = true;
+                else if (nums[i] == nums[i - 1] + 1 && nums[i] == nums[i - 2] + 2 && (i == 2 || dp[i - 3]))
+                    dp[i] = true;
+                else if (nums[i] == nums[i - 1] - 1 && nums[i] == nums[i - 2] - 2 && (i == 2 || dp[i - 3]))
+                    dp[i] = true;
+            }
+            return dp[^1];
+        }
+    }
+}
