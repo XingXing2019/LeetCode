788731abeb780f73d257cc70c936d97b239de5b8
Xing <x.xing.unsw@gmail.com>
2022-08-07 10:13:18 +1000

    init

---
+++ HashTable/LeetCode 2365 - TaskSchedulerII/TaskSchedulerII/TaskSchedulerII/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskSchedulerII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long TaskSchedulerII(int[] tasks, int space)
+        {
+            var dict = new Dictionary<int, long>();
+            long index = 0;
+            foreach (var task in tasks)
+            {
+                index++;
+                if (dict.ContainsKey(task))
+                {
+                    var diff = index - dict[task] - 1;
+                    if (diff < space)
+                        index += space - diff;
+                }
+                dict[task] = index;
+            }
+            return index;
+        }
+    }
+}
