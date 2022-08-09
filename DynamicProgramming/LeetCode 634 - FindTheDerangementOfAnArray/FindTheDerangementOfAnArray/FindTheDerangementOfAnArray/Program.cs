7db552f9bacde0161f177c494715225c4c22aa04
Xing <x.xing.unsw@gmail.com>
2022-08-09 21:27:45 +1000

    init

---
+++ DynamicProgramming/LeetCode 634 - FindTheDerangementOfAnArray/FindTheDerangementOfAnArray/FindTheDerangementOfAnArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FindTheDerangementOfAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int FindDerangement(int n)
+        {
+            if (n == 1) return 0;
+            long first = 0, second = 1, mod = 1_000_000_000 + 7;
+            for (int i = 3; i <= n; i++)
+            {
+                long temp = (first + second) * (i - 1) % mod;
+                first = second;
+                second = temp;
+            }
+            return (int) (second % mod);
+        }
+    }
+}
