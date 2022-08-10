098584caada9f651c1cdc8b42b46326caace0bd1
Xing <x.xing.unsw@gmail.com>
2022-08-10 20:40:44 +1000

    init

---
+++ String/LeetCode 2063 - VowelsOfAllSubstrings/VowelsOfAllSubstrings/VowelsOfAllSubstrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VowelsOfAllSubstrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long CountVowels(string word)
+        {
+            long res = 0;
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!vowels.Contains(word[i])) continue;
+                res += (long) (i + 1) * (word.Length - i);
+            }
+            return res;
+        }
+    }
+}
