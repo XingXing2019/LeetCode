bfe1b9c76651dc24a8dca4d8b5e3df08f0c54031
Xing <x.xing.unsw@gmail.com>
2022-08-12 21:43:16 +1000

    init

---
+++ BinarySearch/LeetCode 1562 - FindLatestGroupOfSizeM/FindLatestGroupOfSizeM/FindLatestGroupOfSizeM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindLatestGroupOfSizeM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int FindLatestStep(int[] arr, int m)
+        {
+            if (arr.Length == m) return arr.Length;
+            var edges = new List<int>{0, arr.Length + 1};
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                var edge = arr[i];
+                var index = edges.BinarySearch(edge);
+                index = ~index;
+                if (edge - edges[index - 1] - 1 == m || edges[index] - edge - 1 == m)
+                    return i;
+                edges.Insert(index, edge);
+            }
+            return -1;
+        }
+    }
+}
