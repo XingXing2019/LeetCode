1fb5eb45ee18522ece907d1facae3c314aa38a5d
Xing <x.xing.unsw@gmail.com>
2022-08-14 14:17:19 +1000

    init

---
+++ Array/LeetCode 2373 - LargestLocalValuesInAMatrix/LargestLocalValuesInAMatrix/LargestLocalValuesInAMatrix/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LargestLocalValuesInAMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] grid = new[]
+            {
+                new[] { 9, 9, 8, 1 },
+                new[] { 5, 6, 2, 6 },
+                new[] { 8, 2, 6, 4 },
+                new[] { 6, 2, 2, 2 },
+            };
+            Console.WriteLine(LargestLocal(grid));
+        }
+
+        public static int[][] LargestLocal(int[][] grid)
+        {
+            int n = grid.Length;
+            var maxGrid = new int[n][];
+            for (int i = 1; i <= n - 2; i++)
+            {
+                maxGrid[i] = new int[n];
+                for (int j = 1; j <= n - 2; j++)
+                {
+                    var max = 0;
+                    max = Math.Max(max, grid[i - 1][j - 1]);
+                    max = Math.Max(max, grid[i - 1][j]);
+                    max = Math.Max(max, grid[i - 1][j + 1]);
+                    max = Math.Max(max, grid[i][j - 1]);
+                    max = Math.Max(max, grid[i][j]);
+                    max = Math.Max(max, grid[i][j + 1]);
+                    max = Math.Max(max, grid[i + 1][j - 1]);
+                    max = Math.Max(max, grid[i + 1][j]);
+                    max = Math.Max(max, grid[i + 1][j + 1]);
+                    maxGrid[i][j] = max;
+                }
+            }
+            var res = new int[n - 2][];
+            for (int i = 1; i < n - 1; i++)
+            {
+                res[i - 1] = new int[n - 2];
+                Array.Copy(maxGrid[i], 1, res[i - 1], 0, n - 2);
+            }
+            return res;
+        }
+    }
+}
---
+++ Search/LeetCode 126 - WordLadderII/WordLadderII/WordLadderII/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordLadderII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string beginWord = "hit", endWord = "cog";
+            var wordList = new List<string> { "hot", "dot", "dog", "lot", "log", "cog" };
+            Console.WriteLine(FindLadders(beginWord, endWord, wordList));
+        }
+
+
+        class Node
+        {
+            public string cur;
+            public string path;
+            public int count;
+            public Node(string cur, string path, int count)
+            {
+                this.cur = cur;
+                this.path = path;
+                this.count = count;
+            }
+        }
+        public static IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList)
+        {
+            var res = new List<IList<string>>();
+            if (!wordList.Contains(endWord))
+                return res;
+            var graph = new Dictionary<string, List<string>>();
+            var dp = new Dictionary<string, Dictionary<string, int>> { { beginWord, new Dictionary<string, int>() } };
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                if (!dp.ContainsKey(wordList[i]))
+                    dp[wordList[i]] = new Dictionary<string, int>();
+                dp[beginWord][wordList[i]] = int.MaxValue;
+                dp[wordList[i]][beginWord] = int.MaxValue;
+                for (int j = 0; j < wordList.Count; j++)
+                {
+                    if (i == j) continue;
+                    dp[wordList[i]][wordList[j]] = int.MaxValue;
+                    if (!IsValid(wordList[i], wordList[j])) continue;
+                    if (!graph.ContainsKey(wordList[i]))
+                        graph[wordList[i]] = new List<string>();
+                    if (!graph.ContainsKey(wordList[j]))
+                        graph[wordList[j]] = new List<string>();
+                    graph[wordList[i]].Add(wordList[j]);
+                    graph[wordList[j]].Add(wordList[i]);
+                }
+                if (!IsValid(beginWord, wordList[i])) continue;
+                if (!graph.ContainsKey(beginWord))
+                    graph[beginWord] = new List<string>();
+                if (!graph.ContainsKey(wordList[i]))
+                    graph[wordList[i]] = new List<string>();
+                graph[beginWord].Add(wordList[i]);
+                graph[wordList[i]].Add(beginWord);
+            }
+            var queue = new Queue<Node>();
+            queue.Enqueue(new Node(beginWord, beginWord, 1));
+            var visited = new HashSet<string>();
+            while (queue.Count != 0)
+            {
+                var cur = queue.Dequeue();
+                if (cur.cur == endWord)
+                {
+                    res.Add(cur.path.Split("->"));
+                    continue;
+                }
+                foreach (var next in graph[cur.cur])
+                {
+                    var path = $"{cur.path}->{next}";
+                    if (dp[cur.cur][next] < cur.count + 1 || visited.Contains(path)) continue;
+                    dp[cur.cur][next] = cur.count + 1;
+                    visited.Add(path);
+                    queue.Enqueue(new Node(next, path, cur.count + 1));
+                }
+            }
+            return res;
+        }
+
+        public static bool IsValid(string word1, string word2)
+        {
+            var count = 0;
+            for (int i = 0; i < word1.Length; i++)
+            {
+                if (word1[i] != word2[i]) count++;
+                if (count > 1) return false;
+            }
+            return count == 1;
+        }
+    }
+}
