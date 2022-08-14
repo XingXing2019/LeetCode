0a44a38cb61bbddfa867ecc919bc01f5343b947b
Xing <x.xing.unsw@gmail.com>
2022-08-14 14:30:40 +1000

    init

---
+++ HashTable/LeetCode 2374 - NodeWithHighestEdgeScore/NodeWithHighestEdgeScore/NodeWithHighestEdgeScore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NodeWithHighestEdgeScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int EdgeScore(int[] edges)
+        {
+            var score = new long[edges.Length];
+            long max = 0;
+            for (int i = 0; i < edges.Length; i++)
+            {
+                score[edges[i]] += i;
+                max = Math.Max(max, score[edges[i]]);
+            }
+            return Array.IndexOf(score, max);
+        }
+    }
+}
