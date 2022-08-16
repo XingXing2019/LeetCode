80370ab3f7850d9e7d649bf6dadba087357c74c3
Xing <x.xing.unsw@gmail.com>
2022-08-16 18:22:15 +1000

    init

---
+++ Database/LeetCode 2377 - SortTheOlympicTable/SortTheOlympicTable_MySQL.sql
@@ -0,0 +1,2 @@
+select * from Olympic
+order by gold_medals desc, silver_medals desc, bronze_medals desc, country asc
---
+++ Database/LeetCode 2377 - SortTheOlympicTable/SortTheOlympicTable_SQLServer.sql
@@ -0,0 +1,2 @@
+SELECT * FROM Olympic
+ORDER BY gold_medals DESC, silver_medals DESC, bronze_medals DESC, country ASC
