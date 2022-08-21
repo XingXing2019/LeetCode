92a16e09e7923bacc43e2e076472a77464fcee58
Xing <x.xing.unsw@gmail.com>
2022-08-21 14:46:29 +1000

    init

--- String/LeetCode 2380 - TimeNeededToRearrangeABinaryString/TimeNeededToRearrangeABinaryString/TimeNeededToRearrangeABinaryString/Program.cs
+++ String/LeetCode 2380 - TimeNeededToRearrangeABinaryString/TimeNeededToRearrangeABinaryString/TimeNeededToRearrangeABinaryString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TimeNeededToRearrangeABinaryString
 {
@@ -6,7 +7,43 @@ namespace TimeNeededToRearrangeABinaryString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var s = "0110101";
+            Console.WriteLine(SecondsToRemoveOccurrences(s));
+        }
+
+        public static int SecondsToRemoveOccurrences(string s)
+        {
+            var res = 0;
+            var before = new StringBuilder(s);
+            while (CanChange(before, out var after))
+            {
+                res++;
+                before = after;
+            }
+            return res;
+        }
+
+        public static bool CanChange(StringBuilder before, out StringBuilder after)
+        {
+            after = new StringBuilder();
+            var canChange = false;
+            for (int i = 0; i < before.Length; i++)
+            {
+                if (before[i] == '0')
+                    after.Append('0');
+                else
+                {
+                    if (after.Length != 0 && before[i - 1] == '0')
+                    {
+                        after[^1] = '1';
+                        after.Append('0');
+                        canChange = true;
+                    }
+                    else
+                        after.Append('1');
+                }
+            }
+            return canChange;
         }
     }
 }
---
+++ String/LeetCode 2381 - ShiftingLettersII/ShiftingLettersII/ShiftingLettersII/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace ShiftingLettersII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "abc";
+            var shifts = new int[][]
+            {
+                new int[] {0, 1, 0},
+                //new int[] {1, 2, 1},
+                //new int[] {0, 2, 1 },
+            };
+            Console.WriteLine(ShiftingLetters(s, shifts));
+        }
+
+        public static string ShiftingLetters(string s, int[][] shifts)
+        {
+            var moves = new int[s.Length + 1];
+            foreach (var shift in shifts)
+            {
+                moves[shift[0]] += shift[2] == 0 ? -1 : 1;
+                moves[shift[1] + 1] += shift[2] == 1 ? -1 : 1;
+            }
+            var res = new StringBuilder();
+            var move = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                move += moves[i];
+                var letter = s[i] + move % 26;
+                if (letter < 'a')
+                    letter = 'z' - ('a' - letter) + 1;
+                else if (letter > 'z')
+                    letter = letter - 'z' + 'a' - 1;
+                res.Append((char)(letter));
+            }
+            return res.ToString();
+        }
+    }
+}
