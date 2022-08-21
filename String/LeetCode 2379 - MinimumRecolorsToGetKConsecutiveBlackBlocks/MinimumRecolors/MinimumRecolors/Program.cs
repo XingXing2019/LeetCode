28280f90b85070fabb3e4bccbeaf2c1ffb39be3d
Xing <x.xing.unsw@gmail.com>
2022-08-21 14:10:22 +1000

    init

---
+++ String/LeetCode 2379 - MinimumRecolorsToGetKConsecutiveBlackBlocks/MinimumRecolors/MinimumRecolors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumRecolors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var blocks = "BBB";
+            var k = 2;
+            Console.WriteLine(MinimumRecolors(blocks, k));
+        }
+
+        public static int MinimumRecolors(string blocks, int k)
+        {
+            var res = int.MaxValue;
+            for (int i = 0; i <= blocks.Length - k; i++)
+                res = Math.Min(res, blocks.Substring(i, k).Count(x => x == 'W'));
+            return res;
+        }
+    }
+}
