618d6056e3621b50cda64659b5f5b0d7b6ed51f7
Xing <x.xing.unsw@gmail.com>
2022-08-21 14:24:53 +1000

    init

---
+++ String/LeetCode 2380 - TimeNeededToRearrangeABinaryString/TimeNeededToRearrangeABinaryString/TimeNeededToRearrangeABinaryString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeNeededToRearrangeABinaryString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
