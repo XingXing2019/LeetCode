248a5cab0373d53fc41b7c7558ebbf0730b1b733
Xing <x.xing.unsw@gmail.com>
2022-08-28 19:31:29 +1000

    init

---
+++ String/LeetCode 2390 - RemovingStarsFromAString/RemovingStarsFromAString/RemovingStarsFromAString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace RemovingStarsFromAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string RemoveStars(string s)
+        {
+            var sb = new StringBuilder();
+            foreach (var l in s)
+            {
+                if (l != '*')
+                    sb.Append(l);
+                else
+                {
+                    if (sb.Length > 0)
+                        sb.Remove(sb.Length - 1, 1);
+                    else
+                        sb.Append(l);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
