970bc1338c46b42f295d6bd3faa48d521732096f
Xing <x.xing.unsw@gmail.com>
2022-09-01 20:24:23 +1000

    init

---
+++ TwoPointers/LeetCode 2393 - CountStrictlyIncreasingSubarrays/CountStrictlyIncreasingSubarrays/CountStrictlyIncreasingSubarrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CountStrictlyIncreasingSubarrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 5, 4, 4, 6 };
+            Console.WriteLine(CountSubarrays(nums));
+        }
+
+        public static long CountSubarrays(int[] nums)
+        {
+            int li = 0, hi = 0;
+            long res = 0;
+            while (hi < nums.Length)
+            {
+                hi++;
+                if (hi >= nums.Length || nums[hi] > nums[hi - 1]) continue;
+                res += (long)(hi - li + 1) * (hi - li) / 2;
+                li = hi;
+            }
+            return res + (long)(hi - li + 1) * (hi - li) / 2;
+        }
+    }
+}
