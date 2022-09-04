c0df76f05e4ddcd5c4c51ebd9c5f1e594f1d47db
Xing <x.xing.unsw@gmail.com>
2022-09-04 14:06:20 +1000

    init

---
+++ HashTable/LeetCode 2399 - CheckDistancesBetweenSameLetters/CheckDistancesBetweenSameLetters/CheckDistancesBetweenSameLetters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckDistancesBetweenSameLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool CheckDistances(string s, int[] distance)
+        {
+            var dict = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dict.ContainsKey(s[i]))
+                    dict[s[i]] = i;
+                else
+                    dict[s[i]] = i - dict[s[i]] - 1;
+            }
+            foreach (var l in dict.Keys)
+            {
+                if (dict[l] == distance[l - 'a']) continue;
+                return false;
+            }
+            return true;
+        }
+    }
+}
