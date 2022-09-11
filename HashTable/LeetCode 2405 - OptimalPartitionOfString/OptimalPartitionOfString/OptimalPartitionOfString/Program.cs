90e3f363ab13b98f93a8d315bbd230e50e881263
Xing <x.xing.unsw@gmail.com>
2022-09-11 14:22:59 +1000

    init

---
+++ HashTable/LeetCode 2405 - OptimalPartitionOfString/OptimalPartitionOfString/OptimalPartitionOfString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OptimalPartitionOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int PartitionString(string s)
+        {
+            var freq = new int[26];
+            var res = 1;
+            foreach (var l in s)
+            {
+                freq[l - 'a']++;
+                if (freq[l - 'a'] <= 1) continue;
+                res++;
+                freq = new int[26];
+                freq[l - 'a']++;
+            }
+            return res;
+        }
+    }
+}
