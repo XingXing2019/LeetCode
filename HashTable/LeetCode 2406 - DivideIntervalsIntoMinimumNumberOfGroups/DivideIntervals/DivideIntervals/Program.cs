f74913fa7f827021922a70aa2ed9bff57cfe2429
Xing <x.xing.unsw@gmail.com>
2022-09-11 15:09:49 +1000

    init

---
+++ HashTable/LeetCode 2406 - DivideIntervalsIntoMinimumNumberOfGroups/DivideIntervals/DivideIntervals/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace DivideIntervals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] intervals = new int[][]
+            {
+                new int[] { 1, 1 }
+            };
+            Console.WriteLine(MinGroups(intervals));
+        }
+
+        public static int MinGroups(int[][] intervals)
+        {
+            var max = intervals.Max(x => x[1]);
+            var freq = new int[max + 2];
+            foreach (var interval in intervals)
+            {
+                freq[interval[0]]++;
+                freq[interval[1] + 1]--;
+            }
+            int sum = 0, res = 0;
+            for (int i = 0; i < freq.Length; i++)
+            {
+                sum += freq[i];
+                res = Math.Max(res, sum);
+            }
+            return res;
+        }
+    }
+}
