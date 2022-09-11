ddcc283281fc40cecde702687296a6788ad5275f
Xing <x.xing.unsw@gmail.com>
2022-09-11 14:17:52 +1000

    init

---
+++ HashTable/LeetCode 2404 - MostFrequentEvenElement/MostFrequentEvenElement/MostFrequentEvenElement/Program.cs
@@ -0,0 +1,14 @@
+﻿int MostFrequentEven(int[] nums)
+{
+    var dict = new Dictionary<int, int>();
+    var max = 0;
+    foreach (var num in nums)
+    {
+        if (num % 2 != 0) continue;
+        if (!dict.ContainsKey(num))
+            dict[num] = 0;
+        dict[num]++;
+        max = Math.Max(max, dict[num]);
+    }
+    return dict.Count == 0 ? -1 : dict.Where(x => x.Value == max).Min(x => x.Key);
+}
