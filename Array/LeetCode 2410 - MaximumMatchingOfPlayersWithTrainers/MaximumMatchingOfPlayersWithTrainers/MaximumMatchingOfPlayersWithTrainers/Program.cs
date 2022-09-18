7ee19a968ba5f5a05558f8073fa95a936e0c63f8
Xing <x.xing.unsw@gmail.com>
2022-09-18 13:29:22 +1000

    init

--- Array/LeetCode 2410 - MaximumMatchingOfPlayersWithTrainers/MaximumMatchingOfPlayersWithTrainers/MaximumMatchingOfPlayersWithTrainers/Program.cs
+++ Array/LeetCode 2410 - MaximumMatchingOfPlayersWithTrainers/MaximumMatchingOfPlayersWithTrainers/MaximumMatchingOfPlayersWithTrainers/Program.cs
@@ -11,7 +11,19 @@ namespace MaximumMatchingOfPlayersWithTrainers
 
         public int MatchPlayersAndTrainers(int[] players, int[] trainers)
         {
-
+            Array.Sort(players);
+            Array.Sort(trainers);
+            int p1 = 0, p2 = 0, res = 0;
+            while (p1 < players.Length && p2 < trainers.Length)
+            {
+                if (players[p1] <= trainers[p2])
+                {
+                    res++;
+                    p1++;
+                }
+                p2++;
+            }
+            return res;
         }
     }
 }
---
+++ BitManipulation/LeetCode 2411 - SmallestSubarraysWithMaximumBitwiseOR/SmallestSubarrays/SmallestSubarrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmallestSubarrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
