3d8a2c9e4cacff94b91d687d84225671b1fc5223
Xing <x.xing.unsw@gmail.com>
2022-09-18 14:18:18 +1000

    init

---
+++ Math/LeetCode 2413 - SmallestEvenMultiple/SmallestEvenMultiple/SmallestEvenMultiple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmallestEvenMultiple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int SmallestEvenMultiple(int n)
+        {
+            return 2 * n / GCD(n, 2);
+        }
+
+        public int GCD(int num1, int num2)
+        {
+            if (num2 == 0)
+                return num1;
+            return GCD(num2, num1 % num2);
+        }
+    }
+}
