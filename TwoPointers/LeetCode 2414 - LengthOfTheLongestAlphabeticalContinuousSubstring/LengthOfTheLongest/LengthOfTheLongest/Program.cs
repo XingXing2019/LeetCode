de9d9748a55f8222f858eeb6546a62ba44a7c1be
Xing <x.xing.unsw@gmail.com>
2022-09-18 14:24:11 +1000

    init

---
+++ TwoPointers/LeetCode 2414 - LengthOfTheLongestAlphabeticalContinuousSubstring/LengthOfTheLongest/LengthOfTheLongest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LengthOfTheLongest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int LongestContinuousSubstring(string s)
+        {
+            int li = 0, hi = 0, res = 0;
+            while (hi < s.Length)
+            {
+                if (li == hi || s[hi] - s[hi - 1] == 1)
+                {
+                    res = Math.Max(res, hi - li + 1);
+                    hi++;
+                }
+                else
+                    li = hi;
+            }
+            return res;
+        }
+    }
+}
