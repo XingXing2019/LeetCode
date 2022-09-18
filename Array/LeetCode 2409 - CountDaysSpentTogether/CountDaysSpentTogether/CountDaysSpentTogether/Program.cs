c171c6b36b4b19de14a2218606257a363dba8315
Xing <x.xing.unsw@gmail.com>
2022-09-18 13:25:32 +1000

    init

---
+++ Array/LeetCode 2409 - CountDaysSpentTogether/CountDaysSpentTogether/CountDaysSpentTogether/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CountDaysSpentTogether
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountDaysTogether(string arriveAlice, string leaveAlice, string arriveBob, string leaveBob)
+        {
+            int[] days = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            for (int i = 1; i < days.Length; i++)
+                days[i] += days[i - 1];
+            var alice = new int[] { GetDate(days, arriveAlice), GetDate(days, leaveAlice) };
+            var bob = new int[] { GetDate(days, arriveBob), GetDate(days, leaveBob) };
+            if (alice[1] < bob[0] || bob[1] < alice[0])
+                return 0;
+            return Math.Min(alice[1], bob[1]) - Math.Max(alice[0], bob[0]) + 1;
+        }
+
+        public int GetDate(int[] days, string date)
+        {
+            var parts = date.Split('-');
+            var month = int.Parse(parts[0]);
+            var day = int.Parse(parts[1]);
+            return days[month - 1] + day;
+        }
+    }
+}
---
+++ Array/LeetCode 2410 - MaximumMatchingOfPlayersWithTrainers/MaximumMatchingOfPlayersWithTrainers/MaximumMatchingOfPlayersWithTrainers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MaximumMatchingOfPlayersWithTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MatchPlayersAndTrainers(int[] players, int[] trainers)
+        {
+
+        }
+    }
+}
