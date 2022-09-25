39dd3d5673c20c60bc32ef8ff49269cec79a8649
Xing <x.xing.unsw@gmail.com>
2022-09-25 14:04:03 +1000

    init

---
+++ Sort/LeetCode 2418 - SortThePeople/SortThePeople/SortThePeople/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace SortThePeople
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string[] SortPeople(string[] names, int[] heights)
+        {
+            var people = new Tuple<string, int>[names.Length];
+            for (int i = 0; i < names.Length; i++)
+                people[i] = new Tuple<string, int>(names[i], heights[i]);
+            return people.OrderByDescending(x => x.Item2).Select(x => x.Item1).ToArray();
+        }
+    }
+}
