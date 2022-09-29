764a70999b51d6d77717187a1a4f093b6c3751f4
Xing <x.xing.unsw@gmail.com>
2022-09-29 19:59:30 +1000

    init

---
+++ TwoPointers/LeetCode 2422 - MergeOperationsToTurnArrayIntoAPalindrome/MergeOperations/MergeOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MergeOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinimumOperations(int[] nums)
+        {
+            int li = 0, hi = nums.Length - 1, res = 0;
+            int left = nums[li], right = nums[hi];
+            while (li < hi)
+            {
+                if (left == right)
+                {
+                    left += nums[++li];
+                    right += nums[--hi];
+                    continue;
+                }
+                if (left < right)
+                    left += nums[++li];
+                else
+                    right += nums[--hi];
+                res++;
+            }
+            return res;
+        }
+    }
+}
