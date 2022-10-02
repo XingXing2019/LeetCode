4d20d318a5a12a92979c5771fe2182f11a8b6a56
Xing <x.xing.unsw@gmail.com>
2022-10-02 20:04:58 +1100

    init

---
+++ BitManipulation/LeetCode 2425 - BitwiseXOROfAllPairings/BitwiseXOROfAllPairings/BitwiseXOROfAllPairings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BitwiseXOROfAllPairings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int XorAllNums(int[] nums1, int[] nums2)
+        {
+            if (nums1.Length % 2 == 0 && nums2.Length % 2 == 0)
+                return 0;
+            if (nums1.Length % 2 != 0 && nums2.Length % 2 == 0)
+                return Xor(nums2);
+            if (nums1.Length % 2 == 0 && nums2.Length % 2 != 0)
+                return Xor(nums1);
+            var freq = nums1.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            foreach (var num in nums2)
+            {
+                if (!freq.ContainsKey(num))
+                    freq[num] = 0;
+                freq[num]++;
+            }
+            return Xor(freq.Where(x => x.Value % 2 != 0).Select(x => x.Key));
+        }
+
+        public int Xor(IEnumerable<int> nums)
+        {
+            var res = 0;
+            foreach (var num in nums)
+                res ^= num;
+            return res;
+        }
+    }
+}
