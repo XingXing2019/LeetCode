27c2b233bc6e846bfc537bf9d909d78caf598167
Xing <x.xing.unsw@gmail.com>
2022-10-02 20:07:52 +1100

    init

---
+++ Math/LeetCode 2427 - NumberOfCommonFactors/NumberOfCommonFactors/NumberOfCommonFactors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumberOfCommonFactors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CommonFactors(int a, int b)
+        {
+            int res = 0, min = Math.Min(a, b);
+            for (int i = 1; i <= min; i++)
+            {
+                if (a % i != 0 || b % i != 0) continue;
+                res++;
+            }
+            return res;
+        }
+    }
+}
