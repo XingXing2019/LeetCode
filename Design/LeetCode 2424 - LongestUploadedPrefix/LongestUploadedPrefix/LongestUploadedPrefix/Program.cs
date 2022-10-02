f4df774a8e65966f898ab4416f5af10428407970
Xing <x.xing.unsw@gmail.com>
2022-10-02 19:59:17 +1100

    init

---
+++ Design/LeetCode 2424 - LongestUploadedPrefix/LongestUploadedPrefix/LongestUploadedPrefix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LongestUploadedPrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class LUPrefix
+    {
+        private int index = 1;
+        private bool[] videos;
+        public LUPrefix(int n)
+        {
+            videos = new bool[n + 1];
+        }
+
+        public void Upload(int video)
+        {
+            videos[video] = true;
+        }
+
+        public int Longest()
+        {
+            while (index < videos.Length && videos[index])
+                index++;
+            return index - 1;
+        }
+    }
+
+}
