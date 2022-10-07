5f20daa2b5ecd15e329a0e2205c8ce24889c208c
Xing <x.xing.unsw@gmail.com>
2022-10-07 18:03:13 +1100

    init

---
+++ Design/LeetCode 732 - MyCalendarIII/MyCalendarIII/MyCalendarIII/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCalendarIII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class MyCalendarThree
+    {
+        private SortedDictionary<int, int> timestamps;
+        public MyCalendarThree()
+        {
+            timestamps = new SortedDictionary<int, int>();
+        }
+
+        public int Book(int start, int end)
+        {
+            timestamps[start] = timestamps.GetValueOrDefault(start, 0) + 1;
+            timestamps[end] = timestamps.GetValueOrDefault(end, 0) - 1;
+            int cur = 0, res = 0;
+            foreach (var timestamp in timestamps.Values)
+            {
+                cur += timestamp;
+                res = Math.Max(res, cur);
+            }
+            return res;
+
+        }
+    }
+}
