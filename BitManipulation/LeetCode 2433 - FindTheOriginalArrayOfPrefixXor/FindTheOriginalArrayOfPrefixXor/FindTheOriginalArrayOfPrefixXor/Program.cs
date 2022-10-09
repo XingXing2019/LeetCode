fbce134149b5b0eb2cf5c5a3b1734e1eb4bb4674
Xing <x.xing.unsw@gmail.com>
2022-10-09 15:24:31 +1100

    init

---
+++ BitManipulation/LeetCode 2433 - FindTheOriginalArrayOfPrefixXor/FindTheOriginalArrayOfPrefixXor/FindTheOriginalArrayOfPrefixXor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindTheOriginalArrayOfPrefixXor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] FindArray(int[] pref)
+        {
+            var res = new int[pref.Length];
+            res[0] = pref[0];
+            for (int i = 1; i < pref.Length; i++)
+                res[i] = pref[i - 1] ^ pref[i];
+            return res;
+        }
+    }
+}
