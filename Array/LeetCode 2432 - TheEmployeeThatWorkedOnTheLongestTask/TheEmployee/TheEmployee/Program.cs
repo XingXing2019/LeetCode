673359721ba2228d57d022405133257b3e2439f3
Xing <x.xing.unsw@gmail.com>
2022-10-09 15:17:44 +1100

    init

---
+++ Array/LeetCode 2432 - TheEmployeeThatWorkedOnTheLongestTask/TheEmployee/TheEmployee/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TheEmployee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int HardestWorker(int n, int[][] logs)
+        {
+            int res = logs[0][0], max = logs[0][1];
+            for (int i = 1; i < logs.Length; i++)
+            {
+                var time = logs[i][1] - logs[i - 1][1];
+                if (time > max || (time == max && logs[i][0] < res))
+                    res = logs[i][0];
+                max = Math.Max(max, time);
+            }
+            return res;
+        }
+    }
+}
