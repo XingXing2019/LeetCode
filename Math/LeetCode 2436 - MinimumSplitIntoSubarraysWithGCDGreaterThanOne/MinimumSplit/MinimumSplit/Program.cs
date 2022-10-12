2ec61a644111d796e2e1300785dcc8d0701747e9
Xing <x.xing.unsw@gmail.com>
2022-10-12 18:03:16 +1100

    init

---
+++ Math/LeetCode 2436 - MinimumSplitIntoSubarraysWithGCDGreaterThanOne/MinimumSplit/MinimumSplit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MinimumSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 12, 3, 14 };
+            Console.WriteLine(MinimumSplits(nums));
+        }
+
+        public static int MinimumSplits(int[] nums)
+        {
+            int index = 1, res = 1, gcd = nums[0];
+            while (index < nums.Length)
+            {
+                gcd = GCD(gcd, nums[index]);
+                if (gcd == 1)
+                {
+                    res++;
+                    gcd = nums[index];
+                }
+                index++;
+            }
+            return res;
+        }
+
+        public static int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+    }
+}
