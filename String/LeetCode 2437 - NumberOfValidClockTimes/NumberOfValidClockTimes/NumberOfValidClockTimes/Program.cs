0af786819e8639b7b8a8281fcc2f3208bd9fe2ca
Xing <x.xing.unsw@gmail.com>
2022-10-16 09:48:05 +1100

    init

---
+++ String/LeetCode 2437 - NumberOfValidClockTimes/NumberOfValidClockTimes/NumberOfValidClockTimes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberOfValidClockTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountTime(string time)
+        {
+            var res = 1;
+            if (time[0] == '?')
+                res *= time[1] == '?' ? 24 : time[1] - '0' < 4 ? 3 : 2;
+            else if (time[1] == '?')
+                res *= time[0] == '?' || time[0] - '0' < 2 ? 10 : 4;
+            if (time[3] == '?')
+                res *= 6;
+            if (time[4] == '?')
+                res *= 10;
+            return res;
+        }
+    }
+}
