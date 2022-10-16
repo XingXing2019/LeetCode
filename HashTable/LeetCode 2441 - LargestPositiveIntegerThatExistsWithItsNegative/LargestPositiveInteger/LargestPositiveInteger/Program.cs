ab7ae789fc1c88f8a342a005c067eb624d51dc95
Xing <x.xing.unsw@gmail.com>
2022-10-16 16:12:38 +1100

    init

---
+++ HashTable/LeetCode 2441 - LargestPositiveIntegerThatExistsWithItsNegative/LargestPositiveInteger/LargestPositiveInteger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LargestPositiveInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int FindMaxK(int[] nums)
+        {
+            HashSet<int> pos = new HashSet<int>(), neg = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (num > 0)
+                    pos.Add(num);
+                else
+                    neg.Add(-num);
+            }
+            var res = -1;
+            foreach (var num in pos)
+            {
+                if (!neg.Contains(num)) continue;
+                res = Math.Max(res, num);
+            }
+            return res;
+        }
+    }
+}
