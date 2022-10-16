e3df3b37117e860ac8efbf3c82d3f40f2e96c083
Xing <x.xing.unsw@gmail.com>
2022-10-16 16:43:46 +1100

    init

---
+++ HashTable/LeetCode 2443 - SumOfNumberAndItsReverse/SumOfNumberAndItsReverse/SumOfNumberAndItsReverse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SumOfNumberAndItsReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 100;
+            Console.WriteLine(SumOfNumberAndReverse(num));
+        }
+
+        public static bool SumOfNumberAndReverse(int num)
+        {
+            if (num == 0) return true;
+            for (int i = 1; i <= num; i++)
+            {
+                if (!IsValid(i, num)) continue;
+                return true;
+            }
+            return false;
+        }
+        
+        public static bool IsValid(int num, int target)
+        {
+            int reverse = 0, copy = num;
+            while (copy != 0)
+            {
+                reverse = reverse * 10 + copy % 10;
+                copy /= 10;
+            }
+            if (num + reverse == target)
+                return true;
+            while (num % 10 != 0 && num + reverse <= target)
+            {
+                if (num + reverse == target)
+                    return true;
+                reverse *= 10;
+            }
+            return false;
+        }
+    }
+}
