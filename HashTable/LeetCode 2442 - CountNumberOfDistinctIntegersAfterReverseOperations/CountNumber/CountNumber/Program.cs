83ca719a7940a4a244abacd328a593560f84993d
Xing <x.xing.unsw@gmail.com>
2022-10-16 16:17:53 +1100

    init

---
+++ HashTable/LeetCode 2442 - CountNumberOfDistinctIntegersAfterReverseOperations/CountNumber/CountNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountDistinctIntegers(int[] nums)
+        {
+            var unique = new HashSet<int>(nums);
+            var res = new HashSet<int>();
+            foreach (var num in unique)
+            {
+                res.Add(num);
+                res.Add(Reverse(num));
+            }
+            return res.Count;
+        }
+
+        private int Reverse(int num)
+        {
+            var res = 0;
+            while (num != 0)
+            {
+                res = res * 10 + num % 10;
+                num /= 10;
+            }
+            return res;
+        }
+    }
+}
