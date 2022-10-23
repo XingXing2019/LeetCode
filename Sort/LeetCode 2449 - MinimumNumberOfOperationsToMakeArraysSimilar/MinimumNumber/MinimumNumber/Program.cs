3490bf6eb01a283243f6b42749bb84a03c927b93
Xing <x.xing.unsw@gmail.com>
2022-10-23 19:39:55 +1100

    init

---
+++ Sort/LeetCode 2449 - MinimumNumberOfOperationsToMakeArraysSimilar/MinimumNumber/MinimumNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimumNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long MakeSimilar(int[] nums, int[] target)
+        {
+            Array.Sort(nums);
+            Array.Sort(target);
+            long diff = 0;
+            var odd = new Queue<int>();
+            var even = new Queue<int>();
+            foreach (var num in nums)
+            {
+                if (num % 2 != 0)
+                    odd.Enqueue(num);
+                else
+                    even.Enqueue(num);
+            }
+            foreach (var num in target)
+                diff += num % 2 != 0 ? Math.Abs(odd.Dequeue() - num) : Math.Abs(even.Dequeue() - num);
+            return diff / 4;
+        }
+    }
+}
