4ed89001bd658f9b8bb904b00b880965335dd154
Xing <x.xing.unsw@gmail.com>
2022-10-23 14:09:51 +1100

    init

---
+++ Tree/LeetCode 2445 - NumberOfNodesWithValueOne/NumberOfNodesWithValueOne/NumberOfNodesWithValueOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberOfNodesWithValueOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int NumberOfNodes(int n, int[] queries)
+        {
+            var freq = queries.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var dp = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+                dp[i] = dp[i / 2] + freq.GetValueOrDefault(i, 0);
+            return dp.Count(x => x % 2 != 0);
+        }
+    }
+}
