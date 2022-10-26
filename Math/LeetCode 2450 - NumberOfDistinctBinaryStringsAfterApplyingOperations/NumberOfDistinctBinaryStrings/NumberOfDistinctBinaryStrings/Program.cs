d8d59a04b121f3817ed8b832b6a4bbb68a4d8bcf
Xing <x.xing.unsw@gmail.com>
2022-10-26 17:54:22 +1100

    init

---
+++ Math/LeetCode 2450 - NumberOfDistinctBinaryStringsAfterApplyingOperations/NumberOfDistinctBinaryStrings/NumberOfDistinctBinaryStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumberOfDistinctBinaryStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "001010011110111000111101101101010000101010011100001";
+            var k = 8;
+            Console.WriteLine(CountDistinctStrings(s, k));
+        }
+
+        public static int CountDistinctStrings(string s, int k)
+        {
+            long num = s.Length - k + 1, mod = 1_000_000_000 + 7;
+            return (int)Pow(num, mod);
+        }
+
+        public static long Pow(long num, long mod)
+        {
+            if (num == 0) return 1;
+            var pow = Pow(num / 2, mod) % mod;
+            return num % 2 == 0 ? pow * pow % mod : pow * pow * 2 % mod;
+        }
+    }
+}
