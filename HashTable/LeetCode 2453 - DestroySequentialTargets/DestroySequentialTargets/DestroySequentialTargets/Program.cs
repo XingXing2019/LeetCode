913ff5ef46d9fbdba2bfe4d889ca01ec7221a029
Xing <x.xing.unsw@gmail.com>
2022-10-30 10:12:59 +1100

    init

---
+++ HashTable/LeetCode 2453 - DestroySequentialTargets/DestroySequentialTargets/DestroySequentialTargets/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DestroySequentialTargets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 304415643, 213512562, 667776528, 854523075, 291102158 };
+            var space = 4;
+            Console.WriteLine(DestroyTargets(nums, space));
+        }
+
+        public static int DestroyTargets(int[] nums, int space)
+        {
+            var freq = nums.GroupBy(x => x % space).ToDictionary(x => x.Key, x => x.ToList());
+            var max = freq.Max(x => x.Value.Count);
+            return freq.Where(x => x.Value.Count == max).SelectMany(x => x.Value).Min();
+        }
+    }
+}
