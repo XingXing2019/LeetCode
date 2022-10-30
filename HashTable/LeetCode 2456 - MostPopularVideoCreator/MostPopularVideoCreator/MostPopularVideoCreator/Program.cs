cb7ecc20f137cc57df27e83f65898f9c9db1be45
Xing <x.xing.unsw@gmail.com>
2022-10-30 15:44:26 +1100

    init

---
+++ HashTable/LeetCode 2456 - MostPopularVideoCreator/MostPopularVideoCreator/MostPopularVideoCreator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MostPopularVideoCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] creators = { "a", "a" };
+            string[] ids = { "aa", "a" };
+            int[] views = { 5, 5 };
+            Console.WriteLine(MostPopularCreator(creators, ids, views));
+        }
+
+        public static IList<IList<string>> MostPopularCreator(string[] creators, string[] ids, int[] views)
+        {
+            var popularity = new Dictionary<string, long>();
+            var videos = new Dictionary<string, long>();
+            long max = 0;
+            for (int i = 0; i < creators.Length; i++)
+            {
+                var creator = creators[i];
+                if (!popularity.ContainsKey(creator))
+                {
+                    popularity[creator] = 0;
+                    videos[creator] = i;
+                }
+                popularity[creator] += views[i];
+                if (views[i] > views[videos[creator]])
+                    videos[creator] = i;
+                else if (views[i] == views[videos[creator]] && ids[i].CompareTo(ids[videos[creator]]) < 0)
+                    videos[creator] = i;
+                max = Math.Max(max, popularity[creator]);
+            }
+            var res = new List<IList<string>>();
+            foreach (var creator in popularity.Keys)
+            {
+                if (popularity[creator] != max) continue;
+                res.Add(new List<string> { creator, ids[videos[creator]] });
+            }
+            return res;
+        }
+    }
+}
