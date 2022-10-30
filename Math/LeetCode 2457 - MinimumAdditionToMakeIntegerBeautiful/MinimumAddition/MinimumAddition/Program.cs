9dc4ebd08f4ba0aa656cb34e4c0f956f15ad8ca9
Xing <x.xing.unsw@gmail.com>
2022-10-30 16:13:55 +1100

    init

---
+++ Math/LeetCode 2457 - MinimumAdditionToMakeIntegerBeautiful/MinimumAddition/MinimumAddition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MinimumAddition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 495;
+            var target = 6;
+            Console.WriteLine(MakeIntegerBeautiful(n, target));
+        }
+
+        public static long MakeIntegerBeautiful(long n, int target)
+        {
+            long pow = 1, res = 0, sum = CalcSum(n);
+            while (sum > target)
+            {
+                var digit = n % 10;
+                var add = 10 - digit;
+                res += add * pow;
+                n = (n + add) / 10;
+                sum = CalcSum(n);
+                pow *= 10;
+            }
+            return res;
+        }
+
+        private static long CalcSum(long n)
+        {
+            long sum = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+}
