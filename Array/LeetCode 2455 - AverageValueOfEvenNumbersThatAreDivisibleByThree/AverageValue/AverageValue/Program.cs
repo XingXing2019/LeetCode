aeb592304538a9ec7de6cc6d046bcf95779ed951
Xing <x.xing.unsw@gmail.com>
2022-10-30 15:24:02 +1100

    init

---
+++ Array/LeetCode 2455 - AverageValueOfEvenNumbersThatAreDivisibleByThree/AverageValue/AverageValue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AverageValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int AverageValue(int[] nums)
+        {
+            int sum = 0, count = 0;
+            foreach (var num in nums)
+            {
+                if (num % 6 != 0) continue;
+                sum += num;
+                count++;
+            }
+            return count == 0 ? 0 : sum / count;
+        }
+    }
+}
