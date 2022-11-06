862e97c5be30f6f916ae2b3d2d60130e57a340a0
Xing <x.xing.unsw@gmail.com>
2022-11-06 15:24:47 +1100

    init

---
+++ Array/LeetCode 2461 - MaximumSumOfDistinctSubarraysWithLengthK/MaximumSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 9, 9, 1, 2, 3 };
+            Console.WriteLine(MaximumSubarraySum(nums, 3));
+        }
+
+        public static long MaximumSubarraySum(int[] nums, int k)
+        {
+            var prefix = new long[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+                prefix[i] = i == 0 ? nums[i] : nums[i] + prefix[i - 1];
+            long res = 0;
+            var set = new HashSet<int>();
+            for (int i = 0; i < k; i++)
+                set.Add(nums[i]);
+            for (int i = k - 1; i < prefix.Length; i++)
+            {
+                set.Add(nums[i]);
+                set.Add(nums[i - k + 1]);
+                if (set.Count == k)
+                    res = Math.Max(res, prefix[i] - (i == k - 1 ? 0 : prefix[i - k]));
+                set.Remove(nums[i - k + 1]);
+            }
+            return res;
+        }
+    }
+}
