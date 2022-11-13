99193f6cd9ccb2a9cb23df2d30dc8f5cbb995fe2
Xing <x.xing.unsw@gmail.com>
2022-11-13 14:14:37 +1100

    init

---
+++ Array/LeetCode 2465 - NumberOfDistinctAverages/NumberOfDistinctAverages/NumberOfDistinctAverages/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfDistinctAverages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int DistinctAverages(int[] nums)
+        {
+            var distinct = new HashSet<double>();
+            Array.Sort(nums);
+            int li = 0, hi = nums.Length - 1;
+            while (li < hi)
+            {
+                var avg = (double)(nums[li++] + nums[hi--]) / 2;
+                distinct.Add(avg);
+            }
+            return distinct.Count;
+        }
+    }
+}
---
+++ DynamicProgramming/LeetCode 2466 - CountWaysToBuildGoodStrings/CountWaysToBuildGoodStrings/CountWaysToBuildGoodStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace CountWaysToBuildGoodStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int low = 2, high = 3, zero = 1, one = 2;
+            Console.WriteLine(CountGoodStrings(low, high, zero, one));
+        }
+        public static int CountGoodStrings(int low, int high, int zero, int one)
+        {
+            var dp = new long[high + 1];
+            long res = 0, mod = 1_000_000_000 + 7;
+            dp[zero]++;
+            dp[one]++;
+            if (zero != one && one % zero == 0)
+                dp[one]++;
+            if (zero != one && zero % one == 0)
+                dp[zero]++;
+            for (int i = Math.Min(zero, one); i <= high; i++)
+            {
+                if (i == zero || i == one) continue;
+                if (i - zero >= 0 && dp[i - zero] != 0)
+                    dp[i] = (dp[i] + dp[i - zero]) % mod;
+                if (i - one >= 0 && dp[i - one] != 0)
+                    dp[i] = (dp[i] + dp[i - one]) % mod;
+            }
+            for (int i = low; i <= high; i++)
+                res = (res + dp[i]) % mod;
+            return (int)(res % mod);
+        }
+    }
+}
