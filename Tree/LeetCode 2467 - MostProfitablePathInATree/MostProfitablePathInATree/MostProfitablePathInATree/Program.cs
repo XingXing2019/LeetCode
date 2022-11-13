bccd014a367afc1fe47243bc90c8e22dce3d496f
Xing <x.xing.unsw@gmail.com>
2022-11-13 21:25:42 +1100

    init

--- Tree/LeetCode 2467 - MostProfitablePathInATree/MostProfitablePathInATree/MostProfitablePathInATree/Program.cs
+++ Tree/LeetCode 2467 - MostProfitablePathInATree/MostProfitablePathInATree/MostProfitablePathInATree/Program.cs
@@ -8,10 +8,22 @@ namespace MostProfitablePathInATree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][] edges = new int[][]
+            {
+                new[] { 0, 2 },
+                new[] { 1, 4 },
+                new[] { 1, 6 },
+                new[] { 2, 4 },
+                new[] { 3, 6 },
+                new[] { 3, 7 },
+                new[] { 5, 7 },
+            };
+            int bob = 4;
+            int[] amount = { -6, -6, -8, -8, 6, -4, -2, -8 };
+            Console.WriteLine(MostProfitablePath(edges, bob, amount));
         }
 
-        public int MostProfitablePath(int[][] edges, int bob, int[] amount)
+        public static int MostProfitablePath(int[][] edges, int bob, int[] amount)
         {
             var n = edges.Length + 1;
             var graph = new List<int>[n];
@@ -22,41 +34,44 @@ namespace MostProfitablePathInATree
                 graph[edge[0]].Add(edge[1]);
                 graph[edge[1]].Add(edge[0]);
             }
-            var queue = new Queue<int>();
-            queue.Enqueue(bob);
             var visited = new HashSet<int> { bob };
             var bobPath = new int[n];
-            var level = 0;
-            while (queue.Count != 0)
-            {
-                var count = queue.Count;
-                for (int i = 0; i < count; i++)
-                {
-                    var cur = queue.Dequeue();
-                    bobPath[cur] = level;
-                    foreach (var next in graph[cur])
-                    {
-                        if (!visited.Add(next)) continue;
-                        queue.Enqueue(next);
-                    }
-                }
-                level++;
-            }
-            return DFS(graph, 0, 0, bobPath, amount, 0, new bool[n]);
+            Array.Fill(bobPath, int.MaxValue);
+            bobPath[bob] = 0;
+            var found = false;
+            BobPath(graph, bob, visited, bobPath, ref found);
+            return AlicePath(graph, 0, bobPath, amount, 0, new HashSet<int> { 0 });
         }
 
-        public int DFS(List<int>[] graph, int cur, int sum, int[] bobPath, int[] amount, int level, bool[] visited)
+        public static int AlicePath(List<int>[] graph, int cur, int[] bobPath, int[] amount, int level, HashSet<int> visited)
         {
-            if (visited[cur])
-                return 0;
-            if (level < bobPath[cur])
-                sum += amount[cur];
-            else if (level == bobPath[cur])
-                sum += amount[cur] / 2;
-            var max = 0;
+            var sum = level < bobPath[cur] ? amount[cur] : level == bobPath[cur] ? amount[cur] / 2 : 0;
+            var max = int.MinValue;
             foreach (var next in graph[cur])
             {
-                
+                if (!visited.Add(next)) continue;
+                max = Math.Max(max, AlicePath(graph, next, bobPath, amount, level + 1, visited));
+                visited.Remove(next);
+            }
+            return max == int.MinValue ? sum : sum + max;
+        }
+
+        public static void BobPath(List<int>[] graph, int cur, HashSet<int> visited, int[] bobPath, ref bool found)
+        {
+            if (found) return;
+            if (cur == 0)
+            {
+                var level = 0;
+                foreach (var node in visited)
+                    bobPath[node] = level++;
+                found = true;
+                return;
+            }
+            foreach (var next in graph[cur])
+            {
+                if (!visited.Add(next)) continue;
+                BobPath(graph, next, visited, bobPath, ref found);
+                visited.Remove(next);
             }
         }
     }
---
+++ Tree/LeetCode 2471 - MinimumNumberOfOperationsToSortABinaryTreeByLevel/MinimumNumberOfOperations/MinimumNumberOfOperations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimumNumberOfOperations
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinimumOperations(TreeNode root)
+        {
+            var res = 0;
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                var count = queue.Count;
+                var nodes = new List<int>();
+                var copy = new List<int>();
+                var noMatch = 0;
+                for (int i = 0; i < count; i++)
+                {
+                    var cur = queue.Dequeue();
+                    nodes.Add(cur.val);
+                    copy.Add(cur.val);
+                    if (cur.left != null) queue.Enqueue(cur.left);
+                    if (cur.right != null) queue.Enqueue(cur.right);
+                }
+                copy.Sort();
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    if (nodes[i] == copy[i]) continue;
+                    noMatch++;
+                }
+                res += (int)Math.Ceiling((double)noMatch / 2);
+            }
+            return res;
+        }
+    }
+}
