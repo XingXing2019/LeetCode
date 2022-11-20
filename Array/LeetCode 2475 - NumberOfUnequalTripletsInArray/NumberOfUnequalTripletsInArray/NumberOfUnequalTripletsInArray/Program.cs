090384cb2d7719b5c13453b47fa55c9c282a5ff3
Xing <x.xing.unsw@gmail.com>
2022-11-20 15:10:08 +1100

    init

---
+++ Array/LeetCode 2475 - NumberOfUnequalTripletsInArray/NumberOfUnequalTripletsInArray/NumberOfUnequalTripletsInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberOfUnequalTripletsInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int UnequalTriplets(int[] nums)
+        {
+            var res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j]) continue;
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        if (nums[k] == nums[j] || nums[k] == nums[i]) continue;
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
