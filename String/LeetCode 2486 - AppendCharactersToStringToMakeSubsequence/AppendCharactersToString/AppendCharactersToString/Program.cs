df8ae7607ad18bb0b10ce628e0178231b76a28c9
Xing <x.xing.unsw@gmail.com>
2022-11-27 22:09:33 +1100

    init

---
+++ String/LeetCode 2486 - AppendCharactersToStringToMakeSubsequence/AppendCharactersToString/AppendCharactersToString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AppendCharactersToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int AppendCharacters(string s, string t)
+        {
+            int p1 = 0, p2 = 0;
+            while (p1 < s.Length && p2 < t.Length)
+            {
+                if (s[p1] == t[p2])
+                    p2++;
+                p1++;
+            }
+            return t.Length - p2;
+        }
+    }
+}
