1376e701ec4c2f4949be6e7c85af070e62797ab7
Xing <x.xing.unsw@gmail.com>
2022-11-27 20:16:49 +1100

    init

---
+++ Math/LeetCode 2481 - MinimumCutsToDivideACircle/MinimumCutsToDivideACircle/MinimumCutsToDivideACircle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MinimumCutsToDivideACircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int NumberOfCuts(int n)
+        {
+            return n == 1 ? 0 : n % 2 == 0 ? n / 2 : n;
+        }
+    }
+}
