1cb257e929bab04edb1e09f320df1b94a0b88410
Xing <x.xing.unsw@gmail.com>
2022-11-27 21:53:23 +1100

    init

---
+++ HashTable/LeetCode 2483 - MinimumPenaltyForAShop/MinimumPenaltyForAShop/MinimumPenaltyForAShop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MinimumPenaltyForAShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customers = "YNYY";
+            Console.WriteLine(BestClosingTime(customers));
+        }
+
+        public static int BestClosingTime(string customers)
+        {
+            var open = new int[customers.Length + 1];
+            var close = new int[customers.Length + 1];
+            int openPenalty = 0, closePenalty = 0;
+            for (int i = 0; i < customers.Length; i++)
+            {
+                open[i] = openPenalty;
+                openPenalty += customers[i] == 'N' ? 1 : 0;
+                closePenalty += customers[^(i + 1)] == 'Y' ? 1 : 0;
+                close[^(i + 2)] = closePenalty;
+            }
+            open[^1] = openPenalty;
+            int res = 0, min = int.MaxValue;
+            for (int i = 0; i <= customers.Length; i++)
+            {
+                if (open[i] + close[i] >= min) continue;
+                res = i;
+                min = open[i] + close[i];
+            }
+            return res;
+        }
+    }
+}
