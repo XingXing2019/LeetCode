d19f183230f0aabbb215c3a0c2e050f6782012ac
Xing <x.xing.unsw@gmail.com>
2022-11-27 22:01:51 +1100

    init

---
+++ Math/LeetCode 2485 - FindThePivotInteger/FindThePivotInteger/FindThePivotInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FindThePivotInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int PivotInteger(int n)
+        {
+            var sqrt = Math.Sqrt((double)(n * n + n) / 2);
+            return sqrt == (int)sqrt ? (int)sqrt : -1;
+        }
+    }
+}
