6903d5debfa093da0364e41e57b4c430e6cde1fd
Xing <x.xing.unsw@gmail.com>
2022-12-04 16:05:48 +1100

    init

---
+++ String/LeetCode 2490 - CircularSentence/CircularSentence/CircularSentence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CircularSentence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsCircularSentence(string sentence)
+        {
+            if (sentence[0] != sentence[^1])
+                return false;
+            var words = sentence.Split(' ');
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i - 1][^1] != words[i][0])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
