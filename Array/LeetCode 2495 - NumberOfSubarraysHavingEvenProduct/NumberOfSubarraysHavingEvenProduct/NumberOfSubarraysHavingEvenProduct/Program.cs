0114b8db5def510b64d3f8112e769a1cde745e86
Xing <x.xing.unsw@gmail.com>
2022-12-07 19:36:25 +1100

    init

---
+++ Array/LeetCode 2495 - NumberOfSubarraysHavingEvenProduct/NumberOfSubarraysHavingEvenProduct/NumberOfSubarraysHavingEvenProduct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumberOfSubarraysHavingEvenProduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 9, 6, 7, 13 };
+            Console.WriteLine(EvenProduct(nums));
+        }
+        public static long EvenProduct(int[] nums)
+        {
+            long res = 0, lastEven = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res += nums[i] % 2 == 0 ? i + 1 : lastEven + 1;
+                lastEven = nums[i] % 2 == 0 ? i : lastEven;
+            }
+            return res;
+        }
+    }
+}
