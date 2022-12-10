6f37c27e1f14ead07b60ea02278d2b8a1bc35f7b
Xing <x.xing.unsw@gmail.com>
2022-12-11 09:30:59 +1100

    init

---
+++ String/LeetCode 2496 - MaximumValueOfAStringInAnArray/MaximumValueOfAStringInAnArray/MaximumValueOfAStringInAnArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaximumValueOfAStringInAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaximumValue(string[] strs)
+        {
+            var res = 0;
+            foreach (var str in strs)
+            {
+                var len = int.TryParse(str, out var temp) ? temp : str.Length;
+                res = Math.Max(res, len);
+            }
+            return res;
+        }
+    }
+}
