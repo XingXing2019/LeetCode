8c53f0aadcef35c093c38422fdd24c8fd42f1cfb
Xing <x.xing.unsw@gmail.com>
2022-12-11 09:41:45 +1100

    init

---
+++ Graph/LeetCode 2497 - MaximumStarSumOfAGraph/MaximumStarSumOfAGraph/MaximumStarSumOfAGraph/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumStarSumOfAGraph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaxStarSum(int[] vals, int[][] edges, int k)
+        {
+            var graph = new List<int>[vals.Length];
+            for (int i = 0; i < vals.Length; i++)
+                graph[i] = new List<int>();
+            foreach (var edge in edges)
+            {
+                graph[edge[0]].Add(edge[1]);
+                graph[edge[1]].Add(edge[0]);
+            }
+            var res = int.MinValue;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                var neighbors = graph[i].Where(next => vals[next] > 0).Select(next => vals[next]).OrderByDescending(x => x).Take(k).ToList();
+                res = Math.Max(res, vals[i] + neighbors.Sum());
+            }
+            return res;
+        }
+    }
+}
