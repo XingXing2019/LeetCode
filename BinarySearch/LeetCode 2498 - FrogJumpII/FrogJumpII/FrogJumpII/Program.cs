680c23389ad52340c44127268c2ba547ece75c74
Xing <x.xing.unsw@gmail.com>
2022-12-11 10:23:55 +1100

    init

---
+++ BinarySearch/LeetCode 2498 - FrogJumpII/FrogJumpII/FrogJumpII/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FrogJumpII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = { 0, 2, 5, 6, 7 };
+            Console.WriteLine(MaxJump(stones));
+        }
+
+        public static int MaxJump(int[] stones)
+        {
+            var max = 0;
+            for (int i = 2; i < stones.Length; i += 2)
+                max = Math.Max(max, stones[i] - stones[i - 2]);
+            for (int i = 3; i < stones.Length; i += 2)
+                max = Math.Max(max, stones[i] - stones[i - 2]);
+            return Math.Max(max, Math.Max(stones[^1] - stones[^2], stones[1] - stones[0]));
+        }
+    }
+}
