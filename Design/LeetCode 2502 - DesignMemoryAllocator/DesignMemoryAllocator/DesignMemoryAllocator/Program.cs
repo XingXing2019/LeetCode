4149ed98d135c427e20f70612b1f5ff1b255c91f
Xing <x.xing.unsw@gmail.com>
2022-12-11 16:25:42 +1100

    init

---
+++ Design/LeetCode 2502 - DesignMemoryAllocator/DesignMemoryAllocator/DesignMemoryAllocator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace DesignMemoryAllocator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var allocator = new Allocator(10);
+            allocator.Allocate(1, 1);
+            allocator.Allocate(1, 2);
+            allocator.Allocate(1, 3);
+            allocator.Free(2);
+            allocator.Allocate(3, 4);
+            allocator.Allocate(1, 1);
+            allocator.Allocate(1, 1);
+            allocator.Free(1);
+            allocator.Allocate(10, 2);
+            allocator.Free(7);
+        }
+    }
+
+    public class Allocator
+    {
+        private int[] memory;
+        public Allocator(int n)
+        {
+            memory = new int[n];
+        }
+
+        public int Allocate(int size, int mID)
+        {
+            int index = 0;
+            while (index < memory.Length)
+            {
+                var temp = size;
+                while (index < memory.Length && memory[index] == 0 && temp != 0)
+                {
+                    index++;
+                    temp--;
+                }
+                if (temp != 0)
+                {
+                    index++;
+                    continue;
+                }
+                while (temp != size)
+                {
+                    memory[--index] = mID;
+                    temp++;
+                }
+                return index;
+            }
+            return -1;
+        }
+
+        public int Free(int mID)
+        {
+            var count = 0;
+            for (int i = 0; i < memory.Length; i++)
+            {
+                if (memory[i] != mID) continue;
+                memory[i] = 0;
+                count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ HashTable/LeetCode 2501 - LongestSquareStreakInAnArray/LongestSquareStreakInAnArray/LongestSquareStreakInAnArray/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestSquareStreakInAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, 3, 6, 16, 8, 2 };
+            Console.WriteLine(LongestSquareStreak(nums));
+        }
+
+        public static int LongestSquareStreak(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            var subsequences = new List<List<int>>();
+            var res = -1;
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num)) continue;
+                var pow = num * num;
+                var sqrt = Math.Sqrt(num);
+                var index = -1;
+                if (dict.ContainsKey(pow))
+                    index = dict[pow];
+                else if (IsPerfectSqrt(num) && dict.ContainsKey((int)sqrt))
+                    index = dict[(int)sqrt];
+                else
+                    index = subsequences.Count;
+                if (index == subsequences.Count)
+                    subsequences.Add(new List<int>());
+                subsequences[index].Add(num);
+                dict[num] = index;
+            }
+            foreach (var subsequence in subsequences)
+            {
+                if (subsequence.Count == 1) continue;
+                res = Math.Max(res, subsequence.Count);
+            }
+            return res;
+        }
+
+        private static bool IsPerfectSqrt(int num, out int sqrt)
+        {
+            sqrt = -1;
+            for (int i = 1; i < Math.Sqrt(num); i++)
+            {
+                if (i * i == num)
+                {
+                    sqrt = i;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
