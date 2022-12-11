a21b92de50fcbb443a0029935fb639d3b6b51f01
Xing <x.xing.unsw@gmail.com>
2022-12-11 15:43:39 +1100

    init

---
+++ Sort/LeetCode 2500 - DeleteGreatestValueInEachRow/DeleteGreatestValueInEachRow/DeleteGreatestValueInEachRow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DeleteGreatestValueInEachRow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int DeleteGreatestValue(int[][] grid)
+        {
+            var res = 0;
+            foreach (var row in grid)
+                Array.Sort(row);
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                var max = grid[0][j];
+                for (int i = 0; i < grid.Length; i++)
+                    max = Math.Max(max, grid[i][j]);
+                res += max;
+            }
+            return res;
+        }
+    }
+}
