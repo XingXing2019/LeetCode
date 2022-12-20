dc4d847489289a120e437107a81aba7dc4455677
Xing <x.xing.unsw@gmail.com>
2022-12-20 16:28:12 +1100

    init

---
+++ HashTable/LeetCode 2506 - CountPairsOfSimilarStrings/CountPairsOfSimilarStrings/CountPairsOfSimilarStrings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountPairsOfSimilarStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "aba", "aabb", "abcd", "bac", "aabc" };
+            Console.WriteLine(SimilarPairs(words));
+        }
+
+        public static int SimilarPairs(string[] words)
+        {
+            var dict = new Dictionary<int, int>();
+            var res = 0;
+            foreach (var word in words)
+            {
+                var code = Encoding(word);
+                if (!dict.ContainsKey(code))
+                    dict[code] = 0;
+                dict[code]++;
+            }
+            foreach (var freq in dict.Values)
+                res += freq * (freq - 1) / 2;
+            return res;
+        }
+
+        public static int Encoding(string word)
+        {
+            var code = 0;
+            foreach (var l in word)
+                code |= 1 << (l - 'a');
+            return code;
+        }
+    }
+}
