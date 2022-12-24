a1883526918b837dc0c5cb437b49fbd2ba951ce2
Xing <x.xing.unsw@gmail.com>
2022-12-24 20:49:05 +1100

    init

---
+++ DynamicProgramming/LeetCode 790 - DominoAndTrominoTiling/DominoAndTrominoTiling/DominoAndTrominoTiling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DominoAndTrominoTiling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int NumTilings(int n)
+        {
+            long mod = 1_000_000_000 + 7;
+            var dp = new long[n + 1][];
+            for (int i = 0; i < dp.Length; i++)
+                dp[i] = new long[2];
+            dp[0][0] = 1;
+            dp[1][0] = 1;
+            for (int i = 2; i < dp.Length; i++)
+            {
+                dp[i][0] = (dp[i - 1][0] + dp[i - 2][0] + 2 * dp[i - 1][1]) % mod;
+                dp[i][1] = (dp[i - 2][0] + dp[i - 1][1]) % mod;
+            }
+            return (int)(dp[^1][0] % mod);
+        }
+    }
+}
