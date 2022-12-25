1df5a92ff6d5a05a66b5b736b4ea560684c34c3e
Xing <x.xing.unsw@gmail.com>
2022-12-26 09:39:14 +1100

    init

---
+++ HashTable/LeetCode 2512 - RewardTopKStudents/RewardTopKStudents/RewardTopKStudents/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RewardTopKStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IList<int> TopStudents(string[] positive_feedback, string[] negative_feedback, string[] report, int[] student_id, int k)
+        {
+            HashSet<string> pos = new HashSet<string>(positive_feedback), neg = new HashSet<string>(negative_feedback);
+            var scores = new Dictionary<int, int>();
+            for (int i = 0; i < report.Length; i++)
+            {
+                var words = report[i].Split(' ');
+                var score = 0;
+                foreach (var word in words)
+                {
+                    if (pos.Contains(word))
+                        score += 3;
+                    else if (neg.Contains(word))
+                        score -= 1;
+                }
+                scores[student_id[i]] = score;
+            }
+            return scores.OrderByDescending(x => x.Value).ThenBy(x => x.Key).Select(x => x.Key).Take(k).ToList();
+        }
+    }
+}
