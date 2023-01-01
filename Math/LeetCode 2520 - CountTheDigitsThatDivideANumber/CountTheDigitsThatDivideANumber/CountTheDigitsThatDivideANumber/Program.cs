745c73785bf9f4a516f48c62b5a83a67c97a5871
Xing <x.xing.unsw@gmail.com>
2023-01-01 21:19:38 +1100

    init

---
+++ Math/LeetCode 2520 - CountTheDigitsThatDivideANumber/CountTheDigitsThatDivideANumber/CountTheDigitsThatDivideANumber/Program.cs
@@ -0,0 +1,12 @@
+﻿int CountDigits(int num)
+{
+    int copy = num, res = 0;
+    while (copy != 0)
+    {
+        var digit = copy % 10;
+        if (num % digit == 0)
+            res++;
+        copy /= 10;
+    }
+    return res;
+}
