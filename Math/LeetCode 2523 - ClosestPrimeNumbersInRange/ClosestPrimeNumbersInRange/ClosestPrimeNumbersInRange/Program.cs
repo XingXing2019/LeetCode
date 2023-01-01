dfc40f18631b570882a8a5b715c8d4261ac23688
Xing <x.xing.unsw@gmail.com>
2023-01-01 22:24:51 +1100

    init

---
+++ Math/LeetCode 2523 - ClosestPrimeNumbersInRange/ClosestPrimeNumbersInRange/ClosestPrimeNumbersInRange/Program.cs
@@ -0,0 +1,39 @@
+﻿int left = 10, right = 19;
+Console.WriteLine(ClosestPrimes(left, right));
+
+int[] ClosestPrimes(int left, int right)
+{
+    var primes = GetPrimes(right);
+    int[] res = { -1, -1 };
+    var diff = int.MaxValue;
+    for (int i = 1; i < primes.Count; i++)
+    {
+        if (primes[i - 1] < left) continue;
+        if (primes[i] - primes[i - 1] < diff)
+        {
+            diff = primes[i] - primes[i - 1];
+            res = new[] { primes[i - 1], primes[i] };
+        }
+    }
+    return res;
+}
+
+List<int> GetPrimes(int n)
+{
+    var dp = new bool[n + 1];
+    Array.Fill(dp, true);
+    var primes = new List<int>();
+    for (int i = 2; i * i <= n; i++)
+    {
+        for (int j = 2; i * j <= n; j++)
+        {
+            dp[i * j] = false;
+        }
+    }
+    for (int i = 2; i < dp.Length; i++)
+    {
+        if (!dp[i]) continue;
+        primes.Add(i);
+    }
+    return primes;
+}
