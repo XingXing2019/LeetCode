556389582eb7c0a95b4a0925836ac88334d290f4
Xing <x.xing.unsw@gmail.com>
2023-01-01 21:55:20 +1100

    init

---
+++ Greedy/LeetCode 2522 - PartitionStringIntoSubstringsWithValuesAtMostK/PartitionString/PartitionString/Program.cs
@@ -0,0 +1,22 @@
+﻿var s = "165462";
+var k = 60;
+Console.WriteLine(MinimumPartition(s, k));
+
+int MinimumPartition(string s, int k)
+{
+    long num = 0;
+    int res = 0, digit = 0;
+    foreach (var l in s)
+    {
+        digit = l - '0';
+        num = num * 10 + digit;
+        if (num > k)
+        {
+            res++;
+            num = digit;
+        }
+        if (num > k)
+            return -1;
+    }
+    return num > k ? -1 : res + 1;
+}
