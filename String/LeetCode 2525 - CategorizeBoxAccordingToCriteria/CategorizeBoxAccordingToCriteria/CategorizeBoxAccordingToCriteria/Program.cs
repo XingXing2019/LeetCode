acd46aa92333a84a7bf85e5ca800dd3f29a009eb
Xing <x.xing.unsw@gmail.com>
2023-01-08 08:40:28 +1100

    init

---
+++ String/LeetCode 2525 - CategorizeBoxAccordingToCriteria/CategorizeBoxAccordingToCriteria/CategorizeBoxAccordingToCriteria/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CategorizeBoxAccordingToCriteria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string CategorizeBox(int length, int width, int height, int mass)
+        {
+            int dimensionLimit = 10_000, volLimit = 1_000_000_000, massLimit = 100;
+            var isBulk = length >= dimensionLimit || width >= dimensionLimit || height >= dimensionLimit || (long)length * width * height >= volLimit;
+            var isHeavy = mass >= massLimit;
+            if (isBulk && isHeavy)
+                return "Both";
+            if (!isBulk && !isHeavy)
+                return "Neither";
+            return isBulk ? "Bulky" : "Heavy";
+        }
+    }
+}
