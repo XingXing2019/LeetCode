556733aabb2d8d08a4d18732bd42182ea3e75a93
Xing <x.xing.unsw@gmail.com>
2023-01-08 08:45:53 +1100

    init

---
+++ Design/LeetCode 2526 - FindConsecutiveIntegersFromADataStream/FindConsecutiveIntegers/FindConsecutiveIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FindConsecutiveIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class DataStream
+    {
+        private readonly int _value;
+        private readonly int _k;
+        private int _count;
+
+        public DataStream(int value, int k)
+        {
+            _value = value;
+            _k = k;
+        }
+
+        public bool Consec(int num)
+        {
+            _count = num == _value ? _count + 1 : 0;
+            return _count >= _k;
+        }
+    }
+}
