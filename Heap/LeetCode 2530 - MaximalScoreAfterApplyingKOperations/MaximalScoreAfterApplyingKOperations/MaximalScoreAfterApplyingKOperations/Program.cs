7bf182ecfaefe76cf55cdb678f824a109dbadedf
Xing <x.xing.unsw@gmail.com>
2023-01-08 15:55:05 +1100

    init

---
+++ Heap/LeetCode 2530 - MaximalScoreAfterApplyingKOperations/MaximalScoreAfterApplyingKOperations/MaximalScoreAfterApplyingKOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿long MaxKelements(int[] nums, int k)
+{
+    var maxHeap = new PriorityQueue<double, double>();
+    foreach (var num in nums)
+        maxHeap.Enqueue(num, -num);
+    double res = 0;
+    for (int i = 0; i < k; i++)
+    {
+        var max = maxHeap.Dequeue();
+        res += max;
+        max = Math.Ceiling(max / 3);
+        maxHeap.Enqueue(max, -max);
+    }
+    return (long)res;
+}
