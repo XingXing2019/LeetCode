909ff0598210b4e5132cae1a12ba83dab363400d
Xing <x.xing.unsw@gmail.com>
2023-01-08 15:47:31 +1100

    init

---
+++ BinarySearch/LeetCode 2529 - MaximumCountOfPositiveIntegerAndNegativeInteger/MaximumCount/MaximumCount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MaximumCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaximumCount(int[] nums)
+        {
+            var neg = BinarySearch(nums, true);
+            var pos = nums.Length - BinarySearch(nums, false);
+            return Math.Max(neg, pos);
+        }
+
+        public int BinarySearch(int[] nums, bool isFirst)
+        {
+            int li = 0, hi = nums.Length - 1;
+            while (li <= hi)
+            {
+                var mid = li + (hi - li) / 2;
+                if (isFirst && nums[mid] >= 0 || !isFirst && nums[mid] > 0)
+                    hi = mid - 1;
+                else
+                    li = mid + 1;
+            }
+            return li;
+        }
+    }
+}
