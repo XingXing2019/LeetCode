ef0e1bbf4b041ddf1b683faa8b2b873d90f96376
Xing <x.xing.unsw@gmail.com>
2023-01-15 15:56:55 +1100

    init

---
+++ Array/LeetCode 2535 - DifferenceBetweenElementSumAndDigitSumOfAnArray/DifferenceBetween/DifferenceBetween/Program.cs
@@ -0,0 +1,14 @@
+﻿int DifferenceOfSum(int[] nums)
+{
+    int elementSum = 0, digitSum = 0;
+    for (var i = 0; i < nums.Length; i++)
+    {
+        elementSum += nums[i];
+        while (nums[i] != 0)
+        {
+            digitSum += nums[i] % 10;
+            nums[i] /= 10;
+        }
+    }
+    return Math.Abs(elementSum - digitSum);
+}
