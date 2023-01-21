04f03750f2d6b2ff7c87833d3da16a5016880439
Xing <x.xing.unsw@gmail.com>
2023-01-22 09:30:22 +1100

    init

---
+++ Greedy/LeetCode 2541 - MinimumOperationsToMakeArrayEqualII/MinimumOperationsToMakeArrayEqualII/MinimumOperationsToMakeArrayEqualII/Program.cs
@@ -0,0 +1,19 @@
+﻿long MinOperations(int[] nums1, int[] nums2, int k)
+{
+    long pos = 0, neg = 0;
+	for (int i = 0; i < nums1.Length; i++)
+	{
+		if (k == 0 && nums1[i] != nums2[i])
+			return -1;
+		var diff = nums1[i] - nums2[i];
+		if (k  != 0 && diff % k != 0)
+			return -1;
+		if (diff >= 0)
+			pos += diff;
+		else
+			neg += diff;
+	}
+	if (pos + neg != 0)
+		return -1;
+	return k == 0 ? 0 : pos / k;
+}
