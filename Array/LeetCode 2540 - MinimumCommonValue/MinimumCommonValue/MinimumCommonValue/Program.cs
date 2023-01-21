c4af41360f9d3c6cf3dafccf92f1d2fb25a3cb87
Xing <x.xing.unsw@gmail.com>
2023-01-22 09:23:27 +1100

    init

---
+++ Array/LeetCode 2540 - MinimumCommonValue/MinimumCommonValue/MinimumCommonValue/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetCommon(int[] nums1, int[] nums2)
+{
+    int p1 = 0, p2 = 0;
+    while (p1 < nums1.Length && p2 < nums2.Length)
+    {
+        if (nums1[p1] == nums2[p2])
+            return nums1[p1];
+        if (nums1[p1] < nums2[p2])
+            p1++;
+        else
+            p2++;
+    }
+    return -1;
+}
