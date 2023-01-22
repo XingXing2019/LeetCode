5a4e9b9f633061417b7b2dd763e3a59f5c09465c
Xing <x.xing.unsw@gmail.com>
2023-01-22 15:26:20 +1100

    init

---
+++ Math/LeetCode 2544 - AlternatingDigitSum/AlternatingDigitSum/AlternatingDigitSum/Program.cs
@@ -0,0 +1,11 @@
+﻿var n = 521;
+Console.WriteLine(AlternateDigitSum(n));
+
+int AlternateDigitSum(int n)
+{
+    var res = 0;
+    var num = n.ToString();
+    for (int i = 0; i < num.Length; i++)
+        res += i % 2 == 0 ? num[i] - '0' : -(num[i] - '0');
+    return res;
+}
