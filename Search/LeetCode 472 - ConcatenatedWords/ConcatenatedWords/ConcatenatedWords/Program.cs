651aba038a84480f6864032dd01a4b23e8a36a36
Xing <x.xing.unsw@gmail.com>
2023-01-27 20:03:27 +1100

    init

---
+++ Search/LeetCode 472 - ConcatenatedWords/ConcatenatedWords/ConcatenatedWords/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+string[] words = { "cat", "dog", "catdog" };
+Console.WriteLine(FindAllConcatenatedWordsInADict(words));
+
+IList<string> FindAllConcatenatedWordsInADict(string[] words)
+{
+    var set = new HashSet<string>(words);
+    var res = new List<string>();
+    foreach (var word in words)
+    {
+        var n = word.Length;
+        var dp = new bool[n + 1];
+        Array.Fill(dp, false);
+        dp[0] = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (!dp[i]) continue;
+            for (int j = i + 1; j <= n; j++)
+            {
+                if (j - i < n && set.Contains(word.Substring(i, j - i)))
+                {
+                    dp[j] = true;
+                }
+            }
+            if (dp[n])
+            {
+                res.Add(word);
+                break;
+            }
+        }
+    }
+    return res;
+}
