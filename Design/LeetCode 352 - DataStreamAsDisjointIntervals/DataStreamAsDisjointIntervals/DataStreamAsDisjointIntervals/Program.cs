90144649d6fac8084947c4a54ba9ea70c528b6c9
Xing <x.xing.unsw@gmail.com>
2023-01-28 19:24:04 +1100

    init

---
+++ Design/LeetCode 352 - DataStreamAsDisjointIntervals/DataStreamAsDisjointIntervals/DataStreamAsDisjointIntervals/Program.cs
@@ -0,0 +1,46 @@
+﻿var summary = new SummaryRanges();
+summary.AddNum(1);
+summary.GetIntervals();
+summary.AddNum(3);
+summary.GetIntervals();
+summary.AddNum(7);
+summary.GetIntervals();
+summary.AddNum(2);
+summary.GetIntervals();
+summary.AddNum(6);
+summary.GetIntervals();
+
+public class SummaryRanges
+{
+    private SortedSet<int> stream;
+    public SummaryRanges()
+    {
+        stream = new SortedSet<int>();
+    }
+
+    public void AddNum(int value)
+    {
+        stream.Add(value);
+    }
+
+    public int[][] GetIntervals()
+    {
+        var res = new List<int[]>();
+        int start = -2, end = start;
+        var inserted = false;
+        foreach (var num in stream)
+        {
+            if (num != end + 1)
+            {
+                if (!inserted)
+                    inserted = true;
+                else
+                    res.Add(new[] { start, end });
+                start = num;
+            }
+            end = num;
+        }
+        res.Add(new[] { start, end });
+        return res.ToArray();
+    }
+}
