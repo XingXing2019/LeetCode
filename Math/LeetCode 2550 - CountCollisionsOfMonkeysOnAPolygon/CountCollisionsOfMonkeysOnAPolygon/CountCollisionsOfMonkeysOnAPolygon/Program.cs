2e37ad60777f1170120b1a87a6a356eaac787877
Xing <x.xing.unsw@gmail.com>
2023-01-29 19:52:12 +1100

    init

---
+++ Math/LeetCode 2550 - CountCollisionsOfMonkeysOnAPolygon/CountCollisionsOfMonkeysOnAPolygon/CountCollisionsOfMonkeysOnAPolygon/Program.cs
@@ -0,0 +1,17 @@
+﻿var n = 500000003;
+Console.WriteLine(MonkeyMove(n));
+
+int MonkeyMove(int n)
+{
+    long mod = 1_000_000_000 + 7;
+    var pow = GetPow(2, n, mod);
+    return (int)((pow - 2) % mod);
+}
+
+long GetPow(long x, long y, long mod)
+{
+    if (y == 0)
+        return 1;
+    var pow = GetPow(x, y / 2, mod) % mod;
+    return y % 2 == 0 ? pow * pow : pow * pow * x;
+}
