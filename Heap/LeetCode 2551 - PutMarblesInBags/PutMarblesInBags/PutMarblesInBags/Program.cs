479d23bc9e27b74c1852beac62f36db32f6c73de
Xing <x.xing.unsw@gmail.com>
2023-02-02 21:22:39 +1100

    init

---
+++ Heap/LeetCode 2551 - PutMarblesInBags/PutMarblesInBags/PutMarblesInBags/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] weights = { 1, 3, 5, 1 };
+var k = 2;
+Console.WriteLine(PutMarbles(weights, k));
+
+long PutMarbles(int[] weights, int k)
+{
+    var minHeap = new PriorityQueue<long, long>();
+    var maxHeap = new PriorityQueue<long, long>();
+    for (int i = 1; i < weights.Length; i++)
+    {
+        var sum = weights[i] + weights[i - 1];
+        minHeap.Enqueue(sum, -sum);
+        maxHeap.Enqueue(sum, sum);
+        if (minHeap.Count == k)
+            minHeap.Dequeue();
+        if (maxHeap.Count == k)
+            maxHeap.Dequeue();
+    }
+    long max = 0, min = 0;
+    while (minHeap.Count != 0)
+    {
+        max += maxHeap.Dequeue();
+        min += minHeap.Dequeue();
+    }
+    return max - min;
+}
