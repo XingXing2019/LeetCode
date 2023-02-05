7f1ecf9c16964b08b6dd2f91d9d88695c843b121
Xing <x.xing.unsw@gmail.com>
2023-02-05 12:57:09 +1100

    init

---
+++ Array/LeetCode 2553 - SeparateTheDigitsInAnArray/SeparateTheDigitsInAnArray/SeparateTheDigitsInAnArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SeparateTheDigitsInAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] SeparateDigits(int[] nums)
+        {
+            return nums.SelectMany(x => ToDigits(x.ToString())).ToArray();
+        }
+
+        private int[] ToDigits(string num)
+        {
+            var res = new int[num.Length];
+            for (int i = 0; i < num.Length; i++)
+                res[i] = num[i] - '0';
+            return res;
+        }
+    }
+}
