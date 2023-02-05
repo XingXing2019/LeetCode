6c6962b3de400a00c4a34cab0001840123c87e67
Xing <x.xing.unsw@gmail.com>
2023-02-05 19:56:05 +1100

    init

--- Search/LeetCode 2556 - DisconnectPathInABinaryMatrixByAtMostOneFlip/DisconnectPathInABinaryMatrix/DisconnectPathInABinaryMatrix/Program.cs
+++ Search/LeetCode 2556 - DisconnectPathInABinaryMatrixByAtMostOneFlip/DisconnectPathInABinaryMatrix/DisconnectPathInABinaryMatrix/Program.cs
@@ -20,28 +20,19 @@ namespace DisconnectPathInABinaryMatrix
 
         public static bool IsPossibleToCutPath(int[][] grid)
         {
-            if (grid.Length * grid[0].Length < 3) return false;
-            return DFS(grid, 0, 0, new HashSet<string>(), new List<HashSet<string>>());
+            if (!DFS(grid, 0, 0)) return true;
+            grid[0][0] = 1;
+            return !DFS(grid, 0, 0);
         }
 
-        private static bool DFS(int[][] grid, int x, int y, HashSet<string> path, List<HashSet<string>> paths)
+        private static bool DFS(int[][] grid, int x, int y)
         {
-            int m = grid.Length, n = grid[0].Length;
-            if (x >= m || y >= n || grid[x][y] == 0)
+            if (x >= grid.Length || y >= grid[0].Length || grid[x][y] == 0)
+                return false;
+            if (x == grid.Length - 1 && y == grid[0].Length - 1)
                 return true;
-            bool isStart = x == 0 && y == 0, isEnd = x == m - 1 && y == n - 1;
-            if (!isStart && !isEnd)
-                path.Add($"{x}:{y}");
-            if (x == m - 1 && y == n - 1)
-            {
-                if (paths.Any(x => x.All(x => !path.Contains(x))))
-                    return false;
-                paths.Add(new HashSet<string>(path));
-            }
-            var res = DFS(grid, x + 1, y, path, paths) && DFS(grid, x, y + 1, path, paths);
-            if (!isStart || !isEnd)
-                path.Remove($"{x}:{y}");
-            return res;
+            grid[x][y] = 0;
+            return DFS(grid, x + 1, y) || DFS(grid, x, y + 1);
         }
     }
 }
