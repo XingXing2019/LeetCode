f1f3e311b7c2060e4ab420ca51a54376b8f163db
Xing <x.xing.unsw@gmail.com>
2023-02-05 14:36:59 +1100

    init

---
+++ BinarySearch/LeetCode 2557 - MaximumNumberOfIntegersToChooseFromARangeII/MaximumNumberOfIntegers/MaximumNumberOfIntegers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MaximumNumberOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] banned = { 271404187 };
+            int n = 506731976;
+            long maxSum = 396871319768398;
+            Console.WriteLine(MaxCount(banned, n, maxSum));
+        }
+
+        public static int MaxCount(int[] banned, int n, long maxSum)
+        {
+            Array.Sort(banned);
+            long existingSum = 0;
+            var res = 0;
+            for (int i = 0; i < banned.Length; i++)
+            {
+                long start = i == 0 ? 1 : banned[i - 1] + 1;
+                long end = banned[i] - 1;
+                if (start > end) continue;
+                var count = BinarySearch(start, end, existingSum, maxSum);
+                res += count;
+                existingSum += (start + start + count - 1) * count / 2;
+            }
+            if (n - banned[^1] > 0)
+                res += BinarySearch(banned[^1] + 1, n, existingSum, maxSum);
+            return res;
+        }
+
+        private static int BinarySearch(long start, long end, long existingSum, long maxSum)
+        {
+            long li = 1, hi = end - start + 1;
+            while (li <= hi)
+            {
+                var mid = li + (hi - li) / 2;
+                var sum = (start + start + mid - 1) * mid / 2;
+                if (existingSum + sum > maxSum)
+                    hi = mid - 1;
+                else
+                    li = mid + 1;
+            }
+            return (int) hi;
+        }
+    }
+}
---
+++ Search/LeetCode 2556 - DisconnectPathInABinaryMatrixByAtMostOneFlip/DisconnectPathInABinaryMatrix/DisconnectPathInABinaryMatrix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DisconnectPathInABinaryMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] grid = new[]
+            {
+                new[] { 1, 1, 1 },
+                new[] { 1, 0, 0 },
+                new[] { 1, 1, 1 },
+            };
+            Console.WriteLine(IsPossibleToCutPath(grid));
+        }
+
+        public static bool IsPossibleToCutPath(int[][] grid)
+        {
+            if (grid.Length * grid[0].Length < 3) return false;
+            var res = 0;
+            var dict = new Dictionary<string, int>();
+            DFS(grid, 0, 0, dict, new HashSet<string>(), ref res);
+            if (res < 2) return true;
+            foreach (var pos in dict.Keys)
+            {
+                if (pos == "0:0" || pos == $"{grid.Length - 1}:{grid[0].Length - 1}") continue;
+                if (dict[pos] == res)
+                    return true;
+            }
+            return false;
+        }
+
+        private static void DFS(int[][] grid, int x, int y, Dictionary<string, int> dict, HashSet<string> visited, ref int res)
+        {
+            if (x < 0 || x >= grid.Length || y < 0 || y >= grid[0].Length || grid[x][y] == 0)
+                return;
+            if (!dict.ContainsKey($"{x}:{y}"))
+                dict[$"{x}:{y}"] = 0;
+            dict[$"{x}:{y}"]++;
+            if (x == grid.Length - 1 && x == grid[0].Length - 1)
+                res++;
+            if (!visited.Add($"{x}:{y}")) return;
+            DFS(grid, x + 1, y, dict, visited, ref res);
+            DFS(grid, x, y + 1, dict, visited, ref res);
+            visited.Remove($"{x}:{y}");
+        }
+    }
+}
