cc471483a07b5c903752edf6d07e1d706cfe724d
Xing <x.xing.unsw@gmail.com>
2023-02-05 13:02:28 +1100

    init

---
+++ HashTable/LeetCode 2554 - MaximumNumberOfIntegersToChooseFromARangeI/MaximumNumberOfIntegers/MaximumNumberOfIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumNumberOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaxCount(int[] banned, int n, int maxSum)
+        {
+            var set = new HashSet<int>(banned);
+            int sum = 0, res = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (set.Contains(i)) continue;
+                if (sum + i > maxSum)
+                    return res;
+                sum += i;
+                res++;
+            }
+            return res;
+        }
+    }
+}
