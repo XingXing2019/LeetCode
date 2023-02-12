2e3d656ca76d935164c1e43d12bbdf16b4b0b652
Xing <x.xing.unsw@gmail.com>
2023-02-12 15:21:39 +1100

    init

---
+++ Array/LeetCode 2562 - FindTheArrayConcatenationValue/FindTheArrayConcatenationValue/FindTheArrayConcatenationValue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindTheArrayConcatenationValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 14, 13, 8, 12 };
+            Console.WriteLine(FindTheArrayConcVal(nums));
+        }
+
+        public static long FindTheArrayConcVal(int[] nums)
+        {
+            long res = 0;
+            int li = 0, hi = nums.Length - 1;
+            while (li <= hi)
+            {
+                var num = li == hi ? nums[li++].ToString() : $"{nums[li++]}{nums[hi--]}";
+                res += long.Parse(num);
+            }
+            return res;
+        }
+    }
+}
