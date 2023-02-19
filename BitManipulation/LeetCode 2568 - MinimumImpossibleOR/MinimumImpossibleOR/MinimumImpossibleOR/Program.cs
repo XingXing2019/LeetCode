849a08b8bd8c45e39ae6ba0acf816f7d668fe1b3
Xing <x.xing.unsw@gmail.com>
2023-02-19 14:53:14 +1100

    init

---
+++ BitManipulation/LeetCode 2568 - MinimumImpossibleOR/MinimumImpossibleOR/MinimumImpossibleOR/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimumImpossibleOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinImpossibleOR(int[] nums)
+        {
+            var set = new HashSet<int>(nums);
+            var res = 1;
+            while (set.Contains(res))
+                res <<= 1;
+            return res;
+        }
+    }
+}
