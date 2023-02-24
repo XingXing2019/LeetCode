e8857ad0c8b170cba6f0b95875f6869694e161e2
Xing <x.xing.unsw@gmail.com>
2023-02-24 14:14:07 +1100

    init

---
+++ Heap/LeetCode 1675 - MinimizeDeviationInArray/MinimizeDeviationInArray/MinimizeDeviationInArray/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums = { 1, 2, 3, 4 };
+Console.WriteLine(MinimumDeviation(nums));
+
+int MinimumDeviation(int[] nums)
+{
+    var maxHeap = new PriorityQueue<int, int>();
+    int min = int.MaxValue, res = int.MaxValue;
+    foreach (var num in nums)
+    {
+        var temp = num % 2 == 0 ? num : num * 2;
+        min = Math.Min(min, temp);
+        maxHeap.Enqueue(temp, -temp);
+    }
+    while (maxHeap.Count != 0 && maxHeap.Peek() % 2 == 0)
+    {
+        var max = maxHeap.Dequeue();
+        max /= 2;
+        min = Math.Min(min, max);
+        maxHeap.Enqueue(max, -max);
+        res = Math.Min(res, maxHeap.Peek() - min);
+    }
+    return res;
+}
