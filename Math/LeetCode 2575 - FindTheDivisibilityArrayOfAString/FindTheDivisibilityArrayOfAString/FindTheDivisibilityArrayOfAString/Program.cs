364abaedee6c35eceb0acc77e9e4a7738f4453e3
Xing <x.xing.unsw@gmail.com>
2023-02-26 15:49:46 +1100

    init

--- Math/LeetCode 2575 - FindTheDivisibilityArrayOfAString/FindTheDivisibilityArrayOfAString/FindTheDivisibilityArrayOfAString/Program.cs
+++ Math/LeetCode 2575 - FindTheDivisibilityArrayOfAString/FindTheDivisibilityArrayOfAString/FindTheDivisibilityArrayOfAString/Program.cs
@@ -8,5 +8,18 @@ namespace FindTheDivisibilityArrayOfAString
         {
             Console.WriteLine("Hello World!");
         }
+
+        public int[] DivisibilityArray(string word, int m)
+        {
+            var res = new int[word.Length];
+            long mod = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                long digit = word[i] - '0';
+                res[i] = (digit + mod) % m == 0 ? 1 : 0;
+                mod = (digit + mod) * 10 % m;
+            }
+            return res;
+        }
     }
 }
