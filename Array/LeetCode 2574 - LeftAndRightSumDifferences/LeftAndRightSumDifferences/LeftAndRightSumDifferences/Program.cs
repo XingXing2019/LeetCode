26a79549b10b5f951539131971757c342d4ddd40
Xing <x.xing.unsw@gmail.com>
2023-02-26 15:15:37 +1100

    init

---
+++ Array/LeetCode 2574 - LeftAndRightSumDifferences/LeftAndRightSumDifferences/LeftAndRightSumDifferences/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeftAndRightSumDifferences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] LeftRigthDifference(int[] nums)
+        {
+            var leftSum = new int[nums.Length];
+            var rightSum = new int[nums.Length];
+            var res = new int[nums.Length];
+            int left = 0, right = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                leftSum[i] = left;
+                left += nums[i];
+                rightSum[^(i + 1)] = right;
+                right += nums[^(i + 1)];
+            }
+            for (int i = 0; i < res.Length; i++)
+                res[i] = Math.Abs(leftSum[i] - rightSum[i]);
+            return res;
+        }
+    }
+}
