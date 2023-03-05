ecb5fd3fe8707b52a97d9b70c3852420be64dd9b
Xing <x.xing.unsw@gmail.com>
2023-03-05 15:40:19 +1100

    init

---
+++ Math/LeetCode 2582 - PassThePillow/PassThePillow/PassThePillow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PassThePillow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int PassThePillow(int n, int time)
+        {
+            time %= 2 * (n - 1);
+            return time > n - 1 ? n - time % (n - 1) : time + 1;
+        }
+    }
+}
