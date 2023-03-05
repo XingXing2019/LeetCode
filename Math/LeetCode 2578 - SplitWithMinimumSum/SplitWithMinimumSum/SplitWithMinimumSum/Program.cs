da2fb6e124d10e653e1887cf0d73d1a28f72f98f
Xing <x.xing.unsw@gmail.com>
2023-03-05 13:37:03 +1100

    init

---
+++ Math/LeetCode 2578 - SplitWithMinimumSum/SplitWithMinimumSum/SplitWithMinimumSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SplitWithMinimumSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int SplitNum(int num)
+        {
+            var digits = new List<int>();
+            while (num != 0)
+            {
+                digits.Add(num % 10);
+                num /= 10;
+            }
+            digits.Sort();
+            int num1 = 0, num2 = 0;
+            for (int i = 0; i < digits.Count; i++)
+            {
+                if (i % 2 == 0)
+                    num1 = num1 * 10 + digits[i];
+                else
+                    num2 = num2 * 10 + digits[i];
+            }
+            return num1 + num2;
+        }
+    }
+}
---
+++ Math/LeetCode 2579 - CountTotalNumberOfColoredCells/CountTotalNumberOfColoredCells/CountTotalNumberOfColoredCells/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CountTotalNumberOfColoredCells
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long ColoredCells(int n)
+        {
+            return 1 + 2 * (long)n * (n - 1);
+        }
+    }
+}
