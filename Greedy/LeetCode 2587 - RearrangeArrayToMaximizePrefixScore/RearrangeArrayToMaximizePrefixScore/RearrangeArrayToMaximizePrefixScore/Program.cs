fadfd36a646c48ceb982a6532ba61f3999147fc1
Xing <x.xing.unsw@gmail.com>
2023-03-12 19:47:58 +1100

    init

---
+++ Greedy/LeetCode 2587 - RearrangeArrayToMaximizePrefixScore/RearrangeArrayToMaximizePrefixScore/RearrangeArrayToMaximizePrefixScore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RearrangeArrayToMaximizePrefixScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaxScore(int[] nums)
+        {
+            Array.Sort(nums, (a, b) => b - a);
+            long prefix = 0;
+            int res = 0;
+            foreach (var num in nums)
+            {
+                prefix += num;
+                if (prefix <= 0)
+                    return res;
+                res++;
+            }
+            return res;
+        }
+    }
+}
