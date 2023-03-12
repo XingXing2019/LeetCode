2bd4f3c725f738778883b3a2b70900456766ce8e
Xing <x.xing.unsw@gmail.com>
2023-03-12 19:44:52 +1100

    init

---
+++ String/LeetCode 2586 - CountTheNumberOfVowelStringsInRange/CountTheNumberOfVowelStringsInRange/CountTheNumberOfVowelStringsInRange/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountTheNumberOfVowelStringsInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int VowelStrings(string[] words, int left, int right)
+        {var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            return words[left..(right + 1)].Count(x => vowels.Contains(x[0]) && vowels.Contains(x[^1]));
+            
+        }
+    }
+}
