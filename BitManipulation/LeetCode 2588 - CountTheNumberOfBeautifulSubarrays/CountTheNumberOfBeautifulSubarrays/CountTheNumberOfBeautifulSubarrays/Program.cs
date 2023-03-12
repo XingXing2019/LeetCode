626fca23e4b5d403be3e8f51b7a60c6a8cd9e814
Xing <x.xing.unsw@gmail.com>
2023-03-12 19:51:58 +1100

    init

---
+++ BitManipulation/LeetCode 2588 - CountTheNumberOfBeautifulSubarrays/CountTheNumberOfBeautifulSubarrays/CountTheNumberOfBeautifulSubarrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountTheNumberOfBeautifulSubarrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long BeautifulSubarrays(int[] nums)
+        {
+            long res = 0;
+            var dict = new Dictionary<int, long> { { 0, 1 } };
+            var xOr = 0;
+            foreach (var num in nums)
+            {
+                xOr ^= num;
+                if (!dict.ContainsKey(xOr))
+                    dict[xOr] = 0;
+                res += dict[xOr];
+                dict[xOr]++;
+            }
+            return res;
+        }
+    }
+}
