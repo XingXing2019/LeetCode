a21310b51d8cabf05c51bf4d3710468cc8e9afa3
Xing <x.xing.unsw@gmail.com>
2023-03-19 13:45:51 +1100

    init

---
+++ TwoPointers/LeetCode 2592 - MaximizeGreatnessOfAnArray/MaximizeGreatnessOfAnArray/MaximizeGreatnessOfAnArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MaximizeGreatnessOfAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaximizeGreatness(int[] nums)
+        {
+            Array.Sort(nums);
+            int p1 = 0, p2 = 0;
+            while (p2 < nums.Length)
+            {
+                if (nums[p2] > nums[p1])
+                    p1++;
+                p2++;
+            }
+            return p1;
+        }
+    }
+}
