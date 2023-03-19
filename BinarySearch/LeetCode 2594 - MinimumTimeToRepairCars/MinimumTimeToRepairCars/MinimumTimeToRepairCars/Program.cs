5f711b0ef4ed8286e78b2cf58b250cd80bf0a5a8
Xing <x.xing.unsw@gmail.com>
2023-03-19 13:56:02 +1100

    init

---
+++ BinarySearch/LeetCode 2594 - MinimumTimeToRepairCars/MinimumTimeToRepairCars/MinimumTimeToRepairCars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MinimumTimeToRepairCars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ranks = { 4, 2, 3, 1 };
+            int cars = 10;
+            Console.WriteLine(RepairCars(ranks, cars));
+        }
+
+        public static long RepairCars(int[] ranks, int cars)
+        {
+            long li = 1, hi = long.MaxValue;
+            while (li <= hi)
+            {
+                var mid = li + (hi - li) / 2;
+                long count = 0;
+                foreach (var rank in ranks)
+                    count += (long)Math.Sqrt(mid / rank);
+                if (count >= cars)
+                    hi = mid - 1;
+                else
+                    li = mid + 1;
+            }
+            return li;
+        }
+    }
+}
