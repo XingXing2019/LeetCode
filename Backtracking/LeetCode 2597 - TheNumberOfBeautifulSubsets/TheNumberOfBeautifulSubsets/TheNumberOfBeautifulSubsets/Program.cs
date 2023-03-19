3cda94e5e80bd430568bef4caea8cb3022a33d77
Xing <x.xing.unsw@gmail.com>
2023-03-19 15:59:12 +1100

    init

---
+++ Backtracking/LeetCode 2597 - TheNumberOfBeautifulSubsets/TheNumberOfBeautifulSubsets/TheNumberOfBeautifulSubsets/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheNumberOfBeautifulSubsets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 4, 6 };
+            int k = 2;
+            Console.WriteLine(BeautifulSubsets(nums, k));
+        }
+
+        public static int BeautifulSubsets(int[] nums, int k)
+        {
+            var res = 0;
+            var freq = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                freq[num] = 0;
+                freq[num - k] = 0;
+                freq[num + k] = 0;
+            }
+            DFS(nums, 0, new List<int>(), k, freq, ref res);
+            return res;
+        }
+
+        public static void DFS(int[] nums, int start, List<int> list, int k, Dictionary<int, int> freq, ref int res)
+        {
+            if (list.Count != 0)
+                res++;
+            for (int i = start; i < nums.Length; i++)
+            {
+                if (freq[nums[i] - k] != 0 || freq[nums[i] + k] != 0) continue;
+                list.Add(nums[i]);
+                freq[nums[i]]++;
+                DFS(nums, i + 1, list, k, freq, ref res);
+                list.RemoveAt(list.Count - 1);
+                freq[nums[i]]--;
+            }
+        }
+    }
+}
---
+++ Search/LeetCode 2596 - CheckKnightTourConfiguration/CheckKnightTourConfiguration/CheckKnightTourConfiguration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CheckKnightTourConfiguration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool CheckValidGrid(int[][] grid)
+        {
+            int[][] moves =
+            {
+                new[] { -2, 1 }, new[] { -1, 2 }, new[] { 1, 2 }, new[] { 2, 1 }, 
+                new[] { 2, -1 }, new[] { 1, -2 }, new[] { -2, -1 }, new[] { -1, -2 }
+            };
+            return DFS(grid, moves, 0, 0, 0);
+        }
+
+        public bool DFS(int[][] grid, int[][] moves, int x, int y, int expected)
+        {
+            if (x < 0 || x >= grid.Length || y < 0 || y >= grid[0].Length || grid[x][y] != expected)
+                return false;
+            if (expected == grid.Length * grid[0].Length - 1)
+                return true;
+            for (int i = 0; i < moves.Length; i++)
+            {
+                int newX = x + moves[i][0], newY = y + moves[i][1];
+                if (DFS(grid, moves, newX, newY, expected + 1))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
