10c14cef65e74ba72df90c9300dbfa769a407a8f
Xing <x.xing.unsw@gmail.com>
2023-03-19 20:04:41 +1100

    init

--- Backtracking/LeetCode 2597 - TheNumberOfBeautifulSubsets/TheNumberOfBeautifulSubsets/TheNumberOfBeautifulSubsets/Program.cs
+++ Backtracking/LeetCode 2597 - TheNumberOfBeautifulSubsets/TheNumberOfBeautifulSubsets/TheNumberOfBeautifulSubsets/Program.cs
@@ -23,21 +23,19 @@ namespace TheNumberOfBeautifulSubsets
                 freq[num - k] = 0;
                 freq[num + k] = 0;
             }
-            DFS(nums, 0, new List<int>(), k, freq, ref res);
+            DFS(nums, 0, 0, k, freq, ref res);
             return res;
         }
 
-        public static void DFS(int[] nums, int start, List<int> list, int k, Dictionary<int, int> freq, ref int res)
+        public static void DFS(int[] nums, int start, int count, int k, Dictionary<int, int> freq, ref int res)
         {
-            if (list.Count != 0)
+            if (count != 0)
                 res++;
             for (int i = start; i < nums.Length; i++)
             {
                 if (freq[nums[i] - k] != 0 || freq[nums[i] + k] != 0) continue;
-                list.Add(nums[i]);
                 freq[nums[i]]++;
-                DFS(nums, i + 1, list, k, freq, ref res);
-                list.RemoveAt(list.Count - 1);
+                DFS(nums, i + 1, count + 1, k, freq, ref res);
                 freq[nums[i]]--;
             }
         }
---
+++ HashTable/LeetCode 2598 - SmallestMissingNonNegativeIntegerAfterOperations/SmallestMissingNonNegativeInteger/SmallestMissingNonNegativeInteger/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmallestMissingNonNegativeInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -11, 0, 1, 2, -17, -16, -15, -14, 3323 };
+            var value = 7;
+            Console.WriteLine(FindSmallestInteger(nums, value));
+        }
+
+        public static int FindSmallestInteger(int[] nums, int value)
+        {
+            var freq = new Dictionary<int, int>();
+            var lists = new List<int>();
+            foreach (var num in nums)
+            {
+                var mod = num % value;
+                if (mod < 0) mod += value;
+                if (!freq.ContainsKey(mod))
+                    freq[mod] = 0;
+                freq[mod]++;
+            }
+            foreach (var num in freq.Keys)
+            {
+                var count = freq[num];
+                for (int i = 0; i < count; i++)
+                    lists.Add(num + value * i);
+            }
+            lists.Sort();
+            for (int i = 0; i < lists.Count; i++)
+            {
+                if (i == lists[i]) continue;
+                return i;
+            }
+            return lists.Count;
+        }
+    }
+}
