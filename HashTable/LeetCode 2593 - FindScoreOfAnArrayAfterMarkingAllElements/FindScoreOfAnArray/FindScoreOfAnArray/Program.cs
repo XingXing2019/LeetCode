a5c34476e0798337ee8b5174622697df99794ed6
Xing <x.xing.unsw@gmail.com>
2023-03-19 14:44:22 +1100

    init

---
+++ HashTable/LeetCode 2593 - FindScoreOfAnArrayAfterMarkingAllElements/FindScoreOfAnArray/FindScoreOfAnArray/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] nums = { 2, 1, 3, 4, 5, 2 };
+Console.WriteLine(FindScore(nums));
+
+long FindScore(int[] nums)
+{
+    var indexes = new Dictionary<int, HashSet<int>>();
+    long res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (!indexes.ContainsKey(nums[i]))
+            indexes[nums[i]] = new HashSet<int>();
+        indexes[nums[i]].Add(i);
+    }
+    var keys = indexes.OrderBy(x => x.Key).Select(x => x.Key).ToList();
+    foreach (var key in keys)
+    {
+        foreach (var index in indexes[key])
+        {
+            res += key;
+            int pre = index - 1, next = index + 1;
+            if (pre >= 0)
+                indexes[nums[pre]].Remove(pre);
+            if (next < nums.Length)
+                indexes[nums[next]].Remove(next);
+        }
+    }
+    return res;
+}
