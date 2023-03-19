2715155cf9c2533ef5399b736123808e95801a5c
Xing <x.xing.unsw@gmail.com>
2023-03-19 15:07:12 +1100

    init

---
+++ BitManipulation/LeetCode 2595 - NumberOfEvenAndOddBits/NumberOfEvenAndOddBits/NumberOfEvenAndOddBits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumberOfEvenAndOddBits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] EvenOddBit(int n)
+        {
+            var res = new int[2];
+            var digits = 0;
+            while (n != 0)
+            {
+                if ((n & 1) == 1)
+                    res[digits % 2]++;
+                digits++;
+                n >>= 1;
+            }
+            return res;
+        }
+    }
+}
