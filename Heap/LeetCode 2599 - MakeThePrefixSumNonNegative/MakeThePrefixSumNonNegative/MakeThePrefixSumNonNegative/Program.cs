df44df0956a95452f5b31b8c3743615e5d3cc524
Xing <x.xing.unsw@gmail.com>
2023-03-23 19:16:25 +1100

    iniy

---
+++ Heap/LeetCode 2599 - MakeThePrefixSumNonNegative/MakeThePrefixSumNonNegative/MakeThePrefixSumNonNegative/Program.cs
@@ -0,0 +1,16 @@
+﻿int MakePrefSumNonNegative(int[] nums)
+{
+    var minHeap = new PriorityQueue<int, int>();
+    long prefix = 0;
+    var res = 0;
+    foreach (var num in nums)
+    {
+        prefix += num;
+        minHeap.Enqueue(num, num);
+        if (prefix >= 0)
+            continue;
+        prefix -= minHeap.Dequeue();
+        res++;
+    }
+    return res;
+}
