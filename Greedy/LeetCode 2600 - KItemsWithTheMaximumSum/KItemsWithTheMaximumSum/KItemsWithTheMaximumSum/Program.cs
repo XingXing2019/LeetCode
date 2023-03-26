f39a4d73d7883245cfb560f6e0c41efc53112f7f
Xing <x.xing.unsw@gmail.com>
2023-03-26 15:37:49 +1100

    init

---
+++ Greedy/LeetCode 2600 - KItemsWithTheMaximumSum/KItemsWithTheMaximumSum/KItemsWithTheMaximumSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KItemsWithTheMaximumSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int KItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k)
+        {
+            return Math.Min(numOnes, k) - Math.Max(0, k - numOnes - numZeros);
+        }
+    }
+}
