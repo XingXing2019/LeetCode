48bce2321c3425627bbe4c8e46457f6c02303ec2
Xing <x.xing.unsw@gmail.com>
2023-04-02 15:39:54 +1000

    init

---
+++ Greedy/LeetCode 2611 - MiceAndCheese/MiceAndCheese/MiceAndCheese/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MiceAndCheese
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MiceAndCheese(int[] reward1, int[] reward2, int k)
+        {
+            var points = new int[reward1.Length][];
+            for (int i = 0; i < reward1.Length; i++)
+                points[i] = new[] { reward1[i] - reward2[i], i };
+            Array.Sort(points, (a, b) => b[0] - a[0]);
+            var res = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                var index = points[i][1];
+                res += i < k ? reward1[index] : reward2[index];
+            }
+            return res;
+        }
+    }
+}
---
+++ HashTable/LeetCode 2610 - ConvertAnArrayIntoA2DArrayWithConditions/ConvertAnArrayIntoA2DArray/ConvertAnArrayIntoA2DArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConvertAnArrayIntoA2DArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            nums = freq.Keys.ToArray();
+            var row = freq.Max(x => x.Value);
+            var res = new List<int>[row];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] ??= new List<int>();
+                foreach (var num in nums)
+                {
+                    if (freq[num] == 0) continue;
+                    res[i].Add(num);
+                    freq[num]--;
+                }
+            }
+            return res;
+        }
+    }
+}
