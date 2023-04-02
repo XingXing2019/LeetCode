9b31405e037db7937208be3e1c66472cf4e5643f
Xing <x.xing.unsw@gmail.com>
2023-04-02 14:26:25 +1000

    init

---
+++ Array/LeetCode 2607 - MakeKSubarraySumsEqual/MakeKSubarraySumsEqual/MakeKSubarraySumsEqual/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MakeKSubarraySumsEqual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 3, 8 };
+            var k = 3;
+            Console.WriteLine(MakeSubKSumEqual(arr, k));
+        }
+
+        public static long MakeSubKSumEqual(int[] arr, int k)
+        {
+            var visited = new HashSet<int>();
+            var groups = new List<HashSet<int>>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (visited.Contains(i)) continue;
+                var group = new HashSet<int>();
+                var index = i;
+                while (group.Add(index))
+                {
+                    index = (index + k) % arr.Length;
+                }
+                visited.UnionWith(group);
+                groups.Add(group);
+            }
+            long res = 0;
+            foreach (var group in groups)
+                res += GetOperations(arr, group);
+            return res;
+        }
+
+        public static long GetOperations(int[] arr, HashSet<int> indexes)
+        {
+            var nums = indexes.Select(x => arr[x]).OrderBy(x => x).ToArray();
+            var median = nums[nums.Length / 2];
+            return nums.Sum(x => Math.Abs(x - median));
+        }
+    }
+}
