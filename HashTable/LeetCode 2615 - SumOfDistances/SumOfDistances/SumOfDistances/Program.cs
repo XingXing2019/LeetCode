883908dc40974a9497d94a303c76301115831e17
Xing <x.xing.unsw@gmail.com>
2023-04-09 14:58:44 +1000

    init

---
+++ HashTable/LeetCode 2615 - SumOfDistances/SumOfDistances/SumOfDistances/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumOfDistances
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 1, 1, 2 };
+            Console.WriteLine(Distance(nums));
+        }
+
+        public static long[] Distance(int[] nums)
+        {
+            var dict = new Dictionary<int, List<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dict.ContainsKey(nums[i]))
+                    dict[nums[i]] = new List<int>();
+                dict[nums[i]].Add(i);
+            }
+            var prefixes = new Dictionary<int, List<long>>();
+            foreach (var num in dict.Keys)
+            {
+                if (prefixes.ContainsKey(num)) continue;
+                var prefix = new List<long>();
+                for (int i = 0; i < dict[num].Count; i++)
+                    prefix.Add(i == 0 ? dict[num][i] : dict[num][i] + prefix[i - 1]);
+                prefixes[num] = prefix;
+            }
+            var res = new long[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+                res[i] = GetDistance(dict[nums[i]], prefixes[nums[i]], i);
+            return res;
+        }
+
+        private static long GetDistance(List<int> indexes, List<long> prefix, int index)
+        {
+            var pos = indexes.BinarySearch(index);
+            long before = pos + 1, after = indexes.Count - before;
+            return (index * before - prefix[pos]) + (prefix[^1] - prefix[pos] - index  * after);
+        }
+    }
+}
---
+++ Math/LeetCode 2614 - PrimeInDiagonal/PrimeInDiagonal/PrimeInDiagonal/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeInDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] nums = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 5, 6, 7 },
+                new int[] { 9, 10, 11 },
+            };
+            Console.WriteLine(DiagonalPrime(nums));
+        }
+
+        public static int DiagonalPrime(int[][] nums)
+        {
+            int n = nums.Length, max = 0, res = 0;
+            var diagonals = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                diagonals.Add(nums[i][i]);
+                max = Math.Max(max, nums[i][i]);
+                diagonals.Add(nums[i][n - i - 1]);
+                max = Math.Max(max, nums[i][n - i - 1]);
+            }
+            var primes = GetPrimes(max);
+            foreach (var num in diagonals)
+            {
+                if (!primes.Contains(num)) continue;
+                res = Math.Max(res, num);
+            }
+            return res;
+        }
+
+        private static HashSet<int> GetPrimes(int num)
+        {
+            var res = new HashSet<int>();
+            var dp = new bool[num + 1];
+            Array.Fill(dp, true);
+            for (int i = 2; i < dp.Length; i++)
+            {
+                if (!dp[i]) continue;
+                for (int j = 2; i * j < dp.Length; j++)
+                    dp[i * j] = false;
+            }
+            for (int i = 2; i < dp.Length; i++)
+            {
+                if (!dp[i]) continue;
+                res.Add(i);
+            }
+            return res;
+        }
+    }
+}
