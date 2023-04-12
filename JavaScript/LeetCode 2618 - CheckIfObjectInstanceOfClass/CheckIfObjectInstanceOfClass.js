ca4cc6c1cad170e1efbe23b830d808aa9388508e
Xing <x.xing.unsw@gmail.com>
2023-04-12 21:32:07 +1000

    init

---
+++ JavaScript/LeetCode 2618 - CheckIfObjectInstanceOfClass/CheckIfObjectInstanceOfClass.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Object} object
+ * @param {Function} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    while (obj != null) {
+        if (obj.constructor == classFunction)
+            return true
+        obj = Object.getPrototypeOf(obj)
+    }
+    return false
+}
