1752e260e76f943f4b577a89d44166d4eb44e93a
Xing <x.xing.unsw@gmail.com>
2023-04-16 09:43:20 +1000

    init

---
+++ Array/LeetCode 2639 - FindTheWidthOfColumnsOfAGrid/FindTheWidthOfColumnsOfAGrid/FindTheWidthOfColumnsOfAGrid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindTheWidthOfColumnsOfAGrid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] FindColumnWidth(int[][] grid)
+        {
+            var res = new int[grid[0].Length];
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                var max = 0;
+                for (int i = 0; i < grid.Length; i++)
+                    max = Math.Max(max, grid[i][j].ToString().Length);
+                res[j] = max;
+            }
+            return res;
+        }
+    }
+}
