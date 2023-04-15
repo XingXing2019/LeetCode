3953a6453dac786070d1ff4197c85db0317c774d
Xing <x.xing.unsw@gmail.com>
2023-04-16 09:47:47 +1000

    init

---
+++ Array/LeetCode 2640 - FindTheScoreOfAllPrefixesOfAnArray/FindTheScoreOfAllPrefixesOfAnArray/FindTheScoreOfAllPrefixesOfAnArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FindTheScoreOfAllPrefixesOfAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long[] FindPrefixScore(int[] nums)
+        {
+            var res = new long[nums.Length];
+            var max = 0L;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                max = Math.Max(max, nums[i]);
+                res[i] = i == 0 ? max + nums[i] : res[i - 1] + max + nums[i];
+            }
+            return res;
+        }
+    }
+}
