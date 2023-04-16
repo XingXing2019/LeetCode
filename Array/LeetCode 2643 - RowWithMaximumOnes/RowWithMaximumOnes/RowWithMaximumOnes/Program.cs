d78b424bc693ff40b9ce855ea15743f816986c4d
Xing <x.xing.unsw@gmail.com>
2023-04-16 17:42:45 +1000

    init

---
+++ Array/LeetCode 2643 - RowWithMaximumOnes/RowWithMaximumOnes/RowWithMaximumOnes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace RowWithMaximumOnes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] RowAndMaximumOnes(int[][] mat)
+        {
+            int res = 0, max = 0;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                var count = mat[i].Count(x => x == 1);
+                if (count <= max) continue;
+                res = i;
+                max = count;
+            }
+            return new[] { res, max };
+        }
+    }
+}
