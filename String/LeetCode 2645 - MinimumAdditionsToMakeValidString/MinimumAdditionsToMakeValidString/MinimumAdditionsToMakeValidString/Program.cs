91eb8e8e5a9170386eb553e67f82570cddfe299a
Xing <x.xing.unsw@gmail.com>
2023-04-16 18:12:13 +1000

    init

---
+++ String/LeetCode 2645 - MinimumAdditionsToMakeValidString/MinimumAdditionsToMakeValidString/MinimumAdditionsToMakeValidString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MinimumAdditionsToMakeValidString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int AddMinimum(string word)
+        {
+            int last = -1, res = 0;
+            foreach (var l in word)
+            {
+                var cur = l - 'a';
+                res += cur > last ? cur - last - 1 : cur + 3 - last - 1;
+                last = cur;
+            }
+            return res + (2 - last);
+        }
+    }
+}
