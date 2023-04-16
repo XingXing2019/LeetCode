ec2c4af5192458c1e3a108173553f751375bacb8
Xing <x.xing.unsw@gmail.com>
2023-04-16 17:50:03 +1000

    init

---
+++ Array/LeetCode 2644 - FindTheMaximumDivisibilityScore/FindTheMaximumDivisibilityScore/FindTheMaximumDivisibilityScore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace FindTheMaximumDivisibilityScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaxDivScore(int[] nums, int[] divisors)
+        {
+            int res = 0, max = -1;
+            foreach (var divisor in divisors)
+            {
+                var count = nums.Count(x => x % divisor == 0);
+                if (count < max) continue;
+                res = count > max ? divisor : Math.Min(res, divisor);
+                max = count;
+            }
+            return res;
+        }
+    }
+}
