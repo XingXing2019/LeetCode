951030802aa38ddcff48992e8fc1680fa0f24ea1
Xing <x.xing.unsw@gmail.com>
2023-04-23 14:24:28 +1000

    init

---
+++ Math/LeetCode 2651 - CalculateDelayedArrivalTime/CalculateDelayedArrivalTime/CalculateDelayedArrivalTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculateDelayedArrivalTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int FindDelayedArrivalTime(int arrivalTime, int delayedTime)
+        {
+            return (arrivalTime + delayedTime) % 24;
+        }
+    }
+}
