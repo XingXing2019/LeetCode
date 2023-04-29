cf6d93e9bcd8d92fe145e2da3bd7aac53e6f0aab
Xing <x.xing.unsw@gmail.com>
2023-04-30 09:46:24 +1000

    init

---
+++ Math/LeetCode 2656 - MaximumSumWithExactlyKElements/MaximumSumWithExactlyKElements/MaximumSumWithExactlyKElements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumSumWithExactlyKElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaximizeSum(int[] nums, int k)
+        {
+            return (2 * nums.Max() + k - 1) * k / 2;
+        }
+    }
+}
