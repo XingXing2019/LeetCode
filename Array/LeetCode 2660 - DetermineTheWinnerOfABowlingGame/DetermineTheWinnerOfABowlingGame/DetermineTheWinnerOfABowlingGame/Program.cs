5259cd427a03cf626aeb3640e3825bfa9e2c8552
Xing <x.xing.unsw@gmail.com>
2023-04-30 17:14:15 +1000

    init

---
+++ Array/LeetCode 2660 - DetermineTheWinnerOfABowlingGame/DetermineTheWinnerOfABowlingGame/DetermineTheWinnerOfABowlingGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DetermineTheWinnerOfABowlingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int IsWinner(int[] player1, int[] player2)
+        {
+            int score1 = GetScore(player1), score2 = GetScore(player2);
+            if (score1 == score2)
+                return 0;
+            return score1 > score2 ? 1 : 2;
+        }
+
+        public int GetScore(int[] player)
+        {
+            var res = 0;
+            for (int i = 0; i < player.Length; i++)
+                res += i >= 2 && player[i - 2] == 10 || i >= 1 && player[i - 1] == 10 ? player[i] * 2 : player[i];
+            return res;
+        }
+    }
+}
