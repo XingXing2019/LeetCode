9a599187c1019c97649e7c6c9a78205af3677561
Xing <x.xing.unsw@gmail.com>
2023-05-14 08:17:14 +1000

    init

---
+++ Array/LeetCode 2679 - SumInAMatrix/SumInAMatrix/SumInAMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumInAMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MatrixSum(int[][] nums)
+        {
+            foreach (var num in nums)
+                Array.Sort(num, (a, b) => b - a);
+            var res = 0;
+            for (int j = 0; j < nums[0].Length; j++)
+            {
+                var max = 0;
+                for (int i = 0; i < nums.Length; i++)
+                    max = Math.Max(max, nums[i][j]);
+                res += max;;
+            }
+            return res;
+        }
+    }
+}
