9d946613d8278a85e302553b97fc709cbda2b0fa
Xing <x.xing.unsw@gmail.com>
2023-05-14 08:13:32 +1000

    init

---
+++ String/LeetCode 2678 - NumberOfSeniorCitizens/NumberOfSeniorCitizens/NumberOfSeniorCitizens/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace NumberOfSeniorCitizens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountSeniors(string[] details)
+        {
+            return details.Count(x => GetAge(x) > 60);
+        }
+
+        public int GetAge(string detail)
+        {
+            return int.Parse(detail.Substring(11, 2));
+        }
+    }
+}
