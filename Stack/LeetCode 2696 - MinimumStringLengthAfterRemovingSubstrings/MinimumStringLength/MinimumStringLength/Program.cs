c5d242955e5d2d3d1413b181f9e37c9d882d31aa
Xing <x.xing.unsw@gmail.com>
2023-05-21 19:50:27 +1000

    init

---
+++ Stack/LeetCode 2696 - MinimumStringLengthAfterRemovingSubstrings/MinimumStringLength/MinimumStringLength/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimumStringLength
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MinLength(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var l in s)
+            {
+                if (stack.Count != 0 && (l == 'B' && stack.Peek() == 'A' || l == 'D' && stack.Peek() == 'C'))
+                    stack.Pop();
+                else
+                    stack.Push(l);
+            }
+
+            return stack.Count;
+        }
+    }
+}
