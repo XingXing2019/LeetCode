67e666f8c8f8e1c9a9ab059b3c02803a929b291f
Xing <x.xing.unsw@gmail.com>
2023-05-21 20:07:43 +1000

    init

---
+++ String/LeetCode 2697 - LexicographicallySmallestPalindrome/LexicographicallySmallestPalindrome/LexicographicallySmallestPalindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace LexicographicallySmallestPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string MakeSmallestPalindrome(string s)
+        {
+            var sb = new StringBuilder(s);
+            int li = 0, hi = sb.Length - 1;
+            while (li < hi)
+            {
+                if (sb[li] < sb[hi])
+                    sb[hi--] = sb[li++];
+                else
+                    sb[li++] = sb[hi--];
+            }
+            return sb.ToString();
+        }
+    }
+}
