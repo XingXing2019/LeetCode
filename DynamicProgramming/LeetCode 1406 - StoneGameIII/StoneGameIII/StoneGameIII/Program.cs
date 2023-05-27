dd6bb0d19aedc8caca6182d26a41b87cee57ed8c
Xing <x.xing.unsw@gmail.com>
2023-05-27 19:45:10 +1000

    init

---
+++ DynamicProgramming/LeetCode 1406 - StoneGameIII/StoneGameIII/StoneGameIII/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StoneGameIII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string StoneGameIII(int[] stoneValue)
+        {
+            var dp = new int[stoneValue.Length];
+            var diff = DFS(stoneValue, 0, dp);
+            return diff == 0 ? "Tie" : diff > 0 ? "Alice" : "Bob";
+        }
+
+        public static int DFS(int[] stoneValue, int index, int[] dp)
+        {
+            if (index >= stoneValue.Length)
+                return 0;
+            if (dp[index] != 0)
+                return dp[index];
+            int max = int.MinValue, sum = 0;
+            for (int i = index; i < Math.Min(stoneValue.Length, index + 3); i++)
+            {
+                sum += stoneValue[i];
+                max = Math.Max(max, sum - DFS(stoneValue, i + 1, dp));
+            }
+            return dp[index] = max;
+        }
+    }
+}
