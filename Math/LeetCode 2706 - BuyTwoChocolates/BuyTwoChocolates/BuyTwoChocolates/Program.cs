87e723b265964b449dfd369451ae3ba04fc8241f
Xing <x.xing.unsw@gmail.com>
2023-05-28 19:47:58 +1000

    init

---
+++ Math/LeetCode 2706 - BuyTwoChocolates/BuyTwoChocolates/BuyTwoChocolates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BuyTwoChocolates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int BuyChoco(int[] prices, int money)
+        {
+            int min = int.MaxValue, secMin = int.MaxValue;
+            foreach (var price in prices)
+            {
+                if (price < min)
+                {
+                    secMin = min;
+                    min = price;
+                }
+                else if (price < secMin)
+                    secMin = price;
+            }
+            return min + secMin <= money ? money - min - secMin : money;
+        }
+    }
+}
