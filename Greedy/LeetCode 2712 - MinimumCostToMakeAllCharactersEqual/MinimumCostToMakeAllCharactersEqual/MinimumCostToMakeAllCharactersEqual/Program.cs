7201b65387595232904530a6f5b42ae52d87fdc4
Xing <x.xing.unsw@gmail.com>
2023-05-28 20:56:30 +1000

    init

---
+++ Greedy/LeetCode 2712 - MinimumCostToMakeAllCharactersEqual/MinimumCostToMakeAllCharactersEqual/MinimumCostToMakeAllCharactersEqual/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MinimumCostToMakeAllCharactersEqual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long MinimumCost(string s)
+        {
+            long res = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1]) continue;
+                res += Math.Min(i, s.Length - i);
+            }
+            return res;
+        }
+    }
+}
