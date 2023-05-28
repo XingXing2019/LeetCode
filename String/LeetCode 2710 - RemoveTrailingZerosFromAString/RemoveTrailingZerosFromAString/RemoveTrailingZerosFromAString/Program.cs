8d1bd43eb2b0c421a5af9a3c48fb974256f5f627
Xing <x.xing.unsw@gmail.com>
2023-05-28 20:12:52 +1000

    init

---
+++ String/LeetCode 2710 - RemoveTrailingZerosFromAString/RemoveTrailingZerosFromAString/RemoveTrailingZerosFromAString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RemoveTrailingZerosFromAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string RemoveTrailingZeros(string num)
+        {
+            var res = "";
+            var seenNum = false;
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                if (num[i] == '0' && seenNum)
+                {
+                    res = num[i] + res;
+                }
+                else if (num[i] != '0')
+                {
+                    res = num[i] + res;
+                    seenNum = true;
+                }
+            }
+            return res;
+        }
+    }
+}
