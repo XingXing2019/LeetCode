815d1c2f59a34a45787161ffa65616d38e9edc16
Xing <x.xing.unsw@gmail.com>
2023-06-07 20:33:12 +1000

    init

---
+++ JavaScript/LeetCode 2723 - AddTwoPromises/AddTwoPromises.js
@@ -0,0 +1,8 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    return Promise.all([promise1, promise2]).then(results => eval(results.join('+')))
+};
