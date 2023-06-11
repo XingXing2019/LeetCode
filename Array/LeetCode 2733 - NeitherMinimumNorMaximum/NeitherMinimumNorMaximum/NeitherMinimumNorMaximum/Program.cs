47d968a4709e930603e0b728eb4d3968a8e54f93
Xing <x.xing.unsw@gmail.com>
2023-06-12 09:37:07 +1000

    init

---
+++ Array/LeetCode 2733 - NeitherMinimumNorMaximum/NeitherMinimumNorMaximum/NeitherMinimumNorMaximum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NeitherMinimumNorMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int FindNonMinOrMax(int[] nums)
+        {
+            int min = int.MaxValue, max = int.MinValue;
+            foreach (var num in nums)
+            {
+                min = Math.Min(min, num);
+                max = Math.Max(max, num);
+            }
+            foreach (var num in nums)
+            {
+                if (num == min || num == max) continue;
+                return num;
+            }
+            return -1;
+        }
+    }
+}
