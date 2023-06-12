fdfff71d5e8d2434f009b660d1307564e440c0ca
Xing <x.xing.unsw@gmail.com>
2023-06-12 10:00:57 +1000

    init

---
+++ String/LeetCode 2734 - LexicographicallySmallestStringAfterSubstringOperation/LexicographicallySmallestString/LexicographicallySmallestString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace LexicographicallySmallestString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "a";
+            Console.WriteLine(SmallestString(s));
+        }
+
+        public static string SmallestString(string s)
+        {
+            var index = 0;
+            var seen = false;
+            var res = new StringBuilder();
+            while (index < s.Length && s[index] == 'a')
+                res.Append(s[index++]);
+            if (index == s.Length)
+                res[^1] = 'z';
+            for (int i = index; i < s.Length; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    res.Append(s[i]);
+                    if (i != 0)
+                        seen = true;
+                }
+                else
+                {
+                    var l = seen ? s[i] : (char)(s[i] - 1);
+                    res.Append(l);
+                }
+            }
+            return res.ToString();
+        }
+    }
+}
