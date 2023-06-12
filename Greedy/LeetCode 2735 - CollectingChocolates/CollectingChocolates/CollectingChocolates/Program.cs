2d89b72e159cbf31381d7c93c751f5de6ca3b6e9
Xing <x.xing.unsw@gmail.com>
2023-06-12 10:18:41 +1000

    init

---
+++ Greedy/LeetCode 2735 - CollectingChocolates/CollectingChocolates/CollectingChocolates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CollectingChocolates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long MinCost(int[] nums, int x)
+        {
+            long res = long.MaxValue;
+            var temp = new long[nums.Length];
+            Array.Fill(temp, long.MaxValue);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                long sum = (long)x * i;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    temp[j] = Math.Min(temp[j], nums[(i + j) % nums.Length]);
+                    sum += temp[j];
+                }
+                res = Math.Min(res, sum);
+            }
+            return res;
+        }
+    }
+}
