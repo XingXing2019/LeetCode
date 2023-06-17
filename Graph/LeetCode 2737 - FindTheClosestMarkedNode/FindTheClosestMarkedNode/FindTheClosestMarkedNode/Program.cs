ba02300394ee990f3c5417776aff25dfbe19594f
Xing <x.xing.unsw@gmail.com>
2023-06-17 21:39:21 +1000

    init

--- Graph/LeetCode 2737 - FindTheClosestMarkedNode/FindTheClosestMarkedNode/FindTheClosestMarkedNode/Program.cs
+++ Graph/LeetCode 2737 - FindTheClosestMarkedNode/FindTheClosestMarkedNode/FindTheClosestMarkedNode/Program.cs
@@ -14,15 +14,10 @@ namespace FindTheClosestMarkedNode
         public int MinimumDistance(int n, IList<IList<int>> edges, int s, int[] marked)
         {
             var graph = new List<int[]>[n];
-            edges = edges.OrderBy(x => x[0]).ThenBy(x => x[1]).ThenBy(x => x[2]).ToArray();
-            var used = new HashSet<string>();
             for (int i = 0; i < n; i++)
                 graph[i] = new List<int[]>();
             foreach (var edge in edges)
-            {
-                if (!used.Add($"{edge[0]}:{edge[1]}")) continue;
                 graph[edge[0]].Add(new[] { edge[1], edge[2] });
-            }
             var queue = new Queue<int[]>();
             queue.Enqueue(new[] { s, 0 });
             var dp = new int[n];
@@ -37,13 +32,9 @@ namespace FindTheClosestMarkedNode
                     queue.Enqueue(new[] { next[0], cur[1] + next[1] });
                 }
             }
-            var targets = new HashSet<int>(marked);
             var res = int.MaxValue;
-            for (int i = 0; i < dp.Length; i++)
-            {
-                if (!targets.Contains(i)) continue;
-                res = Math.Min(res, dp[i]);
-            }
+            foreach (var node in marked)
+                res = Math.Min(res, dp[node]);
             return res == int.MaxValue ? -1 : res;
         }
     }
