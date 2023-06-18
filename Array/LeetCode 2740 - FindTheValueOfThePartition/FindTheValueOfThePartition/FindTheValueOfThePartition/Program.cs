0c6e19da93c993d2d549a0dbd3085a14c9408347
Xing <x.xing.unsw@gmail.com>
2023-06-18 19:20:46 +1000

    init

---
+++ Array/LeetCode 2740 - FindTheValueOfThePartition/FindTheValueOfThePartition/FindTheValueOfThePartition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FindTheValueOfThePartition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 10, 100 };
+            Console.WriteLine(FindValueOfPartition(nums));
+        }
+
+        public static int FindValueOfPartition(int[] nums)
+        {
+            Array.Sort(nums);
+            var res = int.MaxValue;
+            for (int i = 1; i < nums.Length; i++)
+                res = Math.Min(res, nums[i] - nums[i - 1]);
+            return res;
+        }
+    }
+}
