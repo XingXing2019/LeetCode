fa9fdcf0e4e3f1b3c1e35e050ba5c09bdf341ed2
Xing <x.xing.unsw@gmail.com>
2023-06-18 19:08:55 +1000

    init

---
+++ Math/LeetCode 2739 - TotalDistanceTraveled/TotalDistanceTraveled/TotalDistanceTraveled/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TotalDistanceTraveled
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mainTank = 9, addtionalTank = 2;
+            Console.WriteLine(DistanceTraveled(mainTank, addtionalTank));
+        }
+
+        public static int DistanceTraveled(int mainTank, int additionalTank)
+        {
+            var res = 0;
+            while (mainTank >= 5 && additionalTank > 0)
+            {
+                res += 50;
+                mainTank -= 5;
+                mainTank++;
+                additionalTank--;
+            }
+            return res + mainTank * 10;
+        }
+    }
+}
