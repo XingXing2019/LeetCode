c3b946b1f3b17490e909731886bdc40ebc4ab628
Xing <x.xing.unsw@gmail.com>
2023-06-25 13:50:24 +1000

    init

---
+++ Greedy/LeetCode 2745 - ConstructTheLongestNewString/ConstructTheLongestNewString/ConstructTheLongestNewString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConstructTheLongestNewString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int LongestString(int x, int y, int z)
+        {
+            return ((x == y ? x * 2 : Math.Min(x, y) * 2 + 1) + z) * 2;
+        }
+    }
+}
