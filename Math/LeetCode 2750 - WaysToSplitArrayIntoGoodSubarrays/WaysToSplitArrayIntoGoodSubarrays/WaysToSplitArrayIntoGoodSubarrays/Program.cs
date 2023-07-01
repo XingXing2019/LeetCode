34f5b871da548014eb338ae658525ce563e61c3c
Xing <x.xing.unsw@gmail.com>
2023-07-01 16:09:19 +1000

    init

---
+++ Math/LeetCode 2750 - WaysToSplitArrayIntoGoodSubarrays/WaysToSplitArrayIntoGoodSubarrays/WaysToSplitArrayIntoGoodSubarrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WaysToSplitArrayIntoGoodSubarrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 0, 1 };
+            Console.WriteLine(NumberOfGoodSubarraySplits(nums));
+        }
+
+        public static int NumberOfGoodSubarraySplits(int[] nums)
+        {
+            var index = Array.IndexOf(nums, 1) + 1;
+            if (index == 0) return 0;
+            long res = 1, count = 0, mod = 1_000_000_000 + 7;
+            while (index < nums.Length)
+            {
+                count++;
+                if (nums[index++] == 1)
+                {
+                    res = (res * count) % mod;
+                    count = 0;
+                }
+            }
+            return (int)(res % mod);
+        }
+    }
+}
