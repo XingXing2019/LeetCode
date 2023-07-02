23e3db360145fdfe98f44bee3e9ed21b9172b739
Xing <x.xing.unsw@gmail.com>
2023-07-02 18:49:58 +1000

    init

---
+++ Array/LeetCode 2760 - LongestEvenOddSubarrayWithThreshold/LongestEvenOddSubarray/LongestEvenOddSubarray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LongestEvenOddSubarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int LongestAlternatingSubarray(int[] nums, int threshold)
+        {
+            var res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 != 0 || nums[i] > threshold) continue;
+                var index = i;
+                while (index < nums.Length && nums[index] <= threshold)
+                {
+                    if (index != i && nums[index] % 2 == nums[index - 1] % 2)
+                        break;
+                    index++;
+                }
+                res = Math.Max(res, index - i);
+            }
+            return res;
+        }
+    }
+}
