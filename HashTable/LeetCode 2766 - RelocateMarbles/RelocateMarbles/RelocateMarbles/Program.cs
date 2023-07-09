64fcbd8da9e99beeb3a5f732188779d7babe2607
Xing <x.xing.unsw@gmail.com>
2023-07-09 10:09:52 +1000

    init

---
+++ HashTable/LeetCode 2766 - RelocateMarbles/RelocateMarbles/RelocateMarbles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RelocateMarbles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IList<int> RelocateMarbles(int[] nums, int[] moveFrom, int[] moveTo)
+        {
+            var locations = new HashSet<int>(nums);
+            for (int i = 0; i < moveFrom.Length; i++)
+            {
+                locations.Remove(moveFrom[i]);
+                locations.Add(moveTo[i]);
+            }
+            return locations.OrderBy(x => x).ToArray();
+        }
+    }
+}
