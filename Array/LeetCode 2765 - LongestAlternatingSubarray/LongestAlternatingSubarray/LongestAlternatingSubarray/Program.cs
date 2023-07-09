e8d37b3160f2f409124e63dea08a5664dc8397ca
Xing <x.xing.unsw@gmail.com>
2023-07-09 10:03:56 +1000

    init

---
+++ Array/LeetCode 2765 - LongestAlternatingSubarray/LongestAlternatingSubarray/LongestAlternatingSubarray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LongestAlternatingSubarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 3, 5, 3, 4 };
+            Console.WriteLine(AlternatingSubarray(nums));
+        }
+
+        public static int AlternatingSubarray(int[] nums)
+        {
+            var res = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var count = 1;
+                var increase = true;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (increase && nums[j] - nums[j - 1] != 1 || !increase && nums[j - 1] - nums[j] != 1)
+                        break;
+                    count++;
+                    increase = !increase;
+                    res = Math.Max(res, count);
+                }
+            }
+            return res;
+        }
+    }
+}
