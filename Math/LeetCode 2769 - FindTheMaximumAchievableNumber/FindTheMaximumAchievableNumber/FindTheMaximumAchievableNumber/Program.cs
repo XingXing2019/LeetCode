c0d32c6d692123bd1bbb13f16aca6a9832b1c933
Xing <x.xing.unsw@gmail.com>
2023-07-09 14:42:27 +1000

    init

---
+++ Math/LeetCode 2769 - FindTheMaximumAchievableNumber/FindTheMaximumAchievableNumber/FindTheMaximumAchievableNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FindTheMaximumAchievableNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int TheMaximumAchievableX(int num, int t)
+        {
+            return num + 2 * t;
+        }
+    }
+}
