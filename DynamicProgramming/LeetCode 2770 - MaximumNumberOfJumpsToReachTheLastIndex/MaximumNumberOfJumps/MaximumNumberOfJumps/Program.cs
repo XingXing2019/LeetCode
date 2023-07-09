ffc18a708aa3e7ec4d183e82e3caf961a49fd249
Xing <x.xing.unsw@gmail.com>
2023-07-09 14:50:01 +1000

    init

---
+++ DynamicProgramming/LeetCode 2770 - MaximumNumberOfJumpsToReachTheLastIndex/MaximumNumberOfJumps/MaximumNumberOfJumps/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MaximumNumberOfJumps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaximumJumps(int[] nums, int target)
+        {
+            var dp = new int[nums.Length];
+            for (int i = 1; i < dp.Length; i++)
+            {
+                var max = -1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (dp[j] == -1) continue;
+                    if (Math.Abs(nums[i] - nums[j]) <= target)
+                        max = Math.Max(max, dp[j] + 1);
+                }
+                dp[i] = max;
+            }
+            return dp[^1];
+        }
+    }
+}
