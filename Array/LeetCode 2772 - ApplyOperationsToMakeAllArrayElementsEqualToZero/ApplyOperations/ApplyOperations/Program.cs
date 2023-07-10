1ebee864378e903a8172a93ad63ae6b26854699f
Xing <x.xing.unsw@gmail.com>
2023-07-10 19:34:06 +1000

    init

---
+++ Array/LeetCode 2772 - ApplyOperationsToMakeAllArrayElementsEqualToZero/ApplyOperations/ApplyOperations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ApplyOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 60, 72, 87, 89, 63, 52, 64, 62, 31, 37, 57, 83, 98, 94, 92, 77, 94, 91, 87, 100, 91, 91, 50, 26 };
+            var k = 4;
+            Console.WriteLine(CheckArray(nums, k));
+        }
+
+        public static bool CheckArray(int[] nums, int k)
+        {
+            if (k == 1) return true;
+            var decrease = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] -= decrease;
+                if (nums[i] < 0)
+                    return false;
+                decrease += nums[i];
+                if (i - k + 1 >= 0)
+                    decrease -= nums[i - k + 1];
+            }
+            return nums[^1] == 0;
+        }
+    }
+}
