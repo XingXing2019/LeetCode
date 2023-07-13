474e6e44e2c7c4b42e85c4a862cda406a01dc7be
Xing <x.xing.unsw@gmail.com>
2023-07-13 17:53:02 +1000

    init

---
+++ Tree/LeetCode 2773 - HeightOfSpecialBinaryTree/HeightOfSpecialBinaryTree/HeightOfSpecialBinaryTree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HeightOfSpecialBinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new TreeNode(1);
+            var b = new TreeNode(2);
+            a.left = b;
+            Console.WriteLine(HeightOfTree(a));
+        }
+
+        public static int HeightOfTree(TreeNode root)
+        {
+            var leaves = new HashSet<int>();
+            GetLeaves(root, leaves, new HashSet<int>());
+            return DFS(root, leaves);
+        }
+
+        public static void GetLeaves(TreeNode node, HashSet<int> leaves, HashSet<int> visited)
+        {
+            if (node == null || !visited.Add(node.val))
+                return;
+            if (node.left == node.right)
+                leaves.Add(node.val);
+            else if (node.left != null && node.right != null && node.left.right == node)
+                leaves.Add(node.val);
+            GetLeaves(node.left, leaves, visited);
+            GetLeaves(node.right, leaves, visited);
+        }
+
+        public static int DFS(TreeNode node, HashSet<int> leaves)
+        {
+            if (node == null || leaves.Contains(node.val))
+                return 0;
+            var left = DFS(node.left, leaves);
+            var right = DFS(node.right, leaves);
+            return Math.Max(left, right) + 1;
+        }
+    }
+}
