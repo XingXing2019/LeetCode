300a9a403385423f8b34dd1206bead03a7fba562
Xing <x.xing.unsw@gmail.com>
2023-07-16 19:18:29 +1000

    init

---
+++ Array/LeetCode 2778 - SumOfSquaresOfSpecialElements/SumOfSquaresOfSpecialElements/SumOfSquaresOfSpecialElements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfSquaresOfSpecialElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int SumOfSquares(int[] nums)
+        {
+            return nums.Where((x, i) => nums.Length % (i + 1) == 0).Sum(x => x * x);
+        }
+    }
+}
