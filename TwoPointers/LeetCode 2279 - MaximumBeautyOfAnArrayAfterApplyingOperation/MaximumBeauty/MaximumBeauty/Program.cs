6e52787e83d43397365fdca7e68e43cee16f1aaa
Xing <x.xing.unsw@gmail.com>
2023-07-16 19:24:17 +1000

    init

---
+++ TwoPointers/LeetCode 2279 - MaximumBeautyOfAnArrayAfterApplyingOperation/MaximumBeauty/MaximumBeauty/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaximumBeauty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaximumBeauty(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int li = 0, hi = 0, res = 0;
+            while (hi < nums.Length)
+            {
+                while (li < hi && nums[li] + 2 * k < nums[hi])
+                    li++;
+                res = Math.Max(res, hi - li + 1);
+                hi++;
+            }
+            return res;
+        }
+    }
+}
