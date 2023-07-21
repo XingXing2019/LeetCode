8cb458e17e7d53f151b4666e03fb69e5a096dedb
Xing <x.xing.unsw@gmail.com>
2023-07-21 20:09:37 +1000

    init

---
+++ DynamicProgramming/LeetCode 673 - NumberOfLongestIncreasingSubsequence/NumberOfLongestIncreasingSubsequence/NumberOfLongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberOfLongestIncreasingSubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 2 };
+            Console.WriteLine(FindNumberOfLIS(nums));
+        }
+
+        public static int FindNumberOfLIS(int[] nums)
+        {
+            var lenDp = new int[nums.Length];
+            var countDp = new int[nums.Length];
+            lenDp[0] = countDp[0] = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] <= nums[j]) continue;
+                    if (lenDp[j] + 1 > lenDp[i])
+                    {
+                        lenDp[i] = lenDp[j] + 1;
+                        countDp[i] = countDp[j];
+                    } 
+                    else if (lenDp[j] + 1 == lenDp[i])
+                        countDp[i] += countDp[j];
+                }
+            }
+            int max = lenDp.Max(), res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (lenDp[i] == max)
+                    res += countDp[i];
+            }
+            return res;
+        }
+    }
+}
