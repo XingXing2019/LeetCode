2db7cdffb40c930b6b3197bebd1bf3f775118313
Xing <x.xing.unsw@gmail.com>
2023-07-23 09:39:42 +1000

    init

---
+++ String/LeetCode 2785 - SortVowelsInAString/SortVowelsInAString/SortVowelsInAString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortVowelsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string SortVowels(string s)
+        {
+            var vowels = s.Where(x => IsVowels(x)).OrderBy(x => x).ToList();
+            var index = 0;
+            var res = new StringBuilder();
+            foreach (var l in s)
+            {
+                if (!IsVowels(l))
+                    res.Append(l);
+                else
+                    res.Append(vowels[index++]);
+            }
+            return res.ToString();
+        }
+
+        public bool IsVowels(char l)
+        {
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            return vowels.Contains(l);
+        }
+    }
+}
