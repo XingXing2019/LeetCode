1b4e6f1ce2bc6e81c2c27db103019ba09deb47dd
Xing <x.xing.unsw@gmail.com>
2023-07-23 09:35:13 +1000

    init

---
+++ Array/LeetCode 2784 - CheckIfArrayIsGood/CheckIfArrayIsGood/CheckIfArrayIsGood/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace CheckIfArrayIsGood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 3, 2 };
+            Console.WriteLine(IsGood(nums));
+        }
+
+        public static bool IsGood(int[] nums)
+        {
+            var n = nums.Max();
+            var dict = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == n && (!dict.ContainsKey(i) || dict[i] != 2))
+                    return false;
+                if (i != n && (!dict.ContainsKey(i) || dict[i] != 1))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
