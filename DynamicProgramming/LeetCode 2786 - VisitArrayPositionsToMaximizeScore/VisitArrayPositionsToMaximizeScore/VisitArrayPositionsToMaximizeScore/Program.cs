f2d93b565cff818e86b79fc0b931d5a1574aa570
Xing <x.xing.unsw@gmail.com>
2023-07-23 10:30:10 +1000

    init

--- DynamicProgramming/LeetCode 2786 - VisitArrayPositionsToMaximizeScore/VisitArrayPositionsToMaximizeScore/VisitArrayPositionsToMaximizeScore/Program.cs
+++ DynamicProgramming/LeetCode 2786 - VisitArrayPositionsToMaximizeScore/VisitArrayPositionsToMaximizeScore/VisitArrayPositionsToMaximizeScore/Program.cs
@@ -6,12 +6,23 @@ namespace VisitArrayPositionsToMaximizeScore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = { 8, 50, 65 };
+            var x = 74;
+            Console.WriteLine(MaxScore(nums, x));
         }
 
-        public long MaxScore(int[] nums, int x)
+        public static long MaxScore(int[] nums, int x)
         {
-
+            long odd = nums[0] % 2 != 0 ? nums[0] : nums[0] - x;
+            long even = nums[0] % 2 == 0 ? nums[0] : nums[0] - x;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                    even = Math.Max(even + nums[i], odd + nums[i] - x);
+                else
+                    odd = Math.Max(odd + nums[i], even + nums[i] - x);
+            }
+            return Math.Max(odd, even);
         }
     }
 }
