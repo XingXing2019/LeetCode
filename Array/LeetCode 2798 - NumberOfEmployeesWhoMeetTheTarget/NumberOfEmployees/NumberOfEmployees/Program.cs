38ad060d451571a17fee2f4a42cb3f9f942f3d8f
Xing <x.xing.unsw@gmail.com>
2023-07-30 21:23:34 +1000

    init

---
+++ Array/LeetCode 2798 - NumberOfEmployeesWhoMeetTheTarget/NumberOfEmployees/NumberOfEmployees/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace NumberOfEmployees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            return hours.Count(x => x >= target);
+        }
+    }
+}
