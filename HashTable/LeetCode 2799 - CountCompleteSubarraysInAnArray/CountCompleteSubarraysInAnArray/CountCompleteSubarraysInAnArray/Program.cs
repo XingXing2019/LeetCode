816cc4ff606b42ee0b520d4d96ffd01aabe95c2e
Xing <x.xing.unsw@gmail.com>
2023-07-30 22:23:59 +1000

    init

--- HashTable/LeetCode 2799 - CountCompleteSubarraysInAnArray/CountCompleteSubarraysInAnArray/CountCompleteSubarraysInAnArray/Program.cs
+++ HashTable/LeetCode 2799 - CountCompleteSubarraysInAnArray/CountCompleteSubarraysInAnArray/CountCompleteSubarraysInAnArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CountCompleteSubarraysInAnArray
 {
@@ -7,6 +8,24 @@ namespace CountCompleteSubarraysInAnArray
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public int CountCompleteSubarrays(int[] nums)
+        {
+            var unique = new HashSet<int>(nums).Count;
+            var res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var set = new HashSet<int>();
+                for (int j = i; j < nums.Length && set.Count <= unique; j++)
+                {
+                    set.Add(nums[j]);
+                    if (set.Count != unique)
+                        continue;
+                    res++;
+                }
+            }
+            return res;
         }
     }
 }
---
+++ String/LeetCode 2800 - ShortestStringThatContainsThreeStrings/ShortestStringThatContainsThreeStrings/ShortestStringThatContainsThreeStrings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ShortestStringThatContainsThreeStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "cab", b = "a", c = "b";
+            Console.WriteLine(MinimumString(a, b, c));
+        }
+
+        public static string MinimumString(string a, string b, string c)
+        {
+            var abc = Join(Join(a, b), c);
+            var acb = Join(Join(a, c), b);
+            var bac = Join(Join(b, a), c);
+            var bca = Join(Join(b, c), a);
+            var cab = Join(Join(c, a), b);
+            var cba = Join(Join(c, b), a);
+            string[] strs = { abc, acb, bac, bca, cab, cba };
+            return strs.OrderBy(x => x.Length).ThenBy(x => x).First();
+        }
+
+        public static string Join(string s1, string s2)
+        {
+            if (s1.Contains(s2)) return s1;
+            if (s2.Contains(s1)) return s2;
+            var max = 0;
+            for (int i = 1; i <= Math.Min(s1.Length, s2.Length); i++)
+            {
+                var str1 = s1.Substring(s1.Length - i);
+                var str2 = s2.Substring(0, i);
+                if (str1 == str2)
+                    max = Math.Max(max, i);
+            }
+            return s1 + s2.Substring(max);
+        }
+    }
+}
