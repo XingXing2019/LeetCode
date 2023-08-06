bfd26dcd286f1e8cbdd02912c912a79240803e7e
Xing <x.xing.unsw@gmail.com>
2023-08-06 20:06:53 +1000

    init

---
+++ Math/LeetCode 2806 - AccountBalanceAfterRoundedPurchase/AccountBalanceAfterRoundedPurchase/AccountBalanceAfterRoundedPurchase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AccountBalanceAfterRoundedPurchase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int AccountBalanceAfterPurchase(int purchaseAmount)
+        {
+            return 100 - (purchaseAmount % 10 < 5 ? purchaseAmount / 10 * 10 : (purchaseAmount / 10 + 1) * 10);
+        }
+    }
+}
