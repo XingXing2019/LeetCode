81a9e3cfaa4a0247d39d5d34f3f448f530253eb2
Xing <x.xing.unsw@gmail.com>
2023-08-06 20:11:21 +1000

    init

---
+++ LinkedList/LeetCode 2807 - InsertGreatestCommonDivisorsInLinkedList/InsertGreatestCommonDivisorsInLinkedList/InsertGreatestCommonDivisorsInLinkedList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace InsertGreatestCommonDivisorsInLinkedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public ListNode InsertGreatestCommonDivisors(ListNode head)
+        {
+            var point = head;
+            while (point != null && point.next != null)
+            {
+                var gcd = GCD(point.val, point.next.val);
+                var node = new ListNode(gcd, point.next);
+                point.next = node;
+                point = node.next;
+            }
+            return head;
+        }
+
+        public int GCD(int x, int y)
+        {
+            return y == 0 ? x : GCD(y, x % y);
+        }
+    }
+}
