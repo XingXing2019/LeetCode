4e60e54f32135bf8f37ade2a2070c09e16c4eb63
Xing <x.xing.unsw@gmail.com>
2023-08-08 22:37:56 +1000

    init

--- Array/LeetCode 2808 - MinimumSecondsToEqualizeACircularArray/MinimumSecondsToEqualizeACircularArray/MinimumSecondsToEqualizeACircularArray/Program.cs
+++ Array/LeetCode 2808 - MinimumSecondsToEqualizeACircularArray/MinimumSecondsToEqualizeACircularArray/MinimumSecondsToEqualizeACircularArray/Program.cs
@@ -14,42 +14,24 @@ namespace MinimumSecondsToEqualizeACircularArray
 
         public static int MinimumSeconds(IList<int> nums)
         {
-            var dict = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
-            var max = dict.Max(x => x.Value);
-            var keys = dict.Where(x => x.Value == max).Select(x => x.Key).ToList();
+            var maxDis = new Dictionary<int, int>();
+            var firstIndexes = new Dictionary<int, int>();
+            var lastIndexes = new Dictionary<int, int>();
             var res = int.MaxValue;
-            foreach (var key in keys)
-                res = Math.Min(res, GetSeconds(nums, key));
-            return res;
-        }
-
-        public static int GetSeconds(IList<int> nums, int key)
-        {
-            int first = -1, last = -1, res = 0;
             for (int i = 0; i < nums.Count; i++)
             {
-                if (nums[i] == key && first == -1)
-                    first = i;
-                if (nums[^(i + 1)] == key && last == -1)
-                    last = nums.Count - i - 1;
+                if (lastIndexes.ContainsKey(nums[i]))
+                    maxDis[nums[i]] = Math.Max(i - lastIndexes[nums[i]], maxDis.GetValueOrDefault(nums[i], 0));
+                if (!firstIndexes.ContainsKey(nums[i]))
+                    firstIndexes[nums[i]] = i;
+                lastIndexes[nums[i]] = i;
             }
-            int[] prev = new int[nums.Count], post = new int[nums.Count];
-            for (int i = 0; i < nums.Count; i++)
+            foreach (var num in firstIndexes.Keys)
             {
-                if (nums[i] == key)
-                    last = i;
-                else
-                    prev[i] = last > i ? nums.Count - last + i : i - last;
+                var inner = maxDis.ContainsKey(num) ? maxDis[num] / 2 : 0;
+                var outer = (nums.Count - lastIndexes[num] + firstIndexes[num]) / 2;
+                res = Math.Min(res, Math.Max(inner, outer));
             }
-            for (int i = nums.Count - 1; i >= 0; i--)
-            {
-                if (nums[i] == key)
-                    first = i;
-                else
-                    post[i] = first < i ? first + nums.Count - i : first - i;
-            }
-            for (int i = 0; i < nums.Count; i++)
-                res = Math.Max(res, Math.Min(prev[i], post[i]));
             return res;
         }
     }
