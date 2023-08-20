a2b481de409439ad67d561e0fdc3143d765ef438
Xing <x.xing.unsw@gmail.com>
2023-08-20 20:27:00 +1000

    init

---
+++ String/LeetCode 2828 - CheckIfAStringIsAnAcronymOfWords/CheckIfAStringIsAnAcronymOfWords/CheckIfAStringIsAnAcronymOfWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckIfAStringIsAnAcronymOfWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsAcronym(IList<string> words, string s)
+        {
+
+        }
+    }
+}
