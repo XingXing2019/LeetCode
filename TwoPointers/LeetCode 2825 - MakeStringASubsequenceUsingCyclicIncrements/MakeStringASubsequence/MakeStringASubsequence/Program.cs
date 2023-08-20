995535b6e0e86fd5f5ada3623cf6d697102c4d07
Xing <x.xing.unsw@gmail.com>
2023-08-20 19:59:53 +1000

    init

---
+++ TwoPointers/LeetCode 2825 - MakeStringASubsequenceUsingCyclicIncrements/MakeStringASubsequence/MakeStringASubsequence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MakeStringASubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool CanMakeSubsequence(string str1, string str2)
+        {
+            int p1 = 0, p2 = 0;
+            while (p1 < str1.Length && p2 < str2.Length)
+            {
+                var next = str1[p1] + 1 > 'z' ? 'a' : str1[p1] + 1;
+                if (str1[p1] == str2[p2] || next == str2[p2])
+                    p2++;
+                p1++;
+            }
+            return p2 == str2.Length;
+        }
+    }
+}
