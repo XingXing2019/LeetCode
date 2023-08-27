4dc54b8f210b38799b0daa238d9daea55afca399
Xing <x.xing.unsw@gmail.com>
2023-08-27 17:14:54 +1000

    init

---
+++ Greedy/LeetCode 2834 - FindTheMinimumPossibleSumOfABeautifulArray/FindTheMinimumPossibleSum/FindTheMinimumPossibleSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindTheMinimumPossibleSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long MinimumPossibleSum(int n, int target)
+        {
+            var banned = new HashSet<int>();
+            var start = 1;
+            var sum = 0L;
+            while (n != 0)
+            {
+                if (!banned.Contains(start))
+                {
+                    sum += start;
+                    banned.Add(target - start);
+                    n--;
+                }
+                start++;
+            }
+            return sum;
+        }
+    }
+}
