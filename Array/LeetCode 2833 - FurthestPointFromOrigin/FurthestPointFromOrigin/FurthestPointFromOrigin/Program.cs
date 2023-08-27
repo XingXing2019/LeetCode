6c8504fbb9842c2f568c2b4dbd49bbca34fb66d0
Xing <x.xing.unsw@gmail.com>
2023-08-27 17:06:21 +1000

    init

---
+++ Array/LeetCode 2833 - FurthestPointFromOrigin/FurthestPointFromOrigin/FurthestPointFromOrigin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace FurthestPointFromOrigin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public int FurthestDistanceFromOrigin(string moves)
+        {
+            return Math.Max(GetDistance(moves.Replace('_', 'L')), GetDistance(moves.Replace('_', 'R')));
+        }
+
+        public int GetDistance(string moves)
+        {
+            return Math.Abs(moves.Count(x => x == 'L') - moves.Count(x => x == 'R'));
+        }
+    }
+}
