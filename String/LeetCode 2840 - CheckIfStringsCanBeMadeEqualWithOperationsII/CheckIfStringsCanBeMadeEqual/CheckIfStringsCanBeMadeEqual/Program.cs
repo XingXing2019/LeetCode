2be88965195438c860cfb0b90df313e9f8dbf1b5
Xing <x.xing.unsw@gmail.com>
2023-09-03 08:31:46 +1000

    init

---
+++ String/LeetCode 2840 - CheckIfStringsCanBeMadeEqualWithOperationsII/CheckIfStringsCanBeMadeEqual/CheckIfStringsCanBeMadeEqual/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckIfStringsCanBeMadeEqual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool CheckStrings(string s1, string s2)
+        {
+            var str1 = new StringBuilder(s1);
+            var pos = new SortedSet<int>[26][];
+            for (int i = 0; i < 26; i++)
+                pos[i] = new[] {new SortedSet<int>(), new SortedSet<int>()};
+            for (var i = 0; i < s1.Length; i++)
+            {
+                if (i % 2 == 0)
+                    pos[s1[i] - 'a'][0].Add(i);
+                else
+                    pos[s1[i] - 'a'][1].Add(i);
+            }
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] == s2[i]) continue;
+                var targets = i % 2 == 0 ? pos[s2[i] - 'a'][0] : pos[s2[i] - 'a'][1];
+                if (targets.Count == 0) 
+                    return false;
+                var index = targets.Min;
+                targets.Remove(index);
+                if (i % 2 == 0)
+                {
+                    pos[str1[i] - 'a'][0].Remove(i);
+                    pos[str1[i] - 'a'][0].Add(index);
+                }
+                else
+                {
+                    pos[str1[i] - 'a'][1].Remove(i);
+                    pos[str1[i] - 'a'][1].Add(index);
+                }
+                (str1[i], str1[index]) = (str1[index], str1[i]);
+            }
+            return true;
+        }
+    }
+}
---
+++ TwoPointers/LeetCode 2841 - MaximumSumOfAlmostUniqueSubarray/MaximumSumOfAlmostUniqueSubarray/MaximumSumOfAlmostUniqueSubarray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumSumOfAlmostUniqueSubarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new List<int> { 2, 6, 7, 3, 1, 7 };
+            int m = 3, k = 4;
+            Console.WriteLine(MaxSum(nums, m, k));
+        }
+
+        public static long MaxSum(IList<int> nums, int m, int k)
+        {
+            var index = 0;
+            long res = 0, sum = 0;
+            var freq = new Dictionary<int, int>();
+            for (int i = 0; i < k; i++)
+            {
+                if (!freq.ContainsKey(nums[index]))
+                    freq[nums[index]] = 0;
+                freq[nums[index]]++;
+                sum += nums[index++];
+            }
+            if (freq.Count >= m)
+                res = sum;
+            while (index < nums.Count)
+            {
+                var front = nums[index - k];
+                var after = nums[index];
+                freq[front]--;
+                if (freq[front] == 0)
+                    freq.Remove(front);
+                if (!freq.ContainsKey(after))
+                    freq[after] = 0;
+                freq[after]++;
+                sum = sum - front + after;
+                if (freq.Count >= m)
+                    res = Math.Max(res, sum);
+                index++;
+            }
+            return res;
+        }
+    }
+}
