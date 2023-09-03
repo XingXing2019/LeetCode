1bc3fd5162d45c91edb31d5582a1b11e05b14ba8
Xing <x.xing.unsw@gmail.com>
2023-09-03 15:29:09 +1000

    init

---
+++ Math/LeetCode 2843 - CountSymmetricIntegers/CountSymmetricIntegers/CountSymmetricIntegers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CountSymmetricIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int CountSymmetricIntegers(int low, int high)
+        {
+            var res = 0;
+            for (int i = low; i <= high; i++)
+            {
+                if (IsSymmetric(i.ToString()))
+                    res++;
+            }
+            return res;
+        }
+
+        public bool IsSymmetric(string num)
+        {
+            if (num.Length % 2 != 0)
+                return false;
+            int li = 0, hi = num.Length - 1;
+            int front = 0, back = 0;
+            while (li < hi)
+            {
+                front += num[li++];
+                back += num[hi--];
+            }
+            return front == back;
+        }
+    }
+}
