2aa1890643dd9c210608457fec9e8ebf73cb6227
Xing <x.xing.unsw@gmail.com>
2023-09-10 20:51:25 +1000

    init

---
+++ Math/LeetCode 2849 - DetermineIfACellIsReachableAtAGivenTime/DetermineIfACellIsReachableAtAGivenTime/DetermineIfACellIsReachableAtAGivenTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DetermineIfACellIsReachableAtAGivenTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsReachableAtTime(int sx, int sy, int fx, int fy, int t)
+        {
+            if (sx == fx && sy == fy)
+                return t != 1;
+            var dis = Math.Max(Math.Abs(sx - fx), Math.Abs(sy - fy));
+            return dis <= t;
+        }
+    }
+}
