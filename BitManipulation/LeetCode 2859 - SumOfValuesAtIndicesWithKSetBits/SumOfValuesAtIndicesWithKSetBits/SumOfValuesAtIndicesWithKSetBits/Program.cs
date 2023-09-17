6f16f923dbec56944e35423de6af5654f974d03d
Xing <x.xing.unsw@gmail.com>
2023-09-17 20:15:38 +1000

    init

---
+++ BitManipulation/LeetCode 2859 - SumOfValuesAtIndicesWithKSetBits/SumOfValuesAtIndicesWithKSetBits/SumOfValuesAtIndicesWithKSetBits/Program.cs
@@ -0,0 +1,19 @@
+﻿var nums = new List<int> { 5, 10, 1, 5, 2 };
+var k = 1;
+Console.WriteLine(SumIndicesWithKSetBits(nums, k));
+
+int SumIndicesWithKSetBits(IList<int> nums, int k)
+{
+    return nums.Where((x, i) => IsKSetBits(i, k)).Select((x, i) => x).Sum();
+}
+
+bool IsKSetBits(int num, int k)
+{
+    while (num != 0)
+    {
+        if ((num & 1) == 1)
+            k--;
+        num >>= 1;
+    }
+    return k == 0;
+}
