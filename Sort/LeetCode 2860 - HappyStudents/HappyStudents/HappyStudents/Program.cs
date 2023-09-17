d6df23889a01459a8bccae9c5d44d3a5c4140f42
Xing <x.xing.unsw@gmail.com>
2023-09-17 21:19:54 +1000

    init

---
+++ Sort/LeetCode 2860 - HappyStudents/HappyStudents/HappyStudents/Program.cs
@@ -0,0 +1,12 @@
+﻿int CountWays(IList<int> nums)
+{
+    nums = nums.OrderBy(x => x).ToList();
+    nums.Add(int.MaxValue);
+    var res = nums[0] != 0 ? 1 : 0;
+    for (int i = 0; i < nums.Count; i++)
+    {
+        if (i + 1 > nums[i] && i + 1 < nums[i + 1])
+            res++;
+    }
+    return res;
+}
