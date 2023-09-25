9554179078d15635d04916c3f2cfd7d26db46234
Xing <x.xing.unsw@gmail.com>
2023-09-25 20:28:00 +1000

    init

---
+++ Array/LeetCode 2865 - BeautifulTowersI/BeautifulTowersI/BeautifulTowersI/Program.cs
@@ -0,0 +1,30 @@
+﻿var maxHeights = new List<int> { 5, 3, 4, 1, 1 };
+Console.WriteLine(MaximumSumOfHeights(maxHeights));
+
+long MaximumSumOfHeights(IList<int> maxHeights)
+{
+    long res = 0;
+    for (int i = 0; i < maxHeights.Count; i++)
+        res = Math.Max(res, GetSum(maxHeights, i));
+    return res;
+}
+
+long GetSum(IList<int> maxHeights, int index)
+{
+    long res = 0;
+    var last = maxHeights[index];
+    for (int i = index - 1; i >= 0; i--)
+    {
+        var cur = Math.Min(last, maxHeights[i]);
+        res += cur;
+        last = cur;
+    }
+    last = maxHeights[index];
+    for (int i = index + 1; i < maxHeights.Count; i++)
+    {
+        var cur = Math.Min(last, maxHeights[i]);
+        res += cur;
+        last = cur;
+    }
+    return res + maxHeights[index];
+}
--- String/LeetCode 2864 - MaximumOddBinaryNumber/MaximumOddBinaryNumber/MaximumOddBinaryNumber/Program.cs
+++ String/LeetCode 2864 - MaximumOddBinaryNumber/MaximumOddBinaryNumber/MaximumOddBinaryNumber/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string MaximumOddBinaryNumber(string s)
+{
+    int zeros = s.Count(x => x == '0'), one = s.Count(x => x == '1');
+    return $"{new string('1', one - 1)}{new string('0', zeros)}1";
+}
