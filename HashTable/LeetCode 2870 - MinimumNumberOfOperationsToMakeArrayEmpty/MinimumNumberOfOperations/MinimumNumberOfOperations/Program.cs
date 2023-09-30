aefe9f608a2cceda6d2674ad488347b3279ca18a
Xing <x.xing.unsw@gmail.com>
2023-10-01 10:35:22 +1100

    init

---
+++ HashTable/LeetCode 2870 - MinimumNumberOfOperationsToMakeArrayEmpty/MinimumNumberOfOperations/MinimumNumberOfOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿int MinOperations(int[] nums)
+{
+    var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    var res = 0;
+    foreach (var num in freq.Keys)
+    {
+        if (freq[num] == 1)
+            return -1;
+        res += freq[num] % 3 == 0 ? freq[num] / 3 : freq[num] / 3 + 1;
+    }
+    return res;
+}
