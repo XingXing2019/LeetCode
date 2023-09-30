6bb2e5b66df1f34c64fbb5bf2eab28d149cb3ae4
Xing <x.xing.unsw@gmail.com>
2023-10-01 10:25:57 +1100

    init

---
+++ DynamicProgramming/LeetCode 2866 - BeautifulTowersII/MinimumOperationsToCollectElements/MinimumOperationsToCollectElements/Program.cs
@@ -0,0 +1,12 @@
+﻿int MinOperations(IList<int> nums, int k)
+{
+    var collection = new HashSet<int>();
+    for (int i = nums.Count - 1; i >= 0; i--)
+    {
+        if (nums[i] > k) continue;
+        collection.Add(nums[i]);
+        if (collection.Count == k)
+            return nums.Count - i;
+    }
+    return -1;
+}
