a8e0b77bc421d2288ca968346831f4d36c71d2e2
Xing <x.xing.unsw@gmail.com>
2023-10-01 10:52:30 +1100

    init

--- BitManipulation/LeetCode 2871 - SplitArrayIntoMaximumNumberOfSubarrays/SplitArrayIntoMaximumNumberOfSubarrays/SplitArrayIntoMaximumNumberOfSubarrays/Program.cs
+++ BitManipulation/LeetCode 2871 - SplitArrayIntoMaximumNumberOfSubarrays/SplitArrayIntoMaximumNumberOfSubarrays/SplitArrayIntoMaximumNumberOfSubarrays/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] nums = { 0, 1 };
+Console.WriteLine(MaxSubarrays(nums));
+int MaxSubarrays(int[] nums)
+{
+    var totalAnd = int.MaxValue;
+    foreach (var num in nums)
+        totalAnd &= num;
+    if (totalAnd != 0) return 1;
+    int currentAnd = int.MaxValue, res = 0;
+    foreach (var num in nums)
+    {
+        currentAnd &= num;
+        if (currentAnd == totalAnd)
+        {
+            res++;
+            currentAnd = int.MaxValue;
+        }
+    }
+    return res;
+}
