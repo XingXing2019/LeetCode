9640866c985ddc6e9d2ece1c36df584a355f099b
Xing <x.xing.unsw@gmail.com>
2023-10-01 21:03:03 +1100

    init

---
+++ HashTable/LeetCode 2874 - MaximumValueOfAnOrderedTripletII/MaximumValueOfAnOrderedTripletII/MaximumValueOfAnOrderedTripletII/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = { 12, 6, 1, 2, 7 };
+Console.WriteLine(MaximumTripletValue(nums));
+
+long MaximumTripletValue(int[] nums)
+{
+    var prefixMax = new long[nums.Length];
+    var suffixMax = new long[nums.Length];
+    long prefix = long.MinValue, suffix = long.MinValue, res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        prefixMax[i] = prefix;
+        prefix = Math.Max(prefix, nums[i]);
+        suffixMax[^(i + 1)] = suffix;
+        suffix = Math.Max(suffix, nums[^(i + 1)]);
+    }
+    for (int i = 1; i < nums.Length - 1; i++)
+        res = Math.Max(res, (prefixMax[i] - nums[i]) * suffixMax[i]);
+    return res;
+}
