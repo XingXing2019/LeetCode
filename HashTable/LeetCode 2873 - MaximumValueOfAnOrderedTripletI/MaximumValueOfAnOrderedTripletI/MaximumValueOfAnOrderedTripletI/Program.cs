adb74a0a48c97878c5900b7720c7126a6c04b18f
Xing <x.xing.unsw@gmail.com>
2023-10-01 20:46:49 +1100

    init

---
+++ HashTable/LeetCode 2873 - MaximumValueOfAnOrderedTripletI/MaximumValueOfAnOrderedTripletI/MaximumValueOfAnOrderedTripletI/Program.cs
@@ -0,0 +1,19 @@
+﻿long MaximumTripletValue(int[] nums)
+{
+    var suffix = new long[nums.Length];
+    var max = long.MinValue;
+    for (int i = nums.Length - 1; i >= 0; i--)
+    {
+        suffix[i] = max;
+        max = Math.Max(max, nums[i]);
+    }
+    long res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            res = Math.Max(res, (nums[i] - nums[j]) * suffix[j]);
+        }
+    }
+    return res;
+}
