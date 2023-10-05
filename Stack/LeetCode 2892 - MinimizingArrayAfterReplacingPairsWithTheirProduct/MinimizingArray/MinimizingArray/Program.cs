490bdc62f8f154ff896fac1c4ef765b2e45b514d
Xing <x.xing.unsw@gmail.com>
2023-10-05 22:32:27 +1100

    init

---
+++ Stack/LeetCode 2892 - MinimizingArrayAfterReplacingPairsWithTheirProduct/MinimizingArray/MinimizingArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int MinArrayLength(int[] nums, int k)
+{
+    var stack = new Stack<long>();
+    foreach (var num in nums)
+    {
+        long temp = num;
+        while (stack.Count != 0 && stack.Peek() * temp <= k)
+            temp *= stack.Pop();
+        stack.Push(temp);
+    }
+    return stack.Count;
+}
