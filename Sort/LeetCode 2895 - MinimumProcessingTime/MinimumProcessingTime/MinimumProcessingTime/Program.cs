1c08e4f44288ccf35a69ecde828b9c66a229403d
Xing <x.xing.unsw@gmail.com>
2023-10-08 16:07:48 +1100

    init

---
+++ Sort/LeetCode 2895 - MinimumProcessingTime/MinimumProcessingTime/MinimumProcessingTime/Program.cs
@@ -0,0 +1,9 @@
+﻿int MinProcessingTime(IList<int> processorTime, IList<int> tasks)
+{
+    processorTime = processorTime.OrderBy(x => x).ToList();
+    tasks = tasks.OrderByDescending(x => x).ToList();
+    var res = 0;
+    for (int i = 0; i < tasks.Count; i++)
+        res = Math.Max(res, tasks[i] + processorTime[i / 4]);
+    return res;
+}
