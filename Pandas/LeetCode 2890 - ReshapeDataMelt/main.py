2bba1731ead460f4884dcb55a4015f05eb057f8d
Xing <x.xing.unsw@gmail.com>
2023-10-09 18:06:33 +1100

    init

---
+++ Pandas/LeetCode 2890 - ReshapeDataMelt/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return pd.melt(
+        report,
+        id_vars=['product'],
+        value_vars=['quarter_1', 'quarter_2', 'quarter_3', 'quarter_4'],
+        var_name='quarter',
+        value_name='sales'
+    )
