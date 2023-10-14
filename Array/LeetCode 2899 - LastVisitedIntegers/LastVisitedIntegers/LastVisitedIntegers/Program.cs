f311c83311bcec141612c697054d5f400c166a10
Xing <x.xing.unsw@gmail.com>
2023-10-15 09:08:38 +1100

    init

---
+++ Array/LeetCode 2899 - LastVisitedIntegers/LastVisitedIntegers/LastVisitedIntegers/Program.cs
@@ -0,0 +1,23 @@
+﻿var words = new List<string> { "1", "2", "prev", "prev", "prev" };
+Console.WriteLine(LastVisitedIntegers(words));
+
+IList<int> LastVisitedIntegers(IList<string> words)
+{
+    var nums = new List<int>();
+    var res = new List<int>();
+    var count = 0;
+    foreach (var word in words)
+    {
+        if (word != "prev")
+        {
+            nums.Add(int.Parse(word));
+            count = 0;
+        }
+        else
+        {
+            count++;
+            res.Add(count > nums.Count ? -1 : nums[^count]);
+        }
+    }
+    return res;
+}
