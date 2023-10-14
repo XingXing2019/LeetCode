8e5a39bf9b8c8e52097d8befe01e7f806a1b14ca
Xing <x.xing.unsw@gmail.com>
2023-10-15 09:22:25 +1100

    init

---
+++ Array/LeetCode 2900 - LongestUnequalAdjacentGroupsSubsequenceI/LongestUnequalAdjacentGroupsSubsequenceI/LongestUnequalAdjacentGroupsSubsequenceI/Program.cs
@@ -0,0 +1,15 @@
+﻿var n = 1;
+string[] words = { "c" };
+int[] groups = { 0 };
+Console.WriteLine(GetWordsInLongestSubsequence(n, words, groups));
+
+IList<string> GetWordsInLongestSubsequence(int n, string[] words, int[] groups)
+{
+    var res = new List<string> { words[0] };
+    for (int i = 1; i < words.Length; i++)
+    {
+        if (groups[i] != groups[i - 1])
+            res.Add(words[i]);
+    }
+    return res;
+}
