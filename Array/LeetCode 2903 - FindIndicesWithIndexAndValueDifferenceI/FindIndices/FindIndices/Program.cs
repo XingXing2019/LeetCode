8475e633ffa384bb44589239242f32ec529a339b
Xing <x.xing.unsw@gmail.com>
2023-10-15 20:05:47 +1100

    init

---
+++ Array/LeetCode 2903 - FindIndicesWithIndexAndValueDifferenceI/FindIndices/FindIndices/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i; j < nums.Length; j++)
+        {
+            if (Math.Abs(i - j) >= indexDifference && Math.Abs(nums[i] - nums[j]) >= valueDifference)
+                return new[] { i, j };
+        }
+    }
+    return new[] { -1, -1 };
+}
