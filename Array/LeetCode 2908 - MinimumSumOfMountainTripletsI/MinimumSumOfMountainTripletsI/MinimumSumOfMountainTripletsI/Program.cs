0007b0ff41747c25854f0bf9a3bd2f3229dac449
Xing <x.xing.unsw@gmail.com>
2023-10-22 21:30:13 +1100

    init

--- Array/LeetCode 2908 - MinimumSumOfMountainTripletsI/MinimumSumOfMountainTripletsI/MinimumSumOfMountainTripletsI/Program.cs
+++ Array/LeetCode 2908 - MinimumSumOfMountainTripletsI/MinimumSumOfMountainTripletsI/MinimumSumOfMountainTripletsI/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MinimumSum(int[] nums)
+{
+    var res = int.MaxValue;
+    for (int i = 1; i < nums.Length - 1; i++)
+    {
+        int leftMin = int.MaxValue, rightMin = int.MaxValue;
+        for (int j = 0; j < i; j++)
+            leftMin = Math.Min(leftMin, nums[j]);
+        for (int j = i + 1; j < nums.Length; j++)
+            rightMin = Math.Min(rightMin, nums[j]);
+        if (leftMin >= nums[i] || rightMin >= nums[i])
+            continue;
+        res = Math.Min(res, leftMin + nums[i] + rightMin);
+    }
+    return res == int.MaxValue ? -1 : res;
+}
--- HashTable/LeetCode 2910 - MinimumNumberOfGroupsToCreateAValidAssignment/MinimumNumberOfGroups/MinimumNumberOfGroups/Program.cs
+++ HashTable/LeetCode 2910 - MinimumNumberOfGroupsToCreateAValidAssignment/MinimumNumberOfGroups/MinimumNumberOfGroups/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] nums = { 1, 1, 1, 1, 1 };
+Console.WriteLine(MinGroupsForValidAssignment(nums));
+int MinGroupsForValidAssignment(int[] nums)
+{
+    var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    int minFreq = freq.Values.Min(), res = int.MaxValue;
+    for (int i = 1; i <= minFreq; i++)
+    {
+        var groups = GetGroups(freq.Values.ToList(), i);
+        if (groups == -1) continue;
+        res = Math.Min(res, groups);
+    }
+    return res;
+}
+
+int GetGroups(List<int> freqs, int x)
+{
+    var res = 0;
+    foreach (var freq in freqs)
+    {
+        int mod = freq % (x + 1), count = freq / (x + 1);
+        if (mod == 0)
+            res += count;
+        else if (x <= mod + count)
+            res += count + 1;
+        else
+            return -1;
+    }
+    return res;
+}
+
