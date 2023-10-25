ecc4123778955601b25b4cfcd6f8238081d4fcf7
Xing <x.xing.unsw@gmail.com>
2023-10-25 19:49:10 +1100

    jav

--- Recursion/LeetCode 779 - KthSymbolInGrammar/KthSymbolInGrammar_Java/src/com/company/Main.java
+++ Recursion/LeetCode 779 - KthSymbolInGrammar/KthSymbolInGrammar_Java/src/com/company/Main.java
@@ -9,8 +9,6 @@ public class Main {
     public int kthGrammar(int n, int k) {
         if (n == 1 && k == 1)
             return 0;
-        if (k % 2 == 0)
-            return kthGrammar(n - 1, k / 2) == 1 ? 0 : 1;
-        return kthGrammar(n - 1, (k + 1) / 2) == 0 ? 0 : 1;
+        return k % 2 == 0 ? kthGrammar(n - 1, k / 2) ^ 1 : kthGrammar(n - 1, (k + 1) / 2);
     }
 }
