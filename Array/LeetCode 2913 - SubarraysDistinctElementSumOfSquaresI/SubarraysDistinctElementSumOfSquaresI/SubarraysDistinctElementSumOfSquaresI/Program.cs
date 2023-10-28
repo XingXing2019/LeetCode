bbd227337d9459a095259730d1cfa0a77dd7d89a
Xing <x.xing.unsw@gmail.com>
2023-10-29 10:09:33 +1100

    init

---
+++ Array/LeetCode 2913 - SubarraysDistinctElementSumOfSquaresI/SubarraysDistinctElementSumOfSquaresI/SubarraysDistinctElementSumOfSquaresI/Program.cs
@@ -0,0 +1,34 @@
+﻿var nums = new List<int> { 1, 2, 1 };
+Console.WriteLine(SumCounts(nums));
+
+int SumCounts(IList<int> nums)
+{
+    var res = 0;
+    for (int i = 1; i < nums.Count; i++)
+        res += GetSquare(nums, i);
+    return res;
+}
+
+int GetSquare(IList<int> nums, int len)
+{
+    var freq = new Dictionary<int, int>();
+    int index = 0, res = 0;
+    while (index < len)
+    {
+        if (!freq.ContainsKey(nums[index]))
+            freq[nums[index]] = 0;
+        freq[nums[index++]]++;
+    }
+    while (index < nums.Count)
+    {
+        res += freq.Count * freq.Count;
+        if (!freq.ContainsKey(nums[index]))
+            freq[nums[index]] = 0;
+        freq[nums[index]]++;
+        freq[nums[index - len]]--;
+        if (freq[nums[index - len]] == 0)
+            freq.Remove(nums[index - len]);
+        index++;
+    }
+    return res + freq.Count * freq.Count;
+}
