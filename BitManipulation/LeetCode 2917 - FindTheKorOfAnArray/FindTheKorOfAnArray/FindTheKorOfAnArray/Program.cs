709dba2b74170d51e5697f2d9f9ee3affdf36bc4
Xing <x.xing.unsw@gmail.com>
2023-10-29 20:23:21 +1100

    init

---
+++ BitManipulation/LeetCode 2917 - FindTheKorOfAnArray/FindTheKorOfAnArray/FindTheKorOfAnArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int FindKOr(int[] nums, int k)
+{
+    int cur = 1, res = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        var count;
+        foreach (var num in nums)
+        {
+            if ((cur & num) != cur) continue;
+            count++;
+        }
+        if (count >= k)
+            res += cur;
+        cur <<= 1;
+    }
+    return res;
+}
