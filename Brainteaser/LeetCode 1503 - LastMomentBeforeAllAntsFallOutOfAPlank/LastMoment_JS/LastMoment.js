087d9f9f9b74875d3e9ec182100bd6a5008add47
Xing <x.xing.unsw@gmail.com>
2023-11-04 11:12:22 +1100

    js

---
+++ Brainteaser/LeetCode 1503 - LastMomentBeforeAllAntsFallOutOfAPlank/LastMoment_JS/LastMoment.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @param {number[]} left
+ * @param {number[]} right
+ * @return {number}
+ */
+var getLastMoment = function(n, left, right) {
+    return Math.max(Math.max(...left), n - Math.min(...right))
+}
