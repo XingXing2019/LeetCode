60a85352c9b1312e576913f0259cdbcef1566e63
Xing <x.xing.unsw@gmail.com>
2023-11-12 13:09:19 +1100

    init

---
+++ Math/LeetCode 2928 - DistributeCandiesAmongChildrenI/DistributeCandiesAmongChildrenI/DistributeCandiesAmongChildrenI/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 5, limit = 2;
+Console.WriteLine(DistributeCandies(n, limit));
+
+int DistributeCandies(int n, int limit)
+{
+    var res = 0;
+    for (int i = 0; i <= Math.Min(n, limit); i++)
+    {
+        for (int j = 0; j <= Math.Min(n - i, limit); j++)
+        {
+            for (int k = 0; k <= Math.Min(n - i - j, limit); k++)
+            {
+                if (i + j + k != n) continue;
+                res++;
+            }
+        }
+    }
+    return res;
+}
