b1ce63c57c5b9ea2fd75ed829e9cdf39ebea602c
Xing <x.xing.unsw@gmail.com>
2023-11-12 14:00:00 +1100

    init

--- Math/LeetCode 2929 - DistributeCandiesAmongChildrenII/DistributeCandiesAmongChildrenII/DistributeCandiesAmongChildrenII/Program.cs
+++ Math/LeetCode 2929 - DistributeCandiesAmongChildrenII/DistributeCandiesAmongChildrenII/DistributeCandiesAmongChildrenII/Program.cs
@@ -1,7 +1,7 @@
 ﻿long DistributeCandies(int n, int limit)
 {
     long res = 0;
-    for (int i = 0; i < n; i++)
-        res += Math.Max(Math.Min(limit, n - i) - Math.Max(0, n - i - limit), 0);
+    for (int i = 0; i <= Math.Min(n, limit); i++)
+        res += Math.Max(Math.Min(limit, n - i) - Math.Max(0, n - i - limit) + 1, 0);
     return res;
 }
---
+++ Math/LeetCode 2930 - NumberOfStringsWhichCanBeRearrangedToContainSubstring/NumberOfStrings/NumberOfStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿var n = 4;
+Console.WriteLine(StringCount(n));
+
+int StringCount(int n)
+{
+    long mod = 1_000_000_000 + 7;
+    var res = Pow(26, n);
+    res -= Pow(25, n) * 3;
+    res -= Pow(25, n - 1) * n;
+    res += Pow(24, n) * 3;
+    res += Pow(24, n - 1) * n * 2;
+    res -= Pow(23, n);
+    res -= Pow(23, n - 1) * n;
+    res %= mod;
+    if (res < 0) res += mod;
+    return (int)res;
+}
+
+long Pow(long num, int n)
+{
+    long mod = 1_000_000_000 + 7;
+    if (n == 0) return 1;
+    var pow = Pow(num, n / 2) * Pow(num, n / 2) % mod;
+    return n % 2 == 0 ? pow : pow * num % mod;
+}
