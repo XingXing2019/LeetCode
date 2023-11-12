273c0a3a9794f0a80f1ca55de65e6ece1636117c
Xing <x.xing.unsw@gmail.com>
2023-11-12 15:20:30 +1100

    init

---
+++ BitManipulation/LeetCode 2932 - MaximumStrongPairXORI/MaximumStrongPairXORI/MaximumStrongPairXORI/Program.cs
@@ -0,0 +1,13 @@
+﻿int MaximumStrongPairXor(int[] nums)
+{
+    var res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (Math.Abs(nums[i] - nums[j]) > Math.Min(nums[i], nums[j])) continue;
+            res = Math.Max(res, nums[i] ^ nums[j]);
+        }
+    }
+    return res;
+}
