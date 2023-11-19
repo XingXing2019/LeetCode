5de94da19e4e740f8b6e76aad17fc3a9e315abbc
Xing <x.xing.unsw@gmail.com>
2023-11-19 16:07:39 +1100

    init

---
+++ TwoPointers/LeetCode 2938 - SeparateBlackAndWhiteBalls/SeparateBlackAndWhiteBalls/SeparateBlackAndWhiteBalls/Program.cs
@@ -0,0 +1,19 @@
+﻿long MinimumSteps(string s)
+{
+    int li = 0, hi = s.Length - 1;
+    long res = 0;
+    while (li < hi)
+    {
+        if (s[li] != '1')
+            li++;
+        else if (s[hi] != '0')
+            hi--;
+        else
+        {
+            res += hi - li;
+            li++;
+            hi--;
+        }
+    }
+    return res;
+}
