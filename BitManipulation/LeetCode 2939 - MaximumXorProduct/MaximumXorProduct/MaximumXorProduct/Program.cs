d967189b45c6333be97b5008029ad59a98f54cb5
Xing <x.xing.unsw@gmail.com>
2023-11-19 16:57:01 +1100

    init

---
+++ BitManipulation/LeetCode 2939 - MaximumXorProduct/MaximumXorProduct/MaximumXorProduct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+long a = 1, b = 6;
+var n = 3;
+Console.WriteLine(MaximumXorProduct(a, b, n));
+
+int MaximumXorProduct(long a, long b, int n)
+{
+    long x = 0, mask = 1L << (n - 1), mod = 1_000_000_000 + 7;
+    for (int i = 0; i < n; i++)
+    {
+        if ((a & mask) == (b & mask) && (a & mask) == 0)
+            x |= mask;
+        else if ((a & mask) != (b & mask))
+        {
+            long valA = a ^ x, valB = b ^ x;
+            if (valA < valB && (valA & mask) == 0 || valA >= valB && (valB & mask) == 0)
+                x |= mask;
+        }
+        mask >>= 1;
+    }
+    return (int)((a ^ x) % mod * ((b ^ x) % mod) % mod);
+}
