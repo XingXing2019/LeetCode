59860ce44b74bd26e7f2c2340a5a5311b154e92b
Xing <x.xing.unsw@gmail.com>
2023-11-26 21:25:11 +1100

    init

---
+++ Array/LeetCode 2946 - MatrixSimilarityAfterCyclicShifts/MatrixSimilarityAfterCyclicShifts/MatrixSimilarityAfterCyclicShifts/Program.cs
@@ -0,0 +1,23 @@
+﻿int[][] mat = new[]
+{
+    new[] { 1, 2, 1, 2 },
+    new[] { 5, 5, 5, 5 },
+    new[] { 6, 3, 6, 3 },
+};
+var k = 2;
+Console.WriteLine(AreSimilar(mat, k));
+
+bool AreSimilar(int[][] mat, int k)
+{
+    k %= mat[0].Length;
+    for (int i = 0; i < mat.Length; i++)
+    {
+        var temp = mat[i][k..].Concat(mat[i][..k]).ToArray();
+        for (int j = 0; j < mat[i].Length; j++)
+        {
+            if (mat[i][j] == temp[j]) continue;
+            return false;
+        }
+    }
+    return true;
+}
---
+++ String/LeetCode 2947 - CountBeautifulSubstringsI/CountBeautifulSubstringsI/CountBeautifulSubstringsI/Program.cs
@@ -0,0 +1,22 @@
+﻿int BeautifulSubstrings(string s, int k)
+{
+    var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+    var counts = new int[s.Length];
+    var res = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        var vowel = vowels.Contains(s[i]) ? 1 : 0;
+        counts[i] = i == 0 ? 0 + vowel : counts[i - 1] + vowel;
+    }
+    for (int i = 0; i < s.Length; i++)
+    {
+        for (int j = i + 1; j < s.Length; j++)
+        {
+            var vowel = i == 0 ? counts[j] : counts[j] - counts[i - 1];
+            var consonant = j - i + 1 - vowel;
+            if (vowel != consonant || vowel * consonant % k != 0) continue;
+            res++;
+        }
+    }
+    return res;
+}
