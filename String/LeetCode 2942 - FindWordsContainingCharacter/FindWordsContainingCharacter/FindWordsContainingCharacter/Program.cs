688bbb775c61d11cea567d1a25f3eae4dcc4abeb
Xing <x.xing.unsw@gmail.com>
2023-11-26 20:41:13 +1100

    init

---
+++ String/LeetCode 2942 - FindWordsContainingCharacter/FindWordsContainingCharacter/FindWordsContainingCharacter/Program.cs
@@ -0,0 +1,10 @@
+﻿IList<int> FindWordsContaining(string[] words, char x)
+{
+    var res = new List<int>();
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (!words[i].Contains(x)) continue;
+        res.Add(i);
+    }
+    return res;
+}
