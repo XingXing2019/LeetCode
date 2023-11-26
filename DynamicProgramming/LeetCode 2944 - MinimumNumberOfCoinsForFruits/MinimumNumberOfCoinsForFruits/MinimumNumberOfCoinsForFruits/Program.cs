a2d7e8720c647e24f7964f4733b46e86c8919815
Xing <x.xing.unsw@gmail.com>
2023-11-26 21:11:33 +1100

    init

---
+++ DynamicProgramming/LeetCode 2944 - MinimumNumberOfCoinsForFruits/MinimumNumberOfCoinsForFruits/MinimumNumberOfCoinsForFruits/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] prices = { 2, 1, 6, 1 };
+Console.WriteLine(MinimumCoins(prices));
+
+int MinimumCoins(int[] prices)
+{
+    var dp = new int[prices.Length][];
+    dp[0] = new[] { prices[0], prices[0] };
+    for (int i = 1; i < prices.Length; i++)
+    {
+        dp[i] = new int[2];
+        var min = int.MaxValue;
+        for (int j = i - 1; j + j + 1 >= i && j >= 0; j--)
+            min = Math.Min(min, dp[j][0]);
+        dp[i][0] = Math.Min(dp[i - 1][0], dp[i - 1][1]) + prices[i];
+        dp[i][1] = min;
+    }
+    return Math.Min(dp[^1][0], dp[^1][1]);
+}
