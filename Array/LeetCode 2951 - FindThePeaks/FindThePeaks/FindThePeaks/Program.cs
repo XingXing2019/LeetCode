9ce9f4ffd3d402db6ba2e4aae820411a820a4919
Xing <x.xing.unsw@gmail.com>
2023-12-03 20:26:36 +1100

    init

---
+++ Array/LeetCode 2951 - FindThePeaks/FindThePeaks/FindThePeaks/Program.cs
@@ -0,0 +1,10 @@
+﻿IList<int> FindPeaks(int[] mountain)
+{
+    var res = new List<int>();
+    for (int i = 1; i < mountain.Length - 1; i++)
+    {
+        if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1])
+            res.Add(i);
+    }
+    return res;
+}
---
+++ Math/LeetCode 2952 - MinimumNumberOfCoinsToBeAdded/MinimumNumberOfCoinsToBeAdded/MinimumNumberOfCoinsToBeAdded/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] coins = { 1, 1, 1 };
+var target = 20;
+Console.WriteLine(MinimumAddedCoins(coins, target));
+
+int MinimumAddedCoins(int[] coins, int target)
+{
+    Array.Sort(coins);
+    int res = 0, maxReach = 0, index = 0;
+    while (index < coins.Length)
+    {
+        if (maxReach + 1 >= coins[index])
+        {
+            maxReach += coins[index];
+            index++;
+        }
+        else
+        {
+            res++;
+            maxReach = maxReach * 2 + 1;
+        }
+    }
+    while (maxReach < target)
+    {
+        maxReach = maxReach * 2 + 1;
+        res++;
+    }
+    return res;
+}
