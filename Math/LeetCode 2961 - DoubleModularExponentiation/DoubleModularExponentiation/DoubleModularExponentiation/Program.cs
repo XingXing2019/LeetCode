12d13cc3bab241acbdb8e2f4d5af68f3acfa5225
Xing <x.xing.unsw@gmail.com>
2023-12-10 21:00:31 +1100

    init

---
+++ Math/LeetCode 2961 - DoubleModularExponentiation/DoubleModularExponentiation/DoubleModularExponentiation/Program.cs
@@ -0,0 +1,21 @@
+﻿IList<int> GetGoodIndices(int[][] variables, int target)
+{
+    var res = new List<int>();
+    for (int i = 0; i < variables.Length; i++)
+    {
+        int a = variables[i][0], b = variables[i][1], c = variables[i][2], m = variables[i][3];
+        var pow1 = Pow(a, b, 10);
+        var pow2 = Pow(pow1, c, m);
+        if (pow2 == target)
+            res.Add(i);
+    }
+    return res;
+}
+
+int Pow(int num1, int num2, int mod)
+{
+    if (num2 == 0)
+        return 1;
+    var pow = Pow(num1, num2 / 2, mod) % mod;
+    return num2 % 2 == 0 ? pow * pow % mod : pow * pow * num1 % mod;
+}
