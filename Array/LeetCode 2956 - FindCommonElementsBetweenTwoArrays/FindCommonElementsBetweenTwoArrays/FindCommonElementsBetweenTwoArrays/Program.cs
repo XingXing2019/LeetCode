471e89284d10e2d3131613e525d7cf078e51ef59
Xing <x.xing.unsw@gmail.com>
2023-12-10 20:46:46 +1100

    init

--- Array/LeetCode 2956 - FindCommonElementsBetweenTwoArrays/FindCommonElementsBetweenTwoArrays/FindCommonElementsBetweenTwoArrays/Program.cs
+++ Array/LeetCode 2956 - FindCommonElementsBetweenTwoArrays/FindCommonElementsBetweenTwoArrays/FindCommonElementsBetweenTwoArrays/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] FindIntersectionValues(int[] nums1, int[] nums2)
+{
+    var set1 = new HashSet<int>(nums1);
+    var set2 = new HashSet<int>(nums2);
+    var res = new int[2];
+    res[0] = nums1.Count(x => set2.Contains(x));
+    res[1] = nums2.Count(x => set1.Contains(x));
+    return res;
+}
---
+++ Greedy/LeetCode 2957 - RemoveAdjacentAlmostEqualCharacters/RemoveAdjacentAlmostEqualCharacters/RemoveAdjacentAlmostEqualCharacters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+var word = "zyxyxyz";
+Console.WriteLine(RemoveAlmostEqualCharacters(word));
+
+int RemoveAlmostEqualCharacters(string word)
+{
+    var res = 0;
+    for (int i = 1; i < word.Length; i++)
+    {
+        if (Math.Abs(word[i] - word[i - 1]) > 1) 
+            continue;
+        i++;
+        res++;
+    }
+    return res;
+}
