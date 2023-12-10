7a0c5f06185ae1c93f6d5c84bc9da20cc571561e
Xing <x.xing.unsw@gmail.com>
2023-12-10 20:55:06 +1100

    init

---
+++ Array/LeetCode 2960 - CountTestedDevicesAfterTestOperations/CountTestedDevicesAfterTestOperations/CountTestedDevicesAfterTestOperations/Program.cs
@@ -0,0 +1,11 @@
+﻿int CountTestedDevices(int[] batteryPercentages)
+{
+    var res = 0;
+    foreach (var battery in batteryPercentages)
+    {
+        if (battery - res <= 0) 
+            continue;
+        res++;
+    }
+    return res;
+}
