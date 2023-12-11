6bf0a60be4c834f2ee177d07f5051026d3d493a9
Xing <x.xing.unsw@gmail.com>
2023-12-11 20:28:25 +1100

    init

---
+++ HashTable/LeetCode 2962 - CountSubarraysWhereMaxElementAppearsAtLeastKTimes/CountSubarrays/CountSubarrays/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] nums = { 1, 3, 2, 3, 3, 2, 3 };
+var k = 2;
+Console.WriteLine(CountSubarrays(nums, k));
+
+long CountSubarrays(int[] nums, int k)
+{
+    int max = nums.Max(), count = 0, last = 0;
+    var freq = new Dictionary<int, int>();
+    long res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] != max) 
+            res += last;
+        else
+        {
+            count++;
+            freq[count] = i;
+            if (!freq.ContainsKey(count - k + 1)) 
+                continue;
+            res += freq[count - k + 1] + 1;
+            last = freq[count - k + 1] + 1;
+        }
+    }
+    return res;
+}
