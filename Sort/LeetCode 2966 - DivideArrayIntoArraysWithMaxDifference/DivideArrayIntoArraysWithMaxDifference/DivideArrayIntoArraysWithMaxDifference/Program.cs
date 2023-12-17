f5a71b380db54fc20b77dab5bd1d956a09280ff4
Xing <x.xing.unsw@gmail.com>
2023-12-17 20:46:01 +1100

    init

--- Sort/LeetCode 2966 - DivideArrayIntoArraysWithMaxDifference/DivideArrayIntoArraysWithMaxDifference/DivideArrayIntoArraysWithMaxDifference/Program.cs
+++ Sort/LeetCode 2966 - DivideArrayIntoArraysWithMaxDifference/DivideArrayIntoArraysWithMaxDifference/DivideArrayIntoArraysWithMaxDifference/Program.cs
@@ -5,7 +5,7 @@
     for (int i = 0; i <= nums.Length - 3; i += 3)
     {
         if (nums[i + 2] - nums[i] > k)
-            return new int[0][];
+            return Array.Empty<int[]>();
         res.Add(new[] { nums[i], nums[i + 1], nums[i + 2] });
     }
     return res.ToArray();
---
+++ Sort/LeetCode 2967 - MinimumCostToMakeArrayEqualindromic/MinimumCostToMakeArrayEqualindromic/MinimumCostToMakeArrayEqualindromic/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] nums = { 1, 2, 3, 4, 5 };
+Console.WriteLine(MinimumCost(nums));
+
+long MinimumCost(int[] nums)
+{
+    Array.Sort(nums);
+    var median = nums[nums.Length / 2];
+    var palindromic1 = GetPalindromic(median, 1);
+    var palindromic2 = GetPalindromic(median, -1);
+    var sum1 = nums.Sum(x => Math.Abs((long)x - palindromic1));
+    var sum2 = nums.Sum(x => Math.Abs((long)x - palindromic2));
+    return Math.Min(sum1, sum2);
+}
+
+bool IsPalindromic(int num)
+{
+    var str = num.ToString();
+    int li = 0, hi = str.Length - 1;
+    while (li < hi)
+    {
+        if (str[li] != str[hi])
+            return false;
+        li++;
+        hi--;
+    }
+    return true;
+}
+
+int GetPalindromic(int num, int diff)
+{
+    while (!IsPalindromic(num))
+        num += diff;
+    return num;
+}
