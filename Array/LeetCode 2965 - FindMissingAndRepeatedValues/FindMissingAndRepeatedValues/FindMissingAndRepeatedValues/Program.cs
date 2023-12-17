661a174a66b390e3b2e3c13e525446b3476160d6
Xing <x.xing.unsw@gmail.com>
2023-12-17 19:33:15 +1100

    init

---
+++ Array/LeetCode 2965 - FindMissingAndRepeatedValues/FindMissingAndRepeatedValues/FindMissingAndRepeatedValues/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] FindMissingAndRepeatedValues(int[][] grid)
+{
+    var n = grid.Length;
+    var nums = new int[n * n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            nums[grid[i][j]]++;
+        }
+    }
+    var res = new int[2];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] == 2)
+            res[0] = i;
+        else if (nums[i] == 0)
+            res[1] = i;
+    }
+    return res;
+}
