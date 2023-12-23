1274d158de62fcc3006932fe40f0ee740320929e
Xing <x.xing.unsw@gmail.com>
2023-12-24 09:18:11 +1100

    init

--- Greedy/LeetCode 2971 - FindPolygonWithTheLargestPerimeter/FindPolygonWithTheLargestPerimeter/FindPolygonWithTheLargestPerimeter/Program.cs
+++ Greedy/LeetCode 2971 - FindPolygonWithTheLargestPerimeter/FindPolygonWithTheLargestPerimeter/FindPolygonWithTheLargestPerimeter/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿long LargestPerimeter(int[] nums)
+{
+    Array.Sort(nums);
+    var prefix = new long[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+        prefix[i] = i == 0 ? nums[i] : nums[i] + prefix[i - 1];
+    for (int i = nums.Length - 1; i >= 2; i--)
+    {
+        if (nums[i] >= prefix[i - 1]) continue;
+        return prefix[i];
+    }
+    return -1;
+}
