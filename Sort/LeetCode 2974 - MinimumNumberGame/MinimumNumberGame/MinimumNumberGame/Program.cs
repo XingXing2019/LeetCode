763f596d28d9313375060970bd0de1972ec9ae4d
Xing <x.xing.unsw@gmail.com>
2023-12-25 08:48:50 +1100

    init

---
+++ Sort/LeetCode 2974 - MinimumNumberGame/MinimumNumberGame/MinimumNumberGame/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums = { 2, 7, 9, 6, 4, 6 };
+Console.WriteLine(NumberGame(nums));
+
+int[] NumberGame(int[] nums)
+{
+    Array.Sort(nums);
+    var res = new int[nums.Length];
+    int index = 0, p1 = 0, p2 = 1;
+    while (p2 < nums.Length)
+    {
+        res[index++] = nums[p2];
+        res[index++] = nums[p1];
+        p1 += 2;
+        p2 += 2;
+    }
+    return res;
+}
