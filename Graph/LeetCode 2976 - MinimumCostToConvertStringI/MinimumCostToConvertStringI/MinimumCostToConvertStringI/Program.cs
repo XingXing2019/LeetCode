4a288d89ce2d94a10fbb208e02450ba5e48922c2
Xing <x.xing.unsw@gmail.com>
2023-12-26 20:43:34 +1100

    init

---
+++ Graph/LeetCode 2976 - MinimumCostToConvertStringI/MinimumCostToConvertStringI/MinimumCostToConvertStringI/Program.cs
@@ -0,0 +1,50 @@
+﻿string source = "aaaa", target = "bbbb";
+char[] original = { 'a', 'c' };
+char[] changed = { 'c', 'b' };
+int[] cost = { 1, 2 };
+
+Console.WriteLine(MinimumCost(source, target, original, changed, cost));
+
+long MinimumCost(string source, string target, char[] original, char[] changed, int[] cost)
+{
+    var graph = new List<int[]>[26];
+    for (int i = 0; i < 26; i++)
+        graph[i] = new List<int[]>();
+    for (int i = 0; i < original.Length; i++)
+        graph[original[i] - 'a'].Add(new[] { changed[i] - 'a', cost[i] });
+    var dp = new long[26][];
+    for (int i = 0; i < dp.Length; i++)
+    {
+        dp[i] = new long[26];
+        Array.Fill(dp[i], long.MaxValue);
+        dp[i][i] = 0;
+    }
+    long res = 0;
+    for (int i = 0; i < source.Length; i++)
+    {
+        var temp = BFS(source[i] - 'a', target[i] - 'a', graph, dp);
+        if (temp == -1)
+            return -1;
+        res += temp;
+    }
+    return res;
+}
+
+long BFS(int source, int target, List<int[]>[] graph, long[][] dp)
+{
+    var queue = new Queue<int[]>();
+    queue.Enqueue(new[] { source, 0 });
+    while (queue.Count != 0)
+    {
+        var cur = queue.Dequeue();
+        foreach (var next in graph[cur[0]])
+        {
+            if (dp[source][next[0]] > cur[1] + next[1])
+            {
+                dp[source][next[0]] = cur[1] + next[1];
+                queue.Enqueue(new[] { next[0], cur[1] + next[1] });
+            }
+        }
+    }
+    return dp[source][target] == long.MaxValue ? -1 : dp[source][target];
+}
