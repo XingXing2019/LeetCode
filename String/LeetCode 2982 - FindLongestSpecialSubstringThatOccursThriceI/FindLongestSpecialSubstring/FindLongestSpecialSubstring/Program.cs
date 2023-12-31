7a0963c67766c582b9a1dfac3b6a44dfff7dfba1
Xing <x.xing.unsw@gmail.com>
2023-12-31 21:15:31 +1100

    init

---
+++ String/LeetCode 2982 - FindLongestSpecialSubstringThatOccursThriceI/FindLongestSpecialSubstring/FindLongestSpecialSubstring/Program.cs
@@ -0,0 +1,15 @@
+﻿int MaximumLength(string s)
+{
+    var dict = new Dictionary<string, int>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        for (int j = i; j < s.Length; j++)
+        {
+            var str = s.Substring(i, j - i + 1);
+            if (!dict.ContainsKey(str))
+                dict[str] = 0;
+            dict[str]++;
+        }
+    }
+    return dict.Where(x => x.Value >= 3).Max(x => x.Key.Length);
+}
