f2cfb0797f931fe196b352c4a47ff6f1afbec161
Xing <x.xing.unsw@gmail.com>
2024-01-01 16:47:07 +1100

    init

---
+++ Database/LeetCode 2984 - FindPeakCallingHoursForEachCity/FindPeakCallingHoursForEachCity_SQLServer.sql
@@ -0,0 +1,11 @@
+WITH PeakCalls AS (
+	SELECT city, DATEPART(HOUR, call_time) AS peak_calling_hour, COUNT(*) AS number_of_calls FROM Calls
+	GROUP BY city, DATEPART(HOUR, call_time)
+) 
+
+SELECT city, peak_calling_hour, number_of_calls FROM (
+	SELECT city, peak_calling_hour, number_of_calls, RANK() OVER(PARTITION BY city ORDER BY number_of_calls DESC) AS call_rank
+	FROM PeakCalls
+) AS t
+WHERE call_rank = 1
+ORDER BY peak_calling_hour desc, city desc
