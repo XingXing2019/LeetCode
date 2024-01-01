95a347903a646e4222fd1fd68a56a1c12e5335e4
Xing <x.xing.unsw@gmail.com>
2024-01-01 17:35:08 +1100

    init

---
+++ Database/LeetCode 2989 - ClassPerformance/ClassPerformance_MySQL.sql
@@ -0,0 +1,2 @@
+select max(assignment1 + assignment2 + assignment3) - min(assignment1 + assignment2 + assignment3) as difference_in_score
+from Scores
---
+++ Database/LeetCode 2989 - ClassPerformance/ClassPerformance_SQLServer.sql
@@ -0,0 +1,2 @@
+SELECT MAX(assignment1 + assignment2 + assignment3) - MIN(assignment1 + assignment2 + assignment3) AS difference_in_score
+FROM Scores
