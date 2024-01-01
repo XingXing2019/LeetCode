71f95a2373cc2f12da086bccc1cbdb3b203c41f1
Xing <x.xing.unsw@gmail.com>
2024-01-01 17:15:34 +1100

    init

---
+++ Database/LeetCode 2986 - FindThirdTransaction/FindThirdTransaction_MySQL.sql
@@ -0,0 +1,12 @@
+with DateRank as (
+	select *, rank() over(partition by user_id order by transaction_date) as date_rank
+    from Transactions
+)
+
+select d3.user_id, d3.spend as third_transaction_spend, d3.transaction_date as third_transaction_date
+from DateRank d1
+join DateRank d2 on d1.user_id = d2.user_id and d1.date_rank + 1 = d2.date_rank
+join DateRank d3 on d2.user_id = d3.user_id and d2.date_rank + 1 = d3.date_rank
+where d1.spend < d3.spend 
+and d2.spend < d3.spend 
+and d3.date_rank = 3
--- Database/LeetCode 2986 - FindThirdTransaction/FindThirdTransaction_SQLServer.sql
+++ Database/LeetCode 2986 - FindThirdTransaction/FindThirdTransaction_SQLServer.sql
@@ -7,5 +7,6 @@ SELECT d3.user_id, d3.spend AS third_transaction_spend, d3.transaction_date AS t
 FROM DateRank d1
 JOIN DateRank d2 ON d1.user_id = d2.user_id AND d1.date_rank + 1 = d2.date_rank
 JOIN DateRank d3 ON d2.user_id = d3.user_id AND d2.date_rank + 1 = d3.date_rank
-WHERE d1.spend < d3.spend AND d2.spend < d3.spend
+WHERE d1.spend < d3.spend 
+AND d2.spend < d3.spend
 AND d3.date_rank = 3
