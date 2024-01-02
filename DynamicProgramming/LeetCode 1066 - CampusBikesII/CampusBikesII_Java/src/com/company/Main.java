646e704dff74ab18fc84afea8a6b7d1d56d7460b
Xing <52732037+XingXing2019@users.noreply.github.com>
2024-01-02 18:37:36 +1100

    Update Main.java

--- DynamicProgramming/LeetCode 1066 - CampusBikesII/CampusBikesII_Java/src/com/company/Main.java
+++ DynamicProgramming/LeetCode 1066 - CampusBikesII/CampusBikesII_Java/src/com/company/Main.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	    int[][] workers = {{0, 0}, {2, 1}};
-	    int[][] bikes = {{1, 2}, {3, 3}};
+	int[][] workers = {{0, 0}, {2, 1}};
+	int[][] bikes = {{1, 2}, {3, 3}};
         System.out.println(assignBikes(workers, bikes));
     }
 
