f069a66071884cef07d77e429674b7bcc8bd5775
Xing <x.xing.unsw@gmail.com>
2024-01-04 17:32:01 +1100

    init

---
+++ Backtracking/LeetCode 2992 - NumberOfSelfDivisiblePermutations/NumberOfSelfDivisiblePermutations/NumberOfSelfDivisiblePermutations/Program.cs
@@ -0,0 +1,22 @@
+﻿var n = 3;
+Console.WriteLine(SelfDivisiblePermutationCount(n));
+
+int SelfDivisiblePermutationCount(int n)
+{
+    var res = 0;
+    BackTracking(new HashSet<int>(), n, ref res);
+    return res;
+}
+
+void BackTracking(HashSet<int> visited, int n, ref int res)
+{
+    if (visited.Count == n)
+        res++;
+    for (int i = 1; i <= n; i++)
+    {
+        if (visited.Contains(i) || (i % (visited.Count + 1) != 0 && (visited.Count + 1) % i != 0)) continue;
+        visited.Add(i);
+        BackTracking(visited, n, ref res);
+        visited.Remove(i);
+    }
+}
