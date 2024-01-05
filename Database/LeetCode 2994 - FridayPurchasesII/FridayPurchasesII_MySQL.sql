03c031bb2f791ff47f2ca031d0f82b29a28b0ef8
Xing <52732037+XingXing2019@users.noreply.github.com>
2024-01-05 17:15:27 +1100

    Update FridayPurchasesII_MySQL.sql

--- Database/LeetCode 2994 - FridayPurchasesII/FridayPurchasesII_MySQL.sql
+++ Database/LeetCode 2994 - FridayPurchasesII/FridayPurchasesII_MySQL.sql
@@ -1,17 +1,17 @@
 with Fridays as (
 	select 1 as week_of_month, '2023-11-03' as friday
-    union
-    select 2 as week_of_month, '2023-11-10' as friday
-    union
-    select 3 as week_of_month, '2023-11-17' as friday
-    union
-    select 4 as week_of_month, '2023-11-24' as friday
+    	union
+    	select 2 as week_of_month, '2023-11-10' as friday
+    	union
+    	select 3 as week_of_month, '2023-11-17' as friday
+   	union
+    	select 4 as week_of_month, '2023-11-24' as friday
 )
 
 select week_of_month, friday as purchase_date, ifnull(total_amount, 0) as total_amount from Fridays
 left join (
 	select purchase_date, sum(amount_spend) as total_amount
-    from Purchases
-    where purchase_date in (select friday from Fridays)
-    group by purchase_date
+    	from Purchases
+   	where purchase_date in (select friday from Fridays)
+    	group by purchase_date
 ) as t on friday = t.purchase_date
