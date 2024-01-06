5935f64697df769d34495c7ef9dbae9c52b1f652
Xing <x.xing.unsw@gmail.com>
2024-01-07 09:32:19 +1100

    init

---
+++ BitManipulation/LeetCode 2997 - MinimumNumberOfOperationsToMakeArrayXOREqualToK/MinimumNumberOfOperations/MinimumNumberOfOperations/Program.cs
@@ -0,0 +1,10 @@
+﻿int MinOperations(int[] nums, int k)
+{
+    var res = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        var ones = nums.Count(x => ((x >> i) & 1) == 1);
+        res += ((k >> i) & 1) == 1 && ones % 2 == 0 || ((k >> i) & 1) == 0 && ones % 2 != 0 ? 1 : 0;
+    }
+    return res;
+}
--- HashTable/LeetCode 2996 - SmallestMissingIntegerGreaterThanSequentialPrefixSum/SmallestMissingInteger/SmallestMissingInteger/Program.cs
+++ HashTable/LeetCode 2996 - SmallestMissingIntegerGreaterThanSequentialPrefixSum/SmallestMissingInteger/SmallestMissingInteger/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int MissingInteger(int[] nums)
+{
+    var set = new HashSet<int>(nums);
+    var sum = nums[0];
+    for (int i = 1; i < nums.Length && nums[i] == nums[i - 1] + 1; i++)
+        sum += nums[i];
+    for (int i = sum; i < sum + 50; i++)
+    {
+        if (set.Contains(i)) continue;
+        return i;
+    }
+    return -1;
+}
