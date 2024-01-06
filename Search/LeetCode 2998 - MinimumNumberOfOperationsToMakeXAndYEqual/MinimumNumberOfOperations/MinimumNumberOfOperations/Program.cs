27c8ce00867310c4d755980ed5c425c5bc764a2d
Xing <x.xing.unsw@gmail.com>
2024-01-07 09:38:10 +1100

    init

---
+++ Search/LeetCode 2998 - MinimumNumberOfOperationsToMakeXAndYEqual/MinimumNumberOfOperations/MinimumNumberOfOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿int MinimumOperationsToMakeEqual(int x, int y)
+{
+    var queue = new Queue<int>();
+    queue.Enqueue(x);
+    var visited = new HashSet<int> { x };
+    var res = 0;
+    while (queue.Count != 0)
+    {
+        var count = queue.Count;
+        for (int i = 0; i < count; i++)
+        {
+            var cur = queue.Dequeue();
+            if (cur == y)
+                return res;
+            if (cur % 11 == 0 && visited.Add(cur / 11))
+                queue.Enqueue(cur / 11);
+            if (cur % 5 == 0 && visited.Add(cur / 5))
+                queue.Enqueue(cur / 5);
+            if (visited.Add(cur - 1))
+                queue.Enqueue(cur - 1);
+            if (visited.Add(cur + 1))
+                queue.Enqueue(cur + 1);
+        }
+        res++;
+    }
+    return res;
+}
