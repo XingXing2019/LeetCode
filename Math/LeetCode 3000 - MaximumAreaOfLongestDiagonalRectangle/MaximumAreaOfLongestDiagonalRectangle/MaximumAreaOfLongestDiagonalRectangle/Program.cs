7db7b0f84f3d41272c142bf9ae662c58b3412156
Xing <x.xing.unsw@gmail.com>
2024-01-07 19:03:16 +1100

    init

--- Math/LeetCode 3000 - MaximumAreaOfLongestDiagonalRectangle/MaximumAreaOfLongestDiagonalRectangle/MaximumAreaOfLongestDiagonalRectangle/Program.cs
+++ Math/LeetCode 3000 - MaximumAreaOfLongestDiagonalRectangle/MaximumAreaOfLongestDiagonalRectangle/MaximumAreaOfLongestDiagonalRectangle/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int AreaOfMaxDiagonal(int[][] dimensions)
+{
+    dimensions = dimensions.OrderByDescending(x => Math.Sqrt((double)x[0] * x[0] + (double)x[1] * x[1])).ThenByDescending(x => x[0] * x[1]).ToArray();
+    return dimensions[0][0] * dimensions[0][1];
+}
---
+++ Math/LeetCode 3001 - MinimumMovesToCaptureTheQueen/MinimumMovesToCaptureTheQueen/MinimumMovesToCaptureTheQueen/Program.cs
@@ -0,0 +1,28 @@
+﻿var a = 4;
+var b = 5;
+var c = 7;
+var d = 8;
+var e = 2;
+var f = 3;
+Console.WriteLine(MinMovesToCaptureTheQueen(a, b, c, d, e, f));
+
+int MinMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f)
+{
+    if (a == e && b < f && (a != c || d < b || d > f))
+        return 1;
+    if (a == e && b > f && (a != c || d < f || d > b))
+        return 1;
+    if (b == f && a < e && (b != d || c < a || c > e))
+        return 1;
+    if (b == f && a > e && (b != d || c < e || c > a))
+        return 1;
+    if (e - c == f - d && e > c && (a - c != b - d || a < c || a > e))
+        return 1;                        
+    if (c - e == d - f && e < c && (a - e != b - f || a > e || a < c))
+        return 1;                        
+    if (c - e == f - d && e < c && (a - e != f - b || a < c || a > e))
+        return 1;                        
+    if (e - c == d - f && e > c && (e - a != b - f || a > e || a < c))
+        return 1;
+    return 2;
+}
