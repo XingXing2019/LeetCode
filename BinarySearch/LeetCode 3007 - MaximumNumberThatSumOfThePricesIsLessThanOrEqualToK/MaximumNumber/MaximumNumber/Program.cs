e0b0fd08205f1a0cbdc2b62b691ac552e8769225
Xing <x.xing.unsw@gmail.com>
2024-01-14 21:22:50 +1100

    init

---
+++ BinarySearch/LeetCode 3007 - MaximumNumberThatSumOfThePricesIsLessThanOrEqualToK/MaximumNumber/MaximumNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿long k = 9;
+var x = 1;
+Console.WriteLine(FindMaximumNumber(k, x));
+long FindMaximumNumber(long k, int x)
+{
+    long li = 1, hi = (long)1e15;
+    while (li <= hi)
+    {
+        var mid = li + (hi - li) / 2;
+        if (GetPrice(mid + 1, x) <= k)
+            li = mid + 1;
+        else
+            hi = mid - 1;
+    }
+    return hi;
+}
+
+long GetPrice(long num, int x)
+{
+    long count = 0;
+    for (int b = x - 1; b < 60; b += x)
+    {
+        var fullCycles = num / (1L << (b + 1));
+        count += fullCycles * (1L << b);
+        var remaining = num % (1L << (b + 1));
+        count += Math.Max(0, remaining - (1L << b));
+    }
+    return count;
+}
