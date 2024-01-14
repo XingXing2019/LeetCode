6e13ae4bb17e8f7318ac371089c5f62149507034
Xing <x.xing.unsw@gmail.com>
2024-01-14 20:49:13 +1100

    init

---
+++ Array/LeetCode 3005 - CountElementsWithMaximumFrequency/CountElementsWithMaximumFrequency/CountElementsWithMaximumFrequency/Program.cs
@@ -0,0 +1,13 @@
+﻿int MaxFrequencyElements(int[] nums)
+{
+    var freq = new Dictionary<int, int>();
+    var max = 0;
+    foreach (var num in nums)
+    {
+        if (!freq.ContainsKey(num))
+            freq[num] = 0;
+        freq[num]++;
+        max = Math.Max(max, freq[num]);
+    }
+    return freq.Count(x => x.Value == max) * max;
+}
---
+++ BinarySearch/LeetCode 3006 - FindBeautifulIndicesInTheGivenArrayI/FindBeautifulIndices/FindBeautifulIndices/Program.cs
@@ -0,0 +1,25 @@
+﻿IList<int> BeautifulIndices(string s, string a, string b, int k)
+{
+    var indices1 = FindIndices(s, a);
+    var indices2 = FindIndices(s, b);
+    var res = new List<int>();
+    foreach (var index in indices1)
+    {
+        var li = indices2.BinarySearch(index - k);
+        var hi = indices2.BinarySearch(index + k);
+        if (li >= 0 || hi >= 0 || ~li != ~hi)
+            res.Add(index);
+    }
+    return res;
+}
+
+List<int> FindIndices(string s, string word)
+{
+    var res = new List<int>();
+    for (int i = 0; i <= s.Length - word.Length; i++)
+    {
+        if (s.Substring(i, word.Length) != word) continue;
+        res.Add(i);
+    }
+    return res;
+}
