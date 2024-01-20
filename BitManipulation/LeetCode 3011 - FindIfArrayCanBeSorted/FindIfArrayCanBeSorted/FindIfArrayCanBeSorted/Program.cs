277a28224be40d365c5acae6b3a3d25b4ed24d6f
Xing <x.xing.unsw@gmail.com>
2024-01-21 07:35:57 +1100

    init

--- BitManipulation/LeetCode 3011 - FindIfArrayCanBeSorted/FindIfArrayCanBeSorted/FindIfArrayCanBeSorted/Program.cs
+++ BitManipulation/LeetCode 3011 - FindIfArrayCanBeSorted/FindIfArrayCanBeSorted/FindIfArrayCanBeSorted/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] nums = { 8, 4, 2, 30, 15 };
+Console.WriteLine(CanSortArray(nums));
+
+bool CanSortArray(int[] nums)
+{
+    int min = nums[0], max = nums[0];
+    var count = Count(nums[0]);
+    var limits = new List<int[]>();
+    foreach (var num in nums)
+    {
+        var tempCount = Count(num);
+        if (tempCount != count)
+        {
+            limits.Add(new[] { min, max });
+            count = tempCount;
+            min = max = num;
+        }
+        min = Math.Min(min, num);
+        max = Math.Max(max, num);
+    }
+    limits.Add(new[] { min, max });
+    for (int i = 1; i < limits.Count; i++)
+    {
+        if (limits[i - 1][1] > limits[i][0])
+            return false;
+    }
+    return true;
+}
+
+int Count(int num)
+{
+    var res = 0;
+    while (num != 0)
+    {
+        res += num % 2;
+        num /= 2;
+    }
+    return res;
+}
