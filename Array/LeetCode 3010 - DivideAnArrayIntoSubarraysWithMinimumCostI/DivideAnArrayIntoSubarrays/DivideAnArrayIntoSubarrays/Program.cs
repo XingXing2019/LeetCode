6ff7251e695440b3a6398fe861636c50bd179c11
Xing <x.xing.unsw@gmail.com>
2024-01-21 07:26:34 +1100

    init

---
+++ Array/LeetCode 3010 - DivideAnArrayIntoSubarraysWithMinimumCostI/DivideAnArrayIntoSubarrays/DivideAnArrayIntoSubarrays/Program.cs
@@ -0,0 +1,13 @@
+﻿int MinimumCost(int[] nums)
+{
+    int min = int.MaxValue, sum = int.MaxValue;
+    var rightMin = new int[nums.Length];
+    for (int i = nums.Length - 1; i >= 0; i--)
+    {
+        rightMin[i] = min;
+        min = Math.Min(min, nums[i]);
+    }
+    for (int i = 1; i < nums.Length - 1; i++)
+        sum = Math.Min(sum, nums[i] + rightMin[i]);
+    return nums[0] + sum;
+}
---
+++ HashTable/LeetCode 3012 - MinimizeLengthOfArrayUsingOperations/MinimizeLengthOfArrayUsingOperations/MinimizeLengthOfArrayUsingOperations/Program.cs
@@ -0,0 +1,13 @@
+﻿int MinimumArrayLength(int[] nums)
+{
+    var freq = new Dictionary<int, int>();
+    var min = int.MaxValue;
+    foreach (var num in nums)
+    {
+        if (!freq.ContainsKey(num))
+            freq[num] = 0;
+        freq[num]++;
+        min = Math.Min(min, num);
+    }
+    return (int)Math.Ceiling((double)freq[min] / 2);
+}
