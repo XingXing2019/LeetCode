4039f813314fc1d57110a6a764e94c0516c72eaf
Xing <x.xing.unsw@gmail.com>
2024-01-21 21:47:00 +1100

    init

---
+++ Graph/LeetCode 3015 - CountTheNumberOfHousesAtACertainDistanceI/CountTheNumberOfHouses/CountTheNumberOfHouses/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] CountOfPairs(int n, int x, int y)
+{
+    var res = new int[n];
+    var graph = new List<int>[n];
+    for (int i = 0; i < graph.Length; i++)
+        graph[i] = new List<int>();
+    for (int i = 0; i < n - 1; i++)
+        graph[i].Add(i + 1);
+    if (x < y)
+        graph[x - 1].Add(y - 1);
+    else
+        graph[y - 1].Add(x - 1);
+    for (int i = 0; i < n - 1; i++)
+        BFS(graph, i, res);
+    return res;
+}
+
+void BFS(List<int>[] graph, int start, int[] res)
+{
+    var queue = new Queue<int>();
+    queue.Enqueue(start);
+    var visited = new HashSet<int> { start };
+    var level = 0;
+    while (queue.Count != 0)
+    {
+        var count = queue.Count;
+        for (int i = 0; i < count; i++)
+        {
+            var cur = queue.Dequeue();
+            foreach (var next in graph[cur])
+            {
+                if (!visited.Add(next)) continue;
+                queue.Enqueue(next);
+                res[level] += 2;
+            }
+        }
+        level++;
+    }
+}
--- Greedy/LeetCode 3016 - MinimumNumberOfPushesToTypeWordII/MinimumNumberOfPushes/MinimumNumberOfPushes/Program.cs
+++ Greedy/LeetCode 3016 - MinimumNumberOfPushesToTypeWordII/MinimumNumberOfPushes/MinimumNumberOfPushes/Program.cs
@@ -1,4 +1,14 @@
 ﻿int MinimumPushes(string word)
 {
-
+    var freq = word.GroupBy(x => x).OrderByDescending(x => x.Count()).ToDictionary(x => x.Key, x => x.Count());
+    int res = 0, times = 1, count = 0;
+    foreach (var letter in freq.Keys)
+    {
+        res += times * freq[letter];
+        count++;
+        if (count != 8) continue;
+        times++;
+        count = 0;
+    }
+    return res;
 }
