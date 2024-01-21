5097e7f237de81529762185a22cd918d3ba85069
Xing <x.xing.unsw@gmail.com>
2024-01-21 20:40:00 +1100

    init

---
+++ Greedy/LeetCode 3014 - MinimumNumberOfPushesToTypeWordI/MinimumNumberOfPushes/MinimumNumberOfPushes/Program.cs
@@ -0,0 +1,11 @@
+﻿int MinimumPushes(string word)
+{
+    int len = word.Length, res = 0, times = 1;
+    while (len > 0)
+    {
+        res += len > 8 ? 8 * times : len * times;
+        len -= 8;
+        times++;
+    }
+    return res;
+}
