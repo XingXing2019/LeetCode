3bed5aa0f70973aa702d18ca1fe627b2a77f5052
Xing <x.xing.unsw@gmail.com>
2024-01-28 20:53:42 +1100

    init

---
+++ Math/LeetCode 3021 - AliceAndBobPlayingFlowerGame/AliceAndBobPlayingFlowerGame/AliceAndBobPlayingFlowerGame/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = 3, m = 99;
+Console.WriteLine(FlowerGame(n, m));
+
+long FlowerGame(int n, int m)
+{
+    long oddN = n % 2 == 0 ? n / 2 : n / 2 + 1, evenN = n / 2;
+    long oddM = m % 2 == 0 ? m / 2 : m / 2 + 1, evenM = m / 2;
+    return oddN * evenM + evenN * oddM;
+}
