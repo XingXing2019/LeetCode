a5dddc90b80773af81b3a4f560cbc7050251e400
Xing <x.xing.unsw@gmail.com>
2024-02-01 20:11:19 +1100

    init

---
+++ String/LeetCode 3023 - FindPatternInInfiniteStreamI/FindPatternInInfiniteStreamI/FindPatternInInfiniteStreamI/Program.cs
@@ -0,0 +1,20 @@
+﻿int FindPattern(InfiniteStream stream, int[] pattern)
+{
+    var target = string.Join("", pattern);
+    var cur = "";
+    while (cur.Length < target.Length)
+        cur += stream.Next();
+    var index = 0;
+    while (cur != target)
+    {
+        cur = cur.Substring(1) + stream.Next();
+        index++;
+    }
+    return index;
+}
+
+class InfiniteStream
+{
+    public InfiniteStream(int[] bits) {};
+    public int Next() => 0;
+}
