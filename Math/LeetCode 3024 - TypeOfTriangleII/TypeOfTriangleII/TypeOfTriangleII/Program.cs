7baaff0bab14f3ac9767dfc066a7d41be1fc948d
Xing <x.xing.unsw@gmail.com>
2024-02-04 08:44:21 +1100

    init

---
+++ Math/LeetCode 3024 - TypeOfTriangleII/TypeOfTriangleII/TypeOfTriangleII/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] nums = { 9, 4, 9 };
+Console.WriteLine(TriangleType(nums));
+
+string TriangleType(int[] nums)
+{
+    Array.Sort(nums);
+    if (nums[0] + nums[1] <= nums[2])
+        return "none";
+    if (nums.All(x => x == nums[0]))
+        return "equilateral";
+    if (nums[0] == nums[1] || nums[1] == nums[2])
+        return "isosceles";
+    return "scalene";
+}
