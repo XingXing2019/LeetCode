33c9806006df99ae0819f830644be8a384e46d8a
Xing <x.xing.unsw@gmail.com>
2024-02-04 20:05:39 +1100

    init

---
+++ HashTable/LeetCode 3028 - AntOnTheBoundary/AntOnTheBoundary/AntOnTheBoundary/Program.cs
@@ -0,0 +1,11 @@
+﻿int ReturnToBoundaryCount(int[] nums)
+{
+    int res = 0, cur = 0;
+    foreach (var num in nums)
+    {
+        cur += num;
+        if (cur != 0) continue;
+        res++;
+    }
+    return res;
+}
