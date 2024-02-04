e61df021bb221b17858eca197f9f8e8ff20b5ed1
Xing <x.xing.unsw@gmail.com>
2024-02-04 20:23:20 +1100

    init

---
+++ String/LeetCode 3029 - MinimumTimeToRevertWordToInitialStateI/MinimumTimeToRevertWordToInitialStateI/MinimumTimeToRevertWordToInitialStateI/Program.cs
@@ -0,0 +1,17 @@
+﻿var word = "abacabc";
+var k = 3;
+Console.WriteLine(MinimumTimeToInitialState(word, k));
+
+int MinimumTimeToInitialState(string word, int k)
+{
+    var res = 0;
+    var cur = word;
+    while (cur.Length >= k)
+    {
+        cur = cur.Substring(k);
+        res++;
+        if (word.StartsWith(cur))
+            return res;
+    }
+    return res + 1;
+}
