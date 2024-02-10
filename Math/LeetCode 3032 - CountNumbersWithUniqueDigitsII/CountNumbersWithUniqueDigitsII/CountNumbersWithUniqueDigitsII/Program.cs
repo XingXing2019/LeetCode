145b57364780fa1f5d9ffd04ce9d6379116e74cf
Xing <x.xing.unsw@gmail.com>
2024-02-11 06:37:30 +1100

    init

---
+++ Math/LeetCode 3032 - CountNumbersWithUniqueDigitsII/CountNumbersWithUniqueDigitsII/CountNumbersWithUniqueDigitsII/Program.cs
@@ -0,0 +1,22 @@
+﻿int NumberCount(int a, int b)
+{
+    var res = 0;
+    for (int i = a; i <= b; i++)
+    {
+        if (!IsUniqueDigit(i)) continue;
+        res++;
+    }
+    return res;
+}
+
+bool IsUniqueDigit(int n)
+{
+    var unique = new HashSet<int>();
+    while (n != 0)
+    {
+        if (!unique.Add(n % 10))
+            return false;
+        n /= 10;
+    }
+    return true;
+}
