bbfb74c7008d68156f28eb7421c8f9c263a69e7f
Xing <x.xing.unsw@gmail.com>
2024-02-18 09:05:26 +1100

    init

---
+++ Array/LeetCode 3038 - MaximumNumberOfOperationsWithTheSameScoreI/MaximumNumberOfOperations/MaximumNumberOfOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿int MaxOperations(int[] nums)
+{
+    int res = 1, sum = nums[0] + nums[1];
+    for (int i = 3; i < nums.Length; i+=2)
+    {
+        if (nums[i] + nums[i - 1] == sum)
+            res++;
+        else
+            return res;
+    }
+    return res;
+}
