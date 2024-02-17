35b69a467fb2ee2302e0bc682fd3353782fa82e5
Xing <x.xing.unsw@gmail.com>
2024-02-18 09:12:10 +1100

    init

---
+++ String/LeetCode 3039 - ApplyOperationsToMakeStringEmpty/ApplyOperationsToMakeStringEmpty/ApplyOperationsToMakeStringEmpty/Program.cs
@@ -0,0 +1,33 @@
+﻿var s = "aabcbbca";
+Console.WriteLine(LastNonEmptyString(s));
+
+string LastNonEmptyString(string s)
+{
+    var dict = new Dictionary<char, Queue<int>>();
+    var max = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (!dict.ContainsKey(s[i]))
+            dict[s[i]] = new Queue<int>();
+        dict[s[i]].Enqueue(i);
+        max = Math.Max(max, dict[s[i]].Count);
+    }
+    for (int i = 0; i < max - 1; i++)
+    {
+        foreach (var l in dict.Keys)
+        {
+            if (dict[l].Count == 0)
+                dict.Remove(l);
+            else
+                dict[l].Dequeue();
+        }
+    }
+    var indexes = new List<int>();
+    foreach (var l in dict.Keys)
+        indexes.AddRange(dict[l]);
+    indexes.Sort();
+    var res = "";
+    foreach (var index in indexes)
+        res += s[index];
+    return res;
+}
