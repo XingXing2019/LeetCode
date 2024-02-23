724137e416ca4da821404b8b8fea65a7f66d55a6
Xing <x.xing.unsw@gmail.com>
2024-02-23 15:29:00 +1100

    init

---
+++ Trie/LeetCode 3043 - FindTheLengthOfTheLongestCommonPrefix/FindTheLengthOfTheLongestCommonPrefix/FindTheLengthOfTheLongestCommonPrefix/Program.cs
@@ -0,0 +1,41 @@
+﻿int LongestCommonPrefix(int[] arr1, int[] arr2)
+{
+    var root = new TrieNode();
+    foreach (var num in arr1)
+    {
+        var point = root;
+        foreach (var d in num.ToString())
+        {
+            if (point.children[d - '0'] == null)
+                point.children[d - '0'] = new TrieNode();
+            point = point.children[d - '0'];
+        }
+    }
+
+    var res = 0;
+    foreach (var num in arr2)
+    {
+        var point = root;
+        var len = 0;
+        foreach (var d in num.ToString())
+        {
+            if (point.children[d - '0'] == null)
+                break;
+            len++;
+            point = point.children[d - '0'];
+        }
+        res = Math.Max(res, len);
+    }
+    return res;
+}
+
+class TrieNode
+{
+    public int val;
+    public TrieNode[] children;
+
+    public TrieNode()
+    {
+        children = new TrieNode[10];
+    }
+}
