2317d0ed48165e3d3906c5477dc260fb703b168f
Xing <x.xing.unsw@gmail.com>
2024-02-23 15:15:41 +1100

    init

---
+++ String/LeetCode 3042 - CountPrefixAndSuffixPairsI/CountPrefixAndSuffixPairsI/CountPrefixAndSuffixPairsI/Program.cs
@@ -0,0 +1,13 @@
+﻿int CountPrefixSuffixPairs(string[] words)
+{
+    var res = 0;
+    for (int i = 0; i < words.Length; i++)
+    {
+        for (int j = i + 1; j < words.Length; j++)
+        {
+            if (words[j].StartsWith(words[i]) && words[j].EndsWith(words[i]))
+                res++;
+        }
+    }
+    return res;
+}
