5527fb205508ada30beacd5d1276771348741a3f
Xing <x.xing.unsw@gmail.com>
2024-02-29 20:33:40 +1100

    init

---
+++ Database/LeetCode 3061 - CalculateTrappingRainWater/CalculateTrappingRainWater_MySQL.sql
@@ -0,0 +1,15 @@
+with LeftMax as (
+	select id, max(height) over(order by id) as left_max from Heights
+),
+
+RightMax as (
+	select id, max(height) over(order by id desc) as right_max from Heights
+),
+
+MinHeight as (
+	select l.id, if(left_max < right_max, left_max, right_max) as min_height
+    from LeftMax l join RightMax r on l.id = r.id
+)
+
+select sum(if(min_height > height, min_height - height, 0)) as total_trapped_water
+from MinHeight m join Heights h on m.id = h.id
