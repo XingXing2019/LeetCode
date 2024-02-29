6fc24163e3e43ddbd5bd0c797c88815f02ad8079
Xing <x.xing.unsw@gmail.com>
2024-02-29 20:58:19 +1100

    init

---
+++ BitManipulation/LeetCode 3064 - GuessTheNumberUsingBitwiseQuestionsI/GuessTheNumberUsingBitwiseQuestionsI/GuessTheNumberUsingBitwiseQuestionsI/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+class Problem
+{
+    public int CommonSetBits(int num) => 0;
+}
+
+class Solution : Problem
+{
+    public int FindNumber()
+    {
+        var res = 0;
+        for (int i = 0; i < 30; i++)
+        {
+            if (CommonSetBits(1 << i) != 1) continue;
+            res += 1 << i;
+        }
+        return res;
+    }
+}
