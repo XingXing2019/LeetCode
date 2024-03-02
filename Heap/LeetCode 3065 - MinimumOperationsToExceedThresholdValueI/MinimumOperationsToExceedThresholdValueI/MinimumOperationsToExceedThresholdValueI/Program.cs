e77f76cad0403a549bf8581f3bb2cd7bceaec121
Xing <x.xing.unsw@gmail.com>
2024-03-03 08:58:58 +1100

    init

---
+++ Heap/LeetCode 3065 - MinimumOperationsToExceedThresholdValueI/MinimumOperationsToExceedThresholdValueI/MinimumOperationsToExceedThresholdValueI/Program.cs
@@ -0,0 +1,13 @@
+﻿int MinOperations(int[] nums, int k)
+{
+    var res = 0;
+    var heap = new PriorityQueue<int, int>();
+    foreach (var num in nums)
+        heap.Enqueue(num, num);
+    while (heap.Peek() < k)
+    {
+        heap.Dequeue();
+        res++;
+    }
+    return res;
+}
