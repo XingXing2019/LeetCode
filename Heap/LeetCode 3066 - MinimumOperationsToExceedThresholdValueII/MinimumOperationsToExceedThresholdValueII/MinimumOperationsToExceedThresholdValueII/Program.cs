a487c9b7aab3a74caf4dd57698e652b8f2cb77b9
Xing <x.xing.unsw@gmail.com>
2024-03-03 09:04:01 +1100

    init

---
+++ Heap/LeetCode 3066 - MinimumOperationsToExceedThresholdValueII/MinimumOperationsToExceedThresholdValueII/MinimumOperationsToExceedThresholdValueII/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+int MinOperations(int[] nums, int k)
+{
+    var res = 0;
+    var heap = new PriorityQueue<long, long>();
+    foreach (var num in nums)
+        heap.Enqueue(num, num);
+    while (heap.Count != 0 && heap.Peek() < k)
+    {
+        long num1 = heap.Dequeue(), num2 = heap.Dequeue();
+        var num = Math.Min(num1, num2) * 2 + Math.Max(num1, num2);
+        heap.Enqueue(num, num);
+        res++;
+    }
+    return res;
+}
