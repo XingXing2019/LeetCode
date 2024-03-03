0464c974addc889fa6c66b28078ff7d48950b516
Xing <x.xing.unsw@gmail.com>
2024-03-03 19:59:23 +1100

    init

---
+++ Array/LeetCode 3069 - DistributeElementsIntoTwoArraysI/DistributeElementsIntoTwoArraysI/DistributeElementsIntoTwoArraysI/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] ResultArray(int[] nums)
+{
+    var arr1 = new List<int> { nums[0] };
+    var arr2 = new List<int> { nums[1] };
+    for (int i = 2; i < nums.Length; i++)
+    {
+        if (arr1[^1] > arr2[^1])
+            arr1.Add(nums[i]);
+        else
+            arr2.Add(nums[i]);
+    }
+    return arr1.Concat(arr2).ToArray();
+}
