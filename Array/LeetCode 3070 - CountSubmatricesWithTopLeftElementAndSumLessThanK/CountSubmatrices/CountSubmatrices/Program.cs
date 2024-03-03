95d4832c014ea7b72ac01c12ca2287bc61f3e73e
Xing <x.xing.unsw@gmail.com>
2024-03-03 20:24:47 +1100

    init

---
+++ Array/LeetCode 3070 - CountSubmatricesWithTopLeftElementAndSumLessThanK/CountSubmatrices/CountSubmatrices/Program.cs
@@ -0,0 +1,30 @@
+﻿int[][] grid = new int[][]
+{
+    new[] { 7, 2, 9 },
+    new[] { 1, 5, 0 },
+    new[] { 2, 6, 6 },
+};
+var k = 20;
+
+Console.WriteLine(CountSubmatrices(grid, k));
+int CountSubmatrices(int[][] grid, int k)
+{
+    var res = 0;
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[0].Length; j++)
+        {
+            grid[i][j] += j == 0 ? 0 : grid[i][j - 1];
+        }
+    }
+    for (int j = 0; j < grid[0].Length; j++)
+    {
+        for (int i = 0; i < grid.Length; i++)
+        {
+            grid[i][j] += i == 0 ? 0 : grid[i - 1][j];
+            if (grid[i][j] > k) continue;
+            res++;
+        }
+    }
+    return res;
+}
