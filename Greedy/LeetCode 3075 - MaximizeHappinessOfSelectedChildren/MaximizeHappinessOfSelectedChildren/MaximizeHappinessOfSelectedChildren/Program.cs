689efcc4444203df84565122ad6231d0d3bb8136
Xing <x.xing.unsw@gmail.com>
2024-03-10 19:36:44 +1100

    init

--- Greedy/LeetCode 3075 - MaximizeHappinessOfSelectedChildren/MaximizeHappinessOfSelectedChildren/MaximizeHappinessOfSelectedChildren/Program.cs
+++ Greedy/LeetCode 3075 - MaximizeHappinessOfSelectedChildren/MaximizeHappinessOfSelectedChildren/MaximizeHappinessOfSelectedChildren/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿long MaximumHappinessSum(int[] happiness, int k)
+{
+    var decrease = 0;
+    Array.Sort(happiness, (a, b) => b - a);
+    long res = 0;
+    for (int i = 0; i < k; i++)
+    {
+        res += Math.Max(0, happiness[i] -  decrease);
+        decrease++;
+    }
+    return res;
+}
