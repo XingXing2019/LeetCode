cf929f0ebd39c31d73ba3c8ed2f3b730edbd25c9
Xing <x.xing.unsw@gmail.com>
2024-03-10 19:32:36 +1100

    init

---
+++ Sort/LeetCode 3074 - AppleRedistributionIntoBoxes/AppleRedistributionIntoBoxes/AppleRedistributionIntoBoxes/Program.cs
@@ -0,0 +1,14 @@
+﻿int MinimumBoxes(int[] apple, int[] capacity)
+{
+    var sum = apple.Sum();
+    Array.Sort(capacity, (a, b) => b - a);
+    var res = 0;
+    foreach (var c in capacity)
+    {
+        sum -= c;
+        res++;
+        if (sum <= 0)
+            return res;
+    }
+    return -1;
+}
