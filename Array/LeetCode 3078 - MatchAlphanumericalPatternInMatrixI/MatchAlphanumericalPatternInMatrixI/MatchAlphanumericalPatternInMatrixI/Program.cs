040a66330c702fee056b06372bef693ce35588c9
Xing <x.xing.unsw@gmail.com>
2024-03-14 20:55:24 +1100

    init

---
+++ Array/LeetCode 3078 - MatchAlphanumericalPatternInMatrixI/MatchAlphanumericalPatternInMatrixI/MatchAlphanumericalPatternInMatrixI/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] FindPattern(int[][] board, string[] pattern)
+{
+    for (int r = 0; r < board.Length; r++)
+    {
+        for (int c = 0; c < board[0].Length; c++)
+        {
+            if (!IsMatch(board, pattern, r, c)) continue;
+            return new[] { r, c };
+        }
+    }
+    return new[] { -1, -1 };
+}
+
+bool IsMatch(int[][] board, string[] pattern, int r, int c)
+{
+    if (board.Length - r < pattern.Length || board[0].Length - c < pattern[0].Length)
+        return false;
+    var map = new Dictionary<char, int>();
+    var seen = new HashSet<int>();
+    for (int i = 0; i < pattern.Length; i++)
+    {
+        for (int j = 0; j < pattern[0].Length; j++)
+        {
+            var digit = board[r + i][c + j];
+            var letter = pattern[i][j];
+            if (char.IsDigit(letter) && digit != letter - '0')
+                return false;
+            if (!char.IsDigit(letter))
+            {
+                if (map.ContainsKey(letter) && map[letter] != digit || !map.ContainsKey(letter) && !seen.Add(digit))
+                    return false;
+                map[pattern[i][j]] = digit;
+            }
+        }
+    }
+    return true;
+}
--- HashTable/LeetCode 3076 - ShortestUncommonSubstringInnArray/ShortestUncommonSubstringInnArray/ShortestUncommonSubstringInnArray/Program.cs
+++ HashTable/LeetCode 3076 - ShortestUncommonSubstringInnArray/ShortestUncommonSubstringInnArray/ShortestUncommonSubstringInnArray/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ShortestSubstrings(string[] arr)
+{
+    var substrings = new HashSet<string>[arr.Length];
+    var res = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+        substrings[i] = GetSubstrings(arr[i]);
+    for (int i = 0; i < substrings.Length; i++)
+        res[i] = GetShortestNonOccur(substrings, i);
+    return res;
+}
+
+HashSet<string> GetSubstrings(string word)
+{
+    var res = new List<string>();
+    for (int i = 1; i <= word.Length; i++)
+    {
+        for (int j = 0; j <= word.Length - i; j++)
+        {
+            res.Add(word.Substring(j, i));
+        }
+    }
+    return new HashSet<string>(res.OrderBy(x => x.Length).ThenBy(x => x));
+}
+
+string GetShortestNonOccur(HashSet<string>[] substrings, int index)
+{
+    foreach (var word in substrings[index])
+    {
+        var nonOccur = true;
+        for (int i = 0; i < substrings.Length; i++)
+        {
+            if (i == index) continue;
+            if (substrings[i].Contains(word))
+            {
+                nonOccur = false;
+                break;
+            }
+        }
+        if (nonOccur)
+            return word;
+    }
+    return "";
+} 
