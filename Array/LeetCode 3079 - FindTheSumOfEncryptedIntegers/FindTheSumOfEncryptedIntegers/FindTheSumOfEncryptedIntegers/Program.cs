642ea9850b95f67ae5ace9f4dd5db3d6ca4c4ef7
Xing <x.xing.unsw@gmail.com>
2024-03-17 09:06:29 +1100

    init

---
+++ Array/LeetCode 3079 - FindTheSumOfEncryptedIntegers/FindTheSumOfEncryptedIntegers/FindTheSumOfEncryptedIntegers/Program.cs
@@ -0,0 +1,18 @@
+﻿int SumOfEncryptedInt(int[] nums)
+{
+    return nums.Sum(EncryptedInt);
+}
+
+int EncryptedInt(int num)
+{
+    int max = 0, count = 0, res = 0;
+    while (num != 0)
+    {
+        max = Math.Max(max, num % 10);
+        num /= 10;
+        count++;
+    }
+    for (int i = 0; i < count; i++)
+        res = res * 10 + max;
+    return res;
+}
