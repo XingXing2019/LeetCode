f13be6d73d39af8024c3415bc47010df3f17599b
Xing <x.xing.unsw@gmail.com>
2024-03-17 09:45:30 +1100

    init

---
+++ Heap/LeetCode 3081 - ReplaceQuestionMarksInStringToMinimizeItsValue/ReplaceQuestionMarks/ReplaceQuestionMarks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+string MinimizeStringValue(string s)
+{
+    var heap = new PriorityQueue<char, int>();
+    var freq = new int[26];
+    var res = new StringBuilder(s);
+    int pow = 1_000_00, index = 0, count = 0;
+    var candidates = new List<char>();
+    for (var i = 0; i < s.Length; i++)
+    {
+        if (s[i] == '?')
+            count++;
+        else
+            freq[s[i] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+        heap.Enqueue((char)(i + 'a'), freq[i] * pow + i);
+    for (int i = 0; i < count; i++)
+    {
+        var min = heap.Dequeue();
+        freq[min - 'a']++;
+        heap.Enqueue(min, freq[min - 'a'] * pow + (min - 'a'));
+        candidates.Add(min);
+    }
+    candidates = candidates.OrderBy(x => x).ToList();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] != '?') continue;
+        res[i] = candidates[index++];
+    }
+    return res.ToString();
+}
