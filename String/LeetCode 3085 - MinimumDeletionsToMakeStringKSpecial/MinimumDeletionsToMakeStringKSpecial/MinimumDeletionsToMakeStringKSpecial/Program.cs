94a9f891492364b8eae4227f969e9833c71fa18a
Xing <x.xing.unsw@gmail.com>
2024-03-17 20:28:27 +1100

    init

--- String/LeetCode 3085 - MinimumDeletionsToMakeStringKSpecial/MinimumDeletionsToMakeStringKSpecial/MinimumDeletionsToMakeStringKSpecial/Program.cs
+++ String/LeetCode 3085 - MinimumDeletionsToMakeStringKSpecial/MinimumDeletionsToMakeStringKSpecial/MinimumDeletionsToMakeStringKSpecial/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var word = "aabcaba";
+var k = 0;
+Console.WriteLine(MinimumDeletions(word, k));
+
+int MinimumDeletions(string word, int k)
+{
+    var freq = word.GroupBy(x => x).Select(x => x.Count()).OrderBy(x => x).ToList();
+    var prefix = new int[freq.Count];
+    for (int i = 0; i < freq.Count; i++)
+        prefix[i] = i == 0 ? freq[i] : freq[i] + prefix[i - 1];
+    var res = int.MaxValue;
+    for (int i = 0; i < freq.Count; i++)
+    {
+        var remove = i == 0 ? 0 : prefix[i - 1];
+        for (int j = i + 1; j < freq.Count; j++)
+        {
+            if (freq[j] - freq[i] <= k) continue;
+            remove += freq[j] - freq[i] - k;
+        }
+        res = Math.Min(res, remove);
+    }
+    return res;
+}
