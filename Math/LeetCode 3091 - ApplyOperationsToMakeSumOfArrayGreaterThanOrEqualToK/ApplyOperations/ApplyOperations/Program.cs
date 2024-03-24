46a632b089f0fed4ce9b4eecbbf168d31f61cfc3
Xing <x.xing.unsw@gmail.com>
2024-03-24 16:41:01 +1100

    init

---
+++ Math/LeetCode 3091 - ApplyOperationsToMakeSumOfArrayGreaterThanOrEqualToK/ApplyOperations/ApplyOperations/Program.cs
@@ -0,0 +1,13 @@
+﻿var k = 11;
+Console.WriteLine(MinOperations(k));
+
+int MinOperations(int k)
+{
+    var res = int.MaxValue;
+    for (int i = 0; i <= k; i++)
+    {
+        var ops = (i - 1) + (int)Math.Ceiling((double)k / (1 + i));
+        res = Math.Min(res, ops);
+    }
+    return res;
+}
