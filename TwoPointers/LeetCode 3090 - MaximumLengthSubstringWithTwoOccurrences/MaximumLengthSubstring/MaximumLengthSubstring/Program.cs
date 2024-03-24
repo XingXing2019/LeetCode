119967bc0d11163eaa3535aa67cf1a860414ff8f
Xing <x.xing.unsw@gmail.com>
2024-03-24 16:28:15 +1100

    init

---
+++ TwoPointers/LeetCode 3090 - MaximumLengthSubstringWithTwoOccurrences/MaximumLengthSubstring/MaximumLengthSubstring/Program.cs
@@ -0,0 +1,16 @@
+﻿var s = "bcbbbcba";
+Console.WriteLine(MaximumLengthSubstring(s));
+int MaximumLengthSubstring(string s)
+{
+    var freq = new int[26];
+    int li = 0, hi = 0, res = 0;
+    while (hi < s.Length)
+    {
+        freq[s[hi] - 'a']++;
+        while (li < hi && freq[s[hi] - 'a'] > 2)
+            freq[s[li++] - 'a']--;
+        res = Math.Max(res, hi - li + 1);
+        hi++;
+    }
+    return res;
+}
