56c2498fb4a9a9df73db413abe7b1a8ce62a512a
Xing <x.xing.unsw@gmail.com>
2024-03-28 20:08:07 +1100

    init

--- BitManipulation/LeetCode 3094 - GuessTheNumberUsingBitwiseQuestionsII/GuessTheNumber/GuessTheNumber/Program.cs
+++ BitManipulation/LeetCode 3094 - GuessTheNumberUsingBitwiseQuestionsII/GuessTheNumber/GuessTheNumber/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int FindNumber()
+{
+    int res = 0, start = CommonBits(0);
+    CommonBits(0);
+    for (int i = 0; i < 31; i++)
+    {
+        var count = CommonBits(1 << i);
+        if (count > start)
+            res += 1 << i;
+        CommonBits(1 << i);
+    }
+    return res;
+}
+
+
+int CommonBits(int num)
+{
+    return 0;
+}
