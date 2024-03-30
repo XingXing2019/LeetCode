55b449b6ceaff2b5eb63e9598f4f97cb7bce052c
Xing <x.xing.unsw@gmail.com>
2024-03-31 09:20:14 +1100

    init

---
+++ BitManipulation/LeetCode 3095 - ShortestSubarrayWithORAtLeastKI/ShortestSubarrayWithORAtLeastKI/ShortestSubarrayWithORAtLeastKI/Program.cs
@@ -0,0 +1,18 @@
+﻿int MinimumSubarrayLength(int[] nums, int k)
+{
+    var res = int.MaxValue;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var or = 0;
+        for (int j = i; j < nums.Length; j++)
+        {
+            or |= nums[j];
+            if (or >= k)
+            {
+                res = Math.Min(res, j - i + 1);
+                break;
+            }
+        }
+    }
+    return res == int.MaxValue ? -1 : res;
+}
