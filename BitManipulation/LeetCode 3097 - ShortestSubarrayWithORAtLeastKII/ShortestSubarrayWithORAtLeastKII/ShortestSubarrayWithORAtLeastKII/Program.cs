6e993cadd41b6673a9a4a53c08651125c72def5b
Xing <x.xing.unsw@gmail.com>
2024-03-31 10:03:32 +1100

    init

---
+++ BitManipulation/LeetCode 3097 - ShortestSubarrayWithORAtLeastKII/ShortestSubarrayWithORAtLeastKII/ShortestSubarrayWithORAtLeastKII/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] nums = { 1, 2, 32, 21 };
+var k = 55;
+Console.WriteLine(MinimumSubarrayLength(nums, k));
+
+int MinimumSubarrayLength(int[] nums, int k)
+{
+    int li = 0, hi = 0, res = int.MaxValue;
+    var mask = new int[32];
+    while (hi < nums.Length)
+    {
+        SetMask(mask, nums[hi], true);
+        while (li <= hi && GetMask(mask) >= k)
+        {
+            res = Math.Min(res, hi - li + 1);
+            SetMask(mask, nums[li++], false);
+        }
+        hi++;
+    }
+    return res == int.MaxValue ? -1 : res;
+}
+
+int GetMask(int[] mask)
+{
+    var res = 0;
+    for (int i = 0; i < mask.Length; i++)
+    {
+        if (mask[i] == 0) continue;
+        res += 1 << i;
+    }
+    return res;
+}
+
+void SetMask(int[] mask, int num, bool increase)
+{
+    var index = 0;
+    while (num != 0)
+    {
+        if (num % 2 != 0)
+            mask[index] = increase ? mask[index] + 1 : mask[index] - 1;
+        num /= 2;
+        index++;
+    }
+}
--- Greedy/LeetCode 3096 - MinimumLevelsToGainMorePoints/MinimumLevelsToGainMorePoints/MinimumLevelsToGainMorePoints/Program.cs
+++ Greedy/LeetCode 3096 - MinimumLevelsToGainMorePoints/MinimumLevelsToGainMorePoints/MinimumLevelsToGainMorePoints/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MinimumLevels(int[] possible)
+{
+    int front = 0, back = 0;
+    int[] frontOnes = new int[possible.Length], backOnes = new int[possible.Length];
+    for (int i = 0; i < possible.Length; i++)
+    {
+        front += possible[i] == 1 ? 1 : 0;
+        frontOnes[i] = front;
+        backOnes[^(i + 1)] = back;
+        back += possible[^(i + 1)] == 1 ? 1 : 0;
+    }
+    for (int i = 0; i < possible.Length - 1; i++)
+    {
+        var scoreD = frontOnes[i] - (i + 1 - frontOnes[i]);
+        var scoreB = backOnes[i] - (possible.Length - i - 1 - backOnes[i]);
+        if (scoreD > scoreB)
+            return i + 1;
+    }
+    return -1;
+}
