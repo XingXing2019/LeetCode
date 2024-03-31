6057422715a42fee9a9c00459bd75ca2d86652d5
Xing <x.xing.unsw@gmail.com>
2024-03-31 16:24:49 +1100

    init

---
+++ TwoPointers/LeetCode 3101 - CountAlternatingSubarrays/CountAlternatingSubarrays/CountAlternatingSubarrays/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] nums = { 0, 1, 1, 1 };
+Console.WriteLine(CountAlternatingSubarrays(nums));
+
+long CountAlternatingSubarrays(int[] nums)
+{
+    int li = 0, hi = 0;
+    long res = 0;
+    while (hi < nums.Length)
+    {
+        while (hi < nums.Length && (hi == li || nums[hi] != nums[hi - 1]))
+            hi++;
+        res += (long)(1 + hi - li) * (hi - li) / 2;
+        li = hi;
+    }
+    return res;
+}
