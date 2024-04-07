827e3455f4a9fd7f37723ea43ed736c57a6f2ae9
Xing <x.xing.unsw@gmail.com>
2024-04-07 16:42:00 +1000

    init

---
+++ Sort/LeetCode 3107 - MinimumOperationsToMakeMedianOfArrayEqualToK/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] nums = { 2, 5, 6, 8, 5 };
+var k = 4;
+Console.WriteLine(MinOperationsToMakeMedianK(nums, k));
+
+long MinOperationsToMakeMedianK(int[] nums, int k)
+{
+    Array.Sort(nums);
+    var index = nums.Length / 2;
+    long res = Math.Abs(k - nums[index]);
+    if (nums[index] > k)
+    {
+        for (int i = 0; i < index; i++)
+        {
+            res += Math.Max(0, nums[i] - k);
+        }
+    }
+    else
+    {
+        for (int i = index + 1; i < nums.Length; i++)
+        {
+            res += Math.Max(0, k - nums[i]);
+        }
+    }
+    return res;
+}
