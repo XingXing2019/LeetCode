1387e1d57583476a77aa2470a5296a68488df39d
Xing <x.xing.unsw@gmail.com>
2024-04-07 16:34:59 +1000

    init

---
+++ Greedy/LeetCode 3106 - LexicographicallySmallestStringAfterOperationsWithConstraint/LexicographicallySmallestString/LexicographicallySmallestString/Program.cs
@@ -0,0 +1,36 @@
+﻿var s = "uo";
+var k = 5;
+Console.WriteLine(GetSmallestString(s, k));
+
+string GetSmallestString(string s, int k)
+{
+    var res = "";
+    foreach (var l in s)
+    {
+        if (k == 0)
+            res += l;
+        else
+        {
+            var decrease = Math.Min(l - 'a', k);
+            var decreaseL = (char)(l - decrease);
+            var increase = Math.Min('z' - l + 1, k);
+            var increaseL = l + increase > 'z' ? (char)((l + increase) % 'z' + 'a' - 1) : (char)(l + increase);
+            if (decreaseL < increaseL)
+            {
+                k -= decrease;
+                res += decreaseL;
+            }
+            else if (decreaseL > increaseL)
+            {
+                k -= increase;
+                res += increaseL;
+            }
+            else
+            {
+                k -= Math.Min(increase, decrease);
+                res += increaseL;
+            }
+        }
+    }
+    return res;
+}
