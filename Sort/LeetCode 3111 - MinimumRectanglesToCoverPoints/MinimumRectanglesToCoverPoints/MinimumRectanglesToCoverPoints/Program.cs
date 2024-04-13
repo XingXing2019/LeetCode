c5e85ddcc27e1862dbd9c9b2935b2560f150db99
Xing <x.xing.unsw@gmail.com>
2024-04-14 08:40:13 +1000

    init

---
+++ Sort/LeetCode 3111 - MinimumRectanglesToCoverPoints/MinimumRectanglesToCoverPoints/MinimumRectanglesToCoverPoints/Program.cs
@@ -0,0 +1,14 @@
+﻿int MinRectanglesToCoverPoints(int[][] points, int w)
+{
+    points = points.OrderBy(x => x[0]).ToArray();
+    int res = 1, left = points[0][0];
+    for (int i = 1; i < points.Length; i++)
+    {
+        if (points[i][0] - left > w)
+        {
+            left = points[i][0];
+            res++;
+        }
+    }
+    return res;
+}
