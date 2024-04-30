07ce614d3da146eb5e79b73a0780371ac5b78be4
Xing <x.xing.unsw@gmail.com>
2024-04-30 22:02:42 +1000

    init

---
+++ Greedy/LeetCode 3119 - MaximumNumberOfPotholesThatCanBeFixed/MaximumNumberOfPotholes/MaximumNumberOfPotholes/Program.cs
@@ -0,0 +1,13 @@
+﻿int MaxPotholes(string road, int budget)
+{
+    var res = 0;
+    var potholes = road.Split('.', StringSplitOptions.RemoveEmptyEntries).OrderByDescending(x => x.Length);
+    foreach (var pothole in potholes)
+    {
+        if (budget <= 0)
+            break;
+        res += pothole.Length + 1 <= budget ? pothole.Length : budget - 1;
+        budget -= Math.Min(budget, pothole.Length + 1);
+    }
+    return res;
+}
