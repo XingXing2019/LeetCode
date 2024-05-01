2edce510048ada0087a924febc98dd8a6b3fa92c
Xing <x.xing.unsw@gmail.com>
2024-05-01 15:14:46 +1000

    init

---
+++ BitManipulation/LeetCode 3125 - MaximumNumberThatMakesResultOfBitwiseANDZero/MaximumNumber/MaximumNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿long MaxNumber(long n)
+{
+    long res = 0;
+    var index = 0;
+    while (n != 1)
+    {
+        res += 1L << index;
+        index++;
+        n >>= 1;
+    }
+    return res;
+}
