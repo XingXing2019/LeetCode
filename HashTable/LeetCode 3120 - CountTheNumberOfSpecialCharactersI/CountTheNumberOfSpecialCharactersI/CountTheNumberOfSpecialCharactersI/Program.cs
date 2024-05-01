07764c024cb3ce2ed12bec31b2fb3318a05be6a4
Xing <x.xing.unsw@gmail.com>
2024-05-01 14:11:15 +1000

    init

---
+++ HashTable/LeetCode 3120 - CountTheNumberOfSpecialCharactersI/CountTheNumberOfSpecialCharactersI/CountTheNumberOfSpecialCharactersI/Program.cs
@@ -0,0 +1,19 @@
+﻿int NumberOfSpecialChars(string word)
+{
+    var upper = new int[26];
+    var lower = new int[26];
+    foreach (var l in word)
+    {
+        if (char.IsUpper(l))
+            upper[l - 'A']++;
+        else
+            lower[l - 'a']++;
+    }
+    var res = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        if (upper[i] * lower[i] == 0) continue;
+        res++;
+    }
+    return res;
+}
