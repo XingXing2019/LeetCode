5c20a9edd36e0f00e7c4c63beea18c0be2cd6f88
Xing <x.xing.unsw@gmail.com>
2024-05-02 21:26:39 +1000

    init

---
+++ BitManipulation/LeetCode 3133 - MinimumArrayEnd/MinimumArrayEnd/MinimumArrayEnd/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 2, x = 7;
+Console.WriteLine(MinEnd(n, x));
+
+long MinEnd(int n, int x)
+{
+    long res = x;
+    n--;
+    for (int i = 0; i < 64 && n != 0; i++)
+    {
+        if ((x & 1) == 0)
+        {
+            if ((n & 1) == 1)
+                res += 1L << i;
+            n >>= 1;
+        }
+        x >>= 1;
+    }
+    return res;
+}
