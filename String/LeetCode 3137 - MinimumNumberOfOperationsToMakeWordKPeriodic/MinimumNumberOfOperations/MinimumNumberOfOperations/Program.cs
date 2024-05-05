84a0f0fbce4fc57da0588e24f907e81221971d18
Xing <x.xing.unsw@gmail.com>
2024-05-05 15:50:15 +1000

    init

---
+++ String/LeetCode 3137 - MinimumNumberOfOperationsToMakeWordKPeriodic/MinimumNumberOfOperations/MinimumNumberOfOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿int MinimumOperationsToMakeKPeriodic(string word, int k)
+{
+    var freq = new Dictionary<string, int>();
+    var max = 0;
+    var substring = "";
+    for (int i = 0; i < word.Length; i += k)
+    {
+        var sub = word.Substring(i, k);
+        if (!freq.ContainsKey(sub))
+            freq[sub] = 0;
+        freq[sub]++;
+        if (freq[sub] > max)
+        {
+            max = freq[sub];
+            substring = sub;
+        }
+    }
+    return freq.Where(x => x.Key != substring).Sum(x => x.Value);
+}
