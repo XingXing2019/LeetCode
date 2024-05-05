bd8d85c6f2d9fa71cf9f6c28a1a1f92b65dd404a
Xing <x.xing.unsw@gmail.com>
2024-05-05 15:39:40 +1000

    init

---
+++ String/LeetCode 3136 - ValidWord/ValidWord/ValidWord/Program.cs
@@ -0,0 +1,19 @@
+﻿var word = "AhI";
+Console.WriteLine(IsValid(word));
+
+bool IsValid(string word)
+{
+    if (word.Length < 3) return false;
+    var vowel = new HashSet<int> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+    bool hasVowel = false, hasConsonant = false;
+    foreach (var l in word)
+    {
+        if (!char.IsDigit(l) && !char.IsLetter(l))
+            return false;
+        if (vowel.Contains(l))
+            hasVowel = true;
+        else if (char.IsLetter(l) && !vowel.Contains(l))
+            hasConsonant = true;
+    }
+    return hasVowel && hasConsonant;
+}
