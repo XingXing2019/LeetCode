e86c62213c8a81ef9c53a3d785ecfe0659a5dad5
Xing <x.xing.unsw@gmail.com>
2024-05-12 11:28:52 +1000

    init

---
+++ Array/LeetCode 3142 - CheckIfGridSatisfiesConditions/CheckIfGridSatisfiesConditions/CheckIfGridSatisfiesConditions/Program.cs
@@ -0,0 +1,12 @@
+﻿bool SatisfiesConditions(int[][] grid)
+{
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[0].Length; j++)
+        {
+            if (i != 0 && grid[i][j] != grid[i - 1][j]) return false;
+            if (j != 0 && grid[i][j] == grid[i][j - 1]) return false;
+        }
+    }
+    return true;
+}
---
+++ DynamicProgramming/LeetCode 3144 - MinimumSubstringPartitionOfEqualCharacterFrequency/MinimumSubstringPartition/MinimumSubstringPartition/Program.cs
@@ -0,0 +1,39 @@
+﻿var s = "fabccddg";
+Console.WriteLine(MinimumSubstringsInPartition(s));
+int MinimumSubstringsInPartition(string s)
+{
+    var freq = new int[s.Length][];
+    for (int i = 0; i < s.Length; i++)
+    {
+        freq[i] = new int[26];
+        if (i != 0)
+            Array.Copy(freq[i - 1], freq[i], 26);
+        freq[i][s[i] - 'a']++;
+    }
+    var dp = new int[s.Length];
+    dp[0] = 1;
+    for (int i = 1; i < s.Length; i++)
+    {
+        var min = int.MaxValue;
+        for (int j = 0; j <= i; j++)
+        {
+            if (!IsBalance(freq, i, j)) continue;
+            min = Math.Min(min, j == 0 ? 0 : dp[j - 1]);
+        }
+        dp[i] = min + 1;
+    }
+    return dp[^1];
+}
+
+bool IsBalance(int[][] freq, int i, int j)
+{
+    var freq1 = j == 0 ? new int[26] : freq[j - 1];
+    var freq2 = freq[i];
+    var diff = new HashSet<int>();
+    for (int k = 0; k < 26; k++)
+    {
+        if (freq1[k] == freq2[k]) continue;
+        diff.Add(freq2[k] - freq1[k]);
+    } 
+    return diff.Count == 1;
+}
