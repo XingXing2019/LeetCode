595a5e5998c08523f6034713093743b4b990bda9
Xing <x.xing.unsw@gmail.com>
2024-05-12 19:12:10 +1000

    init

---
+++ String/LeetCode 3146 - PermutationDifferenceBetweenTwoStrings/PermutationDifferenceBetweenTwoStrings/PermutationDifferenceBetweenTwoStrings/Program.cs
@@ -0,0 +1,10 @@
+﻿int FindPermutationDifference(string s, string t)
+{
+    var dict = new Dictionary<char, int>();
+    for (int i = 0; i < t.Length; i++)
+        dict[t[i]] = i;
+    var res = 0;
+    for (int i = 0; i < s.Length; i++)
+        res += Math.Abs(i - dict[s[i]]);
+    return res;
+}
