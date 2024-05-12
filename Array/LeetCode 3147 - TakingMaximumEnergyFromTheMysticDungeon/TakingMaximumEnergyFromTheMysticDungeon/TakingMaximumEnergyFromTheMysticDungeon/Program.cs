0dae12f6d9f7d62c6d4ca195b66cc49636ca6ae7
Xing <x.xing.unsw@gmail.com>
2024-05-12 19:30:09 +1000

    init

---
+++ Array/LeetCode 3147 - TakingMaximumEnergyFromTheMysticDungeon/TakingMaximumEnergyFromTheMysticDungeon/TakingMaximumEnergyFromTheMysticDungeon/Program.cs
@@ -0,0 +1,14 @@
+﻿int MaximumEnergy(int[] energy, int k)
+{
+    for (int i = 0; i < energy.Length; i++)
+        energy[i] += i - k >= 0 ? energy[i - k] : 0;
+    var res = int.MinValue;
+    for (int i = 0; i < energy.Length; i++)
+    {
+        var pre = i - k >= 0 ? energy[i - k] : 0;
+        var index = (energy.Length - i) % k == 0 ? ((energy.Length - i) / k - 1) * k + i : (energy.Length - i) / k * k + i;
+        var post = energy[index];
+        res = Math.Max(res, post - pre);
+    }
+    return res;
+}
