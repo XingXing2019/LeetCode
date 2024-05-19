6502600d8cc4979c9efb54e6e3926e9a7e37eba9
Xing <x.xing.unsw@gmail.com>
2024-05-19 18:14:07 +1000

    init

---
+++ Array/LeetCode 3153 - SumOfDigitDifferencesOfAllPairs/SumOfDigitDifferencesOfAllPairs/SumOfDigitDifferencesOfAllPairs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.WebSockets;
+
+int[] nums = { 50, 28 };
+Console.WriteLine(SumDigitDifferences(nums));
+
+long SumDigitDifferences(int[] nums)
+{
+    var freqs = new int[10][];
+    for (int i = 0; i < freqs.Length; i++)
+        freqs[i] = new int[10];
+    foreach (var num in nums)
+    {
+        int index = 0, copy = num;
+        while (copy != 0)
+        {
+            freqs[index++][copy % 10]++;
+            copy /= 10;
+        }
+    }
+    var digits = new long[10];
+    foreach (var num in nums)
+    {
+        int index = 0, copy = num;
+        while (copy != 0)
+        {
+            digits[index] += GetSum(copy % 10, freqs[index++], nums.Length);
+            copy /= 10;
+        }
+    }
+    return digits.Sum(x => x / 2);
+}
+
+long GetSum(int digit, int[] freq, int count)
+{
+    var res = 0L;
+    for (int i = 0; i < freq.Length; i++)
+    {
+        if (i == digit) continue;
+        res += freq[i];
+    }
+    return res;
+}
