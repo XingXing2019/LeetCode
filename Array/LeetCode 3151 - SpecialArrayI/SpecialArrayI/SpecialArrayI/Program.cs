7c8277ab5df04832d14a8e4760af39b2a544775a
Xing <x.xing.unsw@gmail.com>
2024-05-19 16:38:09 +1000

    init

--- Array/LeetCode 3151 - SpecialArrayI/SpecialArrayI/SpecialArrayI/Program.cs
+++ Array/LeetCode 3151 - SpecialArrayI/SpecialArrayI/SpecialArrayI/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsArraySpecial(int[] nums)
+{
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] % 2 == nums[i - 1] % 2)
+            return false;
+    }
+    return true;
+}
