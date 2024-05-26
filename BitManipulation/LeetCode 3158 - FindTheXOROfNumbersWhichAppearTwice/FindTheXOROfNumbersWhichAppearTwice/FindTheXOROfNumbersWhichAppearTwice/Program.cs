20a5ec424cc96f67dec0e66452fff184105cdeec
Xing <x.xing.unsw@gmail.com>
2024-05-26 16:22:09 +1000

    init

--- BitManipulation/LeetCode 3158 - FindTheXOROfNumbersWhichAppearTwice/FindTheXOROfNumbersWhichAppearTwice/FindTheXOROfNumbersWhichAppearTwice/Program.cs
+++ BitManipulation/LeetCode 3158 - FindTheXOROfNumbersWhichAppearTwice/FindTheXOROfNumbersWhichAppearTwice/FindTheXOROfNumbersWhichAppearTwice/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int DuplicateNumbersXOR(int[] nums)
+{
+    nums = nums.GroupBy(x => x).Where(x => x.Count() == 2).Select(x => x.Key).ToArray();
+    var res = 0;
+    foreach (var num in nums)
+        res ^= num;
+    return res;
+}
