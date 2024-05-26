cdc4fda7bff09a075f80df396c35bb073fc6c6f4
Xing <x.xing.unsw@gmail.com>
2024-05-26 16:27:21 +1000

    init

---
+++ HashTable/LeetCode 3159 - FindOccurrencesOfAnElementInAnArray/FindOccurrencesOfAnElementInAnArray/FindOccurrencesOfAnElementInAnArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] OccurrencesOfElement(int[] nums, int[] queries, int x)
+{
+    var dict = new Dictionary<int, int>();
+    var count = 1;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] != x) continue;
+        dict[count++] = i;
+    }
+    var res = new int[queries.Length];
+    for (int i = 0; i < queries.Length; i++)
+        res[i] = dict.GetValueOrDefault(queries[i], -1);
+    return res;
+}
