d135d0974cce520292eef5f2b3dad7a8b7e86064
Xing <x.xing.unsw@gmail.com>
2024-05-26 17:01:37 +1000

    init

---
+++ String/LeetCode 3163 - StringCompressionIII/StringCompressionIII/StringCompressionIII/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+string CompressedString(string word)
+{
+    var res = new StringBuilder();
+    int index = 0, count = 0;
+    var cur = word[0];
+    while (index < word.Length)
+    {
+        cur = word[index];
+        while (index < word.Length && word[index] == cur && count < 9)
+        {
+            index++;
+            count++;
+        }
+        res.Append($"{count}{cur}");
+        count = 0;
+    }
+    return res.ToString();
+}
