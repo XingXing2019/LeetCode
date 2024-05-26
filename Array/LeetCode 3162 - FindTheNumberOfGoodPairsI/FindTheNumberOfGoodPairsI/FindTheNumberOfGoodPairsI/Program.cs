77630ac5b38a1f37e00d6e29cf0dc9e77da6fcc9
Xing <x.xing.unsw@gmail.com>
2024-05-26 16:50:38 +1000

    init

---
+++ Array/LeetCode 3162 - FindTheNumberOfGoodPairsI/FindTheNumberOfGoodPairsI/FindTheNumberOfGoodPairsI/Program.cs
@@ -0,0 +1,14 @@
+﻿int NumberOfPairs(int[] nums1, int[] nums2, int k)
+{
+    nums2 = nums2.Select(x => x * k).ToArray();
+    var res = 0;
+    foreach (var n1 in nums1)
+    {
+        foreach (var n2 in nums2)
+        {
+            if (n1 % n2 != 0) continue;
+            res++;
+        }
+    }
+    return res;
+}
