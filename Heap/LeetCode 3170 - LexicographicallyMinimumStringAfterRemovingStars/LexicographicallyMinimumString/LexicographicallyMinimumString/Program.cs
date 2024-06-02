16d8a84aca1a0d04580baf496a346adc2cf65f6c
Xing <x.xing.unsw@gmail.com>
2024-06-02 18:06:04 +1000

    init

---
+++ Heap/LeetCode 3170 - LexicographicallyMinimumStringAfterRemovingStars/LexicographicallyMinimumString/LexicographicallyMinimumString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+string ClearStars(string s)
+{
+    var delete = new HashSet<int>();
+    var heap = new PriorityQueue<int, int>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] != '*')
+            heap.Enqueue(i, s[i] * s.Length - i);
+        else
+        {
+            delete.Add(i);
+            delete.Add(heap.Dequeue());
+        }
+    }
+    var res = new StringBuilder();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (delete.Contains(i)) continue;
+        res.Append(s[i]);
+    }
+    return res.ToString();
+}
