12338b880c9dd237b5df0635677d2720620e2ab4
Xing <x.xing.unsw@gmail.com>
2024-06-02 17:49:56 +1000

    init

---
+++ String/LeetCode 3168 - MinimumNumberOfChairsInAWaitingRoom/MinimumNumberOfChairsInAWaitingRoom/MinimumNumberOfChairsInAWaitingRoom/Program.cs
@@ -0,0 +1,10 @@
+﻿int MinimumChairs(string s)
+{
+    int count = 0, res = 0;
+    foreach (var l in s)
+    {
+        count += l == 'E' ? 1 : -1;
+        res = Math.Max(res, count);
+    }
+    return res;
+}
