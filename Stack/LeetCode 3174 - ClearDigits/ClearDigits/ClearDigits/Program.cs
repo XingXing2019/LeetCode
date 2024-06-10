84df03422e2fbebe9857d7d75b59a3fb9420ce97
Xing <x.xing.unsw@gmail.com>
2024-06-10 10:04:48 +1000

    init

---
+++ Stack/LeetCode 3174 - ClearDigits/ClearDigits/ClearDigits/Program.cs
@@ -0,0 +1,12 @@
+﻿string ClearDigits(string s)
+{
+    var stack = new Stack<char>();
+    foreach (var l in s)
+    {
+        if (char.IsDigit(l))
+            stack.Pop();
+        else
+            stack.Push(l);
+    }
+    return string.Join("", stack.Reverse());
+}
