70ab4fccf967c0c1b07efb3d20117bddc34bd918
Xing <x.xing.unsw@gmail.com>
2024-06-10 10:21:58 +1000

    init

---
+++ Stack/LeetCode 3175 - FindTheFirstPlayerToWinKGamesInARow/FindTheFirstPlayerToWinKGamesInARow/FindTheFirstPlayerToWinKGamesInARow/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] skills = { 2, 5, 4 };
+var k = 3;
+Console.WriteLine(FindWinningPlayer(skills, k));
+int FindWinningPlayer(int[] skills, int k)
+{
+    var queue = new Queue<int[]>();
+    var max = skills.Max();
+    for (int i = 0; i < skills.Length; i++)
+        queue.Enqueue(new[] { skills[i], i, 0 });
+    var stack = new Stack<int[]>();
+    while (queue.Peek()[2] != k)
+    {
+        var skill = queue.Dequeue();
+        while (stack.Count != 0 && skill[0] > stack.Peek()[0])
+        {
+            skill[2]++;
+            if (skill[2] == k) return skill[1];
+            queue.Enqueue(stack.Pop());
+        }
+        if (stack.Count != 0)
+        {
+            stack.Peek()[2]++;
+            if (stack.Peek()[2] == k) return stack.Peek()[1];
+            queue.Enqueue(skill);
+        }
+        else
+            stack.Push(skill);
+        if (stack.Peek()[0] == max) return stack.Peek()[1];
+    }
+    return -1;
+}
