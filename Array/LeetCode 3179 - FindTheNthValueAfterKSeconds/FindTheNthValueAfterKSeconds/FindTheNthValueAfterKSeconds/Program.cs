02041b5faff760298b68bb5c66154bd25981343b
Xing <x.xing.unsw@gmail.com>
2024-06-10 10:43:39 +1000

    init

---
+++ Array/LeetCode 3179 - FindTheNthValueAfterKSeconds/FindTheNthValueAfterKSeconds/FindTheNthValueAfterKSeconds/Program.cs
@@ -0,0 +1,14 @@
+﻿int ValueAfterKSeconds(int n, int k)
+{
+    var nums = new long[n];
+    var mod = 1_000_000_000 + 7;
+    Array.Fill(nums, 1);
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            nums[j] = j == 0 ? 1 : (nums[j] + nums[j - 1]) % mod;
+        }
+    }
+    return (int)(nums[^1] % mod);
+}
