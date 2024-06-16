95d1eecdcc22eb98c99299bd3db4d08d7735cc62
Xing <x.xing.unsw@gmail.com>
2024-06-16 16:53:22 +1000

    init

--- HashTable/LeetCode 3185 - CountPairsThatFormACompleteDayII/CountPairsThatFormACompleteDayII/CountPairsThatFormACompleteDayII/Program.cs
+++ HashTable/LeetCode 3185 - CountPairsThatFormACompleteDayII/CountPairsThatFormACompleteDayII/CountPairsThatFormACompleteDayII/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿long CountCompleteDayPairs(int[] hours)
+{
+    var freq = new Dictionary<int, long>();
+    var res = 0L;
+    foreach (var hour in hours)
+    {
+        var mod = hour % 24;
+        if (mod != 0 && freq.ContainsKey(24 - mod))
+            res += freq[24 - mod];
+        else if (mod == 0 && freq.ContainsKey(mod))
+            res += freq[mod];
+        if (!freq.ContainsKey(mod))
+            freq[mod] = 0;
+        freq[mod]++;
+    }
+    return res;
+}
