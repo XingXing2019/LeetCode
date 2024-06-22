dc729b0729e69056c3dc2772ccc5223e6c31a562
Xing <x.xing.unsw@gmail.com>
2024-06-22 10:22:38 +1000

    init

---
+++ Greedy/LeetCode 3189 - MinimumMovesToGetAPeacefulBoard/MinimumMovesToGetAPeacefulBoard/MinimumMovesToGetAPeacefulBoard/Program.cs
@@ -0,0 +1,9 @@
+﻿int MinMoves(int[][] rooks)
+{
+    var rows = rooks.Select(x => x[0]).OrderBy(x => x).ToArray();
+    var cols = rooks.Select(x => x[1]).OrderBy(x => x).ToArray();
+    var res = 0;
+    for (int i = 0; i < rooks.Length; i++)
+        res += Math.Abs(rows[i] - i) + Math.Abs(cols[i] - i);
+    return res;
+}
