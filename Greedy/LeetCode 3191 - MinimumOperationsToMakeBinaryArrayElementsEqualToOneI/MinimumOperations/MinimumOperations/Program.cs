19dd8d96cf0abdd19b4e99bffd2c278b72cf9e59
Xing <x.xing.unsw@gmail.com>
2024-06-23 09:13:02 +1000

    init

---
+++ Greedy/LeetCode 3191 - MinimumOperationsToMakeBinaryArrayElementsEqualToOneI/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,13 @@
+﻿int MinOperations(int[] nums)
+{
+    var res = 0;
+    for (int i = 0; i <= nums.Length - 3; i++)
+    {
+        if (nums[i] == 1) continue;
+        nums[i] ^= 1;
+        nums[i + 1] ^= 1;
+        nums[i + 2] ^= 1;
+        res++;
+    }
+    return nums[^1] == 1 && nums[^2] == 1 ? res : -1;
+}
