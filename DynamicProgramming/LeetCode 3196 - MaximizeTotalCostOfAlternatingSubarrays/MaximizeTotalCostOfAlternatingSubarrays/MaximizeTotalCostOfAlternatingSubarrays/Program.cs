a21549e1a2c64efdad0ae021da99b37e64aff7e2
Xing <x.xing.unsw@gmail.com>
2024-06-23 19:45:14 +1000

    init

---
+++ DynamicProgramming/LeetCode 3196 - MaximizeTotalCostOfAlternatingSubarrays/MaximizeTotalCostOfAlternatingSubarrays/MaximizeTotalCostOfAlternatingSubarrays/Program.cs
@@ -0,0 +1,17 @@
+﻿long MaximumTotalCost(int[] nums)
+{
+    if (nums.Length == 1) return nums[0];
+    var dp = new long[nums.Length][];
+    for (int i = 0; i < dp.Length; i++)
+        dp[i] = new long[2];
+    dp[0][0] = nums[0];
+    dp[0][1] = -nums[0];
+    dp[1][0] = nums[0] + nums[1];
+    dp[1][1] = nums[0] - nums[1];
+    for (int i = 2; i < dp.Length; i++)
+    {
+        dp[i][0] = Math.Max(dp[i - 1][0], dp[i - 1][1]) + nums[i];
+        dp[i][1] = dp[i - 1][0] - nums[i];
+    }
+    return Math.Max(dp[^1][0], dp[^1][1]);
+}
