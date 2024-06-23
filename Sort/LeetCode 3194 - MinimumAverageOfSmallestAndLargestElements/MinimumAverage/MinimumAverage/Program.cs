b9736e92934de4597302c58ee8e77bb5cbcd8e4a
Xing <x.xing.unsw@gmail.com>
2024-06-23 16:28:18 +1000

    init

---
+++ Sort/LeetCode 3194 - MinimumAverageOfSmallestAndLargestElements/MinimumAverage/MinimumAverage/Program.cs
@@ -0,0 +1,9 @@
+﻿double MinimumAverage(int[] nums)
+{
+    var res = double.MaxValue;
+    Array.Sort(nums);
+    int li = 0, hi = nums.Length - 1;
+    while (li < hi)
+        res = Math.Min(res, ((double)nums[li++] + nums[hi--]) / 2);
+    return res;
+}
