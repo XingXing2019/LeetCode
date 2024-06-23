e4771202d7479043857994b88e66dacdcd8a9571
Xing <x.xing.unsw@gmail.com>
2024-06-23 16:36:51 +1000

    init

---
+++ Array/LeetCode 3195 - FindTheMinimumAreaToCoverAllOnesI/FindTheMinimumAreaToCoverAllOnesI/FindTheMinimumAreaToCoverAllOnesI/Program.cs
@@ -0,0 +1,24 @@
+﻿int[][] grid = new[]
+{
+    new[] { 0, 1, 0 },
+    new[] { 1, 0, 1 },
+};
+Console.WriteLine(MinimumArea(grid));
+
+int MinimumArea(int[][] grid)
+{
+    int rowLi = grid.Length, rowHi = 0;
+    int colLi = grid[0].Length, colHi = 0;
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[0].Length; j++)
+        {
+            if (grid[i][j] == 0) continue;
+            rowLi = Math.Min(rowLi, i);
+            rowHi = Math.Max(rowHi, i);
+            colLi = Math.Min(colLi, j);
+            colHi = Math.Max(colHi, j);
+        }
+    }
+    return (rowHi - rowLi + 1) * (colHi - colLi + 1);
+}
