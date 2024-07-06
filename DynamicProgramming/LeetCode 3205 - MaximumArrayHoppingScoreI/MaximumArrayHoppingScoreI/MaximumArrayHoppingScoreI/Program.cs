f216b81b9184d33e86a0a4fd297827c093dd5622
Xing <x.xing.unsw@gmail.com>
2024-07-07 09:16:02 +1000

    init

---
+++ DynamicProgramming/LeetCode 3205 - MaximumArrayHoppingScoreI/MaximumArrayHoppingScoreI/MaximumArrayHoppingScoreI/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] nums = { 1, 5, 8 };
+Console.WriteLine(MaxScore(nums));
+int MaxScore(int[] nums)
+{
+    var dp = new int[nums.Length];
+    for (int i = 1; i < dp.Length; i++)
+    {
+        var max = 0;
+        for (int j = 0; j < i; j++)
+            max = Math.Max(max, (j - i) * nums[i] + dp[j]);
+        dp[i] = max;
+    }
+    return dp[^1];
+}
