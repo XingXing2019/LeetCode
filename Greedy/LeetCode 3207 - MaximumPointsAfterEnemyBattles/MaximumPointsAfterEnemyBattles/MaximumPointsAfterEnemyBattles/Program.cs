1caa21e6dcd24fd6f245a83d1e2b32330a8b3e00
Xing <x.xing.unsw@gmail.com>
2024-07-07 09:41:15 +1000

    init

---
+++ Greedy/LeetCode 3207 - MaximumPointsAfterEnemyBattles/MaximumPointsAfterEnemyBattles/MaximumPointsAfterEnemyBattles/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] enemyEnergies = { 3, 2, 2 };
+var currentEnergy = 2;
+Console.WriteLine(MaximumPoints(enemyEnergies, currentEnergy));
+long MaximumPoints(int[] enemyEnergies, int currentEnergy)
+{
+    var res = 0L;
+    var index = enemyEnergies.Length - 1;
+    Array.Sort(enemyEnergies);
+    while (index >= 0 && currentEnergy >= enemyEnergies[0])
+    {
+        var count = currentEnergy / enemyEnergies[0];
+        res += count;
+        currentEnergy -= count * enemyEnergies[0];
+        currentEnergy += enemyEnergies[index--];
+    }
+    return res;
+}
