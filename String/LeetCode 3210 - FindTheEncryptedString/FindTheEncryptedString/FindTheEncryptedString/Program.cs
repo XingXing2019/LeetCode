76bad4f30595565d0bd7008d9dca17186539e960
Xing <x.xing.unsw@gmail.com>
2024-07-07 21:16:10 +1000

    init

---
+++ String/LeetCode 3210 - FindTheEncryptedString/FindTheEncryptedString/FindTheEncryptedString/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+string GetEncryptedString(string s, int k)
+{
+    var res = new StringBuilder();
+    for (int i = 0; i < s.Length; i++)
+        res.Append(s[(i + k) % s.Length]);
+    return res.ToString();
+}
