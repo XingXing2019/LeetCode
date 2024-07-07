30bdabb120bccf3ffff254fc3a36862388e0196c
Xing <x.xing.unsw@gmail.com>
2024-07-07 21:26:04 +1000

    init

---
+++ Search/LeetCode 3211 - GenerateBinaryStringsWithoutAdjacentZeros/GenerateBinaryStrings/GenerateBinaryStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿IList<string> ValidStrings(int n)
+{
+    var res = new List<string>();
+    DFS(n, "", res);
+    return res;
+}
+
+void DFS(int n, string s, List<string> res)
+{
+    if (s.Length == n)
+    {
+        res.Add(s);
+        return;
+    }
+    if (s.Length == 0 || s[^1] == '1')
+        DFS(n, s + '0', res);
+    DFS(n, s + '1', res);
+}
