e27fdb7ab9e7414d8eaffedfcd0b6c1acc4f9447
Xing <x.xing.unsw@gmail.com>
2024-07-14 16:20:52 +1000

    init

---
+++ Greedy/LeetCode 3216 - LexicographicallySmallestStringAfterASwap/LexicographicallySmallestStringAfterASwap/LexicographicallySmallestStringAfterASwap/Program.cs
@@ -0,0 +1,13 @@
+﻿string GetSmallestString(string s)
+{
+    var letters = s.ToCharArray();
+    for (int i = 1; i < letters.Length; i++)
+    {
+        if (letters[i] % 2 == letters[i - 1] % 2 && letters[i] < letters[i - 1])
+        {
+            (letters[i], letters[i - 1]) = (letters[i - 1], letters[i]);
+            return string.Join("", letters);
+        }
+    }
+    return s;
+}
