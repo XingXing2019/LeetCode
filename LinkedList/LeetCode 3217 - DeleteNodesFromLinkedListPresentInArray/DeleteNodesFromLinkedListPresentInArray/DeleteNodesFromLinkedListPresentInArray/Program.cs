2295ba5263c2e18675d63c596af2eebc33a7d48e
Xing <x.xing.unsw@gmail.com>
2024-07-14 16:29:32 +1000

    init

---
+++ LinkedList/LeetCode 3217 - DeleteNodesFromLinkedListPresentInArray/DeleteNodesFromLinkedListPresentInArray/DeleteNodesFromLinkedListPresentInArray/Program.cs
@@ -0,0 +1,29 @@
+﻿ListNode ModifiedList(int[] nums, ListNode head)
+{
+    var set = new HashSet<int>(nums);
+    var dummy = new ListNode(0, head);
+    var point = dummy;
+    while (head != null)
+    {
+        if (!set.Contains(head.val))
+        {
+            point.next = head;
+            point = point.next;
+        }
+        head = head.next;
+    }
+    point.next = null;
+    return dummy.next;
+}
+
+
+class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
