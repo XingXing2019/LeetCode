753212e2f5093d9da5ffc22c42af3bfe351699fd
Xing <x.xing.unsw@gmail.com>
2024-07-21 21:04:03 +1000

    init

---
+++ TwoPointers/LeetCode 3228 - MaximumNumberOfOperationsToMoveOnesToTheEnd/MaximumNumberOfOperations/MaximumNumberOfOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿var s = "1001101";
+Console.WriteLine(MaxOperations(s));
+
+int MaxOperations(string s)
+{
+    int res = 0, one = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] == 1)
+            one++;
+        else if (i != 0 && s[i - 1] != '0')
+            res += one;
+    }
+    return res;
+}
