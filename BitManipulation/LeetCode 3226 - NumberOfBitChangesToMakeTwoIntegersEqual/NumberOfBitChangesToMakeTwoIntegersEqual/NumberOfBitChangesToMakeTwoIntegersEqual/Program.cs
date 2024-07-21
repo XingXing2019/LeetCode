e36522b65e73a3274713719e2ee9d3cf0b20106f
Xing <x.xing.unsw@gmail.com>
2024-07-21 14:21:39 +1000

    init

---
+++ BitManipulation/LeetCode 3226 - NumberOfBitChangesToMakeTwoIntegersEqual/NumberOfBitChangesToMakeTwoIntegersEqual/NumberOfBitChangesToMakeTwoIntegersEqual/Program.cs
@@ -0,0 +1,12 @@
+﻿int MinChanges(int n, int k)
+{
+    var res = 0;
+    while (n != 0 || k != 0)
+    {
+        if (n % 2 != k % 2)
+            res++;
+        n >>= 1;
+        k >>= 1;
+    }
+    return res;
+}
