f3fef85ee30e67a0921e3cbcb6c75f9668f89893
Xing <x.xing.unsw@gmail.com>
2024-07-21 14:32:57 +1000

    init

--- BitManipulation/LeetCode 3226 - NumberOfBitChangesToMakeTwoIntegersEqual/NumberOfBitChangesToMakeTwoIntegersEqual/NumberOfBitChangesToMakeTwoIntegersEqual/Program.cs
+++ BitManipulation/LeetCode 3226 - NumberOfBitChangesToMakeTwoIntegersEqual/NumberOfBitChangesToMakeTwoIntegersEqual/NumberOfBitChangesToMakeTwoIntegersEqual/Program.cs
@@ -3,8 +3,10 @@
     var res = 0;
     while (n != 0 || k != 0)
     {
-        if (n % 2 != k % 2)
+        if (n % 2 != k % 2 && n % 2 != 0)
             res++;
+        else if (n % 2 != k % 2)
+            return -1;
         n >>= 1;
         k >>= 1;
     }
