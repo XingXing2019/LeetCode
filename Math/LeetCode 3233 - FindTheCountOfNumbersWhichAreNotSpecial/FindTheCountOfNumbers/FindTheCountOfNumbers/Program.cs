0740f94b0faf81a28bd9951d03f1357810be912c
Xing <x.xing.unsw@gmail.com>
2024-07-28 15:25:52 +1000

    init

---
+++ Math/LeetCode 3233 - FindTheCountOfNumbersWhichAreNotSpecial/FindTheCountOfNumbers/FindTheCountOfNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿int l = 5, r = 7;
+Console.WriteLine(NonSpecialCount(l, r));
+
+int NonSpecialCount(int l, int r)
+{
+    var sqrtL = (int)Math.Ceiling(Math.Sqrt(l));
+    var sqrtR = (int)Math.Floor(Math.Sqrt(r));
+    var primes = CountPrime(sqrtL, sqrtR);
+    return r - l - primes + 1;
+}
+
+int CountPrime(int l, int r)
+{
+    var dp = new bool[r + 1];
+    Array.Fill(dp, true);
+    dp[0] = dp[1] = false;
+    for (int i = 2; i < dp.Length; i++)
+    {
+        if (!dp[i]) continue;
+        for (int j = 2; i * j < dp.Length; j++)
+        {
+            dp[i * j] = false;
+        }
+    }
+    var res = 0;
+    for (int i = l; i <= r; i++)
+    {
+        if (!dp[i]) continue;
+        res++;
+    }
+    return res;
+}
