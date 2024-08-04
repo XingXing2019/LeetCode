1f314c730cb710ebe46c8aa0136ca5c7565c4e58
Xing <x.xing.unsw@gmail.com>
2024-08-04 19:49:20 +1000

    init

---
+++ Design/LeetCode 3242 - DesignNeighborSumService/DesignNeighborSumService/DesignNeighborSumService/Program.cs
@@ -0,0 +1,39 @@
+﻿class neighborSum
+{
+    private readonly int[][] _grid;
+    private readonly Dictionary<int, int[]> dict;
+
+    public neighborSum(int[][] grid)
+    {
+        _grid = grid;
+        dict = new Dictionary<int, int[]>();
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                dict[grid[i][j]] = new[] { i, j };
+            }
+        }
+    }
+
+    public int AdjacentSum(int value)
+    {
+        var pos = dict[value];
+        int x = pos[0], y = pos[1];
+        int[] dir = { 1, 0, -1, 0, 1 };
+        var res = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = x + dir[i], newY = y + dir[i + 1];
+            if (newX < 0 || newX >= _grid.Length || newY < 0 || newY >= _grid[0].Length)
+                continue;
+            res += _grid[newX][newY];
+        }
+        return res;
+    }
+
+    public int DiagonalSum(int value)
+    {
+
+    }
+}
