fbc8a819dc9b95e401c62bcf3a7ebc18d3e8f043
Xing <x.xing.unsw@gmail.com>
2024-08-04 19:49:46 +1000

    init

--- Design/LeetCode 3242 - DesignNeighborSumService/DesignNeighborSumService/DesignNeighborSumService/Program.cs
+++ Design/LeetCode 3242 - DesignNeighborSumService/DesignNeighborSumService/DesignNeighborSumService/Program.cs
@@ -34,6 +34,17 @@
 
     public int DiagonalSum(int value)
     {
-
+        var pos = dict[value];
+        int x = pos[0], y = pos[1];
+        int[] dir = { 1, 1, -1, -1, 1 };
+        var res = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = x + dir[i], newY = y + dir[i + 1];
+            if (newX < 0 || newX >= _grid.Length || newY < 0 || newY >= _grid[0].Length)
+                continue;
+            res += _grid[newX][newY];
+        }
+        return res;
     }
 }
