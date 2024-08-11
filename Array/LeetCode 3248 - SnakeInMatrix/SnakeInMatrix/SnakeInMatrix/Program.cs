42344076a741bf799cc07a64c5c396dd4c8a47f0
Xing <x.xing.unsw@gmail.com>
2024-08-11 15:08:41 +1000

    init

---
+++ Array/LeetCode 3248 - SnakeInMatrix/SnakeInMatrix/SnakeInMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿int FinalPositionOfSnake(int n, IList<string> commands)
+{
+    int i = 0, j = 0;
+    foreach (var command in commands)
+    {
+        if (command == "UP") i--;
+        else if (command == "RIGHT") j++;
+        else if (command == "DOWN") i++;
+        else j--;
+    }
+    return i * n + j;
+}
