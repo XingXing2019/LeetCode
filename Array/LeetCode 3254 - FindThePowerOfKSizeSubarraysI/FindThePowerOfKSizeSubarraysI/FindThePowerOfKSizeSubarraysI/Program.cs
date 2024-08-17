02abf65ce3edab92c8b977692449bf5afeedbc82
Xing <x.xing.unsw@gmail.com>
2024-08-18 08:58:40 +1000

    init

---
+++ Array/LeetCode 3254 - FindThePowerOfKSizeSubarraysI/FindThePowerOfKSizeSubarraysI/FindThePowerOfKSizeSubarraysI/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = { 1, 2, 3, 4, 3, 2, 5 };
+var k = 3;
+Console.WriteLine(ResultsArray(nums, k));
+
+int[] ResultsArray(int[] nums, int k)
+{
+    var res = new int[nums.Length - k + 1];
+    for (int i = 0; i <= nums.Length - k; i++)
+    {
+        res[i] = int.MaxValue;
+        for (int j = 0; j < k; j++)
+        {
+            if (j == 0 || nums[i + j] == nums[i + j - 1] + 1) continue;
+            res[i] = -1;
+        }
+        res[i] = Math.Min(res[i], nums[i + k - 1]);
+    }
+    return res;
+}
---
+++ DynamicProgramming/LeetCode 3253 - ConstructStringWithMinimumCost/ConstructStringWithMinimumCost/ConstructStringWithMinimumCost/Program.cs
@@ -0,0 +1,29 @@
+﻿var target = "abcdef";
+string[] words = { "abdef", "abc", "d", "def", "ef" };
+int[] costs = { 100, 1, 1, 10, 5 };
+Console.WriteLine(MinimumCost(target, words, costs));
+
+int MinimumCost(string target, string[] words, int[] costs)
+{
+    var dict = new Dictionary<string, int>();
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (!dict.ContainsKey(words[i]))
+            dict[words[i]] = int.MaxValue;
+        dict[words[i]] = Math.Min(dict[words[i]], costs[i]);
+    }
+    var dp = new int[target.Length + 1];
+    Array.Fill(dp, int.MaxValue);
+    dp[0] = 0;
+    for (int i = 1; i < dp.Length; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (dp[j] == int.MaxValue) continue;
+            var word = target.Substring(j, i - j);
+            if (dict.ContainsKey(word))
+                dp[i] = Math.Min(dp[i], dp[j] + dict[word]);
+        }
+    }
+    return dp[^1] == int.MaxValue ? -1 : dp[^1];
+}
