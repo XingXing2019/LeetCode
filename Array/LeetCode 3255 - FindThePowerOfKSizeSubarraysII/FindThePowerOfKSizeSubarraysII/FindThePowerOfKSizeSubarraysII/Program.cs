bb71835f77f3d1f2d8d1a5c9f5b5b8eb1f1ae0b4
Xing <x.xing.unsw@gmail.com>
2024-08-18 09:02:44 +1000

    init

---
+++ Array/LeetCode 3255 - FindThePowerOfKSizeSubarraysII/FindThePowerOfKSizeSubarraysII/FindThePowerOfKSizeSubarraysII/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] ResultsArray(int[] nums, int k)
+{
+    var dp = new int[nums.Length];
+    dp[0] = 1;
+    for (int i = 1; i < dp.Length; i++)
+        dp[i] = nums[i] == nums[i - 1] + 1 ? dp[i - 1] + 1 : 1;
+    var res = new int[nums.Length - k + 1];
+    for (int i = k - 1; i < dp.Length; i++)
+        res[i - k + 1] = dp[i] >= k ? nums[i] : -1;
+    return res;
+}
