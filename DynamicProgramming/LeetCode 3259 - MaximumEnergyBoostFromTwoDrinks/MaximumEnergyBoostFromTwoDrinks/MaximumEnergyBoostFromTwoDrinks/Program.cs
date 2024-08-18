953c4ddc70d7726b0f0cdaebf6b84dc5084a007a
Xing <x.xing.unsw@gmail.com>
2024-08-18 20:06:54 +1000

    init

---
+++ DynamicProgramming/LeetCode 3259 - MaximumEnergyBoostFromTwoDrinks/MaximumEnergyBoostFromTwoDrinks/MaximumEnergyBoostFromTwoDrinks/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] energyDrinkA = { 4, 1, 1 };
+int[] energyDrinkB = { 1, 1, 3 };
+Console.WriteLine(MaxEnergyBoost(energyDrinkA, energyDrinkB));
+
+long MaxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB)
+{
+    var dp = new long[energyDrinkA.Length][];
+    dp[0] = new long[] { energyDrinkA[0], energyDrinkB[0] };
+    for (int i = 1; i < dp.Length; i++)
+    {
+        var a = energyDrinkA[i] + Math.Max(dp[i - 1][0], i == 1 ? 0 : dp[i - 2][1]);
+        var b = energyDrinkB[i] + Math.Max(dp[i - 1][1], i == 1 ? 0 : dp[i - 2][0]);
+        dp[i] = new[] { a, b };
+    }
+    return Math.Max(dp[^1][0], dp[^1][1]);
+}
