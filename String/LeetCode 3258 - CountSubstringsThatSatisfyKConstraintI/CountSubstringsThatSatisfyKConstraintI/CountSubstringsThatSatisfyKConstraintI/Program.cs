16a6d8b80e4e7af1f5ef1f7d288fbde40d265210
Xing <x.xing.unsw@gmail.com>
2024-08-18 19:54:58 +1000

    init

---
+++ String/LeetCode 3258 - CountSubstringsThatSatisfyKConstraintI/CountSubstringsThatSatisfyKConstraintI/CountSubstringsThatSatisfyKConstraintI/Program.cs
@@ -0,0 +1,16 @@
+﻿int CountKConstraintSubstrings(string s, int k)
+{
+    var res = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        int zero = 0, one = 0;
+        for (int j = i; j < s.Length; j++)
+        {
+            zero += s[j] == '0' ? 1 : 0;
+            one += s[j] - '0';
+            if (zero <= k || one <= k)
+                res++;
+        }
+    }
+    return res;
+}
