dba4b15d96c5e934c30b2b351dd15106bba66d59
Xing <x.xing.unsw@gmail.com>
2024-09-01 08:54:51 +1000

    init

---
+++ String/LeetCode 3271 - HashDividedString/HashDividedString/HashDividedString/Program.cs
@@ -0,0 +1,16 @@
+﻿string StringHash(string s, int k)
+{
+    var res = "";
+    var l = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (i != 0 && i % k == 0)
+        {
+            res += (char)(l + 'a');
+            l = s[i] - 'a';
+        }
+        else
+            l += s[i] - 'a';
+    }
+    return res;
+}
