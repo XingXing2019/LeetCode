23a413a7013352a84da8d7fe1010d215372b33d1
Xing <x.xing.unsw@gmail.com>
2024-09-01 08:50:17 +1000

    init

---
+++ Math/LeetCode 3270 - FindTheKeyOfTheNumbers/FindTheKeyOfTheNumbers/FindTheKeyOfTheNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿int GenerateKey(int num1, int num2, int num3)
+{
+    int res = 0, pow = 1;
+    while (num1 * num2 * num3 != 0)
+    {
+        var digit = Math.Min(num1 % 10, Math.Min(num2 % 10, num3 % 10));
+        res += pow * digit;
+        num1 /= 10;
+        num2 /= 10;
+        num3 /= 10;
+        pow *= 10;
+    }
+    return res;
+}
