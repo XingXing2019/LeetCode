12605cb82a8c8b87eb4f02061c3fbfdee19fa549
Xing <x.xing.unsw@gmail.com>
2024-09-01 17:16:40 +1000

    init

---
+++ Heap/LeetCode 3275 - KthNearestObstacleQueries/KthNearestObstacleQueries/KthNearestObstacleQueries/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] ResultsArray(int[][] queries, int k)
+{
+    var heap = new PriorityQueue<int, int>();
+    var res = new int[queries.Length];
+    for (int i = 0; i < queries.Length; i++)
+    {
+        var dis = Math.Abs(queries[i][0]) + Math.Abs(queries[i][1]);
+        heap.Enqueue(dis, -dis);
+        if (heap.Count > k)
+            heap.Dequeue();
+        res[i] = heap.Count == k ? heap.Peek() : -1;
+    }
+    return res;
+}
