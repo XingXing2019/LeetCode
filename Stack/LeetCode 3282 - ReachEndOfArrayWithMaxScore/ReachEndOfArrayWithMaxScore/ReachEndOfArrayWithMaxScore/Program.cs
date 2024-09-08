2d1810cc33f8fdc5137512b1ac59c154e867201c
Xing <x.xing.unsw@gmail.com>
2024-09-08 18:13:48 +1000

    init

---
+++ Stack/LeetCode 3282 - ReachEndOfArrayWithMaxScore/ReachEndOfArrayWithMaxScore/ReachEndOfArrayWithMaxScore/Program.cs
@@ -0,0 +1,23 @@
+﻿var nums = new List<int> { 1, 3, 1, 5 };
+Console.WriteLine(FindMaximumScore(nums));
+
+long FindMaximumScore(IList<int> nums)
+{
+    var nextGreater = new int[nums.Count];
+    Array.Fill(nextGreater, -1);
+    var stack = new Stack<int>();
+    for (int i = 0; i < nums.Count; i++)
+    {
+        while (stack.Count != 0 && nums[i] > nums[stack.Peek()])
+            nextGreater[stack.Pop()] = i;
+        stack.Push(i);
+    }
+    var index = 0;
+    long res = 0;
+    while (nextGreater[index] != -1)
+    {
+        res += (long)nums[index] * (nextGreater[index] - index);
+        index = nextGreater[index];
+    }
+    return res + (long)nums[index] * (nums.Count - 1 - index);
+}
