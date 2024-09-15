12e8e535fae98df23948549878e5132f6e5aef72
Xing <x.xing.unsw@gmail.com>
2024-09-15 21:12:18 +1000

    init

---
+++ Array/LeetCode 3289 - TheTwoSneakyNumbersOfDigitville/TheTwoSneakyNumbersOfDigitville/TheTwoSneakyNumbersOfDigitville/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] GetSneakyNumbers(int[] nums)
+{
+    var dict = new Dictionary<int, int>();
+    foreach (var num in nums)
+    {
+        if (!dict.ContainsKey(num))
+            dict[num] = 0;
+        dict[num]++;
+    }
+    return dict.Where(x => x.Value == 2).Select(x => x.Key).ToArray();
+}
