da75ca9d9e729cef24e751b93194eaf10f5c49b2
Xing <x.xing.unsw@gmail.com>
2024-09-15 20:19:35 +1000

    init

--- Array/LeetCode 3285 - FindIndicesOfStableMountains/FindIndicesOfStableMountains/FindIndicesOfStableMountains/Program.cs
+++ Array/LeetCode 3285 - FindIndicesOfStableMountains/FindIndicesOfStableMountains/FindIndicesOfStableMountains/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿IList<int> StableMountains(int[] height, int threshold)
+{
+    var res = new List<int>();
+    for (int i = 1; i < height.Length; i++)
+    {
+        if (height[i - 1] <= threshold) continue;
+        res.Add(i);
+    }
+    return res;
+}
