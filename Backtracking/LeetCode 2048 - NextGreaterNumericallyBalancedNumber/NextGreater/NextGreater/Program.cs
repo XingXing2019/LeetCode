798c5b131724da9198ffff3a32a32e972588a6ba
Xing <x.xing.unsw@gmail.com>
2024-09-16 20:02:31 +1000

    init

---
+++ Backtracking/LeetCode 2048 - NextGreaterNumericallyBalancedNumber/NextGreater/NextGreater/Program.cs
@@ -0,0 +1,28 @@
+﻿var n = 1;
+Console.WriteLine(NextBeautifulNumber(n));
+
+int NextBeautifulNumber(int n)
+{
+    while (true)
+    {
+        if (IsValid(++n))
+            return n;
+    }
+}
+
+bool IsValid(int n)
+{
+    var digits = new int[10];
+    while (n != 0)
+    {
+        digits[n % 10]++;
+        n /= 10;
+    }
+    for (int i = 0; i < digits.Length; i++)
+    {
+        if (digits[i] == 0) continue;
+        if (digits[i] != i)
+            return false;
+    }
+    return true;
+}
