ce73454f24ceae0d83cd78de3db48507567ade6d
Xing <x.xing.unsw@gmail.com>
2024-09-17 20:40:31 +1000

    init

---
+++ HashTable/LeetCode 1674 - MinimumMovesToMakeArrayComplementary/MinimumMoves/MinimumMoves/Program.cs
@@ -0,0 +1,29 @@
+﻿int MinMoves(int[] nums, int limit)
+{
+    var dict = new Dictionary<int, int>();
+    for (int i = 0; i < nums.Length / 2; i++)
+    {
+        int a = Math.Min(nums[i], nums[^(i + 1)]);
+        int b = Math.Max(nums[i], nums[^(i + 1)]);
+        Record(dict, 2, 2);
+        Record(dict, a + 1, -1);
+        Record(dict, a + b, -1);
+        Record(dict, a + b + 1, 1);
+        Record(dict, b + limit + 1, 1);
+    }
+    dict = dict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+    int sum = 0, res = int.MaxValue;
+    foreach (var key in dict.Keys)
+    {
+        sum += dict[key];
+        res = Math.Min(res, sum);
+    }
+    return res;
+}
+
+void Record(Dictionary<int, int> dict, int key, int value)
+{
+    if (!dict.ContainsKey(key))
+        dict[key] = 0;
+    dict[key] += value;
+}
