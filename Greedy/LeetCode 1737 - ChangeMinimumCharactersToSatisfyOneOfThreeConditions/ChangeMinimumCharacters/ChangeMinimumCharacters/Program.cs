d9683d39c3d180c4281fbbcce27810d4a23f707f
Xing <x.xing.unsw@gmail.com>
2024-09-18 19:36:15 +1000

    init

---
+++ Greedy/LeetCode 1737 - ChangeMinimumCharactersToSatisfyOneOfThreeConditions/ChangeMinimumCharacters/ChangeMinimumCharacters/Program.cs
@@ -0,0 +1,38 @@
+﻿var a = "a";
+var b = "ayza";
+Console.WriteLine(MinCharacters(a, b));
+
+int MinCharacters(string a, string b)
+{
+    var res = int.MaxValue;
+    for (int i = 0; i < 26; i++)
+        res = Math.Min(res, CountChanges(a, b, (char)(i + 'a')));
+    return res;
+}
+
+int CountChanges(string a, string b, char l)
+{
+    var res = a.Count(x => x != l) + b.Count(x => x != l);
+    if (l != 'z')
+    {
+        res = Math.Min(res, Count(a, b, l));
+        res = Math.Min(res, Count(b, a, l));
+    }
+    return res;
+}
+
+int Count(string a, string b, char l)
+{
+    var res = 0;
+    foreach (var c in a)
+    {
+        if (c <= l) continue;
+        res++;
+    }
+    foreach (var c in b)
+    {
+        if (c > l) continue;
+        res++;
+    }
+    return res;
+}
