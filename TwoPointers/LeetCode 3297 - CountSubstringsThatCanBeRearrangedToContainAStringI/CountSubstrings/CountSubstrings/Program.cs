58965f35a21c1f5d5d8d21292e6835e87766efa1
Xing <x.xing.unsw@gmail.com>
2024-09-22 20:57:44 +1000

    init

---
+++ TwoPointers/LeetCode 3297 - CountSubstringsThatCanBeRearrangedToContainAStringI/CountSubstrings/CountSubstrings/Program.cs
@@ -0,0 +1,22 @@
+﻿var word1 = "bb";
+var word2 = "b";
+Console.WriteLine(ValidSubstringCount(word1, word2));
+long ValidSubstringCount(string word1, string word2)
+{
+    int li = 0, hi = 0;
+    long res = 0;
+    var freq = new int[26];
+    foreach (var l in word2)
+        freq[l - 'a']++;
+    while (hi < word1.Length)
+    {
+        freq[word1[hi] - 'a']--;
+        while (freq.All(x => x <= 0) && li <= hi)
+        {
+            res += word1.Length - hi;
+            freq[word1[li++] - 'a']++;
+        }
+        hi++;
+    }
+    return res;
+}
