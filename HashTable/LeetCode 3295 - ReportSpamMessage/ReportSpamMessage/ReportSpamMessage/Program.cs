dfa39ffaec5c26ae93ced4d74a08e493c9ee7e89
Xing <x.xing.unsw@gmail.com>
2024-09-22 20:26:01 +1000

    init

---
+++ HashTable/LeetCode 3295 - ReportSpamMessage/ReportSpamMessage/ReportSpamMessage/Program.cs
@@ -0,0 +1,6 @@
+﻿bool ReportSpam(string[] message, string[] bannedWords)
+{
+    var banned = new HashSet<string>(bannedWords);
+    var count = message.Count(x => banned.Contains(x));
+    return count > 1;
+}
