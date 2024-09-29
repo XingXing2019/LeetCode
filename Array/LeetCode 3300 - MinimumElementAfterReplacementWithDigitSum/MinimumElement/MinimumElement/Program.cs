e42716ba91208f0695f9abe9f60eb80d48d6378a
Xing <x.xing.unsw@gmail.com>
2024-09-29 10:24:01 +1000

    init

---
+++ Array/LeetCode 3300 - MinimumElementAfterReplacementWithDigitSum/MinimumElement/MinimumElement/Program.cs
@@ -0,0 +1,13 @@
+﻿int MinElement(int[] nums)
+{
+    return nums.Select(x =>
+    {
+        var sum = 0;
+        while (x != 0)
+        {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    }).Min();
+}
