234d6962fcde9fb51366569e8f7c013c5f30fde5
Xing <x.xing.unsw@gmail.com>
2024-09-29 17:19:12 +1000

    init

---
+++ String/LeetCode 3304 - FindTheKthCharacterInStringGameI/FindTheKthCharacterInStringGameI/FindTheKthCharacterInStringGameI/Program.cs
@@ -0,0 +1,15 @@
+﻿var k = 5;
+Console.WriteLine(KthCharacter(k));
+
+char KthCharacter(int k)
+{
+    var s = "a";
+    while (s.Length < k)
+    {
+        var temp = "";
+        foreach (var l in s)
+            temp += l == 'z' ? 'a' : (char)(l + 1);
+        s += temp;
+    }
+    return s[k - 1];
+}
