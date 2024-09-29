6870fdf285cf102a8a3b862371dd9686061ec038
Xing <x.xing.unsw@gmail.com>
2024-09-29 11:26:59 +1000

    init

---
+++ Greedy/LeetCode 3302 - FindTheLexicographicallySmallestValidSequence/FindTheLexicographicallySmallest/FindTheLexicographicallySmallest/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+var word1 = "aaaaaa";
+var word2 = "ab";
+Console.WriteLine(ValidSequence(word1, word2));
+
+int[] ValidSequence(string word1, string word2)
+{
+    var res = new List<int>();
+    var suffix = new int[word1.Length];
+    int p1 = word1.Length - 1, p2 = word2.Length - 1, count = 0;
+    while (p1 >= 0 && p2 >= 0)
+    {
+        if (word1[p1] == word2[p2])
+        {
+            count++;
+            p2--;
+        }
+        suffix[p1--] = count;
+    }
+    while (p1 >= 0)
+        suffix[p1--] = count;
+    p2 = 0;
+    var skip = false;
+    for (int i = 0; i < word1.Length; i++)
+    {
+        if (p2 == word2.Length) return res.ToArray();
+        if (word1[i] == word2[p2])
+        {
+            res.Add(i);
+            p2++;
+        }
+        else if (!skip && (i == word1.Length - 1 ? 0 : suffix[i + 1]) + 1 >= word2.Length - p2)
+        {
+            res.Add(i);
+            p2++;
+            skip = true;
+        }
+    }
+    return res.Count == word2.Length ? res.ToArray() : Array.Empty<int>();
+}
