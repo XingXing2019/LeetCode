5d594cf93f4364ab1fc9f55a559e418afc7b85c5
Xing <x.xing.unsw@gmail.com>
2024-09-29 10:37:38 +1000

    init

---
+++ Greedy/LeetCode 3301 - MaximizeTheTotalHeightOfUniqueTowers/MaximizeTheTotalHeight/MaximizeTheTotalHeight/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] maximumHeight = { 15, 10 };
+Console.WriteLine(MaximumTotalSum(maximumHeight));
+
+long MaximumTotalSum(int[] maximumHeight)
+{
+    Array.Sort(maximumHeight, (a, b) => b - a);
+    long res = maximumHeight[0];
+    var max = maximumHeight[0];
+    for (var i = 1; i < maximumHeight.Length; i++)
+    {
+        var height = Math.Min(maximumHeight[i], max);
+        if (height == max) height--;
+        if (height == 0) return -1;
+        res += height;
+        max = height;
+    }
+    return res;
+}
