fa1f5ba78308958a15a804e892318a63f6de685e
Xing <x.xing.unsw@gmail.com>
2024-10-13 12:42:27 +1100

    init

---
+++ BitManipulation/LeetCode 3314 - ConstructTheMinimumBitwiseArrayI/ConstructTheMinimumBitwiseArrayI/ConstructTheMinimumBitwiseArrayI/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] MinBitwiseArray(IList<int> nums)
+{
+    var res = new int[nums.Count];
+    for (int i = 0; i < nums.Count; i++)
+        res[i] = nums[i] % 2 == 0 ? -1 : GetNum(nums[i]);
+    return res;
+}
+
+int GetNum(int num)
+{
+    for (int i = 1; i <= 1000; i++)
+    {
+        if ((i | (i + 1)) == num)
+            return i;
+    }
+    return -1;
+}
