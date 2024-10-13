830fb6a68345d1faddbd2f143570af416218897c
Xing <x.xing.unsw@gmail.com>
2024-10-13 13:25:16 +1100

    init

---
+++ DynamicProgramming/LeetCode 3316 - FindMaximumRemovalsFromSourceString/FindMaximumRemovals/FindMaximumRemovals/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int MaxRemovals(string source, string pattern, int[] targetIndices)
+{
+    var dp = new int[pattern.Length + 1];
+    Array.Fill(dp, int.MaxValue);
+    dp[0] = 0;
+    var isTarget = new HashSet<int>(targetIndices);
+    for (int i = 0; i < source.Length; i++)
+    {
+        for (int j = pattern.Length; j > 0; j--)
+        {
+            if (source[i] == pattern[j - 1] && dp[j - 1] != int.MaxValue)
+            {
+                dp[j] = Math.Min(dp[j], dp[j - 1] + (isTarget.Contains(i) ? 1 : 0));
+            }
+        }
+    }
+    return targetIndices.Length - (dp[^1] == int.MaxValue ? 0 : dp[^1]);
+}
