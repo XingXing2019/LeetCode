05f7c0018238fff25e6960f3a65b1d6b1efb4947
Xing <x.xing.unsw@gmail.com>
2024-10-13 19:47:07 +1100

    init

---
+++ Tree/LeetCode 3319 - KthLargestPerfectSubtreeSizeInBinaryTree/KthLargestPerfectSubtreeSizeInBinaryTree/KthLargestPerfectSubtreeSizeInBinaryTree/Program.cs
@@ -0,0 +1,39 @@
+﻿int KthLargestPerfectSubtree(TreeNode root, int k)
+{
+    var nodes = new List<int>();
+    DFS(root, nodes);
+    nodes.Sort((a, b) => b - a);
+    return nodes.Count < k ? -1 : nodes[k - 1];
+}
+
+int[] DFS(TreeNode root, List<int> nodes)
+{
+    if (root == null)
+        return new[] { 0, 0 };
+    if (root.left == root.right)
+    {
+        nodes.Add(1);
+        return new[] { 1, 1 };
+    }
+    var left = DFS(root.left, nodes);
+    var right = DFS(root.right, nodes);
+    if (left[0] == 1 && right[0] == 1 && left[1] == right[1])
+    {
+        nodes.Add(left[1] * 2 + 1);
+        return new[] { 1, left[1] * 2 + 1 };
+    }
+    return new[] { 0, 0 };
+}
+
+class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
