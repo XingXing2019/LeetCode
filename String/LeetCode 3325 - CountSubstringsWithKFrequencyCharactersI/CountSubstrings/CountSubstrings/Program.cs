7c0e95799ee66626dbacc1219896ea9d1c5fd1d1
Xing <x.xing.unsw@gmail.com>
2024-10-20 15:50:20 +1100

    init

---
+++ String/LeetCode 3325 - CountSubstringsWithKFrequencyCharactersI/CountSubstrings/CountSubstrings/Program.cs
@@ -0,0 +1,22 @@
+﻿var s = "abacb";
+var k = 2;
+Console.WriteLine(NumberOfSubstrings(s, k));
+
+int NumberOfSubstrings(string s, int k)
+{
+    var res = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        var freq = new int[26];
+        for (int j = i; j < s.Length; j++)
+        {
+            freq[s[j] - 'a']++;
+            if (freq[s[j] - 'a'] == k)
+            {
+                res += s.Length - j;
+                break;
+            }
+        }
+    }
+    return res;
+}
