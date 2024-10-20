01c84e2da862334967e80be94d9b20a2ace18ae3
Xing <x.xing.unsw@gmail.com>
2024-10-20 15:41:06 +1100

    init

---
+++ String/LeetCode 3324 - FindTheSequenceOfStringsAppearedOnTheScreen/FindTheSequenceOfStrings/FindTheSequenceOfStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿IList<string> StringSequence(string target)
+{
+    var res = new List<string>();
+    var cur = "a";
+    while (cur != target)
+    {
+        res.Add(cur);
+        if (target.StartsWith(cur))
+            cur += "a";
+        else
+            cur = $"{cur.Substring(0, cur.Length - 1)}{(char)(cur[^1] + 1)}";
+    }
+    res.Add(cur);
+    return res;
+}
