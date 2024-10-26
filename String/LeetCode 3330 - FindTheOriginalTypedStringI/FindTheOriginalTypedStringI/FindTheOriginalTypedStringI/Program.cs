a850ffdcedbe3176deea9e1c6fb094b505f6000f
Xing <x.xing.unsw@gmail.com>
2024-10-27 10:26:12 +1100

    init

---
+++ String/LeetCode 3330 - FindTheOriginalTypedStringI/FindTheOriginalTypedStringI/FindTheOriginalTypedStringI/Program.cs
@@ -0,0 +1,17 @@
+﻿int PossibleStringCount(string word)
+{
+    int res = 0, count = 0;
+    var cur = word[0];
+    foreach (var l in word)
+    {
+        if (l == cur)
+            count++;
+        else
+        {
+            cur = l;
+            res += count - 1;
+            count = 1;
+        }
+    }
+    return res + count;
+}
