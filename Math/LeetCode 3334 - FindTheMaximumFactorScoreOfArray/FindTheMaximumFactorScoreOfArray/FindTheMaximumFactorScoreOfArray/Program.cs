4a83cbdf1cb1aa3e229dcddbfbdfbcc1c10b7760
Xing <x.xing.unsw@gmail.com>
2024-10-27 15:36:48 +1100

    init

---
+++ Math/LeetCode 3334 - FindTheMaximumFactorScoreOfArray/FindTheMaximumFactorScoreOfArray/FindTheMaximumFactorScoreOfArray/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] nums = { 2, 4, 8, 16 };
+Console.WriteLine(MaxScore(nums));
+
+long MaxScore(int[] nums)
+{
+    var res = 0L;
+    for (int i = 0; i < nums.Length; i++)
+        res = Math.Max(res, Math.Max(GetNum(nums, i, true), GetNum(nums, i, false)));
+    return res;
+}
+
+long GetNum(int[] nums, int index, bool remove)
+{
+    var copy = new List<int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (i == index && remove) continue;
+        copy.Add(nums[i]);
+    }
+    if (copy.Count == 0) return 0;
+    if (copy.Count < 2) return copy[0] * copy[0];
+    long gcd = GCD(copy[0], copy[1]), lcm = LCM(copy[0], copy[1]);
+    for (int i = 2; i < copy.Count; i++)
+    {
+        gcd = GCD(gcd, copy[i]);
+        lcm = LCM(lcm, copy[i]);
+    }
+    return gcd * lcm;
+}
+
+long GCD(long a, long b)
+{
+    if (a % b == 0) return b;
+    return GCD(b, a % b);
+}
+
+long LCM(long a, long b)
+{
+    var gcd = GCD(a, b);
+    return a * b / gcd;
+}
---
+++ Tree/LeetCode 3331 - FindSubtreeSizesAfterChanges/FindSubtreeSizesAfterChanges/FindSubtreeSizesAfterChanges/Program.cs
@@ -0,0 +1,50 @@
+﻿int[] parents = { -1, 0, 4, 0, 1, 2 };
+var s = "abbbav";
+Console.WriteLine(FindSubtreeSizes(parents, s));
+
+int[] FindSubtreeSizes(int[] parent, string s)
+{
+    var res = new int[parent.Length];
+    var moveTo = new List<int>[26];
+    var graph = new List<int>[parent.Length];
+    for (int i = 0; i < graph.Length; i++)
+    {
+        graph[i] = new List<int>();
+        moveTo[i] = new List<int>();
+    }
+    for (int i = 0; i < parent.Length; i++)
+    {
+        if (parent[i] == -1) continue;
+        graph[parent[i]].Add(i);
+    }
+    var children = new Dictionary<int, HashSet<int>>();
+    GetChildren(0, graph, res, children);
+    DFS(0, graph, s, parent, res, moveTo, new HashSet<int>(), children);
+    return res;
+}
+
+HashSet<int> GetChildren(int cur, List<int>[] graph, int[] res, Dictionary<int, HashSet<int>> dict)
+{
+    var children = new HashSet<int> { cur };
+    foreach (var next in graph[cur])
+        children.UnionWith(GetChildren(next, graph, res, dict));
+    dict[cur] = new HashSet<int>(children);
+    res[cur] = dict[cur].Count;
+    return children;
+}
+
+void DFS(int cur, List<int>[] graph, string s, int[] parent, int[] res, List<int>[] moveTo, HashSet<int> moved, Dictionary<int, HashSet<int>> children)
+{ 
+    moveTo[s[cur] - 'a'].Add(cur);
+    foreach (var next in graph[cur])
+    {
+        var to = moveTo[s[next] - 'a'].Count == 0 ? -1 : moveTo[s[next] - 'a'][^1];
+        if (to != -1 && to != parent[next] && moved.Add(next) && children[to].Contains(next))
+        {
+            res[parent[next]] -= children[next].Count;
+
+        }
+        DFS(next, graph, s, parent, res, moveTo, moved, children);
+    }
+    moveTo[s[cur] - 'a'].RemoveAt(moveTo[s[cur] - 'a'].Count - 1);
+}
