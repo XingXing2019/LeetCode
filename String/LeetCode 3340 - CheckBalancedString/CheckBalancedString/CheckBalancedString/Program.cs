d16b71d1f8b22f7661c676de7f66db20cf91cf66
Xing <x.xing.unsw@gmail.com>
2024-11-03 16:26:39 +1100

    init

---
+++ String/LeetCode 3340 - CheckBalancedString/CheckBalancedString/CheckBalancedString/Program.cs
@@ -0,0 +1,10 @@
+﻿bool IsBalanced(string num)
+{
+    var res = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        var digit = num[i] - '0';
+        res += i % 2 == 0 ? digit : -digit;
+    }
+    return res == 0;
+}
