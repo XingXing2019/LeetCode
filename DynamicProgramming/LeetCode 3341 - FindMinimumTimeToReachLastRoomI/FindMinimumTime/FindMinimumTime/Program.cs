e2e264943d3b94e6e0ff7416024ca28c324bec21
Xing <x.xing.unsw@gmail.com>
2024-11-03 16:48:58 +1100

    init

---
+++ DynamicProgramming/LeetCode 3341 - FindMinimumTimeToReachLastRoomI/FindMinimumTime/FindMinimumTime/Program.cs
@@ -0,0 +1,36 @@
+﻿int[][] moveTime = new[]
+{
+    new[] { 0, 4 },
+    new[] { 4, 4 },
+};
+Console.WriteLine(MinTimeToReach(moveTime));
+
+int MinTimeToReach(int[][] moveTime)
+{
+    var dp = new int[moveTime.Length][];
+    for (int i = 0; i < dp.Length; i++)
+    {
+        dp[i] = new int[moveTime[0].Length];
+        Array.Fill(dp[i], int.MaxValue);
+    }
+    var queue = new Queue<int[]>();
+    queue.Enqueue(new []{0, 0, 0});
+    int[] dir = { 1, 0, -1, 0, 1 };
+    while (queue.Count != 0)
+    {
+        var cur = queue.Dequeue();
+        int x = cur[0], y = cur[1], time = cur[2];
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = x + dir[i], newY = y + dir[i + 1];
+            if (newX < 0 || newX >= moveTime.Length || newY < 0 || newY >= moveTime[0].Length)
+                continue;
+            if (dp[newX][newY] > Math.Max(time, moveTime[newX][newY]) + 1)
+            {
+                dp[newX][newY] = Math.Max(time, moveTime[newX][newY]) + 1;
+                queue.Enqueue(new []{newX, newY, Math.Max(time, moveTime[newX][newY]) + 1 });
+            }
+        }
+    }
+    return dp[^1][^1];
+}
