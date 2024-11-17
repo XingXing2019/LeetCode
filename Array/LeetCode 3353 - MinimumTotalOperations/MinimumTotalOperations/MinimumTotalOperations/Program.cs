33a9774f1f8626cf5ee93f24e320bac475388238
Xing <x.xing.unsw@gmail.com>
2024-11-17 19:55:54 +1100

    init

---
+++ Array/LeetCode 3353 - MinimumTotalOperations/MinimumTotalOperations/MinimumTotalOperations/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] nums = { -6, -6, -6, -1 };
+Console.WriteLine(MinOperations(nums));
+int MinOperations(int[] nums)
+{
+    int change = 0, res = 0;
+    for (int i = nums.Length - 2; i >= 0; i--)
+    {
+        if (nums[i] + change != nums[i + 1])
+        {
+            change = nums[i + 1] - nums[i];
+            res++;
+        }
+        nums[i] += change;
+    }
+    return res;
+}
