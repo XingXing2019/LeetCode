93c054e27bbbdb022d6d799038fb47b5623b32a9
Xing <x.xing.unsw@gmail.com>
2024-11-25 17:39:38 +1100

    init

---
+++ String/LeetCode 3365 - RearrangeKSubstringsToFormTargetString/RearrangeKSubstringsToFormTargetString/RearrangeKSubstringsToFormTargetString/Program.cs
@@ -0,0 +1,25 @@
+﻿var s = "aabbcc";
+var t = "bbaacc";
+var k = 2;
+Console.WriteLine(IsPossibleToRearrange(s, t, k));
+
+bool IsPossibleToRearrange(string s, string t, int k)
+{
+    var len = s.Length / k;
+    var words = new Dictionary<string, int>();
+    for (int i = 0; i < s.Length; i += len)
+    {
+        var word = s.Substring(i, len);
+        words[word] = words.GetValueOrDefault(word) + 1;
+    }
+    for (int i = 0; i < t.Length; i += len)
+    {
+        var word = t.Substring(i, len);
+        if (!words.ContainsKey(word))
+            return false;
+        words[word]--;
+        if (words[word] == 0)
+            words.Remove(word);
+    }
+    return true;
+}
