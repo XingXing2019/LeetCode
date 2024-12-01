005b21a24650fad9f2089f9b3f5da7213567259d
Xing <x.xing.unsw@gmail.com>
2024-12-01 19:20:36 +1100

    init

---
+++ HashTable/LeetCode 3371 - IdentifyTheLargestOutlierInAnArray/IdentifyTheLargestOutlierInAnArray/IdentifyTheLargestOutlierInAnArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetLargestOutlier(int[] nums)
+{
+    var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    var sum = nums.Sum();
+    var res = int.MinValue;
+    foreach (var num in nums)
+    {
+        var left = sum - num;
+        if (left % 2 != 0 || !freq.ContainsKey(left / 2)) continue;
+        if (left / 2 == num && freq[num] == 1) continue;
+        res = Math.Max(res, num);
+    }
+    return res;
+}
