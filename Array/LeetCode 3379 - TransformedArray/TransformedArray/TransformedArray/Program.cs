37aad6e361ce3c78938095d557d17a2b989c34e5
Xing <x.xing.unsw@gmail.com>
2024-12-08 21:33:16 +1100

    init

---
+++ Array/LeetCode 3379 - TransformedArray/TransformedArray/TransformedArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] ConstructTransformedArray(int[] nums)
+{
+    var res = new int[nums.Length];
+    for (int i = 0; i < res.Length; i++)
+    {
+        var index = i;
+        var abs = Math.Abs(nums[i]) % nums.Length;
+        if (nums[i] < 0)
+            index = (i - abs + nums.Length) % nums.Length;
+        else if (nums[i] > 0)
+            index = (i + abs) % nums.Length;
+        res[i] = nums[index];
+    }
+    return res;
+}
