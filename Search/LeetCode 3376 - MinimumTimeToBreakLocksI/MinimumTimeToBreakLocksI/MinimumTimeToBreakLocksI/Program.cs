0469148d738547bdecb2c524841b43b814833aee
Xing <x.xing.unsw@gmail.com>
2024-12-08 20:07:42 +1100

    init

---
+++ Search/LeetCode 3376 - MinimumTimeToBreakLocksI/MinimumTimeToBreakLocksI/MinimumTimeToBreakLocksI/Program.cs
@@ -0,0 +1,26 @@
+﻿var strength = new List<int> { 3, 4, 1 };
+var k = 1;
+Console.WriteLine(FindMinimumTime(strength, k));
+
+int FindMinimumTime(IList<int> strength, int K)
+{
+    strength = strength.OrderBy(x => x).ToList();
+    var res = int.MaxValue;
+    DFS(strength, 0, new HashSet<int>(), K, 1, 0, ref res);
+    return res;
+}
+
+void DFS(IList<int> strength, int count, HashSet<int> visited, int k, int x, int time, ref int res)
+{
+    if (count == strength.Count)
+        res = Math.Min(res, time);
+    for (int i = 0; i < strength.Count; i++)
+    {
+        if (visited.Contains(i)) continue;
+        if (i != 0 && strength[i] == strength[i - 1] && !visited.Contains(i - 1)) continue;
+        visited.Add(i);
+        var cost = strength[i] % x == 0 ? strength[i] / x : strength[i] / x + 1;
+        DFS(strength, count + 1, visited, k, x + k, time + cost, ref res);
+        visited.Remove(i);
+    }
+}
