2de43f5c09f1b8940d32fb38ad3d164038ce8566
Xing <x.xing.unsw@gmail.com>
2024-12-08 21:34:18 +1100

    init

--- Array/LeetCode 3379 - TransformedArray/TransformedArray/TransformedArray/Program.cs
+++ Array/LeetCode 3379 - TransformedArray/TransformedArray/TransformedArray/Program.cs
@@ -3,12 +3,11 @@
     var res = new int[nums.Length];
     for (int i = 0; i < res.Length; i++)
     {
-        var index = i;
-        var abs = Math.Abs(nums[i]) % nums.Length;
+        int index = i, len = nums.Length, abs = Math.Abs(nums[i]) % len;
         if (nums[i] < 0)
-            index = (i - abs + nums.Length) % nums.Length;
+            index = (i - abs + len) % len;
         else if (nums[i] > 0)
-            index = (i + abs) % nums.Length;
+            index = (i + abs) % len;
         res[i] = nums[index];
     }
     return res;
