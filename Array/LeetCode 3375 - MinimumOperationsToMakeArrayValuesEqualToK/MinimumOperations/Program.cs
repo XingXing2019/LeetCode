a45d72f2fbdcc1720a0967d70aaf4a8fe50554de
Xing <x.xing.unsw@gmail.com>
2024-12-08 19:20:25 +1100

    init

---
+++ Array/LeetCode 3375 - MinimumOperationsToMakeArrayValuesEqualToK/MinimumOperations/Program.cs
@@ -0,0 +1,14 @@
+﻿int MinOperations(int[] nums, int k)
+{
+    Array.Sort(nums);
+    if (nums[0] < k) return -1;
+    var res = 0;
+    for (int i = nums.Length - 2; i >= 0; i--)
+    {
+        if (nums[i] != nums[i + 1])
+            res++;
+        if (nums[i] < k)
+            return -1;
+    }
+    return nums[0] == k ? res : res + 1;
+}
