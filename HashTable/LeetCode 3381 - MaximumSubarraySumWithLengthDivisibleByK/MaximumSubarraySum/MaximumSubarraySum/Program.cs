f1d9de2edc7e32e044a17c497a6db33c5060139d
Xing <x.xing.unsw@gmail.com>
2024-12-10 21:07:21 +1100

    init

---
+++ HashTable/LeetCode 3381 - MaximumSubarraySumWithLengthDivisibleByK/MaximumSubarraySum/MaximumSubarraySum/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = { -5, 1, 2, -3, 4 };
+var k = 2;
+Console.WriteLine(MaxSubarraySum(nums, k));
+
+long MaxSubarraySum(int[] nums, int k)
+{
+    var max = 1_000_000_000_000_000;
+    var prefix = new long[k];
+    Array.Fill(prefix, max);
+    prefix[^1] = 0;
+    long res = -max, pre = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        pre += nums[i];
+        res = Math.Max(res, pre - prefix[i % k]);
+        prefix[i % k] = Math.Min(prefix[i % k], pre);
+    }
+    return res;
+}
