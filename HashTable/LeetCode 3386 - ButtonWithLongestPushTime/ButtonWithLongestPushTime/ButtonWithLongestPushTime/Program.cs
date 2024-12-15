f2c2555ddf946d93ae9e59e2c1fe0b0b3936c0a7
Xing <x.xing.unsw@gmail.com>
2024-12-15 20:56:41 +1100

    index

---
+++ HashTable/LeetCode 3386 - ButtonWithLongestPushTime/ButtonWithLongestPushTime/ButtonWithLongestPushTime/Program.cs
@@ -0,0 +1,19 @@
+﻿int ButtonWithLongestTime(int[][] events)
+{
+    var dict = new Dictionary<int, int>();
+    int res = events[0][0], max = events[0][1];
+    for (int i = 0; i < events.Length; i++)
+    {
+        var time = i == 0 ? events[i][1] : events[i][1] - events[i - 1][1];
+        var index = events[i][0];
+        dict[index] = Math.Max(dict.GetValueOrDefault(index, 0), time);
+        if (dict[index] > max)
+        {
+            res = index;
+            max = dict[index];
+        }
+        if (dict[index] == max && index < res)
+            res = index;
+    }
+    return res;
+}
