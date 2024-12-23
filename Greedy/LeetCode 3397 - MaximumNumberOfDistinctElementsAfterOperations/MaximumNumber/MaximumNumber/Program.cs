037f03015ed3a66245fc5df54e9cd70cda89ea93
Xing <x.xing.unsw@gmail.com>
2024-12-23 15:50:45 +1100

    init

---
+++ Greedy/LeetCode 3397 - MaximumNumberOfDistinctElementsAfterOperations/MaximumNumber/MaximumNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums = { 6, 7, 7, 7, 8, 10, 10 };
+Console.WriteLine(MaxDistinctElements(nums, 1));
+
+int MaxDistinctElements(int[] nums, int k)
+{
+    Array.Sort(nums);
+    int diff = -k;
+    var set = new List<int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (i != 0 && nums[i] == nums[i - 1])
+            diff++;
+        if (diff <= k)
+        {
+            if (set.Count != 0 && nums[i] + diff - set[^1] > 1)
+                diff--;
+            set.Add(nums[i] + diff);
+        }            
+        else
+            set.Add(nums[i] + k);
+    }
+    return new HashSet<int>(set).Count;
+}
--- HashTable/LeetCode 3396 - MinimumNumberOfOperationsToMakeElementsInArrayDistinct/MinimumNumberOfOperations/MinimumNumberOfOperations/Program.cs
+++ HashTable/LeetCode 3396 - MinimumNumberOfOperationsToMakeElementsInArrayDistinct/MinimumNumberOfOperations/MinimumNumberOfOperations/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MinimumOperations(int[] nums)
+{
+    var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    int res = 0, index = 0;
+    while (freq.Any(x => x.Value != 1))
+    {
+        for (int i = 0; i < 3 && index < nums.Length; i++)
+        {
+            freq[nums[index]]--;
+            if (freq[nums[index]] == 0)
+                freq.Remove(nums[index]);
+            index++;
+        }
+        res++;
+    }
+    return res;
+}
