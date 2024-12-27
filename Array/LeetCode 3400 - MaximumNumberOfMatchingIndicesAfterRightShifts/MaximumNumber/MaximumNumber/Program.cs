439fd77fb03595a66afa0794ab3287b24c4ccfca
Xing <x.xing.unsw@gmail.com>
2024-12-27 18:48:04 +1100

    init

---
+++ Array/LeetCode 3400 - MaximumNumberOfMatchingIndicesAfterRightShifts/MaximumNumber/MaximumNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿int MaximumMatchingIndices(int[] nums1, int[] nums2)
+{
+    var res = 0;
+    for (int i = 0; i < nums1.Length; i++)
+        res = Math.Max(res, Count(nums1, nums2, i));
+    return res;
+}
+
+int Count(int[] nums1, int[] nums2, int start)
+{
+    var res = 0;
+    for (int i = 0; i < nums1.Length; i++)
+    {
+        if (nums1[i] == nums2[(start + i) % nums1.Length])
+            res++;
+    }
+    return res;
+}
