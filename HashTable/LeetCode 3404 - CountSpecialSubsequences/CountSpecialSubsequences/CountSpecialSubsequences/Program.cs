a2d41246d6eadf7188b5574e754dba3f9d4cb43a
Xing <x.xing.unsw@gmail.com>
2024-12-29 17:03:00 +1100

    init

--- HashTable/LeetCode 3404 - CountSpecialSubsequences/CountSpecialSubsequences/CountSpecialSubsequences/Program.cs
+++ HashTable/LeetCode 3404 - CountSpecialSubsequences/CountSpecialSubsequences/CountSpecialSubsequences/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿long NumberOfSubsequences(int[] nums)
+{
+    var freq = new Dictionary<double, long>();
+    var res = 0L;
+    for (int r = 3; r < nums.Length - 2; r++)
+    {
+        var q = r - 2;
+        for (int p = 0; p < q - 1; p++)
+        {
+            var ratio = (double)nums[p] / nums[q];
+            freq[ratio] = freq.GetValueOrDefault(ratio, 0) + 1;
+        }
+        for (int s = r + 2; s < nums.Length; s++)
+        {
+            var ratio = (double)nums[s] / nums[r];
+            res += freq.GetValueOrDefault(ratio, 0);
+        }
+    }
+    return res;
+}
