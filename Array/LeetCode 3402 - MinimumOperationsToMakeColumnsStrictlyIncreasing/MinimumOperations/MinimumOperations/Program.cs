a6d65e55589bca56a15f1631ea261fd1df6140d0
Xing <x.xing.unsw@gmail.com>
2024-12-29 16:20:51 +1100

    init

---
+++ Array/LeetCode 3402 - MinimumOperationsToMakeColumnsStrictlyIncreasing/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,14 @@
+﻿int MinimumOperations(int[][] grid)
+{
+    var res = 0;
+	for (int j = 0; j < grid[0].Length; j++)
+	{
+		for (int i = 1; i < grid.Length; i++)
+		{
+			var diff = Math.Max(0, grid[i - 1][j] - grid[i][j] + 1);
+			grid[i][j] += diff;
+            res += diff;
+		}
+	}
+	return res;
+}
--- Array/LeetCode 689 - MaximumSumOf3NonOverlappingSubarrays/MaximumSum/MaximumSum/Program.cs
+++ Array/LeetCode 689 - MaximumSumOf3NonOverlappingSubarrays/MaximumSum/MaximumSum/Program.cs
@@ -7,8 +7,7 @@ int[] MaxSumOfThreeSubarrays(int[] nums, int k)
     var prefix = new int[nums.Length];
     for (int i = 0; i < nums.Length; i++)
         prefix[i] = i == 0 ? nums[i] : nums[i] + prefix[i - 1];
-    var leftMax = new int[nums.Length];
-    var leftIndex = new int[nums.Length];
+    int[] leftMax = new int[nums.Length], leftIndex = new int[nums.Length];
     int max = 0, index = 0;
     for (int i = 0; i <= nums.Length - k; i++)
     {
@@ -21,8 +20,7 @@ int[] MaxSumOfThreeSubarrays(int[] nums, int k)
         leftMax[i] = max;
         leftIndex[i] = index;
     }
-    var rightMax = new int[nums.Length];
-    var rightIndex = new int[nums.Length];
+    int[] rightMax = new int[nums.Length], rightIndex = new int[nums.Length];
     max = 0; index = nums.Length - 1;
     for (int i = nums.Length - k; i >= 0; i--)
     {
@@ -37,7 +35,7 @@ int[] MaxSumOfThreeSubarrays(int[] nums, int k)
     }
     max = 0;
     var res = new int[3];
-    for (int i = k; i < nums.Length - 2 * k; i++)
+    for (int i = k; i <= nums.Length - 2 * k; i++)
     {
         var sum = prefix[i + k - 1] - prefix[i - 1];
         if (leftMax[i - k] + sum + rightMax[i + k] > max)
