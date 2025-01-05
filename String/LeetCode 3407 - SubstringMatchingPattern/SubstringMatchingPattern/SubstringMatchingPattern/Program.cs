2b2107f1a88f55ff83d4b03c3fcbea09b4fb0ca6
Xing <x.xing.unsw@gmail.com>
2025-01-05 20:34:35 +1100

    init

---
+++ String/LeetCode 3407 - SubstringMatchingPattern/SubstringMatchingPattern/SubstringMatchingPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿bool HasMatch(string s, string p)
+{
+    var parts = p.Split('*', StringSplitOptions.RemoveEmptyEntries);
+    if (parts.Length == 0)
+        return true;
+    if (parts.Length == 1)
+        return s.Contains(parts[0]);
+    for (int i = 1; i <= s.Length; i++)
+    {
+        var front = s.Substring(0, i);
+        var back = s.Substring(i);
+        if (front.Contains(parts[0]) && back.Contains(parts[1]))
+            return true;
+    }
+    return false;
+}
