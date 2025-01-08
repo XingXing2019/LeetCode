4c6c8ae1926a78381811418f9846d31b6f02c0ce
Xing <x.xing.unsw@gmail.com>
2025-01-08 18:21:44 +1100

    init

---
+++ HashTable/LeetCode 3412 - FindMirrorScoreOfAString/FindMirrorScoreOfAString/FindMirrorScoreOfAString/Program.cs
@@ -0,0 +1,19 @@
+﻿var s = "aczzx";
+Console.WriteLine(CalculateScore(s));
+
+long CalculateScore(string s)
+{
+    var dict = new Dictionary<char, Stack<int>>();
+	var res = 0L;
+	for (int i = 0; i < s.Length; i++)
+	{
+        if (!dict.ContainsKey(s[i]))
+            dict[s[i]] = new Stack<int>();
+        var mirror = (char)(25 - s[i] + 'a' + 'a');
+		if (dict.ContainsKey(mirror) && dict[mirror].Count != 0)
+			res += i - dict[mirror].Pop();
+		else         
+            dict[s[i]].Push(i);
+    }
+	return res;
+}
