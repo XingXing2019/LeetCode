b33dff4e73bd34158c26666b36c2bc5f15dafb0c
Xing <x.xing.unsw@gmail.com>
2025-01-13 19:34:18 +1100

    init

---
+++ Array/LeetCode 3417 - ZigzagGridTraversalWithSkip/ZigzagGridTraversalWithSkip/ZigzagGridTraversalWithSkip/Program.cs
@@ -0,0 +1,18 @@
+﻿IList<int> ZigzagTraversal(int[][] grid)
+{
+    var res = new List<int>();
+	var skip = false;
+	for (int i = 0; i < grid.Length; i++)
+	{
+		for (int j = 0; j < grid[0].Length; j++)
+		{
+			if (!skip)
+			{
+				var num = i % 2 == 0 ? grid[i][j] : grid[i][^(j + 1)];
+                res.Add(num);
+            }
+			skip = !skip;
+		}
+	}
+	return res;
+}
