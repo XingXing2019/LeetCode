fc24a880ee36bebf1488df21add325fa8ff8e580
Xing <x.xing.unsw@gmail.com>
2025-01-19 11:50:47 +1100

    init

---
+++ Sort/LeetCode 3424 - MinimumCostToMakeArraysIdentical/MinimumCostToMakeArraysIdentical/MinimumCostToMakeArraysIdentical/Program.cs
@@ -0,0 +1,12 @@
+﻿long MinCost(int[] arr, int[] brr, long k)
+{
+    var sortedA = arr.OrderBy(x => x).ToArray();
+    var sortedB = brr.OrderBy(x => x).ToArray();
+    long res = 0L, diff = 0L;
+    for (int i = 0; i < sortedA.Length; i++)
+    {
+        res += Math.Abs(sortedA[i] - sortedB[i]);
+        diff += Math.Abs(arr[i] - brr[i]);
+    }
+    return res == diff ? res : Math.Min(res + k, diff);
+}
