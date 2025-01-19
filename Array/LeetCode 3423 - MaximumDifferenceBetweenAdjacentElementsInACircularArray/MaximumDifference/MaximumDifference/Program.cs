e68789e25db7a2f50d15dcd9163914a7c5f11a96
Xing <x.xing.unsw@gmail.com>
2025-01-19 11:43:25 +1100

    init

---
+++ Array/LeetCode 3423 - MaximumDifferenceBetweenAdjacentElementsInACircularArray/MaximumDifference/MaximumDifference/Program.cs
@@ -0,0 +1,7 @@
+﻿int MaxAdjacentDistance(int[] nums)
+{
+    var res = Math.Abs(nums[0] - nums[^1]);
+    for (int i = 1; i < nums.Length; i++)
+        res = Math.Max(res, Math.Abs(nums[i] - nums[i - 1]));
+    return res;
+}
