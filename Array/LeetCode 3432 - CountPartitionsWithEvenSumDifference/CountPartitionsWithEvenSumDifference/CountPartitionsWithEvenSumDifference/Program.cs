99525422e64b2c970f4ad9752fd7c7fee6ea413f
Xing <x.xing.unsw@gmail.com>
2025-01-26 21:54:03 +1100

    init

---
+++ Array/LeetCode 3432 - CountPartitionsWithEvenSumDifference/CountPartitionsWithEvenSumDifference/CountPartitionsWithEvenSumDifference/Program.cs
@@ -0,0 +1,13 @@
+﻿int CountPartitions(int[] nums)
+{
+    for (int i = 1; i < nums.Length; i++)
+        nums[i] += nums[i - 1];
+    var res = 0;
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        var diff = nums[i] - (nums[^1] - nums[i]);
+        if (diff % 2 == 0)
+            res++;
+    }
+    return res;
+}
