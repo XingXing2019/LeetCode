ad54b75b2a81bc4df9ac49eaf69692f07629148e
Xing <x.xing.unsw@gmail.com>
2025-01-26 22:38:13 +1100

    init

--- Array/LeetCode 3434 - MaximumFrequencyAfterSubarrayOperation/MaximumFrequency/MaximumFrequency/Program.cs
+++ Array/LeetCode 3434 - MaximumFrequencyAfterSubarrayOperation/MaximumFrequency/MaximumFrequency/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MaxFrequency(int[] nums, int k)
+{
+    var freq = new int[51];
+    int res = 0, count = 0;
+    foreach (var num in nums)
+    {
+        freq[num] = Math.Max(freq[num], count) + 1;
+        res += num == k ? 1 : 0;
+        count += num == k ? 1 : 0;
+        res = Math.Max(res, freq[num]);
+    }
+    return res;
+}
