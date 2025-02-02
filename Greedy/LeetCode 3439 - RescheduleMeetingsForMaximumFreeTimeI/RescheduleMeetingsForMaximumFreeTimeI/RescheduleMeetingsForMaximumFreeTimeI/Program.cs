48360ca659b88ff2501da0a77d94017b272d7294
Xing <x.xing.unsw@gmail.com>
2025-02-02 20:56:55 +1100

    init

---
+++ Greedy/LeetCode 3439 - RescheduleMeetingsForMaximumFreeTimeI/RescheduleMeetingsForMaximumFreeTimeI/RescheduleMeetingsForMaximumFreeTimeI/Program.cs
@@ -0,0 +1,24 @@
+﻿int eventTime = 5, k = 2;
+int[] startTime = { 0, 1, 2, 3, 4 }, endTime = { 1, 2, 3, 4, 5 };
+Console.WriteLine(MaxFreeTime(eventTime, k, startTime, endTime));
+int MaxFreeTime(int eventTime, int k, int[] startTime, int[] endTime)
+{
+    var meetings = new int[startTime.Length][];
+    for (int i = 0; i < meetings.Length; i++)
+        meetings[i] = new[] { startTime[i], endTime[i] };
+    Array.Sort(meetings, (a, b) => a[0] - b[0]);
+    var res = meetings[0][0];
+    var hi = 0;
+    for (int i = 0; i < meetings.Length; i++)
+    {
+        res = Math.Max(res, meetings[i][0] - hi);
+        if (k != 0 && meetings[i][0] != hi)
+        {
+            k--;
+            hi += meetings[i][1] - meetings[i][0];
+        }
+        else
+            hi = meetings[i][1];
+    }
+    return Math.Max(res, eventTime - hi);
+}
---
+++ HashTable/LeetCode 3438 - FindValidPairOfAdjacentDigitsInString/FindValidPairOfAdjacentDigitsInString/FindValidPairOfAdjacentDigitsInString/Program.cs
@@ -0,0 +1,16 @@
+﻿var s = "2523533";
+Console.WriteLine(FindValidPair(s));
+
+string FindValidPair(string s)
+{
+    var freq = new int[10];
+    foreach (var l in s)
+        freq[l - '0']++;
+    for (int i = 1; i < s.Length; i++)
+    {
+        if (s[i] == s[i - 1] || freq[s[i] - '0'] != s[i] - '0' || freq[s[i - 1] - '0'] != s[i - 1] - '0')
+            continue;
+        return $"{s[i - 1]}{s[i]}";
+    }
+    return "";
+}
