9ecbde285c71438765ec5b90cf624949673f313c
Xing <x.xing.unsw@gmail.com>
2025-02-02 21:11:59 +1100

    init

--- HashTable/LeetCode 3442 - MaximumDifferenceBetweenEvenAndOddFrequencyI/MaximumDifference/MaximumDifference/Program.cs
+++ HashTable/LeetCode 3442 - MaximumDifferenceBetweenEvenAndOddFrequencyI/MaximumDifference/MaximumDifference/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MaxDifference(string s)
+{
+    var freq = s.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    var odds = freq.Where(x => x.Value % 2 != 0).Select(x => x.Value).OrderBy(x => x).ToList();
+    var evens = freq.Where(x => x.Value % 2 == 0).Select(x => x.Value).OrderBy(x => x).ToList();
+    return Math.Max(odds[0] - evens[^1], odds[^1] - evens[0]);
+}
---
+++ String/LeetCode 3443 - MaximumManhattanDistanceAfterKChanges/MaximumManhattanDistanceAfterKChanges/MaximumManhattanDistanceAfterKChanges/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+var s = "NWSE";
+var k = 1;
+Console.WriteLine(MaxDistance(s, k));
+
+int MaxDistance(string s, int k)
+{
+    string[] dirs = { "NE", "NW", "SE", "SW" };
+    var res = 0;
+    foreach (var dir in dirs)
+    {
+        var copy = new StringBuilder(s);
+        var count = k;
+        for (int i = 0; i < copy.Length; i++)
+        {
+            if (!dir.Contains(copy[i]) && count != 0)
+            {
+                count--;
+                copy[i] = dir[0];
+            }
+        }
+        res = Math.Max(res, GetDistance(copy));
+    }
+    return res;
+}
+
+int GetDistance(StringBuilder s)
+{
+    int x = 0, y = 0;
+    foreach (var l in s.ToString())
+    {
+        if (l == 'N')
+            x--;
+        else if (l == 'S')
+            x++;
+        else if (l == 'W')
+            y--;
+        else
+            y++;
+    }
+    return Math.Abs(x) + Math.Abs(y);
+}
