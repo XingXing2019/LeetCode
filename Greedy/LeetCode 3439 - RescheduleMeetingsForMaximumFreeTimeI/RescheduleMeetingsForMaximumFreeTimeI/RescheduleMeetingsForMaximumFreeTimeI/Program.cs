62c2084a4a2283966dca185975e1075887f2b6f9
Xing <x.xing.unsw@gmail.com>
2025-02-03 21:57:07 +1100

    init

--- Greedy/LeetCode 3439 - RescheduleMeetingsForMaximumFreeTimeI/RescheduleMeetingsForMaximumFreeTimeI/RescheduleMeetingsForMaximumFreeTimeI/Program.cs
+++ Greedy/LeetCode 3439 - RescheduleMeetingsForMaximumFreeTimeI/RescheduleMeetingsForMaximumFreeTimeI/RescheduleMeetingsForMaximumFreeTimeI/Program.cs
@@ -1,24 +1,24 @@
-﻿int eventTime = 5, k = 2;
-int[] startTime = { 0, 1, 2, 3, 4 }, endTime = { 1, 2, 3, 4, 5 };
+﻿int eventTime = 83, k = 1;
+int[] startTime = { 13, 15, 43, 81 }, endTime = { 15, 22, 78, 83 };
 Console.WriteLine(MaxFreeTime(eventTime, k, startTime, endTime));
 int MaxFreeTime(int eventTime, int k, int[] startTime, int[] endTime)
 {
-    var meetings = new int[startTime.Length][];
-    for (int i = 0; i < meetings.Length; i++)
-        meetings[i] = new[] { startTime[i], endTime[i] };
-    Array.Sort(meetings, (a, b) => a[0] - b[0]);
-    var res = meetings[0][0];
-    var hi = 0;
-    for (int i = 0; i < meetings.Length; i++)
+    var meetings = new List<int[]> { new[] { 0, 0 }, new[] { eventTime, eventTime } };
+    for (int i = 0; i < startTime.Length; i++)
+        meetings.Add(new[] { startTime[i], endTime[i] });
+    meetings.Sort((a, b) => a[0] - b[0]);
+    var gaps = new List<int>();
+    for (int i = 1; i < meetings.Count; i++)
+        gaps.Add(meetings[i][0] - meetings[i - 1][1]);
+    var free = 0;
+    for (int i = 0; i < Math.Min(k + 1, gaps.Count); i++)
+        free += gaps[i];
+    var res = free;
+    for (int i = k + 1; i < gaps.Count; i++)
     {
-        res = Math.Max(res, meetings[i][0] - hi);
-        if (k != 0 && meetings[i][0] != hi)
-        {
-            k--;
-            hi += meetings[i][1] - meetings[i][0];
-        }
-        else
-            hi = meetings[i][1];
+        free += gaps[i];
+        free -= gaps[i - k - 1];
+        res = Math.Max(res, free);
     }
-    return Math.Max(res, eventTime - hi);
+    return res;
 }
