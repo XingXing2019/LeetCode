66b888a35d0b74c28f6b6061f4d647e1a9fe3160
Xing <x.xing.unsw@gmail.com>
2025-02-09 16:32:40 +1100

    init

---
+++ Array/LeetCode 3446 - SortMatrixByDiagonals/SortMatrixByDiagonals/SortMatrixByDiagonals/Program.cs
@@ -0,0 +1,55 @@
+﻿int[][] grid = new int[][]
+{
+    new[] { 1, 7, 3 },
+    new[] { 9, 8, 2 },
+    new[] { 4, 5, 6 },
+};
+Console.WriteLine(SortMatrix(grid));
+
+int[][] SortMatrix(int[][] grid)
+{
+    var n = grid.Length;
+    for (int i = 0; i < n; i++)
+    {
+        int x = i, y = 0;
+        var nums = new List<int>();
+        for (int j = 0; x < n && y < n; j++)
+        {
+            nums.Add(grid[x][y]);
+            x++;
+            y++;
+        }
+        nums.Sort((a, b) => b - a);
+        x = i;
+        y = 0;
+        var index = 0;
+        for (int j = 0; x < n && y < n; j++)
+        {
+            grid[x][y] = nums[index++];
+            x++;
+            y++;
+        }
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int x = 0, y = i;
+        var nums = new List<int>();
+        for (int j = 0; x < n && y < n; j++)
+        {
+            nums.Add(grid[x][y]);
+            x++;
+            y++;
+        }
+        nums.Sort();
+        x = 0;
+        y = i;
+        var index = 0;
+        for (int j = 0; x < n && y < n; j++)
+        {
+            grid[x][y] = nums[index++];
+            x++;
+            y++;
+        }
+    }
+    return grid;
+}
--- Backtracking/LeetCode 3437 - PermutationsIII/PermutationsIII/PermutationsIII/Program.cs
+++ Backtracking/LeetCode 3437 - PermutationsIII/PermutationsIII/PermutationsIII/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[][] Permute(int n)
+{
+    var res = new List<int[]>();
+    DFS(n, 0, new HashSet<int>(), res);
+    return res.ToArray();
+}
+
+void DFS(int n, int last, HashSet<int> nums, List<int[]> res)
+{
+    if (nums.Count == n)
+    {
+        res.Add(new List<int>(nums).ToArray());
+        return;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (nums.Contains(i) || last != 0 && i % 2 == last % 2)
+            continue;
+        nums.Add(i);
+        DFS(n, i, nums, res);
+        nums.Remove(i);
+    }
+}
