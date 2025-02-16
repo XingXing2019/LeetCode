84b6d9d14c8f95319732f2e8d34cc7a48896adfe
Xing <x.xing.unsw@gmail.com>
2025-02-16 20:40:59 +1100

    init

---
+++ Array/LeetCode 3452 - SumOfGoodNumbers/SumOfGoodNumbers/SumOfGoodNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumOfGoodNumbers(int[] nums, int k)
+{
+    var res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var prev = i - k >= 0 ? nums[i - k] : int.MinValue;
+        var post = i + k < nums.Length ? nums[i + k] : int.MinValue;
+        if (nums[i] > prev && nums[i] > post)
+            res += nums[i];
+    }
+    return res;
+}
