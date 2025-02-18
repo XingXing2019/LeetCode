33abaced384b14fcc35bcc4e02ca303ba38e7bd1
Xing <x.xing.unsw@gmail.com>
2025-02-18 21:49:52 +1100

    init

---
+++ Greedy/LeetCode 3457 - EatPizzas/EatPizzas/EatPizzas/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] pizzas = { 5, 2, 2, 4, 3, 3, 1, 3, 2, 5, 4, 2 };
+Console.WriteLine(MaxWeight(pizzas));
+
+long MaxWeight(int[] pizzas)
+{
+    Array.Sort(pizzas);
+    int li = 0, hi = pizzas.Length - 1;
+    int days = pizzas.Length / 4, even = days / 2, odd = days - even;
+    var res = 0L;
+    for (int i = 0; i < odd; i++)
+    {
+        res += pizzas[hi--];
+        li += 3;
+    }
+    for (int i = 0; i < even; i++)
+    {
+        res += pizzas[--hi];
+        hi--;
+        li += 2;
+    }
+    return res;
+}
