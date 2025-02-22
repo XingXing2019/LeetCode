590d16b8ae5ae25d704a278bcb3a2d08b0f3798f
Xing <x.xing.unsw@gmail.com>
2025-02-23 09:01:20 +1100

    init

---
+++ String/LeetCode 3460 - LongestCommonPrefixAfterAtMostOneRemoval/LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -0,0 +1,18 @@
+﻿int LongestCommonPrefix(string s, string t)
+{
+    int p1 = 0, p2 = 0, chance = 1, res = 0;
+    while (p1 < s.Length && p2 < t.Length)
+    {
+        if (s[p1] == t[p2])
+        {
+            res++;
+            p2++;
+        }
+        else if (chance != 0)
+            chance--;
+        else
+            return res;
+        p1++;
+    }
+    return res;
+}
