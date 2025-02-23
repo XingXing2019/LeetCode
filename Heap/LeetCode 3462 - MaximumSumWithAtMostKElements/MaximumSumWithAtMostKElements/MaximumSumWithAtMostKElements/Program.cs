5c48a0d2c773da66608494cc84f856e9ce2b1a03
Xing <x.xing.unsw@gmail.com>
2025-02-23 18:00:36 +1100

    init

---
+++ Heap/LeetCode 3462 - MaximumSumWithAtMostKElements/MaximumSumWithAtMostKElements/MaximumSumWithAtMostKElements/Program.cs
@@ -0,0 +1,18 @@
+﻿long MaxSum(int[][] grid, int[] limits, int k)
+{
+    var heap = new PriorityQueue<int, int>();
+    for (var i = 0; i < grid.Length; i++)
+    {
+        Array.Sort(grid[i], (a, b) => b - a);
+        for (int j = 0; j < limits[i]; j++)
+        {
+            heap.Enqueue(grid[i][j], grid[i][j]);
+            if (heap.Count > k)
+                heap.Dequeue();
+        }
+    }
+    var res = 0L;
+    while (heap.Count != 0)
+        res += heap.Dequeue();
+    return res;
+}
---
+++ String/LeetCode 3461 - CheckIfDigitsAreEqualInStringAfterOperationsI/CheckIfDigitsAreEqualInStringAfterOperationsI/CheckIfDigitsAreEqualInStringAfterOperationsI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+bool HasSameDigits(string s)
+{
+    while (s.Length > 2)
+    {
+        var temp = new StringBuilder();
+        for (int i = 0; i < s.Length - 1; i++)
+            temp.Append((s[i] - '0' + s[i + 1] - '0') % 10);
+        s = temp.ToString();
+    }
+    return s[0] == s[1];
+}
