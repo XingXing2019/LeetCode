9d5130a0f49f29b728d0f6ac3f1abce0463f66b8
Xing <x.xing.unsw@gmail.com>
2025-03-12 21:46:59 +1100

    init

---
+++ Array/LeetCode 3467 - TransformArrayByParity/TransformArrayByParity/TransformArrayByParity/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] TransformArray(int[] nums)
+{
+    var res = new int[nums.Length];
+    var index = res.Length - 1;
+    foreach (var num in nums)
+    {
+        if (num % 2 == 0) continue;
+        res[index--] = 1;
+    }
+    return res;
+}
---
+++ Array/LeetCode 3468 - FindTheNumberOfCopyArrays/FindTheNumberOfCopyArrays/FindTheNumberOfCopyArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿int CountArrays(int[] original, int[][] bounds)
+{
+    int li = bounds[0][0], hi = bounds[0][1];
+    for (int i = 1; i < original.Length; i++)
+    {
+        var diff = original[i] - original[i - 1];
+        li += diff;
+        hi += diff;
+        if (hi < bounds[i][0] || li > bounds[i][1])
+            return 0;
+        li = Math.Max(li, bounds[i][0]);
+        hi = Math.Min(hi, bounds[i][1]);
+    }
+    return hi - li + 1;
+}
