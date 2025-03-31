fb41895eb18bb763132a8770010e61d8ea83e41d
Xing <x.xing.unsw@gmail.com>
2025-03-31 20:36:40 +1100

    init

---
+++ Array/LeetCode 3502 - MinimumCostToReachEveryPosition/MinimumCostToReachEveryPosition/MinimumCostToReachEveryPosition/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] MinCosts(int[] cost)
+{
+    var min = int.MaxValue;
+    var res = new int[cost.Length];
+    for (int i = 0; i < cost.Length; i++)
+    {
+        res[i] = Math.Min(min, cost[i]);
+        min = Math.Min(min, cost[i]);
+    }
+    return res;
+}
