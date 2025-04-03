e54239cb1ad7fbd485b6ec24824744a801ec8f93
Xing <x.xing.unsw@gmail.com>
2025-04-03 21:19:52 +1100

    init

---
+++ Greedy/LeetCode 3494 - FindTheMinimumAmountOfTimeToBrewPotions/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] parents;
+int[] rank;
+
+int NumberOfComponents(int[][] properties, int k)
+{
+    parents = new int[properties.Length];
+    for (int i = 0; i < parents.Length; i++)
+        parents[i] = i;
+    var res = properties.Length;
+    for (int i = 0; i < properties.Length; i++)
+    {
+        for (int j = i; j < properties.Length; j++)
+        {
+            if (Intersect(new HashSet<int>(properties[i]), new HashSet<int>(properties[j]), k) && Union(i, j))
+                res--;
+        }
+    }
+    return res;
+}
+
+int Find(int x)
+{
+    if (parents[x] != x)
+        parents[x] = Find(parents[x]);
+    return parents[x];
+}
+
+bool Union(int x, int y)
+{
+    int rootX = Find(x), rootY = Find(y);
+    if (rootX == rootY)
+        return false;
+    if (rank[rootX] >= rank[rootY])
+    {
+        parents[rootY] = rootX;
+        if (rank[rootX] == rank[rootY])
+            rank[rootX]++;
+    }
+    else
+        parents[rootX] = rootY;
+    return true;
+}
+
+bool Intersect(HashSet<int> a, HashSet<int> b, int k)
+{
+    return a.Count(b.Contains) >= k;
+}
