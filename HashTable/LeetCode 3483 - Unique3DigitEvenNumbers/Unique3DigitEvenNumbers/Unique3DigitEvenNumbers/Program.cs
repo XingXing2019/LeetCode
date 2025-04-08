be84812fe6fb1c1eab0246e8f4ea0d472b91ef08
Xing <x.xing.unsw@gmail.com>
2025-04-08 18:02:42 +1000

    init

---
+++ HashTable/LeetCode 3483 - Unique3DigitEvenNumbers/Unique3DigitEvenNumbers/Unique3DigitEvenNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int TotalNumbers(int[] digits)
+{
+    var set = new HashSet<int>();
+    for (int i = 0; i < digits.Length; i++)
+    {
+        for (int j = 0; j < digits.Length; j++)
+        {
+            if (i == j) continue;
+            for (int k = 0; k < digits.Length; k++)
+            {
+                if (j == k) continue;
+                var num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                set.Add(num);
+            }
+        }
+    }
+    return set.Count;
+}
