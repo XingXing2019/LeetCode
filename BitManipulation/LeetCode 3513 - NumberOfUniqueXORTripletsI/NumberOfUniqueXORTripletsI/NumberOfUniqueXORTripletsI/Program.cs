e0dcccfe54ae1d0a076e1549a445483d28b511f3
Xing <x.xing.unsw@gmail.com>
2025-04-15 20:04:05 +1000

    init

---
+++ BitManipulation/LeetCode 3513 - NumberOfUniqueXORTripletsI/NumberOfUniqueXORTripletsI/NumberOfUniqueXORTripletsI/Program.cs
@@ -0,0 +1,18 @@
+﻿int UniqueXorTriplets(int[] nums)
+{
+    var n = nums.Length;
+    if (n < 3) return n;
+    var msb = GetMsb(n);
+    return (int)Math.Pow(2, (double)msb + 1);
+}
+
+int GetMsb(int n)
+{
+    var res = 0;
+    while (n != 0)
+    {
+        res++;
+        n >>= 1;
+    }
+    return res - 1;
+}
