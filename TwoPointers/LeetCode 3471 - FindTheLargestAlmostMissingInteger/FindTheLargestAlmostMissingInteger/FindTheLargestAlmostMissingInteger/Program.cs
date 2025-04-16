9d4bc4fa8d8787735cc50cec0c3f38c3bd55161f
Xing <x.xing.unsw@gmail.com>
2025-04-16 21:36:47 +1000

    init

---
+++ TwoPointers/LeetCode 3471 - FindTheLargestAlmostMissingInteger/FindTheLargestAlmostMissingInteger/FindTheLargestAlmostMissingInteger/Program.cs
@@ -0,0 +1,29 @@
+﻿int LargestInteger(int[] nums, int k)
+{
+    var res = -1;
+    var set = new HashSet<int>(nums);
+    foreach (var num in set)
+    {
+        var count = CountAppears(nums, num, k);
+        if (count == 1 && num > res)
+            res = num;
+    }
+    return res;
+}
+
+int CountAppears(int[] nums, int num, int k)
+{
+    int freq = 0, res = 0;
+    for (int i = 0; i < k; i++)
+    {
+        if (nums[i] != num) continue;
+        freq++;
+    }
+    for (int i = k; i < nums.Length; i++)
+    {
+        if (freq != 0) res++;
+        if (nums[i] == num) freq++;
+        if (nums[i - k] == num) freq--;
+    }
+    return freq == 0 ? res : res + 1;
+}
