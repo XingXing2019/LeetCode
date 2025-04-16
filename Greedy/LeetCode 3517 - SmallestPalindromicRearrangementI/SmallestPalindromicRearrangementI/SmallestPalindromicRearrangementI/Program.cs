9fe490a04de94f458264f3ce182c8a51e56fe180
Xing <x.xing.unsw@gmail.com>
2025-04-16 21:23:01 +1000

    init

---
+++ Greedy/LeetCode 3517 - SmallestPalindromicRearrangementI/SmallestPalindromicRearrangementI/SmallestPalindromicRearrangementI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+string SmallestPalindrome(string s)
+{
+    var freq = new int[26];
+    for (int i = 0; i < s.Length; i++)
+        freq[s[i] - 'a']++;
+    var front = new List<string>();
+    var back = new List<string>();
+    var mid = "";
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] % 2 != 0)
+            mid = ((char)(i + 'a')).ToString();
+        var count = freq[i] / 2;
+        var str = new string((char)(i + 'a'), count);
+        front.Add(str);
+        back.Add(str);
+    }
+    var res = new StringBuilder();
+    for (int i = 0; i < front.Count; i++)
+        res.Append(front[i]);
+    res.Append(mid);
+    for (int i = back.Count - 1; i >= 0; i--)
+        res.Append(back[i]);
+    return res.ToString();
+}
--- Math/LeetCode 3516 - FindClosestPerson/FindClosestPerson/FindClosestPerson/Program.cs
+++ Math/LeetCode 3516 - FindClosestPerson/FindClosestPerson/FindClosestPerson/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int FindClosest(int x, int y, int z)
+{
+    int dis1 = Math.Abs(x - z), dis2 = Math.Abs(y - z);
+    return dis1 == dis2 ? 0 : dis1 < dis2 ? 1 : 2;
+}
