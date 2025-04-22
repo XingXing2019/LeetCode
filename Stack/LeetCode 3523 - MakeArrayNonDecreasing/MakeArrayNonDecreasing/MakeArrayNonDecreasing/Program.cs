3b4d21af9f52c82b6c6fa9e15f4637ff908cc53a
Xing <x.xing.unsw@gmail.com>
2025-04-22 21:28:44 +1000

    init

---
+++ Stack/LeetCode 3523 - MakeArrayNonDecreasing/MakeArrayNonDecreasing/MakeArrayNonDecreasing/Program.cs
@@ -0,0 +1,10 @@
+﻿int MaximumPossibleSize(int[] nums)
+{
+    var stack = new Stack<int>();
+    foreach (var num in nums)
+    {
+        if (stack.Count != 0 && num < stack.Peek()) continue;
+        stack.Push(num);
+    }
+    return stack.Count;
+}
