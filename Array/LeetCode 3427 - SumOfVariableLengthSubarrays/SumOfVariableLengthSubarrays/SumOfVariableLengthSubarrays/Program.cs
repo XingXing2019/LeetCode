fe8aff6a729b33e170c4fbdefa4bc496538790b0
Xing <x.xing.unsw@gmail.com>
2025-04-23 21:25:08 +1000

    init

---
+++ Array/LeetCode 3427 - SumOfVariableLengthSubarrays/SumOfVariableLengthSubarrays/SumOfVariableLengthSubarrays/Program.cs
@@ -0,0 +1,13 @@
+﻿int SubarraySum(int[] nums)
+{
+    var prefix = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+        prefix[i] = i == 0 ? nums[i] : nums[i] + prefix[i - 1];
+    var res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var start = Math.Max(0, i - nums[i]);
+        res += start == 0 ? prefix[i] : prefix[i] - prefix[start - 1];
+    }
+    return res;
+}
