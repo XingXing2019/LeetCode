81c88d3c11ba3b375f2179f00bf6b54fb49d995d
Xing <x.xing.unsw@gmail.com>
2025-04-28 17:35:45 +1000

    init

---
+++ HashTable/LeetCode 3527 - FindTheMostCommonResponse/FindTheMostCommonResponse/FindTheMostCommonResponse/Program.cs
@@ -0,0 +1,23 @@
+﻿string FindCommonResponse(IList<IList<string>> responses)
+{
+    var dict = new Dictionary<string, int>();
+    var res = "";
+    var freq = 0;
+    foreach (var response in responses)
+    {
+        var set = new HashSet<string>(response);
+        foreach (var r in set)
+        {
+            if (!dict.ContainsKey(r))
+                dict[r] = 0;
+            dict[r]++;
+            if (dict[r] >= freq)
+            {
+                if (dict[r] > freq || res.CompareTo(r) > 0)
+                    res = r;
+                freq = dict[r];
+            }
+        }
+    }
+    return res;
+}
