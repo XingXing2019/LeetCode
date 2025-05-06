e205ef46290e506d5720c407033d7c93089796f7
Xing <x.xing.unsw@gmail.com>
2025-05-06 19:51:50 +1000

    init

---
+++ Math/LeetCode 3536 - MaximumProductOfTwoDigits/MaximumProductOfTwoDigits/MaximumProductOfTwoDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿int MaxProduct(int n)
+{
+    var digits = new List<int>();
+    while (n != 0)
+    {
+        digits.Add(n % 10);
+        n /= 10;
+    }
+    int max = 0, secMax = 0;
+    foreach (var digit in digits)
+    {
+        if (digit > max)
+        {
+            secMax = max;
+            max = digit;
+        }
+        else if (digit > secMax)
+            secMax = digit;
+    }
+    return max * secMax;
+}
