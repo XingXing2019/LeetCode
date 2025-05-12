12c49b02c2bc773ada9e7a9e636df4c4e47b51b2
Xing <x.xing.unsw@gmail.com>
2025-05-12 19:34:09 +1000

    init

---
+++ HashTable/LeetCode 3541 - FindMostFrequentVowelAndConsonant/FindMostFrequentVowelAndConsonant/FindMostFrequentVowelAndConsonant/Program.cs
@@ -0,0 +1,19 @@
+﻿var s = "successes";
+Console.WriteLine(MaxFreqSum(s));
+
+int MaxFreqSum(string s)
+{
+    var freq = new int[26];
+    foreach (var l in s)
+        freq[l - 'a']++;
+    var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+    int vowel = 0, consonant = 0;
+    for (int i = 0; i < freq.Length; i++)
+    {
+        if (vowels.Contains((char)(i + 'a')))
+            vowel = Math.Max(vowel, freq[i]);
+        else
+            consonant = Math.Max(consonant, freq[i]);
+    }
+    return vowel + consonant;
+}
