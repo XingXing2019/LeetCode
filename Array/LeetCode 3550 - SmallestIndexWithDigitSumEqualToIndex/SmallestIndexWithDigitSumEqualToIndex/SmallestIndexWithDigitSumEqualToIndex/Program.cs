051ab2eef9bf95282d2f5fcb2c93461917749323
Xing <x.xing.unsw@gmail.com>
2025-05-19 19:17:19 +1000

    init

---
+++ Array/LeetCode 3550 - SmallestIndexWithDigitSumEqualToIndex/SmallestIndexWithDigitSumEqualToIndex/SmallestIndexWithDigitSumEqualToIndex/Program.cs
@@ -0,0 +1,15 @@
+﻿int SmallestIndex(int[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int copy = nums[i], sum = 0;
+        while (copy != 0)
+        {
+            sum += copy % 10;
+            copy /= 10;
+        }
+        if (sum == i)
+            return i;
+    }
+    return -1;
+}
