5dfbee574be641934749d22aca5abc2c2bb8f73d
Xing <x.xing.unsw@gmail.com>
2025-05-27 20:12:33 +1000

    init

---
+++ Stack/LeetCode 3561 - ResultingStringAfterAdjacentRemovals/ResultingStringAfterAdjacentRemovals/ResultingStringAfterAdjacentRemovals/Program.cs
@@ -0,0 +1,15 @@
+﻿var s = "zadb";
+Console.WriteLine(ResultingString(s));
+
+string ResultingString(string s)
+{
+    var stack = new Stack<char>();
+    foreach (var l in s)
+    {
+        if (stack.Count != 0 && (Math.Abs(stack.Peek() - l) == 1 || Math.Abs(stack.Peek() - l) == 25))
+            stack.Pop();
+        else
+            stack.Push(l);
+    }
+    return string.Join("", stack.Reverse().ToArray());
+}
