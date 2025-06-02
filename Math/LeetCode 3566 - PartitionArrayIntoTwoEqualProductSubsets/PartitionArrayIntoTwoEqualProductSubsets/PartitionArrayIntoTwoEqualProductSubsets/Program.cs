bf7924dcef939640bf0f5dafb800a5c3320e9da3
Xing <x.xing.unsw@gmail.com>
2025-06-02 19:30:10 +1000

    init

---
+++ Math/LeetCode 3566 - PartitionArrayIntoTwoEqualProductSubsets/PartitionArrayIntoTwoEqualProductSubsets/PartitionArrayIntoTwoEqualProductSubsets/Program.cs
@@ -0,0 +1,10 @@
+﻿bool CheckEqualPartitions(int[] nums, long target)
+{
+    long prod = 1;
+    foreach (var num in nums)
+    {
+        if (num > target) return false;
+        prod *= num;
+    }
+    return prod == target * target;
+}
