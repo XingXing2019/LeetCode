3f5bc6d67cf17ba93638e240baa17dab8aa41d5f
Xing <x.xing.unsw@gmail.com>
2025-06-09 20:33:41 +1000

    init

---
+++ Sort/LeetCode 3572 - MaximizeYSumByPickingATripletOfDistinctXValues/MaximizeYSum/MaximizeYSum/Program.cs
@@ -0,0 +1,19 @@
+﻿int MaxSumDistinctTriplet(int[] x, int[] y)
+{
+    var dict = new Dictionary<int, List<int>>();
+    for (int i = 0; i < x.Length; i++)
+    {
+        if (!dict.ContainsKey(x[i]))
+            dict[x[i]] = new List<int>();
+        dict[x[i]].Add(i);
+    }
+    if (dict.Count < 3) return -1;
+    var max = new List<int>();
+    foreach (var indexes in dict.Values)
+    {
+        var sorted = indexes.OrderByDescending(i => y[i]).ToList();
+        max.Add(y[sorted[0]]);
+    }
+    max = max.OrderByDescending(i => i).ToList();
+    return max[0] + max[1] + max[2];
+}
