33d2ced3a7240a48c0f1d15141164ed89ab217d5
Xing <x.xing.unsw@gmail.com>
2025-06-11 21:49:06 +1000

    init

---
+++ Math/LeetCode 3577 - CountTheNumberOfComputerUnlockingPermutations/CountTheNumber/CountTheNumber/Program.cs
@@ -0,0 +1,9 @@
+﻿int CountPermutations(int[] complexity)
+{
+    var sorted = complexity.OrderBy(x => x).ToArray();
+    if (sorted[0] != complexity[0] || sorted[0] == sorted[1]) return 0;
+    long res = 1, mod = 1_000_000_000 + 7;
+    for (int i = 1; i < sorted.Length; i++)
+        res = res * i % mod;
+    return (int)res;
+}
