ebf6f96472e81ffe45712ce785359407fca4c4ee
Xing <x.xing.unsw@gmail.com>
2025-06-16 19:06:29 +1000

    init

---
+++ HashTable/LeetCode 3581 - CountOddLettersFromNumber/CountOddLettersFromNumber/CountOddLettersFromNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿int CountOddLetters(int n)
+{
+    string[] nums = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    var freq = new Dictionary<char, int>();
+    while (n != 0)
+    {
+        var num = nums[n % 10];
+        foreach (var l in num)
+        {
+            if (!freq.ContainsKey(l))
+                freq[l] = 0;
+            freq[l]++;
+        }
+        n /= 10;
+    }
+    return freq.Where(x => x.Value % 2 != 0).ToList().Count;
+}
