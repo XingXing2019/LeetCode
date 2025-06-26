94db78baa06fcd356ce6b9329f31e46e394c94ad
Xing <x.xing.unsw@gmail.com>
2025-06-26 18:59:26 +1000

    init

---
+++ Sort/LeetCode 3595 - OnceTwice/OnceTwice/OnceTwice/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] OnceTwice(int[] nums)
+{
+    Array.Sort(nums);
+    int count = 0, cur = nums[0];
+    var res = new int[2];
+    foreach (var num in nums)
+    {
+        if (num == cur)
+            count++;
+        else
+        {
+            if (count == 1)
+                res[0] = cur;
+            else if (count == 2)
+                res[1] = cur;
+            count = 1;
+            cur = num;
+        }
+    }
+    if (count == 1)
+        res[0] = cur;
+    else if (count == 2)
+        res[1] = cur;
+    return res;
+}
