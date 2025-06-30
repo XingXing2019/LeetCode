b2e5649c0fab619c12aa191328cf43798aa30f09
Xing <x.xing.unsw@gmail.com>
2025-06-30 19:56:17 +1000

    init

---
+++ HashTable/LeetCode 3597 - PartitionString/PartitionString/PartitionString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+IList<string> PartitionString(string s)
+{
+    var res = new HashSet<string>();
+    var index = 0;
+    while (index < s.Length)
+    {
+        var sb = new StringBuilder();
+        while (sb.Length == 0 || (res.Contains(sb.ToString()) && index < s.Length))
+            sb.Append(s[index++]);
+        res.Add(sb.ToString());
+    }
+    return res.ToList();
+}
