d2149e646d973ac65a1e0e81b3bbaf205d7c4cb7
Xing <x.xing.unsw@gmail.com>
2025-07-14 20:59:53 +1000

    init

---
+++ String/LeetCode 3612 - ProcessStringWithSpecialOperationsI/ProcessStringWithSpecialOperationsI/ProcessStringWithSpecialOperationsI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+string ProcessStr(string s)
+{
+    var res = new StringBuilder();
+    foreach (var l in s)
+    {
+        if (char.IsLetter(l))
+            res.Append(l);
+        else if (l == '#')
+            res = new StringBuilder(res.ToString() + res);
+        else if (l == '%')
+            res = new StringBuilder(string.Join("", res.ToString().Reverse()));
+        else if (res.Length != 0)
+            res.Remove(res.Length - 1, 1);
+    }
+    return res.ToString();
+}
