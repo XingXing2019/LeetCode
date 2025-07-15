a7b41f72549bc169791490cd8b7f1b0c51210aa2
Xing <x.xing.unsw@gmail.com>
2025-07-15 20:46:46 +1000

    init

---
+++ Array/LeetCode 3616 - NumberOfStudentReplacements/NumberOfStudentReplacements/NumberOfStudentReplacements/Program.cs
@@ -0,0 +1,12 @@
+﻿int TotalReplacements(int[] ranks)
+{
+    var cur = ranks[0];
+    var res = 0;
+    for (int i = 1; i < ranks.Length; i++)
+    {
+        if (ranks[i] >= cur) continue;
+        cur = ranks[i];
+        res++;
+    }
+    return res;
+}
