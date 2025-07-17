9e06438402024591d4553e52a09d9ddf62f1d169
Xing <x.xing.unsw@gmail.com>
2025-07-17 17:47:44 +1000

    init

---
+++ String/LeetCode 3614 - ProcessStringWithSpecialOperationsII/ProcessStringWithSpecialOperationsII/ProcessStringWithSpecialOperationsII/Program.cs
@@ -0,0 +1,32 @@
+﻿char ProcessStr(string s, long k)
+{
+    var len = 0L;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] == '*') 
+            len = Math.Max(0, len - 1);
+        else if (s[i] == '#')
+            len *= 2;
+        else if (char.IsLetter(s[i])) 
+            len++;
+    }
+    if (k >= len) return '.';
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        if (s[i] == '*')
+            len++;
+        else if (s[i] == '#')
+        {
+            len /= 2;
+            k %= len;
+        }
+        else if (s[i] == '%')
+            k = len - 1 - k;
+        else
+        {
+            len--;
+            if (k == len) return s[i];
+        }
+    }
+    return '.';
+}
