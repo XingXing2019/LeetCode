ded1de27e8efac77baf872735436dd92fdc3b669
Xing <x.xing.unsw@gmail.com>
2025-07-21 20:47:31 +1000

    init

---
+++ Math/LeetCode 3622 - CheckDivisibilityByDigitSumAndProduct/CheckDivisibilityByDigitSumAndProduct/CheckDivisibilityByDigitSumAndProduct/Program.cs
@@ -0,0 +1,12 @@
+﻿bool CheckDivisibility(int n)
+{
+    var copy = n;
+    long sum = 0, product = 1;
+    while (copy != 0)
+    {
+        sum += copy % 10;
+        product *= copy % 10;
+        copy /= 10;
+    }
+    return n % (sum + product) == 0;
+}
