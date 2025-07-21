0339c0d66defd1af3906959e95fde580fc4052b1
Xing <x.xing.unsw@gmail.com>
2025-07-21 20:44:33 +1000

    init

---
+++ Search/LeetCode 3619 - CountIslandsWithTotalValueDivisibleByK/CountIslandsWithTotalValueDivisibleByK/CountIslandsWithTotalValueDivisibleByK/Program.cs
@@ -0,0 +1,28 @@
+﻿int CountIslands(int[][] grid, int k)
+{
+    var res = 0;
+    for (int x = 0; x < grid.Length; x++)
+    {
+        for (int y = 0; y < grid[0].Length; y++)
+        {
+            if (grid[x][y] == 0) continue;
+            var sum = DFS(grid, x, y);
+            if (sum % k != 0) continue;
+            res++;
+        }
+    }
+    return res;
+}
+
+int DFS(int[][] grid, int x, int y)
+{
+    if (x < 0 || x >= grid.Length || y < 0 || y >= grid[0].Length || grid[x][y] == 0)
+        return 0;
+    var res = grid[x][y];
+    grid[x][y] = 0;
+    res += DFS(grid, x + 1, y);
+    res += DFS(grid, x - 1, y);
+    res += DFS(grid, x, y + 1);
+    res += DFS(grid, x, y - 1);
+    return res;
+}
