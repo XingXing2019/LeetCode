90d6a54b60f5bfafa64d10a9c3cde33dd1dd01b4
Xing <x.xing.unsw@gmail.com>
2025-08-04 19:39:06 +1000

    init

---
+++ TwoPointers/LeetCode 3634 - MinimumRemovalsToBalanceArray/MinimumRemovalsToBalanceArray/MinimumRemovalsToBalanceArray/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] nums = { 2, 1, 5 };
+var k = 2;
+Console.WriteLine(MinRemoval(nums, k));
+
+int MinRemoval(int[] nums, int k)
+{
+    Array.Sort(nums);
+    var res = int.MaxValue;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var hi = BinarySearch(nums, (long)nums[i] * k);
+        res = Math.Min(res, nums.Length - (hi - i + 1));
+    }
+    return res;
+}
+
+int BinarySearch(int[] nums, long target)
+{
+    int li = 0, hi = nums.Length - 1;
+    while (li <= hi)
+    {
+        var mid = li + (hi - li) / 2;
+        if (nums[mid] <= target)
+            li = mid + 1;
+        else
+            hi = mid - 1;
+    }
+
+    return hi;
+}
