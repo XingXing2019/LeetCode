815ac1ab3ac1e661ca53d06da356dfbfb0a4420f
Xing <x.xing.unsw@gmail.com>
2025-08-05 20:39:29 +1000

    init

---
+++ Array/LeetCode 3637 - TrionicArrayI/TrionicArrayI/TrionicArrayI/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] nums = { 6, 7, 5, 1 };
+Console.WriteLine(IsTrionic(nums));
+
+bool IsTrionic(int[] nums)
+{
+    var count = 0;
+    var increasing = true;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] == nums[i - 1])
+            return false;
+        if (nums[i] > nums[i - 1])
+        {
+            if (increasing) continue;
+            count++;
+            increasing = true;
+        }
+        else
+        {
+            if (i == 1) return false;
+            if (!increasing) continue;
+            count++;
+            increasing = false;
+        }
+    }
+    return count == 2;
+}
