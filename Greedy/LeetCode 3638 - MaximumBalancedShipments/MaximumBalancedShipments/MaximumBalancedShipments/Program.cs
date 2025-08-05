76a2deb719594da477048cae984a29da199d01c2
Xing <x.xing.unsw@gmail.com>
2025-08-05 20:51:52 +1000

    init

---
+++ Greedy/LeetCode 3638 - MaximumBalancedShipments/MaximumBalancedShipments/MaximumBalancedShipments/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] weight = { 2, 5, 1, 4, 3 };
+Console.WriteLine(MaxBalancedShipments(weight));
+
+int MaxBalancedShipments(int[] weight)
+{
+    int res = 0, max = int.MinValue;
+    foreach (var w in weight)
+    {
+        if (w < max)
+        {
+            res++;
+            max = int.MinValue;
+        }
+        else
+            max = Math.Max(max, w);
+    }
+    return res;
+}
