44545e688a8d2c86b8d3d5edcfa995b1228be413
Xing <x.xing.unsw@gmail.com>
2025-08-15 22:08:01 +1000

    init

---
+++ BitManipulation/LeetCode 3644 - MaximumKToSortAPermutation/MaximumKToSortAPermutation/MaximumKToSortAPermutation/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] nums = { 0, 1, 3, 2 };
+Console.WriteLine(SortPermutation(nums));
+
+int SortPermutation(int[] nums)
+{
+    int max = (1 << 30) - 1, res = max;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] == i) continue;
+        res &= nums[i];
+    }
+    return res == max ? 0 : res;
+}
