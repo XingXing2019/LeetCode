4b1af03193a6dbda0b7945582bcaf63ced285943
Xing <x.xing.unsw@gmail.com>
2025-08-15 21:57:25 +1000

    init

---
+++ Heap/LeetCode 3645 - MaximumTotalFromOptimalActivationOrder/MaximumTotalFromOptimalActivationOrder/MaximumTotalFromOptimalActivationOrder/Program.cs
@@ -0,0 +1,20 @@
+﻿long MaxTotal(int[] value, int[] limit)
+{
+    var dict = new Dictionary<int, PriorityQueue<int, int>>();
+    for (int i = 0; i < value.Length; i++)
+    {
+        int lim = limit[i], val = value[i];
+        if (!dict.ContainsKey(lim))
+            dict[lim] = new PriorityQueue<int, int>();
+        dict[lim].Enqueue(val, val);
+        if (dict[lim].Count > lim)
+            dict[lim].Dequeue();
+    }
+    var res = 0L;
+    foreach (var queue in dict.Values)
+    {
+        while (queue.Count != 0)
+            res += queue.Dequeue();
+    }
+    return res;
+}
