856b3747f64021713ad0877740365cb077f66f77
Xing <x.xing.unsw@gmail.com>
2025-08-24 21:38:44 +1000

    init

---
+++ HashTable/LeetCode 3659 - PartitionArrayIntoKDistinctGroups/PartitionArrayIntoKDistinctGroups/PartitionArrayIntoKDistinctGroups/Program.cs
@@ -0,0 +1,12 @@
+﻿bool PartitionArray(int[] nums, int k)
+{
+    if (nums.Length % k != 0) return false;
+    var num = nums.Length / k;
+    var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    foreach (var f in freq.Values)
+    {
+        if (f <= num) continue;
+        return false;
+    }
+    return true;
+}
