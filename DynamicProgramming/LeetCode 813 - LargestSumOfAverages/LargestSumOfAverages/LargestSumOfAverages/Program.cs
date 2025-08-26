8cedb8f3daf305cf146d3fd9922ef2a055070d90
Xing <x.xing.unsw@gmail.com>
2025-08-26 21:57:09 +1000

    init

--- DynamicProgramming/LeetCode 813 - LargestSumOfAverages/LargestSumOfAverages/LargestSumOfAverages/Program.cs
+++ DynamicProgramming/LeetCode 813 - LargestSumOfAverages/LargestSumOfAverages/LargestSumOfAverages/Program.cs
@@ -10,39 +10,35 @@ namespace LargestSumOfAverages
             int K = 3;
             Console.WriteLine(LargestSumOfAverages(A, K));
         }
-        static double LargestSumOfAverages(int[] A, int K)
+        public static double LargestSumOfAverages(int[] nums, int k)
         {
-            double[][] dp = new double[A.Length][];
+            var prefix = new double[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+                prefix[i] = i == 0 ? nums[i] : nums[i] + prefix[i - 1];
+            var dp = new double[k][];
             for (int i = 0; i < dp.Length; i++)
-                dp[i] = new double[2];
-            int minMembers = A.Length / K;
-            int maxMember = A.Length - K + 1;
-            dp[0][0] = A[0];
-            dp[0][1] = 1;
-            double res = A[1] > A[0] ? 0 : A[0];
-            for (int i = 1; i < A.Length; i++)
             {
-                if (dp[i - 1][1] + 1 <= maxMember && dp[i - 1][1] + 1 >= minMembers)
+                dp[i] = new double[nums.Length];
+                Array.Fill(dp[i], -1);
+            }
+            for (int i = 0; i < nums.Length; i++)
+                dp[0][i] = prefix[i] / (i + 1);
+            for (int x = 1; x < dp.Length; x++)
+            {
+                for (int y = x; y < dp[x].Length; y++)
                 {
-                    if(A[i] < A[i - 1])
+                    var max = 0d;
+                    for (int z = 0; z < y; z++)
                     {
-                        dp[i][0] = A[i];
-                        dp[i][1] = 1;
+                        var temp = dp[x - 1][z];
+                        if (temp == -1) continue;
+                        var avg = (prefix[y] - prefix[z]) / (y - z);
+                        max = Math.Max(max, temp + avg);
                     }
-                    else
-                    {
-                        dp[i][0] = (dp[i - 1][0] * dp[i - 1][1] + A[i]) / (dp[i - 1][1] + 1);
-                        dp[i][1] = dp[i - 1][1] + 1;
-                    }
-                }
-                else
-                {
-                    res += dp[i - 1][0];
-                    dp[i][0] = A[i];
-                    dp[i][1] = 1;
+                    dp[x][y] = max;
                 }
             }
-            return res + dp[dp.Length - 1][0];
+            return dp[^1][^1];
         }
     }
 }
