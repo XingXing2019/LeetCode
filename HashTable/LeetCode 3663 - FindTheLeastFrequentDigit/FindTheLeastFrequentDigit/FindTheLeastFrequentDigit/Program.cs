84a17b08c57b1b524e11b3d5b27f541ed32add68
Xing <x.xing.unsw@gmail.com>
2025-08-31 20:47:44 +1000

    init

---
+++ HashTable/LeetCode 3663 - FindTheLeastFrequentDigit/FindTheLeastFrequentDigit/FindTheLeastFrequentDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetLeastFrequentDigit(int n)
+{
+    var freq = new int[10];
+    while (n != 0)
+    {
+        freq[n % 10]++;
+        n /= 10;
+    }
+    int min = int.MaxValue, res = -1;
+    for (int i = 0; i < freq.Length; i++)
+    {
+        if (freq[i] == 0) continue;
+        if (freq[i] < min)
+        {
+            res = i;
+            min = freq[i];
+        }
+    }
+    return res;
+}
