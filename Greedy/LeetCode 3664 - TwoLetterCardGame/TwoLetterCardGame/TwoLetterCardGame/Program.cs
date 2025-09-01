1c63a4b316416e0cc9201553ed1e22a561699450
Xing <x.xing.unsw@gmail.com>
2025-09-01 20:53:40 +1000

    init

---
+++ Greedy/LeetCode 3664 - TwoLetterCardGame/TwoLetterCardGame/TwoLetterCardGame/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] cards = { "ab", "ab", "bc", "bc", "bb", "bc", "bc", "ba", "bc", "cb", "ba", "bb", "cb", "cb", "cb", "ba", "bc", "ba", "bb", "ba", "bb", "ba", "bb" };
+var x = 'b';
+Console.WriteLine(Score(cards, x));
+
+int Score(string[] cards, char x)
+{
+    var first = new Dictionary<string, int>();
+    var second = new Dictionary<string, int>();
+    var both = 0;
+    foreach (var card in cards)
+    {
+        if (card[0] == x && card[1] == x)
+            both++;
+        else if (card[0] == x)
+            first[card] = first.GetValueOrDefault(card, 0) + 1;
+        else if (card[1] == x)
+            second[card] = second.GetValueOrDefault(card, 0) + 1;
+    }
+    var firstCount = Count(first);
+    var secondCount = Count(second);
+    return firstCount[0] + secondCount[0] + Math.Min(both, firstCount[1] + secondCount[1]);
+}
+
+int[] Count(Dictionary<string, int> cards)
+{
+    var freq = cards.Select(x => x.Value).ToArray();
+    var res = 0;
+    for (int i = 0; i < freq.Length; i++)
+    {
+        for (int j = i + 1; j < freq.Length; j++)
+        {
+            if (freq[j] == 0) continue;
+            var pairs = Math.Min(freq[i], freq[j]);
+            res += pairs;
+            freq[i] -= pairs;
+            freq[j] -= pairs;
+        }
+    }
+    return new[] {res, freq.Sum()};
+}
---
+++ Heap/LeetCode 1792 - MaximumAveragePassRatio/MaximumAveragePassRatio/MaximumAveragePassRatio/Program.cs
@@ -0,0 +1,29 @@
+﻿int[][] classes = new int[][]
+{
+    new[] { 2, 4 },
+    new[] { 3, 9 },
+    new[] { 2, 10 },
+};
+var extraStudents = 2;
+Console.WriteLine(MaxAverageRatio(classes, extraStudents));
+
+double MaxAverageRatio(int[][] classes, int extraStudents)
+{
+    var queue = new PriorityQueue<int[], double>();
+    foreach (var c in classes)
+        queue.Enqueue(c, (double)c[0] / c[1] - (double)(c[0] + 1) / (c[1] + 1));
+    for (int i = 0; i < extraStudents; i++)
+    {
+        var c = queue.Dequeue();
+        queue.Enqueue(c, (double)c[0] / c[1] - (double)(c[0] + 1) / (c[1] + 1));
+        c[0]++;
+        c[1]++;
+    }
+    var total = 0d;
+    while (queue.Count != 0)
+    {
+        var c = queue.Dequeue();
+        total += (double)c[0] / c[1];
+    }
+    return total / classes.Length;
+}
