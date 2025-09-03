f8d9acdd98f42444ee4fb9d1a3f4921795e2957b
Xing <x.xing.unsw@gmail.com>
2025-09-03 20:45:57 +1000

    init

---
+++ Math/LeetCode 3027 - FindTheNumberOfWaysToPlacePeopleII/FindTheNumberOfWaysToPlacePeopleII/FindTheNumberOfWaysToPlacePeopleII/Program.cs
@@ -0,0 +1,16 @@
+﻿int NumberOfPairs(int[][] points)
+{
+    var res = 0;
+    points = points.OrderBy(x => x[0]).ThenByDescending(x => x[1]).ToArray();
+    for (int i = 0; i < points.Length; i++)
+    {
+        var max = int.MinValue;
+        for (int j = i + 1; j < points.Length; j++)
+        {
+            if (points[j][1] > points[i][1]) continue;
+            if (points[j][1] > max) res++;
+            max = Math.Max(max, points[j][1]);
+        }
+    }
+    return res;
+}
