aa395eafe89177633b748238e4509b7796a8320a
Xing <x.xing.unsw@gmail.com>
2025-09-06 21:35:01 +1000

    init

---
+++ Math/LeetCode 3495 - MinimumOperationsToMakeArrayElementsZero/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,22 @@
+﻿long MinOperations(int[][] queries)
+{
+    var res = 0L;
+    foreach (var query in queries)
+    {
+        var need = Count(query[1]) - Count(query[0] - 1);
+        res += (need + 1) / 2;
+    }
+    return res;
+}
+
+long Count(int num)
+{
+    if (num < 4) return num;
+    var res = 3L;
+    for (int i = 1; i <= 4; i++)
+    {
+        var k = (num - i) / 4;
+        res += k + Count(k);
+    }
+    return res;
+}
