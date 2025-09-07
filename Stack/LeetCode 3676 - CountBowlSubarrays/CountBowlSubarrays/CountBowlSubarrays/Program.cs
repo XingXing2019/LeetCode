a87fc7e973fe6a040278f58dd6ece6c0de777f1a
Xing <x.xing.unsw@gmail.com>
2025-09-07 19:33:27 +1000

    init

---
+++ Stack/LeetCode 3676 - CountBowlSubarrays/CountBowlSubarrays/CountBowlSubarrays/Program.cs
@@ -0,0 +1,16 @@
+﻿long BowlSubarrays(int[] nums)
+{
+    var res = 0L;
+    var stack = new Stack<int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        while (stack.Count != 0 && nums[i] > nums[stack.Peek()])
+        {
+            stack.Pop();
+            if (stack.Count != 0)
+                res++;
+        }
+        stack.Push(i);
+    }
+    return res;
+}
