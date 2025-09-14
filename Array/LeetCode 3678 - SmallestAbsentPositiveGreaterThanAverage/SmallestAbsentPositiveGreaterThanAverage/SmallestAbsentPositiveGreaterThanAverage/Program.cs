7ce30c08385fd9b787f1efe3cb28bc8235ed457a
Xing <x.xing.unsw@gmail.com>
2025-09-14 19:23:35 +1000

    init

---
+++ Array/LeetCode 3678 - SmallestAbsentPositiveGreaterThanAverage/SmallestAbsentPositiveGreaterThanAverage/SmallestAbsentPositiveGreaterThanAverage/Program.cs
@@ -0,0 +1,11 @@
+﻿int SmallestAbsent(int[] nums)
+{
+    var avg = (double)nums.Sum() / nums.Length;
+    var set = new HashSet<int>(nums);
+    for (int i = 1; i < 200; i++)
+    {
+        if (set.Contains(i) || i <= avg) continue;
+        return i;
+    }
+    return -1;
+}
