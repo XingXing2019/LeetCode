ee8de60e99aced443d09d6630e5731682c36273f
Xing <x.xing.unsw@gmail.com>
2025-09-21 19:32:37 +1000

    init

---
+++ Array/LeetCode 3689 - MaximumTotalSubarrayValueI/MaximumTotalSubarrayValueI/MaximumTotalSubarrayValueI/Program.cs
@@ -0,0 +1,10 @@
+﻿long MaxTotalValue(int[] nums, int k)
+{
+    long min = int.MaxValue, max = int.MinValue;
+    foreach (var num in nums)
+    {
+        min = Math.Min(min, num);
+        max = Math.Max(max, num);
+    }
+    return (max - min) * k;
+}
