a2ef7a77e60adbcf70ff2d5f77591275f6396050
Xing <x.xing.unsw@gmail.com>
2025-09-21 19:28:35 +1000

    init

---
+++ BitManipulation/LeetCode 3688 - BitwiseOROfEvenNumbersInAnArray/BitwiseOROfEvenNumbersInAnArray/BitwiseOROfEvenNumbersInAnArray/Program.cs
@@ -0,0 +1,10 @@
+﻿int EvenNumberBitwiseORs(int[] nums)
+{
+    var res = 0;
+    foreach (var num in nums)
+    {
+        if (num % 2 != 0) continue;
+        res |= num;
+    }
+    return res;
+}
