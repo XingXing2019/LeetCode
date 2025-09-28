9c05bcd141aa99e4c0f5130297fe01b34a1252de
Xing <x.xing.unsw@gmail.com>
2025-09-28 21:13:55 +1000

    init

---
+++ Array/LeetCode 3698 - SplitArrayWithMinimumDifference/SplitArrayWithMinimumDifference/SplitArrayWithMinimumDifference/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] nums = { 1, 2, 4, 4, 3 };
+Console.WriteLine(SplitArray(nums));
+
+long SplitArray(int[] nums)
+{
+    int max = 0, index = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] <= max) continue;
+        max = nums[i];
+        index = i;
+    }
+    var count = nums.Count(x => x == max);
+    if (count > 2) return -1;
+    var res = 0L;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (i < index)
+        {
+            if (i != 0 && nums[i] <= nums[i - 1])
+                return -1;
+            res += nums[i];
+        }
+        else if (i > index)
+        {
+            if (i != index + 1 && nums[i] >= nums[i - 1])
+                return -1;
+            res -= nums[i];
+        }
+    }
+    if (count == 2) return Math.Abs(res + max);
+    return Math.Min(Math.Abs(res + max), Math.Abs(res - max));
+}
