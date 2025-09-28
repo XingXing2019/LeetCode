cf88d80c2de090ab66bdc533fafce5761929b368
Xing <x.xing.unsw@gmail.com>
2025-09-28 20:32:50 +1000

    init

---
+++ DynamicProgramming/LeetCode 3693 - ClimbingStairsII/ClimbingStairsII/ClimbingStairsII/Program.cs
@@ -0,0 +1,19 @@
+﻿var n = 4;
+int[] nums = { 1, 2, 3, 4 };
+Console.WriteLine(ClimbStairs(n, nums));
+
+int ClimbStairs(int n, int[] costs)
+{
+    var dp = new int[n + 1];
+    for (int i = 1; i < dp.Length; i++)
+    {
+        var min = int.MaxValue;
+        for (int j = 1; j <= 3; j++)
+        {
+            if (i - j < 0) continue;
+            min = Math.Min(min, costs[i - j] + j * j + dp[i - j]);
+        }
+        dp[i] = min;
+    }
+    return dp[^1];
+}
---
+++ HashTable/LeetCode 3692 - MajorityFrequencyCharacters/MajorityFrequencyCharacters/MajorityFrequencyCharacters/Program.cs
@@ -0,0 +1,19 @@
+﻿var s = "pfpfgi";
+Console.WriteLine(MajorityFrequencyGroup(s));
+
+string MajorityFrequencyGroup(string s)
+{
+    var freq = s.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    var counts = freq.Values.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    int max = 0, cur = int.MaxValue;
+    foreach (var count in counts.Keys)
+    {
+        if (counts[count] < max) continue;
+        if (counts[count] > max)
+            cur = count;
+        else if (count > cur)
+            cur = count;
+        max = counts[count];
+    }
+    return string.Join("", freq.Where(x => x.Value == cur).Select(x => x.Key));
+}
