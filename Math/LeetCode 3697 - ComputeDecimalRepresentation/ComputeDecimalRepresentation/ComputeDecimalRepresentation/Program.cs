79462dc9f6a75283e87af8e15ab076bb5f787d8e
Xing <x.xing.unsw@gmail.com>
2025-09-28 21:01:01 +1000

    init

---
+++ Math/LeetCode 3697 - ComputeDecimalRepresentation/ComputeDecimalRepresentation/ComputeDecimalRepresentation/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] DecimalRepresentation(int n)
+{
+    var res = new List<int>();
+    var pow = 1;
+    while (n != 0)
+    {
+        var num = n % 10 * pow;
+        if (num != 0)
+            res.Add(num);
+        n /= 10;
+        pow *= 10;
+    }
+    return res.OrderByDescending(x => x).ToArray();
+}
