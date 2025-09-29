2a17701d91671afed1763221088f590ea9cced76
Xing <x.xing.unsw@gmail.com>
2025-09-29 20:24:45 +1000

    init

---
+++ DynamicProgramming/LeetCode 1039 - MinimumScoreTriangulationOfPolygon/MinimumScore/MinimumScore/Program.cs
@@ -0,0 +1,24 @@
+﻿int MinScoreTriangulation(int[] values)
+{
+    var dp = new int[values.Length][];
+    for (int i = 0; i < dp.Length; i++)
+    {
+        dp[i] = new int[values.Length];
+        Array.Fill(dp[i], int.MaxValue);
+    }
+    return DFS(values, 0, values.Length - 1, dp);
+}
+
+int DFS(int[] values, int i, int j, int[][] dp)
+{
+    if (dp[i][j] != int.MaxValue)
+        return dp[i][j];
+    if (j - i + 1 < 3)
+        return 0;
+    var min = int.MaxValue;
+    for (int k = i + 1; k < j; k++)
+    {
+        min = Math.Min(min, values[i] * values[k] * values[j]);
+    }
+    return dp[i][j] = min;
+}
