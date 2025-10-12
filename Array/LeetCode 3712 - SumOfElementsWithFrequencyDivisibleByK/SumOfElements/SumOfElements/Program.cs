604e7bca454bcac2fdde0e8f958d19faa3737f5a
Xing <x.xing.unsw@gmail.com>
2025-10-12 20:25:55 +1100

    init

---
+++ Array/LeetCode 3712 - SumOfElementsWithFrequencyDivisibleByK/SumOfElements/SumOfElements/Program.cs
@@ -0,0 +1,11 @@
+﻿int SumDivisibleByK(int[] nums, int k)
+{
+    var res = 0;
+    var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    foreach (var num in freq.Keys)
+    {
+        if (freq[num] % k != 0) continue;
+        res += num * freq[num];
+    }
+    return res;
+}
---
+++ TwoPointers/LeetCode 3713 - LongestBalancedSubstringI/LongestBalancedSubstringI/LongestBalancedSubstringI/Program.cs
@@ -0,0 +1,37 @@
+﻿var s = "kooo";
+Console.WriteLine(LongestBalanced(s));
+
+int LongestBalanced(string s)
+{
+    var res = 0;
+    var prefix = new int[s.Length][];
+    for (int i = 0; i < s.Length; i++)
+    {
+        prefix[i] = new int[26];
+        if (i != 0)
+            Array.Copy(prefix[i - 1], prefix[i], 26);
+        prefix[i][s[i] - 'a']++;
+    }
+    for (int i = 0; i < s.Length; i++)
+    {
+        for (int j = i; j < s.Length; j++)
+        {
+            if (!IsValid(prefix, i, j)) continue;
+            res = Math.Max(res, j - i + 1);
+        }
+    }
+    return res;
+}
+
+bool IsValid(int[][] prefix, int li, int hi)
+{
+    var left = li == 0 ? new int[26] : prefix[li - 1];
+    var right = prefix[hi];
+    var set = new HashSet<int>();
+    for (int i = 0; i < 26; i++)
+    {
+        if (left[i] == right[i]) continue;
+        set.Add(right[i] - left[i]);
+    }
+    return set.Count == 1;
+}
