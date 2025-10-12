f83f59c927d1af7fb105a50695960727c6efcb93
Xing <x.xing.unsw@gmail.com>
2025-10-12 19:39:28 +1100

    init

---
+++ TwoPointers/LeetCode 3708 - LongestFibonacciSubarray/LongestFibonacciSubarray/LongestFibonacciSubarray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nums = { 1, 1, 1, 1, 2, 3, 5, 1 };
+Console.WriteLine(LongestSubarray(nums));
+
+int LongestSubarray(int[] nums)
+{
+    int li = 0, hi = 0, res = 0;
+    while (hi < nums.Length)
+    {
+        while (hi - li >= 2 && nums[hi] != nums[hi - 1] + nums[hi - 2])
+            li++;
+        res = Math.Max(res, hi - li + 1);
+        hi++;
+    }
+    return res;
+}
