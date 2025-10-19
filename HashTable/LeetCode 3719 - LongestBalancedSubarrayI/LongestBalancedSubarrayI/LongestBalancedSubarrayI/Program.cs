82e799f28f4409fd0bfa950552e7554e4e38a377
Xing <x.xing.unsw@gmail.com>
2025-10-19 17:38:56 +1100

    init

---
+++ HashTable/LeetCode 3719 - LongestBalancedSubarrayI/LongestBalancedSubarrayI/LongestBalancedSubarrayI/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums = { 10, 6, 10, 7 };
+Console.WriteLine(LongestBalanced(nums));
+
+int LongestBalanced(int[] nums)
+{
+    var res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var odd = new HashSet<int>();
+        var even = new HashSet<int>();
+        for (int j = i; j < nums.Length; j++)
+        {
+            if (nums[j] % 2 == 0)
+                even.Add(nums[j]);
+            else
+                odd.Add(nums[j]);
+            if (even.Count != odd.Count)
+                continue;
+            res = Math.Max(res, j - i + 1);
+        }
+    }
+    return res;
+}
