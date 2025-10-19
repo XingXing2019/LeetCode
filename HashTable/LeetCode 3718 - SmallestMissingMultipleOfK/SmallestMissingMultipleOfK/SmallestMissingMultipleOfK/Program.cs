7274682c48f45d1c6ab083a23b9ed3c8ccfbeb25
Xing <x.xing.unsw@gmail.com>
2025-10-19 17:21:03 +1100

    init

---
+++ HashTable/LeetCode 3718 - SmallestMissingMultipleOfK/SmallestMissingMultipleOfK/SmallestMissingMultipleOfK/Program.cs
@@ -0,0 +1,10 @@
+﻿int MissingMultiple(int[] nums, int k)
+{
+    var set = new HashSet<int>(nums);
+    for (int i = 1; i <= k * 200; i++)
+    {
+        if (!set.Contains(i * k))
+            return i * k;
+    }
+    return -1;
+}
