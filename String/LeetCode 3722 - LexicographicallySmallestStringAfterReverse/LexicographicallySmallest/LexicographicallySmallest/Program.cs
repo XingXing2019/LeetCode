5b9b14f3f6f50c7258604a57f3b20fa2cf3ec1f5
Xing <x.xing.unsw@gmail.com>
2025-10-26 21:29:54 +1100

    init

---
+++ String/LeetCode 3722 - LexicographicallySmallestStringAfterReverse/LexicographicallySmallest/LexicographicallySmallest/Program.cs
@@ -0,0 +1,15 @@
+﻿var s = "abba";
+Console.WriteLine(LexSmallest(s));
+
+string LexSmallest(string s)
+{
+    var words = new List<string>();
+    for (int i = 1; i <= s.Length; i++)
+    {
+        var word1 = string.Join("", s.Substring(0, i).Reverse()) + s.Substring(i);
+        var word2 = s.Substring(0, i) + string.Join("", s.Substring(i).Reverse());
+        words.Add(word1);
+        words.Add(word2);
+    }
+    return words.OrderBy(x => x).First();
+}
