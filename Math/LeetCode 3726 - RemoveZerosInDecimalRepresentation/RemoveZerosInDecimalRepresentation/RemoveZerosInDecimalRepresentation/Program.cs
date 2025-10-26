466585fe8fa13d3cdda4f6c69f106bec2539fd25
Xing <x.xing.unsw@gmail.com>
2025-10-26 21:50:18 +1100

    init

---
+++ Math/LeetCode 3726 - RemoveZerosInDecimalRepresentation/RemoveZerosInDecimalRepresentation/RemoveZerosInDecimalRepresentation/Program.cs
@@ -0,0 +1,15 @@
+﻿long RemoveZeros(long n)
+{
+    var res = 0L;
+    var pow = 1L;
+    while (n != 0)
+    {
+        if (n % 10 != 0)
+        {
+            res += n % 10 * pow;
+            pow *= 10;
+        }
+        n /= 10;
+    }
+    return res;
+}
