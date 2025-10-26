3893d2f99e8fa39beeb1e6d718473d9dacbe3a55
Xing <x.xing.unsw@gmail.com>
2025-10-26 21:34:09 +1100

    init

---
+++ Greedy/LeetCode 3723 - MaximizeSumOfSquaresOfDigits/MaximizeSumOfSquaresOfDigits/MaximizeSumOfSquaresOfDigits/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+string MaxSumOfSquares(int num, int sum)
+{
+    var res = new StringBuilder();
+    for (int i = 0; i < num; i++)
+    {
+        var digit = Math.Min(9, Math.Max(0, sum));
+        res.Append(digit);
+        sum -= digit;
+    }
+    return sum == 0 ? res.ToString() : "";
+}
