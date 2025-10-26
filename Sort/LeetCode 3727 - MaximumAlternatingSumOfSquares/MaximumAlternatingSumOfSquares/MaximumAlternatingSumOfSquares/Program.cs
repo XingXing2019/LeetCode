7d9836f59502d4839d917a8c3b05f8343d2f4802
Xing <x.xing.unsw@gmail.com>
2025-10-26 21:53:49 +1100

    init

---
+++ Sort/LeetCode 3727 - MaximumAlternatingSumOfSquares/MaximumAlternatingSumOfSquares/MaximumAlternatingSumOfSquares/Program.cs
@@ -0,0 +1,13 @@
+﻿long MaxAlternatingSum(int[] nums)
+{
+    var res = 0L;
+    nums = nums.OrderBy(x => Math.Abs(x)).ToArray();
+    int li = 0, hi = nums.Length - 1;
+    while (li < hi)
+    {
+        res += nums[hi] * nums[hi] - nums[li] * nums[li];
+        hi--;
+        li++;
+    }
+    return li == hi ? res + nums[li] * nums[li] : res;
+}
