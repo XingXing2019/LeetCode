0bcec0bcbc561cf9f1c273f119c2dce7040b3e57
Xing <x.xing.unsw@gmail.com>
2025-11-02 19:51:32 +1100

    init

---
+++ Sort/LeetCode 3731 - FindMissingElements/FindMissingElements/FindMissingElements/Program.cs
@@ -0,0 +1,16 @@
+﻿IList<int> FindMissingElements(int[] nums)
+{
+    Array.Sort(nums);
+    var res = new List<int>();
+    int count = nums[0], index = 0;
+    while (index < nums.Length)
+    {
+        var num = nums[index];
+        if (num != count)
+            res.Add(count);
+        else
+            index++;
+        count++;
+    }
+    return res;
+}
