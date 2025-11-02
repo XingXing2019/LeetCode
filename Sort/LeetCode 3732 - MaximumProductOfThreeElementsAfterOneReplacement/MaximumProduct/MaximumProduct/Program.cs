f8c0b80db34446b630853525b10c34ba65a1160d
Xing <x.xing.unsw@gmail.com>
2025-11-02 19:55:50 +1100

    init

---
+++ Sort/LeetCode 3732 - MaximumProductOfThreeElementsAfterOneReplacement/MaximumProduct/MaximumProduct/Program.cs
@@ -0,0 +1,9 @@
+﻿long MaxProduct(int[] nums)
+{
+    Array.Sort(nums);
+    var prod = 1_000_00L;
+    var op1 = Math.Abs((long)nums[0] * nums[1] * prod);
+    var op2 = Math.Abs((long)nums[^1] * nums[^2] * prod);
+    var op3 = Math.Abs((long)nums[0] * nums[^1] * prod);
+    return Math.Max(op1, Math.Max(op2, op3));
+}
