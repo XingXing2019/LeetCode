0e94b4f370b56f5e33e5a9d664efdb0f38b3a279
Xing <x.xing.unsw@gmail.com>
2025-11-09 20:24:45 +1100

    init

---
+++ Array/LeetCode 3737 - CountSubarraysWithMajorityElementI/CountSubarraysWithMajorityElementI/CountSubarraysWithMajorityElementI/Program.cs
@@ -0,0 +1,14 @@
+﻿int CountMajoritySubarrays(int[] nums, int target)
+{
+    var res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var count = 0;
+        for (int j = i; j < nums.Length; j++)
+        {
+            count += nums[j] == target ? 1 : 0;
+            res += count * 2 > j - i + 1 ? 1 : 0;
+        }
+    }
+    return res;
+}
---
+++ Array/LeetCode 3738 - LongestNonDecreasingSubarrayAfterReplacingAtMostOneElement/LongestNonDecreasingSubarray/LongestNonDecreasingSubarray/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] nums = { 4 };
+Console.WriteLine(LongestSubarray(nums));
+int LongestSubarray(int[] nums)
+{
+    var left = new int[nums.Length];
+    var right = new int[nums.Length];
+    int l = 0, r = 0, res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        l = i == 0 ? 1 : nums[i] >= nums[i - 1] ? l + 1 : 1;
+        left[i] = l;
+        r = i == 0 ? 1 : nums[^(i + 1)] <= nums[^i] ? r + 1 : 1;
+        right[^(i + 1)] = r;
+    }
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (i != 0)
+            res = Math.Max(res, left[i - 1] + 1);
+        if (i != nums.Length - 1)
+            res = Math.Max(res, right[i + 1] + 1);
+        if (i != 0 && i != nums.Length - 1 && nums[i - 1] <= nums[i + 1])
+            res = Math.Max(res, left[i - 1] + right[i + 1] + 1);
+    }
+    return res;
+}
