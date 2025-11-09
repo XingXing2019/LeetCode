931dcd652da4191c1fd4587420c2748273da3a8c
Xing <x.xing.unsw@gmail.com>
2025-11-09 20:31:31 +1100

    init

---
+++ HashTable/LeetCode 3740 - MinimumDistanceBetweenThreeEqualElementsI/MinimumDistance/MinimumDistance/Program.cs
@@ -0,0 +1,19 @@
+﻿int MinimumDistance(int[] nums)
+{
+    var dict = new Dictionary<int, List<int>>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (!dict.ContainsKey(nums[i]))
+            dict[nums[i]] = new List<int>();
+        dict[nums[i]].Add(i);
+    }
+    var res = int.MaxValue;
+    foreach (var indexes in dict.Values)
+    {
+        for (int i = 0; i <= indexes.Count - 3; i++)
+        {
+            res = Math.Min(res, 2 * indexes[i + 2] - 2 * indexes[i]);
+        }
+    }
+    return res == int.MaxValue ? -1 : res;
+}
