c1e700006041ce676f9515cab813c64b3fcfae6f
Xing <x.xing.unsw@gmail.com>
2025-11-21 20:33:00 +1100

    init

---
+++ Stack/LeetCode 3746 - MinimumStringLengthAfterBalancedRemovals/MinimumStringLength/MinimumStringLength/Program.cs
@@ -0,0 +1,12 @@
+﻿int MinLengthAfterRemovals(string s)
+{
+    var stack = new Stack<char>();
+    foreach (var l in s)
+    {
+        if (stack.Count == 0 || stack.Peek() == l)
+            stack.Push(l);
+        else
+            stack.Pop();
+    }
+    return stack.Count;
+}
