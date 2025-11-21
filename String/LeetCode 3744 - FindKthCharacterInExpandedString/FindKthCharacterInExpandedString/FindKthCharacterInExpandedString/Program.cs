30bc4c68d9de75fae25b8e1a35bb5a6df55d9f67
Xing <x.xing.unsw@gmail.com>
2025-11-21 20:25:53 +1100

    init

---
+++ String/LeetCode 3744 - FindKthCharacterInExpandedString/FindKthCharacterInExpandedString/FindKthCharacterInExpandedString/Program.cs
@@ -0,0 +1,20 @@
+﻿var s = "hello world";
+var k = 17;
+Console.WriteLine(KthCharacter(s, k));
+
+char KthCharacter(string s, long k)
+{
+    var words = s.Split(' ');
+    foreach (var word in words)
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            k -= i + 1;
+            if (k < 0) return word[i];
+        }
+        if (k == 0)
+            return ' ';
+        k--;
+    }
+    return ' ';
+}
