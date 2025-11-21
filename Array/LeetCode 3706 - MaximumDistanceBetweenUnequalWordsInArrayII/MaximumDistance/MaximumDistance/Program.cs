4361623bc68eadb7f318cb9bbe14789ee08db857
Xing <x.xing.unsw@gmail.com>
2025-11-22 09:59:36 +1100

    init

---
+++ Array/LeetCode 3706 - MaximumDistanceBetweenUnequalWordsInArrayII/MaximumDistance/MaximumDistance/Program.cs
@@ -0,0 +1,16 @@
+﻿int MaxDistance(string[] words)
+{
+    var res = 0;
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i] == words[^1]) continue;
+        res = words.Length - i;
+        break;
+    }
+    for (int i = words.Length - 1; i >= 0; i--)
+    {
+        if (words[i] == words[0]) continue;
+        res = Math.Max(res, i + 1);
+    }
+    return res;
+}
