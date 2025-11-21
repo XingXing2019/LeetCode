59324ecd2f122447107d57a1174c933e58336e80
Xing <x.xing.unsw@gmail.com>
2025-11-22 09:29:14 +1100

    init

---
+++ Sort/LeetCode 3730 - MaximumCaloriesBurntFromJumps/MaximumCaloriesBurntFromJumps/MaximumCaloriesBurntFromJumps/Program.cs
@@ -0,0 +1,16 @@
+﻿long MaxCaloriesBurnt(int[] heights)
+{
+    Array.Sort(heights);
+    var res = (long)heights[^1] * heights[^1];
+    int li = 0, hi = heights.Length - 1, count = 0;
+    while (li < hi)
+    {
+        res += ((long)heights[hi] - heights[li]) * ((long)heights[hi] - heights[li]);
+        if (count % 2 == 0)
+            hi--;
+        else
+            li++;
+        count++;
+    }
+    return res;
+}
