d8de4f84f68b1b4e7d83a05e4b4db598d2e54d97
Xing <x.xing.unsw@gmail.com>
2025-11-22 09:51:39 +1100

    init

---
+++ String/LeetCode 3491 - PhoneNumberPrefix/PhoneNumberPrefix/PhoneNumberPrefix/Program.cs
@@ -0,0 +1,12 @@
+﻿bool PhonePrefix(string[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        for (int j = 0; j < numbers.Length; j++)
+        {
+            if (i == j) continue;
+            if (numbers[j].StartsWith(numbers[i])) return false;
+        }
+    }
+    return true;
+}
