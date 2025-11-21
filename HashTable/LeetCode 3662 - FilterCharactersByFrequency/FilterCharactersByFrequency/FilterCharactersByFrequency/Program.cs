ebceb056c4a47571dbb29dab0717bc2c05493ade
Xing <x.xing.unsw@gmail.com>
2025-11-22 09:34:03 +1100

    init

---
+++ HashTable/LeetCode 3662 - FilterCharactersByFrequency/FilterCharactersByFrequency/FilterCharactersByFrequency/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+string FilterCharacters(string s, int k)
+{
+    var freq = s.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    var res = new StringBuilder();
+    foreach (var l in s)
+    {
+        if (freq[l] >= k) continue;
+        res.Append(l);
+    }
+    return res.ToString();
+}
