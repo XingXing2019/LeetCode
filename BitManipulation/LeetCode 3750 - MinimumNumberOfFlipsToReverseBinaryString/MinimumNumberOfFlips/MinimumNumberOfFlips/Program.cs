4cbbce81bf685c75b97f9e267a886e93dc6b462b
Xing <x.xing.unsw@gmail.com>
2025-11-23 20:30:45 +1100

    init

---
+++ BitManipulation/LeetCode 3750 - MinimumNumberOfFlipsToReverseBinaryString/MinimumNumberOfFlips/MinimumNumberOfFlips/Program.cs
@@ -0,0 +1,17 @@
+﻿int MinimumFlips(int n)
+{
+    int flip = 0, copy = n, res = 0;
+    while (copy != 0)
+    {
+        flip = flip * 2 + copy % 2;
+        copy /= 2;
+    }
+    while (flip != 0 || n != 0)
+    {
+        if (flip % 2 != n % 2)
+            res++;
+        flip /= 2;
+        n /= 2;
+    }
+    return res;
+}
