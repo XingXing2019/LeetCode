f9e9881a9b2b388badaf21db2f6d0a66aca47163
Xing <x.xing.unsw@gmail.com>
2025-11-23 20:57:00 +1100

    init

---
+++ Math/LeetCode 3754 - ConcatenateNonZeroDigitsAndMultiplyBySumI/ConcatenateNonZeroDigits/ConcatenateNonZeroDigits/Program.cs
@@ -0,0 +1,16 @@
+﻿long SumAndMultiply(int n)
+{
+    long x = 0, sum = 0, pow = 1;
+    while (n != 0)
+    {
+        var mod = n % 10;
+        if (mod != 0)
+        {
+            sum += mod;
+            x += mod * pow;
+            pow *= 10;
+        }
+        n /= 10;
+    }
+    return x * sum;
+}
