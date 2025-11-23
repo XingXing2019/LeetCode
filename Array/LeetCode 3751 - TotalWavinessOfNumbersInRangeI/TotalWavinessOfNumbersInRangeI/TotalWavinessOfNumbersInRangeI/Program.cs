2254ed911d6cc4c80c168b110b0f7f80a1c5705b
Xing <x.xing.unsw@gmail.com>
2025-11-23 20:34:46 +1100

    init

---
+++ Array/LeetCode 3751 - TotalWavinessOfNumbersInRangeI/TotalWavinessOfNumbersInRangeI/TotalWavinessOfNumbersInRangeI/Program.cs
@@ -0,0 +1,18 @@
+﻿int TotalWaviness(int num1, int num2)
+{
+    var res = 0;
+    for (int i = num1; i <= num2; i++)
+        res += Count(i.ToString());
+    return res;
+}
+
+int Count(string num)
+{
+    var res = 0;
+    for (int i = 1; i < num.Length - 1; i++)
+    {
+        if (num[i] < num[i - 1] && num[i] < num[i + 1] || num[i] > num[i - 1] && num[i] > num[i + 1])
+            res++;
+    }
+    return res;
+}
