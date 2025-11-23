e41000a7f54ed00c91776032e051a21f6c003c5f
Xing <x.xing.unsw@gmail.com>
2025-11-23 21:09:07 +1100

    init

---
+++ HashTable/LeetCode 3755 - FindMaximumBalancedXORSubarrayLength/FindMaximumBalanced/FindMaximumBalanced/Program.cs
@@ -0,0 +1,15 @@
+﻿int MaxBalancedSubarray(int[] nums)
+{
+    int diff = 0, xor = 0, res = 0;
+    var dict = new Dictionary<(int, int), int> { { (diff, xor), -1 } };
+    for (int i = 0; i < nums.Length; i++)
+    {
+        diff += nums[i] % 2 == 0 ? 1 : -1;
+        xor ^= nums[i];
+        if (dict.ContainsKey((diff, xor)))
+            res = Math.Max(res, i - dict[(diff, xor)]);
+        else
+            dict[(diff, xor)] = i;
+    }
+    return res;
+}
