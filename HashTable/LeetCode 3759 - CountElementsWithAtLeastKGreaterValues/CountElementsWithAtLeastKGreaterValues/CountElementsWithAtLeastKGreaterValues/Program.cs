208e63f971e9b7621eeb76ec93cfe0eeb0d019d2
Xing <x.xing.unsw@gmail.com>
2025-12-07 21:10:13 +1100

    init

---
+++ HashTable/LeetCode 3759 - CountElementsWithAtLeastKGreaterValues/CountElementsWithAtLeastKGreaterValues/CountElementsWithAtLeastKGreaterValues/Program.cs
@@ -0,0 +1,13 @@
+﻿int CountElements(int[] nums, int k)
+{
+    if (k == 0) return nums.Length;
+    Array.Sort(nums);
+    var target = nums[^k];
+    var res = 0;
+    foreach (var num in nums)
+    {
+        if (num < target)
+            res++;
+    }
+    return res;
+}
