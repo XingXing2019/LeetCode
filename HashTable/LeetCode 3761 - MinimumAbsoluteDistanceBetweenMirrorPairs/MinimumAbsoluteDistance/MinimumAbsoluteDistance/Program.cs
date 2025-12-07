518c4409b111090514f791e385359098ab551c8c
Xing <x.xing.unsw@gmail.com>
2025-12-07 21:32:36 +1100

    init

---
+++ HashTable/LeetCode 3761 - MinimumAbsoluteDistanceBetweenMirrorPairs/MinimumAbsoluteDistance/MinimumAbsoluteDistance/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] nums = { 12, 21, 45, 33, 54 };
+Console.WriteLine(MinMirrorPairDistance(nums));
+
+int MinMirrorPairDistance(int[] nums)
+{
+    var dict = new Dictionary<int, int>();
+    var res = int.MaxValue;
+    for (int i = nums.Length - 1; i >= 0; i--)
+    {
+        var reversed = Reverse(nums[i]);
+        if (dict.ContainsKey(reversed))
+            res = Math.Min(res, dict[reversed] - i);
+        dict[nums[i]] = i;
+    }
+    return res == int.MaxValue ? -1 : res;
+}
+
+int Reverse(int num)
+{
+    var res = 0;
+    while (num != 0)
+    {
+        res = res * 10 + num % 10;
+        num /= 10;
+    }
+    return res;
+}
