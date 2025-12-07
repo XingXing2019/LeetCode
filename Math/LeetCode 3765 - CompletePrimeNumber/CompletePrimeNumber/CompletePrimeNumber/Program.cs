2d18cd732a355db138dd9f8ad614c23e7661a699
Xing <x.xing.unsw@gmail.com>
2025-12-07 21:55:39 +1100

    init

---
+++ Math/LeetCode 3765 - CompletePrimeNumber/CompletePrimeNumber/CompletePrimeNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿var num = 73;
+Console.WriteLine(CompletePrime(num));
+
+bool CompletePrime(int num)
+{
+    int prefix = 0, suffix = 0, pow = 1;
+    var str = num.ToString();
+    for (int i = 0; i < str.Length; i++)
+    {
+        prefix = prefix * 10 + str[i] - '0';
+        if (!IsPrime(prefix)) return false;
+        suffix += (str[^(i + 1)] - '0') * pow;
+        pow *= 10;
+        if (!IsPrime(suffix)) return false;
+    }
+    return true;
+}
+
+bool IsPrime(int num)
+{
+    if (num == 1) return false;
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i != 0) continue;
+        return false;
+    }
+    return true;
+}
