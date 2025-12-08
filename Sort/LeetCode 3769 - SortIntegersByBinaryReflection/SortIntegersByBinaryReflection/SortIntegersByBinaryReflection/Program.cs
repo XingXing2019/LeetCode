1f4c296798bd3a2657047b8258a9416014572462
Xing <x.xing.unsw@gmail.com>
2025-12-08 22:43:46 +1100

    init

---
+++ Sort/LeetCode 3769 - SortIntegersByBinaryReflection/SortIntegersByBinaryReflection/SortIntegersByBinaryReflection/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] SortByReflection(int[] nums)
+{
+    return nums.OrderBy(Reverse).ThenBy(x => x).ToArray();
+}
+
+int Reverse(int num)
+{
+    var res = 0;
+    while (num != 0)
+    {
+        res = res * 2 + num % 2;
+        num /= 2;
+    }
+    return res;
+}
