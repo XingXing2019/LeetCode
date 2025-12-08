411ae7f4e64ff322b385d641274b768924004697
Xing <x.xing.unsw@gmail.com>
2025-12-08 22:39:58 +1100

    init

---
+++ Greedy/LeetCode 3767 - MaximizePointsAfterChoosingKTasks/MaximizePoints/MaximizePoints/Program.cs
@@ -0,0 +1,14 @@
+﻿long MaxPoints(int[] technique1, int[] technique2, int k)
+{
+    var res = 0L;
+    var diff = new int[technique1.Length];
+    for (int i = 0; i < technique1.Length; i++)
+    {
+        diff[i] = technique2[i] - technique1[i];
+        res += technique1[i];
+    }
+    Array.Sort(diff);
+    for (int i = k; i < diff.Length; i++)
+        res += Math.Max(0, diff[i]);
+    return res;
+}
