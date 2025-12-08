7d2f350066a1c69f52e33d9719ab8164929452db
Xing <x.xing.unsw@gmail.com>
2025-12-08 22:31:32 +1100

    init

---
+++ BitManipulation/LeetCode 3766 - MinimumOperationsToMakeBinaryPalindrome/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] nums = { 4 };
+Console.WriteLine(MinOperations(nums));
+
+int[] MinOperations(int[] nums)
+{
+    var res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = 0; j < nums[i]; j++)
+        {
+            if (IsPalindrome(nums[i] + j) || IsPalindrome(nums[i] - j))
+            {
+                res[i] = j;
+                break;
+            }
+        }
+    }
+    return res;
+}
+
+bool IsPalindrome(int num)
+{
+    var digits = new List<int>();
+    while (num != 0)
+    {
+        digits.Add(num % 2);
+        num /= 2;
+    }
+    int li = 0, hi = digits.Count - 1;
+    while (li < hi)
+    {
+        if (digits[li++] == digits[hi--]) continue;
+        return false;
+    }
+    return true;
+}
