30f37da3a80d1d59ac2778b8d758e5714c4ab265
Xing <x.xing.unsw@gmail.com>
2025-12-08 22:18:54 +1100

    init

---
+++ String/LeetCode 3773 - MaximumNumberOfEqualLengthRuns/MaximumNumberOfEqualLengthRuns/MaximumNumberOfEqualLengthRuns/Program.cs
@@ -0,0 +1,24 @@
+﻿var s = "hello";
+Console.WriteLine(MaxSameLengthRuns(s));
+
+int MaxSameLengthRuns(string s)
+{
+    var dict = new Dictionary<int, int>();
+    int count = 0, max = 0;
+    var letter = s[0];
+    foreach (var l in s)
+    {
+        if (l == letter)
+            count++;
+        else
+        {
+            if (!dict.ContainsKey(count))
+                dict[count] = 0;
+            dict[count]++;
+            max = Math.Max(max, dict[count]);
+            letter = l;
+            count = 1;
+        }
+    }
+    return Math.Max(max, dict.GetValueOrDefault(count, 0) + 1);
+}
