47a2fcd8f10a742e4de49e9da0619657ddc68543
Xing <x.xing.unsw@gmail.com>
2025-12-09 21:48:46 +1100

    init

---
+++ Math/LeetCode 3770 - LargestPrimeFromConsecutivePrimeSum/LargestPrime/LargestPrime/Program.cs
@@ -0,0 +1,33 @@
+﻿int LargestPrime(int n)
+{
+    var primes = GetPrimes(n);
+    int res = 0, sum = 0;
+    foreach (var prime in primes)
+    {
+        sum += prime;
+        if (sum > n || !primes.Contains(sum)) continue;
+        res = Math.Max(res, sum);
+    }
+    return res;
+}
+
+HashSet<int> GetPrimes(int n)
+{
+    var dp = new bool[n + 1];
+    Array.Fill(dp, true);
+    for (int i = 2; i < dp.Length; i++)
+    {
+        if (!dp[i]) continue;
+        for (int j = 2; i * j < dp.Length; j++)
+        {
+            dp[i * j] = false;
+        }
+    }
+    var res = new HashSet<int>();
+    for (int i = 2; i < dp.Length; i++)
+    {
+        if (!dp[i]) continue;
+        res.Add(i);
+    }
+    return res;
+}
