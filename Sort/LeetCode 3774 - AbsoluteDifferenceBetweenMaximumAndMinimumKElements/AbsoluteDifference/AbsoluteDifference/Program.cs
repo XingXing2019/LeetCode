983986824d0ac1b3951cb7c7595c5f7ebda9eafc
Xing <x.xing.unsw@gmail.com>
2025-12-14 21:06:58 +1100

    init

---
+++ Sort/LeetCode 3774 - AbsoluteDifferenceBetweenMaximumAndMinimumKElements/AbsoluteDifference/AbsoluteDifference/Program.cs
@@ -0,0 +1,8 @@
+﻿int AbsDifference(int[] nums, int k)
+{
+    Array.Sort(nums);
+    var res = 0;
+    for (int i = 0; i < k; i++)
+        res += nums[i] - nums[^(i + 1)];
+    return Math.Abs(res);
+}
