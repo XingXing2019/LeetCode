ae33fb864089002aca0a35fb5c66d313a90b852e
Xing <x.xing.unsw@gmail.com>
2025-12-14 21:14:41 +1100

    init

---
+++ String/LeetCode 3775 - ReverseWordsWithSameVowelCount/ReverseWordsWithSameVowelCount/ReverseWordsWithSameVowelCount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+string ReverseWords(string s)
+{
+    var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+    var words = s.Split(' ');
+    var first = 0;
+    for (var i = 0; i < words.Length; i++)
+    {
+        var count = 0;
+        var temp = new StringBuilder();
+        for (int j = words[i].Length - 1; j >= 0; j--)
+        {
+            if (vowels.Contains(words[i][j]))
+                count++;
+            temp.Append(words[i][j]);
+        }
+        if (i == 0)
+            first = count;
+        else if (count == first)
+            words[i] = temp.ToString();
+    }
+    return string.Join(' ', words);
+}
