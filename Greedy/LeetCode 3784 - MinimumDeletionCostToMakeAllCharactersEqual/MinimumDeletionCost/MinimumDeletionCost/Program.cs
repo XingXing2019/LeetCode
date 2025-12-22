34fc7b463c0fb6fb7e714aef9da1f748a78f76a0
Xing <x.xing.unsw@gmail.com>
2025-12-22 21:23:58 +1100

    init

---
+++ Greedy/LeetCode 3784 - MinimumDeletionCostToMakeAllCharactersEqual/MinimumDeletionCost/MinimumDeletionCost/Program.cs
@@ -0,0 +1,15 @@
+﻿long MinCost(string s, int[] cost)
+{
+    long total = 0, res = long.MaxValue;
+    var dict = new Dictionary<int, long>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (!dict.ContainsKey(s[i]))
+            dict[s[i]] = 0;
+        dict[s[i]] += cost[i];
+        total += cost[i];
+    }
+    foreach (var num in dict.Keys)
+        res = Math.Min(res, total - dict[num]);
+    return res;
+}
