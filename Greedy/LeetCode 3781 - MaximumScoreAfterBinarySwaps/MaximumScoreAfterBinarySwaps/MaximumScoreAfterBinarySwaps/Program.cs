28f8361fb6c6fa23cac968c14c6ab29bad658c97
Xing <x.xing.unsw@gmail.com>
2025-12-22 21:16:37 +1100

    init

---
+++ Greedy/LeetCode 3781 - MaximumScoreAfterBinarySwaps/MaximumScoreAfterBinarySwaps/MaximumScoreAfterBinarySwaps/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums = { 3 };
+var s = "1";
+Console.WriteLine(MaximumScore(nums, s));
+long MaximumScore(int[] nums, string s)
+{
+    var res = 0L;
+    var heap = new PriorityQueue<int, int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        heap.Enqueue(nums[i], -nums[i]);
+        if (s[i] == '1')
+        {
+            res += heap.Dequeue();
+        }
+    }
+    return res;
+}
