ff31f9d09c0f7f921f2068e9b872e3bd298e19f6
Xing <x.xing.unsw@gmail.com>
2025-12-22 21:19:42 +1100

    init

---
+++ Math/LeetCode 3783 - MirrorDistanceOfAnInteger/MirrorDistanceOfAnInteger/MirrorDistanceOfAnInteger/Program.cs
@@ -0,0 +1,10 @@
+﻿int MirrorDistance(int n)
+{
+    int copy = n, reversed = 0;
+    while (copy != 0)
+    {
+        reversed = reversed * 10 + copy % 10;
+        copy /= 10;
+    }
+    return Math.Abs(reversed - n);
+}
