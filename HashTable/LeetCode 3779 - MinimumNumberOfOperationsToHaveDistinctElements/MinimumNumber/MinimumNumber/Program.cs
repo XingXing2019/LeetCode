005444968b0b03d28469934d9891cfdeb1b064ca
Xing <x.xing.unsw@gmail.com>
2025-12-22 20:44:03 +1100

    init

---
+++ HashTable/LeetCode 3779 - MinimumNumberOfOperationsToHaveDistinctElements/MinimumNumber/MinimumNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] nums = { 3, 8, 3, 6, 5, 8 };
+Console.WriteLine(MinOperations(nums));
+
+int MinOperations(int[] nums)
+{
+    var dict = new Dictionary<int, List<int>>();
+    var res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (!dict.ContainsKey(nums[i]))
+            dict[nums[i]] = new List<int>();
+        dict[nums[i]].Add(i);
+    }
+    foreach (var pos in dict.Values)
+    {
+        if (pos.Count == 1) continue;
+        res = Math.Max(res, pos[^2] / 3 + 1);
+    }
+    return res;
+}
