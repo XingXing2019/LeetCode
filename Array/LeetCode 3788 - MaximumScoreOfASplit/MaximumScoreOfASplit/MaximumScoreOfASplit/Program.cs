8dc94e860fc9ab2e00144429f97fc4db6c263eab
Xing <x.xing.unsw@gmail.com>
2026-01-01 19:58:27 +1100

    init

---
+++ Array/LeetCode 3788 - MaximumScoreOfASplit/MaximumScoreOfASplit/MaximumScoreOfASplit/Program.cs
@@ -0,0 +1,19 @@
+﻿long MaximumScore(int[] nums)
+{
+    var res = long.MinValue;
+    var prefix = new long[nums.Length];
+    var suffix = new long[nums.Length];
+    var min = nums[^1];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        prefix[i] = i == 0 ? nums[i] : prefix[i - 1] + nums[i];
+        min = Math.Min(min, nums[^(i + 1)]);
+        suffix[^(i + 1)] = min;
+    }
+    for (int i = 0; i < prefix.Length - 1; i++)
+    {
+        var score = prefix[i] - suffix[i + 1];
+        res = Math.Max(res, score);
+    }
+    return res;
+}
