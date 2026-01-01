82cdbb7071b1f21f9760bef8c8e00703606447a5
Xing <x.xing.unsw@gmail.com>
2026-01-01 19:47:12 +1100

    init

---
+++ Math/LeetCode 3789 - MinimumCostToAcquireRequiredItems/MinimumCostToAcquireRequiredItems/MinimumCostToAcquireRequiredItems/Program.cs
@@ -0,0 +1,8 @@
+﻿long MinimumCost(int cost1, int cost2, int costBoth, int need1, int need2)
+{
+    long op1 = (long)cost1 * need1 + (long)cost2 * need2;
+    long op2 = (long)costBoth * Math.Max(need1, need2);
+    long op3 = (long)costBoth * Math.Min(need1, need2) + (long)cost1 * (need1 - Math.Min(need1, need2)) + (long)cost2 * (need2 - Math.Min(need1, need2));
+    var ops = new List<long> { op1, op2, op3 };
+    return ops.Min();
+}
--- Math/LeetCode 3790 - SmallestAllOnesMultiple/SmallestAllOnesMultiple/SmallestAllOnesMultiple/Program.cs
+++ Math/LeetCode 3790 - SmallestAllOnesMultiple/SmallestAllOnesMultiple/SmallestAllOnesMultiple/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MinAllOneMultiple(int k)
+{
+    if (k % 2 == 0) return -1;
+    int reminder = 0, res = 0;
+    for (int i = 0; i < k; i++)
+    {
+        reminder = (reminder * 10 + 1) % k;
+        res++;
+        if (reminder == 0)
+        {
+            return res;
+        }
+    }
+    return -1;
+}
