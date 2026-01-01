bcd1b1e84b795f222de5d088f093abc10f4dcb5c
Xing <x.xing.unsw@gmail.com>
2026-01-01 19:35:41 +1100

    init

---
+++ Math/LeetCode 3792 - SumOfIncreasingProductBlocks/SumOfIncreasingProductBlocks/SumOfIncreasingProductBlocks/Program.cs
@@ -0,0 +1,18 @@
+﻿var n = 3;
+Console.WriteLine(SumOfBlocks(n));
+
+int SumOfBlocks(int n)
+{
+    long res = 0, mod = 1_000_000_000 + 7, cur = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        var temp = 1L;
+        for (int j = 0; j < i; j++)
+        {
+            cur++;
+            temp = temp * cur % mod;
+        }
+        res = (res + temp) % mod;
+    }
+    return (int)(res % mod);
+}
