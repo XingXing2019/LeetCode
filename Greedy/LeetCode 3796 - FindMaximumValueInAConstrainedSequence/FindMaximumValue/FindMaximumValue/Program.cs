510cf866a504756f64ce69104f139c3b964fd6da
Xing <x.xing.unsw@gmail.com>
2026-01-07 21:44:23 +1100

    init

---
+++ Greedy/LeetCode 3796 - FindMaximumValueInAConstrainedSequence/FindMaximumValue/FindMaximumValue/Program.cs
@@ -0,0 +1,13 @@
+﻿int FindMaxVal(int n, int[][] restrictions, int[] diff)
+{
+    var nums = new int[n];
+    Array.Fill(nums, int.MaxValue);
+    nums[0] = 0;
+    foreach (var restriction in restrictions)
+        nums[restriction[0]] = restriction[1];
+    for (int i = 1; i < nums.Length; i++)
+        nums[i] = Math.Min(nums[i], nums[i - 1] + diff[i - 1]);
+    for (int i = nums.Length - 2; i >= 0; i--)
+        nums[i] = Math.Min(nums[i], nums[i + 1] + diff[i]);
+    return nums.Max();
+}
