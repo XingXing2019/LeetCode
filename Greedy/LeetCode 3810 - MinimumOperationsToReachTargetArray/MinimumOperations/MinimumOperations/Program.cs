43ef764ca986222a5167ec460d1d570426b9a3a5
Xing <x.xing.unsw@gmail.com>
2026-01-19 21:51:17 +1100

    init

---
+++ Greedy/LeetCode 3810 - MinimumOperationsToReachTargetArray/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] nums = { 7, 3, 7 };
+int[] target = { 5, 5, 9 };
+Console.WriteLine(MinOperations(nums, target));
+int MinOperations(int[] nums, int[] target)
+{
+    var diff = new Dictionary<int, List<int>>();
+    var res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] == target[i]) continue;
+        if (!diff.ContainsKey(nums[i]))
+            diff[nums[i]] = new List<int>();
+        diff[nums[i]].Add(i);
+    }
+    foreach (var indexes in diff.Values)
+    {
+        foreach (var index in indexes)
+        {
+            if (nums[index] == target[index]) continue;
+            var p = index;
+            while (p < nums.Length && nums[p] == nums[index])
+            {
+                nums[p] = target[p++];
+            }
+        }
+        res++;
+    }
+    return res;
+}
