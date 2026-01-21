1a731e91472ac6522eb2b1903805baf75a39d68e
Xing <x.xing.unsw@gmail.com>
2026-01-21 20:54:09 +1100

    init

---
+++ String/LeetCode 3813 - VowelConsonantScore/VowelConsonantScore/VowelConsonantScore/Program.cs
@@ -0,0 +1,14 @@
+﻿int VowelConsonantScore(string s)
+{
+    var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+    int v = 0, c = 0;
+    foreach (var l in s)
+    {
+        if (!char.IsLetter(l)) continue;
+        if (vowels.Contains(l))
+            v++;
+        else
+            c++;
+    }
+    return c == 0 ? 0 : v / c;
+}
