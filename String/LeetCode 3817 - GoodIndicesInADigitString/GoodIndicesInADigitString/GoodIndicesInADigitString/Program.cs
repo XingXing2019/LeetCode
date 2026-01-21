0c4487b9f7c7e448286cd5a0a111372c235428eb
Xing <x.xing.unsw@gmail.com>
2026-01-21 20:50:27 +1100

    init

---
+++ String/LeetCode 3817 - GoodIndicesInADigitString/GoodIndicesInADigitString/GoodIndicesInADigitString/Program.cs
@@ -0,0 +1,11 @@
+﻿IList<int> GoodIndices(string s)
+{
+    var res = new List<int>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        var target = i.ToString();
+        if (target != s.Substring(i - target.Length + 1, target.Length)) continue;
+        res.Add(i);
+    }
+    return res;
+}
