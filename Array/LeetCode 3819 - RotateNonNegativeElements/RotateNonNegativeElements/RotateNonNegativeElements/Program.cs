8e68aa3c8d51de4f620e967494f9c3ed333de02e
Xing <x.xing.unsw@gmail.com>
2026-01-27 21:04:14 +1100

    init

---
+++ Array/LeetCode 3819 - RotateNonNegativeElements/RotateNonNegativeElements/RotateNonNegativeElements/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] RotateElements(int[] nums, int k)
+{
+    var nonNeg = nums.Where(x => x >= 0).ToArray();
+    if (nonNeg.Length == 0) return nums;
+    k %= nonNeg.Length;
+    var rotated = new int[nonNeg.Length];
+    for (int i = 0; i < rotated.Length; i++)
+        rotated[i] = nonNeg[(i + k) % nonNeg.Length];
+    var index = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] < 0) continue;
+        nums[i] = rotated[index++];
+    }
+    return nums;
+}
