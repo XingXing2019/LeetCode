ad5f8aca07db85a39aaf804aed71e8981078ded2
Xing <x.xing.unsw@gmail.com>
2026-02-04 14:05:24 +1100

    init

---
+++ String/LeetCode 3823 - ReverseLettersThenSpecialCharactersInAString/ReverseLettersThenSpecialCharactersInAString/ReverseLettersThenSpecialCharactersInAString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+string ReverseByType(string s)
+{
+    var letters = new List<char>();
+    var chars = new List<char>();
+    foreach (var l in s)
+    {
+        if (char.IsLetter(l))
+            letters.Add(l);
+        else
+            chars.Add(l);
+    }
+    var res = new StringBuilder();
+    int p1 = letters.Count - 1, p2 = chars.Count - 1;
+    foreach (var l in s)
+        res.Append(char.IsLetter(l) ? letters[p1--] : chars[p2--]);
+    return res.ToString();
+}
