748ff9ec2a701fb3d3b393d29054bd82e91f06db
Xing <x.xing.unsw@gmail.com>
2026-02-04 13:45:49 +1100

    init

---
+++ Array/LeetCode 3640 - TrionicArrayII/TrionicArrayII/TrionicArrayII/Program.cs
@@ -0,0 +1,50 @@
+﻿int[] nums = { 1, 4, 2, 2, 3, 1, 2 };
+Console.WriteLine(MaxSumTrionic(nums));
+
+long MaxSumTrionic(int[] nums)
+{
+    var isDecreasing = nums[1] < nums[0];
+    var decreases = new List<long[]>();
+    long start = 0, sum = 0, len = 0;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] < nums[i - 1])
+        {
+            if (!isDecreasing)
+                isDecreasing = !isDecreasing;
+            sum += nums[i - 1];
+            len++;
+        }
+        else
+        {
+            if (isDecreasing)
+            {
+                isDecreasing = !isDecreasing;
+                decreases.Add(new []{start, sum + nums[i - 1], len + 1 });
+            }
+            sum = 0;
+            start = i;
+            len = 0;
+        }
+    }
+    var res = long.MinValue;
+    foreach (var decrease in decreases)
+    {
+        if (decrease[0] == 0) continue;
+        long l = decrease[0] - 2, r = decrease[0] + decrease[2] + 1;
+        if (nums[decrease[0] - 1] == nums[decrease[0]] || nums[decrease[0] + decrease[2]] == nums[decrease[0] + decrease[2] - 1]) continue;
+        long sumL = nums[decrease[0] - 1], sumR = nums[decrease[0] + decrease[2]], maxL = sumL, maxR = sumR;
+        while (l >= 0 && nums[l] < nums[l + 1])
+        {
+            sumL += nums[l--];
+            maxL = Math.Max(maxL, sumL);
+        }
+        while (r < nums.Length && nums[r] > nums[r - 1])
+        {
+            sumR += nums[r++];
+            maxR = Math.Max(maxR, sumR);
+        }
+        res = Math.Max(res, decrease[1] + maxL + maxR);
+    }
+    return res;
+}
---
+++ Heap/LeetCode 3013 - DivideAnArrayIntoSubarraysWithMinimumCostII/DivideAnArray/DivideAnArray/Program.cs
@@ -0,0 +1,85 @@
+﻿int[] nums = { 10, 8, 18, 9 };
+int k = 3, dist = 1;
+Console.WriteLine(MinimumCost(nums, k, dist));
+
+long MinimumCost(int[] nums, int k, int dist)
+{
+    var minHeap = new SortedDictionary<int, int>();
+    var maxHeap = new SortedDictionary<int, int>();
+    int count = 0;
+    long res = 0L, sum = 0L;
+    for (int i = 1; i < dist + 2; i++)
+    {
+        if (!maxHeap.ContainsKey(nums[i]))
+            maxHeap[nums[i]] = 0;
+        maxHeap[nums[i]]++;
+        count++;
+        var max = count > k - 1 ? maxHeap.Keys.Last() : 0;
+        if (count > k - 1)
+        {
+            maxHeap[max]--;
+            if (maxHeap[max] == 0)
+                maxHeap.Remove(max);
+            if (!minHeap.ContainsKey(max))
+                minHeap[max] = 0;
+            minHeap[max]++;
+            count--;
+        }
+        sum += nums[i] - max;
+        res = sum;
+    }
+    for (int i = dist + 2; i < nums.Length; i++)
+    {
+        int add = nums[i], remove = nums[i - dist - 1];
+        if (maxHeap.ContainsKey(remove))
+        {
+            maxHeap[remove]--;
+            count--;
+            if (maxHeap[remove] == 0)
+                maxHeap.Remove(remove);
+            sum -= remove;
+        }
+        else
+        {
+            minHeap[remove]--;
+            if (minHeap[remove] == 0)
+                minHeap.Remove(remove);
+        }
+        if (!maxHeap.ContainsKey(nums[i]))
+            maxHeap[nums[i]] = 0;
+        maxHeap[nums[i]]++;
+        count++;
+        var max = count > k - 1 ? maxHeap.Keys.Last() : 0;
+        if (count > k - 1)
+        {
+            maxHeap[max]--;
+            if (maxHeap[max] == 0)
+                maxHeap.Remove(max);
+            if (!minHeap.ContainsKey(max))
+                minHeap[max] = 0;
+            minHeap[max]++;
+            count--;
+        }
+        sum += nums[i] - max;
+        var min = minHeap.Keys.Any() ? minHeap.Keys.First() : int.MaxValue;
+        max = maxHeap.Keys.Last();
+        if (max > min)
+        {
+            maxHeap[max]--;
+            if (maxHeap[max] == 0)
+                maxHeap.Remove(max);
+            minHeap[min]--;
+            if (minHeap[min] == 0)
+                minHeap.Remove(min);
+            if (!maxHeap.ContainsKey(min))
+                maxHeap[min] = 0;
+            maxHeap[min]++;
+            if (!minHeap.ContainsKey(max))
+                minHeap[max] = 0;
+            minHeap[max]++;
+            sum += min - max;
+        }
+        res = Math.Min(res, sum);
+    }
+    return res + nums[0];
+}
