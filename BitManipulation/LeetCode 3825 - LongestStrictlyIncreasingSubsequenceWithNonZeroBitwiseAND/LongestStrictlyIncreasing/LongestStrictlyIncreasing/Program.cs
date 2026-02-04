32f6c42a3ca6510274bc579b6160dc8f0552d558
Xing <x.xing.unsw@gmail.com>
2026-02-04 19:22:29 +1100

    init

---
+++ BitManipulation/LeetCode 3825 - LongestStrictlyIncreasingSubsequenceWithNonZeroBitwiseAND/LongestStrictlyIncreasing/LongestStrictlyIncreasing/Program.cs
@@ -0,0 +1,23 @@
+﻿int LongestSubsequence(int[] nums)
+{
+    var res = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        var list = new List<int>();
+        foreach (var num in nums)
+        {
+            if (((num >> i) & 1) == 0) continue;
+            if (list.Count == 0 || list[^1] < num)
+                list.Add(num);
+            else
+            {
+                var index = list.BinarySearch(num);
+                if (index < 0) index = ~index;
+                list[index] = num;
+            }
+        }
+        res = Math.Max(res, list.Count);
+    }
+    return res;
+}
+
