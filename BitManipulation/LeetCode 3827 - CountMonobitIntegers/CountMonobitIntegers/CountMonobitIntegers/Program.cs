33f8e640b0aed73a23574ec42acac1546e7b362c
Xing <x.xing.unsw@gmail.com>
2026-02-04 16:44:21 +1100

    init

---
+++ BitManipulation/LeetCode 3827 - CountMonobitIntegers/CountMonobitIntegers/CountMonobitIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿var n = 1;
+Console.WriteLine(CountMonobit(n));
+
+int CountMonobit(int n)
+{
+    var res = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (!IsMonobit(i)) continue;
+        res++;
+    }
+    return res;
+}
+
+bool IsMonobit(int num)
+{
+    if (num == 0) return true;
+    int one = 0, zero = 0;
+    while (num != 0)
+    {
+        if (num % 2 == 0)
+            zero++;
+        else
+            one++;
+        num >>= 1;
+    }
+    return one == 0 || zero == 0;
+}
