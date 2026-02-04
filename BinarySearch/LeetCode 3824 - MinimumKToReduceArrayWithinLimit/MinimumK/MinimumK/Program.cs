b0b2eb3be4b61bfa40420b1d18c49a6f4b4eb792
Xing <x.xing.unsw@gmail.com>
2026-02-04 15:01:17 +1100

    init

---
+++ BinarySearch/LeetCode 3824 - MinimumKToReduceArrayWithinLimit/MinimumK/MinimumK/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] nums = { 1, 1, 1, 1, 1 };
+Console.WriteLine(MinimumK(nums));
+
+int MinimumK(int[] nums)
+{
+    int li = 1, hi = int.MaxValue;
+    while (li <= hi)
+    {
+        var mid = li + (hi - li) / 2;
+        if (IsValid(nums, mid))
+            hi = mid - 1;
+        else
+            li = mid + 1;
+    }
+    return li;
+}
+
+bool IsValid(int[] nums, int k)
+{
+    var count = 0;
+    foreach (var num in nums)
+        count += num % k == 0 ? num / k : num / k + 1;
+    return count <= (long)k * k;
+}
