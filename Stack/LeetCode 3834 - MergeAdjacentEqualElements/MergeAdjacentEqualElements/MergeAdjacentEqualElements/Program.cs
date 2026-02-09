c6ab16f70fac8dbf043c88e92ca8bb24bbdfd84a
Xing <x.xing.unsw@gmail.com>
2026-02-09 20:37:07 +1100

    init

---
+++ Stack/LeetCode 3834 - MergeAdjacentEqualElements/MergeAdjacentEqualElements/MergeAdjacentEqualElements/Program.cs
@@ -0,0 +1,12 @@
+﻿IList<long> MergeAdjacent(int[] nums)
+{
+    var stack = new Stack<long>();
+    foreach (var num in nums)
+    {
+        long cur = num;
+        while (stack.Count != 0 && stack.Peek() == cur)
+            cur += stack.Pop();
+        stack.Push(cur);
+    }
+    return stack.Reverse().ToList();
+}
