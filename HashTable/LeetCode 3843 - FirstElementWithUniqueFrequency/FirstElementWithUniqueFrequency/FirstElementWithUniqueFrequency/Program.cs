62984a957d3c44312f1a30d64095f0882019e1c1
Xing <x.xing.unsw@gmail.com>
2026-02-15 19:42:49 +1100

    init

---
+++ HashTable/LeetCode 3843 - FirstElementWithUniqueFrequency/FirstElementWithUniqueFrequency/FirstElementWithUniqueFrequency/Program.cs
@@ -0,0 +1,11 @@
+﻿int FirstUniqueFreq(int[] nums)
+{
+    var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    var uniqueFreq = new HashSet<int>(freq.GroupBy(x => x.Value).Where(x => x.Count() == 1).Select(x => x.Key));
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (uniqueFreq.Contains(freq[nums[i]]))
+            return nums[i];
+    }
+    return -1;
+}
