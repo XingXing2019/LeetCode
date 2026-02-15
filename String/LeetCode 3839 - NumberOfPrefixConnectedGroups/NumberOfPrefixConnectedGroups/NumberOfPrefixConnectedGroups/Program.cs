fc7ab4b6872ab8b858acad2b5abc211df58e5c7a
Xing <x.xing.unsw@gmail.com>
2026-02-15 19:25:33 +1100

    init

---
+++ String/LeetCode 3839 - NumberOfPrefixConnectedGroups/NumberOfPrefixConnectedGroups/NumberOfPrefixConnectedGroups/Program.cs
@@ -0,0 +1,13 @@
+﻿int PrefixConnected(string[] words, int k)
+{
+    var dict = new Dictionary<string, int>();
+    foreach (var word in words)
+    {
+        if (word.Length < k) continue;
+        var prefix = word.Substring(0, k);
+        if (!dict.ContainsKey(prefix))
+            dict[prefix] = 0;
+        dict[prefix]++;
+    }
+    return dict.Where(x => x.Value != 1).ToList().Count;
+}
