00f9541f7698e3eba6dd08b01a515cd074acd307
Xing <x.xing.unsw@gmail.com>
2026-02-15 19:32:17 +1100

    init

---
+++ DynamicProgramming/LeetCode 3840 - HouseRobberV/HouseRobberV/HouseRobberV/Program.cs
@@ -0,0 +1,11 @@
+﻿long Rob(int[] nums, int[] colors)
+{
+    var dp = new long[nums.Length];
+    dp[0] = nums[0];
+    for (int i = 1; i < nums.Length; i++)
+    {
+        var pre = i == 1 ? 0 : dp[i - 2];
+        dp[i] = colors[i] == colors[i - 1] ? Math.Max(pre + nums[i], dp[i - 1]) : dp[i - 1] + nums[i];
+    }
+    return dp[^1];
+}
