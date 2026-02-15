fe814674628fe975c9e0a33f97325b9ffa232a8d
Xing <x.xing.unsw@gmail.com>
2026-02-15 19:10:03 +1100

    init

---
+++ BinarySearch/LeetCode 3837 - DelayedCountOfEqualElements/DelayedCountOfEqualElements/DelayedCountOfEqualElements/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] DelayedCount(int[] nums, int k)
+{
+    var dict = new Dictionary<int, List<int>>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (!dict.ContainsKey(nums[i]))
+            dict[nums[i]] = new List<int>();
+        dict[nums[i]].Add(i);
+    }
+    var res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var indexes = dict[nums[i]];
+        var index = indexes.BinarySearch(i + k + 1);
+        if (index < 0) index = ~index;
+        res[i] = Math.Max(0, indexes.Count - index);
+    }
+    return res;
+}
