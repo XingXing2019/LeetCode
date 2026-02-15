09fcbe98f052cf26093233736fd7ef11c377d7a1
Xing <x.xing.unsw@gmail.com>
2026-02-15 19:15:45 +1100

    init

---
+++ String/LeetCode 3838 - WeightedWordMapping/WeightedWordMapping/WeightedWordMapping/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+string MapWordWeights(string[] words, int[] weights)
+{
+    var res = new StringBuilder();
+    foreach (var word in words)
+    {
+        var sum = 0;
+        foreach (var l in word)
+            sum += weights[l - 'a'];
+        var letter = (char)(25 - sum % 26 + 'a');
+        res.Append(letter);
+    }
+    return res.ToString();
+}
