41fae9f0a106d01272f0e2cc80b8dc0837175596
Xing <x.xing.unsw@gmail.com>
2026-02-19 21:40:25 +1100

    init

---
+++ String/LeetCode 3846 - TotalDistanceToTypeAStringUsingOneFinger/TotalDistance/TotalDistance/Program.cs
@@ -0,0 +1,21 @@
+﻿int TotalDistance(string s)
+{
+    var keyboard = new[] { "qwertyuiop", "asdfghjkl", "zxcvbnm" };
+    var dict = new Dictionary<char, int[]>();
+    for (int i = 0; i < keyboard.Length; i++)
+    {
+        for (int j = 0; j < keyboard[i].Length; j++)
+        {
+            dict[keyboard[i][j]] = new int[] { i, j };
+        }
+    }
+    int x = 1, y = 0, res = 0;
+    foreach (var l in s)
+    {
+        var pos = dict[l];
+        res += Math.Abs(pos[0] - x) + Math.Abs(pos[1] - y);
+        x = pos[0];
+        y = pos[1];
+    }
+    return res;
+}
