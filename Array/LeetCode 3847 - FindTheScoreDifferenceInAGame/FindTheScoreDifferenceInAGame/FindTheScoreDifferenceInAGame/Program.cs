2fdc7522c09a8b677fa9e428eb79b9dc4b412cf0
Xing <x.xing.unsw@gmail.com>
2026-02-23 21:22:30 +1100

    init

---
+++ Array/LeetCode 3847 - FindTheScoreDifferenceInAGame/FindTheScoreDifferenceInAGame/FindTheScoreDifferenceInAGame/Program.cs
@@ -0,0 +1,13 @@
+﻿int ScoreDifference(int[] nums)
+{
+    int res = 0, turn = 1;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if ((i + 1) % 6 == 0)
+            turn *= -1;
+        if (nums[i] % 2 != 0)
+            turn *= -1;
+        res += nums[i] * turn;
+    }
+    return res;
+}
