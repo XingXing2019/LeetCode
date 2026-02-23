5f16ea604248f091d042e945a8687da5febfb745
Xing <x.xing.unsw@gmail.com>
2026-02-23 21:28:51 +1100

    init

---
+++ Math/LeetCode 3848 - CheckDigitorialPermutation/CheckDigitorialPermutation/CheckDigitorialPermutation/Program.cs
@@ -0,0 +1,27 @@
+﻿bool IsDigitorialPermutation(int n)
+{
+    int sum = 0, copy = n;
+    var digits = new int[10];
+    while (copy != 0)
+    {
+        var mod = copy % 10;
+        digits[mod]++;
+        sum += GetFactorial(mod);
+        copy /= 10;
+    }
+    while (sum != 0)
+    {
+        var mod = sum % 10;
+        digits[mod]--;
+        sum /= 10;
+    }
+    return digits.All(x => x == 0);
+}
+
+int GetFactorial(int n)
+{
+    var res = 1;
+    for (int i = 1; i <= n; i++)
+        res *= i;
+    return res;
+}
