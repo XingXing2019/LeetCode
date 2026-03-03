c7f3180563fc2e2efde4b06681dc7f6650343fa9
Xing <x.xing.unsw@gmail.com>
2026-03-03 22:06:42 +1100

    init

---
+++ Math/LeetCode 3857 - MinimumCostToSplitIntoOnes/MinimumCostToSplitIntoOnes/MinimumCostToSplitIntoOnes/Program.cs
@@ -0,0 +1,27 @@
+﻿var n = 3;
+Console.WriteLine(MinCost(n));
+
+int MinCost(int n)
+{
+    var res = 0;
+    var ones = new int[n];
+    Array.Fill(ones, 1);
+    while (ones.Length != 1)
+    {
+        var temp = new List<int>();
+        for (int i = 0; i < ones.Length; i += 2)
+        {
+            if (i == ones.Length - 1)
+            {
+                temp.Add(ones[i]);
+            }
+            else
+            {
+                temp.Add(ones[i] + ones[i + 1]);
+                res += ones[i] * ones[i + 1];
+            }
+        }
+        ones = temp.ToArray();
+    }
+    return res;
+}
