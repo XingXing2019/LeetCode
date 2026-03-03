7d798ab27dc3691f2ce0ea7115887f39a39ea6d1
Xing <x.xing.unsw@gmail.com>
2026-03-03 21:55:09 +1100

    init

---
+++ String/LeetCode 3856 - TrimTrailingVowels/TrimTrailingVowels/TrimTrailingVowels/Program.cs
@@ -0,0 +1,15 @@
+﻿string TrimTrailingVowels(string s)
+{
+    var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+    var res = "";
+    var found = false;
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        if (!vowels.Contains(s[i]) || found)
+        {
+            found = true;
+            res = s[i] + res;
+        }
+    }
+    return res;
+}
