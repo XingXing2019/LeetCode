a355623e86b6585a12c07a412b7e07ea0ea701c9
Xing <x.xing.unsw@gmail.com>
2026-03-03 21:49:08 +1100

    init

---
+++ String/LeetCode 3853 - MergeCloseCharacters/MergeCloseCharacters/MergeCloseCharacters/Program.cs
@@ -0,0 +1,25 @@
+﻿var s = "aaaa";
+var k = 2;
+Console.WriteLine(MergeCharacters(s, k));
+
+string MergeCharacters(string s, int k)
+{
+    var indexes = new int[26];
+    Array.Fill(indexes, -s.Length - 1);
+    var remove = 0;
+    var stack = new Stack<char>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        var index = i - remove;
+        if (index - indexes[s[i] - 'a'] <= k)
+        {
+            remove++;
+        }
+        else
+        {
+            indexes[s[i] - 'a'] = index;
+            stack.Push(s[i]);
+        }
+    }
+    return string.Join("", stack.Reverse());
+}
