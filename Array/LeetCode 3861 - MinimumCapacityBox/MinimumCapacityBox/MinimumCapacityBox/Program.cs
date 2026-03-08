908039eced3f44dd49de2261d8e72e20865c2ae3
Xing <x.xing.unsw@gmail.com>
2026-03-08 19:37:31 +1100

    init

---
+++ Array/LeetCode 3861 - MinimumCapacityBox/MinimumCapacityBox/MinimumCapacityBox/Program.cs
@@ -0,0 +1,14 @@
+﻿int MinimumIndex(int[] capacity, int itemSize)
+{
+    int size = int.MaxValue, index = -1;
+    for (int i = 0; i < capacity.Length; i++)
+    {
+        if (capacity[i] < itemSize) continue;
+        if (capacity[i] < size)
+        {
+            size = capacity[i];
+            index = i;
+        }
+    }
+    return index;
+}
