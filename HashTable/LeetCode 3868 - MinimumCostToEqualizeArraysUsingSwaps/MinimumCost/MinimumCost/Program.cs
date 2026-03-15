467f5216ed4827a7556c6596de4bd600655bfbfc
Xing <x.xing.unsw@gmail.com>
2026-03-15 21:32:11 +1100

    init

---
+++ HashTable/LeetCode 3868 - MinimumCostToEqualizeArraysUsingSwaps/MinimumCost/MinimumCost/Program.cs
@@ -0,0 +1,16 @@
+﻿int MinCost(int[] nums1, int[] nums2)
+{
+    var freq1 = nums1.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    var freq2 = nums2.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    var set = new HashSet<int>(nums1);
+    set.UnionWith(nums2);
+    var res = 0;
+    foreach (var num in set)
+    {
+        var f1 = freq1.GetValueOrDefault(num, 0);
+        var f2 = freq2.GetValueOrDefault(num, 0);
+        if ((f1 + f2) % 2 != 0) return -1;
+        res += Math.Abs(f1 - f2) / 2;
+    }
+    return res / 2;
+}
