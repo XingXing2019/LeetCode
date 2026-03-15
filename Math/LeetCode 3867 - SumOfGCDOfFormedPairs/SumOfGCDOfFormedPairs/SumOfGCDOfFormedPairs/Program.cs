c1e3a91db715e6d27be29f70571fe7c6e491e921
Xing <x.xing.unsw@gmail.com>
2026-03-15 21:20:18 +1100

    init

---
+++ Math/LeetCode 3867 - SumOfGCDOfFormedPairs/SumOfGCDOfFormedPairs/SumOfGCDOfFormedPairs/Program.cs
@@ -0,0 +1,24 @@
+﻿long GcdSum(int[] nums)
+{
+    var max = nums[0];
+    var prefixMax = new long[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        max = Math.Max(max, nums[i]);
+        prefixMax[i] = max;
+    }
+    var prefixGcd = new long[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+        prefixGcd[i] = GCD(prefixMax[i], nums[i]);
+    Array.Sort(prefixGcd);
+    int li = 0, hi = prefixGcd.Length - 1;
+    var res = 0L;
+    while (li < hi)
+        res += GCD(prefixGcd[li++], prefixGcd[hi--]);
+    return res;
+}
+
+long GCD(long a, long b)
+{
+    return a % b == 0 ? b : GCD(b, a % b);
+}
