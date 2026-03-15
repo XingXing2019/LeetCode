67cec61b4991e25ab90153ab07996589958dfc41
Xing <x.xing.unsw@gmail.com>
2026-03-15 21:03:37 +1100

    init

---
+++ Array/LeetCode 3865 - ReverseKSubarrays/ReverseKSubarrays/ReverseKSubarrays/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] ReverseSubarrays(int[] nums, int k)
+{
+    var size = nums.Length / k;
+    for (int i = 0; i < nums.Length; i += size)
+    {
+        int li = i, hi = i + size - 1;
+        while (li < hi)
+        {
+            (nums[li], nums[hi]) = (nums[hi++], nums[li--]);
+        }
+    }
+    return nums;
+}
---
+++ String/LeetCode 3863 - MinimumOperationsToSortAString/MinimumOperationsToSortAString/MinimumOperationsToSortAString/Program.cs
@@ -0,0 +1,13 @@
+﻿int MinOperations(string s)
+{
+    var sorted = string.Join("", s.OrderBy(x => x));
+    if (s == sorted) return 0;
+    if (s.Length == 2) return -1;
+    char max = 'a', min = 'z';
+    foreach (var l in s)
+    {
+        if (l > max) max = l;
+        if (l < min) min = l;
+    }
+    return sorted[^1] < min && sorted[0] > max ? 3 : 2;
+}
