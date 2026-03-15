16cd589c702ebbdb84d4b3f0ab717328f5e58460
Xing <x.xing.unsw@gmail.com>
2026-03-15 21:06:43 +1100

    init

---
+++ HashTable/LeetCode 3866 - FirstUniqueEvenElement/FirstUniqueEvenElement/FirstUniqueEvenElement/Program.cs
@@ -0,0 +1,10 @@
+﻿int FirstUniqueEven(int[] nums)
+{
+    var freq = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] % 2 != 0 || freq[nums[i]] > 1) continue;
+        return nums[i];
+    }
+    return -1;
+}
