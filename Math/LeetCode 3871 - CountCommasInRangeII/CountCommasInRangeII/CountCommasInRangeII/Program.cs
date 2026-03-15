3ad5b83c474e4d82e44a22e2007bf0a9bcc0240e
Xing <x.xing.unsw@gmail.com>
2026-03-15 22:28:58 +1100

    init

--- Math/LeetCode 3871 - CountCommasInRangeII/CountCommasInRangeII/CountCommasInRangeII/Program.cs
+++ Math/LeetCode 3871 - CountCommasInRangeII/CountCommasInRangeII/CountCommasInRangeII/Program.cs
@@ -5,7 +5,9 @@ long CountCommas(long n)
 {
     if (n < 1000) return 0;
     var res = 0L;
-    var nums = new long[] { 1_000, 1_000_000, 1_000_000_000, 1_000_000_000_000, 1_000_000_000_000_000, 1_000_000_000_000_000_000 };
+    var nums = new List<long> { 1000 };
+    for (int i = 0; i < 5; i++)
+        nums.Add(nums[^1] * 1000);
     for (int i = 1; i <= 5; i++)
     {
         var hi = Math.Min(n, nums[i] - 1);
