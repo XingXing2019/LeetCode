b45f2d1d40c698babcfeff0e5e899b9ae3933043
Xing <x.xing.unsw@gmail.com>
2026-04-12 09:59:35 +1000

    init

---
+++ Array/LeetCode 3895 - CountDigitAppearances/CountDigitAppearances/CountDigitAppearances/Program.cs
@@ -0,0 +1,15 @@
+﻿int CountDigitOccurrences(int[] nums, int digit)
+{
+    return nums.Sum(x => Count(x, digit));
+}
+
+int Count(int num, int digit)
+{
+    var res = 0;
+    while (num != 0)
+    {
+        if (num % 10 == digit) res++;
+        num /= 10;
+    }
+    return res;
+}
