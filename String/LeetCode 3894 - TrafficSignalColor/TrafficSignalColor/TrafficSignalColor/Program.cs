35b2b23b9d7409a405b6707726cd7a4ae6bda94f
Xing <x.xing.unsw@gmail.com>
2026-04-12 09:54:39 +1000

    init

---
+++ String/LeetCode 3894 - TrafficSignalColor/TrafficSignalColor/TrafficSignalColor/Program.cs
@@ -0,0 +1,6 @@
+﻿string TrafficSignal(int timer)
+{
+    if (timer > 90) return "Invalid";
+    if (timer > 30) return "Red";
+    return timer == 0 ? "Green" : timer == 30 ? "Orange" : "Invalid";
+}
