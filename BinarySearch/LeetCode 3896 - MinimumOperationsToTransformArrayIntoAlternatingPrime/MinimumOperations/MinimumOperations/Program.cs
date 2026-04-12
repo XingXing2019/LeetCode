1e54f4644ce9a69d644f1a8ac9798381c6c02adf
Xing <x.xing.unsw@gmail.com>
2026-04-12 10:15:56 +1000

    init

---
+++ BinarySearch/LeetCode 3896 - MinimumOperationsToTransformArrayIntoAlternatingPrime/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] nums = { 294 };
+Console.WriteLine(MinOperations(nums));
+
+int MinOperations(int[] nums)
+{
+    int res = 0, max = nums.Max();
+    var primes = GetPrimes(max + 100);
+    var list = primes.ToList();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (i % 2 == 0 && !primes.Contains(nums[i]))
+        {
+            var index = ~list.BinarySearch(nums[i]);
+            res += list[index] - nums[i];
+        }
+        else if (i % 2 != 0 && primes.Contains(nums[i]))
+        {
+            res += nums[i] == 2 ? 2 : 1;
+        }
+    }
+    return res;
+}
+
+HashSet<int> GetPrimes(int n)
+{
+    var dp = new bool[n + 1];
+    Array.Fill(dp, true);
+    for (int i = 2; i < dp.Length; i++)
+    {
+        if (!dp[i]) continue;
+        for (int j = 2; i * j < dp.Length; j++)
+        {
+            dp[i * j] = false;
+        }
+    }
+    var res = new HashSet<int>();
+    for (int i = 2; i < dp.Length; i++)
+    {
+        if (!dp[i]) continue;
+        res.Add(i);
+    }
+    if (!res.Contains(n))
+    {
+
+    }
+    return res;
+}
