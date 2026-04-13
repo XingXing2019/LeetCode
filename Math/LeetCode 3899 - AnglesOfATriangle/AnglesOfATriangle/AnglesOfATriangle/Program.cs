9ece2c5637cce1bd2bcd7d2a540bc91a9f448f5d
Xing <x.xing.unsw@gmail.com>
2026-04-13 20:58:43 +1000

    init

---
+++ Math/LeetCode 3899 - AnglesOfATriangle/AnglesOfATriangle/AnglesOfATriangle/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] sides = { 3, 4, 5 };
+Console.WriteLine(InternalAngles(sides));
+
+double[] InternalAngles(int[] sides)
+{
+    Array.Sort(sides);
+    if (sides[0] + sides[1] <= sides[2]) return Array.Empty<double>();
+    var res = new double[3];
+    for (int i = 0; i < 3; i++)
+    {
+        var a = sides[i];
+        var b = i == 0 ? sides[1] : sides[0];
+        var c = i == 2 ? sides[1] : sides[2];
+        var cos = ((double)b * b + c * c - a * a) / (2 * b * c);
+        res[i] = Math.Acos(cos) * (180 / Math.PI);
+    }
+    return res.OrderBy(x => x).ToArray();
+}
