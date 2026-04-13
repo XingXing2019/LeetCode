5aa3bb34468eff80f79f70c83f79a4f457136c98
Xing <x.xing.unsw@gmail.com>
2026-04-13 20:42:36 +1000

    init

---
+++ Graph/LeetCode 3898 - FindTheDegreeOfEachVertex/FindTheDegreeOfEachVertex/FindTheDegreeOfEachVertex/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] FindDegrees(int[][] matrix)
+{
+    var res = new int[matrix.Length];
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        for (int j = 0; j < matrix[i].Length; j++)
+        {
+            if (matrix[i][j] == 0) continue;
+            res[i]++;
+        }
+    }
+    return res;
+}
