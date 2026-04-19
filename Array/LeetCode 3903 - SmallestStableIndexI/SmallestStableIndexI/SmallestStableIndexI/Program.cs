2d4500f0aedef8eb696e7cc2f0b3ab00e75b433f
Xing <x.xing.unsw@gmail.com>
2026-04-19 16:38:50 +1000

    init

---
+++ Array/LeetCode 3903 - SmallestStableIndexI/SmallestStableIndexI/SmallestStableIndexI/Program.cs
@@ -0,0 +1,21 @@
+﻿int FirstStableIndex(int[] nums, int k)
+{
+    int[] prefix = new int[nums.Length];
+    int[] suffix = new int[nums.Length];
+    int min = int.MaxValue, max = int.MinValue;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        max = Math.Max(max, nums[i]);
+        min = Math.Min(min, nums[^(i + 1)];
+        prefix[i] = max;
+        suffix[^(i + 1)] = min;
+    }
+    var res = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var score = prefix[i] - suffix[i];
+        if (score > k) continue;
+        res++;
+    }
+    return res;
+}
