58f20e7548bcb69ea88af2c4815178840ae4374a
Xing <x.xing.unsw@gmail.com>
2026-04-28 20:30:13 +1000

    init

---
+++ Array/LeetCode 3909 - CompareSumsOfBitonicParts/CompareSumsOfBitonicParts/CompareSumsOfBitonicParts/Program.cs
@@ -0,0 +1,13 @@
+﻿int CompareBitonicSums(int[] nums)
+{
+    long increase = nums[0];
+    long decrease = nums.Max();
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] > nums[i - 1])
+            increase += nums[i];
+        else
+            decrease += nums[i];
+    }
+    return increase > decrease ? 0 : increase < decrease ? 1 : -1;
+}
