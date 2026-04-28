e5ca5e39ae8a07884790bc9dc50754557fb981dd
Xing <x.xing.unsw@gmail.com>
2026-04-28 20:26:11 +1000

    init

---
+++ Math/LeetCode 3908 - ValidDigitNumber/ValidDigitNumber/ValidDigitNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 107, x = 7;
+Console.WriteLine(ValidDigit(n, x));
+
+bool ValidDigit(int n, int x)
+{
+    var has = false;
+    while (n / 10 != 0)
+    {
+        has |= n % 10 == x;
+        n /= 10;
+    }
+    return has && n != x;
+}
