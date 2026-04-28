1531f4c3dc4bd2702da98636bf132f8eccd9b5c7
Xing <x.xing.unsw@gmail.com>
2026-04-28 20:58:45 +1000

    init

---
+++ Greedy/LeetCode 3914 - MinimumOperationsToMakeArrayNonDecreasing/MinimumOperations/MinimumOperations/Program.cs
@@ -0,0 +1,14 @@
+﻿long MinOperations(int[] nums)
+{
+    var res = 0L;
+    var increase = 0;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        nums[i] += increase;
+        var diff = Math.Max(0, nums[i - 1] - nums[i]);
+        res += diff;
+        increase += diff;
+        nums[i] += diff;
+    }
+    return res;
+}
