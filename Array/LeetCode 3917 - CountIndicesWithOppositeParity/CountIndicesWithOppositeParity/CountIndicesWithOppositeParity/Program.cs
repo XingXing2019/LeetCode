992e65d2fb26c3fb83b8324800c182a3165aa191
Xing <x.xing.unsw@gmail.com>
2026-05-03 20:43:07 +1000

    init

---
+++ Array/LeetCode 3917 - CountIndicesWithOppositeParity/CountIndicesWithOppositeParity/CountIndicesWithOppositeParity/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] CountOppositeParity(int[] nums)
+{
+    int odd = 0, even = 0;
+    var suffix = new int[nums.Length][];
+    var res = new int[nums.Length];
+    for (int i = nums.Length - 1; i >= 0; i--)
+    {
+        suffix[i] = new[] { even, odd };
+        res[i] = suffix[i][(nums[i] % 2) ^ 1];
+        if (nums[i] % 2 == 0) even++;
+        else odd++;
+    }
+    return res;
+}
---
+++ Math/LeetCode 3918 - SumOfPrimesBetweenNumberAndItsReverse/SumOfPrimes/SumOfPrimes/Program.cs
@@ -0,0 +1,37 @@
+﻿var n = 13;
+Console.WriteLine(SumOfPrimesInRange(n));
+
+int SumOfPrimesInRange(int n)
+{
+    var r = int.Parse(string.Join("", n.ToString().Reverse()));
+    int min = Math.Min(n, r), max = Math.Max(n, r);
+    var primes = GetPrimes(max);
+    var res = 0;
+    for (int i = 0; i < primes.Count; i++)
+    {
+        if (primes[i] < min) continue;
+        res += primes[i];
+    }
+    return res;
+}
+
+List<int> GetPrimes(int n)
+{
+    var dp = new bool[n + 1];
+    Array.Fill(dp, true);
+    for (int i = 2; i < dp.Length; i++)
+    {
+        if (!dp[i]) continue;
+        for (int j = 2; i * j < dp.Length; j++)
+        {
+            dp[i * j] = false;
+        }
+    }
+    var res = new List<int>();
+    for (int i = 2; i < dp.Length; i++)
+    {
+        if (!dp[i]) continue;
+        res.Add(i);
+    }
+    return res;
+}
