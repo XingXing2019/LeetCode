57cc679a77b652602a416fd69e75cba723c544b5
Xing <x.xing.unsw@gmail.com>
2026-05-10 18:28:09 +1000

    init

---
+++ String/LeetCode 3921 - ScoreValidator/ScoreValidator/ScoreValidator/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] ScoreValidator(string[] events)
+{
+    var res = new int[2];
+    foreach (var e in events)
+    {
+        if (res[1] == 10) return res;
+        if (int.TryParse(e, out var score))
+            res[0] += score;
+        else if (e == "W")
+            res[1]++;
+        else
+            res[0]++;
+    }
+    return res;
+}
