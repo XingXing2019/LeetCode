48f95ae21e3af2f73b1dba896917fe7e0c4359fc
Xing <x.xing.unsw@gmail.com>
2026-05-11 21:21:34 +1000

    init

---
+++ Array/LeetCode 3925 - ConcatenateArrayWithReverse/ConcatenateArrayWithReverse/ConcatenateArrayWithReverse/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] ConcatWithReverse(int[] nums)
+{
+    var res = new int[nums.Length * 2];
+    for (int i = 0; i < res.Length; i++)
+    {
+        var index = i < nums.Length ? i : nums.Length - i % nums.Length - 1;
+        res[i] = nums[index];
+    }
+    return res;
+}
