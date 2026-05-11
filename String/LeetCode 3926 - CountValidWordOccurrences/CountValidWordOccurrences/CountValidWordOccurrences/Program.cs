38f73e9e58db7de7aec4261ee66e3e13dfa2e28c
Xing <x.xing.unsw@gmail.com>
2026-05-11 21:39:43 +1000

    init

---
+++ String/LeetCode 3926 - CountValidWordOccurrences/CountValidWordOccurrences/CountValidWordOccurrences/Program.cs
@@ -0,0 +1,53 @@
+﻿string[] chunks = { "nn--hl", "c-z-" };
+string[] queries = { "hlc-z" };
+Console.WriteLine(CountWordOccurrences(chunks, queries));
+
+int[] CountWordOccurrences(string[] chunks, string[] queries)
+{
+    var s = string.Join("", chunks);
+    var freq = new Dictionary<string, int>();
+    var letters = new List<char>();
+    foreach (var l in s)
+    {
+        if (char.IsLetter(l))
+            letters.Add(l);
+        else if (l == ' ')
+        {
+            if (letters.Count == 0) continue;
+            if (letters[^1] == '-')
+                letters.RemoveAt(letters.Count - 1);
+            Add(freq, letters);
+        }
+        else
+        {
+            if (letters.Count == 0) continue;
+            if (letters[^1] == '-')
+            {
+                letters.RemoveAt(letters.Count - 1);
+                Add(freq, letters);
+                continue;
+            }
+            letters.Add(l);
+        }
+    }
+    if (letters.Count != 0)
+    {
+        if (letters[^1] == '-')
+            letters.RemoveAt(letters.Count - 1);
+        Add(freq, letters);
+    }
+    var res = new int[queries.Length];
+    for (int i = 0; i < queries.Length; i++)
+        res[i] = freq.GetValueOrDefault(queries[i], 0);
+    return res;
+}
+
+void Add(Dictionary<string, int> freq, List<char> letters)
+{
+    if (letters.Count == 0) return;
+    var word = string.Join("", letters);
+    if (!freq.ContainsKey(word))
+        freq[word] = 0;
+    freq[word]++;
+    letters.Clear();
+}
