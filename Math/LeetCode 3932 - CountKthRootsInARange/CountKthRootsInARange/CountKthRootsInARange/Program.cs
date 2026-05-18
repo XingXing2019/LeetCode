23b9c2d85cf5f04b1aa1617538b868208b1f6848
Xing <x.xing.unsw@gmail.com>
2026-05-18 20:39:07 +1000

    init

---
+++ Math/LeetCode 3932 - CountKthRootsInARange/CountKthRootsInARange/CountKthRootsInARange/Program.cs
@@ -0,0 +1,28 @@
+﻿int l = 8, r = 30, k = 2;
+Console.WriteLine(CountKthRoots(l, r, k));
+
+int CountKthRoots(int l, int r, int k)
+{
+    if (k == 1) return r - l + 1;
+    var res = 0;
+    for (int i = 0; i <= r; i++)
+    {
+        var pow = GetPow(i, k, r);
+        if (pow >= l && pow <= r)
+            res++;
+        else if (pow > r)
+            return res;
+    }
+    return res;
+}
+
+int GetPow(int num, int k, int r)
+{
+    var res = 1;
+    for (int i = 0; i < k; i++)
+    {
+        res *= num;
+        if (res > r) return int.MaxValue;
+    }
+    return res;
+}
---
+++ String/LeetCode 3931 - CheckAdjacentDigitDifferences/CheckAdjacentDigitDifferences/CheckAdjacentDigitDifferences/Program.cs
@@ -0,0 +1,9 @@
+﻿bool IsAdjacentDiffAtMostTwo(string s)
+{
+    for (int i = 1; i < s.Length; i++)
+    {
+        if (Math.Abs(s[i] - s[i - 1]) > 2)
+            return false;
+    }
+    return true;
+}
