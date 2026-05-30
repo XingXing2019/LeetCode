a484e607b1aa190e41d5e961cb933a8df7be146f
Xing <x.xing.unsw@gmail.com>
2026-05-30 10:47:10 +1000

    init

---
+++ Array/LeetCode 3936 - MinimumSwapsToMoveZerosToEnd/MinimumSwapsToMoveZerosToEnd/MinimumSwapsToMoveZerosToEnd/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] nums = { 0, 1, 0, 3, 12 };
+Console.WriteLine(MinimumSwaps(nums));
+
+int MinimumSwaps(int[] nums)
+{
+    var res = 0;
+    var sort = nums.OrderByDescending(x => x).ToArray();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] == 0 && sort[i] == 0 || nums[i] != 0 && sort[i] != 0) continue;
+        res++;
+    }
+    return res / 2;
+}
