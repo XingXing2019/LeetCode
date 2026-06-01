d91368b5274ee979780c0b2d8f25d5fdb33af318
Xing <x.xing.unsw@gmail.com>
2026-06-01 20:06:32 +1000

    init

---
+++ String/LeetCode 3941 - PasswordStrength/PasswordStrength/PasswordStrength/Program.cs
@@ -0,0 +1,18 @@
+﻿int PasswordStrength(string password)
+{
+    var res = 0;
+    var set = new HashSet<char>();
+    foreach (var l in password)
+    {
+        if (!set.Add(l)) continue;
+        if (l >= 'a' && l <= 'z')
+            res++;
+        else if (l >= 'A' && l <= 'Z')
+            res += 2;
+        else if (l >= '0' && l <= '9')
+            res += 3;
+        else
+            res += 5;
+    }
+    return res;
+}
