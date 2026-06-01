365abb30cc22736e54f1f7c46604c1170f7f1714
Xing <x.xing.unsw@gmail.com>
2026-06-01 19:55:06 +1000

    init

---
+++ HashTable/LeetCode 3945 - DigitFrequencyScore/DigitFrequencyScore/DigitFrequencyScore/Program.cs
@@ -0,0 +1,13 @@
+﻿int DigitFrequencyScore(int n)
+{
+    var freq = new int[10];
+    while (n != 0)
+    {
+        freq[n % 10]++;
+        n /= 10;
+    }
+    var res = 0;
+    for (int i = 0; i < freq.Length; i++)
+        res += i * freq[i];
+    return res;
+}
