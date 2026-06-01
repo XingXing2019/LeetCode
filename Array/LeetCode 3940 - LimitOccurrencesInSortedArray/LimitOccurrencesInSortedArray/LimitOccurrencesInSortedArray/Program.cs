7cf3d423c5e2501163ca2b5ed14def6085731436
Xing <x.xing.unsw@gmail.com>
2026-06-01 20:02:48 +1000

    init

---
+++ Array/LeetCode 3940 - LimitOccurrencesInSortedArray/LimitOccurrencesInSortedArray/LimitOccurrencesInSortedArray/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] LimitOccurrences(int[] nums, int k)
+{
+    var res = new List<int>();
+    int count = 0, cur = nums[0];
+    foreach (var num in nums)
+    {
+        if (num == cur)
+        {
+            count++;
+            if (count > k) continue;
+            res.Add(cur);
+        }
+        else
+        {
+            count = 1;
+            cur = num;
+            res.Add(cur);
+        }
+    }
+    return res.ToArray();
+}
